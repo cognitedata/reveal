e27dd639097a592827f131a3ee9a980c44afa07d
Svenn-Arne Dragly <dragly@cognite.com>
2019-10-14 12:35:03 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    https: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+};
