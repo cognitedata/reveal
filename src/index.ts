a481bf24e0b3f88ad439f1596846b1b3d3f674e6
Lars Moastuen <lars.moastuen@cognite.com>
2019-10-17 15:57:32 +0200

    Extract loader stuff.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
 import * as THREE from 'three';
-import { setUnion, setDifference } from './utils/setUtils';
-import { loadSector, LoadSectorRequest } from './sector/loadSector';
-import { fetchRequest } from './sector/fetchSector';
-import { parseSectorData } from './sector/parseSectorData';
 import { determineSectors } from './sector/determineSectors';
 import { initializeThreeJsView } from './views/threejs/initializeThreeJsView';
+import { initializeSectorLoader } from './sector/initializeSectorLoader';
 
 function main() {
-  const activeSectorIds = new Set<number>();
-  const activeSectorRequests = new Map<number, LoadSectorRequest>();
-
-  const sectorRoot = fetchSectorMetadata();
-  const { rootGroup, discardSector, consumeSector } = initializeThreeJsView(sectorRoot);
-
   const scene = new THREE.Scene();
-  scene.add(rootGroup);
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   camera.position.z = 4;
   const renderer = new THREE.WebGLRenderer();
@@ -36,6 +26,11 @@ function main() {
     renderer.render(scene, camera);
   };
 
+  const sectorRoot = fetchSectorMetadata();
+  const { rootGroup, discardSector, consumeSector } = initializeThreeJsView(sectorRoot);
+  const activateSectors = initializeSectorLoader(discardSector, consumeSector);
+  scene.add(rootGroup);
+
   renderer.domElement.onmousedown = (downEvent) => {
     const {x0, y0} = { x0: downEvent.x, y0: downEvent.y };
     renderer.domElement.onmousemove = async (moveEvent) => {
@@ -44,28 +39,7 @@ function main() {
       camera.position.y = dy / 100;
       
       const wantedSectorIds = await determineSectors(camera);
-      const activeOrInFlight = setUnion(activeSectorIds, new Set<number>(activeSectorRequests.keys()));
-      const newSectorIds = setDifference(wantedSectorIds, activeOrInFlight);
-      const discardedSectorIds = setDifference(activeOrInFlight, wantedSectorIds);
-      console.log("Wanted", wantedSectorIds);
-      console.log("Discarded", discardedSectorIds);
-      console.log("New", newSectorIds);
-
-      for (const id of discardedSectorIds) {
-        const request = activeSectorRequests.get(id);
-        activeSectorRequests.delete(id);
-        discardSector(id, request);
-      }
-
-      for (const id of newSectorIds) {
-        const consumeSectorAndDeleteRequest = (sector) => {
-          activeSectorRequests.delete(id);
-          consumeSector(id, sector);
-        };
-
-        const request = loadSector(id, fetchRequest, parseSectorData, consumeSectorAndDeleteRequest);
-        activeSectorRequests.set(id, request);
-      }
+      activateSectors(wantedSectorIds);
     };    
   };
   renderer.domElement.onmouseup = (upEvent) => {
---
+++ src/sector/initializeSectorLoader.ts
@@ -0,0 +1,34 @@
+import { setUnion, setDifference } from '../utils/setUtils';
+import { loadSector, LoadSectorRequest } from './loadSector';
+import { fetchRequest } from './fetchSector';
+import { parseSectorData } from './parseSectorData';
+import { Sector } from './Sector';
+
+// TODO 20191017 larsmoa: Cleanup in the callbacks here and align with definitions in loadSector.ts
+export function initializeSectorLoader(discardSector: (sectorId: number, request: LoadSectorRequest) => void, consumeSector: (sectorId: number, sector: Sector) => void) {
+  const activeSectorIds = new Set<number>();
+  const activeSectorRequests = new Map<number, LoadSectorRequest>();
+  
+  function activateSectors(wantedSectorIds: Set<number>) {
+    const activeOrInFlight = setUnion(activeSectorIds, new Set<number>(activeSectorRequests.keys()));
+    const newSectorIds = setDifference(wantedSectorIds, activeOrInFlight);
+    const discardedSectorIds = setDifference(activeOrInFlight, wantedSectorIds);
+    console.log("Wanted", wantedSectorIds);
+    console.log("Discarded", discardedSectorIds);
+    console.log("New", newSectorIds);
+    for (const id of discardedSectorIds) {
+      const request = activeSectorRequests.get(id);
+      activeSectorRequests.delete(id);
+      discardSector(id, request);
+    }
+    for (const id of newSectorIds) {
+      const consumeSectorAndDeleteRequest = (sector) => {
+        activeSectorRequests.delete(id);
+        consumeSector(id, sector);
+      };
+      const request = loadSector(id, fetchRequest, parseSectorData, consumeSectorAndDeleteRequest);
+      activeSectorRequests.set(id, request);
+    }
+  }
+  return activateSectors;
+}
