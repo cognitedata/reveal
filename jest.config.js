880c0c17aacf46cffd38817998931967d6ea18d7
Lars Moastuen <lars.moastuen@cognite.com>
2019-10-17 14:41:46 +0200

    Get ready for jest.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec|Test))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)': '<rootDir>/src/$1',
+  }
+};
