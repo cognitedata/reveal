a4a98b180ede54ad69a851689c76b97b79dcfe05
Lars Moastuen <lars.moastuen@cognite.com>
2019-10-18 16:12:23 +0200

    Add more utilities for waiting in tests.

--- src/__tests__/wait.ts
+++ src/__tests__/wait.ts
@@ -1,5 +1,13 @@
 export async function waitUntill(condition: () => boolean): Promise<void> {
   while (!condition()) {
-    await new Promise<void>(resolve => setTimeout(resolve, 0));
+    await yieldProcessing();
   }
+}
+
+export async function yieldProcessing(): Promise<void> {
+  await new Promise<void>(resolve => setImmediate(resolve));
+}
+
+export async function sleep(milliseconds: number): Promise<void> {
+  await new Promise<void>(resolve => setTimeout(resolve, milliseconds));
 }
