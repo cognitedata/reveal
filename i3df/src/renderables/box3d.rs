4b7050a590a40d28ff2af3dbd16f823c6be455cb
Svenn-Arne Dragly <dragly@cognite.com>
2019-12-06 09:53:53 +0100

    Improve support for primitives (#35)
    * Make it possible to retrieve attributes as a JS Map
    * Convert attributes to camel case for JS
    * Push vector data to correct attributes
    * Add support for passing Matrix4 and add instance matrix to Box3D, Circle, TorusSegment, Quad, InstancedMesh and Nut

--- i3df/src/renderables/box3d.rs
+++ i3df/src/renderables/box3d.rs
@@ -1,7 +1,16 @@
+use crate::{Matrix4, Rotation3, Translation3, Vector3};
 use crate::renderables::{Box3D, GeometryCollection, PrimitiveCollections, ToRenderables};
 
 impl ToRenderables for crate::Box3D {
     fn to_renderables(&self, collections: &mut PrimitiveCollections) {
+
+        let translation_matrix = Translation3::from(self.center());
+        let first_rotation = Rotation3::from_axis_angle(&Vector3::z_axis(), self.rotation_angle);
+        let second_rotation = Rotation3::rotation_between(&Vector3::z_axis(), &self.normal.into()).unwrap();
+        let scale_matrix = Matrix4::new_nonuniform_scaling(&self.delta());
+
+        let instance_matrix = Matrix4::from(translation_matrix) * Matrix4::from(second_rotation) * Matrix4::from(first_rotation) * scale_matrix;
+
         collections.box_collection.push(Box3D {
             node_id: self.node_id + 1,
             tree_index: self.tree_index,
@@ -11,6 +20,7 @@ impl ToRenderables for crate::Box3D {
             normal: self.normal.into(),
             rotation_angle: self.rotation_angle,
             delta: self.delta(),
+            instance_matrix,
         });
 
     }
