e7b10fea8d729a67f7eb5a1b3cecd445ebe24aa0
Svenn-Arne Dragly <dragly@cognite.com>
2019-12-16 17:01:08 +0100

    Fix duplicate info in createCache

--- viewer/src/models/createCache.ts
+++ viewer/src/models/createCache.ts
@@ -2,36 +2,6 @@
  * Copyright 2019 Cognite AS
  */
 
-export type RequestDelegate<T_ID, T> = (id: T_ID) => Promise<T>;
-
-export interface SimpleCache<T_ID, T> {
-  request: RequestDelegate<T_ID, T>;
-  clearCache: () => void;
-}
-
-export function createSimpleCache<T_ID, T>(request: RequestDelegate<T_ID, T>): SimpleCache<T_ID, T> {
-  const results = new Map<T_ID, Promise<T>>();
-
-  const requestCached = async (id: T_ID) => {
-    const existing = results.get(id);
-    if (existing) {
-      return existing;
-    }
-    const result: Promise<T> = request(id);
-    results.set(id, result);
-    return result;
-  };
-
-  const clearCache = () => {
-    results.clear();
-  };
-
-  return {
-    request: requestCached,
-    clearCache
-  };
-}
-
 type RequestDelegate<T_ID, T> = (id: T_ID) => Promise<T>;
 
 interface SimpleCache<T_ID, T> {
