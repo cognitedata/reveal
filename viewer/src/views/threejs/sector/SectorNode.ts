4943d1277dd6be4dda4264df7c21de717ca46c7b
Lars Moastuen <lars.moastuen@cognite.com>
2020-01-21 00:35:53 +0100

    SectorNode now supports overriding determineSectors.

--- viewer/src/views/threejs/sector/SectorNode.ts
+++ viewer/src/views/threejs/sector/SectorNode.ts
@@ -3,11 +3,11 @@
  */
 
 import * as THREE from 'three';
-import { SectorModelTransformation, SectorMetadata, SectorScene } from '../../../models/sector/types';
-import { vec3, mat4 } from 'gl-matrix';
-import { fromThreeVector3, fromThreeMatrix } from '../utilities';
-import { determineSectors } from '../../../models/sector/determineSectors';
+import { SectorModelTransformation, SectorScene } from '../../../models/sector/types';
+import { fromThreeVector3, fromThreeMatrix, toThreeMatrix4 } from '../utilities';
 import { SectorActivator } from '../../../models/sector/initializeSectorLoader';
+import { DetermineSectorsDelegate } from '../../../models/sector/delegates';
+import { vec3, mat4 } from 'gl-matrix';
 
 export class SectorNode extends THREE.Group {
   public readonly sectorId: number;
@@ -28,40 +28,50 @@ const updateVars = {
 export class RootSectorNode extends SectorNode {
   public readonly modelTransformation: SectorModelTransformation;
 
-  private readonly simpleActivator: SectorActivator;
-  private readonly detailedActivator: SectorActivator;
+  private determineSectorsCb: DetermineSectorsDelegate;
+  private simpleActivator: SectorActivator;
+  private detailedActivator: SectorActivator;
   private readonly sectorScene: SectorScene;
   private readonly previousCameraMatrix = new THREE.Matrix4();
 
   constructor(
     sectorScene: SectorScene,
     modelTransformation: SectorModelTransformation,
+    determineSectors: DetermineSectorsDelegate,
     simpleActivator: SectorActivator,
     detailedActivator: SectorActivator
   ) {
     super(0, '/');
     this.name = 'Sector model';
     this.sectorScene = sectorScene;
+    this.determineSectorsCb = determineSectors;
     this.simpleActivator = simpleActivator;
     this.detailedActivator = detailedActivator;
     this.modelTransformation = modelTransformation;
     // Ensure camera matrix is unequal on first frame
     this.previousCameraMatrix.elements[0] = Infinity;
+    // Apply model matrix to this model
+    this.applyMatrix(toThreeMatrix4(modelTransformation.modelMatrix));
+  }
+
+  set determineSectors(determineSectors: DetermineSectorsDelegate) {
+    this.determineSectorsCb = determineSectors;
+  }
+
+  get determineSectors() {
+    return this.determineSectorsCb;
   }
 
   public async update(camera: THREE.PerspectiveCamera): Promise<boolean> {
     let needsRedraw = false;
     const { cameraPosition, cameraModelMatrix, projectionMatrix } = updateVars;
     if (!this.previousCameraMatrix.equals(camera.matrixWorld)) {
-      // Need to trigger reload of data
-      // camera.updateMatrix();
-      // camera.updateMatrixWorld();
       camera.matrixWorldInverse.getInverse(camera.matrixWorld);
 
       fromThreeVector3(cameraPosition, camera.position, this.modelTransformation);
       fromThreeMatrix(cameraModelMatrix, camera.matrixWorld, this.modelTransformation);
       fromThreeMatrix(projectionMatrix, camera.projectionMatrix);
-      const wantedSectors = await determineSectors({
+      const wantedSectors = await this.determineSectorsCb({
         scene: this.sectorScene,
         cameraFov: camera.fov,
         cameraPosition,
