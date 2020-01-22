359a4c7d6b4875a9b5d26b2fe1a4dd00a1b70b01
Svenn-Arne Dragly <dragly@cognite.com>
2020-01-22 11:12:24 +0100

    Remove old import

--- viewer/src/threejs.ts
+++ viewer/src/threejs.ts
@@ -7,4 +7,3 @@ export { SectorNode } from './views/threejs/sector/SectorNode';
 export { createThreeJsPointCloudNode } from './views/threejs/pointcloud/createThreeJsPointCloudNode';
 export { createThreeJsSectorNode } from './views/threejs/sector/createThreeJsSectorNode';
 export { toThreeVector3 } from './views/threejs/utilities';
-export { suggestCameraConfig } from './views/threejs/camera';
