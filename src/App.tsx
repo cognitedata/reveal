1df0964a3cb03b6c9d63eacf06e78d643f397efe
Evan Charlton <evan.charlton@cognite.com>
2020-01-31 11:16:44 +0100

    Show version information on the landing page (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>This is v{process.env.REACT_APP_VERSION_NAME || '0.0.0'} :-)</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cog.link/fas"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about how this is hosted
         </a>
       </header>
     </div>
