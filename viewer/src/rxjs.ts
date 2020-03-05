6561ce124995e1100bd2afbdeae49d5ad02f8838
Svenn-Arne Dragly <dragly@cognite.com>
2020-03-05 15:38:16 +0100

    WIP rxjs catch wanted before inserting

--- viewer/src/rxjs.ts
+++ viewer/src/rxjs.ts
@@ -2,7 +2,7 @@
  * Copyright 2020 Cognite AS
  */
 
-import { Observable, from, of, partition, merge, Subject, pipe, GroupedObservable } from 'rxjs';
+import { Observable, from, of, partition, merge, Subject, pipe, GroupedObservable, OperatorFunction, empty } from 'rxjs';
 import { createLocalCadModel } from './datasources/local/cad/createLocalCadModel';
 import {
   switchMap,
@@ -16,12 +16,17 @@ import {
   mergeMap,
   reduce,
   filter,
-  scan
+  scan,
+  publish,
+  delay,
+  startWith,
+  withLatestFrom
 } from 'rxjs/operators';
 import { SectorQuads, WantedSectors, Sector } from './models/cad/types';
 
 enum Lod {
-  Unwanted,
+  Discarded,
+  Hidden,
   Simple,
   Detailed
 }
@@ -55,11 +60,11 @@ interface ParsedSectorDetailed {
   lod: Lod;
 }
 
-type FinalSector = ParsedSectorSimple | ParsedSectorDetailed;
+type FinalSector = WantedSector | ParsedSectorSimple | ParsedSectorDetailed;
 
 export async function testme() {
   const model = await createLocalCadModel('./3d-data/ivar_aasen/ivar-aasen-2020-02-03-self-contained-fix/');
-  const generator = Observable.create(function(observer: any) {
+  const wantedSectors: Observable<WantedSector[]> = Observable.create(function(observer: any) {
     console.log('Creating generator');
     const randomLod = () => {
       switch (Math.floor(Math.random() * 3)) {
@@ -68,92 +73,133 @@ export async function testme() {
         case 1:
           return Lod.Detailed;
         case 2:
-          return Lod.Unwanted;
+          return Lod.Discarded;
         default:
-          return Lod.Unwanted;
+          return Lod.Discarded;
       }
     };
-    setInterval(() => {
+    setTimeout(() => {
       const wanted = [
-        { id: 0, lod: randomLod() },
-        { id: 1, lod: randomLod() },
-        { id: 2, lod: randomLod() },
-        { id: 3, lod: randomLod() },
-        { id: 4, lod: randomLod() }
+        { id: 0, lod: Lod.Simple },
+        { id: 1, lod: Lod.Simple },
+        { id: 2, lod: Lod.Simple },
+        { id: 3, lod: Lod.Simple },
+        { id: 4, lod: Lod.Simple }
+      ];
+      observer.next(wanted);
+    }, 100);
+    setTimeout(() => {
+      const wanted = [
+        { id: 0, lod: Lod.Detailed },
+        { id: 1, lod: Lod.Detailed },
+        { id: 2, lod: Lod.Detailed },
+        { id: 3, lod: Lod.Detailed },
+        { id: 4, lod: Lod.Detailed }
       ];
       observer.next(wanted);
     }, 3000);
   });
-  const wantedSectors = generator.pipe(flatMap((sectors: WantedSector[]) => from(sectors)));
-  const simple = new Subject<WantedSector>();
-  const detailed = new Subject<WantedSector>();
-  const unwanted = new Subject<WantedSector>();
 
-  const lodSubjectMap = new Map<Lod, Subject<WantedSector>>([
-    [Lod.Simple, simple],
-    [Lod.Detailed, simple],
-    [Lod.Unwanted, unwanted]
-  ]);
 
   const distinctUntilLodChanged = pipe(
     groupBy((sector: WantedSector) => sector.id),
-    mergeMap((group: GroupedObservable<number, WantedSector>) =>
-      group.pipe(
-        distinctUntilKeyChanged('lod')
+    mergeMap((group: GroupedObservable<number, WantedSector>) => group.pipe(distinctUntilKeyChanged('lod')))
+  );
+
+  const hideAndScheduleDiscard: OperatorFunction<WantedSector, WantedSector> = pipe(
+    flatMap((sector: WantedSector) => of(sector).pipe(delay(1000), startWith({ ...sector, lod: Lod.Hidden })))
+  );
+
+  const fetchSimple = flatMap(async (sector: WantedSector) => {
+    const fetchedData = await model.fetchSectorSimple(sector.id);
+    return {
+      id: sector.id,
+      lod: sector.lod,
+      data: fetchedData
+    };
+  });
+
+  const parseSimple = flatMap(async (sector: FetchedSectorSimple) => {
+    const parsedData = await model.parseSimple(sector.id, sector.data);
+    return {
+      id: sector.id,
+      lod: sector.lod,
+      data: parsedData
+    };
+  });
+
+  const fetchDetailed = flatMap(async (sector: WantedSector) => {
+    const fetchedData = await model.fetchSectorDetailed(sector.id);
+    return {
+      id: sector.id,
+      lod: sector.lod,
+      data: fetchedData
+    };
+  });
+
+  const parseDetailed = flatMap(async (sector: FetchedSectorDetailed) => {
+    const parsedData = await model.parseDetailed(sector.id, sector.data);
+    return {
+      id: sector.id,
+      lod: sector.lod,
+      data: parsedData
+    };
+  });
+
+  const getFinalSectorByLod: OperatorFunction<WantedSector, FinalSector> = pipe(
+    publish(multicast =>
+      merge(
+        multicast.pipe(
+          filter((sector: WantedSector) => sector.lod === Lod.Discarded),
+          hideAndScheduleDiscard
+        ),
+        multicast.pipe(
+          filter((sector: WantedSector) => sector.lod === Lod.Simple),
+          fetchSimple,
+          parseSimple
+        ),
+        multicast.pipe(
+          filter((sector: WantedSector) => sector.lod === Lod.Detailed),
+          fetchDetailed,
+          delay(5000),
+          parseDetailed
+        )
       )
     )
   );
 
-  const handleWantedSectors = pipe(
+  const handleWantedSectors: OperatorFunction<WantedSector, FinalSector> = pipe(
     distinctUntilLodChanged,
-    scan((sectors: Map<number, WantedSector>, sector: WantedSector) => {
-      sectors.set(sector.id, sector);
-      return sectors;
-    }, new Map()),
+    getFinalSectorByLod
   );
 
   // TODO consider when this subscription should be cancelled
-  wantedSectors.pipe(handleWantedSectors).subscribe((sectors: Map<number, WantedSector>) => {
-    console.log("-------- Results: ---------");
-    for (const [id, sector] of sectors) {
-      console.log(id, sector.lod);
-    }
-  });
-  // const [simple, detailed] = partition(wantedSectors, (sector: WantedSector) => sector.lod === Lod.Simple);
-  // const simpleParsed = simple.pipe(
-  // flatMap(async (sector: WantedSector) => ({
-  // id: sector.id,
-  // data: await model.fetchSectorSimple(sector.id),
-  // lod: sector.lod
-  // })),
-  // flatMap(async (sector: FetchedSectorSimple) => ({
-  // id: sector.id,
-  // data: await model.parseSimple(sector.id, sector.data),
-  // lod: sector.lod
-  // }))
-  // );
-  // const detailedParsed = detailed.pipe(
-  // flatMap(async (sector: WantedSector) => ({
-  // id: sector.id,
-  // data: await model.fetchSectorDetailed(sector.id),
-  // lod: sector.lod
-  // })),
-  // flatMap(async (sector: FetchedSectorDetailed) => ({
-  // id: sector.id,
-  // data: await model.parseDetailed(sector.id, sector.data),
-  // lod: sector.lod
-  // }))
-  // );
-  // const pipeline = merge(simpleParsed, detailedParsed);
-  //// .pipe(
-  //// distinctUntilChanged((previous: FinalSector, current: FinalSector) => previous.id === current.id)
-  //// );
-  // const sectors = new Map<number, FinalSector>();
-  // pipeline.subscribe((x: FinalSector) => {
-  // sectors.set(x.id, x);
-  // console.log('Got', x.id, x.lod);
-  // for (const [id, sector] of sectors) {
-  // console.log(id, sector.lod);
-  // }
-  // });
+  wantedSectors
+    .pipe(
+      publish(multicast =>
+        multicast.pipe(
+          flatMap((sectors: WantedSector[]) => from(sectors)),
+          handleWantedSectors,
+          withLatestFrom(multicast),
+          flatMap(([loaded, wanted]) => {
+            for (const wantedSector of wanted) {
+              if (loaded.id === wantedSector.id && loaded.lod === wantedSector.lod) {
+                return of(loaded);
+              }
+            }
+            return empty();
+          }),
+          scan((sectors: Map<number, FinalSector>, sector: WantedSector) => {
+            sectors.set(sector.id, sector);
+            return sectors;
+          }, new Map())
+        )
+      ),
+    )
+    .subscribe((sectors: Map<number, WantedSector>) => {
+      console.log('-------- Results: ---------');
+      for (const [id, sector] of sectors) {
+        console.log(id, sector.lod);
+      }
+    });
 }
