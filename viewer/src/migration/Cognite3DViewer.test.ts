24cb5cd1e45fe29cedb5372dcaa19f62953a7514
Lars Moastuen <lars.moastuen@cognite.com>
2020-03-17 13:02:50 +0100

    Fix test.

--- viewer/src/migration/Cognite3DViewer.test.ts
+++ viewer/src/migration/Cognite3DViewer.test.ts
@@ -3,12 +3,14 @@
  */
 
 import { Cognite3DViewer } from './Cognite3DViewer';
+import { CogniteClient } from '@cognite/sdk';
 
 describe('Cognite3DViewer', () => {
+  const sdk = new CogniteClient({ appId: 'cognite.reveal.unittest' });
   test('constructor throws error when unsupported options are set', () => {
-    expect(() => new Cognite3DViewer({ enableCache: true })).toThrowError();
-    expect(() => new Cognite3DViewer({ enableCache: false, logMetrics: true })).toThrowError();
-    expect(() => new Cognite3DViewer({ enableCache: false, viewCube: 'topleft' })).toThrowError();
-    expect(() => new Cognite3DViewer({ enableCache: false })).toThrowError();
+    expect(() => new Cognite3DViewer({ sdk, enableCache: true })).toThrowError();
+    expect(() => new Cognite3DViewer({ sdk, enableCache: false, logMetrics: true })).toThrowError();
+    expect(() => new Cognite3DViewer({ sdk, enableCache: false, viewCube: 'topleft' })).toThrowError();
+    expect(() => new Cognite3DViewer({ sdk, enableCache: false })).toThrowError();
   });
 });
