49bdcfae94467929d54a7ce9bd0db246826c3858
Lars Moastuen <lars.moastuen@cognite.com>
2020-03-17 19:29:07 +0100

    Add a UI for loading one or more models

--- examples/src/migration.ts
+++ examples/src/migration.ts
@@ -3,17 +3,14 @@
  */
 import * as reveal_migration from '@cognite/reveal/migration';
 import { CogniteClient } from '@cognite/sdk';
+import dat from 'dat.gui';
 
 async function main() {
   const urlParams = new URL(location.href).searchParams;
   const project = urlParams.get('project');
-  const modelIdStr = urlParams.get('modelId');
-  const revisionIdStr = urlParams.get('revisionId');
-  if (!project || !modelIdStr || !revisionIdStr) {
-    throw new Error('Must provide "project", "modelId" and "revisionId" as URL parameters');
+  if (!project) {
+    throw new Error('Must provide "project"as URL parameter');
   }
-  const modelId = Number.parseInt(modelIdStr, 10);
-  const revisionId = Number.parseInt(revisionIdStr, 10);
   const domElement = document.getElementById('canvas-container')!;
 
   // Login
@@ -21,14 +18,46 @@ async function main() {
   client.loginWithOAuth({ project });
   await client.authenticate();
 
-  // Load and show model
-  const model = await reveal_migration.createCognite3DModel(modelId, revisionId, client);
+  // Prepare viewer
   const viewer = new reveal_migration.Cognite3DViewer({ sdk: client, domElement });
-  const migrationModel = await viewer.addModel(model);
-  migrationModel.renderHints = { showSectorBoundingBoxes: true };
-  viewer.fitCameraToModel(model);
-
   (window as any).viewer = viewer;
+
+  async function addModel(modelId: number, revisionId: number) {
+    const model = await reveal_migration.createCognite3DModel(modelId, revisionId, client);
+    await viewer.addModel(model);
+    viewer.fitCameraToModel(model);
+    models.push(model);
+  }
+
+  // Add GUI for loading models and such
+  const models: reveal_migration.Cognite3DModel[] = [];
+  const guiState = { modelId: 0, revisionId: 0, showSectorBoundingBoxes: false };
+  function applySettingsToModels() {
+    models.forEach(m => {
+      m.renderHints = { ...m.renderHints, showSectorBoundingBoxes: guiState.showSectorBoundingBoxes };
+    });
+  }
+  const guiActions = {
+    addModel: () => addModel(guiState.modelId, guiState.revisionId)
+  };
+  const gui = new dat.GUI();
+  const settingsGui = gui.addFolder('settings');
+  settingsGui
+    .add(guiState, 'showSectorBoundingBoxes')
+    .name('Show bounding boxes')
+    .onChange(applySettingsToModels);
+  gui.add(guiState, 'modelId').name('Model ID');
+  gui.add(guiState, 'revisionId').name('Revision ID');
+  gui.add(guiActions, 'addModel').name('Load model');
+
+  // Load model if provided by URL
+  const modelIdStr = urlParams.get('modelId');
+  const revisionIdStr = urlParams.get('revisionId');
+  if (modelIdStr && revisionIdStr) {
+    const modelId = Number.parseInt(modelIdStr, 10);
+    const revisionId = Number.parseInt(revisionIdStr, 10);
+    addModel(modelId, revisionId);
+  }
 }
 
 main();
