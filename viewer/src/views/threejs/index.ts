cb28a31caeba086cc55f19160bbb52b32526d9df
Joachim Bjørne <joachim.bjorne@cognite.com>
2020-03-17 16:08:09 +0100

    Moved worldToViewport to reaveal_threejs

--- viewer/src/views/threejs/index.ts
+++ viewer/src/views/threejs/index.ts
@@ -9,5 +9,6 @@ export { SectorNode } from './cad/SectorNode';
 export { createThreeJsPointCloudNode } from './pointcloud/createThreeJsPointCloudNode';
 export { RootSectorNode } from './cad/RootSectorNode';
 
+export { worldToViewport } from './worldToViewport';
 export { toThreeVector3, fitCameraToBoundingBox, toThreeMatrix4, toThreeJsBox3 } from './utilities';
 export { SsaoEffect, SsaoPassType } from './post-processing/ssao';
