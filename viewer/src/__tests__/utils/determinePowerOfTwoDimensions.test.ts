1c066b038021643e85f906031e62f202a75027cf
Lars Moastuen <lars.moastuen@cognite.com>
2020-03-21 17:50:09 +0100

    Typo.

--- viewer/src/__tests__/utils/determinePowerOfTwoDimensions.test.ts
+++ viewer/src/__tests__/utils/determinePowerOfTwoDimensions.test.ts
@@ -2,15 +2,15 @@
  * Copyright 2020 Cognite AS
  */
 
-import { determineTextureDimensions } from '../../utils/determinePowerOfTwoDimensions';
+import { determinePowerOfTwoDimensions } from '../../utils/determinePowerOfTwoDimensions';
 
 test('determinePowerOfTwoDimensions', () => {
-  expect(determineTextureDimensions(0)).toEqual({ width: 1, height: 1 });
-  expect(determineTextureDimensions(1)).toEqual({ width: 1, height: 1 });
-  expect(determineTextureDimensions(2)).toEqual({ width: 2, height: 1 });
-  expect(determineTextureDimensions(3)).toEqual({ width: 2, height: 2 });
-  expect(determineTextureDimensions(1025)).toEqual({ width: 64, height: 32 });
-  expect(determineTextureDimensions(2048)).toEqual({ width: 64, height: 32 });
-  expect(determineTextureDimensions(2049)).toEqual({ width: 64, height: 64 });
-  expect(determineTextureDimensions(4096 * 4096 - 1)).toEqual({ width: 4096, height: 4096 });
+  expect(determinePowerOfTwoDimensions(0)).toEqual({ width: 1, height: 1 });
+  expect(determinePowerOfTwoDimensions(1)).toEqual({ width: 1, height: 1 });
+  expect(determinePowerOfTwoDimensions(2)).toEqual({ width: 2, height: 1 });
+  expect(determinePowerOfTwoDimensions(3)).toEqual({ width: 2, height: 2 });
+  expect(determinePowerOfTwoDimensions(1025)).toEqual({ width: 64, height: 32 });
+  expect(determinePowerOfTwoDimensions(2048)).toEqual({ width: 64, height: 32 });
+  expect(determinePowerOfTwoDimensions(2049)).toEqual({ width: 64, height: 64 });
+  expect(determinePowerOfTwoDimensions(4096 * 4096 - 1)).toEqual({ width: 4096, height: 4096 });
 });
