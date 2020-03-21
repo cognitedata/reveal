db0434c0a0a573fdf71c6449697a1f40b977f647
Lars Moastuen <lars.moastuen@cognite.com>
2020-03-21 17:48:04 +0100

    Add utility for computing power-of-two texture size to hold N elements.

---
+++ viewer/src/utils/determinePowerOfTwoDimensions.ts
@@ -0,0 +1,18 @@
+/*!
+ * Copyright 2020 Cognite AS
+ */
+
+/**
+ * Computes minimal power-of-two width and height that holds at least the number of elements provided.
+ * This is useful to compute texture sizes.
+ */
+export function determineTextureDimensions(elementCount: number): { width: number; height: number } {
+  const width = Math.max(1, ceilToPowerOfTwo(Math.sqrt(elementCount)));
+  const height = Math.max(1, ceilToPowerOfTwo(elementCount / width));
+  return { width, height };
+}
+
+const log2 = Math.log(2);
+function ceilToPowerOfTwo(v: number): number {
+  return Math.pow(2, Math.ceil(Math.log(v) / log2));
+}
