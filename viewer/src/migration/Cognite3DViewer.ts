3320c343bfcdc1c82343b7d9ed8862c64a725059
Joachim Bjørne <joachim.bjorne@cognite.com>
2020-03-23 12:48:54 +0100

    Changed return of function
    Relative viewport coordinates have 0,0 at top left corner.
    The range of each axis is 1.

--- viewer/src/migration/Cognite3DViewer.ts
+++ viewer/src/migration/Cognite3DViewer.ts
@@ -12,7 +12,7 @@ import { NotSupportedInMigrationWrapperError } from './NotSupportedInMigrationWr
 import { Intersection } from './intersection';
 import { CogniteClient } from '@cognite/sdk';
 import RenderController from './RenderController';
-import { from3DPositionToNormalizedDeviceCoordinates } from '../views/threejs/worldToViewport';
+import { from3DPositionToRelativeViewportCoordinates } from '../views/threejs/worldToViewport';
 
 export class Cognite3DViewer {
   private get canvas(): HTMLCanvasElement {
@@ -225,9 +225,7 @@ export class Cognite3DViewer {
   }
 
   worldToScreen(_point: THREE.Vector3, _normalize?: boolean): THREE.Vector2 | null {
-    const p = from3DPositionToNormalizedDeviceCoordinates(this.camera, _point);
-    p.y = -p.y; // Upper left should be (0,0)
-    p.addScalar(1).multiplyScalar(0.5); // Transform [-1,1] -> [0,1]
+    const p = from3DPositionToRelativeViewportCoordinates(this.camera, _point);
     if (p.x < 0 || p.x > 1 || p.y < 0 || p.y > 1 || p.z < 0 || p.z > 1) {
       // Return null if point is outside camera frustum.
       return null;
--- viewer/src/views/threejs/worldToViewport.ts
+++ viewer/src/views/threejs/worldToViewport.ts
@@ -10,11 +10,14 @@ export interface Position2D {
   y: number;
 }
 
-export function from3DPositionToNormalizedDeviceCoordinates(
+export function from3DPositionToRelativeViewportCoordinates(
   camera: THREE.PerspectiveCamera,
   position3D: THREE.Vector3
 ): THREE.Vector3 {
-  return position3D.clone().project(camera);
+  const relativePosition = position3D.clone().project(camera);
+  relativePosition.addScalar(1).multiplyScalar(0.5);
+  relativePosition.y = -relativePosition.y; // Is it better to have a multiply with a 0.5, -0.5 vector?
+  return relativePosition;
 }
 
 export function worldToViewport(
@@ -22,9 +25,9 @@ export function worldToViewport(
   camera: THREE.PerspectiveCamera,
   position3D: THREE.Vector3
 ): Position2D {
-  const normalizedDeviceCoordinates = from3DPositionToNormalizedDeviceCoordinates(camera, position3D);
+  const normalizedDeviceCoordinates = from3DPositionToRelativeViewportCoordinates(camera, position3D);
   return {
-    x: Math.round((0.5 + normalizedDeviceCoordinates.x / 2) * (canvas.width / window.devicePixelRatio)),
-    y: Math.round((0.5 + normalizedDeviceCoordinates.y / 2) * (canvas.height / window.devicePixelRatio))
+    x: Math.round(normalizedDeviceCoordinates.x * (canvas.width / window.devicePixelRatio)),
+    y: Math.round(normalizedDeviceCoordinates.y * (canvas.height / window.devicePixelRatio))
   };
 }
