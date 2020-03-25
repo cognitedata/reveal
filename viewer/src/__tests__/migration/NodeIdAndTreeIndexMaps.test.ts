46b852927b922b14c47738a65684a6fe1f85d02c
Svenn-Arne Dragly <dragly@cognite.com>
2020-03-25 15:30:30 +0100

    Fix tests

--- viewer/src/__tests__/migration/NodeIdAndTreeIndexMaps.test.ts
+++ viewer/src/__tests__/migration/NodeIdAndTreeIndexMaps.test.ts
@@ -4,6 +4,7 @@
 
 import { CogniteClient, InternalId } from '@cognite/sdk';
 import { NodeIdAndTreeIndexMaps } from '../../migration/NodeIdAndTreeIndexMaps';
+import { sleep } from '../wait';
 
 jest.mock('@cognite/sdk');
 
@@ -16,7 +17,7 @@ describe('NodeIdAndTreeIndexMaps', () => {
           retrieve3DNodes: async (_modelId: number, _revisionId: number, ids: InternalId[]) => {
             return ids.map((id: InternalId) => {
               return {
-                id,
+                id: id.id,
                 treeIndex: id.id
               };
             });
@@ -33,5 +34,24 @@ describe('NodeIdAndTreeIndexMaps', () => {
     const treeIndex2 = await maps.getTreeIndex(2);
     expect(treeIndex1).toEqual(1);
     expect(treeIndex2).toEqual(2);
+  });
+
+  test('requesting same index twice returns correctly twice', async () => {
+    const client = new CogniteClient({ appId: 'test' });
+    const maps = new NodeIdAndTreeIndexMaps(0, 0, client);
+    const treeIndex1 = await maps.getTreeIndex(1);
+    const treeIndex2 = await maps.getTreeIndex(1);
+    expect(treeIndex1).toEqual(1);
+    expect(treeIndex2).toEqual(1);
+  });
+
+  test('requesting same index twice with sleep in-between returns correctly twice', async () => {
+    const client = new CogniteClient({ appId: 'test' });
+    const maps = new NodeIdAndTreeIndexMaps(0, 0, client);
+    const treeIndex1 = await maps.getTreeIndex(1);
+    await sleep(1000);
+    const treeIndex2 = await maps.getTreeIndex(1);
+    expect(treeIndex1).toEqual(1);
+    expect(treeIndex2).toEqual(1);
   });
 });
