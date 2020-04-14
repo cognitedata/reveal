de0f41af1128fd767c2281a5de140a0822569d04
Lars Moastuen <lars.moastuen@cognite.com>
2020-04-14 17:31:14 +0200

    - Ignore everything /node_modules/ to reduce CPU consumption of webpack-dev-server. - Install fsevents explicitly to fix high CPU consumption of webpack-dev-server.

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -221,7 +221,7 @@ module.exports = env => {
     devtool: development ? "inline-source-map" : "source-map",
     watchOptions: {
       aggregateTimeout: 1500,
-      ignored: ['node_modules/']
+      ignored: [/node_modules/]
     },
     devServer: {
       https: true,
