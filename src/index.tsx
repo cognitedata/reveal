677a406b5f4580cfb2fe88666690878d2ea7ac47
Evan Charlton <evan.charlton@cognite.com>
2020-04-15 12:54:42 +0200

    Use the correct Sentry DSN
    Send the sentry errors to the right place.

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,12 @@ import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css';
 import '@cognite/cogs.js/dist/cogs.css';
 
-// if (process.env.REACT_APP_ENV !== 'development') {
-//   Sentry.init({
-//     dsn: 'https://da67b4b23d3e4baea6c36de155a08491@sentry.io/3541732',
-//   });
-// }
+if (process.env.REACT_APP_ENV !== 'development') {
+  Sentry.init({
+    dsn:
+      'https://4558e4f6faaf4948ab9327457827a301@o124058.ingest.sentry.io/5193370',
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
