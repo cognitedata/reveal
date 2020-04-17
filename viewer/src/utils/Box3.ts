6e8817c18b088eea20306346814470813f5e9c97
Lars Moastuen <lars.moastuen@cognite.com>
2020-04-17 22:31:56 +0200

    Add Box3.intersectsBox() and containsPoint()

--- viewer/src/utils/Box3.ts
+++ viewer/src/utils/Box3.ts
@@ -32,4 +32,27 @@ export class Box3 {
     const pMax = vec3.transformMat4(vec3.create(), this.max, matrix);
     return new Box3([pMin, pMax]);
   }
+
+  containsPoint(p: vec3): boolean {
+    return (
+      p[0] >= this.min[0] &&
+      p[1] >= this.min[1] &&
+      p[2] >= this.min[2] &&
+      p[0] <= this.max[0] &&
+      p[1] <= this.max[1] &&
+      p[2] <= this.max[2]
+    );
+  }
+
+  intersectsBox(b: Box3): boolean {
+    const a = this;
+    return (
+      a.min[0] <= b.max[0] &&
+      a.max[0] >= b.min[0] &&
+      a.min[1] <= b.max[1] &&
+      a.max[1] >= b.min[1] &&
+      a.min[2] <= b.max[2] &&
+      a.max[2] >= b.min[2]
+    );
+  }
 }
