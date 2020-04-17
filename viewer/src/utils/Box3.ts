2a11fc1680b73f131ccf27053bd8503e77ff48d7
Lars Moastuen <lars.moastuen@cognite.com>
2020-04-17 22:31:56 +0200

    Add Box3.intersectsBox()

--- viewer/src/utils/Box3.ts
+++ viewer/src/utils/Box3.ts
@@ -51,4 +51,16 @@ export class Box3 {
       p[2] <= this.max[2]
     );
   }
+
+  intersectsBox(b: Box3): boolean {
+    const a = this;
+    return (
+      a.min[0] <= b.max[0] &&
+      a.max[0] >= b.min[0] &&
+      a.min[1] <= b.max[1] &&
+      a.max[1] >= b.min[1] &&
+      a.min[2] <= b.max[2] &&
+      a.max[2] >= b.min[2]
+    );
+  }
 }
