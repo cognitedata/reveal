35237202f9f3a4c7b8cbf56cd93d9177a009417a
j-bjorne <60690836+j-bjorne@users.noreply.github.com>
2020-04-20 17:39:00 +0200

    Removed cache from RootSectorNode (#380)
    * Reduced cache count to 1 for models in Repo
    Now tightly coupled with threejs Group
    
    Exposed dataRetriever in CadModel (dirty hack).
    Moved parse functionality and transform functionality to separate files.
    
    * Add unit test (review).
    
    * Changed CadModel
    Changed MemoryRequestCache,
    can be renamed to something along the lines of TimeCache?
    
    * Add semaphore to throttle number of concurrent requests (#382)
    
    * Revert "Add semaphore to throttle number of concurrent requests (#382)" (#384)
    
    This reverts commit 43be77a3be2b05a85102fba4e1d8ab580000cd1b.
    
    * Add semaphore to throttle number of concurrent requests (#385)
    
    * Add semaphore to throttle number of concurrent requests
    
    * Rename and add error handling
    
    * Add test
    
    * Cleaned up redundant tests
    
    * Cleaned up some more tests
    commented out failing test which requires some research
    
    Co-authored-by: Lars Moastuen <lars.moastuen@cognite.com>
    Co-authored-by: Svenn-Arne Dragly <dragly@cognite.com>

--- viewer/src/views/threejs/cad/RootSectorNode.ts
+++ viewer/src/views/threejs/cad/RootSectorNode.ts
@@ -2,82 +2,19 @@
  * Copyright 2020 Cognite AS
  */
 
-import * as THREE from 'three';
 import { buildScene } from './buildScene';
 import { SectorNode } from './SectorNode';
 import { toThreeMatrix4 } from '../utilities';
 import { CadModel } from '../../../models/cad/CadModel';
-import { Materials } from './materials';
-import { MemoryRequestCache } from '../../../cache/MemoryRequestCache';
-import { ParsedSector } from '../../../data/model/ParsedSector';
-import { LevelOfDetail } from '../../../data/model/LevelOfDetail';
-import { consumeSectorSimple } from './consumeSectorSimple';
-import { consumeSectorDetailed } from './consumeSectorDetailed';
-import { SectorQuads, Sector } from '../../../models/cad/types';
-import { ConsumedSector } from '../../../data/model/ConsumedSector';
-import { discardSector } from './discardSector';
-
-function hashIdAndLevelOfDetail(id: number, levelOfDetail: LevelOfDetail) {
-  return `${id},${levelOfDetail}`;
-}
 
 export class RootSectorNode extends SectorNode {
   public readonly sectorNodeMap: Map<number, SectorNode>;
-  private readonly materials: Materials;
 
-  private readonly consumeSectorCache: MemoryRequestCache<string, ParsedSector, THREE.Group>;
-
-  constructor(model: CadModel, materials: Materials) {
+  constructor(model: CadModel) {
     super(0, '/');
     const { scene, modelTransformation } = model;
     this.applyMatrix4(toThreeMatrix4(modelTransformation.modelMatrix));
     this.sectorNodeMap = new Map();
-    this.materials = materials;
-
-    this.consumeSectorCache = new MemoryRequestCache<string, ParsedSector, THREE.Group>(
-      (_hash: string, sector: ParsedSector) => this.consumeImpl(sector.id, sector),
-      { remove: (_hash: string, group: THREE.Group) => {
-        group.userData.refCount -= 1;
-        if (group.userData.refCount === 0) {
-          discardSector(group);
-        }
-      }}
-    );
-
     buildScene(scene.root, this, this.sectorNodeMap);
-  }
-
-  public async consumeSector(id: number, sector: ParsedSector): Promise<ConsumedSector> {
-    const { levelOfDetail, metadata } = sector;
-    const group = this.consumeSectorCache.request(hashIdAndLevelOfDetail(id, levelOfDetail), sector);
-    group.userData.refCount += 1;
-
-    return {
-      id,
-      levelOfDetail,
-      metadata,
-      group
-    };
-  }
-
-  private consumeImpl(id: number, sector: ParsedSector) {
-    const { levelOfDetail, metadata, data } = sector;
-    const group = ((): THREE.Group => {
-      switch (levelOfDetail) {
-        case LevelOfDetail.Discarded: {
-          return new THREE.Group();
-        }
-        case LevelOfDetail.Simple: {
-          return consumeSectorSimple(id, data as SectorQuads, metadata, this.materials);
-        }
-        case LevelOfDetail.Detailed: {
-          return consumeSectorDetailed(id, data as Sector, metadata, this.materials);
-        }
-        default:
-          throw new Error(`Unsupported level of detail ${sector.levelOfDetail}`);
-      }
-    })();
-    group.userData.refCount = 1; // cache = 1 user
-    return group;
   }
 }
