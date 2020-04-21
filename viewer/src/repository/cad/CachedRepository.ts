4e27482a48809e5d1bef449c82f13fb43724e795
Lars Moastuen <lars.moastuen@cognite.com>
2020-04-21 13:49:09 +0200

    Typo.

--- viewer/src/repository/cad/CachedRepository.ts
+++ viewer/src/repository/cad/CachedRepository.ts
@@ -60,7 +60,7 @@ export class CachedRepository implements Repository {
 
   // Adding this to support parse map for migration wrapper. Should be removed later.
   private readonly _parsedDataSubject: Subject<{
-    discriptor: string;
+    descriptor: string;
     lod: string;
     data: Sector | SectorQuads;
   }> = new Subject();
@@ -146,7 +146,7 @@ export class CachedRepository implements Repository {
   }
 
   getParsedData(): Observable<{ lod: string; data: Sector | SectorQuads }> {
-    return this._parsedDataSubject.pipe(distinct(keySelector => keySelector.discriptor)); // TODO: Should we do replay subject here instead of variable type?
+    return this._parsedDataSubject.pipe(distinct(keySelector => keySelector.descriptor)); // TODO: Should we do replay subject here instead of variable type?
   }
 
   private loadSectorFromNetwork(): OperatorFunction<WantedSector, ConsumedSector> {
@@ -162,7 +162,7 @@ export class CachedRepository implements Repository {
             this._modelDataParser.parse(),
             map(data => {
               this._parsedDataSubject.next({
-                discriptor: this.cacheKey(wantedSector),
+                descriptor: this.cacheKey(wantedSector),
                 lod: 'simple',
                 data: data as SectorQuads
               }); // TODO: Remove when migration is gone.
@@ -211,7 +211,7 @@ export class CachedRepository implements Repository {
           const networkObservable = zip(i3dFileObservable, ctmFilesObservable).pipe(
             map(([i3dFile, ctmFiles]) => this.finalizeDetailed(i3dFile as ParseSectorResult, ctmFiles)),
             map(data => {
-              this._parsedDataSubject.next({ discriptor: this.cacheKey(wantedSector), lod: 'detailed', data }); // TODO: Remove when migration is gone.
+              this._parsedDataSubject.next({ descriptor: this.cacheKey(wantedSector), lod: 'detailed', data }); // TODO: Remove when migration is gone.
               return { ...wantedSector, data };
             }),
             this._modelDataTransformer.transform(),
