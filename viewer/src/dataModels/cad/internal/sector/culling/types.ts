af4d2330a287b4bdaaa84cfb7cd00e9301a40f0d
Lars Moastuen <lars.moastuen@cognite.com>
2020-05-18 11:50:48 +0200

    Make the new sector culler the default one and remove the old one.

--- viewer/src/dataModels/cad/internal/sector/culling/types.ts
+++ viewer/src/dataModels/cad/internal/sector/culling/types.ts
@@ -5,6 +5,14 @@
 import { WantedSector } from '../WantedSector';
 import { LevelOfDetail } from '../LevelOfDetail';
 import { SectorMetadata } from '../types';
+import { CadModel } from '../../../public/CadModel';
+import { CadLoadingHints } from '../../../public/CadLoadingHints';
+
+export interface DetermineSectorsInput {
+  camera: THREE.PerspectiveCamera;
+  cadModels: CadModel[];
+  loadingHints: CadLoadingHints;
+}
 
 export type PrioritizedWantedSector = WantedSector & { priority: number };
 
