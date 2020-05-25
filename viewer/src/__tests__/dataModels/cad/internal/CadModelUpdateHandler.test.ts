41cf049f78bad82c977b37cd652b870d7b4fdd0d
Lars Moastuen <lars.moastuen@cognite.com>
2020-05-25 13:24:07 +0200

    Add unit test for CadModelUpdateHandler

---
+++ viewer/src/__tests__/dataModels/cad/internal/CadModelUpdateHandler.test.ts
@@ -0,0 +1,55 @@
+/*!
+ * Copyright 2020 Cognite AS
+ */
+
+import * as THREE from 'three';
+
+import { SectorCuller } from '@/dataModels/cad/internal/sector/culling/SectorCuller';
+import { CachedRepository } from '@/dataModels/cad/internal/sector/CachedRepository';
+import { CadModelUpdateHandler } from '@/dataModels/cad/internal/CadModelUpdateHandler';
+import { CadSectorProvider } from '@/dataModels/cad/internal/sector/CadSectorProvider';
+import { CadSectorParser } from '@/dataModels/cad/internal/sector/CadSectorParser';
+import { SimpleAndDetailedToSector3D } from '@/dataModels/cad/internal/sector/SimpleAndDetailedToSector3D';
+import { MaterialManager } from '@/dataModels/cad/internal/MaterialManager';
+import { CadNode } from '@/dataModels/cad/internal/CadNode';
+
+import { createCadModelMetadata } from '../../../testUtils/createCadModelMetadata';
+import { generateSectorTree } from '../../../testUtils/createSectorMetadata';
+
+describe('CadModelUpdateHandler', () => {
+  const modelSectorProvider: CadSectorProvider = {
+    getCadSectorFile: jest.fn()
+  };
+  const materialManager = new MaterialManager();
+  const modelDataParser = new CadSectorParser();
+  const modelDataTransformer = new SimpleAndDetailedToSector3D(materialManager);
+  const repository = new CachedRepository(modelSectorProvider, modelDataParser, modelDataTransformer);
+  const mockCuller: SectorCuller = {
+    determineSectors: jest.fn()
+  };
+
+  const cadModelMetadata = createCadModelMetadata(generateSectorTree(5));
+  const cadModel = new CadNode(cadModelMetadata, materialManager);
+
+  jest.useFakeTimers();
+
+  test('updateCamera(), updateLoadingHints() and updateClipPlanes() triggers SectorCuller.determineSectors()', () => {
+    const updateHandler = new CadModelUpdateHandler(repository, mockCuller);
+    updateHandler.observable().subscribe();
+    updateHandler.updateModels(cadModel);
+
+    updateHandler.updateCamera(new THREE.PerspectiveCamera());
+    jest.advanceTimersByTime(1000);
+    expect(mockCuller.determineSectors).toBeCalledTimes(1);
+
+    jest.resetAllMocks();
+    updateHandler.updateLoadingHints({ maxQuadSize: 10 });
+    jest.advanceTimersByTime(1000);
+    expect(mockCuller.determineSectors).toBeCalledTimes(1);
+
+    jest.resetAllMocks();
+    updateHandler.updateClipPlanes([new THREE.Plane()]);
+    jest.advanceTimersByTime(1000);
+    expect(mockCuller.determineSectors).toBeCalledTimes(1);
+  });
+});
