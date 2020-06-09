e7bd3471e71068716c899c3a6f41c049a7882ab6
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2020-06-09 22:45:48 +0200

    Fixing icon size and color

--- src/Components/VirtualTree/TreeIcon.tsx
+++ src/Components/VirtualTree/TreeIcon.tsx
@@ -4,9 +4,10 @@ import * as Color from "color";
 import { fabric } from "fabric";
 
 const DEFAULT_ICON_SIZE = 18;
-const USE_FABRIC = false; // todo: Mihil - remove once fabric.js evaluation is completed
+const USE_FABRIC = true; // todo: Mihil - remove once fabric.js evaluation is completed
 
-export interface ImageProps {
+export interface ImageProps
+{
   readonly degree?: number;
 }
 const Image = styled.img<ImageProps>`
@@ -18,7 +19,8 @@ export default function TreeIcon(props: {
   alt?: string;
   size?: number;
   color?: Color;
-}) {
+})
+{
   const containerRef = useRef<null | HTMLDivElement>(null);
   const iconSize = props.size || DEFAULT_ICON_SIZE;
 
@@ -26,21 +28,26 @@ export default function TreeIcon(props: {
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  useEffect(() => {
+  useEffect(() =>
+  {
     // only works when USE_FABRIC is true
-    if (props.src && canvasRef.current) {
+    if (props.src && canvasRef.current)
+    {
       const canvas = new fabric.Canvas(canvasRef.current);
-      const imgInstance = fabric.Image.fromURL(props.src!, oImg => {
-        if (oImg) {
+      const imgInstance = fabric.Image.fromURL(props.src!, oImg =>
+      {
+        if (oImg)
+        {
           oImg.scaleToHeight(iconSize);
-          oImg.scaleToWidth(iconSize);
-
-          const filter = new fabric.Image.filters.BlendColor({
-            color: props.color ? props.color.hex() : undefined,
-            mode: "add",
-            alpha: 0.2
-          });
-          oImg.applyFilters([filter]);
+          if (props.color)
+          {
+            const filter = new fabric.Image.filters.BlendColor({
+              color: props.color.hex(),
+              mode: "multiply",
+              alpha: 1
+            });
+            oImg.applyFilters([filter]);
+          }
           canvas.renderAll();
           canvas.add(oImg);
         }
@@ -50,36 +57,41 @@ export default function TreeIcon(props: {
 
   // fabric.js - end
 
-  const paintImage = (container: HTMLElement) => {
+  const paintImage = (container: HTMLElement) =>
+  {
     const canvas = container.getElementsByTagName("canvas")[0] || document.createElement("canvas");
     const image = container.getElementsByClassName("tree-img")[0] as HTMLImageElement;
-    canvas.height = iconSize + 4;
-    canvas.width = iconSize + 4;
+
+    const padding = 1;
+    canvas.height = iconSize + padding * 2;
+    canvas.width = iconSize + padding * 2;
     container.appendChild(canvas);
 
     const ctx = canvas.getContext("2d");
-    if (ctx) {
-      ctx.imageSmoothingEnabled = false;
+    if (ctx)
+    {
+      ctx.imageSmoothingEnabled = true;
       ctx.imageSmoothingQuality = "high";
-      ctx.drawImage(image, 2, 2, iconSize, iconSize);
+      ctx.drawImage(image, padding, padding, iconSize, iconSize);
 
       image.style.display = "none";
-
-      const imageData = ctx.getImageData(0, 0, iconSize, iconSize);
-      const data = imageData.data;
-
       const color = props.color;
+      if (color)
+      {
+        const imageData = ctx.getImageData(0, 0, iconSize + padding * 2, iconSize + padding * 2);
+        const data = imageData.data;
 
-      if (color) {
         const minFaction = 0.0;
         const maxFaction = 0.667;
 
-        for (let i = 0; i < data.length; i += 4) {
+        for (let i = 0; i < data.length; i += 4)
+        {
           let r = data[i];
           let g = data[i + 1];
           let b = data[i + 2];
 
-          if (r === g && g === b && r > 0) {
+          if (r === g && g === b && r > 0)
+          {
             const fraction = r / 255; // white is 1, black is 0
 
             const newColorFrac = minFaction + fraction * (maxFaction - minFaction); // white is 0.75. black is 0.25
@@ -94,28 +106,34 @@ export default function TreeIcon(props: {
             data[i + 2] = b;
           }
         }
+        ctx.putImageData(imageData, 0, 0);
       }
-      ctx.putImageData(imageData, 0, 0);
-    } else {
+    }
+    else
+    {
       // tslint:disable-next-line: no-console
       console.log("canvas: CTX Not found!", props.src);
     }
   };
 
-  const handleImageLoad = () => {
-    if (containerRef.current) {
+  const handleImageLoad = () =>
+  {
+    if (containerRef.current)
+    {
       paintImage(containerRef.current as HTMLElement);
     }
   };
 
-  if (USE_FABRIC) {
+  if (USE_FABRIC)
+  {
     // todo: Mihil - remove this check once fabric.js evaluation is completed
     return (
       <div className="tree-icon center" ref={containerRef}>
         <canvas ref={canvasRef} height={iconSize} width={iconSize} />
       </div>
     );
-  } else {
+  } else
+  {
     return (
       <div className="tree-icon center" ref={containerRef}>
         <img
