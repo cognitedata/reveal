7d603fabdee5061f3cdb792b8b5b858585492156
Joachim <joachim.bjorne@cognite.com>
2020-06-15 13:34:58 +0200

    Removed deleted class from index imports

--- viewer/src/utilities/index.ts
+++ viewer/src/utilities/index.ts
@@ -5,7 +5,6 @@
 // TODO 02-06-2020 j-bjorne: Make index file the source of import for other packages? Adding all relative sub exports to index.ts
 
 export { File3dFormat } from './types';
-export { RateLimiter } from './RateLimiter';
 export { createOffsetsArray } from './arrays';
 export { Box3 } from './Box3';
 export { HtmlOverlayHelper } from './HtmlOverlayHelper';
