24e75d3b7613dcbbe6fa4a2e4ec025ad058627c9
Joachim <joachim.bjorne@cognite.com>
2020-06-17 15:02:31 +0200

    add debug function

--- viewer/src/utilities/rxOperations.ts
+++ viewer/src/utilities/rxOperations.ts
@@ -2,8 +2,18 @@
  * Copyright 2020 Cognite AS
  */
 
-import { of, pipe } from 'rxjs';
-import { delay, startWith, switchMap, distinctUntilChanged } from 'rxjs/operators';
+import { of, pipe, OperatorFunction } from 'rxjs';
+import { delay, startWith, switchMap, distinctUntilChanged, tap } from 'rxjs/operators';
+
+export const debug = <T extends {}>(tag: string = 'none'): OperatorFunction<T, T> => {
+  return tap(
+    // tslint:disable: no-console
+    (obj: T) => console.log(`next ${tag}`, obj),
+    error => console.error(`error ${tag}`, error),
+    () => console.log(`complete ${tag}`)
+    // tslint:enable: no-console
+  );
+};
 
 export const emissionLastMillis = (millis: number) => {
   return pipe(
