057c3ef7284a69a3e5179882b3b171d562e2e7d0
Hasitha Jayasundara <hasithajayasundara1@gmail.com>
2020-06-19 21:32:26 +0530

    Add fixes for interface types (#104)
    Co-authored-by: hasithajayasundara <hasithajayasundara@gmail.com>

--- src/Interface/IWell.ts
+++ src/Interface/IWell.ts
@@ -8,40 +8,39 @@
  */
 
 export interface WellMeta {
-    "active_indicator": string,
-    "basin_name": string,
-    "country_name": string,
-    "create_time": string,
-    "create_user": string,
-    "crs_epsg_orig": number,
-    "crs_epsg_transform_orig": number,
-    "current_crs_name": string,
-    "current_operator": string,
-    "on_off_shore": string,
-    "operatorDiv": string,
-    "spud_date": string,
-    "state_name": string,
-    "type": "Well",
-    "water_depth": number,
-    "water_depth_unit": string,
-    "well_legal_name": string,
-    "well_remark": string,
-    "x_coordinate": number,
-    "y_coordinate": number
-
+  active_indicator: string;
+  basin_name: string;
+  country_name: string;
+  create_time: string;
+  create_user: string;
+  crs_epsg_orig: number | string;
+  crs_epsg_transform_orig: number | string;
+  current_crs_name: string;
+  current_operator: string;
+  on_off_shore: string;
+  operatorDiv: string;
+  spud_date: string;
+  state_name: string;
+  type: string;
+  water_depth: string;
+  water_depth_unit: string;
+  well_legal_name: string;
+  well_remark: string;
+  x_coordinate: string;
+  y_coordinate: string;
 }
 
 export interface Well {
-    "externalId": string,
-    "name": string,
-    "parentId": number,
-    "parentExternalId": string,
-    "description": string,
-    "dataSetId": number,
-    "source": string,
-    "id": number,
-    "createdTime": number,
-    "lastUpdatedTime": number,
-    "rootId": number
-    "metadata": WellMeta,
+  externalId: string;
+  name: string;
+  parentId: number;
+  parentExternalId: string;
+  description: string;
+  dataSetId: number;
+  source: string;
+  id: number;
+  createdTime: string;
+  lastUpdatedTime: string;
+  rootId: number;
+  metadata: WellMeta;
 }
--- src/Interface/IWellbore.ts
+++ src/Interface/IWellbore.ts
@@ -8,44 +8,44 @@
  */
 
 export interface WellboreMeta {
-    "type": "Welllbore",
-    "active_indicator": string,
-    "asset_rig": string,
-    "asset_type": string,
-    "bh_x_coordinate": number,
-    "bh_y_coordinate": number,
-    "create_time": string,
-    "create_user": string,
-    "current_crs_name": string,
-    "elevation_type": string,
-    "elevation_value": number,
-    "elevation_value_unit": string,
-    "field_name": string,
-    "formation_total_depth": string,
-    "is_tight": boolean,
-    "parent_well_common_name": string,
-    "plugged_back_md": string,
-    "plugged_back_unit": string,
-    "total_depth_md": number,
-    "total_depth_unit": string,
-    "wellbore_concept_name": string,
-    "wellbore_lease_name": string,
-    "wellbore_legal_name": string,
-    "wellbore_purpose": string,
-    "wellbore_remark": string
+  type: string;
+  active_indicator: string;
+  asset_rig: string;
+  asset_type: string;
+  bh_x_coordinate: string;
+  bh_y_coordinate: string;
+  create_time: string;
+  create_user: string;
+  current_crs_name: string;
+  elevation_type: string;
+  elevation_value: string;
+  elevation_value_unit: string;
+  field_name: string;
+  formation_total_depth: string;
+  is_tight: string;
+  parent_well_common_name: string;
+  plugged_back_md: string;
+  plugged_back_unit: string;
+  total_depth_md: string;
+  total_depth_unit: string;
+  wellbore_concept_name: string;
+  wellbore_lease_name: string;
+  wellbore_legal_name: string;
+  wellbore_purpose: string;
+  wellbore_remark: string;
 }
 
 export interface Wellbore {
-    "externalId": string,
-    "name": string,
-    "parentId": number,
-    "parentExternalId": string,
-    "description": string,
-    "dataSetId": number,
-    "source": string,
-    "id": number,
-    "createdTime": number,
-    "lastUpdatedTime": number,
-    "rootId": number
-    "metadata": WellboreMeta,
+  externalId: string;
+  name: string;
+  parentId: number;
+  parentExternalId: string;
+  description: string;
+  dataSetId: number;
+  source: string;
+  id: number;
+  createdTime: string;
+  lastUpdatedTime: string;
+  rootId: number;
+  metadata: WellboreMeta;
 }
