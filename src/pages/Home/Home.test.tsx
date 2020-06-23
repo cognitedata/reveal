bdadd7e2b71e5f473eead27088bdccc38c0a4bac
erland-cognite <erland.solstrand@cognite.com>
2020-06-23 13:11:24 +0200

    temporarily disable select linting rules

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-// import { render } from 'utils/test';
-// import { Base } from './Home.stories';
+import { render } from 'utils/test';
+import { Base } from './Home.stories';
 
 describe('<Home />', () => {
   test('renders correct layout', async () => {
-    /*
     const { getByText } = render(<Base />);
     const linkElement = getByText(/learn about/i);
     expect(linkElement).toBeInTheDocument();
-     */
   });
 });
