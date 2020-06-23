7b7645855eca2fa6f1bac634321ef2caf16ca10d
Hasitha Jayasundara <hasithajayasundara1@gmail.com>
2020-06-23 13:08:08 +0530

    Add risk interface (#114)
    Co-authored-by: hasithajayasundara <hasithajayasundara@gmail.com>

---
+++ src/Interface/IRisk.ts
@@ -0,0 +1,55 @@
+/**
+ * Interface for the Risk object at BP
+ */
+
+export interface IRiskMetadata {
+  archived: string;
+  contingency: string;
+  country: string;
+  details: string;
+  diameter_hole_unit: string;
+  duration_unit: string;
+  end_well_top: string;
+  field_name: string;
+  formation: string;
+  further_analysis: string;
+  geological_period: string;
+  hidden: string;
+  lastUpdated: string;
+  lastUpdatedBy: string;
+  lithology: string;
+  md_hole_end: string;
+  md_hole_end_unit: string;
+  md_hole_start: string;
+  md_hole_start_unit: string;
+  name: string;
+  operation: string;
+  parentExternalId: string;
+  probability: string;
+  region: string;
+  risk_sub_category: string;
+  root_cause: string;
+  root_cause_details: string;
+  severity: string;
+  start_well_top: string;
+  summary: string;
+  tvd_offset_hole_end_unit: string;
+  tvd_offset_hole_start_unit: string;
+  type: string;
+  well_name: string;
+}
+
+export interface RiskEvent {
+  externalId: string;
+  dataSetId: number;
+  startTime: number;
+  endTime: number;
+  subtype: number;
+  description: string;
+  metadata: IRiskMetadata;
+  assetIds: number[];
+  source: string;
+  id: number;
+  lastUpdatedTime: string;
+  createdTime: string;
+}
--- src/Interface/index.ts
+++ src/Interface/index.ts
@@ -1,9 +1,11 @@
-import { Trajectory, TrajectoryMeta, TrajectoryColumn } from './ITrajectory';
-import { TrajectoryRows, TrajectoryColumnR, TrajectoryRow } from './ITrajectoryRows';
-import { WellMeta, Well } from './IWell';
-import { WellboreMeta, Wellbore } from './IWellbore';
+import { Trajectory, TrajectoryMeta, TrajectoryColumn } from "./ITrajectory";
+import { TrajectoryRows, TrajectoryColumnR, TrajectoryRow } from "./ITrajectoryRows";
+import { WellMeta, Well } from "./IWell";
+import { WellboreMeta, Wellbore } from "./IWellbore";
+import { IRiskMetadata, RiskEvent } from "./IRisk";
 
 export { Trajectory, TrajectoryColumn, TrajectoryMeta };
-export { TrajectoryRow, TrajectoryRows, TrajectoryColumnR};
+export { TrajectoryRow, TrajectoryRows, TrajectoryColumnR };
 export { Well, WellMeta };
 export { Wellbore, WellboreMeta };
+export { IRiskMetadata, RiskEvent };
