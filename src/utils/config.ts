c194a42974f4a00bc3e1c503cdf81966722a6a90
erland-cognite <erland.solstrand@cognite.com>
2020-06-24 11:29:27 +0200

    move api key out of ze code

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,6 @@ export default {
   api: {
     url: 'https://subsurface-console-cognitedata-development.cognite.ai',
     project: '/subsurface',
-    key: 'Y2MyZDMzNDktYzcyYi00ZjE0LWExYTYtNTZjNjlkMzcxZWIw',
+    key: process.env.REACT_APP_API_KEY as string,
   },
 };
