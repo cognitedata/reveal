ea20d59d3f42be0c3b5a046a72b889d48df313e6
Aksel Slettemark <aksel.slettemark@cognite.com>
2020-07-01 12:40:05 +0200

    Fix the test again

--- examples/src/visual_tests/GithubDiffUploader.js
+++ examples/src/visual_tests/GithubDiffUploader.js
@@ -11,7 +11,7 @@ async function commentOnPR(octokit) {
   const url = "https://github.com/cognitedata/reveal/actions/runs/" + process.env["GITHUB_RUN_ID"];
   const message = "There were failures during the visual regression test stage. " +
     `Any image diffs for visual tests can be downloaded as an artifact [here](${url}). ` +
-    "If the changes are intentional you can update the snapshots by running `yarn snapshots:update`";
+    "If the changes are intentional you can update the snapshots by running `yarn snapshots:update` in `examples/`";
 
   const prNumber = context.payload.pull_request.number;
   await octokit.issues.createComment({
