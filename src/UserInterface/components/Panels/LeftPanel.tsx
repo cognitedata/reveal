d195b37eefbd62464033ac4262aa94c51643c733
malindakpt <malindakpt@gmail.com>
2020-07-06 10:59:03 +0530

    Temporary removed Settings panel and SplitPane (#154)

--- src/UserInterface/components/Panels/LeftPanel.tsx
+++ src/UserInterface/components/Panels/LeftPanel.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import SplitPane from "react-split-pane";
-import Settings from "../Settings/Settings";
+// import SplitPane from "react-split-pane";
+// import Settings from "../Settings/Settings";
 import { Explorer } from "../Explorer/Explorer";
 
 // Renders Explorer and Settings components
 export default function LeftPanel() {
   return (
     <div className="left-panel">
-      <SplitPane split="horizontal" defaultSize={"50%"} primary="second">
-        <Explorer />
-        <Settings />
-      </SplitPane>
+      {/* Removed Settings panel and SplitPane temporary and will be added in future */}
+      {/* <SplitPane split="horizontal" defaultSize={"50%"} primary="second"> */}
+      <Explorer />
+      {/* <Settings /> */}
+      {/* </SplitPane> */}
     </div>
   );
 }
