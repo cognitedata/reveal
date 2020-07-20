1c6b129bcee652f9d2428394c94cdd56a3d505d9
Lars Moastuen <lars.moastuen@cognite.com>
2020-07-20 16:22:04 +0200

    Mark test as skip rather than commenting out the code

--- viewer/src/__tests__/dataModels/cad/internal/CadModelUpdateHandler.test.ts
+++ viewer/src/__tests__/dataModels/cad/internal/CadModelUpdateHandler.test.ts
@@ -2,58 +2,57 @@
  * Copyright 2020 Cognite AS
  */
 
-// TODO 2020-07-15 larsmoa: Test broken due to RxJs async testing issues. RxJs <3
+import * as THREE from 'three';
 
-// import * as THREE from 'three';
+import { CadNode } from '@/datamodels/cad';
+import { MaterialManager } from '@/datamodels/cad/MaterialManager';
+import { CadSectorParser } from '@/datamodels/cad/sector/CadSectorParser';
+import { SimpleAndDetailedToSector3D } from '@/datamodels/cad/sector/SimpleAndDetailedToSector3D';
+import { CachedRepository } from '@/datamodels/cad/sector/CachedRepository';
+import { SectorCuller } from '@/datamodels/cad/sector/culling/SectorCuller';
 
-// import { CadNode } from '@/datamodels/cad';
-// import { MaterialManager } from '@/datamodels/cad/MaterialManager';
-// import { CadSectorParser } from '@/datamodels/cad/sector/CadSectorParser';
-// import { SimpleAndDetailedToSector3D } from '@/datamodels/cad/sector/SimpleAndDetailedToSector3D';
-// import { CachedRepository } from '@/datamodels/cad/sector/CachedRepository';
-// import { SectorCuller } from '@/datamodels/cad/sector/culling/SectorCuller';
+import { createCadModelMetadata } from '../../../testutils/createCadModelMetadata';
+import { generateSectorTree } from '../../../testutils/createSectorMetadata';
+import { CadModelUpdateHandler } from '@/datamodels/cad/CadModelUpdateHandler';
+import { BinaryFileProvider } from '@/utilities/networking/types';
 
-// import { createCadModelMetadata } from '../../../testutils/createCadModelMetadata';
-// import { generateSectorTree } from '../../../testutils/createSectorMetadata';
-// import { CadModelUpdateHandler } from '@/datamodels/cad/CadModelUpdateHandler';
-// import { BinaryFileProvider } from '@/utilities/networking/types';
+describe('CadModelUpdateHandler', () => {
+  const modelSectorProvider: BinaryFileProvider = {
+    getBinaryFile: jest.fn()
+  };
+  const materialManager = new MaterialManager();
+  const modelDataParser = new CadSectorParser();
+  const modelDataTransformer = new SimpleAndDetailedToSector3D(materialManager);
+  const repository = new CachedRepository(modelSectorProvider, modelDataParser, modelDataTransformer);
+  const mockCuller: SectorCuller = {
+    determineSectors: jest.fn()
+  };
 
-// describe('CadModelUpdateHandler', () => {
-//   const modelSectorProvider: BinaryFileProvider = {
-//     getBinaryFile: jest.fn()
-//   };
-//   const materialManager = new MaterialManager();
-//   const modelDataParser = new CadSectorParser();
-//   const modelDataTransformer = new SimpleAndDetailedToSector3D(materialManager);
-//   const repository = new CachedRepository(modelSectorProvider, modelDataParser, modelDataTransformer);
-//   const mockCuller: SectorCuller = {
-//     determineSectors: jest.fn()
-//   };
+  const cadModelMetadata = createCadModelMetadata(generateSectorTree(5));
+  const cadModel = new CadNode(cadModelMetadata, materialManager);
 
-//   const cadModelMetadata = createCadModelMetadata(generateSectorTree(5));
-//   const cadModel = new CadNode(cadModelMetadata, materialManager);
+  jest.useFakeTimers();
 
-//   jest.useFakeTimers();
+  // TODO 2020-07-15 larsmoa: Test broken due to RxJs async testing issues. RxJs <3
+  test.skip('updateCamera(), updateLoadingHints() and updateClipPlanes() triggers SectorCuller.determineSectors()', () => {
+    const updateHandler = new CadModelUpdateHandler(repository, mockCuller);
+    updateHandler.consumedSectorObservable().subscribe();
+    updateHandler.updateModels(cadModel);
 
-//   test('updateCamera(), updateLoadingHints() and updateClipPlanes() triggers SectorCuller.determineSectors()', () => {
-//     const updateHandler = new CadModelUpdateHandler(repository, mockCuller);
-//     updateHandler.consumedSectorObservable().subscribe();
-//     updateHandler.updateModels(cadModel);
+    updateHandler.updateCamera(new THREE.PerspectiveCamera());
+    jest.advanceTimersByTime(2000);
+    expect(mockCuller.determineSectors).toBeCalledTimes(1);
 
-//     updateHandler.updateCamera(new THREE.PerspectiveCamera());
-//     jest.advanceTimersByTime(2000);
-//     expect(mockCuller.determineSectors).toBeCalledTimes(1);
+    updateHandler.clippingPlanes = [new THREE.Plane()];
+    jest.advanceTimersByTime(1000);
+    expect(mockCuller.determineSectors).toBeCalledTimes(2);
 
-//     updateHandler.clippingPlanes = [new THREE.Plane()];
-//     jest.advanceTimersByTime(1000);
-//     expect(mockCuller.determineSectors).toBeCalledTimes(2);
+    updateHandler.clipIntersection = true;
+    jest.advanceTimersByTime(1000);
+    expect(mockCuller.determineSectors).toBeCalledTimes(3);
 
-//     updateHandler.clipIntersection = true;
-//     jest.advanceTimersByTime(1000);
-//     expect(mockCuller.determineSectors).toBeCalledTimes(3);
-
-//     updateHandler.updateLoadingHints({});
-//     jest.advanceTimersByTime(1000);
-//     expect(mockCuller.determineSectors).toBeCalledTimes(4);
-//   });
-// });
+    updateHandler.updateLoadingHints({});
+    jest.advanceTimersByTime(1000);
+    expect(mockCuller.determineSectors).toBeCalledTimes(4);
+  });
+});
