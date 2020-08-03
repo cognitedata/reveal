ebfcc31daf71bda15beb71339ff8df086ee28763
Evan Charlton <evan.charlton@cognite.com>
2020-08-03 08:41:51 +0200

    Upgrade RDT to the latest storybook (#93)
    Modernize the `@cognite/react-document-table` package's use of
    [storybook] by bumping it to 5.3.x and migrating everything accordingly.
    
    [storybook]: https://storybook.js.org/

---
+++ react-document-table/stories/DocumentTable.stories.tsx
@@ -0,0 +1,195 @@
+import 'antd/dist/antd.css';
+import React from 'react';
+
+import { DocumentTable } from '../src';
+
+export default {
+  title: 'DocumentTable',
+};
+
+export const Base = () => (
+  <DocumentTable
+    docs={[
+      {
+        id: 1,
+        fileName: 'file name 1',
+        metadata: {
+          DOC_TITLE: 'document title 1',
+          DOC_TYPE: 'XG',
+        },
+      },
+      {
+        id: 2,
+        fileName: 'file name 2',
+        metadata: {
+          DOC_TITLE: 'document title 2',
+          DOC_TYPE: 'XB',
+        },
+      },
+      {
+        id: 3,
+        fileName: 'file name 3',
+        metadata: {
+          DOC_TITLE: 'document title 3',
+          DOC_TYPE: 'XG',
+        },
+      },
+    ]}
+  />
+);
+
+export const CustomRenderer = () => (
+  <DocumentTable
+    docs={[
+      {
+        id: 1,
+        fileName: 'file name 1',
+        metadata: {
+          DOC_TITLE: 'document title 1',
+          DOC_TYPE: 'VB',
+        },
+      },
+      {
+        id: 2,
+        fileName: 'file name 2',
+        metadata: {
+          DOC_TITLE: 'document title 2',
+          DOC_TYPE: 'MA',
+        },
+      },
+      {
+        id: 3,
+        fileName: 'file name 3',
+        metadata: {
+          DOC_TITLE: 'document title 3',
+          DOC_TYPE: 'XB',
+        },
+      },
+      {
+        id: 4,
+        fileName: 'file name 4',
+        metadata: {
+          DOC_TITLE: 'document title 4',
+          DOC_TYPE: 'XG',
+        },
+      },
+      {
+        id: 5,
+        fileName: 'file name 5',
+        metadata: {
+          DOC_TITLE: 'document title 5',
+          DOC_TYPE: 'AA',
+        },
+      },
+    ]}
+    documentRenderer={(document, i) => (
+      <div>
+        <h1>Document #{i + 1}</h1>
+        <pre>{JSON.stringify(document, null, 2)}</pre>
+      </div>
+    )}
+  />
+);
+
+export const CategorySort = () => (
+  <DocumentTable
+    docs={[
+      {
+        id: 43,
+        fileName: 'file name 43',
+        metadata: {
+          DOC_TITLE: 'document title 43',
+          DOC_TYPE: 'VB',
+        },
+      },
+      {
+        id: 43,
+        fileName: 'file name 43',
+        metadata: {
+          DOC_TITLE: 'document title 43',
+          DOC_TYPE: 'MA',
+        },
+      },
+      {
+        id: 1,
+        fileName: 'file name 1',
+        metadata: {
+          DOC_TITLE: 'document title 1',
+          DOC_TYPE: 'XG',
+        },
+      },
+      {
+        id: 2,
+        fileName: 'file name 2',
+        metadata: {
+          DOC_TITLE: 'document title 2',
+          DOC_TYPE: 'XB',
+        },
+      },
+      {
+        id: 3,
+        fileName: 'file name 3',
+        metadata: {
+          DOC_TITLE: 'document title 3',
+          DOC_TYPE: 'XG',
+        },
+      },
+      {
+        id: 4,
+        fileName: 'file name 4',
+        metadata: {
+          DOC_TITLE: 'document title 4',
+          DOC_TYPE: 'VB',
+        },
+      },
+      {
+        id: 5,
+        fileName: 'file name 5',
+        metadata: {
+          DOC_TITLE: 'document title 5',
+          DOC_TYPE: 'AA',
+        },
+      },
+    ]}
+    categorySort={(a, b) => a.description.localeCompare(b.description)}
+    documentRenderer={(document, i) => (
+      <div>
+        <h1>Document #{i + 1}</h1>
+        <pre>{JSON.stringify(document, null, 2)}</pre>
+      </div>
+    )}
+  />
+);
+
+export const HorizontallyScrollable = () => (
+  <DocumentTable
+    docs={[
+      {
+        id: 1,
+        fileName: 'Cat ipsum',
+        metadata: {
+          DOC_TITLE:
+            'Cat ipsum dolor sit amet, fall over dead (not really but gets sympathy) litter kitter kitty litty little kitten big roar roar feed me but kitty ipsum dolor sit amet. Scratch the furniture attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; whats your problem? i meant to do that',
+          DOC_TYPE: 'XG',
+        },
+      },
+      {
+        id: 2,
+        fileName: 'file name 2',
+        metadata: {
+          DOC_TITLE: 'document title 2',
+          DOC_TYPE: 'XB',
+        },
+      },
+      {
+        id: 3,
+        fileName: 'file name 3',
+        metadata: {
+          DOC_TITLE: 'document title 3',
+          DOC_TYPE: 'XG',
+        },
+      },
+    ]}
+    scrollX
+  />
+);
