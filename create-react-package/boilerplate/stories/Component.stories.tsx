0dc579871b23f2382427835fcf7e017e80a2f79c
Evan Charlton <evan.charlton@cognite.com>
2020-08-03 09:01:03 +0200

    Remove @cognite/create-react-package (#89)
    This has been replaced by a local [plop]-based generator.
    
    [plop]: https://plopjs.com/

--- create-react-package/boilerplate/stories/Component.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import { Component } from '../src/Component';
-
-storiesOf('Component story', module).add('Component', () => <Component />);
