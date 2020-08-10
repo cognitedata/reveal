946332cbbe542ce70777107b1622d89b245bde93
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-08-10 17:05:37 +0200

    docs(PointCloudModel): write missing docs

--- viewer/src/public/migration/CognitePointCloudModel.ts
+++ viewer/src/public/migration/CognitePointCloudModel.ts
@@ -16,11 +16,13 @@ import { Matrix4 } from 'three';
  * @noInheritDoc
  */
 export class CognitePointCloudModel extends THREE.Object3D implements CogniteModelBase {
+  // overrides `THREE.Object3D` type property with 'pointcloud' value
   public readonly type: SupportedModelTypes = SupportedModelTypes.PointCloud;
   public readonly modelId: number;
   public readonly revisionId: number;
   private readonly potreeNode: PotreeNodeWrapper;
 
+  /** @internal */
   constructor(modelId: number, revisionId: number, potreeGroup: PotreeGroupWrapper, potreeNode: PotreeNodeWrapper) {
     super();
     this.modelId = modelId;
@@ -29,14 +31,35 @@ export class CognitePointCloudModel extends THREE.Object3D implements CogniteMod
     this.add(potreeGroup);
   }
 
+  /**
+   * Used to clean up memory.
+   */
   dispose() {
     this.children = [];
   }
 
+  /**
+   * @param outBbox Optional. Used to write result to.
+   * @returns model's bounding box.
+   * @example
+   * ```js
+   * const box = new THREE.Box3()
+   * model.getModelBoundingBox(box);
+   * // box now has the bounding box
+   *```
+   * ```js
+   * // the following code does the same
+   * const box = model.getModelBoundingBox();
+   * ```
+   */
   getModelBoundingBox(outBbox?: THREE.Box3): THREE.Box3 {
     return toThreeJsBox3(outBbox || new THREE.Box3(), this.potreeNode.boundingBox);
   }
 
+  /**
+   * Apply transformation matrix to the model.
+   * @param matrix Matrix to be applied.
+   */
   updateTransformation(matrix: Matrix4): void {
     this.applyMatrix4(matrix);
     this.updateMatrixWorld(false);
@@ -46,14 +69,25 @@ export class CognitePointCloudModel extends THREE.Object3D implements CogniteMod
     return this.potreeNode.pointBudget;
   }
 
+  /**
+   * The point budget limits the number of points loaded and rendered at any given time,
+   * which helps to adapt performance requirements to the capabilities of different hardware.
+   */
   set pointBudget(count: number) {
     this.potreeNode.pointBudget = count;
   }
 
-  get pointColorType() {
+  get pointColorType(): PotreePointColorType {
     return this.potreeNode.pointColorType;
   }
 
+  /**
+   * @see {@link PotreePointColorType} for available types
+   * @example
+   * ```js
+   * model.pointColorType = PotreePointColorType.Rgb
+   * ```
+   */
   set pointColorType(type: PotreePointColorType) {
     this.potreeNode.pointColorType = type;
   }
