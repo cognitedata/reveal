4e2fede50bbb6317f01d312794d8a1586a8c9b37
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-08-12 11:11:26 +0200

    fix(examples): use model ids from .env file (#923)

--- examples/src/process.env.d.ts
+++ examples/src/process.env.d.ts
@@ -6,10 +6,15 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       REACT_APP_PROJECT?: string
-      REACT_APP_CAD_LOCAL_FILENAME?: string
+
       REACT_APP_CAD_ID?: number
+      REACT_APP_CAD_REVISION_ID?: number
+
       REACT_APP_CAD_2_ID?: number
+      REACT_APP_CAD_2_REVISION_ID?: number
+
       REACT_APP_POINTCLOUD_ID?: number
+      REACT_APP_POINTCLOUD_REVISION_ID?: number
     }
   }
 }
