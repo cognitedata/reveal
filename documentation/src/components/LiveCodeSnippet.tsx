d456eea804de44c633345b43a413d0371442fa68
Lars Moastuen <lars.moastuen@cognite.com>
2020-08-31 13:45:51 +0200

    doc: add example for adding custom 3D content to the scene

--- documentation/src/components/LiveCodeSnippet.tsx
+++ documentation/src/components/LiveCodeSnippet.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as THREE from 'three';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import { PrismTheme } from 'prism-react-renderer';
 import clsx from 'clsx';
@@ -7,22 +8,23 @@ import styles from './styles.module.css';
 import oceanicNext from 'prism-react-renderer/themes/oceanicNext';
 const defaultCodeTheme = oceanicNext;
 
-export type LivCodeSnippetProps = {
+export type LiveCodeSnippetProps = {
   children: string;
   theme: PrismTheme;
   transformCode: (code: string) => string;
+  scope: { [key: string]: any };
   props: any;
 }
 
 import { resetViewerEventHandlers } from '../viewerUtilities';
 
-export class LiveCodeSnippet extends React.Component<LivCodeSnippetProps> {
-  constructor(props: LivCodeSnippetProps) {
+export class LiveCodeSnippet extends React.Component<LiveCodeSnippetProps> {
+  constructor(props: LiveCodeSnippetProps) {
     super(props);
   }
 
   render() {
-    const { transformCode, children, theme, props } = this.props;
+    const { transformCode, children, theme, scope, props } = this.props;
     return (
       <LiveProvider
         code={children.replace(/^\s+|\s+$/g, '')}
@@ -37,7 +39,7 @@ export class LiveCodeSnippet extends React.Component<LivCodeSnippetProps> {
           ${(transformCode ? transformCode(code) : code)}`;
           return `<button onClick={() => \{${fullCode}\}}>Run</button>`;
         })}
-        scope={{resetViewerEventHandlers}}
+        scope={{resetViewerEventHandlers, THREE, ...scope}}
         theme={theme || defaultCodeTheme}
         {...props}>
         <div
