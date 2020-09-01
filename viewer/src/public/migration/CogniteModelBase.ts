9dfbe6c58cd488211f71c244b9242b01ddce2f87
Lars Moastuen <lars.moastuen@cognite.com>
2020-09-01 21:25:31 +0200

    doc: remove doc for consistency

--- viewer/src/public/migration/CogniteModelBase.ts
+++ viewer/src/public/migration/CogniteModelBase.ts
@@ -13,11 +13,6 @@ export interface CogniteModelBase {
   readonly type: SupportedModelTypes;
   dispose(): void;
   getModelBoundingBox(outBbox?: THREE.Box3): THREE.Box3;
-  /**
-   * Retrieves the camera position and target stored for the model. Typically this
-   * is used to store a good starting position for a model. Returns `undefined` if there
-   * isn't any stored camera configuration for the model.
-   */
   getCameraConfiguration(): CameraConfiguration | undefined;
 
   /** @internal */
