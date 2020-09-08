bbe8332128f1da8b6fc04f71ba7fe8d498e2e031
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-09-08 14:59:23 +0200

    refactor(docs): ModelViewerWrapper is just another demo

--- documentation/src/components/DemoWrapper.tsx
+++ documentation/src/components/DemoWrapper.tsx
@@ -19,16 +19,21 @@ const DemoLoginCover = React.lazy(() => import('./DemoLoginCover'));
 
 const components: Record<string, ComponentType<DemoProps>> = {
   Cognite3DViewerDemo: React.lazy(() => import('./demos/Cognite3DViewerDemo')),
+  ModelPreviewDemo: React.lazy(() => import('./demos/ModelPreviewDemo')),
 };
 
 // demo wrapper just wraps the demo. don't pass modelId/revisionId to it, they defined in demos
 // different demos might have different ids, e.g. pointcloud/cad
 // also we can use 3ddemo instead of publicdata so here is .env file to help with it
 export function DemoWrapper({ name }: { name: string }) {
+
   if (typeof window === 'undefined') {
     return <div />;
   }
   let LazyComponent = components[name];
+  if (!LazyComponent) {
+    throw new Error(`Demo component with name ${name} is not found.`)
+  }
   return (
     <DemoContainer id="demo-wrapper">
       <Suspense fallback={<div>Loading demo...</div>}>
