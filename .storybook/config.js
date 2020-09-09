5334321c4251b47b6052d9e3e347258f9ee0522c
Peter Nicolai Motzfeldt <peter.motzfeldt@cognite.com>
2020-09-09 12:04:17 +0200

    fix: run configureI18n on storybook startup (#305)
    Storybook failed with not understanding objects as children to the Trans component

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,9 @@ import React from 'react';
 import '@cognite/cogs.js/dist/cogs.css';
 import GlobalStyles from 'global-styles';
 import { addDecorator } from '@storybook/react';
-import { I18nContainer } from '@cognite/react-i18n';
+import { configureI18n, I18nContainer } from '@cognite/react-i18n';
+
+configureI18n();
 
 addDecorator((story) => {
   return (
