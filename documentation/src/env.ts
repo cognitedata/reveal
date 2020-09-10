19540328ccf86c6bbd266f658e761095c70ee83d
Lars Moastuen <lars.moastuen@cognite.com>
2020-09-10 15:49:56 +0200

    doc: use 3D model with colors in documentation (#1076)

--- documentation/src/env.ts
+++ documentation/src/env.ts
@@ -4,7 +4,7 @@ function getNumber(arg: string | undefined): number | undefined {
 
 export const env = {
   project: process.env.PROJECT || 'publicdata',
-  modelId: getNumber(process.env.CAD_ID) || 4715379429968321,
-  revisionId: getNumber(process.env.CAD_REVISION_ID) || 5688854005909501
+  modelId: getNumber(process.env.CAD_ID) || 3356984403684032,
+  revisionId: getNumber(process.env.CAD_REVISION_ID) || 6664823881595566
   // add some pointclouds here later
 }
