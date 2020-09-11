6dc36626a5e44792649587322c263eae49dc2d84
Lars Moastuen <lars.moastuen@cognite.com>
2020-09-11 14:14:48 +0200

    test: try to figure out why test now fails

--- examples/src/pages/Testable.tsx
+++ examples/src/pages/Testable.tsx
@@ -155,8 +155,9 @@ export function Testable() {
         target.z
       );
       controls.update(0.0);
-      camera.near = near;
-      camera.far = far;
+      // camera.near = near;
+      // camera.far = far;
+      // camera.updateProjectionMatrix();
 
       camera.updateMatrixWorld();
       revealManager.update(camera);
