44d82590a43e0f5d7dbf24f627c71f2b4c2bfcd6
Valerii Gusev <valerii.gusev@cognite.com>
2020-09-15 08:40:46 +0200

    Fix sending missing 412 requests from locize (#104)
    According to locize support email:
    
    - i18next-localstorage-backend should not be used locally in order to push missing
    - i18next-http-backend is useless

--- i18n/src/i18n.ts
+++ i18n/src/i18n.ts
@@ -2,7 +2,6 @@ import i18next, { InitOptions } from 'i18next';
 import ChainedBackend from 'i18next-chained-backend';
 import LocalStorageBackend from 'i18next-localstorage-backend';
 import LocizeBackend from 'i18next-locize-backend';
-import HttpBackend from 'i18next-http-backend';
 import Pseudo from 'i18next-pseudo';
 import { initReactI18next } from 'react-i18next';
 
@@ -15,6 +14,8 @@ const {
   REACT_APP_LOCIZE_PROJECT_ID,
   REACT_APP_LOCIZE_API_KEY,
 } = process.env;
+
+const CACHE_TIME_MILLIS = 1000 * 60 * 60 * 24; // 1 day
 
 type ConfigureI18nOptions = {
   debug?: boolean;
@@ -35,7 +36,6 @@ const configureI18n = ({
   debug = REACT_APP_I18N_DEBUG === 'true',
   pseudo = REACT_APP_I18N_PSEUDO === 'true',
   lng = REACT_APP_LANGUAGE,
-  localStorageCacheTimeMillis = 1000 * 60 * 60 * 24, // 1 day
   locize,
   env = REACT_APP_ENV || NODE_ENV,
   ...rest
@@ -49,9 +49,14 @@ const configureI18n = ({
     projectId: REACT_APP_LOCIZE_PROJECT_ID,
     apiKey: REACT_APP_LOCIZE_API_KEY,
   };
+  const isDevEnv = !(env === 'production' || env === 'test');
+  const {
+    // In case we want to trigger missings we should not use LocalStorageBackend
+    localStorageCacheTimeMillis = isDevEnv ? 0 : CACHE_TIME_MILLIS,
+  } = rest;
 
   if (!locizeApiKey) {
-    if (!(env === 'production' || env === 'test')) {
+    if (isDevEnv) {
       // eslint-disable-next-line no-console
       console.warn(
         [
@@ -97,23 +102,16 @@ const configureI18n = ({
     });
   }
 
-  if (locizeApiKey) {
-    addBackend(LocizeBackend, {
-      projectId: locizeProjectId,
-      debug,
-      apiKey: locizeApiKey,
-      referenceLng: 'en',
-    });
-    initOptions.saveMissing = true;
-    initOptions.updateMissing = true;
-  }
-
-  if (locizeProjectId && locizeVersion) {
-    addBackend(HttpBackend, {
-      loadPath: `https://api.locize.app/${locizeProjectId}/${locizeVersion}/{{lng}}/{{ns}}`,
-      addPath: '',
-    });
-  }
+  addBackend(LocizeBackend, {
+    projectId: locizeProjectId,
+    debug,
+    version: locizeVersion,
+    // in case locizeApiKey is undefined it will fetch the translations but will not generate missing network requests
+    apiKey: locizeApiKey,
+    referenceLng: 'en',
+  });
+  initOptions.saveMissing = isDevEnv;
+  initOptions.updateMissing = isDevEnv;
 
   i18next.use(ChainedBackend);
   i18next.use(initReactI18next);
