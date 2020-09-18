d4436a8084c14a2422afd40d7ab5ece45907b859
Lars Moastuen <lars.moastuen@cognite.com>
2020-09-18 13:26:17 +0200

    test: register visual test for ghost mode

--- examples/src/visual_tests/basic.test.ts
+++ examples/src/visual_tests/basic.test.ts
@@ -1,10 +1,11 @@
 import { screenShotTest } from "./VisualTestUtils";
 
 const test_presets = [
-  "clipping",
-  "default_camera",
-  "highlight",
-  "rotate_cad_model"
+  // "clipping",
+  // "default_camera",
+  // "highlight",
+  // "rotate_cad_model",
+  "ghost_mode"
 ]
 
 describe('Testable', () => {
