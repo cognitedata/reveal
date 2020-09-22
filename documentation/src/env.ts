2543852eaf3e99c0b0fc106d900aeeb824c87e5b
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-09-22 09:15:28 +0200

    docs: allow pointclouds for Cognite3DViewerDemo

--- documentation/src/env.ts
+++ documentation/src/env.ts
@@ -5,6 +5,10 @@ function getNumber(arg: string | undefined): number | undefined {
 export const env = {
   project: process.env.PROJECT || 'publicdata',
   modelId: getNumber(process.env.CAD_ID) || 3356984403684032,
-  revisionId: getNumber(process.env.CAD_REVISION_ID) || 6664823881595566
-  // add some pointclouds here later
+  revisionId: getNumber(process.env.CAD_REVISION_ID) || 6664823881595566,
+  pointCloud: {
+    // fixme: here must be defaults from publicdata project (currently has no pointcloud model uploaded)
+    modelId: getNumber(process.env.POINTCLOUD_ID),
+    revisionId: getNumber(process.env.POINTCLOUD_REVISION_ID)
+  }
 }
