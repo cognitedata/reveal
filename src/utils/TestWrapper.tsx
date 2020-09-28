ea8ac706dfc268efed253ab0c55f9fcf39455747
Vegard Økland <vegard.okland@cognite.com>
2020-09-28 11:04:52 +0200

    Create new cache for each test

--- src/utils/TestWrapper.tsx
+++ src/utils/TestWrapper.tsx
@@ -4,21 +4,23 @@ import { MemoryRouter } from 'react-router';
 
 import sdk from 'sdk-singleton';
 
-const queryCache = new QueryCache({
-  defaultConfig: {
-    queries: {
-      staleTime: 60000,
-      cacheTime: 60000,
-      queryFn: sdk.get,
-    },
-  },
-});
+
 
 export default function TestWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryCache = new QueryCache({
+    defaultConfig: {
+      queries: {
+        staleTime: 60000,
+        cacheTime: 60000,
+        queryFn: sdk.get,
+      },
+    },
+  });
+    sdk.get.mockResolvedValue({ items: [mockFunction, mockFunction2] });
   return (
     <ReactQueryCacheProvider queryCache={queryCache}>
       <MemoryRouter>{children}</MemoryRouter>
