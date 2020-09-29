8a4d1a62a764d85171733af8d3789e03ea55076a
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-09-29 13:40:02 +0200

    fix: export PotreePointSizeType enum

--- viewer/src/index.ts
+++ viewer/src/index.ts
@@ -6,12 +6,11 @@ export { CogniteModelBase } from './public/migration/CogniteModelBase';
 export { Cognite3DModel } from './public/migration/Cognite3DModel';
 export { CognitePointCloudModel } from './public/migration/CognitePointCloudModel';
 export * from './public/migration/Cognite3DViewer';
-export { Intersection } from './public/migration/types';
-export { Color, Cognite3DViewerOptions, AddModelOptions } from './public/migration/types';
+export * from './public/migration/types';
 export { CadLoadingHints } from './datamodels/cad/CadLoadingHints';
 export { BoundingBoxClipper } from './utilities';
 
-export { PotreePointColorType, PotreePointShape } from './datamodels/pointcloud/types';
+export * from './datamodels/pointcloud/types';
 
 // Export ThreeJS to enable easy import for our users
 import * as THREE from 'three';
