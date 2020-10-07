7420a6c437ff3370d80a1c7413afe3e590de483b
Nicklas Svendsrud <nicklas.svendsrud@cognite.com>
2020-10-07 09:52:22 +0200

    Home page welcome text (#261)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Body, Row, Col, Title } from '@cognite/cogs.js';
+import { staticText } from './staticText';
+import { Container } from './elements';
 
+// eslint-disable-next-line no-lone-blocks
+{
+  /* eslint-disable react/no-danger */
+}
 const Home = () => {
+  const {
+    heading,
+    intro,
+    latestRelease,
+    upcoming,
+    gettingStarted,
+  } = staticText;
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <Container>
+      <div>
+        <Title level={1}>{heading}</Title>
+        <Body level={1} dangerouslySetInnerHTML={{ __html: intro }} />
+      </div>
+      <Row gutter={32}>
+        <Col span={12}>
+          <h2>{latestRelease.heading}</h2>
+          <div dangerouslySetInnerHTML={{ __html: latestRelease.text }} />
+        </Col>
+        <Col span={12}>
+          <h2>{upcoming.heading}</h2>
+          <div dangerouslySetInnerHTML={{ __html: upcoming.text }} />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <h2>{gettingStarted.heading}</h2>
+          <div dangerouslySetInnerHTML={{ __html: gettingStarted.text }} />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
