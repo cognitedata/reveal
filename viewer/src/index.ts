c0367e9858d55f7e1992ab83ef82181a0c741e05
Lars Moastuen <lars.moastuen@cognite.com>
2020-10-16 12:19:01 +0200

    chore: adjust exports

--- viewer/src/index.ts
+++ viewer/src/index.ts
@@ -9,13 +9,12 @@ export { Cognite3DViewer } from './public/migration/Cognite3DViewer';
 export { Intersection } from './public/migration/types';
 export * from './public/migration/Cognite3DViewer';
 export * from './public/migration/types';
+export { SupportedModelTypes } from '@/datamodels/base/SupportedModelTypes';
 export { CadLoadingHints } from './datamodels/cad/CadLoadingHints';
 export { BoundingBoxClipper } from './utilities';
-
+export * from './revealEnv';
 export * from './datamodels/pointcloud/types';
 
 // Export ThreeJS to enable easy import for our users
 import * as THREE from 'three';
 export { THREE };
-export { SupportedModelTypes } from '@/datamodels/base/SupportedModelTypes';
-export * from './revealEnv';
