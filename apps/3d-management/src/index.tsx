39e1448824c0e7ffedff4e57d8e96ef22bb658cb
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-10-19 12:06:26 +0200

    [D3M-1] fix: specify root element id for single-spa (#67)

--- apps/3d-management/src/index.tsx
+++ apps/3d-management/src/index.tsx
@@ -23,12 +23,16 @@ const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: App,
+
   // @ts-ignore
   errorBoundary() {
     // eslint-disable-line
     // Customize the root error boundary for your microfrontend here.
     return <span>An error occurred in your app</span>;
   },
+
+  // must be aligned with that id https://github.com/cognitedata/cdf-hub/pull/896/files#diff-bb6612be43d4f0ceceee1b286daf301a80e54aa1fc25edd5017a1aff986d6503R96
+  domElementGetter: () => document.getElementById('cdf-3d-management')!,
 });
 
 export const { bootstrap, mount, unmount } = lifecycles;
