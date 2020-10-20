47522d3a26007b9b5e773edb34a98c867d15ae9a
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-10-20 09:54:00 +0200

    [D3M-10] feat(ClassPicker): improve behaviour of All Classes option, rename legacy options, fix antd styles for selected options (#68)
    * fix: antd selected options proper margin
    
    * feat(ClassPicker): remove ReservedOr str parts from labels
    
    * feat(ClassPicker): improve behaviour of All Classes option
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/3d-management/src/components/ThreeDViewerToolbar/ClassPicker.tsx
+++ apps/3d-management/src/components/ThreeDViewerToolbar/ClassPicker.tsx
@@ -10,23 +10,36 @@ type Props = {
 };
 
 export function ClassPicker(props: Props) {
-  const ALL_CLASSES = 'ALL_CLASSES';
+  const ALL_CLASSES = -Infinity;
 
   let userDefinedNameCounter = 0;
-  const keyValueMapping = props.model.getClasses().reduce((acc, classValue) => {
+  const keyValueMapping: Record<
+    string,
+    number
+  > = props.model.getClasses().reduce((acc, classValue) => {
     const key =
       WellKnownAsprsPointClassCodes[classValue] ||
       // eslint-disable-next-line no-plusplus
       `User defined (${userDefinedNameCounter++})`;
 
-    acc[key] = classValue;
+    if (key.startsWith('ReservedOr')) {
+      const betterKey = key.slice('ReservedOr'.length);
+      acc[`${betterKey} (legacy)`] = classValue;
+    } else {
+      acc[key] = classValue;
+    }
+
     return acc;
   }, {});
 
-  const [selectedValues, setSelectedValues] = React.useState([ALL_CLASSES]);
+  const [selectedValues, setSelectedValues] = React.useState(
+    [ALL_CLASSES].concat(Object.values(keyValueMapping))
+  );
 
   React.useEffect(() => {
-    const classesToDisplayInModel = selectedValues.includes(ALL_CLASSES)
+    const classesToDisplayInModel: number[] = selectedValues.includes(
+      ALL_CLASSES
+    )
       ? Object.values(keyValueMapping)
       : selectedValues;
 
@@ -38,10 +51,42 @@ export function ClassPicker(props: Props) {
         classesToDisplayInModel.includes(classValue)
       );
     });
-  }, [selectedValues, props.model, keyValueMapping]);
+  }, [selectedValues, props.model, keyValueMapping, ALL_CLASSES]);
 
-  const onChange = (values) => {
-    setSelectedValues(values);
+  const onChange = (values: number[]) => {
+    const getValues = ({
+      nextValues,
+      prevValues,
+    }: {
+      nextValues: number[];
+      prevValues: number[];
+    }) => {
+      if (prevValues.includes(ALL_CLASSES)) {
+        // something else changed - remove ALL_CLASSES option
+        if (nextValues.includes(ALL_CLASSES)) {
+          return nextValues.filter((v) => v !== ALL_CLASSES);
+        }
+
+        // deselect all
+        return [];
+      }
+
+      // select all
+      if (nextValues.includes(ALL_CLASSES)) {
+        return [ALL_CLASSES].concat(Object.values(keyValueMapping));
+      }
+
+      // add ALL_CLASSES when all is selected
+      if (nextValues.length === Object.values(keyValueMapping).length) {
+        return nextValues.concat(ALL_CLASSES);
+      }
+
+      return values;
+    };
+
+    setSelectedValues(
+      getValues({ nextValues: values, prevValues: selectedValues })
+    );
   };
 
   return (
@@ -62,7 +107,11 @@ export function ClassPicker(props: Props) {
       </Select.Option>
 
       {Object.keys(keyValueMapping).map((key) => (
-        <Select.Option key={key as string} value={keyValueMapping[key]}>
+        // key is key+value to avoid duplicates after trimming of ReservedOr strings
+        <Select.Option
+          key={`${key} ${keyValueMapping[key]}`}
+          value={keyValueMapping[key]}
+        >
           {key}
         </Select.Option>
       ))}
