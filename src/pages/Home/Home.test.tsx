599352b7ed411b4235b81c39cd3433ae068b06fb
Max Khalzov <max.khalzov@gmail.com>
2020-10-28 14:33:47 +0200

    test: skip unit test in more appropriate way

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -3,7 +3,7 @@ import { render } from 'utils/test';
 import { screen } from '@testing-library/react';
 import { Base } from './Home.stories';
 
-xdescribe('<Home />', () => {
+describe.skip('<Home />', () => {
   test('renders learn react link', async () => {
     render(<Base />);
     expect(await screen.findByText(/learn about/i)).toBeInTheDocument();
