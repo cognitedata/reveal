936075b744eb8d2b0994e4ddf3eb7a4c696d6db1
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-11-05 12:31:11 +0100

    fix(docs): no broken links, proper entry file (#1296)

--- viewer/src/revealEnv.ts
+++ viewer/src/revealEnv.ts
@@ -2,8 +2,10 @@
  * Copyright 2020 Cognite AS
  */
 
-// used to specify custom url for worker/wasm files
-// in cases when you need the latest local files or CDN is blocked by CSP
+/**
+ * Used to specify custom url for worker/wasm files
+ * in cases when you need the latest local files or CDN is blocked by CSP.
+ */
 export const revealEnv = {
   publicPath: ''
 };
