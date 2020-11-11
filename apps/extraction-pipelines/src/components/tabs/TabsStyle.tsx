d0a358df4f1d8e4add40aa68fd36ba4e0a09fac7
4ornui <roman.chernenkyi@cognite.com>
2020-11-11 19:54:57 +0200

    Add tabs to Side Bar Panel

---
+++ apps/extraction-pipelines/src/components/tabs/TabsStyle.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+
+const StyledTabs = styled((props) => <Tabs {...props} />)`
+  height: 100%;
+
+  .ant-tabs-nav {
+    padding: 0 1rem 0 1.3125rem;
+    margin-bottom: 0;
+  }
+  .ant-tabs-tabpane {
+    display: grid;
+    grid-template-columns: auto 25rem;
+  }
+  .ant-tabs-tab {
+    padding: 0.625rem 1rem;
+    margin: 0;
+    .ant-tabs-tab-btn {
+      color: #333;
+      font-weight: 600;
+      line-height: 1.25rem;
+    }
+  }
+  .ant-tabs-content {
+    height: 100%;
+  }
+`;
+const StyledTabPane = styled((props) => (
+  <TabPane {...props}>{props.children}</TabPane>
+))``;
+
+export { StyledTabs, StyledTabPane };
