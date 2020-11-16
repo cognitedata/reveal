a11a85818c4f4f8735a33e971350d0fa7b09f717
Begum Ozcan-Caliskaner <begum.ozcan@cognite.com>
2020-11-16 12:37:30 +0100

    Preserve env param when redirecting to explorer (#322)

--- src/app/containers/Exploration/SearchRedirect.tsx
+++ src/app/containers/Exploration/SearchRedirect.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Redirect, useLocation } from 'react-router-dom';
 
 export default function SearchRedirect() {
-  const { pathname } = useLocation();
-  return <Redirect to={`${pathname}/search/asset`} push={false} />;
+  const { pathname, search } = useLocation();
+  return (
+    <Redirect
+      to={{ pathname: `${pathname}/search/asset`, search }}
+      push={false}
+    />
+  );
 }
