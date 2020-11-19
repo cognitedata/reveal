97098a0e8c616e3af9d2da0154ed9d1df21c29c8
Begum Ozcan-Caliskaner <begum.ozcan@cognite.com>
2020-11-19 10:54:47 +0100

    [CDFUX-469] Asset preview (#332)
    Co-authored-by: Frikk Fossan <frikk.fossan@cognite.com>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- src/lib/containers/Assets/AssetDetails/AssetDetails.tsx
+++ src/lib/containers/Assets/AssetDetails/AssetDetails.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
-import { Asset } from '@cognite/sdk';
-import { Title } from '@cognite/cogs.js';
-import {
-  DetailsItem,
-  Loader,
-  ErrorFeedback,
-  TimeDisplay,
-} from 'lib/components';
+import { Asset, DataSet } from '@cognite/sdk';
+import { Body, Colors } from '@cognite/cogs.js';
+import { Loader, ErrorFeedback, TimeDisplay } from 'lib/components';
 import { useCdfItem } from '@cognite/sdk-react-query-hooks';
-import { formatMetadata } from 'lib/utils';
+import Typography from 'antd/lib/typography';
+import styled from 'styled-components';
+
+const { Text } = Typography;
 
 type Props = {
   id: number;
 };
+
+export const AssetDetailsItem = ({
+  name,
+  value,
+  copyable = false,
+}: {
+  name: string;
+  value?: any;
+  copyable?: boolean;
+}) => {
+  return (
+    <div style={{ display: 'inline-block', width: '50%', margin: '15px 0' }}>
+      <Body level={2}>{name}</Body>
+      <Text copyable={!!value && copyable}>{value || <em>Not set</em>}</Text>
+    </div>
+  );
+};
+
 export const AssetDetails = ({ id }: Props) => {
   const { data: asset, error, isFetched } = useCdfItem<Asset>('assets', { id });
+
+  const { data: dataset } = useCdfItem<DataSet>(
+    'datasets',
+    { id: asset?.dataSetId || 0 },
+    { enabled: !!asset && !!asset?.dataSetId }
+  );
 
   if (error) {
     return <ErrorFeedback error={error} />;
@@ -26,30 +48,35 @@ export const AssetDetails = ({ id }: Props) => {
 
   return (
     <div>
-      <Title level={4} style={{ marginTop: 12, marginBottom: 12 }}>
-        Details
-      </Title>
-      <DetailsItem name="ID" value={asset?.id} />
-      <DetailsItem name="Description" value={asset?.description} />
-      <DetailsItem name="Source" value={asset?.source} />
-      <DetailsItem name="External ID" value={asset?.externalId} />
-      <DetailsItem name="Parent ID" value={asset?.parentId} />
-      <DetailsItem
+      <AssetDetailsItem name="Description" value={asset?.description} />
+      <AssetDetailsItem name="External ID" value={asset?.externalId} copyable />
+      <AssetDetailsItem name="Data set" value={dataset?.name} />
+      <AssetDetailsItem name="ID" value={asset?.id} copyable />
+      <AssetDetailsItem
         name="Created at"
         value={asset ? <TimeDisplay value={asset.createdTime} /> : 'Loading...'}
       />
-      <DetailsItem
+      <AssetDetailsItem
         name="Updated at"
         value={
           asset ? <TimeDisplay value={asset.lastUpdatedTime} /> : 'Loading...'
         }
       />
-      <Title level={4} style={{ marginTop: 12, marginBottom: 12 }}>
-        Metadata
-      </Title>
-      {formatMetadata((asset && asset.metadata) ?? {}).map(el => (
-        <DetailsItem key={el.key} name={el.key} value={el.value} />
-      ))}
+      <AssetDetailsItem
+        name="Labels"
+        value={
+          asset?.labels
+            ? asset?.labels.map(label => <Label>{label.externalId}</Label>)
+            : undefined
+        }
+      />
     </div>
   );
 };
+
+const Label = styled.span`
+  background-color: ${Colors['greyscale-grey3'].hex()};
+  padding: 5px;
+  margin-right: 5px;
+  border-radius: 4px;
+`;
