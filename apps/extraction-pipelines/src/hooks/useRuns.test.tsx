51f6d875baaad1ff0227da7ec0b47cc8dedcf3f6
4ornui <roman.chernenkyi@cognite.com>
2020-11-26 19:40:44 +0200

    useRuns was covered by tests

---
+++ apps/extraction-pipelines/src/hooks/useRuns.test.tsx
@@ -0,0 +1,32 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { sdkv3 } from '@cognite/cdf-sdk-singleton';
+import { waitFor } from '@testing-library/react';
+import { useRuns } from './useRuns';
+import { mockError, mockDataRunsResponse } from '../utils/mockResponse';
+
+describe('useRuns', () => {
+  const externalId = 'dataIntegration000-1';
+
+  test('Returns runs on success', async () => {
+    sdkv3.get.mockResolvedValue({ data: { items: mockDataRunsResponse } });
+
+    await act(async () => {
+      const { result } = renderHook(() => useRuns(externalId));
+      await waitFor(() => {
+        expect(result.current.data).toEqual(mockDataRunsResponse);
+      });
+    });
+  });
+
+  test('Returns error on fail', async () => {
+    const rejectValue = mockError;
+    sdkv3.get.mockRejectedValue(rejectValue);
+
+    await act(async () => {
+      const { result } = renderHook(() => useRuns(externalId));
+      await waitFor(() => {
+        expect(result.current.error).toEqual(rejectValue);
+      });
+    });
+  });
+});
