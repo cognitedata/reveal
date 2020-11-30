72f913a967c8f4ba6eeb40be92b666e33b106abd
4ornui <roman.chernenkyi@cognite.com>
2020-11-30 16:14:59 +0200

    Monitoring.test for fail

--- apps/extraction-pipelines/src/components/tabs/Monitoring.test.tsx
+++ apps/extraction-pipelines/src/components/tabs/Monitoring.test.tsx
@@ -1,7 +1,7 @@
 import { sdkv3 } from '@cognite/cdf-sdk-singleton';
 import { screen, waitFor, render } from '@testing-library/react';
 import React from 'react';
-import { getMockResponse } from '../../utils/mockResponse';
+import { mockError, getMockResponse } from '../../utils/mockResponse';
 import Monitoring from '../tabs/Monitoring';
 
 describe('Monitoring', () => {
@@ -12,6 +12,17 @@ describe('Monitoring', () => {
     await waitFor(() => {
       const sidePanelHeading = screen.getByRole('table');
       expect(sidePanelHeading).toBeInTheDocument();
+    });
+  });
+
+  test('Render error on fail', async () => {
+    sdkv3.get.mockRejectedValue(mockError);
+    render(<Monitoring externalId={externalId} />);
+    await waitFor(() => {
+      const errorMessage = screen.getByText(
+        /Multiple authentication headers present/i
+      );
+      expect(errorMessage).toBeInTheDocument();
     });
   });
 });
