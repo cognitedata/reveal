5d6a668e47d830c400c827cda9cfe005a9c20ca1
4ornui <roman.chernenkyi@cognite.com>
2020-12-01 17:25:08 +0200

    switch from mock to API

--- apps/extraction-pipelines/src/utils/RunsAPI.ts
+++ apps/extraction-pipelines/src/utils/RunsAPI.ts
@@ -1,18 +1,14 @@
 import { get } from './baseURL';
 import { RunsAPIResponse, RunResponse } from '../model/Runs';
-import { mockDataRunsResponse } from '../utils/mockResponse';
 
 export const getRuns = async (
   project: string,
   externalId: string
 ): Promise<RunResponse[]> => {
-  /* const response = await get<RunsAPIResponse>(
+  const response = await get<RunsAPIResponse>(
     `/runs`,
     project,
     `?externalId=${externalId}`
-  ); */
-  const response = {
-    data: mockDataRunsResponse,
-  };
+  );
   return response.data.items;
 };
