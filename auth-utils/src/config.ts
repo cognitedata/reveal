f32958408341b6dad22dc5b8237cf34a933d2569
Frikk Fossan <frikk.fossan@cognite.com>
2020-12-10 11:04:15 +0100

    Support cra envs in auth-utils for ADFS (#127)
    * support cra envs in auth-utils
    
    * bump version

--- auth-utils/src/config.ts
+++ auth-utils/src/config.ts
@@ -1,8 +1,8 @@
 export default {
-  cluster: process.env.CLUSTER,
+  cluster: process.env.REACT_APP_CLUSTER,
   oidc: {
-    clientId: process.env.OIDC_CLIENT_ID,
-    authority: process.env.OIDC_AUTHORITY,
-    scope: process.env.OIDC_SCOPE,
+    clientId: process.env.REACT_APP_OIDC_CLIENT_ID,
+    authority: process.env.REACT_APP_OIDC_AUTHORITY,
+    scope: process.env.REACT_APP_OIDC_SCOPE,
   },
 };
