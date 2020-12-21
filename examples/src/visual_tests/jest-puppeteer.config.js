510787325ed9dfcfc122108ee073730b0ce6b7b7
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-12-21 16:44:34 +0100

    updated snapshots
    They differs when I run tests on mac only and when I have a separate screen plugged in.

--- examples/src/visual_tests/jest-puppeteer.config.js
+++ examples/src/visual_tests/jest-puppeteer.config.js
@@ -2,8 +2,14 @@ module.exports = {
   launch: {
     ignoreHTTPSErrors: true,
     // https://peter.sh/experiments/chromium-command-line-switches/
-    args: [],
+    args: ['--allow-insecure-localhost', '--window-size=800,600'],
     headless: true,
-    dumpio: true
-  }
-}
+    defaultViewport: {
+      width: 800,
+      height: 600,
+      deviceScaleFactor: 2, // https://github.com/puppeteer/puppeteer/issues/571
+      isMobile: false
+    },
+    dumpio: false, // get console output
+  },
+};
