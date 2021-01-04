3f3dd92a32995d1c0e8e6340e24d0774c7634e74
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2021-01-04 12:42:49 +0100

    move testCase enum

---
+++ examples/src/visual_tests/TestCase.ts
@@ -0,0 +1,11 @@
+export enum TestCase {
+  default = 'default',
+  clipping = 'clipping',
+  default_camera = 'default_camera',
+  highlight = 'highlight',
+  rotate_cad_model = 'rotate_cad_model',
+  node_transform = 'node_transform',
+  ghost_mode = 'ghost_mode',
+  scaled_model = 'scaled_model',
+  user_render_target = 'user_render_target',
+}
--- examples/src/visual_tests/basic.test.ts
+++ examples/src/visual_tests/basic.test.ts
@@ -1,20 +1,10 @@
+import { TestCase } from './TestCase';
+
 const retry = require('jest-retries');
 
 const RETRIES = parseInt(process.env.RETRIES!) || 3;
 
 jest.retryTimes(RETRIES);
-
-export enum TestCase {
-  default = 'default',
-  clipping = 'clipping',
-  default_camera = 'default_camera',
-  highlight = 'highlight',
-  rotate_cad_model = 'rotate_cad_model',
-  node_transform = 'node_transform',
-  ghost_mode = 'ghost_mode',
-  scaled_model = 'scaled_model',
-  user_render_target = 'user_render_target',
-}
 
 async function screenShotTest(testCase: TestCase) {
   const page = await browser.newPage();
