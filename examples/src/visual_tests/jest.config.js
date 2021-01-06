eaa8c3e1d6941e9997381e77bd26bbaddbe78386
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2021-01-06 15:51:29 +0100

    increase test timeout to allow retries

--- examples/src/visual_tests/jest.config.js
+++ examples/src/visual_tests/jest.config.js
@@ -8,5 +8,5 @@ module.exports = {
     globalSetup: "jest-environment-puppeteer/setup",
     globalTeardown: "jest-environment-puppeteer/teardown",
     testEnvironment: "jest-environment-puppeteer",
-    testTimeout: 10 * 1000
+    testTimeout: 30 * 1000 // typical test ~5s, so 5*retryTimes should be less than timeout or test will fail during retry
 };
