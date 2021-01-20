18d46bdf1ee98f7c21d44488a714886376fb9050
Milad Mobarhan <43236572+miladcognite@users.noreply.github.com>
2021-01-20 19:13:15 +0100

    Initial commit

---
+++ src/declaration.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.css';
+// https://github.com/ant-design/ant-design/issues/13405
+interface ResizeObserver {
+  observe(target: Element): void;
+  unobserve(target: Element): void;
+  disconnect(): void;
+}
