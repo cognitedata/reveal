c8462f394aa09da8af639965cd295567705f6447
Matt Murphy <63432827+mattmurph9@users.noreply.github.com>
2021-01-25 07:29:44 +0100

    Add QueryClient for stories (#485)
    Co-authored-by: Matt Murphy <matt@Matts-MacBook-Pro.local>

--- src/lib/.storybook/preview.js
+++ src/lib/.storybook/preview.js
@@ -7,18 +7,23 @@ import { Container, sdkMock } from '../docs/stub';
 import { DataExplorationProvider } from '../context';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: { expanded: true },
 };
 
+const queryClient = new QueryClient();
+
 export const decorators = [
   (storyFn) => (
     <Container>
       <Router history={createBrowserHistory()}>
         <DataExplorationProvider sdk={sdkMock}>
-          {storyFn()}
+          <QueryClientProvider client={queryClient}>
+            {storyFn()}
+          </QueryClientProvider>
         </DataExplorationProvider>
       </Router>
     </Container>
