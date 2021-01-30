c56d1456a074d9f6682deff183474bc794d77711
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2021-01-30 10:18:16 +0100

    style: flatten code with async/await

--- parser-worker/uploadToCDN.js
+++ parser-worker/uploadToCDN.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const fs = require("fs");
 
 const { Storage } = require("@google-cloud/storage");
 const compressible = require("compressible");
@@ -22,7 +21,7 @@ function getRelativeFilesFromDirectory(directoryPath) {
   );
 }
 
-(function upload() {
+(async function upload() {
   const bucketName = "apps-cdn-bucket-cognitedata-production";
   const gcsPath = getWorkerCDNFolderPath();
 
@@ -30,53 +29,53 @@ function getRelativeFilesFromDirectory(directoryPath) {
 
   console.log(`Using ${buildPath} as a build source ...`);
 
-  return getRelativeFilesFromDirectory(buildPath).then((fileNames) => {
-    const storage = new Storage({
-      credentials: JSON.parse(process.env.APPS_CDN_SERVICE_ACCOUNT_CREDENTIALS),
-    });
-    const bucket = storage.bucket(bucketName);
-    console.log(
-      `Uploading ${fileNames.length} files to ${bucketName}/${gcsPath} ... \n`,
-      JSON.stringify(fileNames, null, 2)
-    );
-    const startMillis = Date.now();
+  const fileNames = await getRelativeFilesFromDirectory(buildPath);
 
-    const promises = fileNames.map(async (fileName) => {
-      const sourcePath = path.join(buildPath, fileName);
-      const destinationPath = path.join(gcsPath, fileName);
-      const mimeType = mime.lookup(sourcePath);
-      const gzip = compressible(mimeType || "");
-      console.log(`Uploading ${fileName} to ${bucketName}/${destinationPath}`, {
-        gzip,
-      });
-
-      const file = bucket.file(destinationPath);
-      const [doesFileExist] = await file.exists();
-
-      if (doesFileExist) {
-        console.log(
-          `Skipping upload since the file ${destinationPath} already exists`
-        );
-        return Promise.resolve();
-      }
-
-      return bucket.upload(sourcePath, {
-        destination: destinationPath,
-        resumable: false,
-        gzip,
-        metadata: {
-          cacheControl: "public, max-age=31536000",
-        },
-      });
-    });
-
-    return Promise.all(promises)
-      .then(() => {
-        console.log(`... all done after ${Date.now() - startMillis}ms`);
-      })
-      .catch((e) => {
-        console.error("Unable to upload all files");
-        throw e;
-      });
+  const storage = new Storage({
+    credentials: JSON.parse(process.env.APPS_CDN_SERVICE_ACCOUNT_CREDENTIALS),
   });
+  const bucket = storage.bucket(bucketName);
+
+  console.log(
+    `Uploading ${fileNames.length} files to ${bucketName}/${gcsPath} ... \n`,
+    JSON.stringify(fileNames, null, 2)
+  );
+  const startMillis = Date.now();
+
+  const promises = fileNames.map(async (fileName) => {
+    const sourcePath = path.join(buildPath, fileName);
+    const destinationPath = path.join(gcsPath, fileName);
+    const mimeType = mime.lookup(sourcePath);
+    const gzip = compressible(mimeType || "");
+    console.log(`Uploading ${fileName} to ${bucketName}/${destinationPath}`, {
+      gzip,
+    });
+
+    const file = bucket.file(destinationPath);
+    const [doesFileExist] = await file.exists();
+
+    if (doesFileExist) {
+      console.log(
+        `Skipping upload since the file ${destinationPath} already exists`
+      );
+      return Promise.resolve();
+    }
+
+    return bucket.upload(sourcePath, {
+      destination: destinationPath,
+      resumable: false,
+      gzip,
+      metadata: {
+        cacheControl: "public, max-age=31536000",
+      },
+    });
+  });
+
+  try {
+    await Promise.all(promises);
+    console.log(`... all done after ${Date.now() - startMillis}ms`);
+  } catch (e) {
+    console.error("Unable to upload all files");
+    throw e;
+  }
 })();
