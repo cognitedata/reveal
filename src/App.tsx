6d1d817f59807e12df01cbc22a56b1c3147433dc
adam tombleson <adam.tombleson@cognite.com>
2021-02-24 13:02:38 +0100

    feat: update container to pass in sidecar (#587)
    * feat: update container
    
    * chore: update lock
    
    * feat: simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import GlobalStyles from 'global-styles';
 import { Switch, Redirect, Route } from 'react-router-dom';
-import { Loader } from '@cognite/cogs.js';
 import { Container } from '@cognite/react-container';
 
+import sidecar from 'utils/sidecar';
 import Info from 'pages/Info';
 import Home from 'pages/Home';
 import CogniteSDK from 'pages/CogniteSDK';
@@ -11,25 +11,21 @@ import { MenuBar, PAGES } from 'pages/Menubar';
 import NotFoundPage from 'pages/Error404';
 
 const App = () => (
-  <>
-    <GlobalStyles />
-    <React.Suspense fallback={<Loader />}>
-      <Container>
-        <>
-          <MenuBar />
+  <Container sidecar={sidecar}>
+    <>
+      <GlobalStyles />
+      <MenuBar />
 
-          <Switch>
-            <Route path={PAGES.HOME} render={() => <Home />} />
-            <Route path={PAGES.INFO} render={() => <Info />} />
-            <Route path={PAGES.SDK} render={() => <CogniteSDK />} />
-            <Redirect from="" to={PAGES.HOME} />
-            <Redirect from="/" to={PAGES.HOME} />
-            <Route render={() => <NotFoundPage />} />
-          </Switch>
-        </>
-      </Container>
-    </React.Suspense>
-  </>
+      <Switch>
+        <Route path={PAGES.HOME} render={() => <Home />} />
+        <Route path={PAGES.INFO} render={() => <Info />} />
+        <Route path={PAGES.SDK} render={() => <CogniteSDK />} />
+        <Redirect from="" to={PAGES.HOME} />
+        <Redirect from="/" to={PAGES.HOME} />
+        <Route render={() => <NotFoundPage />} />
+      </Switch>
+    </>
+  </Container>
 );
 
 export default App;
--- src/utils/sidecar.ts
+++ src/utils/sidecar.ts
@@ -1,46 +1,63 @@
 /* eslint-disable no-underscore-dangle */
-import merge from 'lodash/merge';
-
 type Sidecar = {
   __sidecarFormatVersion: number;
   applicationId: string;
   applicationName: string;
   appsApiBaseUrl: string;
   cdfApiBaseUrl: string;
+  cdfCluster: string;
   docsSiteBaseUrl: string;
   nomaApiBaseUrl: string;
+};
+
+// # -------------------------------------
+// #
+// #
+// #
+// # ONLY CHANGE THESE THINGS: (affects localhost only)
+// #
+// #
+const PROD = false;
+const CLUSTER = 'bluefield';
+// #
+// #
+// #
+// # -------------------------------------
+
+const generateBaseUrls = (cluster: string, prod: boolean = false) => {
+  switch (cluster) {
+    case 'ew1': {
+      return {
+        appsApiBaseUrl: 'https://apps-api.staging.cognite.ai',
+        cdfApiBaseUrl: 'https://api.cognitedata.com',
+        cdfCluster: '',
+      };
+    }
+    default: {
+      return {
+        aadApplicationId: '245a8a64-4142-4226-86fa-63d590de14c9', // bluefield
+        appsApiBaseUrl: prod
+          ? `https://apps-api.${cluster}.cognite.ai`
+          : `https://apps-api.staging.${cluster}.cognite.ai`,
+        cdfApiBaseUrl: `https://${cluster}.cognitedata.com`,
+        cdfCluster: cluster,
+      };
+    }
+  }
 };
 
 // we are overwriting the window.__cogniteSidecar object because the tenant-selector
 // reads from this variable, so when you test on localhost, it (TSA) will not access via this file
 // but via the window.__cogniteSidecar global
 // now that this var is updated, all should work as expected.
-(window as any).__cogniteSidecar = merge(
-  {},
-  {
-    // -- Bluefield -- (Azure)
-    // appsApiBaseUrl: 'https://apps-api.bluefield.cognite.ai',
-    // cdfApiBaseUrl: 'https://bluefield.cognitedata.com',
+(window as any).__cogniteSidecar = {
+  ...generateBaseUrls(CLUSTER, PROD),
+  __sidecarFormatVersion: 1,
+  applicationId: 'fas-demo',
+  applicationName: 'React Demo (staging)',
+  docsSiteBaseUrl: 'https://docs.cognite.com',
+  nomaApiBaseUrl: 'https://noma.development.cognite.ai',
+  ...((window as any).__cogniteSidecar || {}),
+};
 
-    // -- EW1 --
-    appsApiBaseUrl: 'https://apps-api.staging.cognite.ai',
-    cdfApiBaseUrl: 'https://api.cognitedata.com',
-
-    // -- Greenfield --
-    // appsApiBaseUrl: 'https://apps-api.greenfield.cognite.ai',
-    // cdfApiBaseUrl: 'https://greenfield.cognitedata.com',
-
-    cdfCluster: '',
-
-    __sidecarFormatVersion: 1,
-    applicationId: 'fas-demo',
-    applicationName: 'React Demo (staging)',
-    docsSiteBaseUrl: 'https://docs.cognite.com',
-    nomaApiBaseUrl: 'https://noma.development.cognite.ai',
-  },
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  // eslint-disable-next-line no-underscore-dangle
-  (window as any).__cogniteSidecar
-) as Sidecar;
-
-export default (window as any).__cogniteSidecar;
+export default (window as any).__cogniteSidecar as Sidecar;
