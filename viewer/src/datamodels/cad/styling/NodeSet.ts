d4ec71134458df9466113b51e005a0169fcfb3b5
Lars Moastuen <lars.moastuen@cognite.com>
2021-02-25 22:43:44 +0100

    chore: assert

--- viewer/src/datamodels/cad/styling/NodeSet.ts
+++ viewer/src/datamodels/cad/styling/NodeSet.ts
@@ -2,6 +2,7 @@
  * Copyright 2021 Cognite AS
  */
 
+import { assert } from 'console';
 import { EventTrigger } from '../../../utilities/events/EventTrigger';
 import { IndexSet } from '../../../utilities/IndexSet';
 
@@ -9,10 +10,12 @@ export abstract class NodeSet {
   private readonly _changedEvent = new EventTrigger<() => void>();
 
   on(_event: 'changed', listener: () => void): void {
+    assert(_event === 'changed');
     this._changedEvent.subscribe(listener);
   }
 
   off(_event: 'changed', listener: () => void): void {
+    assert(_event === 'changed');
     this._changedEvent.unsubscribe(listener);
   }
 
