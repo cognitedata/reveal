e727bbc36047aed99759c35b0fdf6b27e44098c6
Lars Moastuen <lars.moastuen@cognite.com>
2021-03-09 23:01:07 +0100

    chore: remove misplaced export and TODO

--- viewer/src/utilities/index.ts
+++ viewer/src/utilities/index.ts
@@ -2,18 +2,10 @@
  * Copyright 2021 Cognite AS
  */
 
-// TODO 02-06-2020 j-bjorne: Make index file the source of import for other packages? Adding all relative sub exports to index.ts
-// This would enable/enforce :
-// import { NetworkBlaBlaBla, CogniteClient } from '../utilities';
-// rather than
-// import NetworkBlaBlaBla from './network/types';
-// import CogniteClient from './network/CogniteClient';
-
 export { File3dFormat, CameraConfiguration, CogniteColors, LoadingState } from './types';
 export { LocalModelIdentifier, CdfModelIdentifier } from './networking/types';
 export { createOffsetsArray } from './arrays';
 export { Box3 } from './Box3';
-export { HtmlOverlayTool as HtmlOverlayHelper } from '../tools/HtmlOverlayTool';
 export { worldToViewport } from './worldToViewport';
 export { toThreeVector3, toThreeJsBox3, fromThreeJsBox3 } from './threeConverters';
 export { BoundingBoxClipper } from './BoundingBoxClipper';
