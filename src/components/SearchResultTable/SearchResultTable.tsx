9df8a675d0196b96d7492c9a8efa2defbf83d08e
Vegard Økland <vegard.okland@cognite.com>
2021-03-13 17:06:00 +0100

    Update design

--- src/components/SearchResultTable/SearchResultTable.tsx
+++ src/components/SearchResultTable/SearchResultTable.tsx
@@ -42,65 +42,101 @@ export const SearchResultTable = ({
   return (
     <AssetList>
       {assets.map(({ asset, ts }) => (
-        <li key={asset.id}>
-          <Icon type="ResourceAssets" />
-          <strong style={{ marginLeft: 10 }}>{asset.name}</strong>
-          <span style={{ marginLeft: 10 }}>{asset.description}</span>
-          <TSList>
-            {ts.map((t) => (
-              <li key={t.id}>
-                <p>
-                  <Icon type="ResourceTimeseries" />
-                  {t.name}
-                </p>
-                <p>{t.description}</p>
-                <TimeseriesChart
-                  height={65}
-                  showSmallerTicks
-                  timeseriesId={t.id}
-                  numberOfPoints={100}
-                  showAxis="horizontal"
-                  timeOptions={[]}
-                  showContextGraph={false}
-                  showPoints={false}
-                  enableTooltip={false}
-                  showGridLine="none"
-                  minRowTicks={2}
-                  dateRange={[sparklineStartDate, sparklineEndDate]}
-                />
-                <div>
-                  <Button type="link" onClick={() => onTimeseriesClick(t)}>
-                    <Icon type="Plus" />
-                  </Button>
-                </div>
-              </li>
-            ))}
-          </TSList>
+        <li>
+          <AssetItem key={asset.id}>
+            <Row>
+              <div style={{ padding: 5 }}>
+                <Icon type="ResourceAssets" />
+              </div>
+              <strong style={{ marginLeft: 10 }}>{asset.name}</strong>
+              <span style={{ marginLeft: 10, flexGrow: 2 }}>
+                {asset.description}
+              </span>
+              <AssetCount>{ts.length} </AssetCount>
+            </Row>
+            <Row>
+              <TSList>
+                {ts.map((t) => (
+                  <TSItem key={t.id}>
+                    <Row>
+                      <div style={{ padding: 5 }}>
+                        <Icon type="ResourceTimeseries" />
+                      </div>
+                      <span>{t.name}</span>
+                      <span>{t.description}</span>
+                      <TimeseriesChart
+                        height={65}
+                        showSmallerTicks
+                        timeseriesId={t.id}
+                        numberOfPoints={100}
+                        showAxis="horizontal"
+                        timeOptions={[]}
+                        showContextGraph={false}
+                        showPoints={false}
+                        enableTooltip={false}
+                        showGridLine="none"
+                        minRowTicks={2}
+                        dateRange={[sparklineStartDate, sparklineEndDate]}
+                      />
+
+                      <Button
+                        type="link"
+                        style={{ padding: 5 }}
+                        onClick={() => onTimeseriesClick(t)}
+                      >
+                        <Icon type="Plus" />
+                      </Button>
+                    </Row>
+                  </TSItem>
+                ))}
+              </TSList>
+            </Row>
+          </AssetItem>
         </li>
       ))}
     </AssetList>
   );
 };
 
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const AssetList = styled.ul`
   height: 100%;
   overflow: auto;
   list-style: none;
-  padding: 0 10px;
-  li {
-    border-bottom: 1px solid rgb(232, 232, 232);
-    margin-bottom: 6px;
-    paddding-bottom: 6px;
-  }
+  padding: 0;
+  margin: 0;
+`;
+
+const AssetItem = styled.div`
+  border: 1px solid var(--cogs-greyscale-grey4);
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 10px 15px 0px 15px;
+`;
+
+const AssetCount = styled.span`
+  border: 1px solid var(--cogs-greyscale-grey4);
+  border-radius: 5px;
+  float: right;
+  padding: 5px;
 `;
 
 const TSList = styled.ul`
+  width: 100%;
+  padding: 0;
+  margin: 10px 0 10px 0;
   list-style: none;
-  li {
-    display: flex;
-    flex-direction: row;
-  }
-  .cogs-icon {
-    margin: 0 10px;
+`;
+
+const TSItem = styled.li`
+  border-radius: 5px;
+  :nth-child(odd) {
+    background-color: var(--cogs-greyscale-grey2);
   }
 `;
