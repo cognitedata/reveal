d779772b943f32ae97f7dd56bd8c8cbe0c47262f
Vegard Økland <vegard.okland@cognite.com>
2021-03-16 11:34:37 +0100

    Tweak test setup

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,12 +7,8 @@ cd "$(dirname "$0")"
 yarn react-scripts test "$@" \
   --collectCoverageFrom='!*/**/*.stories.tsx' \
   --ci \
-  --coverage \
   --verbose \
-  --reporters=default \
-  --reporters=jest-junit \
   --runInBand \
-  --watchAll=false \
   --detectOpenHandles \
   --env=jest-environment-jsdom-sixteen \
   "$@"
