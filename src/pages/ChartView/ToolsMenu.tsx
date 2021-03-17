7cf543e48e5182ddbaa15c2c0e4b14c644a8e4b9
Vegard Økland <vegard@okland.org>
2021-03-17 08:38:37 +0100

    PI-139 close row menu on actions (#309)
    * Close menu on updating ts/wf
    
    * Add chart transformations
    
    * Move ts conversion function to charts package
    
    * Different close menu approach
    
    * Copy comment over
    
    Co-authored-by: Vegard Økland <vegard.okland@cognite.com>

---
+++ src/pages/ChartView/ToolsMenu.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Menu } from '@cognite/cogs.js';
+import { AppearanceDropdown } from 'components/AppearanceDropdown';
+
+type Props = {
+  update: (diff: any) => void;
+};
+export default function ToolsMenu({ update }: Props) {
+  return (
+    <Menu.Submenu
+      content={
+        <Menu>
+          <Menu.Submenu
+            content={
+              <AppearanceDropdown
+                onColorSelected={(newColor) =>
+                  update({
+                    color: newColor,
+                  })
+                }
+                onWeightSelected={(newWeight) =>
+                  update({
+                    lineWeight: newWeight,
+                  })
+                }
+                onStyleSelected={(newStyle) =>
+                  update({
+                    lineStyle: newStyle,
+                  })
+                }
+              />
+            }
+          >
+            <span>Appearance</span>
+          </Menu.Submenu>
+          <Menu.Submenu
+            content={
+              <Menu>
+                <Menu.Item>Gaps</Menu.Item>
+                <Menu.Item>Freshness</Menu.Item>
+                <Menu.Item>Drift Detector</Menu.Item>
+              </Menu>
+            }
+          >
+            <span>Data Quality</span>
+          </Menu.Submenu>
+          <Menu.Item>Min / Max</Menu.Item>
+          <Menu.Item>Limit</Menu.Item>
+        </Menu>
+      }
+    >
+      <span>Tools</span>
+    </Menu.Submenu>
+  );
+}
--- src/utils/charts.spec.ts
+++ src/utils/charts.spec.ts
@@ -1,18 +1,26 @@
 import { nanoid } from 'nanoid';
-import { Chart } from 'reducers/charts/types';
-import { duplicate } from './charts';
+import { Chart, ChartTimeSeries, ChartWorkflow } from 'reducers/charts/types';
+import {
+  addTimeseries,
+  addWorkflow,
+  duplicate,
+  removeTimeseries,
+  removeWorkflow,
+  updateTimeseries,
+  updateWorkflow,
+} from './charts';
 
 describe('charts util', () => {
+  const id = nanoid();
+  const chart: Chart = {
+    id,
+    name: 'test chart',
+    user: 'user_1@example.org',
+    public: true,
+    dateFrom: 'then',
+    dateTo: 'now',
+  };
   describe('duplicate', () => {
-    const id = nanoid();
-    const chart: Chart = {
-      id,
-      name: 'test chart',
-      user: 'user_1@example.org',
-      public: true,
-      dateFrom: 'then',
-      dateTo: 'now',
-    };
     it('should update the appropriate fields', () => {
       expect(duplicate(chart, 'user_2@example.org').public).toBe(false);
       expect(duplicate(chart, 'user_2@example.org').user).toEqual(
@@ -26,6 +34,128 @@ describe('charts util', () => {
     it('should not update the other fields', () => {
       expect(duplicate(chart, 'user_2@example.org').dateFrom).toEqual('then');
       expect(duplicate(chart, 'user_2@example.org').dateTo).toEqual('now');
+    });
+  });
+  describe('ChartTimerseries functions', () => {
+    const ts: ChartTimeSeries = {
+      id: '42',
+      name: 'ts1',
+      color: 'red',
+      enabled: true,
+      tsId: 42,
+    };
+    const chartWithTS: Chart = {
+      ...chart,
+      timeSeriesCollection: [ts],
+    };
+    describe('updateTimeseries', () => {
+      it('should do nothing for unknown ts', () => {
+        expect(updateTimeseries(chart, 'foo', { color: 'red' })).toEqual(chart);
+        expect(
+          updateTimeseries(
+            {
+              ...chart,
+              timeSeriesCollection: [],
+            },
+            '42',
+            { color: 'red' }
+          )
+        ).toEqual({
+          ...chart,
+          timeSeriesCollection: [],
+        });
+      });
+      it('should update existing ts', () => {
+        expect(
+          updateTimeseries(chartWithTS, '42', { color: 'blue', enabled: false })
+        ).toEqual({
+          ...chart,
+          timeSeriesCollection: [
+            {
+              ...ts,
+              enabled: false,
+              color: 'blue',
+            },
+          ],
+        });
+      });
+    });
+    describe('removeTimeseries', () => {
+      it('should remove time series, if found', () => {
+        expect(removeTimeseries(chartWithTS, '42')).toEqual({
+          ...chart,
+          timeSeriesCollection: [],
+        });
+      });
+      it('should remove return an unchanged chart for unknown ids', () => {
+        expect(removeTimeseries(chartWithTS, '44')).toEqual(chartWithTS);
+      });
+    });
+    describe('addTimeseries', () => {
+      it('should add timeseries to charts', () => {
+        expect(addTimeseries(chart, ts)).toEqual(chartWithTS);
+      });
+    });
+  });
+
+  describe('ChartWorkflow functions', () => {
+    const wf: ChartWorkflow = {
+      id: '42',
+      name: 'ts1',
+      color: 'red',
+      enabled: true,
+    };
+    const chartWithWF: Chart = {
+      ...chart,
+      workflowCollection: [wf],
+    };
+    describe('updateWorkflow', () => {
+      it('should do nothing for unknown wf', () => {
+        expect(updateWorkflow(chart, 'foo', { color: 'red' })).toEqual(chart);
+        expect(
+          updateWorkflow(
+            {
+              ...chart,
+              workflowCollection: [],
+            },
+            '42',
+            { color: 'red' }
+          )
+        ).toEqual({
+          ...chart,
+          workflowCollection: [],
+        });
+      });
+      it('should update existing ts', () => {
+        expect(
+          updateWorkflow(chartWithWF, '42', { color: 'blue', enabled: false })
+        ).toEqual({
+          ...chart,
+          workflowCollection: [
+            {
+              ...wf,
+              enabled: false,
+              color: 'blue',
+            },
+          ],
+        });
+      });
+    });
+    describe('removeWorkflow', () => {
+      it('should remove time series, if found', () => {
+        expect(removeWorkflow(chartWithWF, '42')).toEqual({
+          ...chart,
+          workflowCollection: [],
+        });
+      });
+      it('should remove return an unchanged chart for unknown ids', () => {
+        expect(removeWorkflow(chartWithWF, '44')).toEqual(chartWithWF);
+      });
+    });
+    describe('addWorkflow', () => {
+      it('should add timeseries to charts', () => {
+        expect(addWorkflow(chart, wf)).toEqual(chartWithWF);
+      });
     });
   });
 });
