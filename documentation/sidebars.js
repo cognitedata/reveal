ffa18e1bd7a900f76ce15ddf67b702ffeadb1c7a
Lars Moastuen <lars.moastuen@cognite.com>
2021-03-23 15:41:11 +0100

    doc: document how to combine two models and align coordinate systems [REV-133] (#1406)

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -31,7 +31,8 @@ module.exports = {
         'examples/pointcloud',
         'examples/pointcloud-intersections',
         'examples/antialiasing',
-        'examples/cad-explode'
+        'examples/cad-explode',
+        'examples/combine-models'
       ],
     },
     {
