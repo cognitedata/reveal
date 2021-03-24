cd7c2ea922afd884a24c70cccae88a3a229366f3
issaafalkattan <issaaf.kattan@cognite.com>
2021-03-24 22:10:03 +0100

    Feat: data set select

---
+++ src/components/DataSetSelect/DataSetSelect.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { action } from '@storybook/addon-actions';
+import { SDKProvider } from '@cognite/sdk-provider';
+import { sdkMock } from 'docs/stub';
+import { DataSetSelect, DataSetSelectProps } from './DataSetSelect';
+
+export default {
+  title: 'Component/DataSetSelect',
+  component: DataSetSelect,
+  decorators: [
+    (storyFn: any) => (
+      <Container>
+        <SDKProvider sdk={sdkMock}>{storyFn()}</SDKProvider>
+      </Container>
+    ),
+  ],
+};
+
+export const Example = () => (
+  <DataSetSelect
+    onSelectionChange={action('onSelectionChange')}
+    style={{ minWidth: '306px', maxHeight: '36px' }}
+    multiple
+    selectedDataSetIds={[]}
+  />
+);
+
+export const SingleSelection = () => (
+  <DataSetSelect
+    onSelectionChange={action('onSelectionChange')}
+    style={{ minWidth: '306px', maxHeight: '36px' }}
+    multiple={false}
+    selectedDataSetIds={[]}
+  />
+);
+
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+`;
