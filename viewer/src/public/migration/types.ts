3da8ce36319ff9eab4985a24b6b16ec821c5e1b0
Lars Moastuen <lars.moastuen@cognite.com>
2021-04-07 20:10:06 +0200

    doc: improve doc for new setting

--- viewer/src/public/migration/types.ts
+++ viewer/src/public/migration/types.ts
@@ -144,15 +144,19 @@ export interface Cognite3DViewerOptions {
  */
 export interface GeometryFilter {
   /**
-   * The bounds to load geometry within. By default this box is in CDF coordinate space.
+   * The bounds to load geometry within. By default this box is in CDF coordinate space which
+   * will be transformed into coordinates relative to the model using the the model transformation
+   * which can be specified using {@link https://docs.cognite.com/api/v1/#operation/update3DRevisions |the CDF API},
+   * or set in {@link https://fusion.cognite.com/ |Cognite Fusion}.
    * @see {@link isBoundingBoxInModelCoordinates}.
    */
   boundingBox?: THREE.Box3;
 
   /**
-   * When set, the geometry filter {@link boundingBox} will be considered to be in model space,
-   * rather than CDF space which is the default. Model space accounts for the model transformation
-   * which can be specified using the CDF API, or set in Fusion.
+   * When set, the geometry filter {@link boundingBox} will be considered to be in "Reveal space".
+   * Rather than CDF space which is the default. When using Reveal space, the model transformation
+   * which can be specified using {@link https://docs.cognite.com/api/v1/#operation/update3DRevisions |the CDF API},
+   * or set in {@link https://fusion.cognite.com/ |Cognite Fusion}.
    * @version New in 1.5.0
    */
   isBoundingBoxInModelCoordinates?: boolean;
