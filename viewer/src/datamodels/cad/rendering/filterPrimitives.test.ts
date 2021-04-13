c324c49ee7118ee8f072093e573b6671aa204650
Lars Moastuen <lars.moastuen@cognite.com>
2021-04-13 20:22:54 +0200

    Test filterPrimitivesOutsideClipBoxByVertices

--- viewer/src/datamodels/cad/rendering/filterPrimitives.test.ts
+++ viewer/src/datamodels/cad/rendering/filterPrimitives.test.ts
@@ -5,7 +5,10 @@
 import * as THREE from 'three';
 import { ParsePrimitiveAttribute } from '@cognite/reveal-parser-worker';
 
-import { filterPrimitivesOutsideClipBoxByCenterAndRadius } from './filterPrimitives';
+import {
+  filterPrimitivesOutsideClipBoxByCenterAndRadius,
+  filterPrimitivesOutsideClipBoxByVertices
+} from './filterPrimitives';
 
 describe('filterPrimitivesOutsideClipBoxByCenterAndRadius', () => {
   let attributes: Map<string, ParsePrimitiveAttribute>;
@@ -46,4 +49,45 @@ describe('filterPrimitivesOutsideClipBoxByCenterAndRadius', () => {
   function createElement(center: [number, number, number], radius: number): [number, number, number, number] {
     return [...center, radius];
   }
+});
+
+describe('filterPrimitivesOutsideClipBoxByVertices', () => {
+  let attributes: Map<string, ParsePrimitiveAttribute>;
+
+  beforeEach(() => {
+    attributes = new Map<string, ParsePrimitiveAttribute>([
+      ['vertex1', { offset: 0, size: 3 * 4 }],
+      ['vertex2', { offset: 12, size: 3 * 4 }],
+      ['vertex3', { offset: 24, size: 3 * 4 }],
+      ['vertex4', { offset: 36, size: 3 * 4 }]
+    ]);
+  });
+
+  test('no clipbox, returns original', () => {
+    const original = new Uint8Array(48);
+    const filtered = filterPrimitivesOutsideClipBoxByVertices(original, attributes, undefined);
+    expect(filtered).toBe(original);
+  });
+
+  test('one accepted, one reject - returns filtered', () => {
+    // Arrange
+    const original = new Float32Array([
+      // Element 1
+      ...[1, 1, 1],
+      ...[2, 2, 2],
+      ...[3, 3, 3],
+      ...[4, 4, 4],
+      // Element 2
+      ...[11, 11, 11],
+      ...[12, 12, 12],
+      ...[13, 13, 13],
+      ...[14, 14, 14]
+    ]);
+    const bytes = new Uint8Array(original.buffer);
+    const clipBox = new THREE.Box3(new THREE.Vector3(0, 0, 0), new THREE.Vector3(2, 2, 2));
+
+    const filtered = filterPrimitivesOutsideClipBoxByVertices(bytes, attributes, clipBox);
+    const filteredFloats = new Float32Array(filtered.buffer);
+    expect(filteredFloats).toEqual(original.subarray(0, original.length / 2));
+  });
 });
