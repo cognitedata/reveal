63f1e2b1789fb59dc0067cc7b4e38a1e55472f43
Begum Ozcan-Caliskaner <begum.ozcan@cognite.com>
2021-04-21 14:06:32 +0200

    Use ghost buttons for date range selector (#383)

--- src/components/DateRangeSelector/DateRangeSelector.tsx
+++ src/components/DateRangeSelector/DateRangeSelector.tsx
@@ -75,6 +75,7 @@ const DateRangeSelector = ({ chart }: DateRangeSelectorProps) => {
                 dateTo: option.dateTo().toDate(),
               })
             }
+            variant="ghost"
           >
             {option.label}
           </Button>
