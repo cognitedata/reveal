13c0e4c9deb3ff219c3a3eb6c11740404a3b077e
Milad Mobarhan <milad.mobarhan@cognite.com>
2021-04-23 00:30:28 +0200

    revert

--- src/components/MapView/constants.ts
+++ src/components/MapView/constants.ts
@@ -1,3 +1,4 @@
 export const MAPBOX_TOKEN =
   'pk.eyJ1IjoiY29nbml0ZS12aXNpb24iLCJhIjoiY2tucTNpczhqMDIyejJvbzd3MThpd2hsNiJ9.fg3or9hNcwZ0lcEoTcCmDQ';
-export const MAPBOX_MAP_ID = 'mapbox://styles/mapbox/light-v9';
+export const MAPBOX_MAP_ID =
+  'mapbox://styles/cognite-vision/cknq4re52018l17mzocn4q6dl';
