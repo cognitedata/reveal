fa5eab210ccefeec43622bda44a458378a24bdd0
Milad Mobarhan <milad.mobarhan@cognite.com>
2021-04-23 00:24:23 +0200

    use standard style

--- src/components/MapView/constants.ts
+++ src/components/MapView/constants.ts
@@ -1,4 +1,3 @@
 export const MAPBOX_TOKEN =
   'pk.eyJ1IjoiY29nbml0ZS12aXNpb24iLCJhIjoiY2tucTNpczhqMDIyejJvbzd3MThpd2hsNiJ9.fg3or9hNcwZ0lcEoTcCmDQ';
-export const MAPBOX_MAP_ID =
-  'mapbox://styles/cognite-vision/cknq4re52018l17mzocn4q6dl';
+export const MAPBOX_MAP_ID = 'mapbox://styles/mapbox/light-v9';
