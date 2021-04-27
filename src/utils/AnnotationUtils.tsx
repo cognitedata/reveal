184bc78bc5547f3299564de5d47e23433563b6c4
issaafalkattan <issaaf.kattan@cognite.com>
2021-04-27 13:12:07 +0200

    Fix createPendingAnnotationsFromJob util

--- src/utils/AnnotationUtils.tsx
+++ src/utils/AnnotationUtils.tsx
@@ -61,7 +61,7 @@ export const createPendingAnnotationsFromJob = async (
           resourceId
         )
       ) {
-        return prev;
+        return;
       }
       prev.push({
         box: entity.boundingBox,
