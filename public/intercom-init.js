0a0f1c286249ebb6acb703a6db4a4c1e204399d8
Ivan Boholiubskyi <ivan.bogoliubskyi@cognite.com>
2021-04-29 11:52:57 +0300

    PI-234: Moved inline script to separate file

---
+++ public/intercom-init.js
@@ -0,0 +1,9 @@
+var APP_ID = 'ou1uyk2p';
+
+window.intercomSettings = {
+  app_id: APP_ID,
+  hide_default_launcher: true,
+  alignment: 'left',
+  horizontal_padding: 220,
+  vertical_padding: 20,
+};
