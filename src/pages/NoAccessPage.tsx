a077fec5b227191261a5ba0ec943cb85decb04d5
Gayan Deegala <Gayan@CreativeSoftware.com>
2021-05-10 18:25:18 +0530

    home button

--- src/pages/NoAccessPage.tsx
+++ src/pages/NoAccessPage.tsx
@@ -1,5 +1,5 @@
-import { Body, Graphic, Title } from '@cognite/cogs.js';
 import React from 'react';
+import { Body, Button, Graphic, Title } from '@cognite/cogs.js';
 import { AccessPermission } from 'src/utils/types';
 import styled from 'styled-components';
 
@@ -19,40 +19,73 @@ const getPrerequisiteList = (capabilities: Array<AccessPermission>) =>
 
 export default function NoAccessPage({ capabilities }: Props) {
   return (
-    <Frame>
-      <div>
-        <TitleContainer>
-          <Title level={2}>Request access to Contextualize Imagery Data</Title>
-        </TitleContainer>
-        <BodyContainer>
-          <Body level={1}>
-            You don’t have an access to view this page yet. Check the access
-            rights needed below:
-          </Body>
-        </BodyContainer>
-        <Infobox>
-          <ListContainer>
-            <Body level={2}>
-              It is prerequisite to have:
-              <ul>{getPrerequisiteList(capabilities)}</ul>
+    <>
+      <HomeButton>
+        <Button
+          size="default"
+          variant="default"
+          type="ghost"
+          icon="ArrowLeft"
+          iconPlacement="left"
+          href="https://dev.fusion.cogniteapp.com/computervision-test/?env=greenfield"
+        >
+          Home
+        </Button>
+      </HomeButton>
+      <Frame>
+        <div>
+          <TitleContainer>
+            <Title level={2}>
+              Request access to Contextualize Imagery Data
+            </Title>
+          </TitleContainer>
+          <BodyContainer>
+            <Body level={1}>
+              You don’t have an access to view this page yet. Check the access
+              rights needed below:
             </Body>
-          </ListContainer>
-        </Infobox>
-        <BodyContainer>
-          <Body level={1}>
-            Ask those responsible within your organisation for access management
-            to grant them to you.
-            <br />
-            Learn more about access management in documentation.
-          </Body>
-        </BodyContainer>
-      </div>
-      <GraphicContainer>
-        <Graphic type="Search" />
-      </GraphicContainer>
-    </Frame>
+          </BodyContainer>
+          <Infobox>
+            <ListContainer>
+              <Body level={2} style={{ color: '#4255bb' }} strong>
+                It is prerequisite to have:
+                <UnorderedList>
+                  {getPrerequisiteList(capabilities)}
+                </UnorderedList>
+              </Body>
+            </ListContainer>
+          </Infobox>
+          <BodyContainer>
+            <Body level={1}>
+              Ask those responsible within your organisation for access
+              management to grant them to you.
+              <br />
+              Learn more about access management in documentation.
+            </Body>
+          </BodyContainer>
+        </div>
+        <GraphicContainer>
+          <Graphic type="Search" />
+        </GraphicContainer>
+      </Frame>
+    </>
   );
 }
+
+const HomeButton = styled.div`
+  /* Auto Layout */
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 12px;
+
+  position: absolute;
+  width: 88px;
+  height: 36px;
+  left: 14px;
+  top: 0;
+`;
 
 const Frame = styled.div`
   /* Frame 5569 */
@@ -67,7 +100,7 @@ const Frame = styled.div`
   width: 899px;
   height: 300px;
   left: 208px;
-  top: 144px;
+  top: 56px;
 `;
 
 const TitleContainer = styled.div`
@@ -117,6 +150,11 @@ const ListContainer = styled.div`
   order: 1;
   flex-grow: 0;
   margin: 16px;
+`;
+
+const UnorderedList = styled.ul`
+  padding: 0 0 0 20px;
+  color: #4255bb;
 `;
 
 const GraphicContainer = styled.div`
