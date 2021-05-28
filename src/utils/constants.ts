fd9f7c3e6b3ca60de34b3aaa4fde65adc121abe1
Krzysztof Olbiński <krzysztof.olbinski@cognite.com>
2021-05-28 11:43:21 +0200

    A BestDay Production Engineer wants to "Open in" Charts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
 export const SEARCH_KEY = 'q';
 export const FLOAT_NUMBER_PATTERN = /\d*\.?\d*$/;
+export const TIMESERIE_IDS_KEY = 'timeserieIds';
+export const START_TIME_KEY = 'startTime';
+export const END_TIME_KEY = 'endTime';
