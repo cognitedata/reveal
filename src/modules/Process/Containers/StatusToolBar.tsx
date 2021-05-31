be7e404c1ff7fd863bdcdeb885b0f9ff818c3ae1
Mihil Ranathunga <mihilranathunga@gmail.com>
2021-05-31 18:16:30 +0530

    status bar

--- src/modules/Process/Containers/StatusToolBar.tsx
+++ src/modules/Process/Containers/StatusToolBar.tsx
@@ -1,15 +1,13 @@
-import { Body, Icon } from '@cognite/cogs.js';
+import { Body } from '@cognite/cogs.js';
 import React from 'react';
 import styled from 'styled-components';
+import { CDFStatus } from 'src/modules/Common/Components/CDFStatus/CDFStatus';
 
 export const StatusToolBar = () => {
   return (
     <Container>
       <Text level={3}>CDF Contextualize Imagery Data</Text>
-      <Status>
-        <Icon type="Checkmark" />
-        {/* <Text level={3}>Saved 8.45 to CDF</Text> */}
-      </Status>
+      <CDFStatus />
     </Container>
   );
 };
@@ -21,10 +19,6 @@ const Container = styled.div`
   height: 40px;
   border: 1px solid #dcdcdc;
   padding: 0 12px;
-`;
-const Status = styled.div`
-  display: flex;
-  color: #8c8c8c;
 `;
 
 const Text = styled(Body)`
