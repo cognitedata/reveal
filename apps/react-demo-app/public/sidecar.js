152055db1afc4c5f9ae19f9f141d297b236d1cd0
Valerii Gusev <valerii.gusev@cognite.com>
2021-06-04 22:36:55 +0200

    chore: Extend Fake IdP to use multiple projects (#724)
    * chore: Extend Fake IdP to use multiple projects
    
    * fixes
    
    * bump tsa
    
    * fix local startIdP script
    
    * Update Jenkinsfile

--- apps/react-demo-app/public/sidecar.js
+++ apps/react-demo-app/public/sidecar.js
@@ -8,14 +8,8 @@ window.__cogniteSidecar = {
       fakeApplicationId: 'user',
       // project to run e2e tests against
       project: 'react-demo-app-e2e-azure-dev',
-      cluster: 'azure-dev',
-    },
-    {
-      roles: ['administer'],
-      groups: ['defaultGroup'],
-      fakeApplicationId: 'admin',
-      // project to run e2e tests against
-      project: 'react-demo-app-e2e-azure-dev',
+      // to match `Login with Fake IDP (react-demo-app-e2e-azure-dev)` button
+      name: 'react-demo-app-e2e-azure-dev',
       cluster: 'azure-dev',
     },
   ],
