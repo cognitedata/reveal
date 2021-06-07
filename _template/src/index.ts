ee920d853d53ed9ac5fd972168607daef269fb89
Valerii Gusev <valerii.gusev@cognite.com>
2021-06-07 14:11:25 +0200

    Move commonjs into packages

--- _template/src/index.ts
+++
@@ -1,6 +0,0 @@
-// All that's left is writing the code!
-// Note that it's preferred to export named constants vs defaults.
-
-export const example = () => {
-  return true;
-};
--- metrics/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Metrics } from './metrics';
-export { Properties, ITimer } from './types';
-export { default as useMetrics } from './useMetrics';
