fb8d7343211424688538a2a82cc5e4b3021881fa
Eirik Vullum <evullum@gmail.com>
2021-06-18 12:08:56 +0200

    Refactor token validation before attempting backend request + use axios to avoid redirect issue

--- src/utils/backendApi.ts
+++ src/utils/backendApi.ts
@@ -1,12 +1,12 @@
 import queryString from 'query-string';
+import axios, { AxiosInstance } from 'axios';
 import { CogniteClient } from '@cognite/sdk';
-import config, { getBackendServiceBaseUrl, getSidecar } from 'config';
+import { getBackendServiceBaseUrl, getSidecar } from 'config';
 import {
   BACKEND_SERVICE_URL_KEY,
   CLUSTER_KEY,
   FALLBACK_TO_FUNCTIONS_URL_KEY,
 } from 'utils/constants';
-import { getLoginStatus } from './login';
 
 export type CogniteFunction = {
   id: number;
@@ -32,41 +32,29 @@ const cdfApiUrl = `https://${
 }.cognitedata.com`;
 
 const getServiceClient = async (sdk: CogniteClient) => {
-  const login = await getLoginStatus(sdk);
+  await sdk.get('/api/v1/token/inspect');
 
-  if (!login?.id) {
-    return sdk;
-  }
-
-  const client = new CogniteClient({
-    appId: config.appId,
-    baseUrl: fallBackToFunctions
+  const client = axios.create({
+    baseURL: fallBackToFunctions
       ? cdfApiUrl
       : backendServiceBaseUrlFromQuery || getBackendServiceBaseUrl(),
-  });
-
-  const accessToken = sdk
-    .getDefaultRequestHeaders()
-    .Authorization.split('Bearer ')[1];
-
-  client.loginWithOAuth({
-    project: sdk.project,
-    accessToken,
+    timeout: 10000,
+    headers: { ...sdk.getDefaultRequestHeaders() },
   });
 
   return client;
 };
 
 export const getCalls = async (sdk: CogniteClient, fnId: number) => {
-  const client = await getServiceClient(sdk);
+  const client = (await getServiceClient(sdk)) as AxiosInstance;
 
   return client
     .get(`/api/playground/projects/${sdk.project}/functions/${fnId}/calls`)
-    .then((response) => response?.data?.items || []);
+    .then((response: any) => response?.data?.items || []);
 };
 
 export async function listFunctions(sdk: CogniteClient) {
-  const client = await getServiceClient(sdk);
+  const client = (await getServiceClient(sdk)) as AxiosInstance;
 
   return client
     .get<{ items: CogniteFunction[] }>(
@@ -80,13 +68,13 @@ export async function callFunction(
   functionId: number,
   data?: object
 ) {
-  const client = await getServiceClient(sdk);
+  const client = (await getServiceClient(sdk)) as AxiosInstance;
 
   return client
     .post(
       `/api/playground/projects/${sdk.project}/functions/${functionId}/call`,
       {
-        data: { data },
+        data,
       }
     )
     .then((r) => r.data);
@@ -97,7 +85,7 @@ export async function getCallStatus(
   functionId: number,
   callId: number
 ) {
-  const client = await getServiceClient(sdk);
+  const client = (await getServiceClient(sdk)) as AxiosInstance;
 
   return client
     .get(
@@ -111,7 +99,7 @@ export async function getCallResponse(
   functionId: number,
   callId: number
 ) {
-  const client = await getServiceClient(sdk);
+  const client = (await getServiceClient(sdk)) as AxiosInstance;
 
   return client
     .get(
