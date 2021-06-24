5d03067e2e97aaa135eb70b79875f546fff57f85
Ostap Partyka <ostap.partyka@M-C02YJ174JHCD.local>
2021-06-24 11:48:42 +0300

    Fixes lint issues

--- src/utils/colors.spec.ts
+++ src/utils/colors.spec.ts
@@ -10,29 +10,37 @@ describe('Colors', () => {
 describe('Stateful colors', () => {
   const chat1 = {
     id: 'chatId1',
-    workflowCollection: [
-      'workflowId1',
-      'workflowId2',
-      'workflowId3',
-    ],
+    workflowCollection: ['workflowId1', 'workflowId2', 'workflowId3'],
   };
   const chat2 = {
     id: 'chatId2',
-    workflowCollection: [
-      'workflowId1',
-      'workflowId2',
-      'workflowId3',
-    ],
+    workflowCollection: ['workflowId1', 'workflowId2', 'workflowId3'],
   };
 
   it('gives incrementing color from map', () => {
-    expect(getEntryColor(chat1.id, chat1.workflowCollection[0])).toBe(availableColors[0]);
-    expect(getEntryColor(chat1.id, chat1.workflowCollection[0])).toBe(availableColors[0]);
-    expect(getEntryColor(chat1.id, chat1.workflowCollection[1])).toBe(availableColors[1]);
-    expect(getEntryColor(chat1.id, chat1.workflowCollection[2])).toBe(availableColors[2]);
-    expect(getEntryColor(chat2.id, chat2.workflowCollection[0])).toBe(availableColors[0]);
-    expect(getEntryColor(chat2.id, chat2.workflowCollection[0])).toBe(availableColors[0]);
-    expect(getEntryColor(chat2.id, chat2.workflowCollection[1])).toBe(availableColors[1]);
-    expect(getEntryColor(chat2.id, chat2.workflowCollection[2])).toBe(availableColors[2]);
+    expect(getEntryColor(chat1.id, chat1.workflowCollection[0])).toBe(
+      availableColors[0]
+    );
+    expect(getEntryColor(chat1.id, chat1.workflowCollection[0])).toBe(
+      availableColors[0]
+    );
+    expect(getEntryColor(chat1.id, chat1.workflowCollection[1])).toBe(
+      availableColors[1]
+    );
+    expect(getEntryColor(chat1.id, chat1.workflowCollection[2])).toBe(
+      availableColors[2]
+    );
+    expect(getEntryColor(chat2.id, chat2.workflowCollection[0])).toBe(
+      availableColors[0]
+    );
+    expect(getEntryColor(chat2.id, chat2.workflowCollection[0])).toBe(
+      availableColors[0]
+    );
+    expect(getEntryColor(chat2.id, chat2.workflowCollection[1])).toBe(
+      availableColors[1]
+    );
+    expect(getEntryColor(chat2.id, chat2.workflowCollection[2])).toBe(
+      availableColors[2]
+    );
   });
 });
