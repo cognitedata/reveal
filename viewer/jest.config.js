917fb661f704d3dfb9369c6f664f2ebc8409fd6e
Christopher Tannum <christopher.tannum@cognite.com>
2021-07-09 00:25:22 +0200

    fix: set test config at root, and fix running tests

---
+++ viewer/jest.config.js
@@ -0,0 +1,37 @@
+/*!
+ * Copyright 2021 Cognite AS
+ */
+
+const path = require('path');
+
+process.env.VERSION = 'test';
+process.env.MIXPANEL_TOKEN = 'test';
+
+module.exports = () => {
+  return {
+    rootDir: '.',
+    roots: ['<rootDir>'],
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest'
+    },
+    testRegex: '(.*\\.test\\..*|\\.(test|spec|Test))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    moduleNameMapper: {
+      '\\.(frag|vert)$': path.resolve(__dirname, './core/src/__mocks__/glslMocks.js'),
+      '\\.css$': path.resolve(__dirname, './core/src/__mocks__/cssMock.js'),
+      '\\.svg$': path.resolve(__dirname, './core/src/__mocks__/svgMock.js')
+    },
+    globals: {
+      __webpack_public_path__: '',
+      // process.env globals are in setupJest
+      'ts-jest': {
+        tsconfig: path.resolve(__dirname, './tsconfig.common.json')
+      }
+    },
+    coverageDirectory: './coverage',
+    collectCoverageFrom: ['!**/__testutilities__/**/*.ts', '!**/.*.test.ts', '!**/*.d.ts', '!**/*.json'],
+    automock: false,
+    setupFiles: [path.resolve(__dirname, './core/src/__testutilities__/setupJest.ts'), 'jest-canvas-mock', 'core-js'],
+    setupFilesAfterEnv: ['jest-extended']
+  };
+};
---
+++ viewer/packages/camera-manager/tests/cameraControls.test.ts
@@ -0,0 +1,12 @@
+/*!
+ * Copyright 2021 Cognite AS
+ */
+
+import * as THREE from 'three';
+
+describe('Camera Controls', () => {
+  test('dummy', () => {
+    const asd = new THREE.Vector3();
+    expect(asd).toEqual(new THREE.Vector3());
+  });
+});
