a24b4a8cc148e1d3c0feeac6017b20ca24776abb
Christopher Tannum <christopher.tannum@cognite.com>
2021-07-08 20:21:14 +0200

    fix: use common webpack config and tsconfig

---
+++ viewer/packages/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => {
+  return {
+
+    mode: 'development',
+
+    entry: path.resolve(env.dir, './app/index.ts'),
+
+    output: {
+      path: path.resolve(env.dir, 'dist'),
+      filename: 'index.js',
+    },
+
+    resolve: {
+      extensions: ['.ts', '.js'],
+      symlinks: false
+    },
+
+    devServer: {
+      contentBase: path.resolve(env.dir, './dist'),
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, '../tsconfig.common.json')
+            }
+          }
+
+        }
+      ]
+    },
+
+    plugins: [new HtmlWebpackPlugin({ title: require(path.resolve(env.dir, './package.json')).name })]
+  }
+}
