c7061d5ece001c1a2a7409e6c731fea67f4ec715
Krzysztof Olbiński <krzysztof.olbinski@cognite.com>
2021-08-02 12:55:20 +0200

    Remove informational panel on asset and time series search, since we now have empty state and recently viewed time series

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Icon, Title, Graphic } from '@cognite/cogs.js';
+import React from 'react';
+import { Graphic } from '@cognite/cogs.js';
 import styled from 'styled-components/macro';
 import { useRecentViewLocalStorage } from 'utils/recentViewLocalstorage';
 
@@ -8,46 +8,7 @@ interface InfoBoxProps {
   query: string;
 }
 
-interface InfoBoxData {
-  title: string;
-  body: any;
-}
-const timeSeriesInfo: InfoBoxData = {
-  title: 'Timeseries ID',
-  body: (
-    <>
-      You can search for Timeseries name (ID) e.g. VAL_21_ZT_1018_04:Z.X.Value
-      or Timeseries description, e.g. Utløpstrykk pigsluse eksportlinje B.
-      {'\n\n'}
-      Read more on
-      <a href="cog.link/charts-doc"> cog.link/charts-doc.</a>
-    </>
-  ),
-};
-
-const tagInfo: InfoBoxData = {
-  title: 'Search Tag numbers',
-  body: (
-    <>
-      Search for Tag number (asset) e.g. 21PT1019 or description (e.g. LAUN TO
-      OIL TRANS LN B){'\n\n'}
-      Read more on
-      <a href="cog.link/charts-doc"> cog.link/charts-doc.</a>
-    </>
-  ),
-};
-
 const InfoBox = ({ infoType, query }: InfoBoxProps) => {
-  const [displayInfo, setDisplayInfo] = useState(
-    localStorage ? !localStorage.getItem(infoType) : true
-  );
-
-  const data = infoType === 'TagHelpBox' ? tagInfo : timeSeriesInfo;
-
-  const handleOnClick = () => {
-    localStorage.setItem(infoType, JSON.stringify({ display: false }));
-    setDisplayInfo(false);
-  };
   const { data: rvResults } = useRecentViewLocalStorage(
     infoType === 'TagHelpBox' ? 'assets' : 'timeseries',
     []
@@ -57,22 +18,6 @@ const InfoBox = ({ infoType, query }: InfoBoxProps) => {
     <InfoBoxContainer
       style={query === '' && !recentViewExists ? { height: '100%' } : {}}
     >
-      {displayInfo && (
-        <InfoBoxWrapper>
-          <StyledIcon type="InfoFilled" />
-          <InfoWrapper>
-            <StyledTitle level={5}>{data.title}</StyledTitle>
-            <TextContainer>{data.body}</TextContainer>
-          </InfoWrapper>
-          <Button
-            icon="Close"
-            size="small"
-            type="ghost"
-            style={{ width: '1.65em', height: '1.65em' }}
-            onClick={handleOnClick}
-          />
-        </InfoBoxWrapper>
-      )}
       {query === '' && !recentViewExists && (
         <EmptyResultsContainer>
           <EmptyResults>
@@ -98,41 +43,6 @@ const InfoBox = ({ infoType, query }: InfoBoxProps) => {
 const InfoBoxContainer = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-const InfoBoxWrapper = styled.div`
-  width: initial;
-  display: flex;
-  flex-direction: row;
-  margin: 5px 5px 10px 0;
-  padding: 1em;
-  min-height: 200px;
-  background-color: #f6f9ff;
-  border: 1px solid var(--cogs-midblue-5);
-  border-radius: 0.5em;
-`;
-
-const TextContainer = styled.div`
-  margin-top: 1em;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 20px;
-  width: fit-content;
-  white-space: pre-line;
-`;
-
-const InfoWrapper = styled.div`
-  width: max-content;
-  flex: 1;
-  padding: 0 1em;
-`;
-
-const StyledTitle = styled(Title)`
-  color: var(--cogs-text-primary);
-`;
-
-const StyledIcon = styled(Icon)`
-  color: var(--cogs-text-accent);
 `;
 
 const EmptyResultsContainer = styled.div`
