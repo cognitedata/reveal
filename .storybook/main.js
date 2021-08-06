8b12b16f09def78000b8bcbf813ae7b44b79886a
Rhuan Barreto <rhuan.barreto@cognite.com>
2021-08-06 16:43:53 +0200

    [CHART-162] Add typescript support for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,4 +11,14 @@ module.exports = {
     },
     '@storybook/addon-viewport/register',
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
 };
