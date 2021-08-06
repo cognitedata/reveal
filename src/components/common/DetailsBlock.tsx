ee0b52c1061a27b397243cf7144aeae53e70dcb0
Rhuan Barreto <rhuan.barreto@cognite.com>
2021-08-06 17:28:56 +0200

    [CHART-162] Spacing adjustments on DetailsBlock

--- src/components/common/DetailsBlock.tsx
+++ src/components/common/DetailsBlock.tsx
@@ -3,18 +3,28 @@ import React, { CSSProperties, FC } from 'react';
 export interface DetailsBlockProps {
   title: string;
   children: React.ReactNode;
+  style?: CSSProperties;
 }
 
 const roundedBoxStyle: CSSProperties = {
   border: '1px solid #D9D9D9',
   boxSizing: 'border-box',
   borderRadius: 6,
-  padding: '9px 16px',
+  padding: '8px',
 };
 
-const DetailsBlock: FC<DetailsBlockProps> = ({ title, children, ...props }) => (
-  <div {...props}>
-    <h3>{title}</h3>
+const mainDivStyle: CSSProperties = {
+  margin: '8px 0',
+};
+
+const DetailsBlock: FC<DetailsBlockProps> = ({
+  title,
+  children,
+  style,
+  ...props
+}) => (
+  <div style={{ ...mainDivStyle, ...style }} {...props}>
+    <h4>{title}</h4>
     <div style={roundedBoxStyle}>
       {children || <span>No info to display</span>}
     </div>
