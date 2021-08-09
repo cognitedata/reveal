4c65ec6d54d62e8b7bc5abadcf6865879ee5d9ad
Rhuan Barreto <rhuan.barreto@cognite.com>
2021-08-09 10:47:23 +0200

    [REACT-17] Update, unify and simplify storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,4 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.@(ts|tsx|mdx)',
-    '../src/**/__stories__/*.stories.@(ts|tsx|mdx)',
-  ],
-  addons: [
-    '@storybook/preset-create-react-app',
-    {
-      name: '@storybook/addon-docs',
-      options: { configureJSX: true },
-    },
-    '@storybook/addon-viewport/register',
-  ],
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
-  },
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/preset-create-react-app'],
 };
