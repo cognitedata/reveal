76a7e7f433d186ff3082845a727122b488122d8a
Rumesh Wickramasinghe <rumesh.wickramasinghe@gmail.com>
2021-08-13 12:23:32 +0530

    fix: image copy to clip board fix (#391)
    * fix: image copy to clip board fix
    
    * fix: copy to clipboard

--- src/Three/Commands/CopyImageCommand.ts
+++ src/Three/Commands/CopyImageCommand.ts
@@ -16,15 +16,25 @@ export class CopyImageCommand extends ThreeRenderTargetCommand {
   // OVERRIDES of BaseCommand
   //= =================================================
 
-  public /* override */ getName(): string { return "Copy a image of the viewer to the clipboard"; }
+  public /* override */ getName(): string { 
+    if(this.isFirefox())
+      return "Copy a image of the viewer to the clipboard (Not supported in Firefox)"; 
+    return "Copy a image of the viewer to the clipboard"; 
+  }
 
   public /* override */ getIcon(): string { return CopyImageIcon; }
   
   protected /* override */ invokeCore(): boolean {
+    if(this.isFirefox()) return true;
     this.target?.domElement.toBlob((blob: any) => {
       // @ts-ignore
       navigator.clipboard.write([new ClipboardItem({ "image/png": blob })]);
     });
     return true;
+  }
+
+  private isFirefox(): boolean {
+    // @ts-ignore
+    return typeof InstallTrigger !== 'undefined';
   }
 }
