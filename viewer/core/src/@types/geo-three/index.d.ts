87d0092339a477379f78f3a64a6c047721531992
Pramod S <87521752+pramodcog@users.noreply.github.com>
2021-08-18 17:09:27 +0530

    Moved geo-three lib into node_module

--- viewer/core/src/@types/geo-three/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './source/Main';
-
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace GEOTHREE;
