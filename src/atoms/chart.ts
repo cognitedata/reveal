8bbeef76664576680b24a625d0269221f68a448c
Eirik Vullum <evullum@gmail.com>
2021-08-24 12:06:27 +0200

    Remove unused selectors

--- src/atoms/chart.ts
+++ src/atoms/chart.ts
@@ -1,52 +1,7 @@
-import { atom, selector } from 'recoil';
-import { Chart, ChartTimeSeries, ChartWorkflow } from 'reducers/charts/types';
+import { atom } from 'recoil';
+import { Chart } from 'reducers/charts/types';
 
 export const chartState = atom({
   key: 'chartState',
   default: undefined as Chart | undefined,
-});
-
-export const chartSourceCountState = selector({
-  key: 'chartSourceCountState',
-  get: ({ get }) => {
-    const chart = get(chartState);
-
-    if (!chart) {
-      return 0;
-    }
-
-    return (
-      (chart.timeSeriesCollection?.length || 0) +
-      (chart.workflowCollection?.length || 0)
-    );
-  },
-});
-
-export const chartTimeseriesFilteredState = selector({
-  key: 'chartTimeseriesFiltered',
-  get: ({ get }) => {
-    const chart = get(chartState);
-
-    return chart?.timeSeriesCollection?.map((t) => {
-      return {
-        ...t,
-        statisticsCalls: undefined,
-      };
-    }) as ChartTimeSeries[];
-  },
-});
-
-export const chartWorkflowsFilteredState = selector({
-  key: 'chartWorkflowsFiltered',
-  get: ({ get }) => {
-    const chart = get(chartState);
-
-    return chart?.workflowCollection?.map((t) => {
-      return {
-        ...t,
-        calls: undefined,
-        statisticsCalls: undefined,
-      };
-    }) as ChartWorkflow[];
-  },
 });
