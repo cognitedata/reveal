b882709ab53c85fce24fd61534a50533e79a7178
drhumlen <niklas.humlen@gmail.com>
2021-09-01 16:03:15 +0200

    Refactor code

--- apps/extraction-pipelines/src/components/integration/ContactsSection.tsx
+++ apps/extraction-pipelines/src/components/integration/ContactsSection.tsx
@@ -21,7 +21,7 @@ import {
 import { StyledTitle2 } from 'styles/StyledHeadings';
 import { Hint } from 'styles/StyledForm';
 import { NotificationUpdateSwitch } from 'components/inputs/NotificationUpdateSwitch';
-import { DetailFieldNames } from 'model/Integration';
+import { DetailFieldNames, Integration } from 'model/Integration';
 import { isOwner, partition } from 'utils/integrationUtils';
 import { User } from 'model/User';
 import { Grid } from 'styles/grid/StyledGrid';
@@ -54,95 +54,39 @@ interface ContactsSectionProps {}
 export const ContactsSection: FunctionComponent<ContactsSectionProps> = () => {
   const { integration } = useSelectedIntegration();
   const { data: current } = useIntegrationById(integration?.id);
-  const { pass: owner } = partition(current?.contacts ?? [], isOwner);
-
-  const renderOwner = (innerOwner: ReadonlyArray<User>) => {
-    if (innerOwner[0]) {
-      return (
-        <>
-          {current?.contacts?.map((contact, index) => {
-            const key = `owner-${index}`;
-            if (!isOwner(contact)) {
-              return null;
-            }
-            return (
-              <Row key={key} className="row-style-even row-height-4">
-                <EditPartContacts
-                  integration={current}
-                  name="contacts"
-                  index={index}
-                  field="role"
-                  label="Role"
-                  schema={contactRoleSchema}
-                  defaultValues={{ role: contact?.role }}
-                />
-                <NotificationUpdateSwitch
-                  integration={current}
-                  name="contacts"
-                  index={index}
-                  field="sendNotification"
-                  defaultValues={{
-                    sendNotification: contact?.sendNotification,
-                  }}
-                />
-                <EditPartContacts
-                  integration={current}
-                  name="contacts"
-                  index={index}
-                  field="name"
-                  label={TableHeadings.NAME}
-                  schema={contactNameSchema}
-                  defaultValues={{ name: contact?.name }}
-                />
-                <EditPartContacts
-                  integration={current}
-                  name="contacts"
-                  index={index}
-                  field="email"
-                  label="Email"
-                  schema={contactEmailSchema}
-                  defaultValues={{ email: contact?.email }}
-                />
-
-                <RemoveContactButton
-                  integration={current}
-                  name="contacts"
-                  index={index}
-                />
-              </Row>
-            );
-          })}
-        </>
-      );
-    }
-    return <AddContact isOwner />;
-  };
+  const { pass: owners, fail: nonOwners } = partition(
+    current?.contacts ?? [],
+    isOwner
+  );
 
   return (
     <ContactsSectionWrapper role="grid">
       <StyledTitle2 id="owner-heading">{DetailFieldNames.OWNER}</StyledTitle2>
       <Hint>{OWNER_HINT}</Hint>
-      <OwnerWrapper>
-        <HeadingRow>
-          <Heading>{ROLE_LABEL}</Heading>
-          <Heading>{NOTIFICATION_LABEL}</Heading>
-          <Heading>{NAME_LABEL}</Heading>
-          <Heading>{EMAIL_LABEL}</Heading>
-        </HeadingRow>
-        {renderOwner(owner)}
-      </OwnerWrapper>
+      {integration && contactTable(owners, integration)}
+      <AddContact isOwner />
 
       <StyledTitle2 id="contacts-heading">
         {TableHeadings.CONTACTS}
       </StyledTitle2>
       <Hint>{CONTACTS_HINT}</Hint>
+      {integration && contactTable(owners, integration)}
+
+      <AddContact />
+    </ContactsSectionWrapper>
+  );
+};
+
+function contactTable(contacts: User[], integration: Integration) {
+  return (
+    <>
       <HeadingRow>
         <Heading>{ROLE_LABEL}</Heading>
         <Heading>{NOTIFICATION_LABEL}</Heading>
         <Heading>{NAME_LABEL}</Heading>
         <Heading>{EMAIL_LABEL}</Heading>
       </HeadingRow>
-      {current?.contacts?.map((contact, index) => {
+      {contacts.map((contact, index) => {
         if (isOwner(contact)) {
           return null;
         }
@@ -150,7 +94,7 @@ export const ContactsSection: FunctionComponent<ContactsSectionProps> = () => {
         return (
           <Row key={key} className="row-style-even row-height-4">
             <EditPartContacts
-              integration={current}
+              integration={integration}
               name="contacts"
               index={index}
               field="role"
@@ -159,14 +103,14 @@ export const ContactsSection: FunctionComponent<ContactsSectionProps> = () => {
               defaultValues={{ role: contact?.role }}
             />
             <NotificationUpdateSwitch
-              integration={current}
+              integration={integration}
               name="contacts"
               index={index}
               field="sendNotification"
               defaultValues={{ sendNotification: contact?.sendNotification }}
             />
             <EditPartContacts
-              integration={current}
+              integration={integration}
               name="contacts"
               index={index}
               field="name"
@@ -175,7 +119,7 @@ export const ContactsSection: FunctionComponent<ContactsSectionProps> = () => {
               defaultValues={{ name: contact?.name }}
             />
             <EditPartContacts
-              integration={current}
+              integration={integration}
               name="contacts"
               index={index}
               field="email"
@@ -185,14 +129,13 @@ export const ContactsSection: FunctionComponent<ContactsSectionProps> = () => {
             />
 
             <RemoveContactButton
-              integration={current}
+              integration={integration}
               name="contacts"
               index={index}
             />
           </Row>
         );
       })}
-      <AddContact />
-    </ContactsSectionWrapper>
+    </>
   );
-};
+}
