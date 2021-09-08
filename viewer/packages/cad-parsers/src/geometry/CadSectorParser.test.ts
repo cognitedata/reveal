9287026873f68088965e3fce74857fd1f3330c87
Håkon Flatval <hakon.flatval@cognite.com>
2021-09-08 15:37:37 +0200

    Fix URL in test

--- viewer/packages/cad-parsers/src/geometry/CadSectorParser.test.ts
+++ viewer/packages/cad-parsers/src/geometry/CadSectorParser.test.ts
@@ -9,7 +9,7 @@ import { CadSectorParser } from './CadSectorParser';
 import { WorkerPool } from '@reveal/utilities';
 import { SectorQuads } from '@cognite/reveal-parser-worker';
 
-jest.mock('../../../utilities/workers/WorkerPool');
+jest.mock('../../../utilities/src/workers/WorkerPool.ts');
 
 describe('CadSectorParser', () => {
   const workerPool: WorkerPool = new WorkerPool();
