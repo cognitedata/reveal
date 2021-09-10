08a6db43238ae9b3f2476157d563475fe7da6c35
Eirik Vullum <evullum@gmail.com>
2021-09-10 13:03:20 +0200

    Update text

--- src/components/UnitDropdown/UnitDropdown.tsx
+++ src/components/UnitDropdown/UnitDropdown.tsx
@@ -44,7 +44,7 @@ export const UnitDropdown = ({
         color: 'var(--cogs-danger)',
       }}
     >
-      Reset
+      Reset unit
     </Menu.Item>,
     ...Object.values(UnitTypes).map((unitType) => (
       <Menu.Item
