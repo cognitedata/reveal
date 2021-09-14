fec0f6d6d81034c7e474008608e9acedcfd68d1e
Lars Moastuen <lars.moastuen@cognite.com>
2021-09-14 14:57:38 +0200

    Alternative implementation of TakenSectorTree to be able to experiment.

--- viewer/core/src/datamodels/cad/sector/culling/ByScreenSizeSectorCuller.ts
+++ viewer/core/src/datamodels/cad/sector/culling/ByScreenSizeSectorCuller.ts
@@ -6,10 +6,18 @@ import * as THREE from 'three';
 
 import { CadModelMetadata, SectorCuller } from '../../../../internals';
 import { WantedSector } from '../types';
-import { TakenSectorMap } from './TakenSectorMap';
-import { DetermineSectorCostDelegate, DetermineSectorsInput, SectorLoadingSpent } from './types';
+import {
+  DetermineSectorCostDelegate,
+  DetermineSectorsInput,
+  PrioritizedWantedSector,
+  SectorCost,
+  SectorLoadingSpent
+} from './types';
 import { computeSectorCost } from './computeSectorCost';
 import { computeNdcAreaOfBox } from './computeNdcAreaOfBox';
+import assert from 'assert';
+import { LevelOfDetail } from '../LevelOfDetail';
+import { CadModelSectorBudget } from '../../CadModelSectorBudget';
 
 export type ByScreenSizeSectorCullerOptions = {
   /**
@@ -31,9 +39,9 @@ export class ByScreenSizeSectorCuller implements SectorCuller {
     spentBudget: SectorLoadingSpent;
   } {
     if (input.clippingPlanes !== null && input.clippingPlanes.length > 0) {
-      throw new Error('');
+      throw new Error('Clipping planes not supported');
     }
-    const takenSectors = new TakenSectorMap(this._determineSectorCost);
+    const takenSectors = new ScheduledSectorTree(this._determineSectorCost);
 
     const { cadModelsMetadata, camera } = input;
     const cameraMatrixWorldInverse = camera.matrixWorldInverse;
@@ -92,4 +100,118 @@ export class ByScreenSizeSectorCuller implements SectorCuller {
   }
 
   dispose(): void {}
+}
+
+class ScheduledSectorTree {
+  private readonly determineSectorCost: DetermineSectorCostDelegate;
+  private readonly _totalCost: SectorCost = { downloadSize: 0, drawCalls: 0 };
+  private readonly _models = new Map<string, { model: CadModelMetadata; sectorIds: Map<number, number> }>();
+
+  get totalCost(): SectorCost {
+    return { ...this._totalCost };
+  }
+
+  constructor(determineSectorCost: DetermineSectorCostDelegate) {
+    this.determineSectorCost = determineSectorCost;
+  }
+
+  initializeScene(modelMetadata: CadModelMetadata) {
+    this._models.set(modelMetadata.modelIdentifier, { model: modelMetadata, sectorIds: new Map<number, number>() });
+  }
+
+  getWantedSectorCount(): number {
+    let count = 0;
+    this._models.forEach(x => {
+      count += x.sectorIds.size;
+    });
+    return count;
+  }
+
+  markSectorDetailed(model: CadModelMetadata, sectorId: number, priority: number) {
+    const entry = this._models.get(model.modelIdentifier);
+    assert(!!entry, `Could not find sector tree for ${model.modelIdentifier}`);
+
+    const allSectors = model.scene.getAllSectors();
+    const { sectorIds } = entry!;
+    let nextSectorIdToAdd = sectorId;
+    if (nextSectorIdToAdd !== -1 && !sectorIds.has(nextSectorIdToAdd)) {
+      const existingPriority = sectorIds.get(nextSectorIdToAdd);
+      if (existingPriority === undefined) {
+        const sectorMetadata = model.scene.getSectorById(sectorId);
+        assert(sectorMetadata !== undefined);
+        const sectorCost = this.determineSectorCost(sectorMetadata!, LevelOfDetail.Detailed);
+        this._totalCost.downloadSize += sectorCost.downloadSize;
+        this._totalCost.drawCalls += sectorCost.drawCalls;
+
+        sectorIds.set(nextSectorIdToAdd, priority);
+      } else {
+        sectorIds.set(nextSectorIdToAdd, Math.max(priority, existingPriority));
+      }
+      const parent = allSectors.find(x => x.children.findIndex(x => x.id === nextSectorIdToAdd));
+      nextSectorIdToAdd = parent !== undefined ? parent.id : -1;
+    }
+  }
+
+  isWithinBudget(budget: CadModelSectorBudget): boolean {
+    return (
+      this._totalCost.downloadSize < budget.geometryDownloadSizeBytes &&
+      this._totalCost.drawCalls < budget.maximumNumberOfDrawCalls
+    );
+  }
+
+  collectWantedSectors(): PrioritizedWantedSector[] {
+    const allWanted = new Array<PrioritizedWantedSector>();
+
+    // Collect sectors
+    for (const [modelIdentifier, sectorsContainer] of this._models) {
+      const { model, sectorIds } = sectorsContainer;
+      for (const [sectorId, priority] of sectorIds) {
+        const sector = model.scene.getSectorById(sectorId)!;
+        const wantedSector: PrioritizedWantedSector = {
+          modelIdentifier,
+          modelBaseUrl: model.modelBaseUrl,
+          geometryClipBox: null,
+          levelOfDetail: LevelOfDetail.Detailed,
+          metadata: sector,
+          priority
+        };
+        allWanted.push(wantedSector);
+      }
+    }
+
+    // Sort by priority
+    allWanted.sort((l, r) => r.priority - l.priority);
+    return allWanted;
+  }
+
+  computeSpentBudget(): SectorLoadingSpent {
+    const wanted = this.collectWantedSectors();
+    const models = Array.from(this._models.values()).map(x => x.model);
+    const nonDiscarded = wanted.filter(x => x.levelOfDetail !== LevelOfDetail.Discarded);
+
+    const totalSectorCount = models.reduce((sum, x) => sum + x.scene.sectorCount, 0);
+    const takenSectorCount = nonDiscarded.length;
+    const takenSimpleCount = nonDiscarded.filter(x => x.levelOfDetail === LevelOfDetail.Simple).length;
+    const forcedDetailedSectorCount = nonDiscarded.filter(x => !Number.isFinite(x.priority)).length;
+    const accumulatedPriority = nonDiscarded
+      .filter(x => Number.isFinite(x.priority) && x.priority > 0)
+      .reduce((sum, x) => sum + x.priority, 0);
+
+    const spentBudget: SectorLoadingSpent = {
+      drawCalls: this.totalCost.drawCalls,
+      downloadSize: this.totalCost.downloadSize,
+      totalSectorCount,
+      forcedDetailedSectorCount,
+      loadedSectorCount: takenSectorCount,
+      simpleSectorCount: takenSimpleCount,
+      detailedSectorCount: takenSectorCount - takenSimpleCount,
+      accumulatedPriority
+    };
+
+    return spentBudget;
+  }
+
+  clear() {
+    this._models.clear();
+  }
 }
