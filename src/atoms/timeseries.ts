23ea3f1da067d464c0afea92ad77ae7c2516077d
Eirik Vullum <evullum@gmail.com>
2021-09-14 15:17:36 +0200

    Calculate min, max, mean for raw values as well

--- src/atoms/timeseries.ts
+++ src/atoms/timeseries.ts
@@ -3,6 +3,7 @@ import {
   StringDatapoints,
   DoubleDatapoints,
 } from '@cognite/sdk';
+import { hasRawPoints } from 'components/PlotlyChart';
 import { atom, selectorFamily } from 'recoil';
 
 export const timeseriesState = atom({
@@ -16,46 +17,82 @@ export const timeseriesSummaryById = selectorFamily({
     (externalId: string | undefined) =>
     ({ get }) => {
       const state = get(timeseriesState);
-      const ts = state.find(
-        (item) => item.externalId === externalId
-      ) as DatapointAggregates;
+      const ts = state.find((item) => item.externalId === externalId) as
+        | DatapointAggregates
+        | DoubleDatapoints;
 
       if (!ts) {
         return undefined;
       }
 
-      const min = ts.datapoints.length
-        ? Math.min(
-            ...ts.datapoints.map((datapoint) =>
-              typeof datapoint.min === 'number' ? datapoint.min : NaN
+      const hasPoints = ts.datapoints.length > 0;
+      const isRaw = hasRawPoints(ts);
+
+      let min: number | undefined;
+      let max: number | undefined;
+      let mean: number | undefined;
+
+      if (isRaw) {
+        const tsRaw = ts as DoubleDatapoints;
+
+        min = hasPoints
+          ? Math.min(...tsRaw.datapoints.map((datapoint) => datapoint.value))
+          : NaN;
+
+        max = hasPoints
+          ? Math.max(...tsRaw.datapoints.map((datapoint) => datapoint.value))
+          : NaN;
+
+        const totalSum = hasPoints
+          ? tsRaw.datapoints.reduce(
+              (total, { value }) => total + (value || 0),
+              0
             )
-          )
-        : NaN;
+          : 0;
 
-      const max = ts.datapoints.length
-        ? Math.max(
-            ...ts.datapoints.map((datapoint) =>
-              typeof datapoint.max === 'number' ? datapoint.max : NaN
+        const totalCount = tsRaw.datapoints.length;
+
+        mean = totalCount ? totalSum / totalCount : undefined;
+      } else {
+        const tsAggregate = ts as DatapointAggregates;
+
+        min = hasPoints
+          ? Math.min(
+              ...tsAggregate.datapoints.map((datapoint) =>
+                typeof datapoint.min === 'number' ? datapoint.min : NaN
+              )
             )
-          )
-        : NaN;
+          : NaN;
 
-      const totalSum = ts.datapoints.length
-        ? ts.datapoints.reduce((total, { sum }) => total + (sum || 0), 0)
-        : 0;
+        max = hasPoints
+          ? Math.max(
+              ...tsAggregate.datapoints.map((datapoint) =>
+                typeof datapoint.max === 'number' ? datapoint.max : NaN
+              )
+            )
+          : NaN;
 
-      const totalCount = ts.datapoints.length
-        ? ts.datapoints.reduce((total, { count }) => total + (count || 0), 0)
-        : undefined;
+        const totalSum = hasPoints
+          ? tsAggregate.datapoints.reduce(
+              (total, { sum }) => total + (sum || 0),
+              0
+            )
+          : 0;
 
-      const mean = totalCount ? totalSum / totalCount : undefined;
+        const totalCount = hasPoints
+          ? tsAggregate.datapoints.reduce(
+              (total, { count }) => total + (count || 0),
+              0
+            )
+          : undefined;
 
-      const result = {
+        mean = totalCount ? totalSum / totalCount : undefined;
+      }
+
+      return {
         min: Number.isNaN(min) ? undefined : min,
         max: Number.isNaN(max) ? undefined : max,
         mean: Number.isNaN(mean) ? undefined : mean,
       };
-
-      return result;
     },
 });
