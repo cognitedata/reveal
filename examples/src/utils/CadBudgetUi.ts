e207d20fee93ae6431f35c598cae679570b0be13
Lars Moastuen <lars@moastuen.com>
2021-09-15 14:06:27 +0200

    De-nit

--- examples/src/utils/CadBudgetUi.ts
+++ examples/src/utils/CadBudgetUi.ts
@@ -17,15 +17,15 @@ export function initialCadBudgetUi(viewer: Cognite3DViewer, uiFolder: dat.GUI) {
   };
   const defaultCadBudget = { ...viewer.cadBudget };
   const onCadBudgetSettingsChanged = () => {
-    const s = state.factor / 100.0;
+    const scale = state.factor / 100.0;
     const { limitDownloadedBytes, limitDrawcalls } = state;
     viewer.cadBudget = {
-      highDetailProximityThreshold: defaultCadBudget.highDetailProximityThreshold * s,
-      geometryDownloadSizeBytes:  limitDownloadedBytes ? defaultCadBudget.geometryDownloadSizeBytes * s : Infinity,
-      maximumNumberOfDrawCalls: limitDrawcalls ? defaultCadBudget.maximumNumberOfDrawCalls * s : Infinity
+      highDetailProximityThreshold: defaultCadBudget.highDetailProximityThreshold * scale,
+      geometryDownloadSizeBytes:  limitDownloadedBytes ? defaultCadBudget.geometryDownloadSizeBytes * scale : Infinity,
+      maximumNumberOfDrawCalls: limitDrawcalls ? defaultCadBudget.maximumNumberOfDrawCalls * scale : Infinity
     };
   };
   uiFolder.add(state, 'factor', 1, 500, 5).name('Budget factor (%)').onChange(onCadBudgetSettingsChanged);
   uiFolder.add(state, 'limitDownloadedBytes').name('Limit bytesize').onChange(onCadBudgetSettingsChanged);
-  uiFolder.add(state, 'limitDrawcalls').name('Limit draw cals').onChange(onCadBudgetSettingsChanged);
+  uiFolder.add(state, 'limitDrawcalls').name('Limit draw calls').onChange(onCadBudgetSettingsChanged);
 }
