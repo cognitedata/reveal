81fd265c665805b34836f33793a919b7eef08c8c
issaafalkattan <issaaf.kattan@cognite.com>
2021-09-21 10:24:59 +0200

    Add overlay description to diagram status

--- src/containers/Files/FilePreview/DiagramStatus.tsx
+++ src/containers/Files/FilePreview/DiagramStatus.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FileInfo } from '@cognite/sdk';
-import { Label, LabelVariants } from '@cognite/cogs.js';
+import { Label, LabelVariants, Tooltip } from '@cognite/cogs.js';
 import {
   PENDING_LABEL,
   INTERACTIVE_LABEL,
@@ -14,6 +14,7 @@ export type ReviewStatus = {
   status: string;
   variant: LabelVariants;
   label: string;
+  tooltip: string;
 };
 
 export const approvalDetails: { [key: string]: ReviewStatus } = {
@@ -21,16 +22,19 @@ export const approvalDetails: { [key: string]: ReviewStatus } = {
     status: PENDING_LABEL.externalId,
     variant: 'warning',
     label: 'Pending approval',
+    tooltip: 'New detected tags that need to be reviewed',
   },
   approved: {
     status: INTERACTIVE_LABEL.externalId,
     variant: 'success',
     label: 'Approved',
+    tooltip: 'All tags have been reviewed',
   },
   unknown: {
     status: 'N\\A',
     variant: 'unknown',
     label: 'N\\A',
+    tooltip: 'No tags were found in the diagram',
   },
 };
 export default function DiagramReviewStatus({ file }: Props) {
@@ -48,8 +52,10 @@ export default function DiagramReviewStatus({ file }: Props) {
   }, [file]);
 
   return (
-    <Label size="small" variant={fileStatus.variant}>
-      {fileStatus.label}
-    </Label>
+    <Tooltip content={fileStatus.tooltip}>
+      <Label size="small" variant={fileStatus.variant}>
+        {fileStatus.label}
+      </Label>
+    </Tooltip>
   );
 }
