eb61063e5443b3f71c4f2ceebc43c25ad0d480e1
Begum Ozcan-Caliskaner <begum.ozcan@cognite.com>
2021-09-23 10:25:09 +0200

    [CHART-75][UX] Add function description modal (#696)
    * [CHART-75] Clean up unnecessary styling
    
    * [CHART-75] Add info button
    
    * [CHART-75] Add modal
    
    * [CHART-75] Style the modal a bit
    
    * [CHART-75] Keep dropdown visible when modal is opened
    
    * [CHART-75] Remove white-space property
    
    * [CHART-75] Remove console.log......
    
    * [CHART-75] Make sure dropdown state stays same

--- src/reducers/charts/Nodes/DSPToolboxFunction.tsx
+++ src/reducers/charts/Nodes/DSPToolboxFunction.tsx
@@ -11,6 +11,7 @@ import {
   Button,
   Menu,
   Checkbox,
+  Modal,
 } from '@cognite/cogs.js';
 import {
   DSPFunction,
@@ -22,6 +23,7 @@ import {
 import { StorableNode } from 'reducers/charts/types';
 import { useAvailableOps } from 'components/NodeEditor/AvailableOps';
 import { trackUsage } from 'utils/metrics';
+import Layers from 'utils/z-index';
 import { ConfigPanelComponentProps } from '../types';
 import { DSPToolboxFunctionInput } from './DSPToolboxFunctionInput';
 
@@ -53,7 +55,10 @@ export const ConfigPanel = ({
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [phrase, setPhrase] = useState<string>('');
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [visible, setVisible] = useState<boolean>(false);
+  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [selectedCategory, setSelectedCategory] = useState<string>();
+  const [selectedFunction, setSelectedFunction] = useState<DSPFunction>();
 
   const [isLoading, error, operations] = useAvailableOps();
 
@@ -139,13 +144,26 @@ export const ConfigPanel = ({
                   toolFunction: storableNextFunc,
                 },
               });
-              setVisible(false);
+              setIsDropdownVisible(false);
               trackUsage('ChartView.SelectFunction', {
                 function: func.name,
               });
             }}
           >
-            <span style={{ textAlign: 'left' }}>{func.name}</span>
+            <FunctionNameWrapper>
+              <span style={{ textAlign: 'left' }}>{func.name}</span>
+              {func.description && (
+                <InfoButton
+                  type="Info"
+                  id={`${func.op}-info-button`}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setSelectedFunction(func);
+                    setIsModalVisible(true);
+                  }}
+                />
+              )}
+            </FunctionNameWrapper>
           </Menu.Item>
         ))}
       </>
@@ -168,27 +186,50 @@ export const ConfigPanel = ({
 
   const renderCategoryMenu = (categories: { [key: string]: DSPFunction[] }) => {
     return (
-      <Menu>
-        {Object.entries(categories).map(([category, toolFunctions]) => {
-          return (
-            <React.Fragment key={category}>
-              <Menu.Submenu
-                content={
-                  !!toolFunctions.length && (
-                    <Menu
-                      className="sub-menu"
-                      style={{ maxHeight: 615, overflowY: 'auto' }}
-                    >
+      <Menu style={{ boxShadow: 'none' }}>
+        {/* Recent category */}
+        {!!categories.Recent.length && (
+          <>
+            <Menu.Submenu
+              content={
+                <Menu style={{ maxHeight: 615, overflowY: 'auto' }}>
+                  {renderFunctionsList('Recent', categories.Recent)}
+                </Menu>
+              }
+            >
+              <span>Recent</span>
+            </Menu.Submenu>
+            <Menu.Divider />
+          </>
+        )}
+        {/* Toolbox function categories */}
+        {Object.entries(categories)
+          .filter(([category]) => category !== 'Recent')
+          .map(
+            ([category, toolFunctions]) =>
+              !!toolFunctions.length && (
+                <Menu.Submenu
+                  key={category}
+                  visible={selectedCategory === category}
+                  trigger={undefined}
+                  content={
+                    <Menu style={{ maxHeight: 615, overflowY: 'auto' }}>
                       {renderFunctionsList(category, toolFunctions)}
                     </Menu>
-                  )
-                }
-              >
-                <>{category}</>
-              </Menu.Submenu>
-            </React.Fragment>
-          );
-        })}
+                  }
+                >
+                  <CategoryItem
+                    role="button"
+                    tabIndex={0}
+                    onClick={() => setSelectedCategory(category)}
+                    onKeyDown={() => setSelectedCategory(category)}
+                    style={{ width: '100%', textAlign: 'left' }}
+                  >
+                    {category}
+                  </CategoryItem>
+                </Menu.Submenu>
+              )
+          )}
       </Menu>
     );
   };
@@ -197,9 +238,13 @@ export const ConfigPanel = ({
     const categories = getCategories(availableOperations);
 
     return (
-      <StyledDropdown
-        visible={visible}
-        onClickOutside={() => setVisible(false)}
+      <ToolboxFunctionsDropdown
+        visible={isDropdownVisible}
+        // Prevent dropdown from closing on modal click
+        onClickOutside={
+          !isModalVisible ? () => setIsDropdownVisible(false) : () => {}
+        }
+        zIndex={Layers.DROPDOWN}
         content={
           <>
             <Input
@@ -222,7 +267,8 @@ export const ConfigPanel = ({
           icon="ChevronDownCompact"
           iconPlacement="right"
           onClick={() => {
-            setVisible(true);
+            setIsDropdownVisible(!isDropdownVisible);
+            setSelectedCategory(undefined);
             setTimeout(() => {
               const phraseEl = document.getElementById('phrase');
               if (phraseEl) {
@@ -234,7 +280,7 @@ export const ConfigPanel = ({
         >
           {functionData?.toolFunction?.name || 'Select tool function'}
         </Button>
-      </StyledDropdown>
+      </ToolboxFunctionsDropdown>
     );
   };
 
@@ -285,95 +331,71 @@ export const ConfigPanel = ({
     : [];
 
   return (
-    <ToolFunctionWrapper>
+    <>
       <h4>Tool Function</h4>
       {error && <Icon style={{ color: 'white' }} type="XLarge" />}
       {isLoading && <Icon style={{ color: 'white' }} type="Loading" />}
       {operations && renderAvailableOperations(operations)}
       {hasParameters && renderParameter(parameters)}
-    </ToolFunctionWrapper>
+      <InfoModal
+        appElement={document.getElementsByTagName('body')}
+        title={selectedFunction?.name}
+        visible={isModalVisible}
+        footer={null}
+        onCancel={() => {
+          setIsModalVisible(false);
+        }}
+        width={750}
+      >
+        <p>{selectedFunction?.description}</p>
+      </InfoModal>
+    </>
   );
 };
 
-const ToolFunctionWrapper = styled.div`
-  .cogs-checkbox .checkbox-ui {
-    margin: 0;
+const ToolboxFunctionsDropdown = styled(Dropdown)`
+  width: 275px;
+
+  .cogs-input-container {
+    padding: 8px;
+
+    input,
+    input:hover,
+    input:focus {
+      color: var(--cogs-greyscale-grey7);
+      background: var(--cogs-greyscale-grey2) !important;
+      box-shadow: none;
+    }
   }
 `;
 
-const StyledDropdown = styled(Dropdown)`
-  &.cogs-dropdown.tippy-box {
-    top: 8px;
-    border-radius: 8px;
-    color: var(--cogs-greyscale-grey7);
-    padding: 8px;
+const FunctionNameWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
-    .cogs-input-container {
-      color: var(--cogs-greyscale-grey6);
-
-      input,
-      input:hover,
-      input:focus {
-        width: 100%;
-        height: 36px;
-        padding-left: 34px;
-        border: none;
-        border-radius: 6px;
-        color: var(--cogs-greyscale-grey7);
-        background: #f1f1f1 !important;
-        box-shadow: none;
-      }
-
-      .cogs-input.with-icon-left ~ .input-icon {
-        left: 10px;
-      }
-    }
-
-    input::placeholder {
-      color: var(--cogs-greyscale-grey7);
-    }
-
-    .cogs-menu.sub-menu {
-      padding: 8px;
-    }
-
-    .cogs-menu {
-      > span:nth-of-type(1) {
-        margin-bottom: 10px;
-
-        > button {
-          margin-bottom: 8px;
-        }
-
-        &::after {
-          position: absolute;
-          left: -8px;
-          content: ' ';
-          display: block;
-          border-bottom: 2px solid var(--cogs-greyscale-grey4);
-          width: 106.5%;
-        }
-      }
-
-      padding: 8px 0;
-      width: 250px;
-      box-shadow: none;
-      border: none;
-      border-radius: 8px;
-
-      .cogs-menu-header {
-        margin: 0;
-        color: var(--cogs-greyscale-grey6);
-        font-weight: 400;
-        text-transform: none;
-      }
-
-      .cogs-menu-item:hover {
-        color: var(--cogs-greyscale-grey9);
-        background: var(--cogs-greyscale-grey3);
-      }
-    }
+  span {
+    line-height: 28px;
   }
+`;
+
+const InfoButton = styled(Icon)`
+  margin-left: 10px;
+`;
+
+const InfoModal = styled(Modal)`
+  .cogs-modal-header {
+    border-bottom: none;
+    font-size: var(--cogs-t3-font-size);
+  }
+`;
+
+const CategoryItem = styled.div`
+  height: 100%;
+  width: 100%;
+  text-align: left;
 `;
 
 export const node = {
