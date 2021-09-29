d945fc2883190c88583a4d088a749fb73ad4f210
Anna Gadacz <61664776+agadacz-cognite@users.noreply.github.com>
2021-09-29 11:29:11 +0200

    fix: [CXT-268] screen flickering while hovering the searchbar (#396)
    * chore(cxt-268): cogs version bump
    
    * refactor(cxt-268): fix the number input on settings page
    
    * fix: cdf-hub styles no longer overwrite context-ui-pnid styles

--- src/pages/PageOptions/AdvancedOptions/OptionMinTokens.tsx
+++ src/pages/PageOptions/AdvancedOptions/OptionMinTokens.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store';
 import { Body, Input } from '@cognite/cogs.js';
 import { changeOptions } from 'modules/workflows';
+import { Flex } from 'components/Common';
 import { OptionWrapper } from 'pages/PageOptions/components';
 
 export const OptionMinTokens = ({ workflowId }: { workflowId: number }) => {
@@ -10,10 +11,16 @@ export const OptionMinTokens = ({ workflowId }: { workflowId: number }) => {
   const { minTokens } = useSelector(
     (state: RootState) => state.workflows.items[workflowId].options
   );
+  const [value, setValue] = useState(minTokens ?? 2);
 
   const onMinTokensChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(changeOptions({ minTokens: Number(event.target.value) }));
+    setValue(Number(event.target.value));
   };
+
+  useEffect(() => {
+    dispatch(changeOptions({ minTokens: value }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
 
   return (
     <OptionWrapper>
@@ -25,15 +32,19 @@ export const OptionMinTokens = ({ workflowId }: { workflowId: number }) => {
         number of tokens. That is, substrings of consecutive letters or
         consecutive digits.
       </Body>
-      <Input
-        type="number"
-        name="minTokensOption"
-        value={minTokens}
-        min={1}
-        step={1}
-        onChange={onMinTokensChange}
-        style={{ width: '240px', marginTop: '12px' }}
-      />
+      <Flex style={{ flex: 1, maxWidth: '240px', marginTop: '12px' }}>
+        <Input
+          type="number"
+          name="minTokensOption"
+          min={1}
+          max={9999}
+          step={1}
+          value={value}
+          setValue={setValue}
+          onChange={onMinTokensChange}
+          width={240}
+        />
+      </Flex>
     </OptionWrapper>
   );
 };
