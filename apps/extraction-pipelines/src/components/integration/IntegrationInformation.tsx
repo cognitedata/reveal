058391036459be42b25387b2633f8b21a002ecc6
drhumlen <niklas.humlen@gmail.com>
2021-09-29 14:07:13 +0200

    Divide side information into sections with individual headers

--- apps/extraction-pipelines/src/components/integration/IntegrationInformation.tsx
+++ apps/extraction-pipelines/src/components/integration/IntegrationInformation.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, PropsWithChildren } from 'react';
 import styled from 'styled-components';
 import InlineEdit from 'components/integration/InlineEdit';
 import { DetailFieldNames } from 'model/Integration';
@@ -17,10 +17,31 @@ import { MetaData } from 'components/integration/MetaData';
 import { EditDataSetId } from 'components/integration/edit/EditDataSetId';
 import { bottomSpacing, sideBarSectionSpacing } from 'styles/StyledVariables';
 
-const Wrapper = styled.div`
-  padding: 2rem;
-  background-color: ${Colors['greyscale-grey1'].hex()};
+const SectionDiv = styled.div`
+  background-color: white;
+  box-shadow: 0px 1px 3px #0002;
+  margin: 10px 0;
+  border-radius: 3px;
 `;
+const SectionHeader = styled.div`
+  font-size: 1.1em;
+  font-weight: 500;
+  padding: 1em;
+  border-bottom: 1px solid #eee;
+`;
+const SectionBody = styled.div`
+  padding: 0.3em;
+`;
+
+const Section = (props: PropsWithChildren<{ title: string }>) => {
+  return (
+    <SectionDiv>
+      <SectionHeader>{props.title}</SectionHeader>
+      <SectionBody>{props.children}</SectionBody>
+    </SectionDiv>
+  );
+};
+
 interface IntegrationInformationProps {
   canEdit: boolean;
 }
@@ -36,67 +57,75 @@ export const IntegrationInformation: FunctionComponent<IntegrationInformationPro
   }
 
   return (
-    <Wrapper>
-      <ContactsView canEdit={canEdit} />
-      <InlineEdit
-        name="externalId"
-        label={DetailFieldNames.EXTERNAL_ID}
-        canEdit={canEdit}
-        schema={yup.object().shape({
-          externalId: yup.string().required('ExternalId is required'),
-        })}
-        defaultValues={{ externalId: integration?.externalId }}
-        fullWidth
-        updateFn={rootUpdate({ integration, name: 'externalId', project })}
-        marginBottom
-        showLabel
-      />
-      <FieldVerticalDisplay
-        label={DetailFieldNames.ID}
-        fieldName="id"
-        fieldValue={integration?.id}
-        marginBottom={bottomSpacing}
-      />
-      <EditDataSetId canEdit={canEdit} />
-      <EditRawTable canEdit={canEdit} />
-      <InlineEdit
-        name="source"
-        label={DetailFieldNames.SOURCE}
-        canEdit={canEdit}
-        schema={yup.object().shape({})}
-        updateFn={rootUpdate({ integration, name: 'source', project })}
-        defaultValues={{
-          source: integration?.source,
-        }}
-        fullWidth
-        showLabel
-        marginBottom
-      />
-      <Schedule
-        name="schedule"
-        integration={integration}
-        label={TableHeadings.SCHEDULE}
-        canEdit={canEdit}
-      />
-      <FieldVerticalDisplay
-        label={DetailFieldNames.CREATED_BY}
-        fieldName="createdBy"
-        fieldValue={integration?.createdBy}
-        marginBottom={bottomSpacing}
-      />
-      <FieldVerticalDisplay
-        label={DetailFieldNames.CREATED_TIME}
-        fieldName="createdTime"
-        fieldValue={integration?.createdTime}
-        marginBottom={bottomSpacing}
-      />
-      <FieldVerticalDisplay
-        label={DetailFieldNames.LAST_UPDATED_TIME}
-        fieldName="lastUpdatedTime"
-        fieldValue={integration?.lastUpdatedTime}
-        marginBottom={sideBarSectionSpacing}
-      />
-      <MetaData canEdit={canEdit} />
-    </Wrapper>
+    <div>
+      <Section title="Basic information">
+        <InlineEdit
+          name="externalId"
+          label={DetailFieldNames.EXTERNAL_ID}
+          canEdit={canEdit}
+          schema={yup.object().shape({
+            externalId: yup.string().required('ExternalId is required'),
+          })}
+          defaultValues={{ externalId: integration?.externalId }}
+          fullWidth
+          updateFn={rootUpdate({ integration, name: 'externalId', project })}
+          marginBottom
+          showLabel
+        />
+        <FieldVerticalDisplay
+          label={DetailFieldNames.ID}
+          fieldName="id"
+          fieldValue={integration?.id}
+          marginBottom={bottomSpacing}
+        />
+        <EditDataSetId canEdit={canEdit} />
+        <EditRawTable canEdit={canEdit} />
+        <InlineEdit
+          name="source"
+          label={DetailFieldNames.SOURCE}
+          canEdit={canEdit}
+          schema={yup.object().shape({})}
+          updateFn={rootUpdate({ integration, name: 'source', project })}
+          defaultValues={{
+            source: integration?.source,
+          }}
+          fullWidth
+          showLabel
+          marginBottom
+        />
+        <Schedule
+          name="schedule"
+          integration={integration}
+          label={TableHeadings.SCHEDULE}
+          canEdit={canEdit}
+        />
+      </Section>
+      <Section title="Contacts">
+        <ContactsView canEdit={canEdit} />
+      </Section>
+      <Section title="Additional metadata">
+        <MetaData canEdit={canEdit} />
+      </Section>
+      <Section title="About extraction pipeline">
+        <FieldVerticalDisplay
+          label={DetailFieldNames.CREATED_BY}
+          fieldName="createdBy"
+          fieldValue={integration?.createdBy}
+          marginBottom={bottomSpacing}
+        />
+        <FieldVerticalDisplay
+          label={DetailFieldNames.CREATED_TIME}
+          fieldName="createdTime"
+          fieldValue={integration?.createdTime}
+          marginBottom={bottomSpacing}
+        />
+        <FieldVerticalDisplay
+          label={DetailFieldNames.LAST_UPDATED_TIME}
+          fieldName="lastUpdatedTime"
+          fieldValue={integration?.lastUpdatedTime}
+          marginBottom={sideBarSectionSpacing}
+        />
+      </Section>
+    </div>
   );
 };
