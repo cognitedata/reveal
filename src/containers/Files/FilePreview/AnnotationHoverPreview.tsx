3f568ee96d040d58e76c1fc1dbe7dc6f0f4aa7d3
Anna Gadacz <anna.gadacz@cognite.com>
2021-09-30 13:40:27 +0200

    feat(cxt-101): new hover preview for bounding boxes

---
+++ src/containers/Files/FilePreview/AnnotationHoverPreview.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { CogniteAnnotation } from '@cognite/annotations';
+import { ProposedCogniteAnnotation } from '@cognite/react-picture-annotation';
+import { Body, Colors, Detail } from '@cognite/cogs.js';
+
+type Props = {
+  annotation: Array<CogniteAnnotation | ProposedCogniteAnnotation>;
+};
+
+export const AnnotationHoverPreview = (props: Props) => {
+  const { annotation } = props;
+  return (
+    <Wrapper>
+      {annotation[0]?.status === 'unhandled' && <NewLabel strong>New</NewLabel>}
+      <Body
+        level={2}
+        strong
+        style={{
+          color: Colors.white.hex(),
+        }}
+      >
+        {annotation[0]?.label ?? 'N/A'}
+      </Body>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: ${Colors['greyscale-grey10'].hex()};
+  border-radius: 6px;
+  padding: 6px;
+`;
+const NewLabel = styled(Detail)`
+  background-color: ${Colors['midblue-6'].hex()};
+  color: ${Colors['midblue-2'].hex()};
+  border-radius: 4px;
+  padding: 2px 6px;
+  margin-right: 8px;
+`;
