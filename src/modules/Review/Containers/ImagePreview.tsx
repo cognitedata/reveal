73d16cb5eaa756f17a9ac3350a06448a51bdb777
Mihil Ranathunga <mihilranathunga@gmail.com>
2021-10-07 01:21:17 +0530

    add custom icon

--- src/modules/Review/Containers/ImagePreview.tsx
+++ src/modules/Review/Containers/ImagePreview.tsx
@@ -157,11 +157,35 @@ export const ImagePreview = ({
         >
           <ExtraToolItem
             type="ghost"
-            icon="ExternalLink"
             aria-label="keyboard shortcuts"
             onClick={onOpenKeyboardShortcuts}
             toggled={showKeyboardShortcutModal}
-          />
+          >
+            <KeyboardShortcutsIcon
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g
+                className={`KeyboardShortsIcon ${
+                  showKeyboardShortcutModal ? 'toggled' : ''
+                }`}
+              >
+                <path
+                  d="M2.92543 8.52667L2.77654 8.6933H3H5.43743V13V13.1H5.53743H10.4585H10.5585V13V8.6933H13H13.2236L13.0745 8.52664L8.07252 2.93334L7.99795 2.84995L7.92341 2.93337L2.92543 8.52667ZM5.33171 7.56938L7.99803 4.5813L10.6681 7.56938H9.47511H9.37511V7.66938V11.9761H6.62489V7.66938V7.56938H6.52489H5.33171Z"
+                  fill="#333333"
+                  stroke="#333333"
+                  strokeWidth="0.2"
+                />
+                <path
+                  d="M3 1.4H13V-1.4H3V1.4ZM14.6 3V13H17.4V3H14.6ZM13 14.6H3V17.4H13V14.6ZM1.4 13V3H-1.4V13H1.4ZM3 14.6C2.11634 14.6 1.4 13.8837 1.4 13H-1.4C-1.4 15.4301 0.569947 17.4 3 17.4V14.6ZM14.6 13C14.6 13.8837 13.8837 14.6 13 14.6V17.4C15.4301 17.4 17.4 15.4301 17.4 13H14.6ZM13 1.4C13.8837 1.4 14.6 2.11634 14.6 3H17.4C17.4 0.569947 15.4301 -1.4 13 -1.4V1.4ZM3 -1.4C0.569947 -1.4 -1.4 0.569947 -1.4 3H1.4C1.4 2.11634 2.11634 1.4 3 1.4V-1.4Z"
+                  fill="#333333"
+                />
+              </g>
+            </KeyboardShortcutsIcon>
+          </ExtraToolItem>
         </Tooltip>
         <Tooltip
           content={<span data-testid="text-content">Collection settings</span>}
@@ -204,4 +228,10 @@ const ExtraToolItem = styled(Button)`
   height: 50px;
   width: 50px;
   border-radius: 50px;
+`;
+
+const KeyboardShortcutsIcon = styled('svg')`
+  & .KeyboardShortsIcon.toggled path {
+    fill: #4a67fb;
+  }
 `;
