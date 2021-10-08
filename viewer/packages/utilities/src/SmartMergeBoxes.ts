63c0277916843d2e3efbee3e60c469f86c604b59
Håkon Flatval <hakon.flatval@cognite.com>
2021-10-08 17:12:24 +0200

    Add final resulting box list squashing
    Making the stupid algorithm ever so slightly less stupid

--- viewer/packages/utilities/src/SmartMergeBoxes.ts
+++ viewer/packages/utilities/src/SmartMergeBoxes.ts
@@ -2,7 +2,21 @@
  * Copyright 2021 Cognite AS
  */
 
-import { Box3 } from 'three';
+import { Box3, Vector3 } from 'three';
+
+
+const BOX_MERGE_MIN_IOU = 0.15;
+
+// IoU - Intersection over Union, measure of overlap between two boxes
+function iou(box1: Box3, box2: Box3): number {
+  const intersection = box1.clone().intersect(box2);
+  const union = box1.clone().union(box2);
+
+  const intsize = intersection.getSize(new Vector3());
+  const unsize = union.getSize(new Vector3());
+
+  return (intsize.x * intsize.y * intsize.z) / (unsize.x * unsize.y * unsize.z);
+}
 
 export class SmartMergeBoxes {
   resultBoxes: Box3[] = [];
@@ -13,7 +27,7 @@ export class SmartMergeBoxes {
 
       for (let i = 0; i < this.resultBoxes.length; i++) {
         if (box.intersectsBox(this.resultBoxes[i])) {
-          this.resultBoxes[i] = this.resultBoxes[i].union(box);
+          this.resultBoxes[i].union(box);
 
           merged = true;
           break;
@@ -25,8 +39,28 @@ export class SmartMergeBoxes {
       }
     }
   }
+  
+  squashBoxes(): void {
+    
+    
+    for (let i = 0; i < this.resultBoxes.length; i++) {
+      for (let j = i + 1; j < this.resultBoxes.length; j++) {
+        const overlap = iou(this.resultBoxes[i], this.resultBoxes[j]);
+        if (overlap >= BOX_MERGE_MIN_IOU) {
+          this.resultBoxes[i].union(this.resultBoxes[j]);
 
-  getBoxes(): Box3[] {
+          // Remove resultBoxes[j], replace by last box in list
+          this.resultBoxes[j] = this.resultBoxes[this.resultBoxes.length - 1];
+          this.resultBoxes.pop();
+        }
+      }
+    }
+  }
+  
+  
+  squashAndGetBoxes(): Box3[] {
+    this.squashBoxes();
+    
     return this.resultBoxes;
   }
 }
