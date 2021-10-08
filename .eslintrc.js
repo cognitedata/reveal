274609fe688dc44a56134ac7c9fde1b9c23de5fb
David Liu <david.liu@cognite.com>
2021-10-08 10:20:07 +0200

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,9 @@
 module.exports = {
-  extends: [
-    'plugin:testing-library/react',
-    'plugin:react-hooks/recommended',
-    '@cognite',
-  ],
-  plugins: ['@cognite', 'testing-library'],
+  extends: './.eslintrc.production.js',
+  // We can relax some settings here for nicer development experience; warnings will crash in CI
   rules: {
-    'react-hooks/exhaustive-deps': [
-      'warn',
-      {
-        additionalHooks: 'useRecoilCallback|useRecoilTransaction_UNSTABLE',
-      },
-    ],
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@cognite/no-unissued-todos': [
-      'error',
-      { issuePattern: '\\(((DEMO)-[0-9]+)\\)' },
-    ],
-    'no-param-reassign': ['error', { props: false }],
-    'react/require-default-props': ['off'],
-    'max-classes-per-file': ['off'],
-    'lines-between-class-members': ['off'],
-    'class-methods-use-this': ['off'],
-
-    'react/jsx-props-no-spreading': ['off'],
-    'react/static-property-placement': ['off'],
-    'react/state-in-constructor': ['off'],
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-
-    'jest/expect-expect': ['off'],
-    'jest/no-test-callback': ['off'],
-    'jest/no-export': ['off'],
-
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars-experimental': [
-      'error',
-      { ignoredNamesRegex: '^_' },
-    ],
     'prettier/prettier': [
-      'error',
+      'warn',
       {
         singleQuote: true,
         trailingComma: 'es5',
@@ -48,21 +11,13 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': 'error',
+    '@cognite/no-unissued-todos': [
+      'warn',
+      { issuePattern: '\\(((DEMO)-[0-9]+)\\)' },
+    ],
+    '@typescript-eslint/no-unused-vars-experimental': [
+      'warn',
+      { ignoredNamesRegex: '^_' },
+    ],
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  overrides: [
-    {
-      files: ['**/*.tsx'],
-      rules: {
-        'react/prop-types': 'off',
-      },
-    },
-  ],
 };
---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+yarn react-scripts test "$@" \
+  --collectCoverageFrom='!*/**/*.stories.tsx' \
+  --ci \
+  --verbose \
+  --runInBand \
+  --detectOpenHandles \
+  "$@"
