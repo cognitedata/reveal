9842e9c6d564960c2aa8c463da2d0dd6ff27f9d7
Håkon Flatval <hakon.flatval@cognite.com>
2021-10-08 15:09:04 +0200

    Lint fix

--- viewer/packages/utilities/src/smartMergeBoxes.ts
+++ viewer/packages/utilities/src/smartMergeBoxes.ts
@@ -1,24 +1,24 @@
-]
+/*!
+ * Copyright 2021 Cognite AS
+ */
 
 import { Box3 } from 'three';
 
 export function mergeBoxesSmart(boxes: Box3[]): Box3[] {
-
   const resultBoxes: Box3[] = [];
-  
+
   for (const box of boxes) {
     let merged = false;
-    
+
     for (let i = 0; i < resultBoxes.length; i++) {
       if (box.intersectsBox(resultBoxes[i])) {
-
         resultBoxes[i] = resultBoxes[i].union(box);
-        
+
         merged = true;
         break;
       }
     }
-    
+
     if (!merged) {
       resultBoxes.push(box);
     }
