970814862afd22584fd244c6e246796f94cbc097
Håkon Flatval <hakon.flatval@cognite.com>
2021-10-11 11:20:39 +0200

    Lint fix

--- viewer/packages/utilities/src/SmartMergeBoxes.test.ts
+++ viewer/packages/utilities/src/SmartMergeBoxes.test.ts
@@ -1,41 +1,42 @@
+/*!
+ * Copyright 2021 Cognite AS
+ */
 import { Box3, Vector3 } from 'three';
 import { SmartMergeBoxes } from './SmartMergeBoxes';
 
-
 describe('SmartMergeBoxes', () => {
-
-  test('add non-intersecting bboxes',  () => {
+  test('add non-intersecting bboxes', () => {
     const mergeBoxes = new SmartMergeBoxes();
-    
+
     const n = 10;
     const s = 10;
     for (let i = 0; i < n; i++) {
       for (let j = 0; j < n; j++) {
         for (let k = 0; k < n; k++) {
-          const box = new Box3(new Vector3(i * s, j * s, k * s),
-                               new Vector3(i * s + s - 1, j * s + s - 1, k * s + s - 1));
+          const box = new Box3(
+            new Vector3(i * s, j * s, k * s),
+            new Vector3(i * s + s - 1, j * s + s - 1, k * s + s - 1)
+          );
           mergeBoxes.addBoxes([box]);
         }
       }
     }
 
     const result = mergeBoxes.squashAndGetBoxes();
-    
+
     expect(result.length == n * n * n);
   });
 
-
   test('add intersecting bboxes', () => {
     const mergeBoxes = new SmartMergeBoxes();
-    
+
     const boxes: Box3[] = [];
 
     const n = 100;
     const s = 10;
-    
+
     for (let i = 0; i < n; i++) {
-      const box = new Box3(new Vector3(0, 0, i * s),
-                           new Vector3(1, 1, i * (s + 1)));
+      const box = new Box3(new Vector3(0, 0, i * s), new Vector3(1, 1, i * (s + 1)));
       boxes.push(box);
     }
 
