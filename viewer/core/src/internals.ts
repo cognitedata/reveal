5230f9d320d4b63f24e3bd0287392c9a4452fba1
Lars Moastuen <lars.moastuen@cognite.com>
2021-10-11 16:51:12 +0200

    Move exports

--- viewer/core/src/internals.ts
+++ viewer/core/src/internals.ts
@@ -5,19 +5,6 @@
 export { RevealManager } from './public/RevealManager';
 export { createLocalRevealManager, createCdfRevealManager } from './public/createRevealManager';
 
-export { CadModelMetadata, SectorMetadata, LevelOfDetail, WantedSector } from '@reveal/cad-parsers';
-export {
-  CadLoadingHints,
-  CadNode,
-  SuggestedCameraConfig,
-  SectorCuller,
-  DetermineSectorsInput,
-  ByVisibilityGpuSectorCuller,
-  GpuOrderSectorsByVisibilityCoverage
-} from '@reveal/cad-geometry-loaders';
-export { NodeAppearance, DefaultNodeAppearance, NodeAppearanceProvider } from '@reveal/cad-styling';
-export { revealEnv } from '@reveal/utilities';
-
 // CAD
 export { intersectCadNodes } from './datamodels/cad/picking';
 export { CadModelSectorLoadStatistics } from './datamodels/cad/CadModelSectorLoadStatistics';
--- viewer/internals.ts
+++ viewer/internals.ts
@@ -3,3 +3,16 @@
  */
 
 export * from './core/internals';
+
+export { CadModelMetadata, SectorMetadata, LevelOfDetail, WantedSector } from './packages/cad-parsers';
+export {
+  CadLoadingHints,
+  CadNode,
+  SuggestedCameraConfig,
+  SectorCuller,
+  DetermineSectorsInput,
+  ByVisibilityGpuSectorCuller,
+  GpuOrderSectorsByVisibilityCoverage
+} from './packages/cad-geometry-loaders';
+export { NodeAppearance, DefaultNodeAppearance, NodeAppearanceProvider } from '@reveal/cad-styling';
+export { revealEnv } from './packages/utilities';
