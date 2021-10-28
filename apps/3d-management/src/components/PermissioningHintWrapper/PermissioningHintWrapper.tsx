b3f45933b17187d931274542c669df758bf89c9d
Michael Susæg <41901890+msusag@users.noreply.github.com>
2021-10-28 16:43:07 +0200

    Extend tooltip about permissions with details about 3d capabilities needed

--- apps/3d-management/src/components/PermissioningHintWrapper/PermissioningHintWrapper.tsx
+++ apps/3d-management/src/components/PermissioningHintWrapper/PermissioningHintWrapper.tsx
@@ -11,7 +11,7 @@ export default ({
   }
   return (
     <Tooltip
-      title="To gain access, please request permission from your administrator."
+      title="To gain access, please request permission from your administrator. You may need 3d:read, 3d:create, 3d:update, 3d:delete and files:read and files:write for full 3d access."
       getPopupContainer={getContainer}
     >
       {children}
