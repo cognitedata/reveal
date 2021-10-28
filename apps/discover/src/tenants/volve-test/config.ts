da1788b736c6bfac6f05f5f90373ab84dcf84c96
Sigurd Holsen <sigurd.holsen@cognite.com>
2021-10-28 09:45:29 +0200

    chore(discover): add volve-test to discover (#1090)
    Link to JIRA:
    https://cognitedata.atlassian.net/browse/WDL-312
    
    # What has changed?
    
    Add `volve-test` to discover. The files are based on `owa-ui-dev`. I've removed some optional properties.

---
+++ apps/discover/src/tenants/volve-test/config.ts
@@ -0,0 +1,16 @@
+import { defaultWellsConfig } from 'tenants/config';
+import { TenantConfig } from 'tenants/types';
+
+import config from '../config';
+
+const defaultConfig: TenantConfig = {
+  ...config, // Contain common configurations for all tenants
+  // searchableLayerTitle: '',
+  ...defaultWellsConfig,
+  seismic: {
+    disabled: true,
+  },
+  enableWellSDKV3: true,
+};
+
+export default defaultConfig;
