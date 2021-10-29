f2c2b59a1b45baa16583298bc3fec2cf9d3d1a9d
Pramod S <pramod.s@cognite.com>
2021-10-29 13:25:40 +0200

    Added Timeline Tool unit test for Play

--- viewer/packages/tools/src/Timeline/TimelineTool.test.ts
+++ viewer/packages/tools/src/Timeline/TimelineTool.test.ts
@@ -3,6 +3,11 @@
  */
 import { Cognite3DModel } from '@reveal/core';
 import { CogniteClient } from '@cognite/sdk';
+import { CadMaterialManager, CadNode } from '@reveal/rendering';
+import { CadModelMetadata } from '@reveal/cad-parsers';
+import { createCadModelMetadata, generateSectorTree } from '../../../../test-utilities';
+import { NodesLocalClient } from '@reveal/nodes-api';
+
 import { IndexSet } from '../../../../packages/utilities';
 import { TreeIndexNodeCollection } from '../../../../packages/cad-styling';
 import nock from 'nock';
@@ -16,7 +21,16 @@ describe('TimelineTool', () => {
   beforeEach(() => {
     client = new CogniteClient({ appId: 'test', baseUrl: 'http://localhost' });
     client.loginWithApiKey({ apiKey: 'dummy', project: 'unittest' });
-    model = { modelId: 112, revisionId: 113 } as Cognite3DModel;
+
+    const materialManager = new CadMaterialManager();
+    const cadRoot = generateSectorTree(3, 3);
+    const cadMetadata: CadModelMetadata = createCadModelMetadata(cadRoot);
+    materialManager.addModelMaterials(cadMetadata.modelIdentifier, cadMetadata.scene.maxTreeIndex);
+
+    const cadNode = new CadNode(cadMetadata, materialManager);
+    const apiClient = new NodesLocalClient();
+
+    model = new Cognite3DModel(1, 2, cadNode, apiClient);
     jest.useFakeTimers();
   });
 
@@ -42,28 +56,37 @@ describe('TimelineTool', () => {
 
   test('Test assign Node & style to the keyframe & Play the Timeline', () => {
     const timelineTool = new TimelineTool(model);
-    const assignStyledNodeCollection = jest.spyOn(model, 'assignStyledNodeCollection');
-    const unassignStyledNodeCollection = jest.spyOn(model, 'unassignStyledNodeCollection');
+    const assignStyledNodeCollectionSpy = jest.spyOn(model, 'assignStyledNodeCollection');
+    const unassignStyledNodeCollectionSpy = jest.spyOn(model, 'unassignStyledNodeCollection');
 
     const kf1 = timelineTool.createKeyframe(new Date('2021-10-25'));
     const kf2 = timelineTool.createKeyframe(new Date('2021-10-26'));
     const kf3 = timelineTool.createKeyframe(new Date('2021-10-27'));
 
-    kf1.assignStyledNodeCollection(new TreeIndexNodeCollection(new IndexSet([1, 2, 3])), { renderGhosted: true });
-    kf2.assignStyledNodeCollection(new TreeIndexNodeCollection(new IndexSet([4, 5, 6])), { renderGhosted: true });
-    kf3.assignStyledNodeCollection(new TreeIndexNodeCollection(new IndexSet([7, 8, 9])), { renderGhosted: true });
+    const kf1Collection = new TreeIndexNodeCollection(new IndexSet([1, 2, 3]));
+    const kf1Appearance = { renderGhosted: false };
+    const kf2Collection = new TreeIndexNodeCollection(new IndexSet([4, 5, 6]));
+    const kf2Appearance = { renderGhosted: true };
+    const kf3Collection = new TreeIndexNodeCollection(new IndexSet([7, 8, 9]));
+    const kf3Appearance = { renderGhosted: false };
 
-    expect(kf1).not.toBeEmpty();
-    expect(kf2).not.toBeEmpty();
-    expect(kf3).not.toBeEmpty();
+    kf1.assignStyledNodeCollection(kf1Collection, kf1Appearance);
+    kf2.assignStyledNodeCollection(kf2Collection, kf2Appearance);
+    kf3.assignStyledNodeCollection(kf3Collection, kf3Appearance);
 
-    timelineTool.play(new Date('2021-10-25'), new Date('2021-10-27'), 20000);
+    timelineTool.play(new Date('2021-10-25'), new Date('2021-10-27'), 30000);
     const current = TWEEN.now();
-    expect(assignStyledNodeCollection).toBeCalled();
-    TWEEN.update(current + 500);
-    expect(assignStyledNodeCollection).toBeCalled();
-    TWEEN.update(current + 2000);
-    expect(assignStyledNodeCollection).toBeCalled();
-    TWEEN.update(current + 3000);
+    TWEEN.update(current + 10000);
+
+    expect(assignStyledNodeCollectionSpy).toBeCalledWith(kf1Collection, kf1Appearance);
+    TWEEN.update(current + 20000);
+
+    expect(assignStyledNodeCollectionSpy).toBeCalledWith(kf2Collection, kf2Appearance);
+    TWEEN.update(current + 30000);
+
+    expect(assignStyledNodeCollectionSpy).toBeCalledWith(kf3Collection, kf3Appearance);
+    TWEEN.update();
+
+    expect(unassignStyledNodeCollectionSpy).toBeCalledWith(kf1Collection);
   });
 });
