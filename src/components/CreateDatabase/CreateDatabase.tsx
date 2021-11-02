39d52527f611388ebdafdbed332101f478ce72e3
Vegard Økland <vegard.okland@cognite.com>
2021-11-02 17:54:38 +0100

    Make CreateDatabase.tsx self contained

--- src/components/CreateDatabase/CreateDatabase.tsx
+++ src/components/CreateDatabase/CreateDatabase.tsx
@@ -1,30 +1,105 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Col, Input, Row } from 'antd';
+import { message, Col, Input, Row, Modal, Alert } from 'antd';
+import { Button, Icon } from '@cognite/cogs.js';
+import { useCreateDatabase } from 'hooks/sdk-queries';
+import { getContainer } from 'utils/utils';
+import { useUserCapabilities } from 'hooks/useUserCapabilities';
+import { trackEvent } from '@cognite/cdf-route-tracker';
+import { useHistory } from 'react-router-dom';
+import { createLink } from '@cognite/cdf-utilities';
 
-interface CreateDbProps {
-  name: string;
-  setName(value: string): void;
-  createDatabase(): void;
-}
-const CreateDatabase = (props: CreateDbProps) => {
+export default function CreateDatabase() {
+  const history = useHistory();
+  const [visible, setVisible] = useState(false);
+  const [name, setName] = useState('');
+
+  const { data: hasWriteAccess } = useUserCapabilities('rawAcl', 'WRITE');
+  const {
+    mutate,
+    isSuccess,
+    isLoading,
+    isError,
+    error,
+    reset,
+  } = useCreateDatabase();
+
+  useEffect(() => {
+    if (isSuccess) {
+      reset();
+      message.success({
+        content: `Database was created!`,
+        key: 'create-database',
+      });
+      history.push(createLink(`/raw-explorer/${name}`));
+      setName('');
+      trackEvent('RAW.Explorer.Database.Create');
+      setVisible(false);
+    }
+  }, [name, isSuccess, reset, history]);
+
   return (
-    <div>
-      <Row>
-        <Col span={6}> Unique name </Col>
-        <Col span={16}>
-          <Input
-            aria-label="Searched database name"
-            value={props.name}
-            autoFocus
-            onChange={(e) => props.setName(e.currentTarget.value)}
-            placeholder="Please enter database name"
-            onPressEnter={() => props.createDatabase()}
-          />
-        </Col>
-      </Row>
-    </div>
-  );
-};
+    <>
+      {visible && (
+        <Modal
+          title={
+            isLoading ? (
+              <>
+                Creating database <Icon type="Loading" />
+              </>
+            ) : (
+              'Create database'
+            )
+          }
+          visible
+          onCancel={() => setVisible(false)}
+          okText="Create"
+          onOk={() => mutate({ name })}
+          okButtonProps={{ disabled: isLoading }}
+          cancelButtonProps={{ disabled: isLoading }}
+          getContainer={getContainer}
+        >
+          <Row>
+            <Col span={6}> Unique name</Col>
+            <Col span={16}>
+              <div>
+                <Input
+                  aria-label="Searched database name"
+                  value={name}
+                  autoFocus
+                  onChange={(e) => setName(e.currentTarget.value)}
+                  placeholder="Please enter database name"
+                  onPressEnter={() => mutate({ name })}
+                />
+              </div>
+            </Col>
+          </Row>
 
-export default CreateDatabase;
+          {isError && (
+            <>
+              <Alert
+                type="error"
+                message={
+                  <>
+                    An error ocurred:{' '}
+                    <pre>{JSON.stringify((error as any)?.errors, null, 2)}</pre>
+                  </>
+                }
+              />
+            </>
+          )}
+        </Modal>
+      )}
+
+      <Button
+        style={{ width: '100%', marginBottom: '5px' }}
+        icon="PlusCompact"
+        type="primary"
+        onClick={() => setVisible(true)}
+        disabled={!hasWriteAccess}
+      >
+        Create Database
+      </Button>
+    </>
+  );
+}
