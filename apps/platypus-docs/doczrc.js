10786e2304e1996dedd8742bbd79e0ea2d0773c9
Denis Pokotylyuk <denis@digits.no>
2021-11-09 10:14:24 +0100

    Improved configuration.

--- apps/platypus-docs/doczrc.js
+++ apps/platypus-docs/doczrc.js
@@ -1,5 +1,9 @@
 export default {
   typescript: true,
+  port: '4838',
+  base: '/',
+  src: './apps/platypus-docs/src/',
+  dest: './dist/apps/platypus-docs',
   title: 'Official Platypus documentation',
   menu: ['Getting Started', 'CLI', 'UI'],
 };
