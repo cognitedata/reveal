dd05ff0c3f4bab40c7f22ed7335ca541008b1dc9
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2021-11-09 12:03:04 +0100

    feat: improve user experience for disable legacy login flow warning (#136)
    * feat: improve user experience for disable legacy login flow warning
    
    - Do not display for users without 'projects:update' capability
    - Move requirements into banner from tooltip
    - Add a confirmation modal before as an additional step
    - Disable action button for 5 seconds
    
    * chore: move requirements text into a separate icon/tooltip
    
    * chore: replace disable with deprecate in content

--- src/pages/IDP/LegacyLoginFlowWarning.tsx
+++ src/pages/IDP/LegacyLoginFlowWarning.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import { useHistory, useRouteMatch } from 'react-router';
 
-import { Button as CogsButton, Icon } from '@cognite/cogs.js';
+import { Button, Colors, Icon, Tooltip } from '@cognite/cogs.js';
 import { useSDK } from '@cognite/sdk-provider';
 import { usePermissions } from '@cognite/sdk-react-query-hooks';
-import { Alert as AntdAlert, Tooltip, notification } from 'antd';
+import { Alert as AntdAlert, Modal, notification } from 'antd';
 import styled from 'styled-components';
 import { useAuthConfiguration, useGroups } from 'hooks';
 import { hasAnyValidGroupForOIDC } from 'pages/Groups/utils';
 
+const NUMBER_OF_SECONDS_TO_ALLOW_DISABLING = 5;
+
 const StyledAlert = styled(AntdAlert)`
   margin-bottom: 16px;
-`;
-
-const StyledButton = styled(CogsButton)`
-  margin: 0 8px 4px 0;
 `;
 
 const StyledIcon = styled(Icon).attrs(({ $success }: { $success: boolean }) => {
@@ -27,6 +25,40 @@ const StyledIcon = styled(Icon).attrs(({ $success }: { $success: boolean }) => {
   margin-right: 6px;
   color: ${({ $success }) =>
     $success ? 'var(--cogs-success)' : 'var(--cogs-danger)'};
+`;
+
+const StyledModalContent = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+`;
+
+const StyledModalWarningIcon = styled(Icon)`
+  color: ${Colors.danger};
+  margin-right: 8px;
+`;
+
+const StyledModalButtons = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+`;
+
+const StyledModalButton = styled(Button)`
+  :not(:last-child) {
+    margin-right: 12px;
+  }
+`;
+
+const StyledDisableButtonSection = styled.div`
+  align-items: center;
+  display: flex;
+  margin-bottom: 4px;
+`;
+
+const StyledHelpIcon = styled(Icon)`
+  color: ${Colors['text-hint']};
+  margin: 4px 0 0 8px;
 `;
 
 const LegacyLoginFlowWarning = () => {
@@ -49,6 +81,12 @@ const LegacyLoginFlowWarning = () => {
   const history = useHistory();
   const match =
     useRouteMatch<{ tenant: string; path: string }>('/:tenant/:path');
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(
+    NUMBER_OF_SECONDS_TO_ALLOW_DISABLING
+  );
+  const timerRef = useRef<NodeJS.Timeout>();
 
   const { mutate: disableLegacyLoginFlow } = useMutation(
     () =>
@@ -90,7 +128,37 @@ const LegacyLoginFlowWarning = () => {
 
   const handleSubmit = () => {
     disableLegacyLoginFlow();
+    closeModal();
   };
+
+  const closeModal = () => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    setRemainingTime(NUMBER_OF_SECONDS_TO_ALLOW_DISABLING);
+    setIsModalVisible(false);
+  };
+
+  const openModal = () => {
+    const decrementRemainingTime = () => {
+      timerRef.current = setTimeout(() => {
+        setRemainingTime(prevRemainingTime => {
+          const decrementedRemainingTime = prevRemainingTime - 1;
+          if (decrementedRemainingTime > 0) {
+            decrementRemainingTime();
+          }
+          return decrementedRemainingTime;
+        });
+      }, 1000);
+    };
+
+    decrementRemainingTime();
+    setIsModalVisible(true);
+  };
+
+  if (!writeOk || !isAuthConfigurationFetched || !areGroupsFetched) {
+    return <></>;
+  }
 
   return (
     <StyledAlert
@@ -108,11 +176,18 @@ const LegacyLoginFlowWarning = () => {
             </a>{' '}
             as soon as possible.
           </p>
-          <Tooltip
-            title={
-              !canLegacyLoginFlowBeDisabled && (
-                <div>
-                  Before you can disable legacy login flow, you should:
+          <StyledDisableButtonSection>
+            <Button
+              disabled={!writeOk || !canLegacyLoginFlowBeDisabled}
+              onClick={openModal}
+              type="danger"
+            >
+              Deprecate Legacy Login
+            </Button>
+            <Tooltip
+              content={
+                <p>
+                  Before you can deprecate legacy login, you must:
                   <br />
                   <StyledIcon $success={isOIDCConfigured} />
                   Configure OIDC for your project
@@ -122,20 +197,34 @@ const LegacyLoginFlowWarning = () => {
                   <br />
                   <StyledIcon $success={hasAnyValidGroup} />
                   Have a group in your project that has a source ID and the{' '}
-                  <b>groups:create</b> capability.
-                </div>
-              )
-            }
-            arrowPointAtCenter
-          >
-            <StyledButton
-              disabled={!writeOk || !canLegacyLoginFlowBeDisabled}
-              onClick={handleSubmit}
-              type="danger"
+                  <b>groups:create</b> capability
+                </p>
+              }
             >
-              Disable Legacy Login Flow
-            </StyledButton>
-          </Tooltip>
+              <StyledHelpIcon size={20} type="HelpFilled" />
+            </Tooltip>
+          </StyledDisableButtonSection>
+          <Modal footer={null} onCancel={closeModal} visible={isModalVisible}>
+            <StyledModalContent>
+              <StyledModalWarningIcon size={20} type="WarningStroke" />
+              Are you sure you want to deprecate legacy login?
+            </StyledModalContent>
+            <StyledModalButtons>
+              <StyledModalButton onClick={closeModal} type="tertiary">
+                Cancel
+              </StyledModalButton>
+              <StyledModalButton
+                disabled={
+                  !writeOk || !canLegacyLoginFlowBeDisabled || remainingTime > 0
+                }
+                onClick={handleSubmit}
+                type="danger"
+              >
+                Deprecate
+                {remainingTime > 0 ? ` (${remainingTime})` : ''}
+              </StyledModalButton>
+            </StyledModalButtons>
+          </Modal>
         </>
       }
       type="error"
