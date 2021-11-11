36ee7ead7d47f7478ccdd235b355d7b536219d4d
Anna Gadacz <anna.gadacz@cognite.com>
2021-11-11 12:37:59 +0100

    refactor: table header basic skeleton

--- src/components/TableContent/components/Header.tsx
+++ src/components/TableContent/components/Header.tsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button, Colors, Flex, Icon, Tabs, Title } from '@cognite/cogs.js';
 
 export const Header = (): JSX.Element => {
-  return <div>:)</div>;
+  const onMenuButtonClick = () => {};
+  const tableHeaderTitle = 'Database';
+
+  return (
+    <Bar justifyContent="space-between" alignItems="center">
+      <Flex justifyContent="center" alignItems="center">
+        <Button
+          icon="HamburgerMenu"
+          variant="ghost"
+          onClick={onMenuButtonClick}
+          style={{ marginRight: '8px' }}
+        />
+        <Title level={3}>{tableHeaderTitle}</Title>
+      </Flex>
+      <Flex justifyContent="center" alignItems="center">
+        <Tabs>
+          <Tabs.TabPane
+            key="1"
+            tab={
+              <span>
+                <Icon type="DataTable" />
+                Data
+              </span>
+            }
+          />
+          <Tabs.TabPane
+            key="2"
+            tab={
+              <span>
+                <Icon type="Profiling" />
+                Profile
+              </span>
+            }
+          />
+        </Tabs>
+      </Flex>
+    </Bar>
+  );
 };
+
+const Bar = styled(Flex)`
+  border-bottom: 1px solid ${Colors['greyscale-grey4'].hex()};
+`;
