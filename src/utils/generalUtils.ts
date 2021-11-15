6c4a0b28b253963ed98cbcdf3b6da98ceb8041cd
Mihil Ranathunga <mihilranathunga@gmail.com>
2021-11-15 16:42:04 +0530

    use split chunks

--- src/utils/generalUtils.ts
+++ src/utils/generalUtils.ts
@@ -1,6 +1,6 @@
 export function splitListIntoChunks<T>(items: T[], batchSize: number): T[][] {
   if (batchSize <= 0) return [items];
-  if (!items.length) return [[]];
+  if (!items.length) return [];
   return items.reduce((acc, _, i) => {
     if (i % batchSize === 0) {
       acc.push(items.slice(i, i + batchSize));
