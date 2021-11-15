a585470a4aa430c66b6ee10a824265d43fd62290
Håkon Flatval <hakon.flatval@cognite.com>
2021-11-15 09:38:33 +0100

    Tidy up export statement

--- viewer/index.ts
+++ viewer/index.ts
@@ -10,5 +10,9 @@
 
 export * from './core/src/index';
 
-export { NodeAppearance, DefaultNodeAppearance, AreaCollection,
-         ClusteredAreaCollection } from './packages/cad-styling';
+export {
+  NodeAppearance,
+  DefaultNodeAppearance,
+  AreaCollection,
+  ClusteredAreaCollection
+} from './packages/cad-styling';
