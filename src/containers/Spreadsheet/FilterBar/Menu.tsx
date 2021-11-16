f47804f0a2e0b611e2bcf198de5bf2f6714e2d23
Anna Gadacz <anna.gadacz@cognite.com>
2021-11-16 14:40:07 +0100

    refactor: restyle dropdown menu

--- src/containers/Spreadsheet/FilterBar/Menu.tsx
+++ src/containers/Spreadsheet/FilterBar/Menu.tsx
@@ -1,16 +1,19 @@
 import React, { useMemo } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { CSVLink } from 'react-csv';
+
+import { Colors, Icon, Menu as CogsMenu } from '@cognite/cogs.js';
 import { createLink } from '@cognite/cdf-utilities';
+
+import styled from 'styled-components';
 import notification from 'antd/lib/notification';
 import Popconfirm from 'antd/lib/popconfirm';
+
 import { useUserCapabilities } from 'hooks/useUserCapabilities';
 import { useDeleteTable } from 'hooks/sdk-queries';
 import { useActiveTable } from 'hooks/table-tabs';
 import { useTableData } from 'hooks/table-data';
 import { escapeCSVValue } from 'utils/utils';
-import DropdownMenu from 'components/DropdownMenu';
-import MenuButton from 'components/MenuButton';
 
 export const Menu = (): JSX.Element => {
   const history = useHistory();
@@ -23,6 +26,7 @@ export const Menu = (): JSX.Element => {
   const [[database, table] = [undefined, undefined]] = useActiveTable();
 
   const canBeDownloaded = isDone && !!rows?.length;
+  const canBeRenamed = false; // TODO
 
   const onDownloadData = useMemo(() => {
     return (
@@ -38,7 +42,29 @@ export const Menu = (): JSX.Element => {
   }, [rows]);
 
   return (
-    <DropdownMenu>
+    <StyledMenu>
+      <CogsMenu.Item aria-label="Button rename table" disabled={!canBeRenamed}>
+        <Item>
+          <Icon type="Edit" />
+          Rename table
+        </Item>
+      </CogsMenu.Item>
+      <CSVLink
+        filename={`cognite-${database}-${table}.csv`}
+        data={onDownloadData}
+      >
+        <CogsMenu.Item
+          aria-label="Button download table"
+          disabled={!canBeDownloaded}
+        >
+          <Item>
+            <Icon type="Download" />
+            Download table
+          </Item>
+        </CogsMenu.Item>
+      </CSVLink>
+      <CogsMenu.Divider />
+      <CogsMenu.Header>Danger zone</CogsMenu.Header>
       <Popconfirm
         title="Are you sure you want to delete this table? Once deleted, the table cannot be recovered."
         onConfirm={() =>
@@ -67,28 +93,31 @@ export const Menu = (): JSX.Element => {
         disabled={!hasWriteAccess}
         cancelButtonProps={{ type: 'default' }}
       >
-        <MenuButton
-          type="ghost-danger"
+        <CogsMenu.Item
           aria-label="Button delete table"
-          icon="Trash"
           disabled={!hasWriteAccess}
         >
-          Delete Table
-        </MenuButton>
+          <Item>
+            <Icon type="Trash" />
+            <span style={{ color: Colors['text-danger'].hex() }}>
+              Delete table
+            </span>
+          </Item>
+        </CogsMenu.Item>
       </Popconfirm>
-      <CSVLink
-        filename={`cognite-${database}-${table}.csv`}
-        data={onDownloadData}
-      >
-        <MenuButton
-          type="ghost"
-          aria-label="Button download table"
-          icon="Download"
-          disabled={!canBeDownloaded}
-        >
-          Download CSV
-        </MenuButton>
-      </CSVLink>
-    </DropdownMenu>
+    </StyledMenu>
   );
 };
+
+const StyledMenu = styled(CogsMenu)`
+  width: 250px;
+  a {
+    color: inherit;
+  }
+`;
+const Item = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-weight: 500;
+`;
