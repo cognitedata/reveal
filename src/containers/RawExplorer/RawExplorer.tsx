2812a99f2ae74b279e1c5bb440b243fb9589d2f1
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2021-11-19 14:25:49 +0100

    feat: add first time user flow

--- src/containers/RawExplorer/RawExplorer.tsx
+++ src/containers/RawExplorer/RawExplorer.tsx
@@ -1,18 +1,16 @@
 import React, { useContext } from 'react';
-import { Body, Colors, Loader, Title } from '@cognite/cogs.js';
 
+import { Loader } from '@cognite/cogs.js';
 import styled from 'styled-components';
 
 import Breadcrumb from 'components/Breadcrumb/Breadcrumb';
 import { BreadcrumbItemProps } from 'components/Breadcrumb/BreadcrumbItem';
 import NoAccessPage from 'components/NoAccessPage/NoAccessPage';
-
 import SidePanel from 'components/SidePanel/SidePanel';
 import TableContent from 'containers/TableContent';
 import TableTabList from 'components/TableTabList';
 import { RawExplorerContext } from 'contexts';
 import { useActiveTable } from 'hooks/table-tabs';
-
 import { useUserCapabilities } from 'hooks/useUserCapabilities';
 import {
   BREADCRUMBS_HEIGHT,
@@ -20,7 +18,7 @@ import {
   SIDE_PANEL_TRANSITION_DURATION,
   SIDE_PANEL_TRANSITION_FUNCTION,
 } from 'utils/constants';
-import icons from 'assets/icons';
+import RawExplorerNotSelected from './RawExplorerNotSelected';
 
 const breadcrumbs: Pick<BreadcrumbItemProps, 'path' | 'title'>[] = [
   {
@@ -57,18 +55,7 @@ const RawExplorer = (): JSX.Element => {
                 <TableContent />
               </>
             ) : (
-              <StyledRawExplorerNotSelectedWrapper>
-                <StyledRawExplorerNotSelectedContent>
-                  <StyledRawExplorerNotSelectedArrow
-                    src={icons.EmptyStateArrowIcon}
-                  />
-                  <Title level={3}>Select a table to view raw data</Title>
-                  <StyledRawExplorerNotSelectedBody>
-                    Use the side menu to navigate between databases and open raw
-                    tables.
-                  </StyledRawExplorerNotSelectedBody>
-                </StyledRawExplorerNotSelectedContent>
-              </StyledRawExplorerNotSelectedWrapper>
+              <RawExplorerNotSelected />
             )}
           </StyledRawExplorerTableContentWrapper>
         </StyledRawExplorerContent>
@@ -96,30 +83,6 @@ const StyledRawExplorerTableContentWrapper = styled.div<{
       ${({ $isSidePanelOpen }) =>
         $isSidePanelOpen ? DATABASE_LIST_WIDTH : 0}px
   );
-`;
-
-const StyledRawExplorerNotSelectedWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-`;
-
-const StyledRawExplorerNotSelectedContent = styled.div`
-  margin-bottom: 30px;
-  position: relative;
-  width: 400px;
-`;
-
-const StyledRawExplorerNotSelectedBody = styled(Body)`
-  color: ${Colors['text-hint'].hex()};
-  margin-top: 12px;
-`;
-
-const StyledRawExplorerNotSelectedArrow = styled.img`
-  position: absolute;
-  left: -225px;
-  top: -150px;
 `;
 
 export default RawExplorer;
---
+++ src/containers/RawExplorer/RawExplorerNotSelected.tsx
@@ -0,0 +1,158 @@
+import React, { useMemo } from 'react';
+
+import { Body, Colors, Detail, Loader, Title } from '@cognite/cogs.js';
+import { RawDB } from '@cognite/sdk';
+import { Carousel } from 'antd';
+import styled from 'styled-components';
+
+import icons from 'assets/icons';
+import SidePanelGraphic from 'assets/SidePanelGraphic.svg';
+import TabsGraphic from 'assets/TabsGraphic.svg';
+import { useDatabases } from 'hooks/sdk-queries';
+
+const FIRST_TIME_CAROUSEL_HEIGHT = 330;
+
+const carouselSlides = [
+  {
+    title: 'Get started with RAW Explorer.',
+    detail: 'Create databases to store and organize raw data in tables.',
+    imageSrc: SidePanelGraphic,
+  },
+  {
+    title: 'View and compare tables as tabs.',
+    detail: 'Open raw data in table format to compare and analyze the content.',
+    imageSrc: TabsGraphic,
+  },
+  {
+    title: 'Get out of the box data profiling.',
+    detail:
+      'Understand structure, content and interrelationships, and identify potential for projects.',
+    imageSrc: TabsGraphic,
+  },
+];
+
+const RawExplorerNotSelected = (): JSX.Element => {
+  const { data, isLoading: isFetchingDatabases } = useDatabases();
+
+  const databases = useMemo(
+    () =>
+      data
+        ? data.pages.reduce(
+            (accl, page) => [...accl, ...page.items],
+            [] as RawDB[]
+          )
+        : ([] as RawDB[]),
+    [data]
+  );
+
+  if (isFetchingDatabases) {
+    return <Loader />;
+  }
+
+  return (
+    <StyledRawExplorerNotSelectedWrapper>
+      {databases.length ? (
+        <StyledRawExplorerReturningUserContent>
+          <StyledRawExplorerReturningUserArrow
+            src={icons.EmptyStateArrowIcon}
+          />
+          <Title level={3}>Select a table to view raw data</Title>
+          <StyledRawExplorerReturningUserBody>
+            Use the side menu to navigate between databases and open raw tables.
+          </StyledRawExplorerReturningUserBody>
+        </StyledRawExplorerReturningUserContent>
+      ) : (
+        <StyledRawExplorerFirstTimeContent>
+          <Carousel dots={{ className: 'first-time-carousel-dot' }}>
+            {carouselSlides.map(({ title, detail, imageSrc }) => (
+              <StyledCarouselPage key={title}>
+                <StyledCarouselPageContent>
+                  <StyledCarouselPageTitle level={4}>
+                    {title}
+                  </StyledCarouselPageTitle>
+                  <StyledCarouselPageDetail>{detail}</StyledCarouselPageDetail>
+                </StyledCarouselPageContent>
+                <StyledCarouselPageImage src={imageSrc} />
+              </StyledCarouselPage>
+            ))}
+          </Carousel>
+        </StyledRawExplorerFirstTimeContent>
+      )}
+    </StyledRawExplorerNotSelectedWrapper>
+  );
+};
+
+const StyledRawExplorerNotSelectedWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const StyledRawExplorerReturningUserContent = styled.div`
+  margin-bottom: 30px;
+  position: relative;
+  width: 400px;
+`;
+
+const StyledRawExplorerReturningUserBody = styled(Body)`
+  color: ${Colors['text-hint'].hex()};
+  margin-top: 12px;
+`;
+
+const StyledRawExplorerReturningUserArrow = styled.img`
+  position: absolute;
+  left: -225px;
+  top: -150px;
+`;
+
+const StyledRawExplorerFirstTimeContent = styled.div`
+  box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.04),
+    0px 3px 8px rgba(0, 0, 0, 0.06);
+  border-radius: 16px;
+  height: ${FIRST_TIME_CAROUSEL_HEIGHT}px;
+  position: relative;
+  width: 642px;
+
+  .first-time-carousel-dot {
+    li button {
+      background-color: ${Colors['bg-control--disabled-hover'].hex()};
+      border-radius: 8px;
+      height: 8px;
+    }
+
+    li.slick-active button {
+      background-color: ${Colors['text-hint'].hex()};
+    }
+  }
+`;
+
+const StyledCarouselPage = styled.div`
+  display: flex !important; /* overrides antd style */
+  height: ${FIRST_TIME_CAROUSEL_HEIGHT}px;
+  padding: 0px 48px 48px;
+`;
+
+const StyledCarouselPageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 100px 16px 0 0;
+  height: 100%;
+  width: 200px;
+`;
+
+const StyledCarouselPageTitle = styled(Title)`
+  color: ${Colors['text-primary'].hex()};
+  margin-bottom: 16px;
+`;
+
+const StyledCarouselPageDetail = styled(Detail)`
+  color: ${Colors['text-hint'].hex()};
+`;
+
+const StyledCarouselPageImage = styled.img`
+  width: auto;
+  margin-left: auto;
+`;
+
+export default RawExplorerNotSelected;
