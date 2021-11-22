56e4a20d6636fbe5b66e5749005fe9864dcf6975
Vegard Økland <vegard.okland@cognite.com>
2021-11-22 15:48:29 +0100

    Create active table context
    This context will allow downstream components to assume that database/table is set and valid.
    useActiveTable  in 'hooks/table-tabs' can _not_ make this assumption, it will only return whatever is
    in the query param, if any.

---
+++ src/contexts/ActiveTableContext.tsx
@@ -0,0 +1,106 @@
+import React, { createContext, useContext } from 'react';
+import { Colors, Title, Body, Loader } from '@cognite/cogs.js';
+import { useTableRows } from 'hooks/sdk-queries';
+import { SpecificTable, useActiveTable } from 'hooks/table-tabs';
+import icons from 'assets/icons';
+import styled from 'styled-components';
+import {
+  SIDE_PANEL_TRANSITION_DURATION,
+  SIDE_PANEL_TRANSITION_FUNCTION,
+} from 'utils/constants';
+
+type ActiveTableState = {
+  database: string;
+  table: string;
+  view?: string;
+  update: (_: SpecificTable) => void;
+};
+
+export const ActiveTableContext = createContext<ActiveTableState>(
+  {} as ActiveTableState
+);
+
+type ActiveTableProviderProps = {
+  children: React.ReactNode;
+};
+
+export const useActiveTableContext = () => useContext(ActiveTableContext);
+
+export const ActiveTableProvider = ({ children }: ActiveTableProviderProps) => {
+  const [[database, table, view] = [], update] = useActiveTable();
+  const { isFetched, isError } = useTableRows(
+    { database: database!, table: table!, pageSize: 1 },
+    { enabled: !!database && !!table }
+  );
+
+  if (!database || !table || isError) {
+    return (
+      <StyledRawExplorerNotSelectedWrapper>
+        <StyledRawExplorerNotSelectedContent>
+          <StyledRawExplorerNotSelectedArrow src={icons.EmptyStateArrowIcon} />
+          <Title level={3}>Select a table to view raw data</Title>
+          {isError && (
+            <StyledRawExplorerNotSelectedBody>
+              The specified table was not found!
+            </StyledRawExplorerNotSelectedBody>
+          )}
+          <StyledRawExplorerNotSelectedBody>
+            Use the side menu to navigate between databases and open raw tables.
+          </StyledRawExplorerNotSelectedBody>
+        </StyledRawExplorerNotSelectedContent>
+      </StyledRawExplorerNotSelectedWrapper>
+    );
+  }
+
+  if (!isFetched) {
+    return <Loader />;
+  }
+
+  return (
+    <ActiveTableContext.Provider
+      value={{
+        database,
+        table,
+        update,
+        view,
+      }}
+    >
+      {children}
+    </ActiveTableContext.Provider>
+  );
+};
+
+const StyledRawExplorerNotSelectedWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding: 0 128px;
+  position: relative;
+`;
+
+const StyledRawExplorerNotSelectedContent = styled.div`
+  margin-bottom: 30px;
+  width: 400px;
+`;
+
+const StyledRawExplorerNotSelectedBody = styled(Body)`
+  color: ${Colors['text-hint'].hex()};
+  margin-top: 12px;
+`;
+
+const StyledRawExplorerNotSelectedArrow = styled.img`
+  position: absolute;
+  left: 125px;
+  opacity: 1;
+  transform: translateY(calc(-100% - 75px));
+  transition: opacity ${SIDE_PANEL_TRANSITION_DURATION}s
+    ${SIDE_PANEL_TRANSITION_FUNCTION};
+  top: calc(50%);
+  max-height: 180px;
+  width: calc(50% - 400px);
+
+  @media screen and (max-width: 1400px) {
+    opacity: 0;
+  }
+`;
