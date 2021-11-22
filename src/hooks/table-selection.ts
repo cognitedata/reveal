340ce4f97bad8f017cd70b58b05b14bbf93481d7
Anna Gadacz <anna.gadacz@cognite.com>
2021-11-22 12:14:58 +0100

    refactor: remove redundant code

--- src/hooks/table-selection.ts
+++ src/hooks/table-selection.ts
@@ -1,6 +1,4 @@
 import { useContext } from 'react';
-import isNil from 'lodash/isNil';
-
 import { RawExplorerContext } from 'contexts';
 
 const NO_CELL_SELECTED = { rowIndex: undefined, columnIndex: undefined };
@@ -16,8 +14,7 @@ export const useTableSelection = () => {
   };
 
   const deselectCell = () => {
-    if (!isNil(selectedCell.rowIndex) && !isNil(selectedCell.columnIndex))
-      setSelectedCell(NO_CELL_SELECTED);
+    setSelectedCell(NO_CELL_SELECTED);
   };
 
   return { selectedCell, setSelectedCell, isCellSelected, deselectCell };
