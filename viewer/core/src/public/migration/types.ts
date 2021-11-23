684e1e4c827a5f4429a1548f7f1d49a0ea6192f8
Savokr <savelii.novikov@cognite.com>
2021-11-23 13:19:29 +0100

    Lint fix

--- viewer/core/src/public/migration/types.ts
+++ viewer/core/src/public/migration/types.ts
@@ -196,9 +196,9 @@ export type CameraControlsOptions = {
    *
    * Modes:
    *
-   * 'zoomToTarget' - zooms just to the current target (center of the screen) of the camera,.
+   * 'zoomToTarget' - zooms just to the current target (center of the screen) of the camera.
    *
-   * 'zoomPastCursor' - zooms in the direction of the ray coming from camera through cursor screen position, allows going through objects,.
+   * 'zoomPastCursor' - zooms in the direction of the ray coming from camera through cursor screen position, allows going through objects.
    *
    * 'zoomToCursor' - mouse wheel scroll zooms towards the point on the model where cursor is hovering over, doesn't allow going through objects.
    *
