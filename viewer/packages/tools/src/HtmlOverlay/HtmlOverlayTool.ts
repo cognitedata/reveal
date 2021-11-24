0c0349ca8ffc713aaf64542ba7f65435f4cb3674
Lars Moastuen <lars.moastuen@cognite.com>
2021-11-24 08:29:21 +0100

    Add explicit return types + lint

--- viewer/packages/tools/src/HtmlOverlay/HtmlOverlayTool.ts
+++ viewer/packages/tools/src/HtmlOverlay/HtmlOverlayTool.ts
@@ -92,8 +92,8 @@ type HtmlOverlayElement = {
 };
 
 /**
- * Manages HTMLoverlays for {@see Cognite3DViewer}. Attaches HTML elements to a 
- * 3D position and updates its position/visibility as user moves the camera. This is 
+ * Manages HTMLoverlays for {@see Cognite3DViewer}. Attaches HTML elements to a
+ * 3D position and updates its position/visibility as user moves the camera. This is
  * useful to create HTML overlays to highlight information about key positions in the 3D model.
  *
  * Attached elements *must* have CSS style 'position: absolute'. It's also recommended
