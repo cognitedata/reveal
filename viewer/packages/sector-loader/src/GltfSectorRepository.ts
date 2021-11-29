2c5825cc527a325b289bc069e9e61aaeb9fadf67
Christopher Tannum <christopher.tannum@cognite.com>
2021-11-29 16:16:25 +0100

    fix: remove unused code

--- viewer/packages/sector-loader/src/GltfSectorRepository.ts
+++ viewer/packages/sector-loader/src/GltfSectorRepository.ts
@@ -109,38 +109,5 @@ export class GltfSectorRepository implements SectorRepository {
     };
   }
 
-  // private getShaderMaterial(type: RevealGeometryCollectionType, materials: Materials) {
-  //   switch (type) {
-  //     case RevealGeometryCollectionType.BoxCollection:
-  //       return materials.box;
-  //     case RevealGeometryCollectionType.CircleCollection:
-  //       return materials.circle;
-  //     case RevealGeometryCollectionType.ConeCollection:
-  //       return materials.cone;
-  //     case RevealGeometryCollectionType.EccentricConeCollection:
-  //       return materials.eccentricCone;
-  //     case RevealGeometryCollectionType.EllipsoidSegmentCollection:
-  //       return materials.ellipsoidSegment;
-  //     case RevealGeometryCollectionType.GeneralCylinderCollection:
-  //       return materials.generalCylinder;
-  //     case RevealGeometryCollectionType.GeneralRingCollection:
-  //       return materials.generalRing;
-  //     case RevealGeometryCollectionType.QuadCollection:
-  //       return materials.quad;
-  //     case RevealGeometryCollectionType.TorusSegmentCollection:
-  //       return materials.torusSegment;
-  //     case RevealGeometryCollectionType.TrapeziumCollection:
-  //       return materials.trapezium;
-  //     case RevealGeometryCollectionType.NutCollection:
-  //       return materials.nut;
-  //     case RevealGeometryCollectionType.TriangleMesh:
-  //       return materials.triangleMesh;
-  //     case RevealGeometryCollectionType.InstanceMesh:
-  //       return materials.instancedMesh;
-  //     default:
-  //       assertNever(type);
-  //   }
-  // }
-
   clear(): void {}
 }
