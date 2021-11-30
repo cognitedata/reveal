5afa1df3be3c4814d0d7b6a25ad342b00c34800e
Rhuan Barreto <rhuan.barreto@cognite.com>
2021-11-30 14:56:11 +0100

    Add Chromatic (#826)
    * ci: add visual testing workflow
    
    * ci: cleanup old storybook config
    
    * ci: add new main config
    
    * ci: change storybook preview file
    
    * ci: update packages
    
    * ci: fix storybook package upgrade issue
    Bug reported on https://github.com/storybookjs/storybook/issues/16837

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
 import '@cognite/cogs.js/dist/cogs.css';
 import 'antd/dist/antd.css';
 import 'react-datepicker/dist/react-datepicker.css';
+
+export const parameters = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
