c5bfd55de050f3e16c56f25a5a8e8d9f4d05bad4
Pramod S <pramod.s@cognite.com>
2021-12-01 15:17:50 +0100

    Fixed Lint error & updated yarn.lock

--- viewer/packages/tools/src/Toolbar/DefaultToolbar.ts
+++ viewer/packages/tools/src/Toolbar/DefaultToolbar.ts
@@ -29,7 +29,7 @@ export class DefaultToolbar {
     this.cameraControlOption = {
       onClickTargetChange: false,
       mouseWheelAction: 'zoomPastCursor'
-    }
+    };
     this.createDefaultToolbar();
   }
 
