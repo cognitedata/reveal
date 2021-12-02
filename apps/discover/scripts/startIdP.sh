82ad89844ef127814aebd4d95bc3b2b864ca7172
Samir Ajdarpasic <78414491+samir-ajdarpasic@users.noreply.github.com>
2021-12-02 16:28:09 +0100

    chore(discover): create only one fakeIdp container (#1391)
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/discover/scripts/startIdP.sh
+++ apps/discover/scripts/startIdP.sh
@@ -4,6 +4,13 @@
 # and put it into this file in the root:
 PRIVATE_KEY=$(cat "./private-keys/$REACT_APP_PROJECT.jwk-key.json")
 
+# Check if we have and existing container
+COUNT=$(docker ps -a | grep "fakeIdp" | wc -l)
+if (($COUNT > 0)); then
+    echo "Removing existing container..."
+    docker rm -f fakeIdp
+fi
+
 # Run the final command
 docker pull eu.gcr.io/cognitedata/fake-idp:latest
-docker run -e IDP_CLUSTER=$REACT_APP_CLUSTER -p 8200:8200 -e PORT=8200 -e PRIVATE_KEY="$PRIVATE_KEY" eu.gcr.io/cognitedata/fake-idp:latest
+docker run --name fakeIdp -e IDP_CLUSTER=$REACT_APP_CLUSTER -p 8200:8200 -e PORT=8200 -e PRIVATE_KEY="$PRIVATE_KEY" eu.gcr.io/cognitedata/fake-idp:latest
