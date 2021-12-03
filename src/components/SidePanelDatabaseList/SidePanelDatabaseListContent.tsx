bf30ef0ab8ba9023cf583f4f4b1b93a1314ae5de
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2021-12-03 10:17:13 +0100

    chore: update texts for side panel list empty search states

--- src/components/SidePanelDatabaseList/SidePanelDatabaseListContent.tsx
+++ src/components/SidePanelDatabaseList/SidePanelDatabaseListContent.tsx
@@ -38,9 +38,10 @@ const SidePanelDatabaseListContent = ({
         ))
       ) : (
         <StyledNoItemsWrapper>
-          <Title level={6}>There are no items here.</Title>
+          <Title level={6}>No results found.</Title>
           <StyledNoItemsDetail strong>
-            Search for documents by source, format, type, creation date and more
+            The search “{searchQuery}” did not match any databases. Please try
+            another search.
           </StyledNoItemsDetail>
         </StyledNoItemsWrapper>
       )}
