9eb8400c521e4de13de2e8a5ed9385cfe1c3412f
Anna Gadacz <anna.gadacz@cognite.com>
2021-12-06 11:58:47 +0100

    refactor: add custom array and object icons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -1,18 +1,22 @@
+import { default as ArrayIcon } from './ArrayIcon.svg';
 import { default as DateIcon } from './DateIcon.svg';
 import { default as DocumentIcon } from './DocumentIcon.svg';
 import { default as DocumentIconDisabled } from './DocumentIconDisabled.svg';
 import { default as DocumentIconHover } from './DocumentIconHover.svg';
 import { default as EmptyStateArrowIcon } from './EmptyStateArrowIcon.svg';
 import { default as KeyIcon } from './KeyIcon.svg';
+import { default as ObjectIcon } from './ObjectIcon.svg';
 import { default as UnknownPrimaryKeyIcon } from './UnknownPrimaryKeyIcon.svg';
 
 const icons = {
+  ArrayIcon,
   DateIcon,
   DocumentIcon,
   DocumentIconDisabled,
   DocumentIconHover,
   EmptyStateArrowIcon,
   KeyIcon,
+  ObjectIcon,
   UnknownPrimaryKeyIcon,
 };
 
