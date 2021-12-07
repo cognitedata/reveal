75a33767caa1c642ea9f6d4e3b98621c97caa8df
Håkon Flatval <hakon.flatval@cognite.com>
2021-12-07 14:32:21 +0100

    fixup! Add initial V9 geometry filterer

--- viewer/packages/cad-parsers/src/cad/V9GeometryFilterer.test.ts
+++ viewer/packages/cad-parsers/src/cad/V9GeometryFilterer.test.ts
@@ -13,21 +13,19 @@ import {
   getCollectionType
 } from '../../../../test-utilities/src/primitives/primitiveTypes';
 
-import { createPrimitiveInterleavedGeometry, parseInterleavedGeometry } from '../../../../test-utilities/src/primitives/primitiveThreeBuffers';
-
+import {
+  createPrimitiveInterleavedGeometry,
+  parseInterleavedGeometry
+} from '../../../../test-utilities/src/primitives/primitiveThreeBuffers';
 
 function assertSecondCulledAwayFromBoxAtX10Yneg10Z0(primitives: any[], primitiveType: PrimitiveType) {
-
   const bufferGeometry = createPrimitiveInterleavedGeometry(primitiveType, primitives);
-
 
   const clipBox = new THREE.Box3(new THREE.Vector3(8, -12, -2), new THREE.Vector3(12, -8, 2));
 
   const collectionType = getCollectionType(primitiveType);
 
-  const filteredGeometry = V9GeometryFilterer.filterGeometry(bufferGeometry,
-                                                             collectionType,
-                                                             clipBox);
+  const filteredGeometry = V9GeometryFilterer.filterGeometry(bufferGeometry, collectionType, clipBox);
 
   const newPrimitives = parseInterleavedGeometry(primitiveType, filteredGeometry);
 
@@ -35,10 +33,8 @@ function assertSecondCulledAwayFromBoxAtX10Yneg10Z0(primitives: any[], primitive
   expect(newPrimitives[0]).toContainEntries(Object.entries(primitives[0]));
 }
 
-
 describe('V9GeometryFilterer accepts empty clipBox', () => {
   test('no clipbox, returns original', () => {
-
     const ellipsoid: Ellipsoid = {
       horizontalRadius: 10,
       verticalRadius: 10,
@@ -48,9 +44,11 @@ describe('V9GeometryFilterer accepts empty clipBox', () => {
 
     const bufferGeometry = createPrimitiveInterleavedGeometry(PrimitiveType.Ellipsoid, [ellipsoid]);
 
-    const newGeometry = V9GeometryFilterer.filterGeometry(bufferGeometry,
-                                                          RevealGeometryCollectionType.EllipsoidSegmentCollection,
-                                                          undefined);
+    const newGeometry = V9GeometryFilterer.filterGeometry(
+      bufferGeometry,
+      RevealGeometryCollectionType.EllipsoidSegmentCollection,
+      undefined
+    );
 
     expect(newGeometry).toBe(bufferGeometry);
   });
@@ -58,31 +56,39 @@ describe('V9GeometryFilterer accepts empty clipBox', () => {
 
 describe('V9GeometryFilterer filters primitives correctly', () => {
   test('Two ellipsoid segments: one accepted, one rejected - returns filtered', () => {
-    const ellipsoids: Ellipsoid[] = [{
-      horizontalRadius: 1,
-      verticalRadius: 1,
-      height: 1,
-      center: [10, -10, 0],
-    }, {
-      horizontalRadius: 1,
-      verticalRadius: 1,
-      height: 1,
-      center: [10, 10, 10]
-    }];
+    const ellipsoids: Ellipsoid[] = [
+      {
+        horizontalRadius: 1,
+        verticalRadius: 1,
+        height: 1,
+        center: [10, -10, 0]
+      },
+      {
+        horizontalRadius: 1,
+        verticalRadius: 1,
+        height: 1,
+        center: [10, 10, 10]
+      }
+    ];
 
     assertSecondCulledAwayFromBoxAtX10Yneg10Z0(ellipsoids, PrimitiveType.Ellipsoid);
   });
 
   test('Two boxes: one accepted, one rejected - returns filtered', () => {
-    const mat0 = new THREE.Matrix4().makeTranslation(10, -10, 0).multiply(
-      new THREE.Matrix4().makeRotationFromEuler(new THREE.Euler(0, 1, 2, 'XYZ')));
-    const mat1 = new THREE.Matrix4().makeTranslation(-10, 5, 10).multiply(
-      new THREE.Matrix4().makeRotationFromEuler(new THREE.Euler(0, 1, 2, 'XYZ')));
-    const boxes: Box[] = [{
-      instanceMatrix: mat0.toArray()
-    }, {
-      instanceMatrix: mat1.toArray()
-    }];
+    const mat0 = new THREE.Matrix4()
+      .makeTranslation(10, -10, 0)
+      .multiply(new THREE.Matrix4().makeRotationFromEuler(new THREE.Euler(0, 1, 2, 'XYZ')));
+    const mat1 = new THREE.Matrix4()
+      .makeTranslation(-10, 5, 10)
+      .multiply(new THREE.Matrix4().makeRotationFromEuler(new THREE.Euler(0, 1, 2, 'XYZ')));
+    const boxes: Box[] = [
+      {
+        instanceMatrix: mat0.toArray()
+      },
+      {
+        instanceMatrix: mat1.toArray()
+      }
+    ];
 
     assertSecondCulledAwayFromBoxAtX10Yneg10Z0(boxes, PrimitiveType.Box);
   });
