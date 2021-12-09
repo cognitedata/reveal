e9eb541ec1af7740bbeeedc7ef7b1eb1f422a510
Vegard Økland <vegard.okland@cognite.com>
2021-12-09 16:03:12 +0100

    Add test for hook
    The original bug that caused an incidents was that the second step after creating a group, updating
    service accounts was omitted (testing was done on a OIDC-project, where service accounts isn't a
    thing).

---
+++ src/hooks.test.ts
@@ -0,0 +1,237 @@
+import { Group } from '@cognite/sdk';
+import { forUnitTests } from './hooks';
+
+describe('hooks', () => {
+  describe('group update', () => {
+    const { getUpdater } = forUnitTests;
+    test('service account update should not be called if a group does not have any service accounts', async () => {
+      const sdk = {
+        projects: {
+          retrieve: jest.fn().mockResolvedValue({
+            name: 'test-project',
+            urlName: 'test-project',
+            defaultGroupId: 1,
+          }),
+          updateProject: jest.fn().mockResolvedValue(undefined),
+        },
+        groups: {
+          addServiceAccounts: jest.fn().mockResolvedValue([]),
+          listServiceAccounts: jest.fn().mockResolvedValue([]),
+          create: jest.fn().mockResolvedValue([
+            {
+              id: 2,
+              isDeleted: false,
+              deletedTime: -1,
+              name: 'new-group',
+            },
+          ]),
+          delete: jest.fn().mockResolvedValue(),
+        },
+      };
+      const update = getUpdater(sdk!, test);
+
+      const oldGroup: Group = {
+        id: 1,
+        name: 'test-group',
+        capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+      };
+      await update(oldGroup);
+      expect(sdk.groups.listServiceAccounts).toHaveBeenCalled();
+      expect(sdk.groups.create).toHaveBeenCalledWith([
+        {
+          name: 'test-group',
+          capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+        },
+      ]);
+      expect(sdk.groups.addServiceAccounts).not.toHaveBeenCalledWith(2, [42]);
+    });
+
+    test('service account should be update when editing a group with service accounts', async () => {
+      const sdk = {
+        projects: {
+          retrieve: jest.fn().mockResolvedValue({
+            name: 'test-project',
+            urlName: 'test-project',
+            defaultGroupId: 1,
+          }),
+          updateProject: jest.fn().mockResolvedValue(undefined),
+        },
+        groups: {
+          addServiceAccounts: jest.fn().mockResolvedValue([]),
+          listServiceAccounts: jest.fn().mockResolvedValue([
+            {
+              name: 'vegard.okland@cognite.com',
+              groups: [1],
+              id: 42,
+              isDeleted: false,
+              deletedTime: -1,
+            },
+          ]),
+          create: jest.fn().mockResolvedValue([
+            {
+              id: 2,
+              isDeleted: false,
+              deletedTime: -1,
+              name: 'new-group',
+            },
+          ]),
+          delete: jest.fn().mockResolvedValue(),
+        },
+      };
+      const update = getUpdater(sdk!, test);
+
+      const oldGroup: Group = {
+        id: 1,
+        name: 'test-group',
+        capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+      };
+      await update(oldGroup);
+      expect(sdk.groups.listServiceAccounts).toHaveBeenCalled();
+      expect(sdk.groups.create).toHaveBeenCalledWith([
+        {
+          name: 'test-group',
+          capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+        },
+      ]);
+      expect(sdk.groups.addServiceAccounts).toHaveBeenCalledWith(2, [42]);
+    });
+
+    test('service account should be update when editing a group with service accounts', async () => {
+      const sdk = {
+        projects: {
+          retrieve: jest.fn().mockResolvedValue({
+            name: 'test-project',
+            urlName: 'test-project',
+            defaultGroupId: 1,
+          }),
+          updateProject: jest.fn().mockResolvedValue(undefined),
+        },
+        groups: {
+          addServiceAccounts: jest.fn().mockResolvedValue([]),
+          listServiceAccounts: jest.fn().mockResolvedValue([
+            {
+              name: 'vegard.okland@cognite.com',
+              groups: [1],
+              id: 42,
+              isDeleted: false,
+              deletedTime: -1,
+            },
+          ]),
+          create: jest.fn().mockResolvedValue([
+            {
+              id: 2,
+              isDeleted: false,
+              deletedTime: -1,
+              name: 'new-group',
+            },
+          ]),
+          delete: jest.fn().mockResolvedValue(),
+        },
+      };
+      const update = getUpdater(sdk!, test);
+
+      const oldGroup: Group = {
+        id: 1,
+        name: 'test-group',
+        capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+      };
+      await update(oldGroup);
+      expect(sdk.groups.listServiceAccounts).toHaveBeenCalled();
+      expect(sdk.groups.create).toHaveBeenCalledWith([
+        {
+          name: 'test-group',
+          capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+        },
+      ]);
+      expect(sdk.groups.addServiceAccounts).toHaveBeenCalledWith(2, [42]);
+    });
+
+    test('default groups should not be updated when updating a group that is not the default grouop', async () => {
+      const sdk = {
+        projects: {
+          retrieve: jest.fn().mockResolvedValue({
+            name: 'test-project',
+            urlName: 'test-project',
+            defaultGroupId: 42,
+          }),
+          updateProject: jest.fn().mockResolvedValue(undefined),
+        },
+        groups: {
+          addServiceAccounts: jest.fn().mockResolvedValue([]),
+          listServiceAccounts: jest.fn().mockResolvedValue([]),
+          create: jest.fn().mockResolvedValue([
+            {
+              id: 2,
+              isDeleted: false,
+              deletedTime: -1,
+              name: 'new-group',
+            },
+          ]),
+          delete: jest.fn().mockResolvedValue(),
+        },
+      };
+      const update = getUpdater(sdk!, test);
+
+      const oldGroup: Group = {
+        id: 1,
+        name: 'test-group',
+        capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+      };
+      await update(oldGroup);
+      expect(sdk.groups.listServiceAccounts).toHaveBeenCalled();
+      expect(sdk.groups.create).toHaveBeenCalledWith([
+        {
+          name: 'test-group',
+          capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+        },
+      ]);
+      expect(sdk.projects.updateProject).not.toHaveBeenCalled();
+    });
+
+    test('default groups should be updated when updating a group that is not the default grouop', async () => {
+      const sdk = {
+        projects: {
+          retrieve: jest.fn().mockResolvedValue({
+            name: 'test-project',
+            urlName: 'test-project',
+            defaultGroupId: 1,
+          }),
+          updateProject: jest.fn().mockResolvedValue(undefined),
+        },
+        groups: {
+          addServiceAccounts: jest.fn().mockResolvedValue([]),
+          listServiceAccounts: jest.fn().mockResolvedValue([]),
+          create: jest.fn().mockResolvedValue([
+            {
+              id: 2,
+              isDeleted: false,
+              deletedTime: -1,
+              name: 'new-group',
+            },
+          ]),
+          delete: jest.fn().mockResolvedValue(),
+        },
+      };
+      const update = getUpdater(sdk!, test);
+
+      const oldGroup: Group = {
+        id: 1, // same as default above
+        name: 'test-group',
+        capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+      };
+      await update(oldGroup);
+      expect(sdk.groups.listServiceAccounts).toHaveBeenCalled();
+      expect(sdk.groups.create).toHaveBeenCalledWith([
+        {
+          name: 'test-group',
+          capabilities: [{ assetsAcl: { scope: { all: {} } } }],
+        },
+      ]);
+      expect(sdk.projects.updateProject).toHaveBeenCalledWith('test-project', {
+        update: {
+          defaultGroupId: { set: 2 },
+        },
+      });
+    });
+  });
+});
