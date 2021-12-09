83fa0b85a1db251caa7e43ace61a63e93b8b396e
Soumesh Banerjee <soumesh.banerjee@cognite.com>
2021-12-09 19:11:22 +0400

    Remove docs DX-198 (#175)
    * Remove docs DX-198
    
    * Add yarn.lock
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/platypus-docs/doczrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  typescript: true,
-  port: '4838',
-  base: '/platypus',
-  src: './apps/platypus-docs/src/',
-  dest: 'docs',
-  title: 'Official Platypus documentation',
-  menu: ['Getting Started', 'CLI', 'UI'],
-};
