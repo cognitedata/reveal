f2b4a9a691eb57e2cd62575b7675e2dd78341712
Lars Moastuen <lars.moastuen@cognite.com>
2021-12-10 17:59:54 +0100

    Merge branch 'master' into release/3.0-alpha

--- documentation/versioned_docs/version-1.x/components/customScope.ts
+++ documentation/versioned_docs/version-1.x/components/customScope.ts
@@ -2,6 +2,7 @@ import { Water } from 'three/examples/jsm/objects/Water';
 import { resetViewerEventHandlers } from '../utils/viewerUtilities';
 import { DragControls } from 'three/examples/jsm/controls/DragControls';
 import { ExplodedViewTool, HtmlOverlayTool } from '@cognite/reveal-1.x/tools';
+import * as THREE from 'three';
 
 const reveal = typeof window === 'undefined' ? {} : require('@cognite/reveal-1.x');
 
@@ -9,7 +10,7 @@ export const customScope: Record<string, any> = {
   resetViewerEventHandlers,
   HtmlOverlayTool,
   ExplodedViewTool,
-  THREE: reveal.THREE,
+  THREE: THREE,
   Water,
   DragControls,
   // you can't simply call useBaseUrl here because it's a react hook...
