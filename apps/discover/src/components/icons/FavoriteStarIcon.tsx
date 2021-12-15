d9838420cfc49998eb40977a129d21457c1c550a
manoj-cognite <91716778+manoj-cognite@users.noreply.github.com>
2021-12-15 09:19:42 +0530

    Fix(Discover): Favorite star icon testid rename (#1498)
    # What has changed?
    Rename the test id of favorite star icon

--- apps/discover/src/components/icons/FavoriteStarIcon.tsx
+++ apps/discover/src/components/icons/FavoriteStarIcon.tsx
@@ -5,7 +5,7 @@ import { FavoriteIndicatorContainer } from 'pages/authorized/search/elements';
 export const FavoriteStarIcon = () => {
   return (
     <FavoriteIndicatorContainer>
-      <Icon data-testId="favorite-star-icon" type="FavoriteFilled" />
+      <Icon data-testid="favorite-star-icon" type="FavoriteFilled" />
     </FavoriteIndicatorContainer>
   );
 };
