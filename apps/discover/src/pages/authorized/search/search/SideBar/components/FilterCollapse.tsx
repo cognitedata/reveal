42e88dc60e32233eab9c0ba43c9e3c03ba114744
manoj-cognite <91716778+manoj-cognite@users.noreply.github.com>
2022-01-05 17:36:42 +0530

    Fix(Discover): Sidebar filter drop-down fix (#1591)
    * fix: sidebar filter dropdown visible fix
    
    * fix: lint issues

--- apps/discover/src/pages/authorized/search/search/SideBar/components/FilterCollapse.tsx
+++ apps/discover/src/pages/authorized/search/search/SideBar/components/FilterCollapse.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import noop from 'lodash/noop';
-import styled from 'styled-components/macro';
 
 import { Collapse, Button } from '@cognite/cogs.js';
 
@@ -10,60 +9,10 @@ import { CollapseIcon } from 'components/icons/CollapseIcon';
 import { updateCategoryCollapseKey } from 'modules/sidebar/actions';
 import { useFilterActiveKeys } from 'modules/sidebar/selectors';
 import { CategoryTypes } from 'modules/sidebar/types';
-import { FlexColumn, FlexGrow, sizes } from 'styles/layout';
+import { FlexGrow } from 'styles/layout';
 
+import { Container, Panel } from './elements';
 import { FilterTitle } from './FilterTitle';
-
-const { Panel: DefaultPanel } = Collapse;
-
-const Panel = styled(DefaultPanel)`
-  /* Collapse in cogs.js isn't following cogs.js style (have to override temp.) */
-  &:hover:not(.rc-collapse-item-active) {
-    .rc-collapse-header h6 {
-      transition: 250ms;
-      color: var(--cogs-midblue-3);
-    }
-    .rc-collapse-header i {
-      color: var(--cogs-midblue-3);
-    }
-  }
-  &:hover.rc-collapse-item-active {
-    .rc-collapse-header h6 {
-      color: var(--cogs-greyscale-grey9);
-    }
-  }
-  & > .rc-collapse-header {
-    height: 52px;
-    border-bottom: none !important;
-    background: transparent !important;
-    padding: 12px 16px !important;
-  }
-  & > .rc-collapse-anim {
-    overflow: hidden !important;
-  }
-  & > .rc-collapse-content-active {
-    border-top: 1px solid var(--cogs-color-strokes-default);
-    background: transparent;
-    padding: 0 16px !important;
-    & > .rc-collapse-content-box {
-      margin: 0;
-      padding-top: ${sizes.normal};
-      padding-bottom: ${sizes.extraSmall};
-    }
-  }
-  border-top: none !important;
-  margin: 10px 0;
-  background-color: var(--cogs-greyscale-grey1);
-  border-radius: 6px;
-  min-width: 290px;
-  width: fit-content;
-`;
-
-const Container = styled(FlexColumn)`
-  justify-content: center;
-  order: -1;
-  margin-right: 8px;
-`;
 
 interface FilterCollapseProps {
   children: React.ReactNode;
--- apps/discover/src/pages/authorized/search/search/SideBar/components/elements.ts
+++ apps/discover/src/pages/authorized/search/search/SideBar/components/elements.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/macro';
 
-import { Body, Button, Icon, Menu, Badge } from '@cognite/cogs.js';
+import { Body, Button, Icon, Menu, Badge, Collapse } from '@cognite/cogs.js';
 
 import { FlexAlignItems, FlexColumn, sizes } from 'styles/layout';
 
@@ -89,4 +89,56 @@ export const ResultsCountBadge = styled(Badge)`
       props.disabled ? '--cogs-greyscale-grey7' : '--cogs-midblue-2'}
   ) !important;
   font-weight: 500;
+`;
+
+const { Panel: DefaultPanel } = Collapse;
+
+export const Panel = styled(DefaultPanel)`
+  /* Collapse in cogs.js isn't following cogs.js style (have to override temp.) */
+  &:hover:not(.rc-collapse-item-active) {
+    .rc-collapse-header h6 {
+      transition: 250ms;
+      color: var(--cogs-midblue-3);
+    }
+    .rc-collapse-header i {
+      color: var(--cogs-midblue-3);
+    }
+  }
+  &:hover.rc-collapse-item-active {
+    .rc-collapse-header h6 {
+      color: var(--cogs-greyscale-grey9);
+    }
+  }
+  & > .rc-collapse-header {
+    height: 52px;
+    border-bottom: none !important;
+    background: transparent !important;
+    padding: 12px 16px !important;
+  }
+  & > .rc-collapse-anim {
+    overflow: hidden !important;
+  }
+  & > .rc-collapse-content-active {
+    border-top: 1px solid var(--cogs-color-strokes-default);
+    background: transparent;
+    padding: 0 16px !important;
+    overflow: visible;
+    & > .rc-collapse-content-box {
+      margin: 0;
+      padding-top: ${sizes.normal};
+      padding-bottom: ${sizes.extraSmall};
+    }
+  }
+  border-top: none !important;
+  margin: 10px 0;
+  background-color: var(--cogs-greyscale-grey1);
+  border-radius: 6px;
+  min-width: 290px;
+  width: fit-content;
+`;
+
+export const Container = styled(FlexColumn)`
+  justify-content: center;
+  order: -1;
+  margin-right: 8px;
 `;
