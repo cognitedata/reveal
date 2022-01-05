b10f29e0958b21e456d0dc840079a6ce04849ce9
Pramod S <pramod.s@cognite.com>
2022-01-05 20:01:14 +0100

    Updated unit test

--- viewer/packages/tools/src/Toolbar/ToolbarTool.test.ts
+++ viewer/packages/tools/src/Toolbar/ToolbarTool.test.ts
@@ -1,29 +1,38 @@
 /*!
  * Copyright 2021 Cognite AS
  */
-import { Cognite3DViewer } from '@reveal/core';
+import { Cognite3DViewer, CogniteModelBase } from '@reveal/core';
 import { CogniteClient } from '@cognite/sdk';
 import { ToolbarTool } from './ToolbarTool';
 import { createGlContext, mockClientAuthentication } from '../../../../test-utilities';
 import * as THREE from 'three';
 
 describe('ToolbarTool', () => {
-  const sdk = new CogniteClient({ appId: 'cognite.reveal.unittest' });
-  mockClientAuthentication(sdk);
+  let viewer: Cognite3DViewer;
+  let models: CogniteModelBase[];
+  let tool: ToolbarTool;
 
-  const context = createGlContext(64, 64, { preserveDrawingBuffer: true });
-  const renderer = new THREE.WebGLRenderer({ context });
+  let canvasContainer: HTMLDivElement;
+
   const styles = {
     container: 'reveal-viewer-toolbar-container',
     bottom: 'reveal-viewer-toolbar-container--bottom'
   };
-  const canvasContainer = document.createElement('div');
-  canvasContainer.style.width = '640px';
-  canvasContainer.style.height = '480px';
 
-  const viewer = new Cognite3DViewer({ domElement: canvasContainer, sdk, renderer });
-  const models = viewer.models.slice();
-  const tool = new ToolbarTool(viewer, models[0]);
+  beforeEach(() => {
+    const sdk = new CogniteClient({ appId: 'cognite.reveal.unittest' });
+    mockClientAuthentication(sdk);
+
+    const context = createGlContext(64, 64, { preserveDrawingBuffer: true });
+    const renderer = new THREE.WebGLRenderer({ context });
+    canvasContainer = document.createElement('div');
+    canvasContainer.style.width = '640px';
+    canvasContainer.style.height = '480px';
+
+    viewer = new Cognite3DViewer({ domElement: canvasContainer, sdk, renderer });
+    models = viewer.models.slice();
+    tool = new ToolbarTool(viewer, models[0]);
+  });
 
   afterEach(() => {
     jest.restoreAllMocks();
@@ -57,43 +66,22 @@ describe('ToolbarTool', () => {
     expect(toolbarContainer[0].classList.contains(styles.container));
   });
 
-  test('Add Axis Tool to the Toolbar', () => {
+  test('Add Axis Tool, screenshot tool, camera target click toggle, zoom past, camera fit to model to the Toolbar', () => {
     const createElementMock = jest.spyOn(document, 'createElement');
+    const toolbarContainer = canvasContainer.getElementsByClassName(styles.container);
+
+    expect(toolbarContainer[0].hasChildNodes()).toBeFalse;
 
     tool.addAxisToolToggle();
-
-    expect(createElementMock).toBeCalledTimes(2);
-  });
-
-  test('Add take screenshot tool to the Toolbar', () => {
-    const createElementMock = jest.spyOn(document, 'createElement');
-
     tool.addTakeScreenshotTool();
-
-    expect(createElementMock).toBeCalledTimes(2);
-  });
-
-  test('Add Camera target on click toggle to the Toolbar', () => {
-    const createElementMock = jest.spyOn(document, 'createElement');
-
     tool.addCameraTargetOnClickToggle();
-
-    expect(createElementMock).toBeCalledTimes(2);
-  });
-
-  test('Add Zoom past to cursor to the Toolbar', () => {
-    const createElementMock = jest.spyOn(document, 'createElement');
-
     tool.addZoomPastToCursorToggle();
-
-    expect(createElementMock).toBeCalledTimes(2);
-  });
-
-  test('Add Fit camera to model to the Toolbar', () => {
-    const createElementMock = jest.spyOn(document, 'createElement');
-
     tool.addFitCameraToModel();
 
-    expect(createElementMock).toBeCalledTimes(2);
+    expect(createElementMock).toBeCalledTimes(10);
+
+    expect(toolbarContainer[0].hasChildNodes()).toBeTrue;
+
+    expect(toolbarContainer[0].childElementCount).toBe(5);
   });
 });
