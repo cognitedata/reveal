e47188250b669564b99a4ce078fc1108d8de8d35
Savokr <savelii.novikov@cognite.com>
2022-01-11 13:33:59 +0100

    Lint

--- viewer/core/src/public/migration/Cognite3DViewer.test.ts
+++ viewer/core/src/public/migration/Cognite3DViewer.test.ts
@@ -232,7 +232,9 @@ describe('Cognite3DViewer', () => {
     // Assert
     const newCameraControlsOptions = viewer.getCameraControlsOptions();
 
-    expect(newCameraControlsOptions.changeCameraTargetOnClick).not.toEqual(originalCameraControlsOptions.changeCameraTargetOnClick);
+    expect(newCameraControlsOptions.changeCameraTargetOnClick).not.toEqual(
+      originalCameraControlsOptions.changeCameraTargetOnClick
+    );
     expect(newCameraControlsOptions.mouseWheelAction).toEqual('zoomToTarget');
   });
 });
