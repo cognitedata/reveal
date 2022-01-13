8f6e49d48da171d9eb595db44ef73fa9641d22cb
Mihil Ranathunga <mihilranathunga@gmail.com>
2022-01-14 01:39:24 +0530

    added test cases for most error scenarios

--- src/store/hooks/tests/useAssetLinkWarning.spec.tsx
+++ src/store/hooks/tests/useAssetLinkWarning.spec.tsx
@@ -7,13 +7,14 @@ import { VisionAPIType } from 'src/api/types';
 import React from 'react';
 import { waitFor } from '@testing-library/react';
 import { WrappedWithProviders } from 'src/__test-utils/renderer';
+import { AnnotationTableItem } from 'src/modules/Review/types';
+import { FileInfo } from '@cognite/cdf-sdk-singleton';
 
 describe('tests useAssetLinkWarningHook', () => {
   const getDummyAnnotation = (
     id?: number,
     modelType?: number,
-    linkedResourceId?: number,
-    status?: AnnotationStatus
+    linkedResourceId?: number
   ) => {
     return {
       ...AnnotationUtils.createVisionAnnotationStub(
@@ -22,112 +23,178 @@ describe('tests useAssetLinkWarningHook', () => {
         modelType || 1,
         1,
         123,
-        124,
-        { shape: 'rectangle', vertices: [] },
-        undefined,
-        undefined,
-        status || AnnotationStatus.Unhandled,
-        undefined,
-        undefined,
-        undefined,
-        linkedResourceId
+        124
       ),
+      linkedResourceId,
       show: true,
       selected: false,
-    };
+    } as AnnotationTableItem;
   };
 
-  const getDummyFile = (linkedAssetIds?: number[]) => {
-    return {
-      id: 1,
-      uploaded: true,
-      name: 'one',
-      assetIds: linkedAssetIds || [],
-      createdTime: new Date(2021, 0, 1, 7, 0, 0, 0),
-      lastUpdatedTime: new Date(2021, 0, 1, 7, 0, 0, 0),
-      sourceCreatedTime: new Date(2021, 0, 1, 7, 0, 0, 0),
-      uploadedTime: new Date(2021, 0, 1, 7, 0, 0, 0),
-    };
+  const dummyFile: FileInfo = {
+    id: 1,
+    uploaded: true,
+    name: 'one',
+    createdTime: new Date(2021, 0, 1, 7, 0, 0, 0),
+    lastUpdatedTime: new Date(2021, 0, 1, 7, 0, 0, 0),
+    sourceCreatedTime: new Date(2021, 0, 1, 7, 0, 0, 0),
+    uploadedTime: new Date(2021, 0, 1, 7, 0, 0, 0),
   };
+
+  const approveAnnotation = (annotation: AnnotationTableItem) => ({
+    ...annotation,
+    status: AnnotationStatus.Verified,
+  });
+  const rejectAnnotation = (annotation: AnnotationTableItem) => ({
+    ...annotation,
+    status: AnnotationStatus.Rejected,
+  });
+
+  const linkFileToAssets = (assetIds?: number[]) => ({
+    ...dummyFile,
+    assetIds,
+  });
+
+  const getRenderProps = (
+    annotation: AnnotationTableItem,
+    annotationSibling: AnnotationTableItem,
+    fileAssetIds: number[]
+  ) => ({
+    annotation,
+    file: linkFileToAssets(fileAssetIds),
+    allAnnotations: [annotation, annotationSibling],
+  });
+
+  const annotationAssetId = 1;
+  const annotationSiblingAssetId = 2;
+  const tagAnnotation = getDummyAnnotation(
+    1,
+    VisionAPIType.TagDetection,
+    annotationAssetId
+  );
+  const tagAnnotationSibling = getDummyAnnotation(
+    2,
+    VisionAPIType.TagDetection,
+    annotationSiblingAssetId
+  );
 
   test('initial value', async () => {
     jest.mock('@cognite/cdf-sdk-singleton');
     jest.useFakeTimers();
 
-    // @ts-ignore
+    // render hook for first time
     const { result, rerender } = renderHook(
       ({ annotation, file, allAnnotations }) =>
         useAssetLinkWarning(annotation, file, allAnnotations),
       {
         wrapper: WrappedWithProviders,
-        initialProps: {
-          annotation: getDummyAnnotation(1, VisionAPIType.TagDetection, 1),
-          file: getDummyFile(),
-          allAnnotations: [
-            getDummyAnnotation(1, VisionAPIType.TagDetection, 1),
-            getDummyAnnotation(2, VisionAPIType.TagDetection, 2),
-          ],
-        },
+        initialProps: getRenderProps(tagAnnotation, tagAnnotationSibling, []),
       }
     );
 
+    // assert initial no warning
     await waitFor(() => {
-      // assert initial state
       expect(result.current).toBe(AssetWarnTypes.NoWarning);
     });
 
+    // approve tag annotation and asset linked to file
     act(() => {
-      // approve one annotation
-      rerender({
-        annotation: getDummyAnnotation(
-          1,
-          VisionAPIType.TagDetection,
-          1,
-          AnnotationStatus.Verified
-        ),
-        file: getDummyFile([1]),
-        allAnnotations: [
-          getDummyAnnotation(
-            1,
-            VisionAPIType.TagDetection,
-            1,
-            AnnotationStatus.Verified
-          ),
-          getDummyAnnotation(2, VisionAPIType.TagDetection, 2),
-        ],
-      });
+      rerender(
+        getRenderProps(approveAnnotation(tagAnnotation), tagAnnotationSibling, [
+          annotationAssetId,
+        ])
+      );
     });
+
+    // assert no warning after approval
     await waitFor(() => {
-      // assert initial state
       expect(result.current).toBe(AssetWarnTypes.NoWarning);
     });
 
+    // approve tag annotation but asset not linked to file
     act(() => {
-      // approve one annotation but asset not linked
-      rerender({
-        annotation: getDummyAnnotation(
-          1,
-          VisionAPIType.TagDetection,
-          1,
-          AnnotationStatus.Verified
-        ),
-        file: getDummyFile(),
-        allAnnotations: [
-          getDummyAnnotation(
-            1,
-            VisionAPIType.TagDetection,
-            1,
-            AnnotationStatus.Verified
-          ),
-          getDummyAnnotation(2, VisionAPIType.TagDetection, 2),
-        ],
-      });
+      rerender(
+        getRenderProps(
+          approveAnnotation(tagAnnotation),
+          tagAnnotationSibling,
+          []
+        )
+      );
     });
+
+    // assert error, file not linked after approval
     await waitFor(() => {
-      // assert initial state
       expect(result.current).toBe(
         AssetWarnTypes.ApprovedAnnotationAssetNotLinkedToFile
       );
+    });
+
+    // approve both annotations but file only linked to sibling annotation asset
+    act(() => {
+      rerender(
+        getRenderProps(
+          approveAnnotation(tagAnnotation),
+          approveAnnotation(tagAnnotationSibling),
+          [annotationSiblingAssetId]
+        )
+      );
+    });
+
+    // assert error file not linked after approval
+    await waitFor(() => {
+      expect(result.current).toBe(
+        AssetWarnTypes.ApprovedAnnotationAssetNotLinkedToFile
+      );
+    });
+
+    // approve both annotations file linked to both assets
+    act(() => {
+      rerender(
+        getRenderProps(
+          approveAnnotation(tagAnnotation),
+          approveAnnotation(tagAnnotationSibling),
+          [annotationAssetId, annotationSiblingAssetId]
+        )
+      );
+    });
+
+    // assert no warning
+    await waitFor(() => {
+      expect(result.current).toBe(AssetWarnTypes.NoWarning);
+    });
+
+    // reject annotation file still linked to annotation asset
+    act(() => {
+      rerender(
+        getRenderProps(
+          rejectAnnotation(tagAnnotation),
+          approveAnnotation(tagAnnotationSibling),
+          [annotationAssetId, annotationSiblingAssetId]
+        )
+      );
+    });
+
+    // assert error  file still linked to asset
+    await waitFor(() => {
+      expect(result.current).toBe(
+        AssetWarnTypes.RejectedAnnotationAssetLinkedToFile
+      );
+    });
+
+    // reject both annotations file still linked to annotation sibling asset
+    act(() => {
+      rerender(
+        getRenderProps(
+          rejectAnnotation(tagAnnotation),
+          rejectAnnotation(tagAnnotationSibling),
+          [annotationSiblingAssetId]
+        )
+      );
+    });
+
+    // assert no warning
+    await waitFor(() => {
+      expect(result.current).toBe(AssetWarnTypes.NoWarning);
     });
   });
 });
