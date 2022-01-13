a786a97a7c5ecd2b49d7687d5848c297cfdd0cb0
Lars Moastuen <lars.moastuen@cognite.com>
2022-01-13 11:25:29 +0100

    Add treeIndex+subtreeSize

--- examples/src/utils/InspectNodeUi.ts
+++ examples/src/utils/InspectNodeUi.ts
@@ -1,6 +1,6 @@
 import * as dat from 'dat.gui';
 import { Cognite3DModel } from "@cognite/reveal";
-import { CogniteClient } from "@cognite/sdk";
+import { CogniteClient, Node3D } from "@cognite/sdk";
 import assert from 'assert';
 
 export class InspectNodeUI {
@@ -26,20 +26,26 @@ export class InspectNodeUI {
     for (const node of nodes) {
       const nodeUi = this._uiFolder.addFolder(`${node.name} [depth=${node.depth}]`);
       this.registerUiFolder(nodeUi);
-      if (node.properties === undefined) {
-        continue;
-      }
-
-      for (const [category, entries] of Object.entries(node.properties)) {
-        const categoryFolder = nodeUi.addFolder(category);
-
-        for (const key of Object.keys(entries)) {
-          categoryFolder.add(entries, key).name(key);
-        }
-      }
+      this.createNodePropertiesFolder(node, nodeUi);
     }
   }
   
+  private createNodePropertiesFolder(node: Node3D, nodeUi: dat.GUI) {
+    nodeUi.add(node, 'treeIndex');
+    nodeUi.add(node, 'subtreeSize');
+
+    if (node.properties === undefined) {
+      return;
+    }
+    for (const [category, entries] of Object.entries(node.properties)) {
+      const categoryFolder = nodeUi.addFolder(category);
+
+      for (const key of Object.keys(entries)) {
+        categoryFolder.add(entries, key).name(key);
+      }
+    }
+  }
+
   private createNoNodes() {
     this.registerUiFolder(this._uiFolder.addFolder('Could not find node'));
   }
