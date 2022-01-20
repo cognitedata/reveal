3fbb065aaa207359ec9a5b09afeb4778f429cf9b
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2022-01-20 12:39:58 +0100

    fix: fix nothing being loaded after regaining connection (#1793)

--- viewer/packages/cad-geometry-loaders/src/utilities/PromiseUtils.ts
+++ viewer/packages/cad-geometry-loaders/src/utilities/PromiseUtils.ts
@@ -3,25 +3,46 @@
  */
 
 /**
+ * PromiseResult - Contains the result from a
+ * promise together with and indicator of whether the
+ * the promise resolved or rejected
+ */
+type PromiseResult<T> = {
+  result?: T;
+  error?: any;
+};
+
+/**
  * Extension utilities for Promise.
  */
 export class PromiseUtils {
   /**
    * Awaits all promises in the list of promises available, returning them in an async iterator
    * as they finish. The function completes when all promises has completed and the results have
-   * been yielded, or one of the promises provided rejects. This function is similar to
+   * been yielded. Each result is given as a PromiseResult object. This function is similar to
    * Promise.all(), except that it returns results as they are ready and doesn't wait for all
    * operations to finish.
    *
    * @param promises A list of promises to complete.
    */
-  static async *raceUntilAllCompleted<T>(promises: Promise<T>[]): AsyncIterable<T> {
+  static async *raceUntilAllCompleted<T>(promises: Promise<T>[]): AsyncIterable<PromiseResult<T>> {
     // Inspired by https://stackoverflow.com/a/42898229
-    const remaining = new Map(promises.map(p => [p, p.then(() => [p])]));
+    const remaining = new Map(
+      promises.map(p => [
+        p,
+        p
+          .then(res => {
+            return [p, { result: res }] as [Promise<T>, PromiseResult<T>];
+          })
+          .catch(error => {
+            return [p, { error: error }] as [Promise<T>, PromiseResult<T>];
+          })
+      ])
+    );
     while (remaining.size > 0) {
-      const [completed] = await Promise.race(remaining.values());
-      remaining.delete(completed);
-      yield completed;
+      const [promise, result] = await Promise.race(remaining.values());
+      remaining.delete(promise);
+      yield result;
     }
   }
 }
