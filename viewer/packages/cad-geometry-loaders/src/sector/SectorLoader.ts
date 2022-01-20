2ee8711a186e89c9ad27884a2897883ca9a6e4ca
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2022-01-20 11:45:23 +0100

    Update viewer/packages/cad-geometry-loaders/src/sector/SectorLoader.ts
    Co-authored-by: Lars Moastuen <lars.moastuen@cognite.com>

--- viewer/packages/cad-geometry-loaders/src/sector/SectorLoader.ts
+++ viewer/packages/cad-geometry-loaders/src/sector/SectorLoader.ts
@@ -88,7 +88,7 @@ export class SectorLoader {
       const filteredSectors = await this.filterSectors(sectorCullerInput, batch, sectorCuller, progressHelper);
       const consumedPromises = this.startLoadingBatch(filteredSectors, progressHelper, cadModels);
       for await (const consumed of PromiseUtils.raceUntilAllCompleted(consumedPromises)) {
-        if (consumed.result) {
+        if (consumed.result !== undefined) {
           this._modelStateHandler.updateState(consumed.result);
           yield consumed.result;
         }
