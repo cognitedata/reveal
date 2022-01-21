b865d2bb2c85a8a949665e89f93be1b3621aba5c
Deep <70804363+deep-cognite@users.noreply.github.com>
2022-01-21 11:23:06 +0100

    fix(discover): missing return statement (#1728)

--- apps/discover/src/components/metadataTable/formatItem.tsx
+++ apps/discover/src/components/metadataTable/formatItem.tsx
@@ -26,9 +26,11 @@ const maxLabels = 9;
 export const formatItem = ({ value, type, actions }: FormatItemProps) => {
   if (type === 'text') {
     if (actions) {
-      <Text>
-        {value || 'Unknown'} {actions}
-      </Text>;
+      return (
+        <Text>
+          {value || 'unknown'} {actions}
+        </Text>
+      );
     }
 
     return (
