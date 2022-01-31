ebd3fe4d37edb345da0bff9213cb594f0e7d47c5
saagar-cognite <saagar.takhi@cognite.com>
2022-01-31 13:06:15 +0100

    chore(node-visualizer): add test configuration

---
+++ packages/node-visualizer/src/setupTests.ts
@@ -0,0 +1,14 @@
+// jest.mock('@cognite/seismic-sdk-js', () => {
+//   class CogniteSeismicClient {
+//     public file = {
+//       getTextHeader: () =>
+//         Promise.resolve({
+//           meta: {
+//             header: 'test',
+//           },
+//         }),
+//     };
+//   }
+//
+//   return { CogniteSeismicClient };
+// });
