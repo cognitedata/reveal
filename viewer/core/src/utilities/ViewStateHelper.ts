9b45ba798015761a1f31d0fe2fb4831385f3eddb
Lars Moastuen <lars.moastuen@cognite.com>
2022-02-01 12:59:58 +0100

    Add clipping planes to state

--- viewer/core/src/utilities/ViewStateHelper.ts
+++ viewer/core/src/utilities/ViewStateHelper.ts
@@ -5,12 +5,12 @@ import * as THREE from 'three';
 
 import { Cognite3DModel } from '../public/migration/Cognite3DModel';
 import { Cognite3DViewer } from '../public/migration/Cognite3DViewer';
+import { NodeCollectionDeserializer } from '../datamodels/cad/styling/NodeCollectionDeserializer';
 
 import { RevealCameraControls } from '@reveal/camera-manager';
 import { NodeAppearance } from '@reveal/cad-styling';
 
 import { CogniteClient } from '@cognite/sdk';
-import { NodeCollectionDeserializer } from '@reveal/core';
 
 export type ViewerState = {
   camera?: {
@@ -18,6 +18,14 @@ export type ViewerState = {
     target: { x: number; y: number; z: number };
   };
   models?: ModelState[];
+  clippingPlanes?: ClippingPlanesState[];
+};
+
+export type ClippingPlanesState = {
+  nx: number;
+  ny: number;
+  nz: number;
+  constant: number;
 };
 
 export type ModelState = {
@@ -40,8 +48,33 @@ export class ViewStateHelper {
 
   public getCurrentState(): ViewerState {
     const cameraState = this._cameraControls.getState();
+    const modelStates = this.getModelsState();
+    const clippingPlanesState = this.getClippingPlanesState();
 
-    const modelStates = this._viewer.models
+    return {
+      camera: {
+        position: cameraState.position,
+        target: cameraState.target
+      },
+      models: modelStates,
+      clippingPlanes: clippingPlanesState
+    };
+  }
+
+  public async setState(viewerState: ViewerState): Promise<void> {
+    if (viewerState.camera !== undefined) {
+      this.setCameraFromState(viewerState.camera);
+    }
+    if (viewerState.models !== undefined) {
+      this.setModelState(viewerState.models);
+    }
+    if (viewerState.clippingPlanes !== undefined) {
+      this.setClippingPlanesState(viewerState.clippingPlanes);
+    }
+  }
+
+  private getModelsState(): ModelState[] {
+    return this._viewer.models
       .filter(model => model instanceof Cognite3DModel)
       .map(model => model as Cognite3DModel)
       .map(model => {
@@ -61,23 +94,12 @@ export class ViewStateHelper {
           styledSets: styledCollections
         } as ModelState;
       });
-
-    return {
-      camera: {
-        position: cameraState.position,
-        target: cameraState.target
-      },
-      models: modelStates
-    };
   }
 
-  public async setState(viewerState: ViewerState): Promise<void> {
-    if (viewerState.camera !== undefined) {
-      this.setCameraFromState(viewerState.camera);
-    }
-    if (viewerState.models !== undefined) {
-      this.setModelState(viewerState.models);
-    }
+  private getClippingPlanesState(): ClippingPlanesState[] {
+    return this._viewer
+      .getClippingPlanes()
+      .map(p => ({ nx: p.normal.x, ny: p.normal.y, nz: p.normal.z, constant: p.constant }));
   }
 
   private setCameraFromState(cameraState: Exclude<ViewerState['camera'], undefined>) {
@@ -89,7 +111,7 @@ export class ViewStateHelper {
     );
   }
 
-  private async setModelState(modelsState: Exclude<ViewerState['models'], undefined>) {
+  private async setModelState(modelsState: ModelState[]) {
     const cadModels = this._viewer.models
       .filter(model => model instanceof Cognite3DModel)
       .map(model => model as Cognite3DModel);
@@ -123,5 +145,10 @@ export class ViewStateHelper {
           );
         })
     );
+  }
+
+  private setClippingPlanesState(clippingPlanes: ClippingPlanesState[]) {
+    const planes = clippingPlanes.map(p => new THREE.Plane().setComponents(p.nx, p.ny, p.nz, p.constant));
+    this._viewer.setClippingPlanes(planes);
   }
 }
