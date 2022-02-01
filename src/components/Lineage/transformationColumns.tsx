f64ebd5e1eccc17e8dc7d7c4def3000cf5759eb6
Anna Gadacz <anna.gadacz@cognite.com>
2022-02-01 13:09:16 +0100

    refactor: clean up transformation columns

--- src/components/Lineage/transformationColumns.tsx
+++ src/components/Lineage/transformationColumns.tsx
@@ -4,6 +4,7 @@ import { trackEvent } from '@cognite/cdf-route-tracker';
 import sdk from '@cognite/cdf-sdk-singleton';
 import moment from 'moment';
 import { Icon } from '@cognite/cogs.js';
+import styled from 'styled-components';
 import { DataSet } from 'utils/types';
 import HiddenTransformation from './HiddenTranformation';
 
@@ -11,77 +12,83 @@ const transformationsColumns = (dataSet: DataSet) => [
   {
     key: 'name',
     title: 'Transform',
-    // eslint-disable-next-line no-restricted-globals
     sorter: (a: any, b: any) => stringCompare(a?.name, b?.name),
-    render: (_text: string, transform: any) =>
-      transform.hidden ? (
-        <HiddenTransformation
-          transformation={transform.storedData}
-          dataSet={dataSet}
-        />
-      ) : (
+    render: (_text: string, transform: any) => {
+      const { hidden, storedData, id, name } = transform;
+      const onTransformationClick = () =>
+        trackEvent(
+          'DataSets.LineageFlow.Clicked on an external transformation'
+        );
+      const env = getStringCdfEnv() ? `?env=${getStringCdfEnv()}` : '';
+      const href = `/${sdk.project}/transformations/${id}${env}`;
+      if (hidden)
+        return (
+          <HiddenTransformation transformation={storedData} dataSet={dataSet} />
+        );
+      return (
         <a
-          onClick={() =>
-            trackEvent(
-              'DataSets.LineageFlow.Clicked on an external transformation'
-            )
-          }
-          href={`/${sdk.project}/transformations/${transform.id}${
-            getStringCdfEnv() ? `?env=${getStringCdfEnv()}` : ''
-          }`}
+          onClick={onTransformationClick}
+          href={href}
           target="_blank"
           rel="noopener noreferrer"
         >
-          {transform.name}
+          {name}
         </a>
-      ),
+      );
+    },
   },
   {
     key: 'created',
     title: 'Created',
-    render: (_text: string, transform: any) => (
-      <p>
-        {transform.hidden ? (
-          <p style={{ fontStyle: 'italic' }}> Not available</p>
-        ) : (
-          moment(transform.created).toString()
-        )}
-      </p>
-    ),
+    render: (_text: string, transform: any) => {
+      const { hidden, created } = transform;
+      const cellText = hidden ? 'Not available' : moment(created).toString();
+      return (
+        <CellTransformation $hidden={hidden}>{cellText}</CellTransformation>
+      );
+    },
   },
   {
     key: 'updated',
     title: 'Updated',
-    render: (_text: string, transform: any) => (
-      <p>
-        {transform.hidden ? (
-          <p style={{ fontStyle: 'italic' }}> Not available</p>
-        ) : (
-          moment(transform.updated).toString()
-        )}
-      </p>
-    ),
+    render: (_text: string, transform: any) => {
+      const { hidden, updated } = transform;
+      const cellText = hidden ? 'Not available' : moment(updated).toString();
+      return (
+        <CellTransformation $hidden={hidden}>{cellText}</CellTransformation>
+      );
+    },
   },
   {
     key: 'owner',
     title: 'Owner',
-    render(_text: string, transform: any) {
-      if (transform.hidden) {
-        return <p style={{ fontStyle: 'italic' }}> Not available</p>;
-      }
-      if (transform.ownerIsCurrentUser) {
-        return (
-          <>
-            <Icon type="User" /> me
-          </>
-        );
-      }
-      if (transform.owner) {
-        return transform.owner.user;
-      }
-      return '';
+    render: (_text: string, transform: any) => {
+      const { hidden, owner: _owner, ownerIsCurrentUser } = transform;
+      const owner = ownerIsCurrentUser ? (
+        <>
+          <Icon type="User" /> me
+        </>
+      ) : (
+        _owner?.user ?? ''
+      );
+      const cellText = hidden ? 'Not available' : owner;
+      return (
+        <CellTransformation $hidden={transform.hidden}>
+          {cellText}
+        </CellTransformation>
+      );
     },
   },
 ];
 
 export default transformationsColumns;
+
+const CellTransformation = styled.p.attrs(
+  ({ $hidden }: { $hidden?: boolean }) => {
+    const style: React.CSSProperties = {};
+    if ($hidden) style.fontStyle = 'italic';
+    return { style };
+  }
+)<{ $hidden?: boolean }>`
+  margin-bottom: 0;
+`;
