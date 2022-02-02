30ce7e52f3b2ba46c843ae5a3fca48f74ec1d935
Soumesh Banerjee <soumesh.banerjee@cognite.com>
2022-02-02 10:27:00 +0100

    Comply command templates list (#228)

--- apps/platypus-cdf-cli/src/app/cmds/templates/list.ts
+++ apps/platypus-cdf-cli/src/app/cmds/templates/list.ts
@@ -1,39 +1,34 @@
 import { TemplatesApiService } from '@platypus/platypus-core';
-import { CommandArgument, CommandArgumentType } from '../../types';
 import { CLICommand } from '@cognite/platypus-cdf-cli/app/common/cli-command';
-import Response from '@cognite/platypus-cdf-cli/app/utils/logger';
+import Response, {
+  DEBUG as _DEBUG,
+} from '@cognite/platypus-cdf-cli/app/utils/logger';
 
 import { getCogniteSDKClient } from '@cognite/platypus-cdf-cli/app/utils/cogniteSdk';
 
-export const commandArgs = [
-  {
-    name: 'full',
-    description: 'add --full for the full schema including generated types',
-    prompt: 'Do you want to see the response as JSON',
-    type: CommandArgumentType.BOOLEAN,
-    initial: false,
-    example: '$0 templates list --full',
-  },
-] as CommandArgument[];
-
+const DEBUG = _DEBUG.extend('platypus-cdf-cli:templates:list');
 export class TemplatesListCommand extends CLICommand {
-  async execute(args) {
+  async execute() {
     const client = getCogniteSDKClient();
+    DEBUG('CDF Clint initialized');
+
     const templatesApi = new TemplatesApiService(client);
+    DEBUG('TemplatesApiService initialized');
 
     const templates = await templatesApi.listTemplateGroups();
+    DEBUG(
+      'List of templates retrieved successfully, %o',
+      JSON.stringify(templates, null, 2)
+    );
 
-    if (args.full === true) {
-      Response.log(JSON.stringify(templates, null, 2));
-      return;
-    }
-
-    Response.log(templates.map((template) => template.externalId).join('\n'));
+    Response.success(
+      templates.map((template) => template.externalId).join('\n')
+    );
   }
 }
 
 export default new TemplatesListCommand(
   'list',
   'List all template groups for current project',
-  commandArgs
+  []
 );
