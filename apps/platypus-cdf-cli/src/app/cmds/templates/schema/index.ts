9943bd4429686ad34d03a3e9057c213b5281f6c6
Soumesh Banerjee <soumesh.banerjee@cognite.com>
2022-02-02 10:36:49 +0100

    Add schema pull command (#230)
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/platypus-cdf-cli/src/app/cmds/templates/schema/index.ts
+++ apps/platypus-cdf-cli/src/app/cmds/templates/schema/index.ts
@@ -1,12 +1,13 @@
 import { Argv } from 'yargs';
 import * as versionsCmd from './versions';
 import publishCmd from './publish';
+import pull from './pull';
 
 export const command = 'schema <command>';
 export const desc = 'Manages templates schemas';
 
 export const builder = (yargs: Argv) =>
-  yargs.command(versionsCmd).command(publishCmd).demandCommand();
+  yargs.command(versionsCmd).command(publishCmd).command(pull).demandCommand();
 
 export const handler = () => {
   return;
---
+++ apps/platypus-cdf-cli/src/app/cmds/templates/schema/pull.ts
@@ -0,0 +1,53 @@
+import { TemplatesApiService } from '@platypus/platypus-core';
+import { Arguments } from 'yargs';
+import CS from 'configstore';
+import { CLICommand } from '@cognite/platypus-cdf-cli/app/common/cli-command';
+import { getCogniteSDKClient } from '@cognite/platypus-cdf-cli/app/utils/cogniteSdk';
+import Response, {
+  DEBUG as _DEBUG,
+} from '@cognite/platypus-cdf-cli/app/utils/logger';
+import { BaseArgs } from '@cognite/platypus-cdf-cli/app/types';
+import { injectRCFile } from '@cognite/platypus-cdf-cli/app/common/config';
+import { CONSTANTS } from '@cognite/platypus-cdf-cli/app/constants';
+import { cwd } from 'process';
+import { join } from 'path';
+
+export const command = 'pull';
+export const desc = `Downloads the schema for the given template group (version is read from "${CONSTANTS.PROJECT_CONFIG_FILE_NAME}" file).`;
+
+const DEBUG = _DEBUG.extend('templates:schema:pull');
+
+export class TemplatesSchemaPullCommand extends CLICommand {
+  @injectRCFile()
+  async execute(args: Arguments<BaseArgs>) {
+    const client = getCogniteSDKClient();
+    DEBUG`CDF Clint initialized`;
+
+    const templatesApi = new TemplatesApiService(client);
+    DEBUG`TemplatesApiService initialized`;
+    DEBUG`Retrieving list of versions for template group`;
+
+    const schema = await templatesApi.listSchemaVersions({
+      solutionId: args.solutionConfig.config.templateId,
+      version: args.solutionConfig.config.templateVersion.toString(),
+    });
+    DEBUG`Got the schema, ${JSON.stringify(schema)}`;
+
+    if (!schema || !schema.length) {
+      return Response.error(
+        'There is no schema for this template group and version combination. Please check once again!'
+      );
+    }
+
+    if (schema.length > 1) {
+      DEBUG('More than one schema found for this version. %o', schema);
+      return Response.error(
+        'More than one schema found for this version, skipping download.'
+      );
+    }
+
+    Response.success(schema[0].schema);
+  }
+}
+
+export default new TemplatesSchemaPullCommand(command, desc, []);
