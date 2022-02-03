c71ae9315c411938ab4850f0ed83871821992a94
Håkon Flatval <hakon.flatval@cognite.com>
2022-02-03 13:14:34 +0100

    fix: explicitly set alpha value to 1 in color

--- viewer/packages/rendering/src/glsl/base/determineColor.glsl
+++ viewer/packages/rendering/src/glsl/base/determineColor.glsl
@@ -3,9 +3,9 @@
 vec4 determineColor(vec3 originalColor, NodeAppearance nodeAppearance) {
     vec4 overrideColor = nodeAppearance.colorTexel;
     if (any(greaterThan(overrideColor.rgb, vec3(0.0)))) {
-      return overrideColor;
+      return vec4(overrideColor.rgb, 1.0);
     }
-    return vec4(originalColor.rgb, overrideColor.a);
+    return vec4(originalColor.rgb, 1.0);
 }
 
 #pragma glslify: export(determineColor)
