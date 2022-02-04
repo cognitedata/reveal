2680f97a222a6827207bf263961253ff8284c53b
Lars Moastuen <lars.moastuen@cognite.com>
2022-02-04 23:26:08 +0100

    feat: add clipping planes to viewer state (#1828)

--- viewer/core/src/public/migration/Cognite3DViewer.ts
+++ viewer/core/src/public/migration/Cognite3DViewer.ts
@@ -802,7 +802,7 @@ export class Cognite3DViewer {
    * Returns the current active clipping planes.
    */
   getClippingPlanes(): THREE.Plane[] {
-    return this.revealManager.clippingPlanes;
+    return this.revealManager.clippingPlanes.map(p => p.clone());
   }
 
   /**
