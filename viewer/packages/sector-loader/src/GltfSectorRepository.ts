6f2ebddba6bdf9525b091d7e5d88a129ae4b8b70
Håkon Flatval <hakon.flatval@cognite.com>
2022-02-04 09:00:35 +0100

    refac: Move glTF loading logic to GltfSectorLoader

--- viewer/packages/sector-loader/src/GltfSectorRepository.ts
+++ viewer/packages/sector-loader/src/GltfSectorRepository.ts
@@ -1,152 +1,54 @@
 /*!
  * Copyright 2021 Cognite AS
  */
-import * as THREE from 'three';
 
 import { ConsumedSector, V9SectorMetadata, WantedSector, LevelOfDetail } from '@reveal/cad-parsers';
 import { BinaryFileProvider } from '@reveal/modeldata-api';
 import { CadMaterialManager } from '@reveal/rendering';
-import { GltfSectorParser, ParsedGeometry, RevealGeometryCollectionType } from '@reveal/sector-parser';
-import { MetricsLogger } from '@reveal/metrics';
-import { AutoDisposeGroup, assertNever, incrementOrInsertIndex } from '@reveal/utilities';
-
 import { SectorRepository } from './SectorRepository';
-import { filterGeometryOutsideClipBox } from '../../cad-parsers/src/cad/filterPrimitivesV9';
-
-import assert from 'assert';
+import { GltfSectorLoader } from './GltfSectorLoader';
 
 export class GltfSectorRepository implements SectorRepository {
-  private readonly _gltfSectorParser: GltfSectorParser;
-  private readonly _sectorFileProvider: BinaryFileProvider;
-  private readonly _materialManager: CadMaterialManager;
+  private readonly _gltfSectorLoader: GltfSectorLoader;
 
   constructor(sectorFileProvider: BinaryFileProvider, materialManager: CadMaterialManager) {
-    this._gltfSectorParser = new GltfSectorParser();
-    this._sectorFileProvider = sectorFileProvider;
-    this._materialManager = materialManager;
+    this._gltfSectorLoader = new GltfSectorLoader(sectorFileProvider, materialManager);
+  }
+
+  private async getEmptySectorWithLod(
+    lod: LevelOfDetail,
+    modelIdentifier: string,
+    metadata: V9SectorMetadata
+  ): Promise<ConsumedSector> {
+    return Promise.resolve({
+      modelIdentifier,
+      metadata,
+      levelOfDetail: lod,
+      group: undefined,
+      instancedMeshes: []
+    });
+  }
+
+  private async getEmptyDetailedSector(modelIdentifier: string, metadata: V9SectorMetadata) {
+    return this.getEmptySectorWithLod(LevelOfDetail.Detailed, modelIdentifier, metadata);
+  }
+
+  private async getEmptyDiscardedSector(modelIdentifier: string, metadata: V9SectorMetadata) {
+    return this.getEmptySectorWithLod(LevelOfDetail.Discarded, modelIdentifier, metadata);
   }
 
   async loadSector(sector: WantedSector): Promise<ConsumedSector> {
     const metadata = sector.metadata as V9SectorMetadata;
 
     if (metadata.sectorFileName === undefined || metadata.downloadSize === 0) {
-      return Promise.resolve({
-        modelIdentifier: sector.modelIdentifier,
-        metadata: metadata,
-        levelOfDetail: LevelOfDetail.Detailed,
-        group: undefined,
-        instancedMeshes: []
-      });
+      return this.getEmptyDetailedSector(sector.modelIdentifier, metadata);
     }
 
     if (sector.levelOfDetail === LevelOfDetail.Discarded) {
-      return Promise.resolve({
-        modelIdentifier: sector.modelIdentifier,
-        metadata: metadata,
-        levelOfDetail: LevelOfDetail.Discarded,
-        instancedMeshes: [],
-        group: undefined
-      });
+      return this.getEmptyDiscardedSector(sector.modelIdentifier, metadata);
     }
 
-    try {
-      const sectorByteBuffer = await this._sectorFileProvider.getBinaryFile(
-        sector.modelBaseUrl,
-        metadata.sectorFileName!
-      );
-
-      const group = new AutoDisposeGroup();
-
-      const parsedSectorGeometry = this._gltfSectorParser.parseSector(sectorByteBuffer);
-
-      const materials = this._materialManager.getModelMaterials(sector.modelIdentifier);
-
-      const geometryBatchingQueue: ParsedGeometry[] = [];
-
-      parsedSectorGeometry.forEach(parsedGeometry => {
-        const type = parsedGeometry.type as RevealGeometryCollectionType;
-
-        const filteredGeometryBuffer = filterGeometryOutsideClipBox(
-          parsedGeometry.geometryBuffer,
-          type,
-          sector.geometryClipBox ?? undefined
-        );
-
-        if (!filteredGeometryBuffer) return;
-
-        switch (type) {
-          case RevealGeometryCollectionType.BoxCollection:
-          case RevealGeometryCollectionType.CircleCollection:
-          case RevealGeometryCollectionType.ConeCollection:
-          case RevealGeometryCollectionType.EccentricConeCollection:
-          case RevealGeometryCollectionType.EllipsoidSegmentCollection:
-          case RevealGeometryCollectionType.GeneralCylinderCollection:
-          case RevealGeometryCollectionType.GeneralRingCollection:
-          case RevealGeometryCollectionType.QuadCollection:
-          case RevealGeometryCollectionType.TorusSegmentCollection:
-          case RevealGeometryCollectionType.TrapeziumCollection:
-          case RevealGeometryCollectionType.NutCollection:
-            geometryBatchingQueue.push({
-              type,
-              geometryBuffer: filteredGeometryBuffer,
-              instanceId: RevealGeometryCollectionType[type].toString()
-            });
-            break;
-          case RevealGeometryCollectionType.InstanceMesh:
-            geometryBatchingQueue.push({
-              type,
-              geometryBuffer: filteredGeometryBuffer,
-              instanceId: parsedGeometry.instanceId!
-            });
-            break;
-          case RevealGeometryCollectionType.TriangleMesh:
-            this.createMesh(group, parsedGeometry.geometryBuffer, materials.triangleMesh);
-            break;
-          default:
-            assertNever(type);
-        }
-      });
-
-      return {
-        levelOfDetail: sector.levelOfDetail,
-        group: group,
-        instancedMeshes: [],
-        metadata: metadata,
-        modelIdentifier: sector.modelIdentifier,
-        geometryBatchingQueue: geometryBatchingQueue
-      };
-    } catch (error) {
-      MetricsLogger.trackError(error as Error, { moduleName: 'GltfSectorRepository', methodName: 'loadSector' });
-      throw error;
-    }
-  }
-
-  private createTreeIndexSet(geometry: THREE.BufferGeometry): Map<number, number> {
-    const treeIndexAttribute = geometry.attributes['treeIndex'];
-    assert(treeIndexAttribute !== undefined);
-
-    const treeIndexSet = new Map<number, number>();
-
-    for (let i = 0; i < treeIndexAttribute.count; i++) {
-      incrementOrInsertIndex(treeIndexSet, treeIndexAttribute.getX(i));
-    }
-
-    return treeIndexSet;
-  }
-
-  private createMesh(group: AutoDisposeGroup, geometry: THREE.BufferGeometry, material: THREE.ShaderMaterial) {
-    const mesh = new THREE.Mesh(geometry, material);
-    group.add(mesh);
-    mesh.frustumCulled = false;
-
-    mesh.userData.treeIndices = this.createTreeIndexSet(geometry);
-
-    if (material.uniforms.inverseModelMatrix === undefined) return;
-
-    mesh.onBeforeRender = () => {
-      const inverseModelMatrix: THREE.Matrix4 = material.uniforms.inverseModelMatrix.value;
-      inverseModelMatrix.copy(mesh.matrixWorld).invert();
-    };
+    return this._gltfSectorLoader.loadSector(sector);
   }
 
   clear(): void {}
