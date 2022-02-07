94b8824f02855b05da572b35a8e6f9c2319f2d0c
Thomas F <12623909+Olsworn@users.noreply.github.com>
2022-02-07 09:48:54 +0100

    chore(fas-demo): trigger spinnaker pipeline (#1882)

--- rules/spinnaker/spinnaker_deployment.template.bash
+++ rules/spinnaker/spinnaker_deployment.template.bash
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# This is a comment to trigger spinnaker_deploy
-
 set -e
 
 echo '{"name":"@@SERVICE_NAME@@","pipelines":@@PIPELINES@@,"manifest":"@@MANIFEST@@","docker_repository":"@@DOCKER_REPOSITORY@@"}'
--- rules/spinnaker/spinnaker_pipeline.template.bash
+++ rules/spinnaker/spinnaker_pipeline.template.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This is a comment to trigger spinnaker_pipeline
+
 set -e
 
 echo @@APP_CONFIG_PATH@@
