65a7494bdbaaf93d16c5bc9a0bb15537c9e33596
Soumesh Banerjee <soumeshbanerjee@live.com>
2022-02-11 10:38:02 +0100

    Update Publish command to use cdfrc.json (DX-316) (#247)
    * Update Publish command to use cdfrc.json
    
    * Fix the schema file check
    
    * update args
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/platypus-cdf-cli/src/app/cmds/templates/schema/publish.ts
+++ apps/platypus-cdf-cli/src/app/cmds/templates/schema/publish.ts
@@ -2,45 +2,23 @@ import { CreateSchemaDTO, TemplatesApiService } from '@platypus/platypus-core';
 import { readFileSync } from 'fs';
 import { Arguments } from 'yargs';
 import {
+  BaseArgs,
   CommandArgument,
   CommandArgumentType,
 } from '@cognite/platypus-cdf-cli/app/types';
 import { CLICommand } from '@cognite/platypus-cdf-cli/app/common/cli-command';
 import { getCogniteSDKClient } from '@cognite/platypus-cdf-cli/app/utils/cogniteSdk';
 import Response from '@cognite/platypus-cdf-cli/app/utils/logger';
+import { injectRCFile } from '@cognite/platypus-cdf-cli/app/common/config';
 
 const commandArgs = [
-  {
-    name: 'externalId',
-    description: 'Template Group ExternalId',
-    prompt: 'Please specify the template group External Id',
-    type: CommandArgumentType.STRING,
-    required: true,
-    example:
-      '$0 platypus templates schema publish --externalId=some-external-id --file=./file-path.graphql --templateVersion=1 --conflictMode=Update',
-  },
-  {
-    name: 'file',
-    description: 'Schema file path',
-    prompt:
-      'Please specify the the path to the schema file that you want to publish',
-    type: CommandArgumentType.STRING,
-    required: true,
-  },
-  {
-    name: 'templateVersion',
-    description: 'Specific template version',
-    prompt: 'Please specify the version that you want to use',
-    type: CommandArgumentType.NUMBER,
-    required: false,
-  },
   {
     name: 'conflictMode',
     description:
       'The Template Group conflict mode that will be used when an existing schema already exists.',
     prompt: 'Please specify the conflict mode that you want to use',
     type: CommandArgumentType.SELECT,
-    defaultValue: 'Patch',
+    initial: 'Patch',
     required: false,
     options: {
       choices: ['Patch', 'Update', 'Force'],
@@ -52,35 +30,34 @@ export const command = 'publish';
 
 export const desc = 'A new version of the template schema will be published';
 
-interface PublishSchemaArgs {
-  file: string;
-  externalId: string;
-  version?: number;
+type PublishSchemaArgs = BaseArgs & {
   conflictMode: string;
-}
+};
 
 export class TemplatesPublishSchemaCommand extends CLICommand {
+  @injectRCFile()
   async execute(args: Arguments<PublishSchemaArgs>) {
     const client = getCogniteSDKClient();
     const templatesApi = new TemplatesApiService(client);
-    const { externalId, conflictMode } = args;
 
-    let graphqlSchema;
-    try {
-      graphqlSchema = readFileSync(args.file, {
-        encoding: 'utf-8',
-      }).toString();
-    } catch (ex) {
-      Response.error(args.verbose ? JSON.stringify(ex, null, 2) : ex.message);
-      return;
+    const { conflictMode } = args;
+    const { templateId, templateVersion, schema } =
+      args.solutionConfig.all.config;
+
+    if (!schema) {
+      throw new Error(
+        'No schema found in solution config, please run pull to download schema first or edit and add the schema field in the solution config the the schema file.'
+      );
     }
+
+    const graphqlSchema = readFileSync(schema, {
+      encoding: 'utf-8',
+    }).toString();
 
     const dto = {
       schema: graphqlSchema,
-      solutionId: externalId,
-      version: args.templateVersion
-        ? args.templateVersion.toString()
-        : undefined,
+      solutionId: templateId,
+      version: templateVersion ? templateVersion.toString() : undefined,
     } as CreateSchemaDTO;
 
     try {
@@ -88,10 +65,10 @@ export class TemplatesPublishSchemaCommand extends CLICommand {
         ? templatesApi.publishSchema(dto)
         : templatesApi.updateSchema(dto, conflictMode === 'Force'));
 
-      Response.log(
+      Response.success(
         conflictMode === 'Update'
-          ? `Template group ${externalId} is created successfully`
-          : `Template group "${externalId}" was updated successfully`
+          ? `Template group ${templateId} is created successfully`
+          : `Template group "${templateId}" was updated successfully`
       );
     } catch (error) {
       if (error.type === 'BREAKING_CHANGE') {
