d63b29f7ed43a8f45c6d55e495caeb25b7af53d6
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2022-02-23 10:00:48 +0100

    refac: move RenderAlreadyLoadedGeometryProvider (#1881)
    * refactor: move RenderAlreadyLoadedGeometryProvider

--- viewer/packages/cad-geometry-loaders/src/sector/culling/OccludingGeometryProvider.ts
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright 2021 Cognite AS
- */
-
-export interface OccludingGeometryProvider {
-  renderOccludingGeometry(target: THREE.WebGLRenderTarget | null, camera: THREE.PerspectiveCamera): void;
-}
