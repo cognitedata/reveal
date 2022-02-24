cognitedata/reveal

commit 4ceb7950150b9a006bfa2c384f5ad40c42c63b52

feat(react-pid): auto-save/load legend  (#2007)
First PR 🎉 

**User story:** As a labeller, I want to be able to use the same legend across multiple documents without uploading it every time. 

This will save/load legend by DocumentType. 

Currently, this will save every time the symbols change. I don't think this will cause any performance issues, but if it does, we can tune it for performance. 

## Testing
Minimum test case should be some permutation of: 

- Load PID
- Load legend
- Reload
- Load ISO - confirm you don't get the wrong set of symbols
- Reload
- Load PID - confirm you get the same symbols as you previously used.

packages/pid-tools/src/cognite-pid.ts
@@ -83,12 +83,20 @@ export interface SaveSymbolData {
   direction?: Orientation;
 }
 
+export enum EventType {
+  LOAD = 'onLoad',
+}
+
+type EventListener = (ref: CognitePid) => void;
+
 export class CognitePid {
   host: HTMLDivElement;
   document: string | undefined;
   pidDocument: PidDocumentWithDom | undefined;
   svg: SVGSVGElement | undefined;
   isDrawing = false;
+
+  private eventListenersByEventType = new Map<EventType, EventListener[]>();
 
   private activeTool: ToolType = 'addSymbol';
   private activeToolSubscriber: ActiveToolCallback | undefined;
@@ -145,6 +153,37 @@ export class CognitePid {
     }
     this.host = host;
   }
+
+  addEventListener = (eventType: EventType, listenerFn: EventListener) => {
+    const eventListeners = this.eventListenersByEventType.get(eventType) ?? [];
+    this.eventListenersByEventType.set(eventType, [
+      ...eventListeners,
+      listenerFn,
+    ]);
+  };
+
+  removeEventListener = (eventType: EventType, listenerFn: EventListener) => {
+    const eventListeners = this.eventListenersByEventType.get(eventType);
+    if (eventListeners === undefined) {
+      return;
+    }
+
+    this.eventListenersByEventType.set(
+      eventType,
+      eventListeners.filter(
+        (existingListenerFn) => existingListenerFn !== listenerFn
+      )
+    );
+  };
+
+  emit = (eventType: EventType) => {
+    const eventListeners = this.eventListenersByEventType.get(eventType);
+    if (eventListeners === undefined) {
+      return;
+    }
+
+    eventListeners.forEach((listener) => listener(this));
+  };
 
   addSvgDocument(svgDocument: File) {
     svgDocument
@@ -459,7 +498,6 @@ export class CognitePid {
     loadSymbolsFromJson(
       graphDocument,
       setSymbols,
-      this.symbols,
       this.pidDocument,
       setSymbolInstances,
       this.symbolInstances,
@@ -514,6 +552,7 @@ export class CognitePid {
     this.host.appendChild(svg);
 
     this.pidDocument = PidDocumentWithDom.fromSVG(svg, allSvgElements);
+    this.emit(EventType.LOAD);
 
     this.refresh();
   }

packages/react-pid/src/ReactPid.tsx
@@ -1,31 +1,37 @@
 /* eslint-disable no-param-reassign */
 
-import React, { useEffect, useCallback, useRef, useState } from 'react';
 import {
-  DiagramSymbol,
-  DiagramLineInstance,
-  DiagramConnection,
-  DiagramSymbolInstance,
-  DiagramEquipmentTagInstance,
-  DocumentType,
-  PidDocumentWithDom,
-  DocumentMetadata,
-  PidDocumentMetadata,
-  IsoDocumentMetadata,
-  GraphDocument,
   CognitePid,
-  ToolType,
+  DiagramConnection,
+  DiagramEquipmentTagInstance,
+  DiagramLineInstance,
+  DiagramSymbol,
+  DocumentMetadata,
+  DocumentType,
+  EventType,
+  GraphDocument,
+  IsoDocumentMetadata,
+  PidDocumentMetadata,
+  PidDocumentWithDom,
   SaveSymbolData,
+  ToolType,
   saveGraphAsJson,
 } from '@cognite/pid-tools';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-import { ReactPidWrapper, ReactPidLayout } from './elements';
 import { SidePanel } from './components';
-import { Viewport } from './components/viewport/Viewport';
+import useSymbolState from './components/side-panel/useSymbolState';
 import { Toolbar } from './components/toolbar/Toolbar';
+import { Viewport } from './components/viewport/Viewport';
+import { ReactPidLayout, ReactPidWrapper } from './elements';
 
 export const ReactPid: React.FC = () => {
+  const [, setHasPidViewerLoadedDocument] = useState(false);
   const pidViewer = useRef<CognitePid>();
+
+  const getPidDocument = (): PidDocumentWithDom | undefined => {
+    return pidViewer.current?.pidDocument;
+  };
 
   const [activeTool, setActiveTool] = useState<ToolType>('selectDocumentType');
   const [fileUrl, setFileUrl] = useState<string>('');
@@ -34,10 +40,8 @@ export const ReactPid: React.FC = () => {
     name: 'Unknown',
     unit: 'Unknown',
   });
-  const [symbols, setSymbols] = useState<DiagramSymbol[]>([]);
-  const [symbolInstances, setSymbolInstances] = useState<
-    DiagramSymbolInstance[]
-  >([]);
+  const { symbols, setSymbols, symbolInstances, setSymbolInstances } =
+    useSymbolState(documentMetadata.type, getPidDocument());
   const [lines, setLines] = useState<DiagramLineInstance[]>([]);
   const [connections, setConnections] = useState<DiagramConnection[]>([]);
   const [equipmentTags, setEquipmentTags] = useState<
@@ -79,7 +83,16 @@ export const ReactPid: React.FC = () => {
 
   const initPid = (instance: CognitePid) => {
     pidViewer.current = instance;
+    instance.addEventListener(EventType.LOAD, () =>
+      setHasPidViewerLoadedDocument(true)
+    );
   };
+
+  useEffect(() => {
+    if (pidViewer.current) {
+      pidViewer.current?.setSymbolInstances(symbolInstances);
+    }
+  }, [symbolInstances]);
 
   useEffect(() => {
     if (pidViewer.current) {
@@ -195,10 +208,6 @@ export const ReactPid: React.FC = () => {
       pidViewer.current?.setActiveTagId(null);
     }
   }, [activeTool]);
-
-  const getPidDocument = (): PidDocumentWithDom | undefined => {
-    return pidViewer.current?.pidDocument;
-  };
 
   const setActiveToolWrapper = (tool: ToolType) => {
     if (!pidViewer.current) return;

packages/react-pid/src/components/side-panel/useSymbolState.ts
@@ -1,0 +1,76 @@
+import { useEffect, useState } from 'react';
+import {
+  computeSymbolInstances,
+  DiagramSymbol,
+  DiagramSymbolInstance,
+  DocumentType,
+  PidDocumentWithDom,
+} from '@cognite/pid-tools';
+
+import usePrevious from '../../utils/usePrevious';
+
+import ParserStorage from './ParserStorage';
+
+const shouldLoadFromStorage = (
+  previousDocumentType: DocumentType,
+  documentType: DocumentType
+) => {
+  return documentType !== DocumentType.unknown;
+};
+
+const shouldSaveToStorage = (
+  previousDocumentType: DocumentType,
+  documentType: DocumentType
+) => {
+  // Skip saving if document type just changed, otherwise we would immediately
+  // overwrite save empty symbols.
+  const didDocumentTypeChangeOnLastRender =
+    previousDocumentType !== documentType;
+
+  return (
+    documentType !== DocumentType.unknown && !didDocumentTypeChangeOnLastRender
+  );
+};
+
+const useSymbolState = (
+  documentType: DocumentType,
+  pidDocument: PidDocumentWithDom | undefined
+) => {
+  const previousDocumentType = usePrevious<DocumentType>(
+    documentType ?? DocumentType.unknown
+  );
+
+  const [symbolInstances, setSymbolInstances] = useState<
+    DiagramSymbolInstance[]
+  >([]);
+  const [symbols, setSymbols] = useState<DiagramSymbol[]>(
+    ParserStorage.symbols.load(documentType)
+  );
+
+  useEffect(() => {
+    if (shouldLoadFromStorage(previousDocumentType, documentType)) {
+      setSymbols(ParserStorage.symbols.load(documentType));
+    }
+  }, [documentType, previousDocumentType]);
+
+  useEffect(() => {
+    if (shouldSaveToStorage(previousDocumentType, documentType)) {
+      ParserStorage.symbols.save(documentType, symbols);
+    }
+  }, [documentType, symbols, previousDocumentType]);
+
+  useEffect(() => {
+    if (pidDocument !== undefined) {
+      setSymbolInstances(computeSymbolInstances(symbols, pidDocument));
+    }
+  }, [symbols, pidDocument]);
+
+  return {
+    symbols,
+    setSymbols,
+    symbolInstances,
+    setSymbolInstances,
+  };
+};
+
+export default useSymbolState;

packages/pid-tools/src/utils/jsonUtils.ts
@@ -167,10 +167,27 @@ export const isValidSymbolFileSchema = (
   return true;
 };
 
+export const computeSymbolInstances = (
+  symbols: DiagramSymbol[],
+  pidDocument: PidDocument
+): DiagramSymbolInstance[] => {
+  let allNewSymbolInstances: DiagramSymbolInstance[] = [];
+  symbols.forEach((symbol) => {
+    const newSymbolInstances = (
+      pidDocument as PidDocument
+    ).findAllInstancesOfSymbol(symbol);
+    allNewSymbolInstances = getNoneOverlappingSymbolInstances(
+      pidDocument,
+      allNewSymbolInstances,
+      newSymbolInstances
+    );
+  });
+  return allNewSymbolInstances;
+};
+
 export const loadSymbolsFromJson = (
   jsonData: GraphDocument,
   setSymbols: (diagramSymbols: DiagramSymbol[]) => void,
-  symbols: DiagramSymbol[],
   pidDocument: PidDocument,
   setSymbolInstances: (diagramSymbolInstances: DiagramSymbolInstance[]) => void,
   symbolInstances: DiagramSymbolInstance[],
@@ -187,21 +204,13 @@ export const loadSymbolsFromJson = (
 ) => {
   if ('symbols' in jsonData) {
     const newSymbols = jsonData.symbols as DiagramSymbol[];
-    setSymbols([...symbols, ...newSymbols]);
+    setSymbols([...newSymbols]);
 
     if (!('symbolInstances' in jsonData)) {
-      let allNewSymbolInstances: DiagramSymbolInstance[] = [];
-      newSymbols.forEach((newSymbol) => {
-        const newSymbolInstances = (
-          pidDocument as PidDocument
-        ).findAllInstancesOfSymbol(newSymbol);
-        allNewSymbolInstances = getNoneOverlappingSymbolInstances(
-          pidDocument,
-          allNewSymbolInstances,
-          newSymbolInstances
-        );
-      });
-      setSymbolInstances([...symbolInstances, ...allNewSymbolInstances]);
+      setSymbolInstances([
+        ...symbolInstances,
+        ...computeSymbolInstances(newSymbols, pidDocument),
+      ]);
     }
   }
   if ('lines' in jsonData) {

packages/react-pid/src/components/side-panel/ParserStorage.ts
@@ -1,0 +1,51 @@
+import { DiagramSymbol, DocumentType } from '@cognite/pid-tools';
+
+const LEGEND_LOCALSTORAGE_KEY_PREFIX = 'PID_TOOL_';
+
+const getLegendKeyByDocumentType = (documentType: DocumentType) => {
+  return `${LEGEND_LOCALSTORAGE_KEY_PREFIX}${documentType}`;
+};
+
+const ParserStorage = {
+  symbols: {
+    load: (documentType: DocumentType): DiagramSymbol[] => {
+      try {
+        const key = getLegendKeyByDocumentType(documentType);
+        const json = window.localStorage.getItem(key);
+
+        if (!json) {
+          // eslint-disable-next-line no-console
+          console.debug(`${key} did not exist in localStorage`);
+          return [];
+        }
+
+        const { symbols }: { symbols: DiagramSymbol[] } = JSON.parse(json);
+
+        if (!symbols) {
+          // eslint-disable-next-line no-console
+          console.warn('Symbols did not exist in localStorage');
+          return [];
+        }
+
+        if (!Array.isArray(symbols)) {
+          throw new Error('Symbols had unexpected type');
+        }
+
+        return symbols;
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(`Could not load legend from localStorage ${error}`);
+        return [];
+      }
+    },
+    save: (documentType: DocumentType, symbols: DiagramSymbol[]): void =>
+      window.localStorage.setItem(
+        getLegendKeyByDocumentType(documentType),
+        JSON.stringify({
+          symbols,
+        })
+      ),
+  },
+};
+
+export default ParserStorage;

packages/react-pid/src/utils/usePrevious.ts
@@ -1,0 +1,11 @@
+import { useRef, useEffect } from 'react';
+
+const usePrevious = <T>(value: T) => {
+  const ref = useRef<T>(value);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export default usePrevious;

commit 29daa6cd75e906a1b1f16bcf00c94c327ad6896b

fix(react-pid): fix auto load (#2032)
Unfortunately, I broke master in #2007 . Seems like the issue was here; https://github.com/cognitedata/applications/pull/2007#discussion_r811720316 

Not sure why it never occurred originally while I was developing? 

This is fixed in this branch by letting CognitePid owning the state for symbols and symbolInstances and simply letting the component keep a copy of that state.

packages/pid-tools/src/cognite-pid.ts
@@ -222,7 +222,7 @@ export class CognitePid {
     this.activeToolSubscriber = callback;
   }
 
-  private setSymbols(symbols: DiagramSymbol[]) {
+  setSymbols(symbols: DiagramSymbol[]) {
     this.symbols = symbols;
     if (this.symbolsSubscriber) {
       this.symbolsSubscriber(symbols);

packages/react-pid/src/ReactPid.tsx
@@ -26,7 +26,7 @@ import { Viewport } from './components/viewport/Viewport';
 import { ReactPidLayout, ReactPidWrapper } from './elements';
 
 export const ReactPid: React.FC = () => {
-  const [, setHasPidViewerLoadedDocument] = useState(false);
+  const [hasDocumentLoaded, setHasDocumentLoaded] = useState(false);
   const pidViewer = useRef<CognitePid>();
 
   const getPidDocument = (): PidDocumentWithDom | undefined => {
@@ -41,7 +41,7 @@ export const ReactPid: React.FC = () => {
     unit: 'Unknown',
   });
   const { symbols, setSymbols, symbolInstances, setSymbolInstances } =
-    useSymbolState(documentMetadata.type, getPidDocument());
+    useSymbolState(pidViewer.current, documentMetadata.type, hasDocumentLoaded);
   const [lines, setLines] = useState<DiagramLineInstance[]>([]);
   const [connections, setConnections] = useState<DiagramConnection[]>([]);
   const [equipmentTags, setEquipmentTags] = useState<
@@ -83,16 +83,8 @@ export const ReactPid: React.FC = () => {
 
   const initPid = (instance: CognitePid) => {
     pidViewer.current = instance;
-    instance.addEventListener(EventType.LOAD, () =>
-      setHasPidViewerLoadedDocument(true)
-    );
+    instance.addEventListener(EventType.LOAD, () => setHasDocumentLoaded(true));
   };
-
-  useEffect(() => {
-    if (pidViewer.current) {
-      pidViewer.current?.setSymbolInstances(symbolInstances);
-    }
-  }, [symbolInstances]);
 
   useEffect(() => {
     if (pidViewer.current) {

packages/react-pid/src/components/side-panel/useSymbolState.ts
@@ -1,69 +1,46 @@
-import { useEffect, useState } from 'react';
 import {
+  CognitePid,
   computeSymbolInstances,
   DiagramSymbol,
   DiagramSymbolInstance,
   DocumentType,
-  PidDocumentWithDom,
 } from '@cognite/pid-tools';
-
-import usePrevious from '../../utils/usePrevious';
+import { useEffect, useState } from 'react';
 
 import ParserStorage from './ParserStorage';
 
-const shouldLoadFromStorage = (
-  previousDocumentType: DocumentType,
-  documentType: DocumentType
-) => {
-  return documentType !== DocumentType.unknown;
-};
-
-const shouldSaveToStorage = (
-  previousDocumentType: DocumentType,
-  documentType: DocumentType
-) => {
-  // Skip saving if document type just changed, otherwise we would immediately
-  // overwrite save empty symbols.
-  const didDocumentTypeChangeOnLastRender =
-    previousDocumentType !== documentType;
-
-  return (
-    documentType !== DocumentType.unknown && !didDocumentTypeChangeOnLastRender
-  );
-};
-
 const useSymbolState = (
+  pidRef: CognitePid | undefined,
   documentType: DocumentType,
-  pidDocument: PidDocumentWithDom | undefined
+  hasDocumentLoaded: boolean
 ) => {
-  const previousDocumentType = usePrevious<DocumentType>(
-    documentType ?? DocumentType.unknown
-  );
+  const [hasLegendBeenLoaded, setHasLegendBeenLoaded] = useState(false);
 
   const [symbolInstances, setSymbolInstances] = useState<
     DiagramSymbolInstance[]
   >([]);
-  const [symbols, setSymbols] = useState<DiagramSymbol[]>(
-    ParserStorage.symbols.load(documentType)
-  );
+
+  const [symbols, setSymbols] = useState<DiagramSymbol[]>([]);
 
   useEffect(() => {
-    if (shouldLoadFromStorage(previousDocumentType, documentType)) {
-      setSymbols(ParserStorage.symbols.load(documentType));
+    const pidDocument = pidRef?.pidDocument;
+    const shouldLoadFromStorage =
+      hasDocumentLoaded && pidDocument !== undefined;
+    if (shouldLoadFromStorage) {
+      const loadedSymbols = ParserStorage.symbols.load(documentType);
+      pidRef?.setSymbols(loadedSymbols);
+      pidRef?.setSymbolInstances(
+        computeSymbolInstances(loadedSymbols, pidDocument)
+      );
+      setHasLegendBeenLoaded(true);
     }
-  }, [documentType, previousDocumentType]);
+  }, [hasDocumentLoaded]);
 
   useEffect(() => {
-    if (shouldSaveToStorage(previousDocumentType, documentType)) {
+    if (hasDocumentLoaded && hasLegendBeenLoaded) {
       ParserStorage.symbols.save(documentType, symbols);
     }
-  }, [documentType, symbols, previousDocumentType]);
-
-  useEffect(() => {
-    if (pidDocument !== undefined) {
-      setSymbolInstances(computeSymbolInstances(symbols, pidDocument));
-    }
-  }, [symbols, pidDocument]);
+  }, [documentType, symbols, hasDocumentLoaded, hasLegendBeenLoaded]);
 
   return {
     symbols,