897998fa39f98bcaa58fcbd1acd50d94a131a99d
Håkon Flatval <hakon.flatval@cognite.com>
2022-02-25 13:06:50 +0100

    test: add test for createRevealManager
    Just because I did a stupid mistake and don't want anyone else to follow in my foot steps

---
+++ viewer/core/src/public/createRevealManager.test.ts
@@ -0,0 +1,25 @@
+/*!
+ * Copyright 2022 Cognite AS
+ */
+
+import { createRevealManager } from './createRevealManager';
+
+import { ModelDataProvider, ModelMetadataProvider } from '@reveal/modeldata-api';
+
+import { Mock } from 'moq.ts';
+
+import * as THREE from 'three';
+
+describe('createRevealManager', () => {
+  test('createRevealManager does not throw on empty internal options', () => {
+    expect(() => createRevealManager(
+      'some-project',
+      'some-application-id',
+      new Mock<ModelMetadataProvider>().object(),
+      new Mock<ModelDataProvider>().object(),
+      new Mock<THREE.WebGLRenderer>().object(),
+      new THREE.Scene(),
+      {}
+    )).not.toThrow();
+  });
+});
