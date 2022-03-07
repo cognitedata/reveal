9f1e47bbd58fc0be01af3347ce8aca5e97ba1734
Milad Mobarhan <milad.mobarhan@cognite.com>
2022-03-07 09:02:41 +0100

    Add keypoint validator

--- src/api/vision/detectionModels/typeValidators.ts
+++ src/api/vision/detectionModels/typeValidators.ts
@@ -1,4 +1,8 @@
-import { TagDetectionJobAnnotation, VisionJobAnnotation } from './types';
+import {
+  GaugeReaderJobAnnotation,
+  TagDetectionJobAnnotation,
+  VisionJobAnnotation,
+} from './types';
 
 export function validBoundingBox(visionJobAnnotation: VisionJobAnnotation) {
   return (
@@ -15,5 +19,22 @@ export function validImageAssetLink(visionJobAnnotation: VisionJobAnnotation) {
   return (
     validBoundingBox(visionJobAnnotation) &&
     !!(visionJobAnnotation as TagDetectionJobAnnotation).assetIds
+  );
+}
+
+export function validKeypointCollection(
+  visionJobAnnotation: VisionJobAnnotation
+) {
+  if (!(visionJobAnnotation as GaugeReaderJobAnnotation).data) {
+    return false;
+  }
+
+  const annotation = visionJobAnnotation as GaugeReaderJobAnnotation;
+  return (
+    !!annotation.region &&
+    annotation.region.shape === 'points' &&
+    annotation.region.vertices.length ===
+      annotation.data.keypoint_names.length &&
+    annotation.region.vertices.every((item) => 'x' in item && 'y' in item)
   );
 }
