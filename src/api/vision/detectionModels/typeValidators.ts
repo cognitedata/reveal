a4dde7314b8e8ba6604b6108c9a0dccbec120420
Milad Mobarhan <milad.mobarhan@cognite.com>
2022-03-08 09:00:38 +0100

    Add normalization check to `validBoundingBox`
    Also check that vertices is an array

--- src/api/vision/detectionModels/typeValidators.ts
+++ src/api/vision/detectionModels/typeValidators.ts
@@ -1,13 +1,22 @@
 import { RegionShape } from 'src/api/annotation/types';
-import { TagDetectionJobAnnotation, VisionJobAnnotation } from './types';
+import {
+  TagDetectionJobAnnotation,
+  Vertex,
+  VisionJobAnnotation,
+} from './types';
+
+export function vertexIsNormalized(vertex: Vertex) {
+  return vertex.x >= 0 && vertex.x <= 1 && vertex.y >= 0 && vertex.y <= 1;
+}
 
 export function validBoundingBox(visionJobAnnotation: VisionJobAnnotation) {
   return (
     !!visionJobAnnotation.region &&
     visionJobAnnotation.region.shape === RegionShape.Rectangle &&
+    Array.isArray(visionJobAnnotation.region.vertices) &&
     visionJobAnnotation.region.vertices.length === 2 &&
     visionJobAnnotation.region.vertices.every(
-      (item) => 'x' in item && 'y' in item
+      (item) => 'x' in item && 'y' in item && vertexIsNormalized(item)
     )
   );
 }
