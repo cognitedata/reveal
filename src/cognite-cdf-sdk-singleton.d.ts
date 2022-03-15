589fff1b959a0f53c8d850f9e6b22db90ea90638
Baris Demirel <baris.demirel@cognite.com>
2022-03-15 16:43:17 +0100

    fix: update IDP type for legacy

--- src/cognite-cdf-sdk-singleton.d.ts
+++ src/cognite-cdf-sdk-singleton.d.ts
@@ -1,7 +1,8 @@
 declare module '@cognite/cdf-sdk-singleton' {
+  import { IDPType } from '@cognite/login-utils';
   import { CogniteClient } from '@cognite/sdk';
 
-  export declare function getFlow(): { flow: string };
+  export declare function getFlow(): { flow: IDPType };
   export declare function getToken(): Promise<string>;
   export declare function loginAndAuthIfNeeded(
     newTenant: string,
