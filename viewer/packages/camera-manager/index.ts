3eb2e1b1d25c4f113b6d47d93dd70e4f25a26a3e
Savelii Novikov <45129444+Savokr@users.noreply.github.com>
2022-03-23 09:40:21 +0100

    feat: Added documentation for camera manager together with a few fixes (#1955)
    Added documentation for creating custom CameraManager. Also, ComboControls are exposed now for DefaultCameraManager. CameraManagerHelper class was added for easier implementation of custom CameraManager.

--- viewer/packages/camera-manager/index.ts
+++ viewer/packages/camera-manager/index.ts
@@ -2,6 +2,8 @@
  * Copyright 2021 Cognite AS
  */
 export { DefaultCameraManager } from './src/DefaultCameraManager';
+export { CameraManagerHelper } from './src/CameraManagerHelper';
 export { CameraManager } from './src/CameraManager';
+export { ComboControls } from './src/ComboControls';
 
 export * from './src/types';
