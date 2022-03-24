144ec461337278ab5ddf9d60205f9a87d5bace0d
Håkon Flatval <hakon.flatval@cognite.com>
2022-03-24 16:03:00 +0100

    fix: weird transformed bounding box computation

--- viewer/packages/pointclouds/src/potree-three-loader/utils/bounds.ts
+++ viewer/packages/pointclouds/src/potree-three-loader/utils/bounds.ts
@@ -6,13 +6,12 @@ import { Box3, Matrix4, Vector3 } from 'three';
 export function computeTransformedBoundingBox(box: Box3, transform: Matrix4): Box3 {
   return new Box3().setFromPoints([
     new Vector3(box.min.x, box.min.y, box.min.z).applyMatrix4(transform),
-    new Vector3(box.min.x, box.min.y, box.min.z).applyMatrix4(transform),
-    new Vector3(box.max.x, box.min.y, box.min.z).applyMatrix4(transform),
-    new Vector3(box.min.x, box.max.y, box.min.z).applyMatrix4(transform),
     new Vector3(box.min.x, box.min.y, box.max.z).applyMatrix4(transform),
+    new Vector3(box.min.x, box.max.y, box.min.z).applyMatrix4(transform),
     new Vector3(box.min.x, box.max.y, box.max.z).applyMatrix4(transform),
-    new Vector3(box.max.x, box.max.y, box.min.z).applyMatrix4(transform),
+    new Vector3(box.max.x, box.min.y, box.min.z).applyMatrix4(transform),
     new Vector3(box.max.x, box.min.y, box.max.z).applyMatrix4(transform),
+    new Vector3(box.max.x, box.max.y, box.min.z).applyMatrix4(transform),
     new Vector3(box.max.x, box.max.y, box.max.z).applyMatrix4(transform)
   ]);
 }
