6d2b91d206e8caf99c817151e3b61f133fa89fbe
Pramod S <pramod.s@cognite.com>
2022-03-26 14:55:07 +0100

    Fix for unit test in finite state

--- viewer/packages/pointclouds/src/PotreeGroupWrapper.test.ts
+++ viewer/packages/pointclouds/src/PotreeGroupWrapper.test.ts
@@ -28,18 +28,7 @@ describe('PotreeGroupWrapper', () => {
     const model = new PotreeNodeWrapper(dummyNode);
     const manager = new PotreeGroupWrapper(pollLoadingStatusInterval);
 
-    expectObservable(
-      manager.getLoadingStateObserver().pipe(map(x => x.isLoading)),
-      [
-        // // Initially reported as not loading
-        // false,
-        // // Load started when model added
-        // true,
-        // Loading flag is reset after a while
-        false
-      ],
-      done
-    );
+    expectObservable(manager.getLoadingStateObserver().pipe(map(x => x.isLoading)), [false], done);
     manager.addPointCloud(model);
   });
 });
