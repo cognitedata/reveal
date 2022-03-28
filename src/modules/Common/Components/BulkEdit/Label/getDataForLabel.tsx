dbcc3dc34280caa69df543c40a3be12fccf3c9ae
Gayan Deegala <37881194+gayanDeegala@users.noreply.github.com>
2022-03-28 21:25:32 +0530

    [VIS-736] Label bulk edit design refactor (#650)
    * refactor original items
    
    * refactor additional items
    
    Co-authored-by: Gayan Deegala <Gayan@CreativeSoftware.com>

--- src/modules/Common/Components/BulkEdit/Label/getDataForLabel.tsx
+++ src/modules/Common/Components/BulkEdit/Label/getDataForLabel.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { ExternalId } from '@cognite/sdk';
 import { Tag } from 'antd';
 import { BulkEditTableDataType } from 'src/modules/Common/Components/BulkEdit/BulkEditTable/BulkEditTable';
 import { BulkEditUnsavedState } from 'src/modules/Common/store/common/types';
 import { VisionFile } from 'src/modules/Common/store/files/types';
 import styled from 'styled-components';
+
+export const getTagForOriginal = ({
+  item,
+  unsavedItems,
+}: {
+  item: ExternalId;
+  unsavedItems: ExternalId[] | undefined;
+}): JSX.Element => {
+  const newItem = item.externalId;
+  if (
+    unsavedItems &&
+    unsavedItems.some((unsavedItem) => unsavedItem.externalId === newItem)
+  ) {
+    return (
+      <Tag key={newItem} color="blue">
+        {newItem}
+      </Tag>
+    );
+  }
+  return <Tag key={newItem}>{newItem}</Tag>;
+};
+
+export const getNewItems = ({
+  unsavedItems,
+  existingItems,
+}: {
+  unsavedItems: ExternalId[];
+  existingItems: ExternalId[] | undefined;
+}): ExternalId[] => {
+  if (existingItems) {
+    return unsavedItems.filter(
+      (unsavedItem) =>
+        !existingItems.some(
+          (existingItem) => existingItem.externalId === unsavedItem.externalId
+        )
+    );
+  }
+  return unsavedItems;
+};
 
 export const getDataForLabel = ({
   selectedFiles,
@@ -13,43 +53,33 @@ export const getDataForLabel = ({
   bulkEditUnsaved: BulkEditUnsavedState;
 }): BulkEditTableDataType[] => {
   return selectedFiles.map((file) => {
-    const { name, id, labels } = file;
-    const newLabels = bulkEditUnsaved.labels;
+    const { name, id, labels: existingLabels } = file;
+    const { labels: unsavedLabels } = bulkEditUnsaved;
     return {
       name,
       id,
-      original: labels ? (
+      original: existingLabels ? (
         <CellContainer>
-          {labels.map((label) =>
-            newLabels?.some(
-              (newLabel) => newLabel.externalId === label.externalId
-            ) ? (
-              <Tag key={label.externalId} color="green">
-                {label.externalId}
-              </Tag>
-            ) : (
-              <Tag key={label.externalId}>{label.externalId}</Tag>
-            )
+          {existingLabels.map((existingLabel) =>
+            getTagForOriginal({
+              item: existingLabel,
+              unsavedItems: unsavedLabels,
+            })
           )}
         </CellContainer>
       ) : (
         <></>
       ),
-      updated: newLabels ? (
+      updated: unsavedLabels ? (
         <CellContainer>
-          {newLabels.map((newLabel) =>
-            labels?.some(
-              (label) => label.externalId === newLabel.externalId
-            ) ? (
-              <Tag key={newLabel.externalId} color="green">
-                {newLabel.externalId}
-              </Tag>
-            ) : (
-              <Tag key={newLabel.externalId} color="blue">
-                {newLabel.externalId}
-              </Tag>
-            )
-          )}
+          {getNewItems({
+            unsavedItems: unsavedLabels,
+            existingItems: existingLabels,
+          }).map((newLabel) => (
+            <Tag key={newLabel.externalId} color="green">
+              {newLabel.externalId}
+            </Tag>
+          ))}
         </CellContainer>
       ) : (
         <></>
