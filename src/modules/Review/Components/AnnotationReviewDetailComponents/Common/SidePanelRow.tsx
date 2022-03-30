61dcfcc1e3dc88e2020fbb49d8fe6330cbb8ff51
Mihil Ranathunga <mihilranathunga@gmail.com>
2022-03-30 10:00:11 +0530

    [VIS-468] Annotation Details enhancements  (#646)
     * create new keypoint and classification annotation categories
     * remove labels for id and external id for tag annotation review
     * hide categories with 0 annotations

--- src/modules/Review/Components/AnnotationReviewDetailComponents/Common/SidePanelRow.tsx
+++ src/modules/Review/Components/AnnotationReviewDetailComponents/Common/SidePanelRow.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const SidePanelRow = styled.div`
   height: 30px;
   border-radius: 4px;
-  padding: 0 0 0 28px;
+  padding: 0 0 0 38px;
   display: flex;
   align-items: center;
 `;
--- src/modules/Review/Components/AnnotationReviewDetailComponents/TagAnnotationReviewRow.tsx
+++ src/modules/Review/Components/AnnotationReviewDetailComponents/TagAnnotationReviewRow.tsx
@@ -3,9 +3,6 @@ import { FileInfo } from '@cognite/sdk';
 import { AnnotationTableItem } from 'src/modules/Review/types';
 import { AnnotationReviewProps } from 'src/modules/Review/Containers/VirtualizedAnnotationsReview';
 import { KeyboardShortCutSelectable } from 'src/modules/Review/Containers/KeyboardShortKeys/KeyboardShortCutSelectable';
-import styled from 'styled-components';
-import { Detail } from '@cognite/cogs.js';
-import { CopyableText } from 'src/modules/FileDetails/Components/FileMetadata/CopyableText';
 import { AssetLinkWarning } from 'src/modules/Review/Components/AnnotationReviewDetailComponents/Common/AssetLinkWarning';
 import { SidePanelRow } from 'src/modules/Review/Components/AnnotationReviewDetailComponents/Common/SidePanelRow';
 import { AnnotationTableRow } from 'src/modules/Review/Components/AnnotationReviewDetailComponents/Common/AnnotationTableRow';
@@ -44,54 +41,7 @@ export const TagAnnotationReviewRow = ({
             showColorCircle
           />
         </SidePanelRow>
-        {annotation.linkedResourceExternalId && (
-          <SidePanelRow>
-            <IDContainer>
-              <AnnotationLbl>
-                <CopyableText
-                  copyable
-                  text={annotation.linkedResourceExternalId}
-                >
-                  <Detail style={{ color: 'inherit', width: 190 }}>
-                    {`External ID: ${annotation.linkedResourceExternalId}`}
-                  </Detail>
-                </CopyableText>
-              </AnnotationLbl>
-            </IDContainer>
-          </SidePanelRow>
-        )}
-        <SidePanelRow>
-          <IDContainer>
-            <AnnotationLbl>
-              <CopyableText copyable text={annotation.linkedResourceId}>
-                <Detail style={{ color: 'inherit', width: 190 }}>
-                  {`Internal ID: ${annotation.linkedResourceId}`}
-                </Detail>
-              </CopyableText>
-            </AnnotationLbl>
-          </IDContainer>
-        </SidePanelRow>
       </KeyboardShortCutSelectable>
     </AssetLinkWarning>
   );
 };
-
-const IDContainer = styled.div`
-  padding-left: 34px;
-`;
-
-type AnnotationLabelOpts = {
-  color?: string;
-};
-
-const AnnotationLbl = styled.div<AnnotationLabelOpts>`
-  width: 100%;
-  height: 100%;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 16px;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  color: ${(props) => props.color || 'inherit'};
-`;
