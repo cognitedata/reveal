3eea31098d6aed08f71e6da5f971cfbadd0ff536
Samir Ajdarpasic <78414491+samir-ajdarpasic@users.noreply.github.com>
2022-03-31 10:06:32 +0200

    fix(node-visualizer): removed missing storybook import (#2304)
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- packages/node-visualizer/stories/StoryWrapper.tsx
+++ packages/node-visualizer/stories/StoryWrapper.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import '@cognite/cogs.js/dist/cogs.css';
-import '@cognite/node-visualizer/node-visualizer.css';
 
 export const StoryWrapper = ({ children }) => {
   return <div style={{ flex: '1' }}>{children}</div>;
