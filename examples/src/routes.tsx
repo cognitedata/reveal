7534a329e008448200964c22e76c4e92402376e5
Savokr <savelii.novikov@cognite.com>
2022-04-04 18:11:55 +0200

    removed newline

--- examples/src/routes.tsx
+++ examples/src/routes.tsx
@@ -14,6 +14,7 @@ import { TwoModels } from './pages/TwoModels';
 import { WalkablePath } from './pages/WalkablePath';
 import { visualTests } from './pages/e2e/visualTests';
 import { CustomDataSource } from './pages/CustomDataSource';
+
 const cadTestBasePath = '/test/cad/';
 const pointcloudTestBasePath = '/test/pointcloud/';
 
