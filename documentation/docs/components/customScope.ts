0b6e13b4b7ed6d37e86be86ca56941e28864ab8a
Pramod S <pramod.s@cognite.com>
2022-04-06 17:20:30 +0200

    Created new ocean simulation

--- documentation/docs/components/customScope.ts
+++ documentation/docs/components/customScope.ts
@@ -3,6 +3,7 @@ import { THREE } from '@cognite/reveal';
 import { Water } from 'three/examples/jsm/objects/Water';
 import { DragControls } from 'three/examples/jsm/controls/DragControls';
 import * as viewerUtils from '../utils/viewerUtilities';
+import { FragmentShader, VertexShader} from '../utils/Ocean';
 
 const reveal = typeof window === 'undefined' ? {} : require('@cognite/reveal');
 const revealTools = typeof window === 'undefined' ? {} : require('@cognite/reveal/tools');
@@ -11,8 +12,8 @@ export const customScope: Record<string, any> = {
   ...viewerUtils,
   ...reveal,
   ...revealTools,
-  THREE,
   Water,
+  THREE,
   DragControls,
   // you can't simply call useBaseUrl here because it's a react hook...
   urls: {
@@ -23,5 +24,9 @@ export const customScope: Record<string, any> = {
     HereMapAPI: 'HqSchC7XT2PA9qCfxzFq',
     HereMapAppCode: '5rob9QcZ70J-m18Er8-rIA',
     MapboxAPI: 'pk.eyJ1IjoicHJhbW9kLXMiLCJhIjoiY2tzb2JkbXdyMGd5cjJubnBrM3IwMTd0OCJ9.jA9US2D2FRXUlldhE8bZgA'
+  },
+  WaterShader: {
+    Vertex: VertexShader,
+    Fragment: FragmentShader
   }
 };
