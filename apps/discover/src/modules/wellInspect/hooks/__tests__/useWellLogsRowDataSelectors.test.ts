1dc6295a2215f677e0322e1ca85c7ec195361e96
Saagar Takhi <86822653+saagar-cognite@users.noreply.github.com>
2022-04-07 08:52:53 +0200

    chore(discover): add useWellLogsRowDataSelectors test (#2272)
    Link to JIRA: https://cognitedata.atlassian.net/browse/PP-2610
    
    # What has changed?
    
    > improve test coverage

---
+++ apps/discover/src/modules/wellInspect/hooks/__tests__/useWellLogsRowDataSelectors.test.ts
@@ -0,0 +1,50 @@
+import 'services/well/__mocks/setupWellsMockSDK';
+import { renderHook } from '@testing-library/react-hooks';
+import { setupServer } from 'msw/node';
+import { getMockDepthMeasurementData } from 'services/well/measurements/__mocks/mockMeasurements';
+
+import { getMockDepthMeasurementDataWellboreOne } from '__test-utils/fixtures/measurements';
+import { testWrapper } from '__test-utils/renderer';
+
+import { useWellLogsRowDataKeyBySource } from '../useWellLogsRowDataSelectors';
+
+const sequenceId = '23';
+const sourceName = 'test-sourceName';
+
+const mockServer = setupServer(
+  getMockDepthMeasurementData(
+    0,
+    getMockDepthMeasurementDataWellboreOne({
+      source: {
+        sequenceExternalId: sequenceId,
+        sourceName,
+      },
+      nextCursor: undefined,
+    })
+  )
+);
+
+describe('useWellLogsRowDataKeyBySource', () => {
+  beforeAll(() => mockServer.listen());
+  afterAll(() => mockServer.close());
+
+  test('should be ok', async () => {
+    const { result, waitFor } = renderHook(
+      () => useWellLogsRowDataKeyBySource([sequenceId]),
+      { wrapper: testWrapper }
+    );
+
+    expect(result.current.data).toEqual({});
+
+    await waitFor(() => result.current.isLoading === false);
+
+    expect(result.current.data[sequenceId]).toEqual(
+      expect.objectContaining({
+        source: {
+          sequenceExternalId: sequenceId,
+          sourceName,
+        },
+      })
+    );
+  });
+});
