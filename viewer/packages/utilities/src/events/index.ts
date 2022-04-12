8e02b75b1dcba47e4ab6167b9efc5a115ecbfe0c
Savelii Novikov <45129444+Savokr@users.noreply.github.com>
2022-04-12 16:47:24 +0200

    feat: Added support for clicked button data for Cognite3DViewer.on('click',...) method (#2045)
    Added clicked button data for `.on('click', ...)` method. Also some refactoring to have only one type for all eventDelegate s.

--- viewer/packages/utilities/src/events/index.ts
+++ viewer/packages/utilities/src/events/index.ts
@@ -4,3 +4,4 @@
 export { clickOrTouchEventOffset } from './clickOrTouchEventOffset';
 export { EventTrigger } from './EventTrigger';
 export { InputHandler, disposeOfAllEventListeners } from './InputHandler';
+export * from './types';
