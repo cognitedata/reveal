1301db9e220b4ed68956506aa7422ea8a6e94acd
Christopher J. Tannum <christopher.tannum@cognite.com>
2022-04-20 15:14:49 +0200

    chore: cherry pick improper type exports and bump to 3.0.1 (#2078)
    * fix: unproper type exports (#2076)
    
    * fix: unproper type exports
    
    * fix: improper type in test
    
    * chore: bump reveal to 3.0.1 (#2077)
    
    * fix: unproper type exports
    
    * fix: improper type in test
    
    * chore: bump to 3.0.1
    
    * chore: update reveal 3.x version

--- viewer/internals.ts
+++ viewer/internals.ts
@@ -6,6 +6,8 @@ export * from './core/internals';
 
 export { CadModelMetadata, SectorMetadata, LevelOfDetail, WantedSector } from './packages/cad-parsers';
 export { SectorCuller } from './packages/cad-geometry-loaders';
+export { PotreeNodeWrapper } from './packages/pointclouds/src/PotreeNodeWrapper';
+export { PotreeGroupWrapper } from './packages/pointclouds/src/PotreeGroupWrapper';
 
 export { CadNode, RenderOptions, defaultRenderOptions } from './packages/rendering';
 export { NodeAppearanceProvider } from './packages/cad-styling';
