a387fca547a28c0eb24d675b0c89a949a62aecf6
Christopher J. Tannum <christopher.tannum@cognite.com>
2022-04-20 12:44:00 +0200

    fix: unproper type exports (#2076)
    * fix: unproper type exports
    
    * fix: improper type in test

--- viewer/packages/model-base/index.ts
+++ viewer/packages/model-base/index.ts
@@ -5,6 +5,4 @@
 export { IntersectInput, LoadingState } from './src/types';
 export { SupportedModelTypes } from './src/SupportedModelTypes';
 export { CogniteModelBase } from './src/CogniteModelBase';
-
-export { NodeIdAndTreeIndexMaps } from './src/NodeIdAndTreeIndexMaps';
 export { MetadataRepository } from './src/MetadataRepository';
