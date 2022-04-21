42f04864b266c76ad00e866fc7e2c2e3b233d317
Thomas F <12623909+Olsworn@users.noreply.github.com>
2022-04-21 17:16:57 +0200

    fix(explorer): use of containers was breaking CD (#2423)
    # What has changed?
    
    Explorer was using the experimental container builds, we are removing it because it was causing problem with deployments in CD

--- apps/react-demo-app/src/index.tsx
+++ apps/react-demo-app/src/index.tsx
@@ -32,6 +32,6 @@ if (process.env.REACT_APP_MIXPANEL_TOKEN) {
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// Unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
