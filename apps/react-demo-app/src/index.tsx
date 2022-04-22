f479d6294f9e94c50decd25cfa19c0323f26051e
Thomas F <12623909+Olsworn@users.noreply.github.com>
2022-04-22 12:19:47 +0200

    fix(vite): build had symlinks (#2434)
    Link to JIRA:
    [FAS-330]
    
    # What has changed?
    
    Publish fas was not working properly because of the new build_vite rule. We ended up with symlinks instead of the files
    
    
    [FAS-330]: https://cognitedata.atlassian.net/browse/FAS-330?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- apps/react-demo-app/src/index.tsx
+++ apps/react-demo-app/src/index.tsx
@@ -33,5 +33,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
