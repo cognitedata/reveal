5322e9408f784c14fd3c00a9ba3bbfd9cc002e18
Pramod S <pramod.s@cognite.com>
2022-04-28 15:14:06 +0200

    Refined the UI in migration for measurement tool

--- examples/src/utils/MeasurementUi.ts
+++ examples/src/utils/MeasurementUi.ts
@@ -1,33 +1,68 @@
 import { Cognite3DViewer } from "@cognite/reveal";
 import { MeasurementLineOptions } from "@cognite/reveal/tools";
 import { MeasurementTool } from "@cognite/reveal/tools";
+import dat from "dat.gui";
 
 export class MeasurementUi {
   private readonly _viewer: Cognite3DViewer;
   private readonly _measurementTool: MeasurementTool;
+  private _gui: dat.GUI;
+  private _guiController: any[];
+
+  private state = {
+    linewidth: 0.02,
+    color: '#00FFFF'
+  };
+
+  private types = {
+    pointDistance: false
+  }
 
   constructor(viewer: Cognite3DViewer, ui: dat.GUI) {
     this._viewer = viewer;
     this._measurementTool = new MeasurementTool(this._viewer);
     this._measurementTool.addMeasurementDistance();
+    this._gui = ui.addFolder('Line Options');
+    this._guiController = [];
+    const addDistanceOptions = this.addDistanceOptions.bind(this);
 
-    const state = {
-      linewidth: 0.02,
-      color: '#00FFFF'
-    };
+    this._gui.add(this.types, 'pointDistance').name('Point To Point Distance').onChange(addDistanceOptions);
+  }
 
-    const gui = ui.addFolder('Line Options');
-    gui.add(state, 'linewidth').name('Line Width').onFinishChange(linewidth => {
-      const options: MeasurementLineOptions = {
-        lineWidth: linewidth
+  addDistanceOptions(enable: boolean) {
+    if (enable && this._guiController.length === 0) {
+      this._guiController.push(this._gui.add(this.state, 'linewidth').name('Line Width').onFinishChange(linewidth => {
+        const options: MeasurementLineOptions = {
+          lineWidth: linewidth
+        }
+        this._measurementTool.setLineOptions(options);
+        this.state.linewidth = linewidth;
+      }));
+      this._guiController.push(this._gui.addColor(this.state, 'color').name('Line Color').onFinishChange(color => {
+        const options: MeasurementLineOptions = {
+          color: color
+        }
+        this._measurementTool.setLineOptions(options);
+        this.state.color = color;
+      }));
+    } else if(!enable && this._guiController.length > 0) {
+      this._guiController.forEach(guiController => {
+        guiController.remove();
+      });
+      this._guiController.splice(0, this._guiController.length)
+    }
+  }
+
+  getController(name: string): dat.GUIController | null {
+    let controller = null;
+    const controllers = this._gui.__controllers;
+    for (let i = 0; i < controllers.length; i++) {
+      let c = controllers[i];
+      if (c.property == name) {
+        controller = c;
+        break;
       }
-      this._measurementTool.setLineOptions(options);
-    });
-    gui.addColor(state, 'color').name('Line Color').onFinishChange(color => {
-      const options: MeasurementLineOptions = {
-        color: color
-      }
-      this._measurementTool.setLineOptions(options);
-    });
+    }
+    return controller;
   }
 }
