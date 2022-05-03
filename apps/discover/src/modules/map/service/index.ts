06e70618cb95bc12b5f38e53b54fcbeeec4c51aa
Saagar Takhi <86822653+saagar-cognite@users.noreply.github.com>
2022-05-03 10:23:51 +0200

    chore(discover): remove spatial playground api usages (#2528)
    Link to JIRA:
    https://cognitedata.atlassian.net/browse/SBS-4861
    
    # What has changed?
    
    > remove direct usage of playground spatial api
    > remove ipn-dev & ipn-prod layers files, project config will be used instead to add layers now.
    
    # Any other notes?
    
    > Need to add a way to update/edit JSON file in project config layers.

--- apps/discover/src/modules/map/service/index.ts
+++ apps/discover/src/modules/map/service/index.ts
@@ -1,33 +1,5 @@
-import {
-  getFields,
-  getDiscoveries,
-  getLicenses,
-  getIdemitsuLicenses,
-  getQuadrants,
-  getBlocks,
-  getWellbores,
-  getStructuralElements,
-  getFacilities,
-  getPipelines,
-  getWellboresExplorationActive,
-  getProspects,
-  getTrajectories,
-} from './geospatial';
 import { getMapContent } from './getMapContent';
 
 export const mapService = {
   getMapContent,
-  getFields,
-  getDiscoveries,
-  getLicenses,
-  getIdemitsuLicenses,
-  getQuadrants,
-  getBlocks,
-  getWellbores,
-  getStructuralElements,
-  getFacilities,
-  getPipelines,
-  getWellboresExplorationActive,
-  getProspects,
-  getTrajectories,
 };
