25195fd3e961e8786908db044025c62d399d1427
Thomas F <12623909+Olsworn@users.noreply.github.com>
2022-05-05 10:57:07 +0200

    fix(vite): sidecar source (#2544)

--- rules/vite/sed_darwin.sh
+++ rules/vite/sed_darwin.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-cp -L -r $1/* $2 && LC_CTYPE=C LANG=C find -L $2/index.html -type f | xargs sed -i '' 's,\/PUBLIC_URL_VALUE,PUBLIC_URL_VALUE,g'
+cp -L -r $1/* $2 && LC_CTYPE=C LANG=C find -L $2/index.html -type f | xargs sed -i '' 's,\/PUBLIC_URL_VALUE/sidecar.js,/sidecar.js,g'
+LC_CTYPE=C LANG=C find -L $2/index.html -type f | xargs sed -i '' 's,\/PUBLIC_URL_VALUE,PUBLIC_URL_VALUE,g'
 LC_CTYPE=C LANG=C find -L $2/assets/*.css -type f | xargs sed -i '' 's,\/PUBLIC_URL_VALUE,PUBLIC_URL_VALUE,g'
--- rules/vite/sed_linux.sh
+++ rules/vite/sed_linux.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-cp -L -r $1/* $2 && find -L $2/index.html -type f | xargs sed -i 's,\/PUBLIC_URL_VALUE,PUBLIC_URL_VALUE,g'
+cp -L -r $1/* $2 && find -L $2/index.html -type f | xargs sed -i 's,\/PUBLIC_URL_VALUE/sidecar.js,/sidecar.js,g'
+find -L $2/index.html -type f | xargs sed -i 's,\/PUBLIC_URL_VALUE,PUBLIC_URL_VALUE,g'
 find -L $2/assets/*.css -type f | xargs sed -i 's,\/PUBLIC_URL_VALUE,PUBLIC_URL_VALUE,g'
