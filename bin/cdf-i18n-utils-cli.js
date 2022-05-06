110e260959df6312c2e050d2b11ffa47a0bd6cbe
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2022-05-06 09:21:20 +0200

    feat: add save-missing script

--- bin/cdf-i18n-utils-cli.js
+++ bin/cdf-i18n-utils-cli.js
@@ -6,6 +6,7 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
 const pull = require('./pull');
+const saveMissing = require('./save-missing');
 
 function main() {
   yargs(hideBin(process.argv))
@@ -42,6 +43,53 @@ function main() {
       },
       (argv) => {
         pull({
+          projectId: argv.projectId,
+          namespace: argv.namespace,
+          version: argv.version,
+          path: argv.path,
+        });
+      }
+    )
+    .command(
+      'save-missing [apiKey] [projectId] [namespace] [version] [path]',
+      'saves missing keys to locize from your repository (does not update the existing values in locize)',
+      (yargs) => {
+        return yargs
+          .env('LOCIZE')
+          .option('apiKey', {
+            alias: 'k',
+            describe: 'The api key to use for operations',
+            type: 'string',
+            demandOption: true,
+          })
+          .option('projectId', {
+            alias: 'i',
+            describe: 'The project id to use for operations',
+            default: '0f0363e6-4ef6-49cf-8f1b-e0d993b4b828',
+            type: 'string',
+          })
+          .option('namespace', {
+            alias: 'n',
+            describe: 'The namespace to target for translations',
+            type: 'string',
+            demandOption: true,
+          })
+          .option('version', {
+            alias: 'v',
+            describe: 'The version to target for translations',
+            default: 'staging',
+            type: 'string',
+          })
+          .option('path', {
+            alias: 'p',
+            describe: 'The path to download translations',
+            default: './src/common/i18n/translations',
+            type: 'string',
+          });
+      },
+      (argv) => {
+        saveMissing({
+          apiKey: argv.apiKey,
           projectId: argv.projectId,
           namespace: argv.namespace,
           version: argv.version,
