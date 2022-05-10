0d0053646f8b4236be4d225971bb88cc3af23490
Samir Ajdarpasic <78414491+samir-ajdarpasic@users.noreply.github.com>
2022-05-10 19:00:14 +0200

    feat(discover): render treemap component as html (#2589)
    # What has changed?
    
    the component was rendering as SVG now it's HTML

--- apps/discover/src/components/Treemap/Treemap.tsx
+++ apps/discover/src/components/Treemap/Treemap.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 
 import { select, hierarchy, treemap } from 'd3';
 
-import { useDebounce } from '../../hooks/useDebounce';
+import { useDebounce } from 'hooks/useDebounce';
 
 import { TreemapWrapper } from './elements';
 
@@ -20,66 +20,51 @@ export interface Props {
 }
 
 export const Treemap: React.FC<Props> = ({ data, onTileClicked }) => {
-  const svgRef = useRef<SVGSVGElement>(null);
-  let treemapRoot;
+  const divRef = useRef<HTMLDivElement>(null);
 
-  function renderTreemap() {
-    const svg = select(svgRef.current);
+  const renderTreemap = () => {
+    const rootDiv = select(divRef.current);
 
     const root = hierarchy(data)
-      .sum((d) => d.value || 0)
-      .sort((a, b) => (b.data.value || 0) - (a.data.value || 0));
+      .sum((data) => data.value || 0)
+      .sort(
+        (node1, node2) => (node2.data.value || 0) - (node1.data.value || 0)
+      );
 
-    treemapRoot = treemap<TreeMapData>()
+    const treemapRoot = treemap<TreeMapData>()
       .size([
-        svgRef?.current?.width.baseVal.value || 0,
-        svgRef?.current?.height.baseVal.value || 0,
+        rootDiv.node()?.getBoundingClientRect().width || 0,
+        rootDiv.node()?.getBoundingClientRect().height || 0,
       ])
       .padding(4)(root);
 
-    const nodes = svg
-      .selectAll('g')
+    rootDiv.selectAll('.node').remove();
+
+    const nodes = rootDiv
+      .selectAll('.node')
       .data(treemapRoot.leaves())
-      .join('g')
-      .attr('transform', (d) => `translate(${d.x0},${d.y0})`);
+      .enter()
+      .append('div')
+      .on('click', (_event, node) => onTileClicked(node.data))
+      .attr('class', 'node rect')
+      .style('left', (node) => `${node.x0}px`)
+      .style('top', (node) => `${node.y0}px`)
+      .style('width', (node) => `${node.x1 - node.x0}px`)
+      .style('height', (node) => `${node.y1 - node.y0}px`)
+      .style('position', 'absolute');
 
-    // this is used for dynamic coloring of the chart
-    // const fader = (color: string | d3.ColorCommonInstance) =>
-    //   d3.interpolateRgb(color, '#fff')(0.3);
-    // const colorScale = d3.scaleOrdinal(d3.schemeCategory10.map(fader));
-
-    nodes.selectAll('rect').remove();
-    nodes.selectAll('text').remove();
-
+    // // title
     nodes
-      .append('rect')
-      .on('click', (e, d) => {
-        onTileClicked(d.data);
-      })
-      .attr('class', 'rect')
-      .attr('width', (d) => d.x1 - d.x0)
-      .attr('height', (d) => d.y1 - d.y0)
-      .attr('rx', 4);
-
-    // title
-    nodes
-      .append('text')
-      .text((d) => `${d.data?.title || ''}`)
-      .attr('class', 'title-text')
-      .attr('x', 24)
-      .attr('y', 30)
-      // .attr('textLength', '100%')
-      .attr('lengthAdjust', 'spacing');
+      .append('p')
+      .text((node) => `${node.data?.title || ''}`)
+      .attr('class', 'title-text');
 
     // description
-
     nodes
-      .append('text')
-      .text((d) => `${d.data?.description || ''}`)
-      .attr('class', 'description-text')
-      .attr('x', 24)
-      .attr('y', 50);
-  }
+      .append('p')
+      .text((node) => `${node.data?.description || ''}`)
+      .attr('class', 'description-text');
+  };
 
   useEffect(() => {
     renderTreemap();
@@ -97,9 +82,5 @@ export const Treemap: React.FC<Props> = ({ data, onTileClicked }) => {
     };
   }, []);
 
-  return (
-    <TreemapWrapper>
-      <svg ref={svgRef} height="100%" width="100%" />
-    </TreemapWrapper>
-  );
+  return <TreemapWrapper ref={divRef} />;
 };
--- apps/discover/src/components/Treemap/elements.ts
+++ apps/discover/src/components/Treemap/elements.ts
@@ -1,25 +1,40 @@
 import styled from 'styled-components/macro';
 
+import { sizes } from 'styles/layout';
+
 export const TreemapWrapper = styled.div`
   width: 100%;
   height: 100%;
+  position: relative;
 
   .rect {
-    fill: var(--cogs-surface--status-neutral--muted--default);
+    background: var(--cogs-surface--status-neutral--muted--default);
+    border-radius: 4px;
 
     &:hover {
-      fill: var(--cogs-surface--status-neutral--muted--hover);
+      background: var(--cogs-surface--status-neutral--muted--hover);
     }
   }
 
   .title-text {
     font-size: 16px;
     font-weight: bold;
-    fill: var(--cogs-text-icon--status-neutral);
+    color: var(--cogs-text-icon--status-neutral);
+    padding-top: ${sizes.normal};
+    padding-left: ${sizes.normal};
+    margin-bottom: ${sizes.extraSmall} !important;
+    line-height: 16px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 
   .description-text {
     font-size: 14px;
-    fill: var(--cogs-text-icon--status-neutral);
+    color: var(--cogs-text-icon--status-neutral);
+    padding-left: ${sizes.normal};
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 `;
