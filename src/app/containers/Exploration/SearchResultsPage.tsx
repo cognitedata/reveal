5f1d3fcf25742f736509f93e1205a700cffe5c3d
Shubham Singh <shubham88singh@gmail.com>
2022-05-24 14:43:55 +0200

    Persisting filter in the URL string (#665)
    * Adding the filter in the form of json in the url
    
    * Removing the filterquery in the props
    
    * Fixed the issue after the PR
    review
    
    * Removing the function dependencies from the useEffect hook
    
    * Bumping up the version
    
    Co-authored-by: Shubham Singh <shubham.singh@cognite.com>

--- src/app/containers/Exploration/SearchResultsPage.tsx
+++ src/app/containers/Exploration/SearchResultsPage.tsx
@@ -27,7 +27,7 @@ import {
   useCurrentResourceType,
   useCurrentResourceId,
 } from 'app/hooks';
-import { SEARCH_KEY, CART_KEY } from 'app/utils/constants';
+import { SEARCH_KEY, CART_KEY, FILTER } from 'app/utils/constants';
 import SelectedResults from 'app/components/SelectionResults/SelectionResults';
 import { ExplorationSearchBar } from 'app/containers/Exploration/ExplorationSearchBar';
 import { useDateRange } from 'app/context/DateRangeContext';
@@ -77,6 +77,92 @@ function SearchPage() {
     setFileFilter,
     mode,
   } = useContext(ResourceSelectionContext);
+
+  const [filterQuery, setFilterQuery] = useQueryString(FILTER);
+
+  // Stringify the JSON file in order to ad in search query
+  const assetsFilterStringify = JSON.stringify(assetFilter);
+  const eventsFilterStringify = JSON.stringify(eventFilter);
+  const timeseriesFilterStringify = JSON.stringify(timeseriesFilter);
+  const fileFilterStringify = JSON.stringify(fileFilter);
+  const sequenceFilterStringify = JSON.stringify(sequenceFilter);
+
+  // Checking the length of filter to avoid the unneccesarry use of useEffect
+  const isEventFilterApplied = Object.keys(eventFilter).length > 0;
+  const isAssetFilterApplied = Object.keys(assetFilter).length > 0;
+  const isTimeseriesFilterApplied = Object.keys(timeseriesFilter).length > 0;
+  const isFileFilterApplied = Object.keys(fileFilter).length > 0;
+  const isSequenceFilterApplied = Object.keys(sequenceFilter).length > 0;
+
+  useEffect(() => {
+    if (currentResourceType === 'asset' && isAssetFilterApplied) {
+      setFilterQuery(assetsFilterStringify);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAssetFilterApplied, assetsFilterStringify, currentResourceType]);
+
+  useEffect(() => {
+    if (currentResourceType === 'event' && isEventFilterApplied) {
+      setFilterQuery(eventsFilterStringify);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEventFilterApplied, eventsFilterStringify, currentResourceType]);
+
+  useEffect(() => {
+    if (currentResourceType === 'timeSeries' && isTimeseriesFilterApplied) {
+      setFilterQuery(timeseriesFilterStringify);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    isTimeseriesFilterApplied,
+    timeseriesFilterStringify,
+    currentResourceType,
+  ]);
+
+  useEffect(() => {
+    if (currentResourceType === 'file' && isFileFilterApplied) {
+      setFilterQuery(fileFilterStringify);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    isFileFilterApplied,
+    fileFilterStringify,
+    currentResourceType,
+    fileFilter,
+  ]);
+
+  useEffect(() => {
+    if (currentResourceType === 'sequence' && isSequenceFilterApplied) {
+      setFilterQuery(sequenceFilterStringify);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSequenceFilterApplied, sequenceFilterStringify, currentResourceType]);
+
+  // Applying the filter from the url on component mount
+  useEffect(() => {
+    const filters = filterQuery.length > 0 && JSON.parse(filterQuery);
+    const isUrlFilterAvailable = filters || Object.keys(filters).length > 0;
+    if (isUrlFilterAvailable) {
+      switch (currentResourceType) {
+        case 'asset':
+          setAssetFilter(filters);
+          break;
+        case 'event':
+          setEventFilter(filters);
+          break;
+        case 'file':
+          setFileFilter(filters);
+          break;
+        case 'sequence':
+          setSequenceFilter(filters);
+          break;
+        case 'timeSeries':
+          setTimeseriesFilter(filters);
+          break;
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onSelect = (item: ResourceItem) => {
     const newCart = cart.includes(item.id)
--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,2 +1,3 @@
 export const SEARCH_KEY = 'q';
 export const CART_KEY = 'cart';
+export const FILTER = 'filter';
