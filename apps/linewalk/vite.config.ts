48590481a94ab3c7ba864d67db15fc818d16a0db
Thomas F <12623909+Olsworn@users.noreply.github.com>
2022-05-30 11:07:46 +0200

    chore(linewalk): remove react-scripts in favor of vite (#2384)
    Link to JIRA:
    [FAS-328]
    
    # What has changed?
    
    We are removing react-scripts from linewalk, and using vite instead
    
    
    [FAS-328]: https://cognitedata.atlassian.net/browse/FAS-328?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ apps/linewalk/vite.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import macrosPlugin from 'vite-plugin-babel-macros';
+
+export default defineConfig(({ command }) => {
+  let env = {};
+  const baseConfig: Record<string, any> = {};
+  if (command === 'serve') {
+    baseConfig.server = {
+      fs: {
+        allow: ['../..'],
+      },
+    };
+    env = {
+      NODE_ENV: 'development',
+      ...loadEnv('development', process.cwd(), 'REACT_APP_'),
+      ...loadEnv('development', process.cwd(), 'PUBLIC_URL'),
+    };
+  } else {
+    env = {
+      NODE_ENV: 'production',
+      ...loadEnv('production', process.cwd(), 'REACT_APP_'),
+      ...loadEnv('production', process.cwd(), 'PUBLIC_URL'),
+    };
+  }
+  return {
+    ...baseConfig,
+    plugins: [
+      react(),
+      tsConfigPaths({
+        root: `${process.cwd()}/../..`,
+        projects: ['tsconfig.json', 'apps/linewalk/tsconfig.json'],
+      }),
+      svgr(),
+      macrosPlugin(),
+    ],
+    resolve: {
+      preserveSymlinks: true,
+    },
+    base: command === 'build' ? '/PUBLIC_URL_VALUE' : '/',
+    define: {
+      'process.env': env,
+    },
+    build: {
+      sourcemap: command === 'build',
+      commonjsOptions: {
+        include: [],
+      },
+      rollupOptions: { treeshake: false },
+    },
+  };
+});
