2788a005acad73a42b84f4bc5670865fb96567f1
Mihil Ranathunga <mihilranathunga@gmail.com>
2022-06-08 13:22:12 +0530

    fix playground auth issue

--- src/api/annotation/CognitePlaygroundClient.ts
+++ src/api/annotation/CognitePlaygroundClient.ts
@@ -2,10 +2,8 @@ import { CogniteClientPlayground } from '@cognite/sdk-playground';
 import { getProject } from '@cognite/cdf-utilities';
 import sdk, { getToken } from '@cognite/cdf-sdk-singleton';
 
-const subAppName = 'cdf-vision-subapp';
-
 export const cognitePlaygroundClient = new CogniteClientPlayground({
-  appId: subAppName,
+  appId: sdk.project,
   baseUrl: sdk.getBaseUrl(),
   project: getProject(),
   getToken: () => getToken(),
