b3a15be196cdf7ef0874b270ded05703617921b0
Håkon Flatval <hakon.flatval@cognite.com>
2022-06-21 09:37:47 +0200

    feat: migrate examples to webpack-5

--- examples/config-overrides.js
+++ examples/config-overrides.js
@@ -1,3 +1,5 @@
+const { override } = require('customize-cra');
+
 module.exports = {
   devServer: function (configFunction) {
     return function (proxy, allowedHost) {
@@ -5,11 +7,35 @@ module.exports = {
 
       // Set loose allow origin header to prevent CORS issues (used for local-cdn case)
       config.headers = { 'Access-Control-Allow-Origin': '*' };
-
-      // Add some delay to aggregate file changes and post build scripts
-      config.watchOptions.aggregateTimeout = 2000;
-
       return config;
     };
   },
+  watchOptions: {
+    aggregateTimeout: 2000
+  }
 };
+
+const addIgnoreSourcemapsloaderWarnings = config => {
+  config.ignoreWarnings = [
+    // Ignore warnings raised by source-map-loader.
+    // some third party packages may ship miss-configured sourcemaps, that interrupts the build
+    // See: https://github.com/facebook/create-react-app/discussions/11278#discussioncomment-1780169
+    /**
+     * @param {import('webpack').WebpackError} warning
+     * @returns {boolean}
+     */
+    function ignoreSourcemapsloaderWarnings(warning) {
+      return (
+        warning.module &&
+        warning.module.resource.includes('node_modules') &&
+        warning.details &&
+        warning.details.includes('source-map-loader')
+      );
+    },
+  ];
+  return config;
+};
+
+module.exports = override(
+  addIgnoreSourcemapsloaderWarnings
+);
