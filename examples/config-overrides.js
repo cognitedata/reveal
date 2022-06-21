f96d4c9b07263cd6ad098a73cd8776b37bb1d7fe
Håkon Flatval <hakon.flatval@cognite.com>
2022-06-21 11:47:55 +0200

    improvement: simplify and de-deprecate config-overrides

--- examples/config-overrides.js
+++ examples/config-overrides.js
@@ -5,37 +5,24 @@ module.exports = {
     return function (proxy, allowedHost) {
       const config = configFunction(proxy, allowedHost);
 
+      // Removes deprecation warning
+      config.server = 'https';
+      delete config.https;
+
+      // Removes deprecation warning
+      config.setupMiddlewares = (middleWares) => middleWares;
+      delete config.onAfterSetupMiddleware;
+      delete config.onBeforeSetupMiddleware;
+
       // Set loose allow origin header to prevent CORS issues (used for local-cdn case)
       config.headers = { 'Access-Control-Allow-Origin': '*' };
       return config;
     };
   },
+  webpack: function (config) {
+    return { ...config, ignoreWarnings: [/Failed to parse source map/] }
+  },
   watchOptions: {
-    aggregateTimeout: 2000
+    aggregateTimeout: 2000,
   }
 };
-
-const addIgnoreSourcemapsloaderWarnings = config => {
-  config.ignoreWarnings = [
-    // Ignore warnings raised by source-map-loader.
-    // some third party packages may ship miss-configured sourcemaps, that interrupts the build
-    // See: https://github.com/facebook/create-react-app/discussions/11278#discussioncomment-1780169
-    /**
-     * @param {import('webpack').WebpackError} warning
-     * @returns {boolean}
-     */
-    function ignoreSourcemapsloaderWarnings(warning) {
-      return (
-        warning.module &&
-        warning.module.resource.includes('node_modules') &&
-        warning.details &&
-        warning.details.includes('source-map-loader')
-      );
-    },
-  ];
-  return config;
-};
-
-module.exports = override(
-  addIgnoreSourcemapsloaderWarnings
-);
