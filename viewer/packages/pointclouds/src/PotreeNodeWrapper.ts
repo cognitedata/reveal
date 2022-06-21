64d7c46793475ce165dfef9dc2dc7f3b925dc8c4
Savokr <savelii.novikov@cognite.com>
2022-06-21 14:19:17 +0200

    lint

--- viewer/packages/pointclouds/src/PotreeNodeWrapper.ts
+++ viewer/packages/pointclouds/src/PotreeNodeWrapper.ts
@@ -85,7 +85,7 @@ export class PotreeNodeWrapper {
   pick(renderer: THREE.WebGLRenderer, camera: THREE.Camera, ray: THREE.Ray): PickPoint | null {
     return this.octree.pick(renderer, camera, ray, { pickWindowSize: 20 });
   }
-  
+
   get stylableObjects(): RawStylableObject[] {
     return this._stylableObjects;
   }
