2469e831435e6fd327b7ad9115581f418bc86f38
Håkon Flatval <hakon.flatval@cognite.com>
2022-06-21 12:36:26 +0200

    fix: correct watchOptions.ignored field

--- viewer/webpack.config.js
+++ viewer/webpack.config.js
@@ -116,7 +116,7 @@ module.exports = env => {
     devtool: development ? 'inline-source-map' : 'source-map',
     watchOptions: {
       aggregateTimeout: 1500,
-      ignored: [/node_modules/]
+      ignored: /node_modules/
     },
     optimization: {
       usedExports: true
