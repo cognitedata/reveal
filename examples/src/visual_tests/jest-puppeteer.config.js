a8a678cb53ce58a7564edd22c1812c66d7ecc3e7
Håkon Flatval <hakon.flatval@cognite.com>
2022-06-23 15:53:08 +0200

    fix: use better OpenGL backend in puppeteer

--- examples/src/visual_tests/jest-puppeteer.config.js
+++ examples/src/visual_tests/jest-puppeteer.config.js
@@ -2,7 +2,11 @@ module.exports = {
   launch: {
     ignoreHTTPSErrors: true,
     // https://peter.sh/experiments/chromium-command-line-switches/
-    args: ['--allow-insecure-localhost', '--window-size=800,600', '--no-sandbox'],
+      args: ['--allow-insecure-localhost',
+             '--window-size=800,600',
+             '--no-sandbox',
+             '--use-gl=angle'
+            ],
     headless: true,
     defaultViewport: {
       width: 1280,
