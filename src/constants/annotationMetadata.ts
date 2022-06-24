fd72e3ed9492e2d001c2e41071e46d93e17a3261
Milad Mobarhan <43236572+miladcognite@users.noreply.github.com>
2022-06-24 17:33:09 +0200

    Revert "Temporarily chagne creating app name (#801)" (#804)
    This reverts commit 13c54a65b848a20bad5b7b22be198c9c4edc1794.

--- src/constants/annotationMetadata.ts
+++ src/constants/annotationMetadata.ts
@@ -1,5 +1,5 @@
 import { AnnotatedResourceType } from '@cognite/sdk-playground';
 
-export const CREATING_APP = 'Fusion: Vision (next-release)';
+export const CREATING_APP = 'Fusion: Vision';
 export const CREATING_APP_VERSION = '0.0.1';
 export const ANNOTATED_RESOURCE_TYPE = 'file' as AnnotatedResourceType;
