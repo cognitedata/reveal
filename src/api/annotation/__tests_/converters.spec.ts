0e15837f98f9cbe38fa58e987d64c4be0b66ee72
Gayan Deegala <Gayan@CreativeSoftware.com>
2022-07-06 15:58:43 +0530

    new tests were added

--- src/api/annotation/__tests_/converters.spec.ts
+++ src/api/annotation/__tests_/converters.spec.ts
@@ -3,6 +3,7 @@ import {
   CDFAnnotationV1,
   ImageAssetLink,
   ImageClassification,
+  ImageExtractedText,
   ImageKeypointCollection,
   ImageObjectDetectionBoundingBox,
   ImageObjectDetectionPolygon,
@@ -11,6 +12,7 @@ import {
   Status,
 } from 'src/api/annotation/types';
 import {
+  convertCDFAnnotationToVisionAnnotations,
   convertCDFAnnotationV1StatusToStatus,
   convertCDFAnnotationV1ToImageAssetLink,
   convertCDFAnnotationV1ToImageClassification,
@@ -27,6 +29,7 @@ import {
   KeypointItem,
 } from 'src/utils/AnnotationUtilsV1/AnnotationUtilsV1';
 import {
+  VisionAnnotation,
   VisionImageAssetLinkAnnotation,
   VisionImageExtractedTextAnnotation,
   VisionImageKeypointCollectionAnnotation,
@@ -34,6 +37,13 @@ import {
 } from 'src/modules/Common/types';
 import { getDummyAnnotation } from 'src/__test-utils/annotations';
 import { VisionDetectionModelType } from 'src/api/vision/detectionModels/types';
+import {
+  invalidCDFAnnotation1,
+  sampleCDFAnnotation5,
+  sampleCDFAnnotation6,
+  sampleCDFAnnotation7,
+  sampleCDFAnnotations,
+} from 'src/__test-utils/annotationsCDF';
 
 describe('Test convertCDFAnnotationV1ToImageClassification', () => {
   test('Missing text', () => {
@@ -554,5 +564,176 @@ describe('Test convertCDFAnnotationV1ToVisionAnnotation', () => {
         vertices: cdfAnnotationWithPolyline.region?.vertices,
       },
     } as VisionImageObjectDetectionAnnotation);
+  });
+});
+
+describe('Test convertCDFAnnotationToVisionAnnotations fn', () => {
+  test('for empty array', () => {
+    expect(convertCDFAnnotationToVisionAnnotations([])).toStrictEqual([]);
+  });
+
+  test('annotation with invalid annotation type should ignored', () => {
+    expect(
+      convertCDFAnnotationToVisionAnnotations([invalidCDFAnnotation1])
+    ).toStrictEqual([]);
+  });
+
+  describe('for CDF annotations', () => {
+    const visionAnnotations =
+      convertCDFAnnotationToVisionAnnotations(sampleCDFAnnotations);
+
+    describe('selected properties should copy as it is and should get time from Dates', () => {
+      for (let i = 0; i < 10; i++) {
+        test(`sampleCDFAnnotations-${i}`, () => {
+          expect(visionAnnotations[i].id).toStrictEqual(
+            sampleCDFAnnotations[i].id
+          );
+          expect(visionAnnotations[i].createdTime).toStrictEqual(
+            sampleCDFAnnotations[i].createdTime.getTime()
+          );
+          expect(visionAnnotations[i].lastUpdatedTime).toStrictEqual(
+            sampleCDFAnnotations[i].lastUpdatedTime.getTime()
+          );
+          expect(visionAnnotations[i].status).toStrictEqual(
+            sampleCDFAnnotations[i].status
+          );
+          expect(visionAnnotations[i].annotatedResourceId).toStrictEqual(
+            sampleCDFAnnotations[i].annotatedResourceId
+          );
+          expect(visionAnnotations[i].annotationType).toStrictEqual(
+            sampleCDFAnnotations[i].annotationType
+          );
+        });
+      }
+    });
+
+    describe('testing the conversion of data', () => {
+      describe('images.TextRegion annotation', () => {
+        for (let i = 0; i < 4; i++) {
+          test(`TextRegion annotation - sample-${i + 1}`, () => {
+            const convertedAnnotation = visionAnnotations[
+              i
+            ] as VisionAnnotation<ImageExtractedText>;
+
+            expect({
+              confidence: convertedAnnotation.confidence,
+              text: convertedAnnotation.text,
+              textRegion: convertedAnnotation.textRegion,
+            }).toStrictEqual(sampleCDFAnnotations[i].data);
+          });
+        }
+      });
+
+      describe('images.KeypointCollection annotation', () => {
+        test('KeypointCollection annotation - sample-5', () => {
+          const convertedAnnotation =
+            visionAnnotations[4] as VisionAnnotation<ImageKeypointCollection>;
+          expect(convertedAnnotation.label).toStrictEqual(
+            sampleCDFAnnotation5.data.label
+          );
+          expect(convertedAnnotation.keypoints).toStrictEqual([
+            {
+              label: 'center',
+              point: sampleCDFAnnotation5.data.keypoints.center.point,
+              confidence: undefined,
+            },
+            {
+              label: 'start',
+              point: sampleCDFAnnotation5.data.keypoints.start.point,
+              confidence: undefined,
+            },
+            {
+              label: 'stop',
+              point: sampleCDFAnnotation5.data.keypoints.stop.point,
+              confidence: undefined,
+            },
+            {
+              label: 'tip',
+              point: sampleCDFAnnotation5.data.keypoints.tip.point,
+              confidence: undefined,
+            },
+          ]);
+        });
+
+        test('KeypointCollection annotation - sample-6', () => {
+          const convertedAnnotation =
+            visionAnnotations[5] as VisionAnnotation<ImageKeypointCollection>;
+          expect(convertedAnnotation.label).toStrictEqual(
+            sampleCDFAnnotation6.data.label
+          );
+          expect(convertedAnnotation.keypoints).toStrictEqual([
+            {
+              label: 'point 1',
+              point: sampleCDFAnnotation6.data.keypoints['point 1'].point,
+              confidence:
+                sampleCDFAnnotation6.data.keypoints['point 1'].confidence,
+            },
+            {
+              label: 'point 2',
+              point: sampleCDFAnnotation6.data.keypoints['point 2'].point,
+              confidence:
+                sampleCDFAnnotation6.data.keypoints['point 2'].confidence,
+            },
+            {
+              label: 'point 3',
+              point: sampleCDFAnnotation6.data.keypoints['point 3'].point,
+              confidence:
+                sampleCDFAnnotation6.data.keypoints['point 3'].confidence,
+            },
+          ]);
+        });
+
+        test('KeypointCollection annotation - sample-7', () => {
+          const convertedAnnotation =
+            visionAnnotations[6] as VisionAnnotation<ImageKeypointCollection>;
+          expect(convertedAnnotation.label).toStrictEqual(
+            sampleCDFAnnotation7.data.label
+          );
+          expect(convertedAnnotation.keypoints).toStrictEqual([
+            {
+              label: '1',
+              point: sampleCDFAnnotation7.data.keypoints['1'].point,
+              confidence: sampleCDFAnnotation7.data.keypoints['1'].confidence,
+            },
+          ]);
+        });
+      });
+
+      describe('images.ObjectDetection annotation', () => {
+        test('ObjectDetection annotation - sample-8', () => {
+          const convertedAnnotation =
+            visionAnnotations[7] as VisionAnnotation<ImageObjectDetectionBoundingBox>;
+
+          expect({
+            label: convertedAnnotation.label,
+            boundingBox: convertedAnnotation.boundingBox,
+          }).toStrictEqual(sampleCDFAnnotations[7].data);
+        });
+
+        test('ObjectDetection annotation - sample-9', () => {
+          const convertedAnnotation =
+            visionAnnotations[8] as VisionAnnotation<ImageObjectDetectionPolygon>;
+
+          expect({
+            label: convertedAnnotation.label,
+            polygon: convertedAnnotation.polygon,
+          }).toStrictEqual(sampleCDFAnnotations[8].data);
+        });
+      });
+
+      describe('images.AssetLink annotation', () => {
+        test('AssetLink annotation - sample-10', () => {
+          const convertedAnnotation =
+            visionAnnotations[9] as VisionAnnotation<ImageAssetLink>;
+
+          expect({
+            text: convertedAnnotation.text,
+            confidence: convertedAnnotation.confidence,
+            assetRef: convertedAnnotation.assetRef,
+            textRegion: convertedAnnotation.textRegion,
+          }).toStrictEqual(sampleCDFAnnotations[9].data);
+        });
+      });
+    });
   });
 });
