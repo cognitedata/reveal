32dab928bf2b1ac43c8d9369e6d9f3bb4222b5bf
Håkon Flatval <hakon.flatval@cognite.com>
2022-07-07 15:17:10 +0200

    feat: use bounding volume hierarchy for point-object assignment

--- viewer/packages/pointclouds/src/potree-three-loader/workers/assignObjects.ts
+++ viewer/packages/pointclouds/src/potree-three-loader/workers/assignObjects.ts
@@ -6,11 +6,31 @@ import { StylableObject } from '../../styling/StylableObject';
 
 import * as THREE from 'three';
 
+import { BvhElement } from '../../bvh/BvhElement';
+import { BoundingVolumeHierarchy } from '../../bvh/BoundingVolumeHierarchy';
+
+class StylableObjectBvhElement implements BvhElement {
+  _cachedBox: THREE.Box3 | undefined;
+
+  constructor(public stylableObject: StylableObject) {}
+
+  getBox(): THREE.Box3 {
+    if (!this._cachedBox) {
+      this._cachedBox = this.stylableObject.shape.createBoundingBox();
+    }
+
+    return this._cachedBox!;
+  }
+}
+
 export function computeObjectIdBuffer(
   positionBuffer: Float32Array,
   objectList: StylableObject[],
   pointOffset: THREE.Vector3
 ): ArrayBuffer {
+  const bvhElements = objectList.map(obj => new StylableObjectBvhElement(obj));
+  const bvh = new BoundingVolumeHierarchy(bvhElements);
+
   const numPoints = positionBuffer.length / 3;
   const rawObjectIdBuffer = new ArrayBuffer(2 * numPoints);
   const objectIdBufferView = new Uint16Array(rawObjectIdBuffer);
@@ -18,11 +38,12 @@ export function computeObjectIdBuffer(
   for (let i = 0; i < objectIdBufferView.length; i++) {
     const position = new THREE.Vector3().fromArray(positionBuffer.slice(3 * i, 3 * (i + 1))).add(pointOffset);
 
+    const elements = bvh.findContainingElements(position);
 
-    for (const obj of objectList) {
-      if (obj.shape.containsPoint(position)) {
+    for (const element of elements) {
+      if (element.stylableObject.shape.containsPoint(position)) {
         // NB: We use the first object that is found to contain the point
-        objectIdBufferView[i] = obj.objectId;
+        objectIdBufferView[i] = element.stylableObject.objectId;
         break;
       }
     }
