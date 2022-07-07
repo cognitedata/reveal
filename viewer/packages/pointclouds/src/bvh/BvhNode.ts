4fe0442b1678258ea6c3d255ef01b1c70a5c5428
Håkon Flatval <hakon.flatval@cognite.com>
2022-07-07 15:13:31 +0200

    chore: lint fix and smaller improvements in BVH

--- viewer/packages/pointclouds/src/bvh/BvhNode.ts
+++ viewer/packages/pointclouds/src/bvh/BvhNode.ts
@@ -2,17 +2,15 @@
  * Copyright 2022 Cognite AS
  */
 
-import { unionBoxes } from '@reveal/utilities';
+import { unionBoxes } from './unionBoxes';
 import * as THREE from 'three';
 
 import { BvhElement } from './BvhElement';
 
 import { findBestSplit } from './bvhUtils';
 
-import assert from 'assert';
 
 export class BvhNode<T extends BvhElement> {
-
   // Either _children or _elements is defined. Not both, not none.
   private readonly _children: [BvhNode<T>, BvhNode<T>] | undefined;
   private readonly _elements: T[] | undefined;
@@ -29,8 +27,7 @@ export class BvhNode<T extends BvhElement> {
 
     const [firstElements, lastElements] = findBestSplit(elements);
 
-    this._children = [new BvhNode<T>(firstElements),
-                      new BvhNode<T>(lastElements)];
+    this._children = [new BvhNode<T>(firstElements), new BvhNode<T>(lastElements)];
 
     this._boundingBox = this._children[0].boundingBox.clone().union(this._children[1].boundingBox);
   }
@@ -39,9 +36,8 @@ export class BvhNode<T extends BvhElement> {
     return this._boundingBox;
   }
 
-  findContainingElements(point: THREE.Vector3, resultList: T[]) {
+  findContainingElements(point: THREE.Vector3, resultList: T[]): void {
     if (this._elements) {
-
       for (const element of this._elements) {
         if (element.getBox().containsPoint(point)) {
           resultList.push(element);
@@ -51,14 +47,13 @@ export class BvhNode<T extends BvhElement> {
       return;
     }
 
-    assert(this._children);
 
-    if (this._children[0].boundingBox.containsPoint(point)) {
-      this._children[0].findContainingElements(point, resultList);
+    if (this._children![0].boundingBox.containsPoint(point)) {
+      this._children![0].findContainingElements(point, resultList);
     }
 
-    if (this._children[1].boundingBox.containsPoint(point)) {
-      this._children[1].findContainingElements(point, resultList);
+    if (this._children![1].boundingBox.containsPoint(point)) {
+      this._children![1].findContainingElements(point, resultList);
     }
   }
-};
+}
