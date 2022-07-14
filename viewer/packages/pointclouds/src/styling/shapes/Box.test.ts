aa68f3796e937d55b128b1e4e19e2a3069a161e7
Håkon Flatval <hakon.flatval@cognite.com>
2022-07-14 09:29:57 +0200

    test: fix geometry tests

--- viewer/packages/pointclouds/src/styling/shapes/Box.test.ts
+++ viewer/packages/pointclouds/src/styling/shapes/Box.test.ts
@@ -3,35 +3,25 @@
  */
 
 import { Box } from './Box';
-import { Mat4 } from './linalg';
 
-function createIdentityMatrix(): Mat4 {
-  const arr = new Array<number>(16);
-  for (let i = 0; i < 16; i++) {
-    // Set every element on the diagonal to 1,
-    // all others to 0
-    arr[i] = i % 5 == 0 ? 1 : 0;
-  }
-
-  return { data: arr };
-}
+import * as THREE from 'three';
 
 describe(Box.name, () => {
   test('constructing box does not throw error', () => {
     expect(() => {
-      new Box(createIdentityMatrix());
+      new Box(new THREE.Matrix4());
     }).not.toThrow();
   });
 
   test('origin is within identity box', () => {
-    const box = new Box(createIdentityMatrix());
+    const box = new Box(new THREE.Matrix4());
 
-    expect(box.containsPoint([0, 0, 0])).toBeTrue();
+    expect(box.containsPoint(new THREE.Vector3(0, 0, 0))).toBeTrue();
   });
 
   test('point [1, 1, 1] is not within identity box', () => {
-    const box = new Box(createIdentityMatrix());
+    const box = new Box(new THREE.Matrix4());
 
-    expect(box.containsPoint([1, 1, 1])).toBeFalse();
+    expect(box.containsPoint(new THREE.Vector3(1, 1, 1))).toBeFalse();
   });
 });
