1c2fcafe48f62efbe9939efe27c61a6e4eed5a90
Gayan Deegala <Gayan@CreativeSoftware.com>
2022-07-19 11:03:02 +0530

    fix padding issue

--- src/modules/Common/Components/BulkEdit/Source/getDataForSource.tsx
+++ src/modules/Common/Components/BulkEdit/Source/getDataForSource.tsx
@@ -1,7 +1,6 @@
 import { BulkEditUnsavedState } from 'src/modules/Common/store/common/types';
 import { VisionFile } from 'src/modules/Common/store/files/types';
 import { BulkEditTableDataType } from 'src/modules/Common/Components/BulkEdit/BulkEditTable/BulkEditTable';
-import styled from 'styled-components';
 import { getOriginalValue } from 'src/modules/Common/Components/BulkEdit/utils/getOriginalValue';
 import { getUpdatedValue } from 'src/modules/Common/Components/BulkEdit/utils/getUpdatedValue';
 
@@ -24,9 +23,3 @@ export const getDataForSource = ({
     };
   });
 };
-
-export const CellContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  row-gap: 2px;
-`;
