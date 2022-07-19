f26fd0f2cd6f1a71ff24fc7d803292d994f69ba9
adam tombleson <rekarnar@gmail.com>
2022-07-19 10:32:32 +0200

    chore(discover): remove old docker test container (#3000)
    Requested from security to cleanup potentially vulnerable things.

--- apps/discover/scripts/docker/docker-run.sh
+++
@@ -1,5 +0,0 @@
-echo ''
-echo '## Starting testcafe runner ##'
-echo ''
-
-../../node_modules/.bin/testcafe chromium:headless '--use-gl=swiftshader --enable-logging=stderr --disable-software-rasterizer --window-size=1500,1040' /home/node/testcafe/tests --stop-on-first-fail $META 2>&1 | tee testcafe/logs/run.log
--- apps/discover/scripts/docker/docker-serve.sh
+++
@@ -1,9 +0,0 @@
-echo ''
-echo '## Starting testcafe server ##'
-echo ''
-# echo "PROJECT: ${REACT_APP_API_KEY_PROJECT}"
-# echo "KEY: ${REACT_APP_API_KEY}"
-echo ''
-
-sed -i "s/CDF_CLUSTER/$CDF_CLUSTER/g" /home/node/build/sidecar.js
-../../node_modules/.bin/serve -s build -l 3000 2>&1 | tee /home/node/testcafe/logs/serve.log
