1671e84d6292f3e2cb2039f38a791844fc6815d5
Mihil Ranathunga <mihilranathunga@gmail.com>
2022-07-25 19:39:04 +0530

    Preliminary change to update react-router dependency (#839)
    Apply suggestions as detailed in https://reactrouter.com/docs/en/v6/upgrading/v5 as preliminary steps to update the react-router dependency
     Also some refactoring to reduce complexity

--- src/modules/Common/Components/LazyWrapper.tsx
+++ src/modules/Common/Components/LazyWrapper.tsx
@@ -1,23 +1,14 @@
 import { Loader } from '@cognite/cogs.js';
-import React, {
-  ComponentType,
-  lazy,
-  ReactNode,
-  Suspense,
-  useMemo,
-} from 'react';
+import React, { ComponentType, lazy, Suspense, useMemo } from 'react';
 
-export function LazyWrapper<P>(props: {
-  routeProps: P;
+export function LazyWrapper(props: {
   importFn: () => Promise<{ default: ComponentType<any> }>;
-  loadingComponent?: ReactNode;
 }) {
-  const loader = props.loadingComponent || <Loader />;
   const LazyComp = useMemo(() => lazy(props.importFn), [props.importFn]);
 
   return (
-    <Suspense fallback={loader}>
-      <LazyComp {...props.routeProps} />
+    <Suspense fallback={<Loader />}>
+      <LazyComp />
     </Suspense>
   );
 }
