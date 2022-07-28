401f95cae4254a76443860aad04a606ebe1ce277
Stefan Maretić <stefan.maretic@cognite.com>
2022-07-28 17:03:13 +0200

    feat: add antd theme

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -12,72 +12,12 @@ import {
   Dropdown,
   Spin,
 } from 'antd';
-// TODO: remove those we don't need anymore
-import affixStyle from 'antd/es/affix/style/index.less';
-import alertStyle from 'antd/es/alert/style/index.less';
-import breadcrumbStyle from 'antd/es/breadcrumb/style/index.less';
-import buttonStyle from 'antd/es/button/style/index.less';
-import cardStyle from 'antd/es/card/style/index.less';
-import carouselStyle from 'antd/es/carousel/style/index.less';
-import coreStyle from 'antd/es/style/core/index.less';
-import drawerStyle from 'antd/es/drawer/style/index.less';
-import gridStyle from 'antd/es/grid/style/index.less';
-import inputNumberStyle from 'antd/es/input-number/style/index.less';
-import inputStyle from 'antd/es/input/style/index.less';
-import layoutStyle from 'antd/es/layout/style/index.less';
-import menuStyle from 'antd/es/menu/style/index.less';
-import messageStyle from 'antd/es/message/style/index.less';
-import modalStyle from 'antd/es/modal/style/index.less';
-import notificationStyle from 'antd/es/notification/style/index.less';
-import paginationStyle from 'antd/es/pagination/style/index.less';
-import popconfirmStyle from 'antd/es/popconfirm/style/index.less';
-import popoverStyle from 'antd/es/popover/style/index.less';
-import progressStyle from 'antd/es/progress/style/index.less';
-import selectStyle from 'antd/es/select/style/index.less';
-import spinStyle from 'antd/es/spin/style/index.less';
-import tableStyle from 'antd/es/table/style/index.less';
-import tagStyle from 'antd/es/tag/style/index.less';
-import tooltipStyle from 'antd/es/tooltip/style/index.less';
-import uploadStyle from 'antd/es/upload/style/index.less';
-import skeletonStyle from 'antd/es/skeleton/style/index.less';
 
-const antdStyles = [
-  affixStyle,
-  alertStyle,
-  breadcrumbStyle,
-  buttonStyle,
-  cardStyle,
-  carouselStyle,
-  coreStyle,
-  drawerStyle,
-  gridStyle,
-  inputNumberStyle,
-  inputStyle,
-  layoutStyle,
-  menuStyle,
-  messageStyle,
-  modalStyle,
-  notificationStyle,
-  paginationStyle,
-  popconfirmStyle,
-  popoverStyle,
-  progressStyle,
-  selectStyle,
-  spinStyle,
-  tableStyle,
-  tagStyle,
-  tooltipStyle,
-  uploadStyle,
-  skeletonStyle,
-];
+import antdTheme from './antd-theme.less';
 
 Modal.defaultProps = {
   ...Modal.defaultProps,
   getContainer,
-  bodyStyle: {
-    padding: '20px',
-    borderRadius: '10px',
-  },
 };
 
 Tooltip.defaultProps = {
@@ -97,7 +37,7 @@ Dropdown.defaultProps = {
 Spin.setDefaultIndicator(<Icon type="Loader" />);
 
 export function GlobalStyles(props: { children: React.ReactNode }) {
-  const isInjectedStyles = useGlobalStyles([...antdStyles, cogsStyles]);
+  const isInjectedStyles = useGlobalStyles([antdTheme, cogsStyles]);
 
   if (!isInjectedStyles) {
     return <Loader />;
