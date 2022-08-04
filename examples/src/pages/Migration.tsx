047a94058d9e03f6f1d2095135811731d6c14cb8
Savelii Novikov <kvaka2000@gmail.com>
2022-08-04 14:21:44 +0200

    Changed console.log in migration

--- examples/src/pages/Migration.tsx
+++ examples/src/pages/Migration.tsx
@@ -382,7 +382,7 @@ export function Migration() {
             case 'pointcloud':
               {
                 const { point } = intersection;
-                console.log(`Clicked point assigned to the object with annotationId:`, intersection.annotationId, `at`, point);
+                console.log(`Clicked point assigned to the object with annotationId: ${intersection.annotationId} at`, point);
                 const sphere = new THREE.Mesh(new THREE.SphereBufferGeometry(0.1), new THREE.MeshBasicMaterial({ color: 'red' }));
                 sphere.position.copy(point);
                 viewer.addObject3D(sphere);
