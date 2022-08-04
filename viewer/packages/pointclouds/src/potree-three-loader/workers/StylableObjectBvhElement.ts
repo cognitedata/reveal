09ca297d8b1aca79c351efac71d7254cd5ab1577
Håkon Flatval <hakon.flatval@cognite.com>
2022-08-04 09:03:14 +0200

    fix: add file with stylable object bvh element implementation

---
+++ viewer/packages/pointclouds/src/potree-three-loader/workers/StylableObjectBvhElement.ts
@@ -0,0 +1,27 @@
+/*!
+ * Copyright 2022 Cognite AS
+ */
+
+import { BvhElement } from '../../bvh/BvhElement';
+import { StylableObject } from '../../styling/StylableObject';
+
+import * as THREE from 'three';
+
+export class StylableObjectBvhElement implements BvhElement {
+
+  private _box: THREE.Box3;
+  private _stylableObject: StylableObject;
+
+  constructor(stylableObject: StylableObject) {
+    this._stylableObject = stylableObject;
+    this._box = stylableObject.shape.createBoundingBox();
+  }
+
+  get stylableObject(): StylableObject {
+    return this._stylableObject;
+  }
+
+  getBox(): THREE.Box3 {
+    return this._box;
+  }
+}
