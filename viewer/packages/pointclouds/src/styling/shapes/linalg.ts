40178c7acfe275f1fc3549ea5894fd9c90185478
Håkon Flatval <hakon.flatval@cognite.com>
2022-08-04 09:25:51 +0200

    fix: add Vec3WithIndex data type

--- viewer/packages/pointclouds/src/styling/shapes/linalg.ts
+++ viewer/packages/pointclouds/src/styling/shapes/linalg.ts
@@ -3,6 +3,7 @@
  */
 
 export type Vec3 = [number, number, number];
+export type Vec3WithIndex = [number, number, number, number];
 export type AABB = { min: Vec3; max: Vec3 };
 export type Mat4 = {
   data: number[];
