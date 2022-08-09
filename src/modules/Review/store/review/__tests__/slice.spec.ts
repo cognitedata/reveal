dc9f51d2ccab793c257419966c7d152551c8e081
Gayan Deegala <Gayan@CreativeSoftware.com>
2022-08-09 09:25:24 +0530

    test toggleAnnotationVisibility

--- src/modules/Review/store/review/__tests__/slice.spec.ts
+++ src/modules/Review/store/review/__tests__/slice.spec.ts
@@ -1,11 +1,13 @@
 import reducer, {
   initialState,
   setReviewFileIds,
+  toggleAnnotationVisibility,
 } from 'src/modules/Review/store/review/slice';
 import { ReviewState } from 'src/modules/Review/store/review/types';
 
 const mockReviewState: ReviewState = {
   ...initialState,
+  hiddenAnnotationIds: [1, 2, 3],
 };
 
 describe('Test review slice', () => {
@@ -19,6 +21,24 @@ describe('Test review slice', () => {
       test('can set file ids back to empty array', () => {
         const newState = reducer(mockReviewState, setReviewFileIds([]));
         expect(newState.fileIds).toEqual([]);
+      });
+    });
+
+    describe('action toggleAnnotationVisibility', () => {
+      test("remove id from hidden id if it's already exist", () => {
+        const newState = reducer(
+          mockReviewState,
+          toggleAnnotationVisibility({ annotationId: 3 })
+        );
+        expect(newState.hiddenAnnotationIds).toEqual([1, 2]);
+      });
+
+      test("add annotation id to hidden ids if it's not previously added", () => {
+        const newState = reducer(
+          mockReviewState,
+          toggleAnnotationVisibility({ annotationId: 4 })
+        );
+        expect(newState.hiddenAnnotationIds).toEqual([1, 2, 3, 4]);
       });
     });
   });
