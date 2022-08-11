91728402e0b8a0d33dd253105135a1a2962cef6f
adam tombleson <rekarnar@gmail.com>
2022-08-11 08:41:06 +0200

    chore(discover): remove unused map files (#3067)

--- apps/discover/src/pages/authorized/search/search/MapTools/elements.ts
+++
@@ -1,3 +0,0 @@
-import styled from 'styled-components/macro';
-
-export const MapToolWrapper = styled.div``;
--- apps/discover/src/pages/authorized/search/search/MapTools/types.ts
+++
@@ -1,8 +0,0 @@
-export interface MapToolsProp {
-  disabled?: boolean;
-  text: string;
-}
-
-export interface DrawMode {
-  state?: 'draw_free_polygon' | 'draw_line_string';
-}
