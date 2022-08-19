39662997461782db5312d699ba032340b46d893f
Håkon Flatval <hakon.flatval@cognite.com>
2022-08-19 11:24:06 +0200

    feat: Webassembly setup

---
+++ viewer/wasm/index.ts
@@ -0,0 +1,18 @@
+/*!
+ * Copyright 2022 Cognite AS
+ */
+
+import init, { add_three } from './pkg/reveal_rust_wasm';
+import wasm from './pkg/reveal_rust_wasm_bg.wasm';
+
+
+function getWasmInitPromise(): Promise<void> {
+  return typeof init === 'function' ? init(wasm).then(() => {}) : Promise.resolve();
+}
+
+export async function addThree(
+  input: number,
+): Promise<number> {
+  const wasmPromise = getWasmInitPromise();
+  return wasmPromise.then(() => add_three(input));
+}
