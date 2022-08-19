74f625cf78f23ac930cd7cd246f3b4071a561708
Håkon Flatval <hakon.flatval@cognite.com>
2022-08-19 09:30:12 +0200

    chore: add lint rules and LINT FIX

--- viewer/packages/pointclouds/wasm/index.ts
+++ viewer/packages/pointclouds/wasm/index.ts
@@ -9,7 +9,12 @@ function getWasmInitPromise(): Promise<void> {
   return typeof init === 'function' ? (init as (buffer: any) => Promise<any>)(wasm).then(() => {}) : Promise.resolve();
 }
 
-export async function assignPoints(input_shapes: Array<any>, input_points: Float32Array, input_bounding_box: object, input_point_offset: Array<any>): Promise<Uint16Array> {
+export async function assignPoints(
+  input_shapes: Array<any>,
+  input_points: Float32Array,
+  input_bounding_box: object,
+  input_point_offset: Array<any>
+): Promise<Uint16Array> {
   const wasm_init = getWasmInitPromise();
   return wasm_init.then(() => assign_points(input_shapes, input_points, input_bounding_box, input_point_offset));
 }
