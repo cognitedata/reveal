04aa8e586a327b5f1720e250ef72579331076c7a
Håkon Flatval <hakon.flatval@cognite.com>
2022-08-22 12:55:01 +0200

    chore: lint fix

--- viewer/packages/pointclouds/wasm/index.ts
+++ viewer/packages/pointclouds/wasm/index.ts
@@ -6,7 +6,7 @@ import init, { add_three } from './pkg/pointclouds_wasm';
 import wasm from './pkg/pointclouds_wasm_bg.wasm';
 
 function getWasmInitPromise(): Promise<void> {
-  return typeof init === 'function' ? (init as ((buffer: any) => Promise<any>))(wasm).then(() => {}) : Promise.resolve();
+  return typeof init === 'function' ? (init as (buffer: any) => Promise<any>)(wasm).then(() => {}) : Promise.resolve();
 }
 
 export async function addThree(input: number): Promise<number> {
