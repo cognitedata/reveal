3ebe483aae048762e990996b713b8ce284dc96ff
Håkon Flatval <hakon.flatval@cognite.com>
2022-08-23 11:11:12 +0200

    chore: update package name

--- viewer/packages/pointclouds/wasm/index.ts
+++ viewer/packages/pointclouds/wasm/index.ts
@@ -2,8 +2,8 @@
  * Copyright 2022 Cognite AS
  */
 
-import init, { add_three } from './pkg/reveal_rust_wasm';
-import wasm from './pkg/reveal_rust_wasm_bg.wasm';
+import init, { add_three } from './pkg/pointclouds_wasm';
+import wasm from './pkg/pointclouds_wasm_bg.wasm';
 
 function getWasmInitPromise(): Promise<void> {
   return typeof init === 'function' ? (init as (buffer: any) => Promise<any>)(wasm).then(() => {}) : Promise.resolve();
