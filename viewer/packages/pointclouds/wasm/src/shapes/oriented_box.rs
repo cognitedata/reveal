fb4d1ea2c683c77ce7fbcd38b945b628d51ce73b
Håkon Flatval <hakon.flatval@cognite.com>
2022-08-23 15:40:18 +0200

    test: add oriented_box tests

--- viewer/packages/pointclouds/wasm/src/shapes/oriented_box.rs
+++ viewer/packages/pointclouds/wasm/src/shapes/oriented_box.rs
@@ -56,3 +56,94 @@ impl shape::Shape for OrientedBox {
         self.object_id
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use super::OrientedBox;
+    use crate::shapes::shape::Shape;
+    use crate::linalg::Mat4;
+    use super::{vec3, vec4};
+
+    use nalgebra_glm::{abs, comp_max, half_pi, inverse, max2, min2, rotate_x, scale, translate, vec4_to_vec3};
+
+    #[wasm_bindgen_test]
+    fn identity_oriented_box_contains_origin() {
+        let bb = OrientedBox::new(Mat4::identity(), 0);
+        assert!(bb.contains_point(&vec3(0.0, 0.0, 0.0)));
+    }
+
+    #[wasm_bindgen_test]
+    fn identity_oriented_box_does_not_contain_cardinal_unit_vectors() {
+        let bb = OrientedBox::new(Mat4::identity(), 0);
+        assert!(!bb.contains_point(&vec3(1.0, 0.0, 0.0)));
+        assert!(!bb.contains_point(&vec3(0.0, 1.0, 0.0)));
+        assert!(!bb.contains_point(&vec3(0.0, 0.0, 1.0)));
+    }
+
+
+    #[wasm_bindgen_test]
+    fn translated_oriented_box_contains_only_translated_origin() {
+        let bb = OrientedBox::new(
+            nalgebra_glm::inverse(&translate(&Mat4::identity(), &vec3(1.0, 1.0, 1.0))),
+            0);
+        assert!(!bb.contains_point(&vec3(0.0, 0.0, 0.0)));
+        assert!(bb.contains_point(&vec3(1.0, 1.0, 1.0)));
+    }
+
+
+    #[wasm_bindgen_test]
+    fn scaled_oriented_box_contains_only_points_in_scaled_direction() {
+        let bb = OrientedBox::new(
+            inverse(&scale(&Mat4::identity(), &vec3(3.0, 1.0, 1.0))),
+            0);
+        assert!(bb.contains_point(&vec3(1.0, 0.0, 0.0)));
+        assert!(!bb.contains_point(&vec3(0.0, 1.0, 0.0)));
+        assert!(!bb.contains_point(&vec3(0.0, 0.0, 1.0)));
+    }
+
+    #[wasm_bindgen_test]
+    fn scaled_and_rotated_oriented_box_contains_the_right_points() {
+
+        let matrix = inverse(
+            &scale(
+                &rotate_x(
+                    &Mat4::identity(), half_pi()), &vec3(1.0, 3.0, 1.0)));
+
+        let bb = OrientedBox::new(matrix, 0);
+        assert!(!bb.contains_point(&vec3(1.0, 0.0, 0.0)));
+        assert!(!bb.contains_point(&vec3(0.0, 1.0, 0.0)));
+        assert!(bb.contains_point(&vec3(0.0, 0.0, 1.0)));
+    }
+
+    #[wasm_bindgen_test]
+    fn identitys_bounding_box_is_identity() {
+
+        let original_box = OrientedBox::new(Mat4::identity(), 0);
+        let bounding_box = original_box.create_bounding_box();
+
+        assert!(comp_max(&abs(&(bounding_box.min - vec3(-0.5, -0.5, -0.5)))) < 1e-2);
+        assert!(comp_max(&abs(&(bounding_box.max - vec3(0.5, 0.5, 0.5)))) < 1e-2);
+    }
+
+    #[wasm_bindgen_test]
+    fn bounding_box_contains_transformed_origin() {
+
+        let matrix =
+            translate(
+                &scale(
+                    &rotate_x(&Mat4::identity(), 1.0),
+                    &vec3(2.0, 1.0, 0.5)),
+                &vec3(10.0, 93.0, 0.2));
+
+
+        let original_box = OrientedBox::new(inverse(&matrix), 0);
+        let bounding_box = original_box.create_bounding_box();
+
+        let transformed_origin = vec4_to_vec3(&(matrix * vec4(0.0, 0.0, 0.0, 1.0)));
+
+        assert_eq!(max2(&bounding_box.max, &transformed_origin), bounding_box.max);
+        assert_eq!(min2(&bounding_box.min, &transformed_origin), bounding_box.min);
+    }
+}
