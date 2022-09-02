d4e91739d832c3900768e2cc26a1e72a9f2581de
Brian Kuzma <brian.kuzma@cognite.com>
2022-09-02 10:55:05 +0200

    Add href back to docs button link (#593)

--- apps/platypus/src/app/components/DocLinkButtonGroup/DocLinkButtonGroup.tsx
+++ apps/platypus/src/app/components/DocLinkButtonGroup/DocLinkButtonGroup.tsx
@@ -7,6 +7,7 @@ export const DocLinkButtonGroup = () => {
   return (
     <Button
       aria-label={t('btn_link_cli_docs', 'Visit docs page')}
+      href="https://pr-1279.docs.preview.cogniteapp.com/dev/concepts/resource_types/fdm/guides/manage_dm"
       type="ghost"
       icon="Documentation"
       target="_blank"
