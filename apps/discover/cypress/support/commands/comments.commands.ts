fdfb46ac3c03270f84397bbbe12f9bd130d138af
Saagar Takhi <86822653+saagar-cognite@users.noreply.github.com>
2022-09-02 10:49:10 +0200

    fix(discover): fix create favorites e2e with intercepting comments (#3232)
    No jira link as this is fixing a flaky test
    
    # What has changed?
    
    > adds intercept for comments to wait for assertion

--- apps/discover/cypress/support/commands/comments.commands.ts
+++ apps/discover/cypress/support/commands/comments.commands.ts
@@ -1,6 +1,11 @@
+import { PROJECT } from '../../app.constants';
+
 const COMMENT_TEXT = `comment text, ${Date.now()}`;
 const COMMENT_EDIT = '-edit';
 const COMMENT_TEXT_EDITED = `${COMMENT_TEXT}${COMMENT_EDIT}`;
+const POST_COMMENT = 'postComment';
+const EDIT_COMMENT = 'editComment';
+const DELETE_COMMENT = 'deleteComment';
 
 const openCommentBar = () => {
   cy.log('Open the comment bar');
@@ -9,17 +14,25 @@ const openCommentBar = () => {
 
 const postComment = () => {
   cy.log('Post a comment');
+  cy.intercept({
+    url: `/${PROJECT}/comment`,
+    method: 'POST',
+  }).as(POST_COMMENT);
 
   cy.findByTestId('comments-root').then(($subject) => {
     cy.wrap($subject).findByTestId('editable-area').type(COMMENT_TEXT);
     cy.wrap($subject).findByText('Send').click();
-    cy.wait(1500);
+    cy.wait(`@${POST_COMMENT}`, { requestTimeout: 10000 });
     cy.wrap($subject).findByText(COMMENT_TEXT).should('be.visible');
   });
 };
 
 const editComment = () => {
   cy.log('Edit a comment');
+  cy.intercept({
+    url: `/${PROJECT}/comment/*`,
+    method: 'POST',
+  }).as(EDIT_COMMENT);
 
   cy.findByTestId('comments-root').then(($subject) => {
     cy.wrap($subject).findByText(COMMENT_TEXT);
@@ -32,16 +45,23 @@ const editComment = () => {
       .type(COMMENT_EDIT);
     cy.log('Save the modified comment');
     cy.findByText('Save').click({ force: true });
+    cy.wait(`@${EDIT_COMMENT}`, { requestTimeout: 10000 });
     cy.wrap($subject).findByText(COMMENT_TEXT_EDITED).should('be.visible');
   });
 };
 
 const deleteComment = () => {
   cy.log('Should be able to remove comments');
+  cy.intercept({
+    url: `/${PROJECT}/comment/delete`,
+    method: 'POST',
+  }).as(DELETE_COMMENT);
+
   cy.findByTestId('comments-root').then(($subject) => {
     cy.wrap($subject).findByText(COMMENT_TEXT_EDITED).should('be.visible');
     cy.log('Clicking on the delete button');
     cy.wrap($subject).findByLabelText('Delete message').click({ force: true });
+    cy.wait(`@${DELETE_COMMENT}`, { requestTimeout: 10000 });
     cy.log('Checking if the comment no longer exists');
     cy.wrap($subject).findByText(COMMENT_TEXT_EDITED).should('not.exist');
   });
