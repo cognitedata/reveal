6baa3c14d7e749d882d4b71a3fd130561c87ded2
Christopher J. Tannum <christopher.tannum@cognite.com>
2022-09-02 16:02:28 +0200

    feat: add option to Cognite3DViewer to override resolution cap (#2381)
    * feat: add resize logic to basic pipeline executor
    
    * fix: add resizing to first render
    
    * feat: add auto resizing renderer capability to basic pipeline executor
    
    * feat: expose an option to override resolution cap
    
    * fix: dispose resize observer on viewer.dispose()
    
    * fix: add ResizeObserver node.js polyfill and restore tests
    
    * nit: add changes from QA
    
    * fix: recreate visual test
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- viewer/packages/rendering/src/pipeline-executors/BasicPipelineExecutor.ts
+++ viewer/packages/rendering/src/pipeline-executors/BasicPipelineExecutor.ts
@@ -6,18 +6,96 @@ import * as THREE from 'three';
 import { RenderPipelineExecutor } from '../RenderPipelineExecutor';
 import { RenderPipelineProvider } from '../RenderPipelineProvider';
 
-export class BasicPipelineExecutor implements RenderPipelineExecutor {
-  private readonly _renderer: THREE.WebGLRenderer;
+export type BasicPipelineExecutorOptions = {
+  autoResizeRenderer: boolean;
+  resolutionThreshold?: number;
+};
 
-  constructor(renderer: THREE.WebGLRenderer) {
+export class BasicPipelineExecutor implements RenderPipelineExecutor {
+  private readonly _defaultResolutionThreshold = 1.4e6;
+
+  private readonly _renderer: THREE.WebGLRenderer;
+  private readonly _resolutionThreshold: number;
+  private readonly _resizeObserver: ResizeObserver | undefined;
+
+  private _shouldResize: boolean;
+
+  constructor(renderer: THREE.WebGLRenderer, options?: BasicPipelineExecutorOptions) {
     this._renderer = renderer;
+    this._resolutionThreshold = options?.resolutionThreshold ?? this._defaultResolutionThreshold;
+
+    const autoResizeRenderer = options?.autoResizeRenderer ?? false;
+    this._shouldResize = autoResizeRenderer;
+
+    this._resizeObserver = this.setupResizeListener(autoResizeRenderer, renderer);
+
     renderer.info.autoReset = false;
   }
 
-  public render(renderPipeline: RenderPipelineProvider, camera: THREE.Camera): void {
+  public render(renderPipeline: RenderPipelineProvider, camera: THREE.PerspectiveCamera): void {
     this._renderer.info.reset();
+
+    if (this._shouldResize) {
+      this.resize(camera);
+      this._shouldResize = false;
+    }
+
     for (const renderPass of renderPipeline.pipeline(this._renderer)) {
       renderPass.render(this._renderer, camera);
     }
+  }
+
+  private setupResizeListener(autoResizeRenderer: boolean, renderer: THREE.WebGLRenderer): ResizeObserver | undefined {
+    if (!autoResizeRenderer) {
+      return;
+    }
+    const domElement = renderer.domElement.parentElement;
+    if (!domElement) {
+      throw new Error('Canvas does not have a parent element');
+    }
+    const resizeObserver = new ResizeObserver(() => (this._shouldResize = true));
+    resizeObserver.observe(domElement);
+
+    return resizeObserver;
+  }
+
+  private resize(camera: THREE.PerspectiveCamera): void {
+    const canvas = this._renderer.domElement;
+    const domElement = canvas.parentElement;
+
+    if (!domElement) {
+      throw new Error('Canvas does not have a parent element');
+    }
+
+    const virtualFramebufferSize = this._renderer.getSize(new THREE.Vector2());
+    const pixelRatio = this._renderer.getPixelRatio();
+
+    const virtualDomElementWidth = domElement.clientWidth !== 0 ? domElement.clientWidth : canvas.clientWidth;
+
+    const virtualDomElementHeight = domElement.clientHeight !== 0 ? domElement.clientHeight : canvas.clientHeight;
+
+    const domElementPhysicalWidth = virtualDomElementWidth * pixelRatio;
+    const domElementPhysicalHeight = virtualDomElementHeight * pixelRatio;
+    const domElementPhysicalNumberOfPixels = domElementPhysicalWidth * domElementPhysicalHeight;
+
+    const downScale =
+      domElementPhysicalNumberOfPixels > this._resolutionThreshold
+        ? Math.sqrt(this._resolutionThreshold / domElementPhysicalNumberOfPixels)
+        : 1;
+
+    const newVirtualWidth = virtualDomElementWidth * downScale;
+    const newVirtualHeight = virtualDomElementHeight * downScale;
+
+    if (newVirtualWidth === virtualFramebufferSize.x && newVirtualHeight === virtualFramebufferSize.y) {
+      return;
+    }
+
+    camera.aspect = newVirtualWidth / newVirtualHeight;
+    camera.updateProjectionMatrix();
+    this._renderer.setDrawingBufferSize(newVirtualWidth, newVirtualHeight, pixelRatio);
+  }
+
+  dispose(): void {
+    this._resizeObserver?.disconnect();
   }
 }
