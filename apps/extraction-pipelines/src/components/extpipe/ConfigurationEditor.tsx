1858a624cb665ec0bf87cb82310403f2a24be954
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2022-09-15 13:52:59 +0200

    feat: add the option of code snippet for configuration revisions

--- apps/extraction-pipelines/src/components/extpipe/ConfigurationEditor.tsx
+++ apps/extraction-pipelines/src/components/extpipe/ConfigurationEditor.tsx
@@ -3,19 +3,21 @@ import { useExtpipeConfig } from 'hooks/config';
 import { StreamLanguage } from '@codemirror/language';
 import { yaml } from '@codemirror/legacy-modes/mode/yaml';
 import ConfigurationErrorFeedback from './ConfigurationError';
-import { CodeEditor } from '@cognite/cdf-utilities';
+import { CodeEditor, CodeSnippet } from '@cognite/cdf-utilities';
 
 type Props = {
   externalId: string;
   revision?: number;
   editable?: boolean;
   onChange?: (s: string) => void;
+  isSnippet?: boolean;
 };
 export default function ConfigurationEditor({
   externalId,
   revision,
   editable = false,
   onChange,
+  isSnippet,
 }: Props) {
   const {
     data: configuration,
@@ -29,15 +31,22 @@ export default function ConfigurationEditor({
   return (
     <>
       {!editable && <ConfigurationErrorFeedback error={error} />}
-      {(isSuccess || editable) && (
-        <CodeEditor
-          disabled={!editable}
-          extensions={[StreamLanguage.define(yaml)]}
-          onChange={onChange}
-          lang="yaml"
-          value={configuration?.config || ''}
-        />
-      )}
+      {(isSuccess || editable) &&
+        (isSnippet ? (
+          <CodeSnippet
+            extensions={[StreamLanguage.define(yaml)]}
+            lang="yaml"
+            value={configuration?.config || ''}
+          />
+        ) : (
+          <CodeEditor
+            disabled={!editable}
+            extensions={[StreamLanguage.define(yaml)]}
+            onChange={onChange}
+            lang="yaml"
+            value={configuration?.config || ''}
+          />
+        ))}
     </>
   );
 }
