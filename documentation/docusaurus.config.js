7c828d95b33410cf30ef618468ccaa7ee3885831
Håkon Flatval <hakon.flatval@cognite.com>
2022-09-19 12:17:04 +0200

    fix: make search work for all versions

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -15,12 +15,6 @@ module.exports = {
       contextualSearch: true,
 
       // Optional: Algolia search parameters
-      searchParameters: {
-        // https://www.algolia.com/doc/api-reference/api-parameters/facetFilters/?client=javascript
-        // Excludes 'next'
-        facetFilters: [['version:1.x', 'version:2.x', 'version:3.x']],
-      },
-
       //... other Algolia params https://autocomplete-experimental.netlify.app/docs/docsearchmodal/#reference
     },
     prism: {
