d4564f87f552f33e6b1145c62491c222a1e1e8e2
Håkon Flatval <hakon.flatval@cognite.com>
2022-09-19 15:18:58 +0200

    docs: remove unnecessary argument to demo viewer

--- documentation/versioned_docs/version-1.x/components/DemoWrapper.tsx
+++ documentation/versioned_docs/version-1.x/components/DemoWrapper.tsx
@@ -6,7 +6,6 @@ import React, { Suspense } from 'react';
 import styled from 'styled-components';
 import { DemoLoginCover } from './DemoLoginCover';
 import { CogniteClient } from '@cognite/sdk-1.x';
-import { Cognite3DViewerDemo } from './Cognite3DViewerDemo';
 import { env } from '@site/versioned_docs/version-1.x/utils/env';
 
 const DemoContainer = styled.div`
@@ -20,12 +19,10 @@ const DemoContainer = styled.div`
 // different demos might have different ids, e.g. pointcloud/cad
 // also we can use 3ddemo instead of publicdata so here is .env file to help with it
 export function DemoWrapper({
-  name,
   ids,
   modelType,
   ...rest
 }: {
-  name: string;
   ids?: { modelId: number; revisionId: number };
   modelType?: string;
   [key: string]: any; // any other props that might be bypassed to specific Demo
@@ -37,13 +34,17 @@ export function DemoWrapper({
   let modelAndRevisionIds =
     ids || (modelType === 'pointcloud' ? env.pointCloud : env.cad);
 
+  const ViewerDemo = React.lazy(() =>
+    import('@site/docs/components/Cognite3DViewerDemo')
+  );
+
   return (
     <div>
       <DemoContainer id="demo-wrapper">
         <Suspense fallback={<div>Loading demo...</div>}>
           <DemoLoginCover>
             {(client: CogniteClient) => (
-              <Cognite3DViewerDemo
+              <ViewerDemo
                 client={client}
                 {...rest}
                 modelId={modelAndRevisionIds.modelId}
