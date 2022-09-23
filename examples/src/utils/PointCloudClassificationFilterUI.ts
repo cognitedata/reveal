6287ce81df0aaebe45a63e03ee98bf9e9d603d82
Håkon Flatval <hakon.flatval@cognite.com>
2022-09-23 10:24:52 +0200

    improvement: another variable rename

--- examples/src/utils/PointCloudClassificationFilterUI.ts
+++ examples/src/utils/PointCloudClassificationFilterUI.ts
@@ -4,13 +4,13 @@ import dat from 'dat.gui';
 export class PointCloudClassificationFilterUI {
   constructor(ui: dat.GUI, model: CognitePointCloudModel) {
     const classes = model.getClasses();
-    const visibility = classes.reduce((visibility, clazz) => {
-      return {...visibility, [`${clazz}`]: model.isClassVisible(clazz.code) };
+    const visibility = classes.reduce((visibility, pointClass) => {
+      return {...visibility, [`${pointClass}`]: model.isClassVisible(pointClass.code) };
     }, {});
 
-    classes.forEach(clazz => {
-      ui.add(visibility, `${clazz}`).name(clazz.name).onChange((visible: boolean) => {
-        model.setClassVisible(clazz.code, visible);
+    classes.forEach(pointClass => {
+      ui.add(visibility, `${pointClass}`).name(pointClass.name).onChange((visible: boolean) => {
+        model.setClassVisible(pointClass.code, visible);
       });
     });
   }
