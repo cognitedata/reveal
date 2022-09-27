7cd294c1b83b35aa4163bbde6cc159c6c4be14b8
Christopher Tannum <christopher.tannum@cognite.com>
2022-09-27 15:43:51 +0200

    feat: add endpoint for adding 360 images

--- viewer/packages/api/visual-tests/Default.VisualTest.ts
+++ viewer/packages/api/visual-tests/Default.VisualTest.ts
@@ -1,6 +1,7 @@
 /*!
  * Copyright 2022 Cognite AS
  */
+import * as THREE from 'three';
 
 import {
   ViewerTestFixtureComponents,
@@ -9,7 +10,12 @@ import {
 
 // Sanity test for loading model
 export default class DefaultVisualTest extends ViewerVisualTestFixture {
-  public setup(_: ViewerTestFixtureComponents): Promise<void> {
+  public async setup(components: ViewerTestFixtureComponents): Promise<void> {
+    const { viewer } = components;
+    const rotation = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), 0.1);
+    const translation = new THREE.Matrix4().makeTranslation(-18, 1, -13);
+    const transform = translation.multiply(rotation);
+    await viewer.add360ImageSet('events', { site_id: '6th floor v3 - enterprise' }, transform);
     return Promise.resolve();
   }
 }
