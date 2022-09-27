eceab9cde2179ad4132531a9957645a15816b912
Christopher Tannum <christopher.tannum@cognite.com>
2022-09-27 15:58:32 +0200

    fix: revert default visual test

--- viewer/packages/api/visual-tests/Default.VisualTest.ts
+++ viewer/packages/api/visual-tests/Default.VisualTest.ts
@@ -1,7 +1,6 @@
 /*!
  * Copyright 2022 Cognite AS
  */
-import * as THREE from 'three';
 
 import {
   ViewerTestFixtureComponents,
@@ -10,12 +9,7 @@ import {
 
 // Sanity test for loading model
 export default class DefaultVisualTest extends ViewerVisualTestFixture {
-  public async setup(components: ViewerTestFixtureComponents): Promise<void> {
-    const { viewer } = components;
-    const rotation = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), 0.1);
-    const translation = new THREE.Matrix4().makeTranslation(-18, 1, -13);
-    const transform = translation.multiply(rotation);
-    await viewer.add360ImageSet('events', { site_id: '6th floor v3 - enterprise' }, transform);
+  public async setup(_: ViewerTestFixtureComponents): Promise<void> {
     return Promise.resolve();
   }
 }
