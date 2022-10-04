5a3590613baa5ecf9ab56525bb673c5f63af9967
Vegard Økland <vegard.okland@cognite.com>
2022-10-04 15:51:30 +0200

    Setup routing and create empty list component

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,3 +3,4 @@ export const CANVAS_SIDE_PANEL_MIN_SIZE = 320;
 export const CANVAS_SIDE_PANEL_PREFERRED_SIZE = CANVAS_SIDE_PANEL_MIN_SIZE;
 export const CANVAS_DRAG_AND_DROP_DATA_TRANSFER_IDENTIFIER =
   '@cognite/flows-data-transfer';
+export const CANVAS_PATH = 'flows';
---
+++ src/pages/flow-list/index.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+export default function FlowList({}: Props) {
+  return (
+    <section>
+      <h1>Flow list</h1>
+    </section>
+  );
+}
