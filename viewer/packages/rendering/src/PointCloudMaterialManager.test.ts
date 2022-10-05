36418246a8b8445c77e74a69e1d456b18fda49f6
Savelii Novikov <kvaka2000@gmail.com>
2022-10-05 12:42:44 +0200

    lint

--- viewer/packages/rendering/src/PointCloudMaterialManager.test.ts
+++ viewer/packages/rendering/src/PointCloudMaterialManager.test.ts
@@ -2,50 +2,50 @@
  * Copyright 2022 Cognite AS
  */
 
-import * as THREE from "three";
-import { PointCloudMaterialManager } from "./PointCloudMaterialManager";
+import * as THREE from 'three';
+import { PointCloudMaterialManager } from './PointCloudMaterialManager';
 
 describe('PointCloudMaterialManager', () => {
-    let materialManager: PointCloudMaterialManager;
+  let materialManager: PointCloudMaterialManager;
 
-    beforeEach(() => {
-        materialManager = new PointCloudMaterialManager();
-    });
+  beforeEach(() => {
+    materialManager = new PointCloudMaterialManager();
+  });
 
-    test('addModelMaterial creates material and sets corresponding value in the map', () => {
-        const modelIdentifier = Symbol('model');
-        materialManager.addModelMaterial(modelIdentifier);
+  test('addModelMaterial creates material and sets corresponding value in the map', () => {
+    const modelIdentifier = Symbol('model');
+    materialManager.addModelMaterial(modelIdentifier);
 
-        expect(materialManager.getModelMaterial(modelIdentifier)).not.toBeEmpty();
-    });
+    expect(materialManager.getModelMaterial(modelIdentifier)).not.toBeEmpty();
+  });
 
-    test('removeModelMaterial removes material from the map', () => {
-        const modelIdentifier = Symbol('model');
-        materialManager.addModelMaterial(modelIdentifier);
+  test('removeModelMaterial removes material from the map', () => {
+    const modelIdentifier = Symbol('model');
+    materialManager.addModelMaterial(modelIdentifier);
 
-        expect(materialManager.getModelMaterial(modelIdentifier)).not.toBeEmpty();
+    expect(materialManager.getModelMaterial(modelIdentifier)).not.toBeEmpty();
 
-        materialManager.removeModelMaterial(modelIdentifier);
-        
-        expect(() => materialManager.getModelMaterial(modelIdentifier)).toThrow();
-    });
+    materialManager.removeModelMaterial(modelIdentifier);
 
-    test('setModelsMaterialParameters sets material parameters for all models', () => {
-        const modelIdentifier1 = Symbol('model');
-        const modelIdentifier2 = Symbol('model');
-        materialManager.addModelMaterial(modelIdentifier1);
-        materialManager.addModelMaterial(modelIdentifier2);
+    expect(() => materialManager.getModelMaterial(modelIdentifier)).toThrow();
+  });
 
-        const material1 = materialManager.getModelMaterial(modelIdentifier1);
-        const material2 = materialManager.getModelMaterial(modelIdentifier2);
+  test('setModelsMaterialParameters sets material parameters for all models', () => {
+    const modelIdentifier1 = Symbol('model');
+    const modelIdentifier2 = Symbol('model');
+    materialManager.addModelMaterial(modelIdentifier1);
+    materialManager.addModelMaterial(modelIdentifier2);
 
-        const materialParameters = { weighted: true, blending: THREE.AdditiveBlending };
-        materialManager.setModelsMaterialParameters(materialParameters);
+    const material1 = materialManager.getModelMaterial(modelIdentifier1);
+    const material2 = materialManager.getModelMaterial(modelIdentifier2);
 
-        expect(material1.weighted).toBe(materialParameters.weighted);
-        expect(material2.weighted).toBe(materialParameters.weighted);
+    const materialParameters = { weighted: true, blending: THREE.AdditiveBlending };
+    materialManager.setModelsMaterialParameters(materialParameters);
 
-        expect(material1.blending).toBe(materialParameters.blending);
-        expect(material2.blending).toBe(materialParameters.blending);
-    });
+    expect(material1.weighted).toBe(materialParameters.weighted);
+    expect(material2.weighted).toBe(materialParameters.weighted);
+
+    expect(material1.blending).toBe(materialParameters.blending);
+    expect(material2.blending).toBe(materialParameters.blending);
+  });
 });
