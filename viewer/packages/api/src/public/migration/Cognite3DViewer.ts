d75d541bdfcb900dd3ef175e085d24e2fe89720c
Lars Moastuen <lars.moastuen@cognite.com>
2022-10-07 09:17:51 +0200

    refac: remove optional options parameter to Cognite3DViewer.getIntersectionFromPixel

--- viewer/packages/api/src/public/migration/Cognite3DViewer.ts
+++ viewer/packages/api/src/public/migration/Cognite3DViewer.ts
@@ -33,14 +33,7 @@ import {
   PointCloudPickingHandler
 } from '@reveal/pointclouds';
 
-import {
-  AddModelOptions,
-  Cognite3DViewerOptions,
-  Intersection,
-  CadModelBudget,
-  IntersectionFromPixelOptions,
-  CadIntersection
-} from './types';
+import { AddModelOptions, Cognite3DViewerOptions, Intersection, CadModelBudget, CadIntersection } from './types';
 import { RevealManager } from '../RevealManager';
 import { RevealOptions } from '../types';
 
@@ -1078,7 +1071,6 @@ export class Cognite3DViewer {
    * Raycasting model(s) for finding where the ray intersects with the model.
    * @param offsetX X coordinate in pixels (relative to the domElement).
    * @param offsetY Y coordinate in pixels (relative to the domElement).
-   * @param options Options to control the behavior of the intersection operation. Optional (new in 1.3.0).
    * @returns A promise that if there was an intersection then return the intersection object - otherwise it
    * returns `null` if there were no intersections.
    * @see {@link https://en.wikipedia.org/wiki/Ray_casting}.
@@ -1108,20 +1100,6 @@ export class Cognite3DViewer {
    *   ' at this exact point ', intersection.point
    *   );
    * ```
-   */
-  async getIntersectionFromPixel(offsetX: number, offsetY: number): Promise<null | Intersection>;
-  /**
-   * @deprecated Since 3.1 options argument have no effect.
-   */
-  async getIntersectionFromPixel(
-    offsetX: number,
-    offsetY: number,
-    options: IntersectionFromPixelOptions
-  ): Promise<null | Intersection>;
-  /**
-   * @obvious
-   * @param offsetX
-   * @param offsetY
    */
   async getIntersectionFromPixel(offsetX: number, offsetY: number): Promise<null | Intersection> {
     return this.intersectModels(offsetX, offsetY);
