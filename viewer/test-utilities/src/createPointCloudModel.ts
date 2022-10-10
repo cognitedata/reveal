6e1e9fa8556a480e85cc23209aa3ef0037c905b9
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2022-10-10 15:00:14 +0200

    Update viewer/test-utilities/src/createPointCloudModel.ts
    Co-authored-by: Christopher J. Tannum <christopher.tannum@cognite.com>

--- viewer/test-utilities/src/createPointCloudModel.ts
+++ viewer/test-utilities/src/createPointCloudModel.ts
@@ -22,9 +22,7 @@ import { IPointCloudTreeGeometry } from '../../packages/pointclouds/src/potree-t
 export function createPointCloudModel(modelId: number, revisionId: number): CognitePointCloudModel {
   const pointCloudNode = createPointCloudNode();
 
-  const pointCloudModel = new CognitePointCloudModel(modelId, revisionId, pointCloudNode);
-
-  return pointCloudModel;
+ return new CognitePointCloudModel(modelId, revisionId, pointCloudNode);
 }
 
 export function createPointCloudNode(): PointCloudNode {
