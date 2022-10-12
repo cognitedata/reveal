4044467a2296f156fffcd08abc081f042ca6af8e
Christopher Tannum <christopher.tannum@cognite.com>
2022-10-12 18:23:13 +0200

    chore: add primitive test fixture

--- viewer/packages/sector-parser/visual-tests/GeneralCylinder.VisualTest.ts
+++ viewer/packages/sector-parser/visual-tests/GeneralCylinder.VisualTest.ts
@@ -2,58 +2,79 @@
  * Copyright 2022 Cognite AS
  */
 import * as THREE from 'three';
-import {
-  createPrimitiveInterleavedGeometry,
-  GeneralCylinder,
-  PrimitiveName
-} from '../../../test-utilities/src/primitives';
-import { SimpleTestFixtureComponents, SimpleVisualTestFixture } from '../../../visual-tests';
-import { setConeGeometry } from '../src/reveal-glb-parser/primitiveGeometries';
-import { RevealGeometryCollectionType } from '../src/types';
-import { getMaterialsMap } from './testMaterials';
+import { GeneralCylinder, PrimitiveName } from '../../../test-utilities/src/primitives';
+import { PrimitivesVisualTestFixture } from './PrimitivesVisualTestFixture';
 
-export default class GeneralCylinderVisualTest extends SimpleVisualTestFixture {
-  public setup(simpleTestFixtureComponents: SimpleTestFixtureComponents): Promise<void> {
-    const { camera, scene } = simpleTestFixtureComponents;
+type GeneralCylinderAttributes = {
+  treeIndex: number;
+  color: THREE.Vector4;
+  centerA: THREE.Vector3;
+  centerB: THREE.Vector3;
+  radius: number;
+  planeA: THREE.Vector4;
+  planeB: THREE.Vector4;
+  localXAxis: THREE.Vector3;
+  angle: number;
+  arcAngle: number;
+};
 
-    const generalCylinderDescriptor = {
-      treeIndex: 0,
-      color: [0, 0, 0, 1],
-      centerA: [0, 0, 1.4663076400756836],
-      centerB: [0, 0, -1],
-      radius: 0.2,
-      planeA: [0.4161977469921112, 0.07338689267635345, 0.9063078165054321, 2],
-      planeB: [0, 0, -1, 0],
-      localXAxis: [1, 0, 0],
-      angle: 0,
-      arcAngle: Math.PI * 2
-    } as GeneralCylinder;
+export default class GeneralCylinderVisualTest extends PrimitivesVisualTestFixture {
+  constructor() {
+    const testCylinders: GeneralCylinderAttributes[] = [
+      {
+        treeIndex: 0,
+        color: new THREE.Vector4(1, 0, 0, 1),
+        centerA: new THREE.Vector3(0, 0.5, 1),
+        centerB: new THREE.Vector3(0, 0, 0.5),
+        radius: 0.2,
+        planeA: new THREE.Vector4(0.4161, 0.0733, 0.9063, 2),
+        planeB: new THREE.Vector4(0, 0, -1, 0),
+        localXAxis: new THREE.Vector3(1, 0, 0),
+        angle: 0,
+        arcAngle: Math.PI * 2
+      },
+      {
+        treeIndex: 1,
+        color: new THREE.Vector4(255, 0, 0, 1),
+        centerA: new THREE.Vector3(0, 0.5, 1),
+        centerB: new THREE.Vector3(0, 0, 0.5),
+        radius: 0.6,
+        planeA: new THREE.Vector4(0.4161, 0.0733, 0.9063, 2),
+        planeB: new THREE.Vector4(0, 0, -1, 0),
+        localXAxis: new THREE.Vector3(1, 0, 0),
+        angle: 0,
+        arcAngle: Math.PI * 1.5
+      },
+      {
+        treeIndex: 2,
+        color: new THREE.Vector4(255, 0, 0, 1),
+        centerA: new THREE.Vector3(2, 0, -1),
+        centerB: new THREE.Vector3(-2, 0, -1),
+        radius: 0.5,
+        planeA: new THREE.Vector4(0.577, 0.577, 0.577, 2),
+        planeB: new THREE.Vector4(0.577, 0.577, -0.577, 0),
+        localXAxis: new THREE.Vector3(0, 1, 0),
+        angle: Math.PI * 0.3,
+        arcAngle: Math.PI * 1.7
+      }
+    ];
 
-    const group = this.initializeGroup(scene);
-    const geometry = createPrimitiveInterleavedGeometry(PrimitiveName.GeneralCylinder, [generalCylinderDescriptor]);
-    setConeGeometry(geometry);
-    const materials = getMaterialsMap(1);
+    super(PrimitiveName.GeneralCylinder, testCylinders.map(getCylinderFromAttributes));
 
-    const generalCylinderMaterial = materials.get(RevealGeometryCollectionType.GeneralCylinderCollection)!;
-    const mesh = new THREE.Mesh(geometry, generalCylinderMaterial);
-    mesh.frustumCulled = false;
-    mesh.onBeforeRender = () => {
-      (generalCylinderMaterial.uniforms.inverseModelMatrix?.value as THREE.Matrix4)?.copy(mesh.matrixWorld).invert();
-      (generalCylinderMaterial.uniforms.cameraPosition?.value as THREE.Vector3)?.copy(camera.position);
-    };
-    group.add(mesh);
-
-    camera.position.set(0, 0, -3);
-    camera.lookAt(0, 0, 0);
-
-    return Promise.resolve();
-  }
-
-  private initializeGroup(scene: THREE.Scene) {
-    const group = new THREE.Group();
-    group.frustumCulled = false;
-    group.applyMatrix4(this.cadFromCdfToThreeMatrix);
-    scene.add(group);
-    return group;
+    function getCylinderFromAttributes(attributes: GeneralCylinderAttributes): GeneralCylinder {
+      const { treeIndex, color, centerA, centerB, planeA, planeB, radius, angle, arcAngle, localXAxis } = attributes;
+      return {
+        treeIndex,
+        color: color.toArray(),
+        centerA: centerA.toArray(),
+        centerB: centerB.toArray(),
+        planeA: planeA.toArray(),
+        planeB: planeB.toArray(),
+        radius,
+        angle,
+        arcAngle,
+        localXAxis: localXAxis.toArray()
+      };
+    }
   }
 }
