62d3f1904307746ff55fa19336234f1284989e1c
Stefan Maretić <stefan.maretic@cognite.com>
2022-10-13 13:59:23 +0200

    Update function signature

--- src/hooks/useResourceAggregates.ts
+++ src/hooks/useResourceAggregates.ts
@@ -1,8 +1,9 @@
 import sdk from '@cognite/cdf-sdk-singleton';
 import { useQueries } from 'react-query';
 
-const callWithDataSetFilters = (fn: (object: any) => any) => (id: number) =>
-  fn({ filter: { dataSetIds: [{ id }] } });
+const getDataSetFilter = (id: number) => ({
+  filter: { dataSetIds: [{ id }] },
+});
 
 export function useResourceAggregates(dataSetId: number) {
   const getQueryKey = (resource: string) => [resource, 'aggregate', dataSetId];
@@ -10,29 +11,23 @@ export function useResourceAggregates(dataSetId: number) {
   return useQueries([
     {
       queryKey: getQueryKey('assets'),
-      queryFn: () => callWithDataSetFilters(sdk.assets.aggregate)(dataSetId),
+      queryFn: () => sdk.assets.aggregate(getDataSetFilter(dataSetId)),
     },
     {
       queryKey: getQueryKey('timeseries'),
-      queryFn: () =>
-        callWithDataSetFilters(sdk.timeseries.aggregate)(dataSetId),
+      queryFn: () => sdk.timeseries.aggregate(getDataSetFilter(dataSetId)),
     },
     {
       queryKey: getQueryKey('files'),
-      queryFn: () => callWithDataSetFilters(sdk.files.aggregate)(dataSetId),
+      queryFn: () => sdk.files.aggregate(getDataSetFilter(dataSetId)),
     },
     {
       queryKey: getQueryKey('events'),
-      queryFn: () =>
-        sdk.events.aggregate.count({
-          filter: {
-            dataSetIds: [{ id: dataSetId }],
-          },
-        }),
+      queryFn: () => sdk.events.aggregate.count(getDataSetFilter(dataSetId)),
     },
     {
       queryKey: getQueryKey('sequences'),
-      queryFn: () => callWithDataSetFilters(sdk.sequences.aggregate)(dataSetId),
+      queryFn: () => sdk.sequences.aggregate(getDataSetFilter(dataSetId)),
     },
   ]);
 }
