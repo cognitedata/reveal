a912da232ee18753a91a310325920345d013fcf3
Håkon Flatval <hakon.flatval@cognite.com>
2022-10-17 16:16:35 +0200

    fix: clarify promise awaiting with Promise.all

--- viewer/packages/pointclouds/src/PointCloudFactory.ts
+++ viewer/packages/pointclouds/src/PointCloudFactory.ts
@@ -43,8 +43,8 @@ export class PointCloudFactory {
     const annotationInfoPromise = this._pointCloudObjectProvider.getPointCloudObjects(modelIdentifier);
     const classSchemaPromise = this._classificationsProvider.getClassifications(modelMetadata);
 
-    const annotationInfo = await annotationInfoPromise;
-    const classSchema = await classSchemaPromise;
+    const [annotationInfo, classSchema] = await Promise.all([annotationInfoPromise,
+                                                             classSchemaPromise]);
 
     this._pointCloudMaterialManager.addModelMaterial(modelIdentifier.revealInternalId,
                                                      createObjectIdMaps(annotationInfo));
