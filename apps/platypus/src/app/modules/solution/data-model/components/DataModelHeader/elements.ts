d93c081953a064305387b57633472207e65805e5
23andreas <andreas.skonberg@gmail.com>
2022-10-24 09:26:45 +0200

    add !important to button background (#692)

--- apps/platypus/src/app/modules/solution/data-model/components/DataModelHeader/elements.ts
+++ apps/platypus/src/app/modules/solution/data-model/components/DataModelHeader/elements.ts
@@ -9,14 +9,14 @@ export const DiscardButton = styled(Button)`
 // These colors are from the secondary variant of the Button in our design system
 // For some reason the Cogs secondary Button component uses a different color
 export const ReturnButton = styled(Button)`
-  background: rgba(83, 88, 127, 0.08);
+  background: rgba(83, 88, 127, 0.08) !important;
 
   &:hover {
-    background: rgba(83, 88, 127, 0.12);
+    background: rgba(83, 88, 127, 0.12) !important;
   }
 
   &:active {
-    background: rgba(83, 88, 127, 0.16);
+    background: rgba(83, 88, 127, 0.16) !important;
   }
 `;
 
