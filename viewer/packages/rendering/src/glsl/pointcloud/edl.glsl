61d3d3b597b9a18bf714524d1fe2e8d9be58717f
Håkon Flatval <hakon.flatval@cognite.com>
2022-11-01 09:34:37 +0100

    format: remove whitespace from line ends

--- viewer/packages/rendering/src/glsl/pointcloud/edl.glsl
+++ viewer/packages/rendering/src/glsl/pointcloud/edl.glsl
@@ -1,23 +1,23 @@
-// Based on Potree shader and https://github.com/CloudCompare/CloudCompare/blob/master/plugins/core/GL/qEDL/shaders/EDL/edl_shade.frag 
+// Based on Potree shader and https://github.com/CloudCompare/CloudCompare/blob/master/plugins/core/GL/qEDL/shaders/EDL/edl_shade.frag
 // Function calculating obscurance of specified fragment.
 // Passed colorTexture is assumed to have logarithmic depth value written to alpha channel.
 
 #define MAX_FLOAT 3.402823466e+38;
 
-vec2 calculateObscurance(float depth, float screenWidth, float screenHeight, vec2 neighbours[NEIGHBOUR_COUNT], 
+vec2 calculateObscurance(float depth, float screenWidth, float screenHeight, vec2 neighbours[NEIGHBOUR_COUNT],
 	float radius, vec2 vUv, sampler2D depthTexture, sampler2D colorTexture) {
-		
+
 	vec2 uvRadius = radius / vec2(screenWidth, screenHeight);
 
 	float sum = 0.0;
-	float minNeighbourDepth = MAX_FLOAT; 
+	float minNeighbourDepth = MAX_FLOAT;
 	vec2 minNeighbourUv = vec2(0.0, 0.0);
 
 	for(int i = 0; i < NEIGHBOUR_COUNT; i++){
 		vec2 uvNeighbor = vUv + uvRadius * neighbours[i];
 
 		float neighbourDepth = texture(colorTexture, uvNeighbor).a;
-		
+
 		if (neighbourDepth < minNeighbourDepth) {
 			minNeighbourDepth = neighbourDepth;
 			minNeighbourUv = uvNeighbor;
