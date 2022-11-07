2845895e4ac1fc294890a0070f7b5507c5e65eee
Deep <70804363+deep-cognite@users.noreply.github.com>
2022-11-07 15:09:03 +0100

    fix: cogs styling overrides to work with subapps (#456)
    * fix: cogs styling overrides to work with subapps
    
    * fix: lint issue

--- src/context/DataExplorationContext.tsx
+++ src/context/DataExplorationContext.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CogniteClient } from '@cognite/sdk';
 import { ResourcePreviewProvider } from 'context/ResourcePreviewContext';
 import { ResourceSelectorProvider } from 'context/ResourceSelectorContext';
@@ -6,12 +6,14 @@ import { FileContextualizationContextProvider } from 'context/FileContextualizat
 import { SDKProvider } from '@cognite/sdk-provider';
 import { CogniteFileViewer } from '@cognite/react-picture-annotation';
 import { Flow, AppContextProvider, OverrideURLMap } from './AppContext';
+import { Tooltip } from '@cognite/cogs.js';
 
 export type DataExplorationProviderProps = {
   flow: Flow;
   overrideURLMap?: OverrideURLMap;
   sdk: CogniteClient;
   userInfo: any;
+  styleScopeId?: string;
 };
 
 export const DataExplorationProvider = ({
@@ -20,30 +22,42 @@ export const DataExplorationProvider = ({
   overrideURLMap,
   sdk,
   userInfo,
+  styleScopeId,
 }: DataExplorationProviderProps & {
   children: React.ReactNode;
-}) => (
-  <SDKProvider sdk={sdk}>
-    <CogniteFileViewer.Provider
-      sdk={sdk}
-      disableAutoFetch
-      overrideURLMap={{
-        ...(overrideURLMap?.pdfjsWorkerSrc && {
-          pdfjsWorkerSrc: overrideURLMap?.pdfjsWorkerSrc,
-        }),
-      }}
-    >
-      <AppContextProvider
-        flow={flow}
-        overrideURLMap={overrideURLMap}
-        userInfo={userInfo}
+}) => {
+  useEffect(() => {
+    if (!styleScopeId) return;
+
+    Tooltip.defaultProps = {
+      ...Tooltip.defaultProps,
+      appendTo: () => document.getElementsByClassName(styleScopeId).item(0)!,
+    };
+  }, [styleScopeId]);
+
+  return (
+    <SDKProvider sdk={sdk}>
+      <CogniteFileViewer.Provider
+        sdk={sdk}
+        disableAutoFetch
+        overrideURLMap={{
+          ...(overrideURLMap?.pdfjsWorkerSrc && {
+            pdfjsWorkerSrc: overrideURLMap?.pdfjsWorkerSrc,
+          }),
+        }}
       >
-        <FileContextualizationContextProvider>
-          <ResourcePreviewProvider>
-            <ResourceSelectorProvider>{children}</ResourceSelectorProvider>
-          </ResourcePreviewProvider>
-        </FileContextualizationContextProvider>
-      </AppContextProvider>
-    </CogniteFileViewer.Provider>
-  </SDKProvider>
-);
+        <AppContextProvider
+          flow={flow}
+          overrideURLMap={overrideURLMap}
+          userInfo={userInfo}
+        >
+          <FileContextualizationContextProvider>
+            <ResourcePreviewProvider>
+              <ResourceSelectorProvider>{children}</ResourceSelectorProvider>
+            </ResourcePreviewProvider>
+          </FileContextualizationContextProvider>
+        </AppContextProvider>
+      </CogniteFileViewer.Provider>
+    </SDKProvider>
+  );
+};
