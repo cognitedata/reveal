01c9ba6af720c49f25a22ee92d798e9e70d913c7
JiMin <borajimin@gmail.com>
2022-11-08 14:46:30 +0100

    fix: unit test failure (#727)
    * fix: unit test failure
    
    * Update scripts/test.sh
    
    Co-authored-by: 23andreas <andreas.skonberg@gmail.com>
    
    * remove spaces
    
    * update: jest dependencies
    
    Co-authored-by: 23andreas <andreas.skonberg@gmail.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,14 +5,12 @@ set -e
 project="${1:-platypus,platypus-core,platypus-common-utils}"
 
 nx run-many --target=test --projects=${project} "$@" \
-  --collectCoverageFrom='!*/**/*.stories.tsx' \
   --ci \
   --verbose \
   --runInBand \
-  --codeCoverage \
   --detectOpenHandles \
   --reporters=default \
   --reporters=jest-junit \
   --coverageReporters=html,lcov,json,text-summary,cobertura \
+  --collectCoverage \
   --watchAll=false
-
