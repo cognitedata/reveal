9957dc46065f8787a8b640de76bb8b34ab505d1a
Christopher Tannum <christopher.tannum@cognite.com>
2022-11-15 16:55:50 +0100

    feat: add preloading to api helper

--- viewer/packages/api/src/api-helpers/Image360ApiHelper.ts
+++ viewer/packages/api/src/api-helpers/Image360ApiHelper.ts
@@ -131,13 +131,18 @@ export class Image360ApiHelper {
       { x: ndcCoordinates.x, y: ndcCoordinates.y },
       this._activeCameraManager.getCamera()
     );
+
+    if (entity === this._interactionState.lastHoveredState) {
+      return;
+    }
+
     if (entity !== undefined) {
       entity.icon.hoverSpriteVisible = true;
+      this._image360Facade.preload(entity);
     }
-    if (entity !== this._interactionState.lastHoveredState) {
-      this._requestRedraw();
-      this._interactionState.lastHoveredState = entity;
-    }
+
+    this._requestRedraw();
+    this._interactionState.lastHoveredState = entity;
   }
 
   private exit360ImageOnEscape(event: KeyboardEvent) {
