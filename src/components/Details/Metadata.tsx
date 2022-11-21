cc071b6f517a19926f55c883b1ba5d2e0008987c
manoj-cognite <91716778+manoj-cognite@users.noreply.github.com>
2022-11-21 14:41:38 +0530

    fix: Filter color adjustment (#521)

--- src/components/Details/Metadata.tsx
+++ src/components/Details/Metadata.tsx
@@ -1,8 +1,14 @@
 import React, { useState, useMemo } from 'react';
-import { Button, Input, Title } from '@cognite/cogs.js';
-import styled from 'styled-components';
+import { Button, Title } from '@cognite/cogs.js';
 import { Table } from 'components/Table';
 import { ColumnDef } from '@tanstack/react-table';
+import {
+  FilterContainer,
+  FilterInput,
+  MetadataCard,
+  MetadataHeader,
+  MetadataTableContainer,
+} from './elements';
 
 // TODO  Needs to be removed once implemented in our library
 interface DataSource {
@@ -61,7 +67,7 @@ export function Metadata({ metadata }: { metadata?: { [k: string]: string } }) {
       <MetadataHeader>
         <Title level={5}>Metadata</Title>
         <FilterContainer>
-          <Input
+          <FilterInput
             value={query}
             placeholder="Filter"
             size="small"
@@ -89,42 +95,3 @@ export function Metadata({ metadata }: { metadata?: { [k: string]: string } }) {
     </MetadataCard>
   );
 }
-
-const MetadataCard = styled.div`
-  background-color: var(--cogs-surface--medium);
-  border-radius: 8px;
-`;
-
-const MetadataHeader = styled.div`
-  padding: 16px 12px;
-  height: 53px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  border-bottom: 1px solid var(--cogs-border--muted);
-`;
-
-const MetadataTableContainer = styled.div`
-  white-space: pre;
-  overflow: auto;
-
-  & > div {
-    padding: 0px;
-  }
-
-  .data-exploration-table {
-    background-color: var(--cogs-surface-medium);
-    & > div {
-      max-width: 100%;
-    }
-  }
-`;
-
-const FilterContainer = styled.div`
-  display: flex;
-  margin-bottom: 16px;
-
-  .cogs-input {
-    margin-right: 8px;
-  }
-`;
---
+++ src/components/Details/elements.ts
@@ -0,0 +1,45 @@
+import { Input } from '@cognite/cogs.js';
+import styled from 'styled-components';
+
+export const MetadataCard = styled.div`
+  background-color: var(--cogs-surface--medium);
+  border-radius: 8px;
+`;
+
+export const MetadataHeader = styled.div`
+  padding: 16px 12px;
+  height: 53px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid var(--cogs-border--muted);
+`;
+
+export const MetadataTableContainer = styled.div`
+  white-space: pre;
+  overflow: auto;
+
+  & > div {
+    padding: 0px;
+  }
+
+  .data-exploration-table {
+    background-color: var(--cogs-surface-medium);
+    & > div {
+      max-width: 100%;
+    }
+  }
+`;
+
+export const FilterContainer = styled.div`
+  display: flex;
+  margin-bottom: 16px;
+
+  .cogs-input {
+    margin-right: 8px;
+  }
+`;
+
+export const FilterInput = styled(Input)`
+  background: #efefef !important;
+`;
