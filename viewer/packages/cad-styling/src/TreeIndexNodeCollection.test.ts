87a1bbac6d895249ce615286e8094745d2bf2481
Lars Moastuen <lars.moastuen@cognite.com>
2022-11-22 16:12:23 +0100

    fix: TreeIndexNodeCollection.updateSet erroneously clears IndexSet (#2710)

---
+++ viewer/packages/cad-styling/src/TreeIndexNodeCollection.test.ts
@@ -0,0 +1,15 @@
+/*!
+ * Copyright 2022 Cognite AS
+ */
+import { IndexSet, NumericRange } from '@reveal/utilities';
+import { TreeIndexNodeCollection } from './TreeIndexNodeCollection';
+
+describe(TreeIndexNodeCollection.classToken, () => {
+  test('updateSet with same IndexSet as current, doesnt clear set', () => {
+    const set = new IndexSet();
+    const collection = new TreeIndexNodeCollection(set);
+    set.addRange(new NumericRange(1, 10));
+    collection.updateSet(set);
+    expect(set.count).not.toEqual(0);
+  });
+});
