87f6aabda58c14d1b20aea126fb7a37731fce3ea
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2022-11-28 10:16:46 +0100

    feat: hide unreleased extractors if feature flag is not enabled

--- src/hooks/useExtractorsList.ts
+++ src/hooks/useExtractorsList.ts
@@ -1,8 +1,25 @@
+import { useFlag } from '@cognite/react-feature-flags';
 import { useQuery } from 'react-query';
 import { getExtractorsWithReleases } from 'service/extractors';
 
-export const getExtractorsListQueryKey = () => ['extractors', 'list'];
+export const getExtractorsListQueryKey = (shouldShowUnreleased: boolean) => [
+  'extractors',
+  'list',
+  shouldShowUnreleased,
+];
 
 export const useExtractorsList = () => {
-  return useQuery(getExtractorsListQueryKey(), getExtractorsWithReleases);
+  const { isEnabled: shouldShowUnreleased } = useFlag(
+    'FUSION_UNRELEASED_EXTRACTORS'
+  );
+
+  return useQuery(getExtractorsListQueryKey(shouldShowUnreleased), async () => {
+    const extractorsWithReleases = await getExtractorsWithReleases();
+
+    if (!shouldShowUnreleased) {
+      return extractorsWithReleases.filter(({ type }) => type !== 'unreleased');
+    }
+
+    return extractorsWithReleases;
+  });
 };
