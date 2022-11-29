f0d8fc02169eca7ad119ce6c978ccf2d4154d06d
Håkon Flatval <hakon.flatval@cognite.com>
2022-11-29 14:33:32 +0100

    improvement: strip down `Image360Visualization` even more

--- viewer/packages/360-images/src/Image360Visualization.ts
+++ viewer/packages/360-images/src/Image360Visualization.ts
@@ -1,23 +1,13 @@
 /*!
  * Copyright 2022 Cognite AS
  */
-import * as THREE from 'three';
 
+/**
+ * Interface to control visualization of a 360 image
+ */
 export interface Image360Visualization {
   /**
    * Opaqueness of the 360 image
    */
   opacity: number;
-  /**
-   * Visibility of the 360 image
-   */
-  set visible(value: boolean);
-  /**
-   * Scale of the box containing the 360 image
-   */
-  set scale(value: THREE.Vector3);
-  /**
-   * Render order of the mesh containing the 360 image
-   */
-  set renderOrder(value: number);
 }
