87a7b040e16f5c904e2747c9d2fbc2aec2a77306
Stefan Maretić <stefan.maretic@cognite.com>
2022-11-30 13:19:24 +0100

    Change document.title

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const App = () => {
       >
         <QueryClientProvider client={queryClient}>
           <GlobalStyles>
-            <SubAppWrapper title="Data Sets">
+            <SubAppWrapper title="Data Catalog">
               <AuthWrapper
                 loadingScreen={<Loader />}
                 login={() => loginAndAuthIfNeeded(projectName, env)}
