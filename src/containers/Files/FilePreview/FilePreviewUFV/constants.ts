350f03f3da86176042287a4526c53c6dcc23d593
Marvin <marvinreza@outlook.com>
2022-12-01 17:05:14 +0100

    fix: increase max container width and height (#580)
    As per
    https://cognitedata.slack.com/archives/C019GLXD0T0/p1669883030459099?thread_ts=1669714686.892559&cid=C019GLXD0T0,
    the previewing quality of larger P&ID documents can be quite bad. This
    commit alleviates this issue by increasing the maximum allowed container
    width and height. The values chosen here was adjusted so that the
    document mentioned by the user at least will look good.
    
    Co-authored-by: Marvin Reza <marvinreza@Marvins-MacBook-Pro.local>

--- src/containers/Files/FilePreview/FilePreviewUFV/constants.ts
+++ src/containers/Files/FilePreview/FilePreviewUFV/constants.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_ZOOM_SCALE = 0.8;
-export const MAX_CONTAINER_WIDTH = 1500;
-export const MAX_CONTAINER_HEIGHT = 1000;
+export const MAX_CONTAINER_WIDTH = 5000;
+export const MAX_CONTAINER_HEIGHT = 5000;
