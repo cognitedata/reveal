53ebe8c4d6c32af53f6f73064c7299ac884d65b6
23andreas <andreas.skonberg@gmail.com>
2022-12-15 11:10:46 +0100

    Refactor CI/CD (#793)
    * test
    
    * test
    
    * add run-many.js
    
    * rename workflow
    
    * test
    
    * exclude platypus-cdf-cli e2e tests from test task
    
    * remove old failing test
    
    * test
    
    * test
    
    * add cypress project id
    
    * test
    
    * test
    
    * test
    
    * remove e2e
    
    * remove lint and test from e2e workflow
    
    * remove cypress
    
    * revert cypress stuff
    
    * add workflow to secrets exclusions
    
    * fix ","
    
    * remove empty line
    
    * fix typo
    
    * ignore e2e tests
    
    * fix
    
    Co-authored-by: JiMin <borajimin@gmail.com>

---
+++ scripts/run-many.js
@@ -0,0 +1,29 @@
+const execSync = require('child_process').execSync;
+
+const target = process.argv[2];
+const jobIndex = Number(process.argv[3]);
+const jobCount = Number(process.argv[4]);
+const baseSha = process.argv[5];
+const headSha = process.argv[6];
+
+const affected = execSync(
+  `npx nx print-affected --base=${baseSha} --head=${headSha} --target=${target}`
+).toString('utf-8');
+const array = JSON.parse(affected)
+  .tasks.map((t) => t.target.project)
+  .slice()
+  .sort();
+const sliceSize = Math.max(Math.floor(array.length / jobCount), 1);
+const projects =
+  jobIndex < jobCount
+    ? array.slice(sliceSize * (jobIndex - 1), sliceSize * jobIndex)
+    : array.slice(sliceSize * (jobIndex - 1));
+
+if (projects.length > 0) {
+  execSync(
+    `npx nx run-many --configuration=ci --target=${target} --projects=${projects} --parallel`,
+    {
+      stdio: [0, 1, 2],
+    }
+  );
+}
