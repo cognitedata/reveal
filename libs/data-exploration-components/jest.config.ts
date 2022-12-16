5e8f36b5bf6726cf5630d073cc16ba762c496683
Deep Singh <deep.singh@cognite.com>
2022-12-16 14:52:31 +0100

    fix: remove tests

--- libs/data-exploration-components/jest.config.ts
+++ libs/data-exploration-components/jest.config.ts
@@ -1,9 +1,14 @@
-/* eslint-disable */
 export default {
   displayName: 'data-exploration-components',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!d3|d3-array|internmap|delaunator|robust-predicates|@cognite/unified-file-viewer)"
+  ],
+  moduleNameMapper: {
+    'canvas': 'jest-canvas-mock',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/data-exploration-components',
