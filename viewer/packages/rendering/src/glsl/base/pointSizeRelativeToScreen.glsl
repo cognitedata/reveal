da701a073cc6d55c060ea8dc5f7f14af8036522a
Håkon Flatval <hakon.flatval@cognite.com>
2022-12-20 16:23:10 +0100

    fix: correct shader typos

--- viewer/packages/rendering/src/glsl/base/pointSizeRelativeToScreen.glsl
+++ viewer/packages/rendering/src/glsl/base/pointSizeRelativeToScreen.glsl
@@ -2,4 +2,4 @@
 // The unit of point sizes, relative to the screen size
 // A value of e.g. 1 / 720 means that 720 points of fixed unit size 1 will
 // fit in the screen height. This is the basis for computation for adaptive point size and EDL radius
-#define point_size_relative_to_screen_height (1.0 / 720)
+#define point_size_relative_to_screen_height (1.0 / 720.0)
