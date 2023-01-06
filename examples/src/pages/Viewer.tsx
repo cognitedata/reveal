782b88f33885a381627c104a7c5b3f62745c1ce0
Astrid Kløve-Graue <astrid.graue@cognite.com>
2023-01-06 09:29:51 +0100

    Reorder attributes again for simpler diff

--- examples/src/pages/Viewer.tsx
+++ examples/src/pages/Viewer.tsx
@@ -270,25 +270,25 @@ export function Viewer() {
             canvasWrapperRef.current!.style.position = 'relative';
             canvasWrapperRef.current!.style.width = '';
             canvasWrapperRef.current!.style.height = '';
+            canvasWrapperRef.current!.style.flexGrow = '';
             canvasWrapperRef.current!.style.left = '';
             canvasWrapperRef.current!.style.top = '';
-            canvasWrapperRef.current!.style.flexGrow = '';
             break;
           case 'halfScreen':
             canvasWrapperRef.current!.style.position = 'absolute';
             canvasWrapperRef.current!.style.width = '50%';
             canvasWrapperRef.current!.style.height = '100%';
+            canvasWrapperRef.current!.style.flexGrow = '1';
             canvasWrapperRef.current!.style.left = '25%';
             canvasWrapperRef.current!.style.top = '0%';
-            canvasWrapperRef.current!.style.flexGrow = '1';
             break;
           case 'quarterScreen':
             canvasWrapperRef.current!.style.position = 'absolute';
             canvasWrapperRef.current!.style.width = '50%';
             canvasWrapperRef.current!.style.height = '50%';
+            canvasWrapperRef.current!.style.flexGrow = '0.5';
             canvasWrapperRef.current!.style.left = '25%';
             canvasWrapperRef.current!.style.top = '25%';
-            canvasWrapperRef.current!.style.flexGrow = '0.5';
             break;
         }
       });
