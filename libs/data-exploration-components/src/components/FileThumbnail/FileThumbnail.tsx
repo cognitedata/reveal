24952104fc3239e452af009325e2b8c373376e38
Mihil Ranathunga <mihil.ranathunga@cognite.com>
2023-01-10 16:04:43 +0530

    fix: Fix excel file preview overflow (#937)
    * Fix excel preview overflowing
    
    * Fix review comments
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- libs/data-exploration-components/src/components/FileThumbnail/FileThumbnail.tsx
+++ libs/data-exploration-components/src/components/FileThumbnail/FileThumbnail.tsx
@@ -4,12 +4,13 @@ import { Body, DocumentIcon } from '@cognite/cogs.js';
 import { Loader } from '@data-exploration-components/components';
 import { FileInfo } from '@cognite/sdk';
 import { Document } from '@data-exploration-components/domain/documents';
+import Styled from 'styled-components';
 
 export const FileThumbnail = ({ file }: { file: FileInfo | Document }) => {
   const { data: filePreviewUrl, isError, isFetching } = useFilePreviewURL(file);
 
   if (filePreviewUrl) {
-    return <img src={filePreviewUrl} height="200px" alt="" />;
+    return <ImagePreview src={filePreviewUrl} alt="" />;
   }
   if (!isError && isFetching) {
     return <Loader />;
@@ -21,3 +22,7 @@ export const FileThumbnail = ({ file }: { file: FileInfo | Document }) => {
     </>
   );
 };
+
+const ImagePreview = Styled.img`
+ max-height: 200px;
+`;
