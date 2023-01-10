995cbcdb5735f024b5abef6c58a3d45d1deb075a
Håkon Flatval <hakon.flatval@cognite.com>
2023-01-10 13:15:12 +0100

    chore: add clipping plane UI for models

---
+++ examples/src/utils/ClippingUIs.ts
@@ -0,0 +1,27 @@
+import * as dat from 'dat.gui';
+
+import { Cognite3DViewer, CogniteCadModel, CognitePointCloudModel } from '@cognite/reveal';
+import { SingleClippingUI } from './SingleClippingUI';
+
+export class ClippingUIs {
+  private _rootUi: dat.GUI;
+  private _globalUi: SingleClippingUI;
+  private _modelCount = 0;
+
+  constructor(uiFolder: dat.GUI, viewer: Cognite3DViewer) {
+    this._rootUi = uiFolder;
+    const globalFolder = uiFolder.addFolder('Global');
+    this._globalUi = new SingleClippingUI(globalFolder, planes => viewer.setClippingPlanes(planes));
+  }
+
+  updateWorldBounds(bounds: THREE.Box3) {
+    this._globalUi.updateBounds(bounds);
+  }
+
+  addModel(model: CogniteCadModel | CognitePointCloudModel) {
+    this._modelCount++;
+    const modelFolder = this._rootUi.addFolder(`Model #${this._modelCount}`);
+    const clippingUi = new SingleClippingUI(modelFolder, planes => model.setClippingPlanes(planes));
+    clippingUi.updateBounds(model.getModelBoundingBox());
+  }
+}
