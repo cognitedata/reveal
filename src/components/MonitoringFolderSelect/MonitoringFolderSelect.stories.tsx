e61b99f1f416cafe3bcfffcd53aa9ff79855ff72
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2023-01-18 11:52:14 +0100

    feat(monitoring) : [Part-1] add select component for selecting/adding folders (#1395)
    * Added Monitoring Folder Select Component
    
    * Updated input element
    
    * Adding story
    
    * Updating
    
    * Removing JSON.stringify
    
    * Removing inputref
    
    * Updating
    
    * Feedback
    
    * Update src/components/MonitoringFolderSelect/hooks.tsx
    
    Co-authored-by: Shekhar K. Sharma <shekhar.sharma@cognite.com>
    
    * Feedback
    
    Co-authored-by: Shekhar K. Sharma <shekhar.sharma@cognite.com>

---
+++ src/components/MonitoringFolderSelect/MonitoringFolderSelect.stories.tsx
@@ -0,0 +1,40 @@
+/**
+ * MonitoringFolderSelect story
+ */
+import { Meta, Story } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useForm } from 'react-hook-form';
+import MonitoringFolderSelect from './MonitoringFolderSelect';
+
+type Props = React.ComponentProps<typeof MonitoringFolderSelect>;
+const queryClient = new QueryClient();
+
+export default {
+  component: MonitoringFolderSelect,
+  title: 'Components/MonitoringFolderSelect/MonitoringFolderSelect',
+  decorators: [
+    (story) => (
+      <QueryClientProvider client={queryClient}>{story()}</QueryClientProvider>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<Props> = (args) => {
+  const { control } = useForm({
+    mode: 'all',
+    defaultValues: {
+      name: '',
+    },
+  });
+  return (
+    <div style={{ width: '400px' }}>
+      <MonitoringFolderSelect control={control} {...args} />
+    </div>
+  );
+};
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  inputName: 'name',
+};
---
+++ src/components/MonitoringFolderSelect/MonitoringFolderSelect.tsx
@@ -0,0 +1,151 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { Divider, Row } from 'antd';
+import { Controller } from 'react-hook-form';
+
+import { Button, Select, toast } from '@cognite/cogs.js';
+import { makeDefaultTranslations } from 'utils/translations';
+import { delay, head } from 'lodash';
+import styled from 'styled-components';
+import { useCreateMonitoringFolder, useMonitoringFolders } from './hooks';
+
+const defaultTranslations = makeDefaultTranslations(
+  'Unable to create monitoring job',
+  'Folder is required',
+  'Create folder:'
+);
+
+type Props = {
+  translations?: typeof defaultTranslations;
+  control: any;
+  inputName: string;
+};
+const MonitoringFolderSelect: React.FC<Props> = ({
+  translations,
+  control,
+  inputName,
+}) => {
+  const t = {
+    ...defaultTranslations,
+    ...translations,
+  };
+  const { data: folderList } = useMonitoringFolders();
+  const {
+    mutate: createMonitoringJob,
+    isError: createMonitoringJobError,
+    error: createMonitoringJobErrorMsg,
+  } = useCreateMonitoringFolder();
+  const [name, setName] = useState('');
+
+  const createMonitoringJobErrorText = t['Unable to create monitoring job'];
+
+  useEffect(() => {
+    if (createMonitoringJobError) {
+      toast.error(createMonitoringJobErrorText, {
+        toastId: 'create-monitoring-job-error',
+      });
+    }
+  }, [
+    createMonitoringJobError,
+    createMonitoringJobErrorMsg,
+    createMonitoringJobErrorText,
+  ]);
+
+  const onNameChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setName(event.target.value);
+    },
+    []
+  );
+
+  const addItem = (
+    e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>
+  ) => {
+    if (name !== '') {
+      createMonitoringJob({
+        folderExternalID: `charts-folder-${name}`,
+        folderName: `${name}`,
+      });
+      e.preventDefault();
+      setName('');
+    }
+  };
+
+  /**
+   * Attaches a change event listener on the Cogs Select Input
+   */
+  const injectInputChangeListener = () => {
+    const intvl = setInterval(() => {
+      const inputs = document.querySelectorAll('#select-monitoring-input');
+      if (inputs.length > 0) {
+        clearInterval(intvl);
+        const inputElement: HTMLInputElement = head(inputs) as HTMLInputElement;
+        inputElement.addEventListener('input', (e) => {
+          delay(onNameChange, 300, e);
+        });
+
+        inputElement.addEventListener('blur', (e) => {
+          delay(onNameChange, 300, e);
+        });
+      }
+    }, 200);
+  };
+
+  useEffect(() => {
+    injectInputChangeListener();
+  }, []);
+
+  const showCreateButton = name.length > 0;
+  return (
+    <Controller
+      control={control}
+      name={inputName}
+      rules={{
+        required: t['Folder is required'],
+      }}
+      render={({ field: { onChange, onBlur, value, ref } }) => {
+        return (
+          <Select
+            inputId="select-monitoring-input"
+            value={value}
+            ref={ref}
+            onBlur={onBlur}
+            onChange={(selectOption: any) => {
+              onChange(selectOption);
+            }}
+            options={
+              folderList?.map((item) => ({
+                label: item.name,
+                value: item.id,
+              })) || []
+            }
+            dropdownRender={(menu) => (
+              <>
+                {menu}
+                {showCreateButton && (
+                  <>
+                    <DividerStyled />
+                    <Row>
+                      <ButtonStyled type="primary" onClick={addItem}>
+                        {t['Create folder:']} {name}
+                      </ButtonStyled>
+                    </Row>
+                  </>
+                )}
+              </>
+            )}
+          />
+        );
+      }}
+    />
+  );
+};
+
+const DividerStyled = styled(Divider)`
+  margin: 8px 0;
+`;
+
+const ButtonStyled = styled(Button)`
+  width: 100%;
+`;
+
+export default MonitoringFolderSelect;
