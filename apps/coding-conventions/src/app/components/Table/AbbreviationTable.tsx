cd4f8a5907e6595698d08e0428c320f90401105e
Deep <70804363+deep-cognite@users.noreply.github.com>
2023-01-31 21:40:03 +0100

    fix: abbrv. table (#1121)

--- apps/coding-conventions/src/app/components/Table/AbbreviationTable.tsx
+++ apps/coding-conventions/src/app/components/Table/AbbreviationTable.tsx
@@ -1,5 +1,6 @@
 import { Table } from '@cognite/cogs.js';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { Row } from 'react-table';
 import { Convention } from '../../types';
 
 interface Props {
@@ -7,51 +8,112 @@ interface Props {
   conventions?: Convention[];
 }
 
+const defaultColumns = [
+  {
+    Header: 'Abbreviation',
+    accessor: 'key',
+  },
+  {
+    Header: 'Description',
+    accessor: 'description',
+  },
+];
+
+const dependencyColumns = [
+  {
+    Header: 'Dependency',
+    key: 'dependsOn',
+  },
+  ...defaultColumns,
+];
+
+const RenderSubTable = ({
+  row,
+  convention,
+  conventions,
+}: {
+  row: Row<any>;
+  conventions?: Convention[];
+  convention: Convention;
+}) => {
+  const memoTable = useMemo(() => {
+    const data = convention.definitions?.filter(
+      (item) => item.dependsOn === row.original.id
+    );
+
+    return (
+      <Table<any>
+        pagination={false}
+        dataSource={data || []}
+        columns={defaultColumns}
+      />
+    );
+  }, [row.original.id, convention.definitions]);
+
+  if (!convention?.dependency) {
+    return null;
+  }
+
+  return <>{memoTable}</>;
+};
+
 export const AbbreviationTable: React.FC<Props> = ({
   convention,
   conventions,
 }) => {
-  const [rows, setRows] = useState<Record<string, boolean>>({});
-  console.log('🚀 ~ file: AbbreviationTable.tsx:11 ~ rows', rows);
-  const abbreviationDefinitions = (convention.definitions || []).filter(
-    (item) => item.type === 'Abbreviation'
+  const [expandedRows, setExpandedRows] = useState<Record<string, boolean>>({});
+  const isDependantConvention = !!convention.dependency;
+
+  const abbreviationDefinitions = (
+    (isDependantConvention
+      ? conventions
+          ?.find((item) => item.id === convention.dependency)
+          ?.definitions?.filter((item) =>
+            convention.definitions?.some(
+              (subitem) => item.id === subitem.dependsOn
+            )
+          )
+      : convention.definitions) || []
+  ).filter((item) => item.type === 'Abbreviation');
+
+  useEffect(() => {
+    if (!isDependantConvention) return;
+
+    const defaultExpandedRow = abbreviationDefinitions.reduce((acc, item) => {
+      return { ...acc, [item.id]: true };
+    }, {});
+
+    setExpandedRows(defaultExpandedRow);
+  }, []);
+
+  const columns = useMemo(
+    () => (convention.dependency ? dependencyColumns : defaultColumns),
+    [convention.dependency]
   );
 
   return (
     <Table<any>
       dataSource={abbreviationDefinitions}
-      columns={[
-        {
-          Header: 'Abbreviation',
-          accessor: 'key',
-        },
-        {
-          Header: 'Description',
-          accessor: 'description',
-        },
-      ]}
+      columns={columns}
       pagination={false}
-      expandedIds={rows}
+      expandedIds={expandedRows}
       onRowClick={(row) => {
-        console.log('row', row);
-        setRows((prevState) => ({
+        setExpandedRows((prevState) => ({
           ...prevState,
           [`${row.original.id}`]: !prevState[row.original.id],
         }));
       }}
       renderSubRowComponent={
-        convention.dependency
-          ? () => {
-              const dependentConvention = conventions?.find(
-                (item) => item.id === convention.dependency
-              );
-              return <p>hi: {dependentConvention?.name}</p>;
-            }
+        isDependantConvention
+          ? (row) => (
+              <RenderSubTable
+                convention={convention}
+                conventions={conventions}
+                row={row}
+              />
+            )
           : undefined
       }
-      // onRow={() => ({
-      //   onClick: action('onClick'),
-      // })}
     />
   );
 };
