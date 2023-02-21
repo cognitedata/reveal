a87919a9fd62c99ea9ce75876d2795e566f9a82b
Christopher Tannum <christopher.tannum@cognite.com>
2023-02-21 13:07:45 +0100

    feat: add initial method for fetching lods from octree

--- viewer/packages/360-images/src/collection/IconOctree.ts
+++ viewer/packages/360-images/src/collection/IconOctree.ts
@@ -2,9 +2,10 @@
  * Copyright 2023 Cognite AS
  */
 
+import { getApproximateProjectedBounds, getScreenArea } from '@reveal/utilities';
 import pullAll from 'lodash/pullAll';
 import { Node, PointOctant, PointOctree } from 'sparse-octree';
-import { Box3, Vector3 } from 'three';
+import { Box3, Matrix4, Vector3 } from 'three';
 import { Image360Icon } from '../entity/Image360Icon';
 
 export class IconOctree extends PointOctree<Image360Icon> {
@@ -25,12 +26,48 @@ export class IconOctree extends PointOctree<Image360Icon> {
     return this._nodeCenters.get(node);
   }
 
+  public getLOD(threshold: number, viewProjectionMatrix: Matrix4): Set<PointOctant<Image360Icon>> {
+    const root = this.findNodesByLevel(0)[0];
+    const selectedNodes = new Set<PointOctant<Image360Icon>>();
+
+    const nodesToProcess = [root];
+
+    while (nodesToProcess.length > 0) {
+      const currentNode = nodesToProcess.shift()!;
+      if (!this.isPointOctant(currentNode)) {
+        continue;
+      }
+      if (!this.hasChildren(currentNode) && this.hasData(currentNode)) {
+        selectedNodes.add(currentNode);
+        continue;
+      }
+      currentNode.children?.forEach(child => {
+        if (!this.isPointOctant(child)) {
+          return;
+        }
+        const projectedArea = computeNodeProjectedArea(child);
+        if (projectedArea > threshold) {
+          nodesToProcess.push(child);
+        } else {
+          selectedNodes.add(currentNode);
+        }
+      });
+    }
+
+    return selectedNodes;
+
+    function computeNodeProjectedArea(node: Node) {
+      const rootProjectedBounds = getApproximateProjectedBounds(new Box3(node.min, node.max), viewProjectionMatrix);
+      return getScreenArea(rootProjectedBounds);
+    }
+  }
+
   private populateNodeCenters(): Map<Node, Vector3> {
     const nodeCenters = new Map<Node, Vector3>();
 
     this.traverseLevelsBottomUp(nodes => {
       nodes.forEach(node => {
-        if (this.nodeHasData(node)) {
+        if (this.hasData(node)) {
           nodeCenters.set(node, this.centerOfPoints(node.data.points));
         } else if (this.hasChildren(node)) {
           const points = node.children!.map(child => nodeCenters.get(child)!);
@@ -62,11 +99,15 @@ export class IconOctree extends PointOctree<Image360Icon> {
   }
 
   private isEmptyLeaf(node: Node) {
-    return node.children === null && !this.nodeHasData(node);
+    return node.children === null && !this.hasData(node);
   }
 
-  private nodeHasData(node: Node): node is PointOctant<Image360Icon> {
-    return node instanceof PointOctant && node.data !== null;
+  private hasData(node: Node): node is PointOctant<Image360Icon> {
+    return this.isPointOctant(node) && node.data !== null;
+  }
+
+  private isPointOctant(node: Node): node is PointOctant<Image360Icon> {
+    return node instanceof PointOctant;
   }
 
   private centerOfPoints(points: Vector3[]): Vector3 {
