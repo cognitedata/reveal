1806cd6f0ce31b1252a8edad68742eec71eeefcd
Vegard Økland <vegard.okland@cognite.com>
2023-02-22 15:53:26 +0100

    Disable 403 retry

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Details from 'pages/Details';
 import QuickMatch from 'pages/QuickMatch';
 import CreatePipeline from 'pages/CreatePipeline';
+import { CogniteError } from '@cognite/sdk/dist/src';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry(count, error) {
+        if ((error as CogniteError).status === 403) {
+          return false;
+        }
+        return count <= 3;
+      },
+    },
+  },
+});
 const env = getEnv();
 const project = getProject();
 
