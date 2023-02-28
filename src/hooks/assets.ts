aa8c3adec8fc93ef62062236ec9dd0affce164c6
Vegard Økland <vegard@okland.org>
2023-02-28 15:45:26 +0100

    Add resource aggregates

--- src/hooks/assets.ts
+++ src/hooks/assets.ts
@@ -1,15 +1,24 @@
-import { Asset, AssetFilterProps, CogniteError } from '@cognite/sdk';
+import {
+  Asset,
+  AssetFilterProps,
+  CogniteClient,
+  CogniteError,
+} from '@cognite/sdk';
 import { useSDK } from '@cognite/sdk-provider';
 import {
+  QueryClient,
   QueryKey,
   useInfiniteQuery,
   UseInfiniteQueryOptions,
   useQuery,
   UseQueryOptions,
 } from '@tanstack/react-query';
+import { PropertyAggregate, PropertyAggregateResponse } from 'common/types';
+
+const assetRootKey = ['assets'];
 
 const useAssetsKey = (limit: number, filter?: AssetFilterProps): QueryKey => [
-  'assets',
+  ...assetRootKey,
   'list',
   { limit, filter },
 ];
@@ -38,7 +47,7 @@ export const useAssets = (
 const useAssetSearchKey = (
   query: string,
   filter?: AssetFilterProps
-): QueryKey => ['assets', 'search', query, { filter }];
+): QueryKey => [...assetRootKey, , 'search', query, { filter }];
 
 export const useAssetSearch = <T>(
   query: string,
@@ -50,5 +59,55 @@ export const useAssetSearch = <T>(
     () => sdk.assets.search({ search: { query }, limit: 1000 }),
 
     opts
+  );
+};
+
+export const getPropertiesAggregateKey = (): QueryKey => [
+  ...assetRootKey,
+  'properties-aggregate',
+];
+
+/**
+ * NOTE: metadata aggreates are always downcased since metadata filters are case-insensitive.
+ */
+export const getPropertiesAggregate = async (sdk: CogniteClient) => {
+  const topLevelProperties: PropertyAggregate[] = [
+    { values: [{ property: ['name'] }] },
+    { values: [{ property: ['description'] }] },
+    { values: [{ property: ['source'] }] },
+  ];
+  return sdk
+    .post<PropertyAggregateResponse>(
+      `/api/v1/projects/${sdk.project}/assets/aggregate`,
+      {
+        headers: {
+          'cdf-version': 'alpha',
+        },
+        data: { aggregate: 'uniqueProperties', path: ['metadata'] },
+      }
+    )
+    .then((r) => {
+      if (r.status === 200) {
+        return [...topLevelProperties, ...r.data.items];
+      } else {
+        return Promise.reject(r);
+      }
+    });
+};
+
+export const fetchProperties = async (sdk: CogniteClient, qc: QueryClient) => {
+  return qc.fetchQuery(getPropertiesAggregateKey(), () =>
+    getPropertiesAggregate(sdk)
+  );
+};
+
+export const useProperties = (
+  options?: UseQueryOptions<PropertyAggregate[], CogniteError>
+) => {
+  const sdk = useSDK();
+  return useQuery(
+    getPropertiesAggregateKey(),
+    () => getPropertiesAggregate(sdk),
+    options
   );
 };
--- src/hooks/timeseries.ts
+++ src/hooks/timeseries.ts
@@ -3,9 +3,11 @@ import {
   CogniteError,
   TimeseriesFilter,
   TimeSeriesUpdate,
+  CogniteClient,
 } from '@cognite/sdk';
 import { useSDK } from '@cognite/sdk-provider';
 import {
+  QueryClient,
   QueryKey,
   useInfiniteQuery,
   UseInfiniteQueryOptions,
@@ -14,6 +16,9 @@ import {
   useQuery,
   UseQueryOptions,
 } from '@tanstack/react-query';
+import { PropertyAggregate, PropertyAggregateResponse } from 'common/types';
+import { downcaseMetadata } from 'utils';
+
 import { TABLE_ITEMS_PER_PAGE } from '../constants';
 
 type TSParams = {
@@ -119,6 +124,56 @@ export const useUpdateTimeseries = (
     (changes) => {
       return sdk.timeseries.update(changes);
     },
+    options
+  );
+};
+
+export const getPropertiesAggregateKey = (): QueryKey => [
+  'timeseries',
+  'properties-aggregate',
+];
+
+/**
+ * NOTE: metadata aggreates are always downcased since metadata filters are case-insensitive.
+ */
+export const getPropertiesAggregate = async (sdk: CogniteClient) => {
+  const topLevelProperties: PropertyAggregate[] = [
+    { values: [{ property: ['name'] }] },
+    { values: [{ property: ['description'] }] },
+    { values: [{ property: ['unit'] }] },
+  ];
+  return sdk
+    .post<PropertyAggregateResponse>(
+      `/api/v1/projects/${sdk.project}/timeseries/aggregate`,
+      {
+        headers: {
+          'cdf-version': 'alpha',
+        },
+        data: { aggregate: 'uniqueProperties', path: ['metadata'] },
+      }
+    )
+    .then((r) => {
+      if (r.status === 200) {
+        return [...topLevelProperties, ...r.data.items];
+      } else {
+        return Promise.reject(r);
+      }
+    });
+};
+
+export const fetchProperties = async (sdk: CogniteClient, qc: QueryClient) => {
+  return qc.fetchQuery(getPropertiesAggregateKey(), () =>
+    getPropertiesAggregate(sdk)
+  );
+};
+
+export const useProperties = (
+  options?: UseQueryOptions<PropertyAggregate[], CogniteError>
+) => {
+  const sdk = useSDK();
+  return useQuery(
+    getPropertiesAggregateKey(),
+    () => getPropertiesAggregate(sdk),
     options
   );
 };
