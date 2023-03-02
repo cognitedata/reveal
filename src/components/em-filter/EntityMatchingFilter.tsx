6003b864c481fd5c0dc4ec9e28d02ec5898a2679
Vebjørn Gilberg <vebjorngilberg@gmail.com>
2023-03-02 13:01:58 +0100

    add filter placeholder string

--- src/components/em-filter/EntityMatchingFilter.tsx
+++ src/components/em-filter/EntityMatchingFilter.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 import { Input } from '@cognite/cogs.js';
+import { useTranslation } from 'common/i18n';
 
 type EMFilterProps = {
   query: string;
   setQuery: (value: string) => void;
 };
 const EntityMatchingFilter = ({ query, setQuery }: EMFilterProps) => {
+  const { t } = useTranslation();
   return (
     <StyledInput
-      placeholder="Filter by name..."
+      placeholder={t('filter-placeholder')}
       onChange={(e) => setQuery(e.currentTarget.value)}
       value={query}
     />
