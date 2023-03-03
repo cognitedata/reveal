eae2be2831c4e0eb7782c2f8d961ed11e76fd0d0
Sennen De Almeida <84778551+sennen-cognite@users.noreply.github.com>
2023-03-03 13:40:12 +0530

    fix(discover): app crash when validating array (#3789)

--- apps/discover/src/pages/authorized/search/well/inspect/modules/stickChart/utils/hasAtLeasOneValidValue.ts
+++ apps/discover/src/pages/authorized/search/well/inspect/modules/stickChart/utils/hasAtLeasOneValidValue.ts
@@ -4,6 +4,10 @@ import { withoutNil } from 'utils/withoutNil';
 
 export const hasAtLeasOneValidValue = (data: Partial<PlotData>[]) => {
   return data.some(({ x }) => {
+    if (!x) {
+      return false;
+    }
+
     const validValues = withoutNil(x as (number | null)[]);
     return !isEmpty(validValues);
   });
