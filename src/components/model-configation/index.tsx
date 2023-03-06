8bc24c2d0a6509dff8c04e2341081305756aae7d
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2023-03-06 15:39:48 +0100

    chore: update styles for sections in configure model page
    DOG-1123

--- src/components/model-configation/index.tsx
+++ src/components/model-configation/index.tsx
@@ -5,14 +5,18 @@ import {
   Checkbox,
   RadioGroup,
   Radio,
+  Colors,
 } from '@cognite/cogs.js';
+
 import { useTranslation } from 'common';
+import { QUICK_MATCH_CONFIGURE_MODEL_PAGE_WIDTH } from 'common/constants';
 import FieldMapping from 'components/field-mapping';
 import {
   EMFeatureType,
   Scope,
   useQuickMatchContext,
 } from 'context/QuickMatchContext';
+import styled from 'styled-components';
 
 export default function ModelConfiguration() {
   const { t } = useTranslation();
@@ -28,101 +32,108 @@ export default function ModelConfiguration() {
     setScope,
   } = useQuickMatchContext();
   return (
-    <Flex direction="column" gap={12}>
-      <Title level={4}>{t('model-configuration-fields-header')}</Title>
-      <Body>
-        <p>{t('model-configuration-fields-body-p1')}</p>
-        <p>{t('model-configuration-fields-body-p2')}</p>
-      </Body>
-      <Flex>
-        <FieldMapping
-          sourceType={sourceType}
-          targetType="assets"
-          modelFieldMapping={modelFieldMapping}
-          setModelFieldMapping={setModelFieldMapping}
-        />
-      </Flex>
-      <Title level={4}>{t('model-configuration-model-type-header')}</Title>
-      <Body>
-        <p>{t('model-configuration-model-type-body-p1')}</p>
-      </Body>
-      <Body>
+    <Container>
+      <Section>
+        <SectionTitle level={5}>
+          {t('model-configuration-fields-header')}
+        </SectionTitle>
+        <SectionBody>{t('model-configuration-fields-body')}</SectionBody>
+        <Flex>
+          <FieldMapping
+            sourceType={sourceType}
+            targetType="assets"
+            modelFieldMapping={modelFieldMapping}
+            setModelFieldMapping={setModelFieldMapping}
+          />
+        </Flex>
+      </Section>
+      <Section>
+        <SectionTitle>
+          {t('model-configuration-model-type-header')}
+        </SectionTitle>
+        <SectionBody>{t('model-configuration-model-type-body')}</SectionBody>
         <Checkbox
           checked={supervisedMode}
           onChange={(e) => setSupervisedMode(e.target.checked)}
           label={t('model-configuration-supervised-mode')}
         />
-      </Body>
-      <Title level={4}>{t('model-configuration-model-score-header')}</Title>
-      <Body>
-        <p>{t('model-configuration-model-score-body-p1')}</p>
-      </Body>
-
-      <RadioGroup
-        name="featureType"
-        value={featureType}
-        onChange={(e) => setFeatureType(e.target.value as EMFeatureType)}
-      >
-        <Radio
-          value="simple"
-          name="simple"
-          label={t('model-configuration-model-score-simple-header')}
-        />
-        <span>{t('model-configuration-model-score-simple-desc')}</span>
-        <Radio
-          value="bigram"
-          name="bigram"
-          label={t('model-configuration-model-score-bigram-header')}
-        />
-        <span>{t('model-configuration-model-score-bigram-desc')}</span>
-
-        <Radio
-          value="fwb"
-          name="fwb"
-          label={t('model-configuration-model-score-fwb-header')}
-        />
-        <span>{t('model-configuration-model-score-fwb-desc')}</span>
-
-        <Radio
-          value="bigramextratokenizers"
-          name="bigramextratokenizers"
-          label={t(
-            'model-configuration-model-score-bigram-extra-tokenizers-header'
-          )}
-        />
-        <span>
-          {t('model-configuration-model-score-bigram-extra-tokenizers-header')}
-        </span>
-
-        <Radio
-          value="bigramcombo"
-          name="bigramcombo"
-          label={t('model-configuration-model-score-combined-header')}
-        />
-        <span>{t('model-configuration-model-score-combined-desc')}</span>
-      </RadioGroup>
-
-      <Title level={4}>{t('model-configuration-scope-header')}</Title>
-      <Body>
-        <p>{t('model-configuration-scope-desc')}</p>
-      </Body>
-      <RadioGroup
-        name="scope"
-        value={scope}
-        onChange={(e) => setScope(e.target.value as Scope)}
-      >
-        <Radio
-          value="all"
-          name="all"
-          label={t('model-configuration-scope-all')}
-        />
-
-        <Radio
-          value="unmatched"
-          name="unmatched"
-          label={t('model-configuration-scope-unmatched-only')}
-        />
-      </RadioGroup>
-    </Flex>
+      </Section>
+      <Section>
+        <SectionTitle>
+          {t('model-configuration-model-score-header')}
+        </SectionTitle>
+        <SectionBody>{t('model-configuration-model-score-body')}</SectionBody>
+        <RadioGroup
+          name="featureType"
+          value={featureType}
+          onChange={(e) => setFeatureType(e.target.value as EMFeatureType)}
+        >
+          <Radio
+            value="simple"
+            name="simple"
+            label={t('model-configuration-model-score-simple')}
+          />
+          <Radio
+            value="bigram"
+            name="bigram"
+            label={t('model-configuration-model-score-bigram')}
+          />
+          <Radio
+            value="fwb"
+            name="fwb"
+            label={t('model-configuration-model-score-fwb')}
+          />
+          <Radio
+            value="bigramextratokenizers"
+            name="bigramextratokenizers"
+            label={t('model-configuration-model-score-bigram-extra-tokenizers')}
+          />
+          <Radio
+            value="bigramcombo"
+            name="bigramcombo"
+            label={t('model-configuration-model-score-combined')}
+          />
+        </RadioGroup>
+      </Section>
+      <Section>
+        <SectionTitle>{t('model-configuration-scope-header')}</SectionTitle>
+        <SectionBody>{t('model-configuration-scope-desc')}</SectionBody>
+        <RadioGroup
+          direction="horizontal"
+          name="scope"
+          value={scope}
+          onChange={(e) => setScope(e.target.value as Scope)}
+        >
+          <Radio
+            value="all"
+            name="all"
+            label={t('model-configuration-scope-all')}
+          />
+          <Radio
+            value="unmatched"
+            name="unmatched"
+            label={t('model-configuration-scope-unmatched-only')}
+          />
+        </RadioGroup>
+      </Section>
+    </Container>
   );
 }
+
+const Section = styled(Flex).attrs({ gap: 8, direction: 'column' })``;
+
+const SectionTitle = styled(Title).attrs({ level: 5 })`
+  color: ${Colors['text-icon--status-neutral']};
+`;
+
+const SectionBody = styled(Body).attrs({ level: 2 })``;
+
+const Container = styled(Flex).attrs({ direction: 'column' })`
+  width: ${QUICK_MATCH_CONFIGURE_MODEL_PAGE_WIDTH}px;
+
+  ${Section}:not(:first-child) {
+    border-top: 1px solid ${Colors['border--interactive--default']};
+    margin-top: 16px;
+    padding-top: 16px;
+  }
+`;
