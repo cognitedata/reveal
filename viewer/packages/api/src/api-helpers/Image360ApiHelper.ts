1d28fcce4eb9a258f8e8d8a8657a172197151b18
Astrid Kløve-Graue <astrid.graue@cognite.com>
2023-03-07 12:50:26 +0100

    Rename

--- viewer/packages/api/src/api-helpers/Image360ApiHelper.ts
+++ viewer/packages/api/src/api-helpers/Image360ApiHelper.ts
@@ -291,7 +291,7 @@ export class Image360ApiHelper {
         .forEach(imageCollection => imageCollection.events.image360Exited.fire());
       this._interactionState.currentImage360Entered.image360Visualization.visible = false;
       this._interactionState.currentImage360Entered = undefined;
-      this._interactionState.currentImage360Clicked = undefined;
+      this._interactionState.image360SelectedForEntry = undefined;
       MetricsLogger.trackEvent('360ImageExited', {});
     }
     const { position, rotation } = this._image360Navigation.getCameraState();
