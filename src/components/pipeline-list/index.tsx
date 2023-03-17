a19778f222095aad5b653a9cdf4f75a355f1ee71
Vegard Økland <vegard@okland.org>
2023-03-17 13:31:20 +0100

    Improve pipeline loading feedback

--- src/components/pipeline-list/index.tsx
+++ src/components/pipeline-list/index.tsx
@@ -1,4 +1,4 @@
-import { Icon } from '@cognite/cogs.js';
+import { Flex, Icon, Colors } from '@cognite/cogs.js';
 import { useEMPipelines } from 'hooks/contextualization-api';
 import PipelineTable from './PipelineTable';
 import PipelineListEmpty from './PipelineListEmpty';
@@ -7,7 +7,15 @@ export default function PipelineList() {
   const { data, isInitialLoading } = useEMPipelines();
 
   if (isInitialLoading) {
-    return <Icon type="Loader" />;
+    return (
+      <Flex justifyContent="space-around" style={{ marginTop: 150 }}>
+        <Icon
+          size={32}
+          type="Loader"
+          css={{ color: Colors['text-icon--muted'] }}
+        />
+      </Flex>
+    );
   }
 
   return <>{data?.length ? <PipelineTable /> : <PipelineListEmpty />}</>;
