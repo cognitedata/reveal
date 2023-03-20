70c26b4f31d17c1e27cac6ab0314167dc5edb373
Mustafa Ilker Sarac <104766613+milkersrc@users.noreply.github.com>
2023-03-20 12:01:49 +0100

    feat(data-exploration-containers): move common asset filter (#1423)
    * feat(data-exploration-containers): move common asset filter
    
    * fix: rename
    
    * fix: pr comments

---
+++ libs/data-exploration/containers/src/Filters/AssetSelectFilter/AssetSelectFilter.spec.tsx
@@ -0,0 +1,18 @@
+import { AssetSelectFilter } from './AssetSelectFilter';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('AssetSelectFilter', () => {
+  describe('Base', () => {
+    test('Show no options in the list', () => {
+      render(<AssetSelectFilter options={[]} />);
+
+      expect(screen.getByTestId('filter-label')).toBeInTheDocument();
+      expect(screen.getByText(/asset/i)).toBeInTheDocument();
+
+      userEvent.click(screen.getByText('Select...'));
+
+      expect(screen.getByText('No options')).toBeInTheDocument();
+    });
+  });
+});
