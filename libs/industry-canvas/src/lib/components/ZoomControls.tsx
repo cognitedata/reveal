7c2208069235169db988d18988fa126e0ce0b6c4
Marvin <marvinreza@outlook.com>
2023-03-22 11:25:08 +0100

    feat(industry-canvas): updated zoom toolbar design (#1444)
    * feat(industry-canvas): updated zoom toolbar design
    
    * feat: use setScale and getScale to get correct behaviors
    
    * refactor: lint and move ZoomControls into its own component file
    
    * chore(deps): bump ufv
    
    * chore(deps): bump UFV
    
    * refactor: normalizedValue --> value
    
    * refactor: pass zoom actions as props to ZoomControls
    
    * chore: add story for zoom controls
    
    * fix: fix setZoomScale
    
    * fix: make percentage button width fixed
    
    Also tweaks size of the zoom buttons as per Jonas' suggestions
    
    ---------
    
    Co-authored-by: Marvin Reza <marvinreza@Marvins-MacBook-Pro.local>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

---
+++ libs/industry-canvas/src/lib/components/ZoomControls.tsx
@@ -0,0 +1,77 @@
+import { Button, Dropdown, Menu, ToolBar } from '@cognite/cogs.js';
+import styled from 'styled-components';
+import { PERCENTAGE_VALUES } from '../constants';
+import convertToPercentage from '../utils/convertToPercentage';
+
+export type ZoomControlsProps = {
+  currentZoomScale: number;
+  zoomIn?: () => void;
+  zoomOut?: () => void;
+  zoomToFit?: () => void;
+  setZoomScale?: (newScale: number) => void;
+};
+
+const ZoomControls: React.FC<ZoomControlsProps> = ({
+  currentZoomScale,
+  zoomIn,
+  zoomOut,
+  zoomToFit,
+  setZoomScale,
+}) => (
+  <ToolBar direction="horizontal">
+    <>
+      <Button
+        aria-label="ZoomOut"
+        type="ghost"
+        icon="ZoomOut"
+        size="small"
+        onClick={zoomOut}
+      />
+      <Dropdown
+        placement="top-start"
+        content={
+          <Menu>
+            {Object.entries(PERCENTAGE_VALUES).map(([key, value]) => (
+              <Menu.Item
+                onClick={() => {
+                  if (setZoomScale !== undefined) {
+                    setZoomScale(value);
+                  }
+                }}
+              >
+                {key}
+              </Menu.Item>
+            ))}
+          </Menu>
+        }
+      >
+        <PercentageButton
+          aria-label="CurrentZoomScale"
+          type="ghost"
+          size="small"
+        >
+          {convertToPercentage(currentZoomScale)}%
+        </PercentageButton>
+      </Dropdown>
+      <Button
+        aria-label="ZoomIn"
+        type="ghost"
+        icon="ZoomIn"
+        size="small"
+        onClick={zoomIn}
+      />
+    </>
+    <Button
+      aria-label="Expand"
+      type="ghost"
+      icon="Expand"
+      onClick={zoomToFit}
+    />
+  </ToolBar>
+);
+
+const PercentageButton = styled(Button)`
+  width: 50px;
+`;
+
+export default ZoomControls;
