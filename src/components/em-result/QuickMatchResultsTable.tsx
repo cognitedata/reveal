1a8b45e0e17d970bbb7ca26d650abee79d8230ff
Vegard Økland <vegard.okland@cognite.com>
2023-03-22 10:34:39 +0100

    Fix result table sorting

--- src/components/em-result/QuickMatchResultsTable.tsx
+++ src/components/em-result/QuickMatchResultsTable.tsx
@@ -2,7 +2,6 @@ import { useMemo } from 'react';
 import { ColumnType, Table } from '@cognite/cdf-utilities';
 import { useTranslation } from 'common';
 import { Prediction, PredictionObject } from 'hooks/contextualization-api';
-import { PredictionsTableTypes } from 'types/types';
 import { formatPredictionObject } from 'utils';
 import ConfidenceScore from './Confidence';
 import { Checkbox, Flex } from '@cognite/cogs.js';
@@ -11,14 +10,14 @@ type Predictions = {
   predictions: Prediction[];
 };
 
-type PredictionsTableRecord = { key: string } & Pick<
-  Prediction,
-  PredictionsTableTypes
->;
+type PredictionsTableRecord = {
+  key: string;
+  score?: number;
+} & Prediction;
 
 type ResultsTableRecordCT = ColumnType<PredictionsTableRecord> & {
   title: string;
-  key: PredictionsTableTypes;
+  score?: number;
 };
 
 const QuickMatchResultsTable = ({ predictions }: Predictions): JSX.Element => {
@@ -28,7 +27,11 @@ const QuickMatchResultsTable = ({ predictions }: Predictions): JSX.Element => {
     () =>
       predictions
         ?.filter((prediction) => prediction.matches.length > 0)
-        .map((a) => ({ ...a, key: a.source.id.toString() })) || [],
+        .map((a) => ({
+          ...a,
+          key: a.source.id.toString(),
+          score: a.matches[0]?.score,
+        })) || [],
     [predictions]
   );
 
@@ -40,6 +43,11 @@ const QuickMatchResultsTable = ({ predictions }: Predictions): JSX.Element => {
         key: 'source',
         render: (source: PredictionObject) =>
           formatPredictionObject(source) || '—',
+        sorter: (a: Prediction, b: Prediction) =>
+          formatPredictionObject(a.source).localeCompare(
+            formatPredictionObject(b.source)
+          ),
+        sortDirections: ['descend', 'ascend'],
       },
       {
         title: t('qm-result-target'),
@@ -47,26 +55,31 @@ const QuickMatchResultsTable = ({ predictions }: Predictions): JSX.Element => {
         key: 'matches',
         render: (matches: any[]) =>
           formatPredictionObject(matches[0]?.target) || '—',
+        sorter: (a: Prediction, b: Prediction) =>
+          formatPredictionObject(a.matches[0]?.target).localeCompare(
+            formatPredictionObject(b.matches[0]?.target)
+          ),
       },
 
       {
         title: t('confidence'),
-        dataIndex: 'matches',
-        key: 'matches',
+        dataIndex: 'score',
+        key: 'score',
         align: 'center',
-        render: (matches: any[]) => (
-          <ConfidenceScore score={matches[0]?.score} />
+        render: (score: number | undefined) => (
+          <ConfidenceScore score={score} />
         ),
         sorter: (a: Prediction, b: Prediction) =>
           (a.matches[0]?.score ?? 0) - (b.matches[0]?.score ?? 0),
         sortDirections: ['descend', 'ascend'],
         defaultSortOrder: 'descend',
+        width: 100,
       },
       {
         title: t('confirm'),
         dataIndex: 'source',
         key: 'source',
-        width: 10,
+        width: 100,
         render: (source: PredictionObject) => {
           return (
             <Flex justifyContent="center">
