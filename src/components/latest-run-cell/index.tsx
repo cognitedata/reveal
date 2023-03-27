a8907593bb187ba018eb4188a34ebb27b1f71b81
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2023-03-27 12:44:41 +0200

    feat: add status icon to latest run cell

--- src/components/latest-run-cell/index.tsx
+++ src/components/latest-run-cell/index.tsx
@@ -1,9 +1,10 @@
 import { Timestamp } from '@cognite/cdf-utilities';
-import { Body } from '@cognite/cogs.js';
+import { Body, Flex } from '@cognite/cogs.js';
 import { Link } from 'react-router-dom';
 
 import { PipelineWithLatestRun } from 'hooks/entity-matching-pipelines';
 import { createInternalLink } from 'utils';
+import QueryStatusIcon from 'components/QueryStatusIcon';
 
 type LatestRunCellProps = {
   latestRun?: PipelineWithLatestRun['latestRun'];
@@ -12,13 +13,16 @@ type LatestRunCellProps = {
 const LatestRunCell = ({ latestRun }: LatestRunCellProps): JSX.Element => {
   if (latestRun) {
     return (
-      <Body level={2} strong>
-        <Link
-          to={createInternalLink(`pipeline/${latestRun?.pipelineId}/results`)}
-        >
-          <Timestamp timestamp={latestRun?.createdTime} />
-        </Link>
-      </Body>
+      <Flex alignItems="center" gap={4}>
+        <QueryStatusIcon status={latestRun.status} />
+        <Body level={2} strong>
+          <Link
+            to={createInternalLink(`pipeline/${latestRun.pipelineId}/results`)}
+          >
+            <Timestamp timestamp={latestRun.createdTime} />
+          </Link>
+        </Body>
+      </Flex>
     );
   }
 
