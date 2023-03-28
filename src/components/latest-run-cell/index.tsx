a1f90faa6eba9376260f9279bb250468af2b975c
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2023-03-28 10:01:16 +0200

    fix merge conflicts

--- src/components/latest-run-cell/index.tsx
+++ src/components/latest-run-cell/index.tsx
@@ -1,17 +1,10 @@
 import { Timestamp } from '@cognite/cdf-utilities';
-<<<<<<< HEAD
 import { Body, Flex } from '@cognite/cogs.js';
-=======
-import { Body } from '@cognite/cogs.js';
->>>>>>> 3eb46f3 (feat: add latest run column to pipeline table)
 import { Link } from 'react-router-dom';
 
 import { PipelineWithLatestRun } from 'hooks/entity-matching-pipelines';
 import { createInternalLink } from 'utils';
-<<<<<<< HEAD
 import QueryStatusIcon from 'components/QueryStatusIcon';
-=======
->>>>>>> 3eb46f3 (feat: add latest run column to pipeline table)
 
 type LatestRunCellProps = {
   latestRun?: PipelineWithLatestRun['latestRun'];
@@ -20,7 +13,6 @@ type LatestRunCellProps = {
 const LatestRunCell = ({ latestRun }: LatestRunCellProps): JSX.Element => {
   if (latestRun) {
     return (
-<<<<<<< HEAD
       <Flex alignItems="center" gap={4}>
         <QueryStatusIcon status={latestRun.status} />
         <Body level={2} strong>
@@ -33,15 +25,6 @@ const LatestRunCell = ({ latestRun }: LatestRunCellProps): JSX.Element => {
           </Link>
         </Body>
       </Flex>
-=======
-      <Body level={2} strong>
-        <Link
-          to={createInternalLink(`pipeline/${latestRun?.pipelineId}/results`)}
-        >
-          <Timestamp timestamp={latestRun?.createdTime} />
-        </Link>
-      </Body>
->>>>>>> 3eb46f3 (feat: add latest run column to pipeline table)
     );
   }
 
