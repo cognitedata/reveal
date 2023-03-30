311ecc80a35774b0355c652ea72217b4e5853119
David Liu <35227776+dlcognite@users.noreply.github.com>
2023-03-30 10:09:22 +0200

    feat: add contextualization library (#1520)
    * feat: add_contextualization
    
    * label

---
+++ libs/contextualization/src/lib/contextualization.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ContextualizationProps {}
+
+export function Contextualization(_props: ContextualizationProps) {
+  return (
+    <div>
+      <h1>Welcome to Contextualization!</h1>
+    </div>
+  );
+}
+
+export default Contextualization;
