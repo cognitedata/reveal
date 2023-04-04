6066729027fa74f4003a82f767d453b4da47e67b
gayanDeeegala <gayandeegala@gmail.com>
2023-04-04 13:11:52 +0530

    NewTheeDView flag is added

---
+++ apps/data-exploration/src/app/hooks/flags/useFlagNewThreeDView.ts
@@ -0,0 +1,15 @@
+import { useFlag } from '@cognite/react-feature-flags';
+
+/**
+ * Flag used for developing the new 3D model selection page
+ *
+ * Story: https://cognitedata.atlassian.net/browse/DEGR-1767
+ */
+export const useFlagANewTheeDView = () => {
+  const { isEnabled } = useFlag('DATA_EXPLORATION_new_threeD_view', {
+    forceRerender: true,
+    fallback: false,
+  });
+
+  return isEnabled;
+};
