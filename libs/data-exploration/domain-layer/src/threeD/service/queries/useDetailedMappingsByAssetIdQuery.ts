21b7cd30af27422a243fd3dc49df679108109e59
Pramod S <87521752+pramodcog@users.noreply.github.com>
2023-04-17 20:26:15 +0200

    fixed detailed mapped node query condition which missed next page data (#1612)

--- libs/data-exploration/domain-layer/src/threeD/service/queries/useDetailedMappingsByAssetIdQuery.ts
+++ libs/data-exploration/domain-layer/src/threeD/service/queries/useDetailedMappingsByAssetIdQuery.ts
@@ -64,7 +64,7 @@ export const useDetailedMappingsByAssetIdQuery = (
     },
     {
       ...options,
-      enabled: !!isFetchingMore,
+      enabled: data.length > 0 && !canFetchMore,
     }
   );
 };
