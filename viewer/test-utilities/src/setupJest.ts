5184f0c38111b272b1043168a309ee2352a51407
Christopher Tannum <christopher.tannum@cognite.com>
2023-04-20 13:04:15 +0200

    fix: remove recreating window in jest setup

--- viewer/test-utilities/src/setupJest.ts
+++ viewer/test-utilities/src/setupJest.ts
@@ -4,21 +4,21 @@
 
 // fetch() polyfill
 import 'whatwg-fetch';
+import { jest } from '@jest/globals';
 import { TextDecoder, TextEncoder } from 'util';
 import ResizeObserver from 'resize-observer-polyfill';
 
 // To avoid warnings from CogniteClient that checks whether we use SSL
-// global.window = Object.create(window);
-// const url = 'https://api.cognitedata.com';
-// Object.defineProperty(window, 'location', {
-//   value: {
-//     href: url,
-//     origin: url,
-//     protocol: 'https:'
-//   }
-// });
+const url = 'https://api.cognitedata.com';
+Object.defineProperty(window, 'location', {
+  value: {
+    href: url,
+    origin: url,
+    protocol: 'https:'
+  }
+});
 
-// window.URL.createObjectURL = jest.fn<() => string>();
+window.URL.createObjectURL = jest.fn<() => string>();
 
 // Mock Worker for web workers
 class StubWorker {
