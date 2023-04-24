92d3ed0608efe5f0808dceded62ec39c517af68d
Håkon Flatval <hakon.flatval@cognite.com>
2023-04-24 16:07:49 +0200

    docs: update typedoc for styling types

--- viewer/packages/360-images/src/annotation-styling/types.ts
+++ viewer/packages/360-images/src/annotation-styling/types.ts
@@ -10,6 +10,12 @@ export type Image360AnnotationAppearance = {
   visible: boolean;
 };
 
+/**
+ * A change to an annotation style. Undefined fields are ignored
+ */
 export type Image360AnnotationAppearanceEdit = Partial<Image360AnnotationAppearance>;
 
+/**
+ * A predicate function used for selectively applying styles to annotations
+ */
 export type Image360AnnotationFilter = (annotation: AnnotationModel) => boolean;
