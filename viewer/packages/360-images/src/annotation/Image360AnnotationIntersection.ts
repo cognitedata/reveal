f715a164a4475ec79b0f865b27eee5c204998ac8
Håkon Flatval <hakon.flatval@cognite.com>
2023-05-08 09:57:00 +0200

    fix: add missing type file

---
+++ viewer/packages/360-images/src/annotation/Image360AnnotationIntersection.ts
@@ -0,0 +1,21 @@
+/*!
+ * Copyright 2023 Cognite AS
+ */
+
+import { Image360Annotation } from './Image360Annotation';
+import { Vector3 } from 'three';
+
+export type Image360AnnotationIntersection = {
+  /**
+   * The intersection type.
+   */
+  type: 'image360Annotation';
+  /**
+   * The intersected annotation
+   */
+  annotation: Image360Annotation;
+  /**
+   * The world direction from the camera to the intersection point
+   */
+  direction: Vector3;
+};
