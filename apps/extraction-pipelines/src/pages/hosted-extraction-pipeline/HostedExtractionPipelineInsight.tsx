b11a1f039e3821e8de6c7912ad87c903eb3d85a6
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2023-05-09 07:55:05 +0200

    feat: add empty box for insights tab

--- apps/extraction-pipelines/src/pages/hosted-extraction-pipeline/HostedExtractionPipelineInsight.tsx
+++ apps/extraction-pipelines/src/pages/hosted-extraction-pipeline/HostedExtractionPipelineInsight.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+import { Box } from 'components/box/Box';
 import { MQTTSourceWithJobMetrics } from 'hooks/hostedExtractors';
+import { Illustrations } from '@cognite/cogs.js';
 
 type HostedExtractionPipelineInsightProps = {
   source: MQTTSourceWithJobMetrics;
@@ -8,5 +12,17 @@ type HostedExtractionPipelineInsightProps = {
 
 export const HostedExtractionPipelineInsight =
   ({}: HostedExtractionPipelineInsightProps): JSX.Element => {
-    return <div>HostedExtractionPipelineInsight</div>;
+    return (
+      <TemporaryBox>
+        <Illustrations.Solo type="MaintenanceFixTools" />
+      </TemporaryBox>
+    );
   };
+
+const TemporaryBox = styled(Box)`
+  align-items: center;
+  display: flex;
+  height: 256px;
+  justify-content: center;
+  width: 1024px;
+`;
