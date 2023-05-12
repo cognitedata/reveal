f2a3eb77a092171d2e3bbe51b66b03d276962871
Savelii Novikov <45129444+Savokr@users.noreply.github.com>
2023-05-12 18:00:30 +0200

    feat: implement SmartOverlayTool that visualises overlays in 3D. (#3217)
    Initial support for 3d overlays via SmartOverlayTool. New 3d-overlays package was created with classes shared between 360 images icons and SmartOverlayTool.
    ---------
    
    Co-authored-by: Christopher Tannum <christopher.tannum@cognite.com>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

---
+++ viewer/packages/3d-overlays/src/Overlay3D.ts
@@ -0,0 +1,29 @@
+/*!
+ * Copyright 2023 Cognite AS
+ */
+export interface Overlay3D<MetadataType> {
+  /**
+   * Set whether this overlay should be visible.
+   * */
+  set visible(visible: boolean);
+  /**
+   * Get whether this overlay is visible.
+   */
+  get visible(): boolean;
+  /**
+   * Get the position of this overlay.
+   */
+  get position(): THREE.Vector3;
+  /**
+   * Set the display color of this overlay.
+   * */
+  setColor(color: THREE.Color): void;
+  /**
+   * Get the display color of this overlay.
+   * */
+  get color(): THREE.Color;
+  /**
+   * Get the metadata associated with this overlay.
+   * */
+  getMetadata(): MetadataType | undefined;
+}
