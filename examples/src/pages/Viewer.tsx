a54ed3ff94d48deeeb1061b626c4cf8c963ffa10
Christopher Tannum <christopher.tannum@cognite.com>
2023-05-12 16:43:48 +0200

    chore: bump examples to react 18

--- examples/src/pages/Viewer.tsx
+++ examples/src/pages/Viewer.tsx
@@ -50,7 +50,7 @@ export function Viewer() {
     // Check in order to avoid double initialization of everything, especially dat.gui.
     // See https://reactjs.org/docs/strict-mode.html#detecting-unexpected-side-effects for why its called twice.
     if (!canvasWrapperRef.current) {
-      return () => {};
+      return () => { };
     }
 
     const gui = new dat.GUI({ width: Math.min(500, 0.8 * window.innerWidth) });
@@ -106,9 +106,9 @@ export function Viewer() {
           pointBlending: urlParams.get('pointBlending') === 'true' ?? undefined,
           edlOptions: edlEnabled
             ? {
-                strength: parseFloat(urlParams.get('edlStrength') ?? '0.5'),
-                radius: parseFloat(urlParams.get('edlRadius') ?? '2.2')
-              }
+              strength: parseFloat(urlParams.get('edlStrength') ?? '0.5'),
+              radius: parseFloat(urlParams.get('edlRadius') ?? '2.2')
+            }
             : 'disabled'
         }
       };
@@ -122,12 +122,12 @@ export function Viewer() {
       } else if (!project) {
         throw new Error(
           'A "project" URL parameter is needed to load models from CDF.' +
-            'Optionally, use "modelUrl" to load local models.'
+          'Optionally, use "modelUrl" to load local models.'
         );
       } else if (!environment && !overrideToken) {
         throw new Error(
           'You must provide either "env" or "token" as URL parameters to load models from CDF.' +
-            'Optionally, use "modelUrl" to load local models.'
+          'Optionally, use "modelUrl" to load local models.'
         );
       }
 
