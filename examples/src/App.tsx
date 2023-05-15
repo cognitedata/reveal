c6f97189f9b2d0ac7080c7a3c308db3b4090f080
Christopher Tannum <christopher.tannum@cognite.com>
2023-05-15 10:00:06 +0200

    chore: remove test code

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -4,7 +4,6 @@
 
 import styled from 'styled-components';
 import { Viewer } from './pages/Viewer';
-import { Image360HistoricalDetailsPanel } from '@cognite/reveal-react-components';
 
 const PageContainer = styled.div`
   min-height: 100vh;
@@ -16,7 +15,6 @@ export default function App(): JSX.Element {
   return (
     <PageContainer>
       <Viewer key="/" />
-      <Image360HistoricalDetailsPanel {...{ revisionCount: 5, onDetailsClick: () => { } }} />
     </PageContainer>
   );
 }
