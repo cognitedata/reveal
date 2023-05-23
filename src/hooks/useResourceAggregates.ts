bd96c1be3b5ab32880ce2520750450a247ba581a
Andreas S. Skønberg <andreas.skonberg@gmail.com>
2023-05-23 13:39:18 +0200

    fix lint

--- src/hooks/useResourceAggregates.ts
+++ src/hooks/useResourceAggregates.ts
@@ -9,21 +9,27 @@ export function useResourceAggregates(dataSetId: number) {
   const getQueryKey = (resource: string) => [resource, 'aggregate', dataSetId];
 
   return useQueries({
-    queries: [{
-      queryKey: getQueryKey('assets'),
-      queryFn: () => sdk.assets.aggregate(getDataSetFilter(dataSetId))
-    }, {
-      queryKey: getQueryKey('timeseries'),
-      queryFn: () => sdk.timeseries.aggregate(getDataSetFilter(dataSetId))
-    }, {
-      queryKey: getQueryKey('files'),
-      queryFn: () => sdk.files.aggregate(getDataSetFilter(dataSetId))
-    }, {
-      queryKey: getQueryKey('events'),
-      queryFn: () => sdk.events.aggregate.count(getDataSetFilter(dataSetId))
-    }, {
-      queryKey: getQueryKey('sequences'),
-      queryFn: () => sdk.sequences.aggregate(getDataSetFilter(dataSetId))
-    }]
+    queries: [
+      {
+        queryKey: getQueryKey('assets'),
+        queryFn: () => sdk.assets.aggregate(getDataSetFilter(dataSetId)),
+      },
+      {
+        queryKey: getQueryKey('timeseries'),
+        queryFn: () => sdk.timeseries.aggregate(getDataSetFilter(dataSetId)),
+      },
+      {
+        queryKey: getQueryKey('files'),
+        queryFn: () => sdk.files.aggregate(getDataSetFilter(dataSetId)),
+      },
+      {
+        queryKey: getQueryKey('events'),
+        queryFn: () => sdk.events.aggregate.count(getDataSetFilter(dataSetId)),
+      },
+      {
+        queryKey: getQueryKey('sequences'),
+        queryFn: () => sdk.sequences.aggregate(getDataSetFilter(dataSetId)),
+      },
+    ],
   });
 }
