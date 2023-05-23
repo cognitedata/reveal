77ea544287629ed258af6aecbc8efd37753902ea
Shubham Singh <shubham88singh@gmail.com>
2023-05-23 08:48:35 -0400

    feat(data-explorer): Adding the File Related resource in AssetDetailsTab (#1825)
    * Adding the File Related resource
    
    * Removing the external link button
    
    * Adding the fetch more option
    
    * Fixing the height after Jonas comment
    
    ---------
    
    Co-authored-by: Shubham SIngh <shubham.singh@cognite.com>

--- libs/data-exploration/containers/src/ResourceDetails/AssetDetails/AssetDetails.tsx
+++ libs/data-exploration/containers/src/ResourceDetails/AssetDetails/AssetDetails.tsx
@@ -1,8 +1,16 @@
+import { createLink } from '@cognite/cdf-utilities';
 import { Collapse, Title } from '@cognite/cogs.js';
+import { ResourceType } from '@data-exploration-lib/core';
 
-import { useAssetsByIdQuery } from '@data-exploration-lib/domain-layer';
-import { ResourceDetailsTemplate } from '@data-exploration/components';
+import {
+  InternalDocument,
+  useAssetsByIdQuery,
+  useDocumentSearchResultQuery,
+} from '@data-exploration-lib/domain-layer';
+import { ResourceDetailsTemplate, Table } from '@data-exploration/components';
+import { ColumnDef } from '@tanstack/react-table';
 import React, { FC, useMemo } from 'react';
+import styled from 'styled-components';
 import { AssetInfo } from '../../Info';
 
 interface Props {
@@ -22,6 +30,24 @@ export const AssetDetails: FC<Props> = ({
     return data ? data[0] : undefined;
   }, [data]);
 
+  const onOpenResources = (resourceType: ResourceType, id: number) => {
+    const link = createLink(`/explore/search/${resourceType}/${id}`);
+    window.open(link, '_blank');
+  };
+  const columns = useMemo(
+    () => [Table.Columns.name()],
+    []
+  ) as ColumnDef<InternalDocument>[];
+  const {
+    results: relatedFiles = [],
+    hasNextPage,
+    fetchNextPage,
+    isLoading,
+  } = useDocumentSearchResultQuery({
+    filter: { assetSubtreeIds: [{ value: assetId }] },
+    limit: 10,
+  });
+
   return (
     <ResourceDetailsTemplate
       title={asset ? asset.name : ''}
@@ -38,8 +64,28 @@ export const AssetDetails: FC<Props> = ({
             <Title level={5}>No Details Available</Title>
           )}
         </Collapse.Panel>
-        <Collapse.Panel header={<h4>Assets</h4>}>test 2</Collapse.Panel>
+        <Collapse.Panel header={<h4>Files</h4>}>
+          <Wrapper>
+            <Table<InternalDocument>
+              data={relatedFiles}
+              columns={columns}
+              hasNextPage={hasNextPage}
+              showLoadButton
+              fetchMore={fetchNextPage}
+              isLoadingMore={isLoading}
+              onRowClick={(file) => onOpenResources('file', file.id)}
+              id="related-file-asset-details"
+              enableSelection
+              hideColumnToggle
+            />
+          </Wrapper>
+        </Collapse.Panel>
       </Collapse>
     </ResourceDetailsTemplate>
   );
 };
+
+const Wrapper = styled.div`
+  max-height: 240px;
+  overflow: auto;
+`;
