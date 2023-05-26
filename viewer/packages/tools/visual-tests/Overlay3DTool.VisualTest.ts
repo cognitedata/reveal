ab0c269449d5c6a44c4a9d94d44495350cbeebca
Savelii Novikov <kvaka2000@gmail.com>
2023-05-26 18:15:28 +0200

    add visual test

---
+++ viewer/packages/tools/visual-tests/Overlay3DTool.VisualTest.ts
@@ -0,0 +1,78 @@
+/*!
+ * Copyright 2022 Cognite AS
+ */
+
+import { DefaultCameraManager } from '../../camera-manager';
+import * as THREE from 'three';
+import {
+  ViewerTestFixtureComponents,
+  ViewerVisualTestFixture
+} from '../../../visual-tests/test-fixtures/ViewerVisualTestFixture';
+import { Overlay3DTool } from '../src/Overlay3D/Overlay3DTool';
+import { OverlayInfo } from '../../3d-overlays/src/OverlayCollection';
+
+export default class DefaultVisualTest extends ViewerVisualTestFixture {
+  public setup(_: ViewerTestFixtureComponents): Promise<void> {
+    const { viewer } = _;
+
+    const cameraManager = viewer.cameraManager as DefaultCameraManager;
+    viewer.setBackgroundColor({ color: new THREE.Color().setScalar(0.9) });
+    //viewer.setResolutionOptions({maxRenderResolution: Infinity})
+
+    cameraManager.setCameraControlsOptions({ mouseWheelAction: 'zoomToCursor', changeCameraTargetOnClick: false });
+
+    const smartOverlayTool = new Overlay3DTool<{ text: string; id: number }>(viewer);
+    smartOverlayTool.setTextOverlayVisible(false);
+
+    smartOverlayTool.on('hover', ({ htmlOverlay, targetOverlay }) => {
+      const { text, id } = targetOverlay.getContent();
+      htmlOverlay.innerText = text + ', #' + id + ' hovered';
+      targetOverlay.setColor(new THREE.Color('red'));
+      viewer.requestRedraw();
+    });
+
+    smartOverlayTool.on('click', ({ htmlOverlay, targetOverlay }) => {
+      htmlOverlay.innerText = 'Haha, you clicked me!';
+      targetOverlay.setVisible(false);
+      viewer.requestRedraw();
+    });
+
+    const labels: OverlayInfo<{ text: string; id: number }>[] = [];
+    const overlays = [];
+
+    const reusableVec = new THREE.Vector3();
+
+    const boxSize = 10;
+    const overlaysOffset = new THREE.Vector3(0, -10, -10);
+
+    for (let i = boxSize / -2; i < boxSize / 2; i++) {
+      for (let x = boxSize / -2; x < boxSize / 2; x++) {
+        for (let y = boxSize / -2; y < boxSize / 2; y++) {
+          if (x * x + y * y - 0.7 * i * i < 750) {
+            const id = i + ' ' + x + ' ' + y;
+            labels.push({
+              content: {
+                text: 'Meow ' + id,
+                id: i + x + y
+              },
+              position: reusableVec.set(x, i, y).multiplyScalar(0.9).clone().add(overlaysOffset),
+              color: new THREE.Color(
+                Math.abs(i / (boxSize / 2)),
+                Math.abs(x / (boxSize / 2)),
+                Math.abs(y / (boxSize / 2))
+              )
+            });
+          }
+        }
+      }
+      overlays.push(smartOverlayTool.createOverlayCollection(labels));
+      labels.splice(0, labels.length);
+    }
+
+    // Testing proper removal of overlays
+    smartOverlayTool.removeOverlayCollection(overlays[2]);
+    overlays[5].removeOverlays(overlays[5].getOverlays().slice(0, 50));
+
+    return Promise.resolve();
+  }
+}
