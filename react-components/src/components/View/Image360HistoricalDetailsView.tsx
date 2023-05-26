06d086285cbd3f2682a31fa654cd41924e685ff0
Pramod S <pramod.s@cognite.com>
2023-05-26 15:57:01 +0200

    refracted detail view component

--- react-components/src/components/View/Image360HistoricalDetailsView.tsx
+++ react-components/src/components/View/Image360HistoricalDetailsView.tsx
@@ -3,7 +3,7 @@
  */
 
 import { Cognite3DViewer, Image360 } from '@cognite/reveal';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image360HistoricalDetailsPanel } from '../Panel/Image360HistoricalDetailsPanel';
 import { Image360HistoricalOverviewToolbar } from '../Toolbar/Image360HistoricalOverviewToolbar';
 import { formatDate } from '../utils/FormatDate';
@@ -36,51 +36,37 @@ export const Image360HistoricalDetailsView = ({
   const [imageUrls, setImageUrls] = useState<(string | undefined)[]>([]);
 
   useEffect(() => {
-    return () => {
-      setImageUrls((urls) => {
-        urls.forEach((url) => {
-          if (url) {
-            URL.revokeObjectURL(url);
+    const fetchRevisionCollection = async () => {
+      if (image360Entity) {
+        // Remove old image urls
+        setImageUrls((urls) => {
+          urls.forEach((url) => {
+            if (url) {
+              URL.revokeObjectURL(url);
+            }
+          });
+          return [];
+        });
+
+        const revisions = image360Entity.getRevisions();
+        const revisionDates = revisions.map((revision) => revision.date);
+        const imageDatas = await Promise.all(revisions.map( async revision => await revision.getPreviewThumbnailUrl()));
+        setImageUrls(imageDatas);
+
+        const collection = revisionDates.map((date, index) => {
+          return {
+            date: formatDate(date!),
+            imageUrl: imageDatas[index]!,
+            index: index,
+            image360Entity: image360Entity
           }
         });
-        return [];
-      });
+
+        setRevisionCollection(collection);
+      }
     };
-  }, [imageUrls]);
 
-  useMemo(async () => {
-    if (image360Entity) {
-      const revisionDates = image360Entity
-          .getRevisions()
-          .map((revision) => revision.date);
-        const revisionDetails: { date: Date | undefined; imageUrl: string }[] =
-          [];
-        revisionDates.forEach((date) => {
-          revisionDetails.push({
-            date: date !== undefined ? date : undefined,
-            imageUrl: '',
-          });
-        });
-
-      const revisions = image360Entity.getRevisions();
-      const imageDatas = await Promise.all(revisions.map( async revision => await revision.getPreviewThumbnailUrl()));
-      setImageUrls(imageDatas);
-      const collection: {
-        date: string;
-        imageUrl: string | undefined;
-        index: number;
-        image360Entity: Image360;
-      }[] = [];
-      revisionDetails.forEach((details, index) => {
-        collection.push({
-          date: formatDate(details.date!),
-          imageUrl: imageUrls[index]!,
-          index: index,
-          image360Entity: image360Entity,
-        });
-      });
-      setRevisionCollection(collection);
-    }
+    fetchRevisionCollection();
   }, [image360Entity]);
 
   return(
