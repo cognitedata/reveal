5146ce7b7a7f6adb57966a4b95a7085180d32702
Shubham Singh <shubham88singh@gmail.com>
2023-05-31 01:38:47 +0300

    fix(data-explorer): Adding more tables in timeseries details page (#1891)
    Co-authored-by: Shubham SIngh <shubham.singh@cognite.com>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- libs/data-exploration/containers/src/ResourceDetails/TimeseriesDetails/TimeseriesDetails.tsx
+++ libs/data-exploration/containers/src/ResourceDetails/TimeseriesDetails/TimeseriesDetails.tsx
@@ -9,12 +9,23 @@ import { TimeseriesChart } from '@cognite/plotting-components';
 
 import {
   useAssetsByIdQuery,
+  useDocumentSearchResultQuery,
+  useEventsListQuery,
+  useSequenceListQuery,
   useTimeseriesByIdsQuery,
+  useTimeseriesListQuery,
 } from '@data-exploration-lib/domain-layer';
 
-import { AssetDetailsTable } from '../../DetailsTable';
+import {
+  AssetDetailsTable,
+  EventDetailsTable,
+  FileDetailsTable,
+  SequenceDetailsTable,
+  TimeseriesDetailsTable,
+} from '../../DetailsTable';
 import { TimeseriesInfo } from '../../Info';
 import { onOpenResources } from '../AssetDetails';
+import { EVENTS, FILES, SEQUENCES, TIME_SERIES } from '../constant';
 
 interface Props {
   timeseriesId: number;
@@ -28,19 +39,69 @@ export const TimeseriesDetails: FC<Props> = ({
   onSelectClicked,
   onClose,
 }) => {
-  const { data, isFetched: isTimeseriesFetched } = useTimeseriesByIdsQuery([
-    { id: timeseriesId },
-  ]);
+  const {
+    data,
+    isFetched: isTimeseriesFetched,
+    isLoading: isParentTimeseriesLoading,
+  } = useTimeseriesByIdsQuery([{ id: timeseriesId }]);
   const timeseries = useMemo(() => {
     return data ? data[0] : undefined;
   }, [data]);
 
   const assetIds = timeseries?.assetId ? [timeseries.assetId] : [];
+
+  const isQueryEnabled = assetIds.length > 0;
   const { data: relatedAssets = [], isLoading: isAssetsLoading } =
     useAssetsByIdQuery(
       assetIds.map((id) => ({ id })),
-      { enabled: isTimeseriesFetched && assetIds.length > 0 }
+      { enabled: isTimeseriesFetched && isQueryEnabled }
     );
+  const {
+    hasNextPage: hasEventNextPage,
+    fetchNextPage: hasEventFetchNextPage,
+    isLoading: isEventsLoading,
+    data: events,
+  } = useEventsListQuery({ filter: { assetIds } }, { enabled: isQueryEnabled });
+
+  const {
+    hasNextPage: hasTimeseriesNextPage,
+    fetchNextPage: hasTimeseriesFetchNextPage,
+    isLoading: isTimeseriesLoading,
+    data: relatedTimeseries,
+  } = useTimeseriesListQuery(
+    { filter: { assetIds } },
+    { enabled: isQueryEnabled }
+  );
+
+  const {
+    hasNextPage: hasDocumentsNextPage,
+    fetchNextPage: hasDocumentsFetchNextPage,
+    isLoading: isDocumentsLoading,
+    results: relatedDocuments = [],
+  } = useDocumentSearchResultQuery(
+    {
+      filter: {
+        assetSubtreeIds: assetIds.map((value) => ({
+          value,
+        })),
+      },
+    },
+    { enabled: isQueryEnabled }
+  );
+
+  const {
+    hasNextPage: hasSequencesNextPage,
+    fetchNextPage: hasSequencesFetchNextPage,
+    isLoading: isSequencesLoading,
+    data: sequences = [],
+  } = useSequenceListQuery(
+    {
+      filter: {
+        assetIds,
+      },
+    },
+    { enabled: isQueryEnabled }
+  );
 
   return (
     <ResourceDetailsTemplate
@@ -50,7 +111,7 @@ export const TimeseriesDetails: FC<Props> = ({
       onClose={onClose}
       onSelectClicked={onSelectClicked}
     >
-      <StyledCollapse accordion ghost defaultActiveKey="details">
+      <StyledCollapse accordion ghost defaultActiveKey="preview">
         {timeseries ? (
           <Collapse.Panel key="preview" header={<h4>Preview</h4>}>
             <TimeseriesChart
@@ -65,7 +126,7 @@ export const TimeseriesDetails: FC<Props> = ({
             />
           </Collapse.Panel>
         ) : null}
-        <Collapse.Panel key="details" header={<h4>Details</h4>}>
+        <Collapse.Panel key="timeseries-details" header={<h4>Details</h4>}>
           {timeseries ? (
             <TimeseriesInfo timeseries={timeseries} />
           ) : (
@@ -73,16 +134,62 @@ export const TimeseriesDetails: FC<Props> = ({
           )}
         </Collapse.Panel>
         <Collapse.Panel header={<h4>Assets</h4>}>
-          <Wrapper>
-            <AssetDetailsTable
-              id="related-asset-timeseries-details"
-              data={relatedAssets}
-              isLoadingMore={isAssetsLoading}
-              onRowClick={(currentAsset) =>
-                onOpenResources('asset', currentAsset.id)
-              }
-            />
-          </Wrapper>
+          <AssetDetailsTable
+            id="related-asset-timeseries-details"
+            data={relatedAssets}
+            isLoadingMore={isAssetsLoading}
+            onRowClick={(currentAsset) =>
+              onOpenResources('asset', currentAsset.id)
+            }
+          />
+        </Collapse.Panel>
+        <Collapse.Panel
+          key="event-timeseries-detail"
+          header={<h4>{TIME_SERIES}</h4>}
+        >
+          <TimeseriesDetailsTable
+            id="timeseries-resource-event-detail-table"
+            data={relatedTimeseries}
+            hasNextPage={hasTimeseriesNextPage}
+            fetchMore={hasTimeseriesFetchNextPage}
+            isDataLoading={isParentTimeseriesLoading || isTimeseriesLoading}
+          />
+        </Collapse.Panel>
+        <Collapse.Panel
+          key="timeseries-documents-detail"
+          header={<h4>{FILES}</h4>}
+        >
+          <FileDetailsTable
+            id="documents-resource-timeseries-detail-table"
+            data={relatedDocuments}
+            hasNextPage={hasDocumentsNextPage}
+            fetchMore={hasDocumentsFetchNextPage}
+            isDataLoading={isParentTimeseriesLoading || isDocumentsLoading}
+          />
+        </Collapse.Panel>
+        <Collapse.Panel
+          key="timeseries-events-detail"
+          header={<h4>{EVENTS}</h4>}
+        >
+          <EventDetailsTable
+            id="event-resource-timeseries-detail-table"
+            data={events}
+            hasNextPage={hasEventNextPage}
+            fetchMore={hasEventFetchNextPage}
+            isDataLoading={isParentTimeseriesLoading || isEventsLoading}
+          />
+        </Collapse.Panel>
+        <Collapse.Panel
+          key="timeseries-sequence-detail"
+          header={<h4>{SEQUENCES}</h4>}
+        >
+          <SequenceDetailsTable
+            id="sequence-resource-timeseries-detail-table"
+            data={sequences}
+            hasNextPage={hasSequencesNextPage}
+            fetchMore={hasSequencesFetchNextPage}
+            isDataLoading={isParentTimeseriesLoading || isSequencesLoading}
+          />
         </Collapse.Panel>
       </StyledCollapse>
     </ResourceDetailsTemplate>
@@ -90,10 +197,5 @@ export const TimeseriesDetails: FC<Props> = ({
 };
 
 const StyledCollapse = styled(Collapse)`
-  overflow: auto;
-`;
-
-const Wrapper = styled.div`
-  max-height: 240px;
   overflow: auto;
 `;
