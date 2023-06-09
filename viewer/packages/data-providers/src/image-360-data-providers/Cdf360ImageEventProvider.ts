a67fbd5076f97aaea54a74010c3cac2cbc9502ac
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2023-06-09 14:47:36 +0200

    fix: make sure autoPagingToArray gives all results (#3364)
    * fix: make sure autoPagingToArray gives all results
    
    * refac: use common function to filter assets

--- viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageEventProvider.ts
+++ viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageEventProvider.ts
@@ -22,7 +22,8 @@ import {
   Metadata,
   CogniteInternalId,
   AnnotationsCogniteAnnotationTypesImagesAssetLink,
-  AnnotationData
+  AnnotationData,
+  AnnotationFilterProps
 } from '@cognite/sdk';
 import { Historical360ImageSet, Image360EventDescriptor, Image360Face, Image360FileDescriptor } from '../types';
 import { Image360Provider } from '../Image360Provider';
@@ -77,16 +78,10 @@ export class Cdf360ImageEventProvider implements Image360Provider<Metadata> {
   public async get360ImageAnnotations(descriptors: Image360FileDescriptor[]): Promise<AnnotationModel[]> {
     const fileIds = descriptors.map(o => ({ id: o.fileId }));
 
-    const annotationsResult = this._client.annotations.list({
-      filter: {
-        annotatedResourceType: 'file',
-        annotatedResourceIds: fileIds
-      }
+    return this.listFileAnnotations({
+      annotatedResourceType: 'file',
+      annotatedResourceIds: fileIds
     });
-
-    const annotationArray = await annotationsResult.autoPagingToArray();
-
-    return annotationArray;
   }
 
   public async getFilesByAssetRef(assetRef: IdEither): Promise<CogniteInternalId[]> {
@@ -339,15 +334,12 @@ export class Cdf360ImageEventProvider implements Image360Provider<Metadata> {
   public async get360ImageAssets(image360FileDescriptors: Image360FileDescriptor[]): Promise<IdEither[]> {
     const fileIds = image360FileDescriptors.map(desc => desc.fileId);
     const assetListPromises = chunk(fileIds, 1000).map(async idList => {
-      const annotationArray = await this._client.annotations
-        .list({
-          filter: {
-            annotatedResourceIds: idList.map(id => ({ id })),
-            annotatedResourceType: 'file',
-            annotationType: 'images.AssetLink'
-          }
-        })
-        .autoPagingToArray();
+      const annotationArray = await this.listFileAnnotations({
+        annotatedResourceIds: idList.map(id => ({ id })),
+        annotatedResourceType: 'file',
+        annotationType: 'images.AssetLink'
+      });
+
       const assetIds = annotationArray.map(annotation => {
         assert(isAssetLinkAnnotationData(annotation.data), 'Received annotation that was not an assetLink');
         return annotation.data.assetRef;
@@ -359,6 +351,15 @@ export class Cdf360ImageEventProvider implements Image360Provider<Metadata> {
     const assetIds = (await Promise.all(assetListPromises)).flat().filter(isIdEither);
 
     return assetIds;
+  }
+
+  private async listFileAnnotations(filter: AnnotationFilterProps): Promise<AnnotationModel[]> {
+    return this._client.annotations
+      .list({
+        limit: 1000,
+        filter
+      })
+      .autoPagingToArray({ limit: Infinity });
   }
 }
 
