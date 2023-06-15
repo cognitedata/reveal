eef14be0061c8387176590ac56c290ee95614d9e
Branko  Dimitrijoski <branko.dimitrijoski@cognitedata.com@BrankosgniteMBP.home>
2023-06-15 14:42:18 +0200

    Move file and folders into appropriate loc, removing autogenerated components

--- apps/entity-matching/src/app/App.tsx
+++ apps/entity-matching/src/app/App.tsx
@@ -1,49 +1,91 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import styled from 'styled-components/macro';
-import { ToastContainer } from '@cognite/cogs.js';
+import { I18nWrapper } from '@cognite/cdf-i18n-utils';
+import sdk, { loginAndAuthIfNeeded } from '@cognite/cdf-sdk-singleton';
+import {
+  AuthWrapper,
+  getEnv,
+  getProject,
+  SubAppWrapper,
+} from '@cognite/cdf-utilities';
+import { Loader, ToastContainer } from '@cognite/cogs.js';
+import { SDKProvider } from '@cognite/sdk-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { getProject } from '@cognite/cdf-utilities';
-import { queryClient } from './queryClient';
 
-import Routes from './Routes';
+import { translations } from 'common/i18n';
+import RootList from 'pages/Home';
+import GlobalStyles from 'styles/GlobalStyles';
+import { FlagProvider } from '@cognite/react-feature-flags';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Details from 'pages/Details';
+import QuickMatch from 'pages/quick-match';
+import { CogniteError } from '@cognite/sdk/dist/src';
+import Pipeline from 'pages/pipeline';
 
-function App() {
-  const project = getProject();
-  const basename = `${project}/entity-matching`;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchIntervalInBackground: true,
+      retry(count, error) {
+        if ((error as CogniteError).status === 403) {
+          return false;
+        }
+        return count <= 3;
+      },
+    },
+  },
+});
+const env = getEnv();
+const project = getProject();
 
+const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <ToastContainer />
-      <StyledWrapper>
-        <Router
-          basename={basename}
-          window={window}
-          children={
-            <StyledPage>
-              <Routes />
-            </StyledPage>
-          }
-        />
-      </StyledWrapper>
-    </QueryClientProvider>
+    <FlagProvider
+      appName="cdf-ui-entity-matching"
+      apiToken="v2Qyg7YqvhyAMCRMbDmy1qA6SuG8YCBE"
+      projectName={project}
+    >
+      <I18nWrapper
+        translations={translations}
+        defaultNamespace="entity-matching"
+      >
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyles>
+            <ToastContainer />
+            <SubAppWrapper title="Entity matching">
+              <AuthWrapper
+                loadingScreen={<Loader />}
+                login={() => loginAndAuthIfNeeded(project, env)}
+              >
+                <SDKProvider sdk={sdk}>
+                  <BrowserRouter>
+                    <Routes>
+                      <Route
+                        path="/:projectName/:subAppPath"
+                        element={<RootList />}
+                      />
+                      <Route
+                        path="/:projectName/:subAppPath/quick-match*"
+                        element={<QuickMatch />}
+                      />
+                      <Route
+                        path="/:projectName/:subAppPath/pipeline/:pipelineId*"
+                        element={<Pipeline />}
+                      />
+                      <Route
+                        path="/:projectName/:subAppPath/:id"
+                        element={<Details />}
+                      />
+                    </Routes>
+                  </BrowserRouter>
+                </SDKProvider>
+              </AuthWrapper>
+            </SubAppWrapper>
+          </GlobalStyles>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </I18nWrapper>
+    </FlagProvider>
   );
-}
+};
 
 export default App;
-
-const StyledWrapper = styled.div`
-  display: flex;
-  flex-flow: column;
-  height: 100%;
-  overflow: hidden;
-`;
-
-const StyledPage = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  overflow: hidden;
-`;
