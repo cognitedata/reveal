622cb797522ee86b04b3c431a43fa1ce104afeb4
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2023-06-19 11:25:08 +0200

    Update

--- apps/functions-ui/node_modules/cron-validator/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare type Options = {
-    alias: boolean;
-    seconds: boolean;
-    allowBlankDay: boolean;
-    allowSevenAsSunday: boolean;
-};
-export declare const isValidCron: (cron: string, options?: Partial<Options> | undefined) => boolean;
-export {};
