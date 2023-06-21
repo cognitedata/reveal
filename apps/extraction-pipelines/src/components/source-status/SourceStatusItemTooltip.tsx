2a83bf3d1f27a7b3df85ec39e32b1678800b31e0
Marissa Pang <43666277+marsipang@users.noreply.github.com>
2023-06-21 11:58:01 -0500

    Update src/components/source-status/SourceStatusItemTooltip.tsx
    Co-authored-by: Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>

--- apps/extraction-pipelines/src/components/source-status/SourceStatusItemTooltip.tsx
+++ apps/extraction-pipelines/src/components/source-status/SourceStatusItemTooltip.tsx
@@ -86,7 +86,7 @@ const SourceStatusItemTooltip = ({
           <Flex direction="column">
             <span>{formatTime(aggregation.endTime, true)}</span>
             <span>
-              <Flex css={{ gap: '5px' }}>
+              <Flex gap={5}>
                 {aggregation.uptimePercentage !== -1 &&
                   isPausedEntireTime === false &&
                   getUptimeIcon(aggregation.uptimePercentage)}
