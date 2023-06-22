c559f9c21eb881b0f88d4069ef77f69790f8ff83
Christopher Tannum <christopher.tannum@cognite.com>
2023-06-22 09:56:28 +0200

    asd

--- react-components/src/components/index.ts
+++ react-components/src/components/index.ts
@@ -1,2 +1,3 @@
 import '@cognite/cogs.js/dist/cogs.css';
+export { default as RevealContainer } from './RevealContainer/RevealContainer';
 export { Image360HistoricalDetails } from './Image360HistoricalDetails/Image360HistoricalDetails';
