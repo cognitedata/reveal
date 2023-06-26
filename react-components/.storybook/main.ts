3d23d4641a60ed3cedb510f6c4509e0d0d93082e
Christopher Tannum <christopher.tannum@cognite.com>
2023-06-26 12:40:04 +0200

    fix: set proper base URL for build

--- react-components/.storybook/main.ts
+++ react-components/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
