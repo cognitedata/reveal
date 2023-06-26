9d92258e3c82379a6e3aa91aa7322996521ee055
Christopher Tannum <christopher.tannum@cognite.com>
2023-06-26 15:04:00 +0200

    fix: QA

--- react-components/src/stories/CogniteCadModelContainer.stories.tsx
+++ react-components/src/stories/CogniteCadModelContainer.stories.tsx
@@ -1,31 +1,41 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { CogniteCadModelContainer, RevealContainer } from '..';
-import { CogniteClient } from '@cognite/sdk';
-import { Color, Matrix4 } from 'three';
+import type { Meta, StoryObj } from "@storybook/react";
+import { CogniteCadModelContainer, RevealContainer } from "..";
+import { CogniteClient } from "@cognite/sdk";
+import { Color, Matrix4 } from "three";
 
 const meta = {
-  title: 'Example/CogniteCadModelContainer',
+  title: "Example/CogniteCadModelContainer",
   component: CogniteCadModelContainer,
-  tags: ['autodocs']
+  tags: ["autodocs"],
 } satisfies Meta<typeof CogniteCadModelContainer>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const token = new URLSearchParams(window.location.search).get('token')!;
-const sdk = new CogniteClient({ appId: 'reveal.example', baseUrl: 'https://greenfield.cognitedata.com', project: '3d-test', getToken: () => Promise.resolve(token) });
+// TODO: See if we can improve this with some proper authentication
+const token = new URLSearchParams(window.location.search).get("token")!;
+const sdk = new CogniteClient({
+  appId: "reveal.example",
+  baseUrl: "https://greenfield.cognitedata.com",
+  project: "3d-test",
+  getToken: () => Promise.resolve(token),
+});
 
 export const Main: Story = {
   args: {
     addModelOptions: {
       modelId: 1791160622840317,
-      revisionId: 498427137020189
+      revisionId: 498427137020189,
     },
-    transform: new Matrix4().makeTranslation(0, 10, 0)
+    transform: new Matrix4().makeTranslation(0, 10, 0),
   },
-  render: ({ addModelOptions, transform }) =>
-    <RevealContainer sdk={sdk} color={new Color(0x4a4a4a)} >
+  render: ({ addModelOptions, transform }) => (
+    <RevealContainer sdk={sdk} color={new Color(0x4a4a4a)}>
       <CogniteCadModelContainer addModelOptions={addModelOptions} />
-      <CogniteCadModelContainer addModelOptions={addModelOptions} transform={transform} />
+      <CogniteCadModelContainer
+        addModelOptions={addModelOptions}
+        transform={transform}
+      />
     </RevealContainer>
+  ),
 };
