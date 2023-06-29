30af87f0a0b176898bdeda7d870500baa366feb4
Christopher J. Tannum <christopher.tannum@cognite.com>
2023-06-29 10:16:52 +0200

    feat(flexible-data-explorer): add initial scaffolding for 3D page (#2274)
    * feat(flexible-data-explorer): add initial scaffolding for 3D page
    
    * chore: replace mock model with 'primitives' model
    
    * chore: bump reveal-react-components
    
    * chore: cleanup methods / simplify based on QA
    
    * fix: remove code comments
    
    * chore: simplify hook usage
    
    * fix: change type to react FC
    
    ---------
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/flexible-data-explorer/src/config/Cognite.ts
+++ apps/flexible-data-explorer/src/config/Cognite.ts
@@ -22,4 +22,20 @@ export const cogniteConfig: ProjectConfig[] = [
     ],
     nodeSpaces: undefined,
   },
+  {
+    site: 'My Site',
+    description: 'Test site for 3D visualization.',
+    imageUrl:
+      'https://probea.no/wp-content/uploads/2019/03/probea_fornebu-17-1440x760.jpg',
+    project: '3d-test',
+    dataModels: [],
+    nodeSpaces: undefined,
+    threeDResources: [
+      {
+        type: 'cad',
+        modelId: 1791160622840317,
+        revisionId: 498427137020189,
+      },
+    ],
+  },
 ];
