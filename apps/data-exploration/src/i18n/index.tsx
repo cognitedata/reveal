cc41728635ee41abf443c98df6c2d2420b43ae63
Branko Dimitrijoski <40126033+bdimitrijoski@users.noreply.github.com>
2023-06-30 13:03:28 +0200

    added 10 langs for data-exploration (#2288)
    Co-authored-by: Branko  Dimitrijoski <branko.dimitrijoski@cognitedata.com@BrankosgniteMBP.home>

--- apps/data-exploration/src/i18n/index.tsx
+++ apps/data-exploration/src/i18n/index.tsx
@@ -1,8 +1,28 @@
+import * as de from './de/data-exploration.json';
 import * as en from './en/data-exploration.json';
+import * as es from './es/data-exploration.json';
+import * as fr from './fr/data-exploration.json';
+import * as it from './it/data-exploration.json';
+import * as ja from './ja/data-exploration.json';
+import * as ko from './ko/data-exploration.json';
+import * as nl from './nl/data-exploration.json';
+import * as pt from './pt/data-exploration.json';
+import * as sv from './sv/data-exploration.json';
+import * as zh from './zh/data-exploration.json';
 
 /**
  * Platypus app translations
  */
 export const translations = {
   en: { 'data-exploration': en },
+  de: { 'data-exploration': de },
+  es: { 'data-exploration': es },
+  fr: { 'data-exploration': fr },
+  it: { 'data-exploration': it },
+  ja: { 'data-exploration': ja },
+  ko: { 'data-exploration': ko },
+  nl: { 'data-exploration': nl },
+  pt: { 'data-exploration': pt },
+  sv: { 'data-exploration': sv },
+  zh: { 'data-exploration': zh },
 };
