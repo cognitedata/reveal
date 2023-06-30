e36144d19879b594d1f0d1a2fcbbe28e5b4953af
Branko Dimitrijoski <40126033+bdimitrijoski@users.noreply.github.com>
2023-06-30 15:22:05 +0200

    feat: add industrial-canvas translations (#2292)
    * add industrial-canvas translations
    
    * fixed tests
    
    ---------
    
    Co-authored-by: Branko  Dimitrijoski <branko.dimitrijoski@cognitedata.com@BrankosgniteMBP.home>

--- libs/industry-canvas/src/lib/common/i18n/index.tsx
+++ libs/industry-canvas/src/lib/common/i18n/index.tsx
@@ -1,12 +1,30 @@
-import en from './en/industrial-canvas.json';
-import ja from './ja/industrial-canvas.json';
+import * as de from './de/industrial-canvas.json';
+import * as en from './en/industrial-canvas.json';
+import * as es from './es/industrial-canvas.json';
+import * as fr from './fr/industrial-canvas.json';
+import * as it from './it/industrial-canvas.json';
+import * as ja from './ja/industrial-canvas.json';
+import * as ko from './ko/industrial-canvas.json';
+import * as nl from './nl/industrial-canvas.json';
+import * as pt from './pt/industrial-canvas.json';
+import * as sv from './sv/industrial-canvas.json';
+import * as zh from './zh/industrial-canvas.json';
 
 /**
- * industry-canvas-ui app translations
+ * Platypus app translations
  */
 export const translations = {
-  en: { 'industrial-canvas': en },
-  ja: { 'industrial-canvas': ja },
+  en: { 'data-exploration': en },
+  de: { 'data-exploration': de },
+  es: { 'data-exploration': es },
+  fr: { 'data-exploration': fr },
+  it: { 'data-exploration': it },
+  ja: { 'data-exploration': ja },
+  ko: { 'data-exploration': ko },
+  nl: { 'data-exploration': nl },
+  pt: { 'data-exploration': pt },
+  sv: { 'data-exploration': sv },
+  zh: { 'data-exploration': zh },
 };
 
 export * from './translationKeys';
