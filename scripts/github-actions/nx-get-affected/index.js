bc37868abfbe98db914d35504c011e02e50bccff
Samir Ajdarpasic <samir.ajdarpasic@cognite.com>
2023-07-10 15:29:03 +0200

    fix

--- scripts/github-actions/nx-get-affected/index.js
+++ scripts/github-actions/nx-get-affected/index.js
@@ -2,34 +2,32 @@ const { execSync } = require('child_process');
 
 const { setFailed, setOutput, getInput, info } = require('@actions/core');
 
-const run = async () => {
-  try {
-    const base = getInput('base') || 'origin/master';
-    const head = getInput('head') || 'HEAD';
-    const target = getInput('target') || 'build';
-    const type = getInput('type');
+try {
+  const base = getInput('base') || 'origin/master';
+  const head = getInput('head') || 'HEAD';
+  const target = getInput('target') || 'build';
+  const type = getInput('type');
 
-    const projects = execSync(
-      `npx nx show projects --affected  --json --withTarget=${target} --base=${base} --head=${head} ${
-        type ? `--projects ${type}/*` : ''
-      }`
-    ).toString('utf-8');
+  const projects = execSync(
+    `npx nx show projects --affected  --json --withTarget=${target} --base=${base} --head=${head} ${
+      type ? `--projects ${type}/*` : ''
+    }`
+  ).toString('utf-8');
 
-    info(`Output from NX: ${projects}`);
+  console.log(projects);
 
-    const parsedOutput = JSON.parse(projects);
-    setOutput('list', parsedOutput);
+  info(`Output from NX: ${projects}`);
 
-    parsedOutput.forEach((project) => {
-      setOutput(project, true);
-    });
-  } catch (error) {
-    if (error instanceof Error || typeof error === 'string') {
-      setFailed(error);
-    } else {
-      setFailed('Unknown error occured.');
-    }
+  const parsedOutput = JSON.parse(projects);
+  setOutput('list', parsedOutput);
+
+  parsedOutput.forEach((project) => {
+    setOutput(project, true);
+  });
+} catch (error) {
+  if (error instanceof Error || typeof error === 'string') {
+    setFailed(error);
+  } else {
+    setFailed('Unknown error occured.');
   }
-};
-
-run();
+}
