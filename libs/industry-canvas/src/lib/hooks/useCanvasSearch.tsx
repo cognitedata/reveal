db46f8f2f50caeb15476509f27ad6e6602355826
Marvin <marvinreza@outlook.com>
2023-07-12 03:13:51 +0200

    fix(industrial-canvas): fix home page and canvas dropdown UX (#2370)
    * fix(industrial-canvas): fix home page UX
    
    * Fix debounce logic for a bit better performance when using the search
      bar
    * Show loader when the canvases are being listed
    * Disable create canvas button when canvases are being listed
    * Sort by last updated time in FDM, rather than in the frontend
    
    * fix: add translation for loading canvas text
    
    * chore: remove unnecessary css
    
    * fix: fix focus bug with canvas dropdown
    
    * fix: fix canvas search in canvas dropdown
    
    * When a project had a lot of canvases, we would previously try to
      render all of them, which would freeze the application. Now we
      dynamically render the canvas entries in the dropdown, based on the
      current scroll height.
    * Fix search performance by applying debouncing
    * Fix placeholder for canvas dropdown input field
    
    ---------
    
    Co-authored-by: Marvin Reza <marvinreza@Marvins-MacBook-Pro.local>
    Co-authored-by: Marvin Reza <marvinreza@Marvins-MBP.home>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- libs/industry-canvas/src/lib/hooks/useCanvasSearch.tsx
+++ libs/industry-canvas/src/lib/hooks/useCanvasSearch.tsx
@@ -1,11 +1,7 @@
 import { useMemo } from 'react';
 
-import { useDebounce } from 'use-debounce';
-
 import { UserProfile } from '../UserProfileProvider';
 import caseInsensitiveIncludes from '../utils/caseInsensitiveIncludes';
-
-const SEARCH_DEBOUNCE_MS = 100;
 
 type UseCanvasSearchProps<T> = {
   canvases: T[];
@@ -27,23 +23,21 @@ const useCanvasSearch = <
   canvases,
   searchString,
 }: UseCanvasSearchProps<T>): UseCanvasSearchReturnType<T> => {
-  const [debouncedSearchString] = useDebounce(searchString, SEARCH_DEBOUNCE_MS);
-
   const filteredCanvases = useMemo(
     () =>
       canvases.filter(
         (canvas) =>
-          caseInsensitiveIncludes(canvas.name, debouncedSearchString) ||
+          caseInsensitiveIncludes(canvas.name, searchString) ||
           caseInsensitiveIncludes(
             canvas.createdByUserProfile?.displayName,
-            debouncedSearchString
+            searchString
           ) ||
           caseInsensitiveIncludes(
             canvas.updatedByUserProfile?.displayName,
-            debouncedSearchString
+            searchString
           )
       ),
-    [debouncedSearchString, canvases]
+    [searchString, canvases]
   );
 
   return {
