2601efc2fd41c8351d8409a3625af919f7342c07
sennen-cognite <sennen.almeida@cognite.com>
2023-07-17 08:41:58 +0530

    fix: sequence relationship table

--- libs/data-exploration/containers/src/Search/SearchResults/SequenceSearchResults/index.ts
+++ libs/data-exploration/containers/src/Search/SearchResults/SequenceSearchResults/index.ts
@@ -2,3 +2,4 @@ export * from './SequenceSearchResults';
 export * from './SequenceLinkedSearchResults';
 export * from './SequenceTable';
 export * from './useSequencesMetadataColumns';
+export * from './SequenceRelatedSearchResults';
