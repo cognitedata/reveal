6a48fd1e3aaa4766cb055008f9f82755363f3f44
Stefan Maretic <stefan.maretic@gmail.com>
2023-07-21 11:14:13 +0200

    feat: modify unifiedSigninTokenProvider

--- libs/shared/cdf-sdk-singleton/src/unifiedSigninTokenProvider.ts
+++ libs/shared/cdf-sdk-singleton/src/unifiedSigninTokenProvider.ts
@@ -7,49 +7,60 @@ import {
 
 import { SdkClientTokenProvider } from './types';
 
+// TODO: we need a way to read the url login hints as well, but need to make
+// sure they're always present if we want to rely on them.
 const { organization, cluster, idpInternalId } = readLoginHints() ?? {};
 
 export class UnifiedSigninTokenProvider implements SdkClientTokenProvider {
   private service: OidcService | undefined;
+
   getAppId() {
     return 'apps.cognite.com/cdf';
   }
 
   async getToken() {
-    const service = await this.getService();
-    return service.acquireTokenSilent(cluster);
+    await this.initService();
+    const token = await this.service?.acquireTokenSilent(cluster);
+    if (!token) {
+      throw new Error('Failed to get token');
+    }
+    return token;
   }
 
   async getUserInformation() {
-    const service = await this.getService();
-    return service.getUser();
+    await this.initService();
+    return this.service?.getUser();
   }
 
-  getFlow() {
-    if (!this.service) {
-      return Promise.reject(new Error('Service is not initialized'));
-    }
-    return { flow: this.service.idp.type as string };
+  async getFlow() {
+    await this.initService();
+    return { flow: this.service?.idp.type as string };
   }
 
   async logout() {
-    const service = await this.getService();
-    service.logout();
+    await this.initService();
+    this.service?.logout();
+  }
+
+  private async initService() {
+    if (!this.service) {
+      this.service = await this.getService();
+    }
+    // If the IdP has changed, we need to reinitialize the service.
+    if (this.service.idp.internalId !== idpInternalId) {
+      this.service = await this.getService();
+    }
   }
 
   private async getService() {
-    if (!this.service) {
-      if (!organization) {
-        return Promise.reject(new Error('Missing login hints'));
-      }
-      const dlc = await getDlc(organization);
-      const idp = await getIdp(dlc.idps, idpInternalId);
-      if (!idp) {
-        return Promise.reject(new Error('Missing idp'));
-      }
-      this.service = new OidcService(idp, organization, cluster);
+    if (!organization || !idpInternalId || !cluster) {
+      return Promise.reject(new Error('Missing login hints'));
     }
-
-    return this.service;
+    const dlc = await getDlc(organization);
+    const idp = await getIdp(dlc.idps, idpInternalId);
+    if (!idp) {
+      return Promise.reject(new Error('Missing idp'));
+    }
+    return new OidcService(idp, organization, cluster);
   }
 }
