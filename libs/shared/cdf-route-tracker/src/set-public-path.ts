4f211bec1bc7711909ffb615653df1c593046ca8
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2023-07-21 15:46:07 +0200

    fix: disable setPublicPath call for route-tracker (#2489)

--- libs/shared/cdf-route-tracker/src/set-public-path.ts
+++ libs/shared/cdf-route-tracker/src/set-public-path.ts
@@ -1,8 +1,11 @@
-import { setPublicPath } from 'systemjs-webpack-interop';
+// import { setPublicPath } from 'systemjs-webpack-interop';
 /* This dynamically sets the webpack public path so that code splits work properly. See related:
  * https://github.com/joeldenning/systemjs-webpack-interop#what-is-this
  * https://webpack.js.org/guides/public-path/#on-the-fly
  * https://single-spa.js.org/docs/faq/#code-splits
  */
 
-setPublicPath('@cognite/cdf-route-tracker');
+// we don't use systemjs is business-shell so commenting out related codes
+// in fusion, cdf-route-tracker is available as an external dependency
+// so it should work for both sides
+// setPublicPath('@cognite/cdf-route-tracker');
