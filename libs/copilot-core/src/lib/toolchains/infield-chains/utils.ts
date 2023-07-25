834b3b04bf4e0840d4dbfbd907dbcde0acfc77c3
Espen Aakre Sjo <espen.sjo@cognite.com>
2023-07-25 14:10:11 +0200

    feat(copilot-core): Added support for properties and relevant docs (metadata) + bugfixes (#2444)
    * initial commit with components for infield logic
    
    * working test env for gpt at infield-dev/infield-app/
    
    * added template for functions
    
    * deleted
    
    * add vectorstore api
    
    * added some infield relevant examples
    
    * started work on a summaryChain, not yet done
    
    * add retrieveContext from vectorstore API
    
    * bug fix in vectorstore api call
    
    * changed structure
    
    * Working ish query bot
    
    * updated prompts
    
    * prompt engineering
    
    * add more information in query_prompt
    
    * remove hardcoding of queryContext function
    
    * Updated support for ada embedding
    
    * cleaned up a little
    
    * increased context window
    
    * updated prompting and started on indexing answer
    
    * Improved prompt
    
    * implemented map reduce for gpt calls
    
    Co-authored-by: mariebri <mariebri@users.noreply.github.com>
    
    * resolved conflicts with new main. PS: Does not work with yarn link now due to @fusion/data-modeling
    
    * refactor
    
    * refactor
    
    * updated prompt and querychain
    
    * Added a starting point for summaryChain
    
    * Added sourceList to retrieve source information from context
    
    * fixed bug
    
    * refactor and updated filtering useless answers
    
    * bugfix
    
    * added ability to communicate in different languages
    
    * Added a "Get Source" button
    
    * Minor cleanup after merging
    
    * sends page language to chat-GPT
    
    * removed typo
    
    * Implemented a function for calling GPT with retries and timeouts
    
    * add try-catch
    
    * Modifications to the "Get source" button
    
    * add language support in cogpilot hello msg
    
    * fixed comments on pr
    
    * fixed small errors
    
    * Fixing after merge with main
    
    * updated the printSources function
    
    * Implemented refine method to summaryChain
    
    * moved docQuery prompts to llm-hub
    
    * Updated prompt and description
    
    * deleted old summaryChain and moved prompts externally
    
    * updated import names
    
    * Resolved duplicated items in sourceList
    
    * Minor changes
    
    * linked to llm-hub
    
    * Linked to llm-hub in summaryChain
    
    * WIP
    
    * feat: error handling and fully connected search
    
    * llm-hub linking fixed
    
    * wip
    
    * fix
    
    * fix
    
    * wip
    
    * Added "Get Source"-button to new UI
    
    * Initial functionality for WorkorderChain
    
    * Minor fixes after merge
    
    * updated from test-infield-2 with new changes
    
    * upd
    
    * eslint
    
    * updated visualizing of sources (added buttons)
    
    * Functioning workorderChain
    
    * not needed
    
    * Added language detection to workOrderChain
    
    * input cleaning, underways messages and failsafes
    
    * Fine tuned prompt
    
    * Loading message for workorderChain
    
    * updated structure of source function
    
    * Fixed prompts
    
    * small fixes
    
    * fix
    
    * fix: copilot timeout
    
    * Changed chain structure after merge update
    
    * fix:
    
    * bugs
    
    * return relevant docs
    
    * recommending relevant documents when no answer
    
    * removed unused
    
    * connecting with infield properly (finally removing hardcode)
    
    * bug
    
    * Modified inputs to workOrderChain
    
    * Including support for searching metadata (properties)
    
    * removed unused
    
    * small update
    
    * Update getActions.ts
    
    * Delete webpack-config.js
    
    * Updated summaryChain to include new changes
    
    * updated dependency and fixed small bug
    
    * lock
    
    * updated package.json
    
    * lock?
    
    * Minor bug fixes
    
    * lock delete
    
    * updated pushing of id and page to infield
    
    * Added "Unsupported" feature for assets not ready for CogPilot
    
    ---------
    
    Co-authored-by: Elise Müller <elise.muller@cognite.com>
    Co-authored-by: Marie Bringsvor <marie.bringsvor@cognite.com>
    Co-authored-by: mariebri <mariebri@users.noreply.github.com>
    Co-authored-by: David Liu <david.liu@cognite.com>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: mariebri <77635579+mariebri@users.noreply.github.com>

--- libs/copilot-core/src/lib/toolchains/infield-chains/utils.ts
+++ libs/copilot-core/src/lib/toolchains/infield-chains/utils.ts
@@ -1,4 +1,5 @@
 import { generalLanguageDetectTranslationPrompt } from '@cognite/llm-hub';
+import { Asset, CogniteClient } from '@cognite/sdk/dist/src';
 
 import {
   CogniteBaseChain,
@@ -6,8 +7,20 @@ import {
   safeConvertToJson,
 } from '../../CogniteBaseChain';
 import { CopilotAction } from '../../types';
-import { addToCopilotEventListener, sendFromCopilotEvent } from '../../utils';
-import { sourceResponse, translationResponse } from '../../utils/types';
+import {
+  addToCopilotEventListener,
+  sendFromCopilotEvent,
+  sendToCopilotEvent,
+} from '../../utils';
+import {
+  queryResponse,
+  sourceResponse,
+  translationResponse,
+} from '../../utils/types';
+import { retrieveAsset } from '../../utils/utils';
+
+export let externalAssetIdGlobal: string = '';
+export let assetDescriptionGlobal: string = '';
 
 // Function for sending event to Infield, and retrieving the external asset id.
 export const getExternalId = async () => {
@@ -20,44 +33,74 @@ export const getExternalId = async () => {
   });
 };
 
-// Function for sending event and documentId to Infield, letting Infield handle logic for viewing document.
-export const pushDocumentId = async (docId: string) => {
-  return new Promise((resolve) => {
-    const unmount = addToCopilotEventListener('PUSH_DOC_ID', (e) => {
-      unmount();
-      resolve(e);
-    });
-    sendFromCopilotEvent('PUSH_DOC_ID', { content: docId });
-  });
-};
-
 // Function for printing the sources used to find multiple answers
 // Also used to generate CopilotActions for opening the documents
 export function prepareSources(sourceList: sourceResponse[]) {
+  if (sourceList.length === 0) {
+    return { sourceString: '', openDocActionList: [] };
+  }
+
   let sourceString = '';
   const openDocActionList: CopilotAction[] = [];
   for (let i = 0; i < sourceList.length; i++) {
+    let page;
+    try {
+      page = parseInt(sourceList[i].page) + 1;
+    } catch (e) {
+      page = sourceList[i].page;
+    }
     if (
-      !(
-        sourceString.includes(sourceList[i].source) &&
-        sourceString.includes(sourceList[i].page)
+      sourceString.includes(
+        `$Source: ${sourceList[i].source} \n [Page: ${page}]`
       )
     ) {
-      if (openDocActionList.length < 3) {
-        openDocActionList.push({
-          content: 'Open: ' + (openDocActionList.length + 1),
-          onClick: () => {
-            pushDocumentId('446078535171616'); //DOCUMENT ID HERE
-          },
-        } as CopilotAction);
-      }
-
-      sourceString += `${openDocActionList.length}. Source: ${sourceList[i].source} \n Page: ${sourceList[i].page} \n\n`;
+      continue;
     }
+    if (openDocActionList.length < 5) {
+      openDocActionList.push({
+        content: 'Open: ' + (openDocActionList.length + 1),
+        fromCopilotEvent: [
+          'PUSH_DOC_ID_AND_PAGE',
+          {
+            docId: sourceList[i].fileId,
+            page: String(page) || '1',
+          },
+        ],
+      } as CopilotAction);
+    }
+
+    sourceString += `${i + 1}. ${sourceList[i].source} [Page: ${page}] \n`;
   }
+
   console.log(sourceString);
-  return [sourceString, openDocActionList];
+  return { sourceString: sourceString, openDocActionList: openDocActionList };
 }
+
+export const prepareRelevantSoruces = (queryContext: queryResponse) => {
+  const slicedSources = queryContext.items.slice(0, 3);
+  const sourceString = slicedSources
+    .map(
+      (s, i) =>
+        `${i + 1}. ${s.metadata.source} [Page: ${
+          (parseInt(s.metadata.page) || 0) + 1
+        }]`
+    )
+    .join('\n');
+  const openDocActionList = slicedSources.map(
+    (s, i) =>
+      ({
+        content: 'Open: ' + (i + 1),
+        fromCopilotEvent: [
+          'PUSH_DOC_ID_AND_PAGE',
+          {
+            docId: s.metadata.fileId,
+            page: String((parseInt(s.metadata.page) || 0) + 1) || '1',
+          },
+        ],
+      } as CopilotAction)
+  );
+  return { sourceString: sourceString, openDocActionList: openDocActionList };
+};
 
 // Function for sending getLanguage event to Infield, and retrieving the language of the page.
 export const getPageLanguage = async () => {
@@ -67,6 +110,17 @@ export const getPageLanguage = async () => {
       resolve(e.content || '');
     });
     sendFromCopilotEvent('GET_LANGUAGE', undefined);
+  });
+};
+
+// Function for getting the activities from Infield
+export const getActivities = async () => {
+  return new Promise((resolve) => {
+    const unmount = addToCopilotEventListener('GET_ACTIVITIES', (e) => {
+      unmount();
+      resolve(e.content || '');
+    });
+    sendFromCopilotEvent('GET_ACTIVITIES', undefined);
   });
 };
 
@@ -86,4 +140,48 @@ export const translateInputToEnglish = async (
   } catch (error) {
     return { language: 'english', translation: input } as translationResponse;
   }
+};
+
+export const summarizeAssetProps = (chain: CogniteBaseChain, asset: Asset) => {
+  const { name, description, metadata } = asset;
+  let metadataString;
+  try {
+    metadataString = Object.keys(metadata || {})
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      .map((key) =>
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        !['', 'NA', 'null'].includes(metadata![key])
+          ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            '' + key + ': ' + metadata![key]
+          : ''
+      )
+      .join(' \n');
+  } catch (error) {
+    metadataString = '';
+  }
+  console.log(metadataString);
+
+  return (
+    'Name: ' +
+    name +
+    '\n' +
+    'Description: ' +
+    description +
+    '\n' +
+    'Metadata: ' +
+    metadataString
+  );
+};
+
+export const runInfieldStartUp = async (
+  externalAssetId: string,
+  sdk: CogniteClient,
+  chain: CogniteBaseChain
+) => {
+  sendToCopilotEvent('LOADING_STATUS', {
+    status: 'Setting up...',
+  });
+  externalAssetIdGlobal = externalAssetId;
+  const asset = (await retrieveAsset(externalAssetId, sdk))[0];
+  assetDescriptionGlobal = summarizeAssetProps(chain, asset);
 };
