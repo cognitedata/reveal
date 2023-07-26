fbbd3d055f20cb90ff59fed3183e4108ec53fb6d
Stefan Maretic <stefan.maretic@cognite.com>
2023-07-26 15:17:48 +0200

    Remove unused improt

--- apps/charts/src/App.tsx
+++ apps/charts/src/App.tsx
@@ -25,7 +25,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 
 import sdk, { getFlow } from '@cognite/cdf-sdk-singleton';
-import { getProject, isUsingUnifiedSignin } from '@cognite/cdf-utilities';
+import { getProject } from '@cognite/cdf-utilities';
 import { ToastContainer } from '@cognite/cogs.js';
 import { FlagProvider } from '@cognite/react-feature-flags';
 
