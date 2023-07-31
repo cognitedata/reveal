620f5082251304387796ed7beed1d2d0561059fb
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2023-07-31 12:10:24 +0200

    fix: define global in vite config (#2552)
    * fix: define global in vite config
    
    * fix: set window.global in main.tsx
    
    * fix: use correct secret key for google cloud authentication
    
    ---------
    
    Co-authored-by: Shubham Singh <shubham88singh@gmail.com>
    Co-authored-by: anjalirai2626 <anjali.rai@cognite.com>

--- apps/business-shell/src/main.tsx
+++ apps/business-shell/src/main.tsx
@@ -3,5 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { AppWrapper } from './AppWrapper';
 
+window.global ||= window;
+
 const root = createRoot(document.getElementById('root')!);
 root.render(<AppWrapper />);
