1ceeb6b016f4f5652f1ab9047bbaaf074acc5c89
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2023-07-31 16:33:00 +0200

    feat: Info card overlay (#3521)
    * feat: initial info card overlay
    
    * feat: Support multiple ViewerAnchors
    
    ---------
    
    Co-authored-by: Christopher J. Tannum <christopher.tannum@cognite.com>

---
+++ react-components/src/components/ViewerAnchor/ViewerAnchor.tsx
@@ -0,0 +1,64 @@
+/*!
+ * Copyright 2023 Cognite AS
+ */
+
+import { useEffect, useRef, type ReactElement, type RefObject } from 'react';
+import { type Vector3 } from 'three';
+
+import { useReveal } from '../RevealContainer/RevealContext';
+
+import { HtmlOverlayTool } from '@cognite/reveal/tools';
+import { useAuxillaryDivContext } from './AuxillaryDivProvider';
+
+export type ViewerAnchorElementMapping = {
+  ref: RefObject<HTMLElement>;
+  position: Vector3;
+};
+
+export type ViewerAnchorProps = {
+  position: Vector3;
+  children: ReactElement;
+  uniqueKey: string;
+};
+
+export const ViewerAnchor = ({
+  position,
+  children,
+  uniqueKey
+}: ViewerAnchorProps): ReactElement => {
+  const viewer = useReveal();
+
+  const htmlTool = useRef<HtmlOverlayTool>(new HtmlOverlayTool(viewer));
+
+  const auxContext = useAuxillaryDivContext();
+
+  const htmlRef = useRef<HTMLDivElement>(null);
+  const element = (
+    <div key={uniqueKey} ref={htmlRef} style={{ position: 'absolute' }}>
+      {children}
+    </div>
+  );
+
+  useEffect(() => {
+    auxContext.addElement(element);
+    return () => {
+      auxContext.removeElement(element);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (htmlRef.current === null) {
+      return;
+    }
+
+    const elementRef = htmlRef.current;
+
+    htmlTool.current.add(elementRef, position);
+
+    return () => {
+      htmlTool.current.remove(elementRef);
+    };
+  }, [auxContext, children, htmlRef.current]);
+
+  return <></>;
+};
