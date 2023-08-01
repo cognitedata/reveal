664bdb7daac4ac636223f62357adc26ee961630d
Christopher Tannum <christopher.tannum@cognite.com>
2023-08-01 09:55:58 +0200

    refactor(react-components): simplify viewer anchor

--- react-components/src/components/ViewerAnchor/ViewerAnchor.tsx
+++ react-components/src/components/ViewerAnchor/ViewerAnchor.tsx
@@ -2,13 +2,10 @@
  * Copyright 2023 Cognite AS
  */
 
-import { useEffect, useRef, type ReactElement, type RefObject } from 'react';
-import { type Vector3 } from 'three';
+import { useEffect, useRef, type ReactElement, type RefObject, useState } from 'react';
+import { Vector2, type Vector3 } from 'three';
 
 import { useReveal } from '../RevealContainer/RevealContext';
-
-import { HtmlOverlayTool } from '@cognite/reveal/tools';
-import { useAuxillaryDivContext } from './AuxillaryDivProvider';
 
 export type ViewerAnchorElementMapping = {
   ref: RefObject<HTMLElement>;
@@ -27,38 +24,34 @@ export const ViewerAnchor = ({
   uniqueKey
 }: ViewerAnchorProps): ReactElement => {
   const viewer = useReveal();
+  const [vec, setVec] = useState(new Vector2());
 
-  const htmlTool = useRef<HtmlOverlayTool>(new HtmlOverlayTool(viewer));
+  const sceneRendered = (): void => {
+    const screenSpacePosition = viewer.worldToScreen(position);
+    if (screenSpacePosition !== null) {
+      setVec(screenSpacePosition);
+    }
+  };
 
-  const auxContext = useAuxillaryDivContext();
+  useEffect(() => {
+    viewer.on('sceneRendered', sceneRendered);
+    return () => {
+      viewer.off('sceneRendered', sceneRendered);
+    };
+  }, [sceneRendered]);
 
   const htmlRef = useRef<HTMLDivElement>(null);
-  const element = (
-    <div key={uniqueKey} ref={htmlRef} style={{ position: 'absolute' }}>
+  return (
+    <div
+      key={uniqueKey}
+      ref={htmlRef}
+      style={{
+        position: 'absolute',
+        left: '0px',
+        top: '0px',
+        transform: `translateX(${vec.x}px) translateY(${vec.y}px)`
+      }}>
       {children}
     </div>
   );
-
-  useEffect(() => {
-    auxContext.addElement(element);
-    return () => {
-      auxContext.removeElement(element);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (htmlRef.current === null) {
-      return;
-    }
-
-    const elementRef = htmlRef.current;
-
-    htmlTool.current.add(elementRef, position);
-
-    return () => {
-      htmlTool.current.remove(elementRef);
-    };
-  }, [auxContext, children, htmlRef.current]);
-
-  return <></>;
 };
