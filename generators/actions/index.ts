2cfb0637c045ce49bc9dc8ab7a1033e4ab392c24
Luis Pereira <91738660+lpereiracgn@users.noreply.github.com>
2023-08-01 10:30:07 +0200

    Remove generators

--- generators/actions/index.ts
+++
@@ -1,6 +0,0 @@
-import addLibrary from './addLibrary';
-import copyMany from './copyMany';
-import disableIntercom from './disableIntercom';
-import disableExperimental from './disableExperimental';
-
-export { addLibrary, copyMany, disableIntercom, disableExperimental };
