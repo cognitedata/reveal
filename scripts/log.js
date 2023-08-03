1465b84b1880f705c8c524ea2d3fcb0e3024d154
Luis Pereira <91738660+lpereiracgn@users.noreply.github.com>
2023-08-03 12:44:42 +0200

    Remove config files

--- scripts/log.js
+++
@@ -1,8 +0,0 @@
-console.log('');
-console.log('##################################################');
-console.log(
-  `---> Build finished for: ${process.argv[2].replace('--banner=//', '')}`
-);
-console.log('##################################################');
-console.log('');
-console.log('');
