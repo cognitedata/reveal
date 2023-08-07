ba0494b8d99036d27d8af250196b6a65f7462f22
Håkon Flatval <hakon.flatval@cognite.com>
2023-08-07 13:11:47 +0200

    feat: useClickedNode hook

---
+++ react-components/src/higher-order-components/ClickedNode.tsx
@@ -0,0 +1,48 @@
+/*!
+ * Copyright 2023 Cognite AS
+ */
+
+import { PointerEventData } from '@cognite/reveal';
+import { ReactElement, createContext, useContext, useEffect, useState } from 'react';
+import { FdmAssetMappingsConfig, NodeDataResult, useReveal } from '..';
+import { queryMappedData } from '../components/Reveal3DResources/queryMappedData';
+import { useFdmSdk, useSDK } from '../components/RevealContainer/SDKProvider';
+
+
+type ClickedNodeProps = {
+  children?: ReactElement;
+  fdmConfig?: FdmAssetMappingsConfig;
+};
+
+const ClickedNodeContext = createContext<NodeDataResult | undefined | null>(undefined);
+
+export const ClickedNode = ({ fdmConfig, children }: ClickedNodeProps) => {
+  const viewer = useReveal();
+  const sdk = useSDK();
+  const fdmClient = useFdmSdk();
+
+  const [clickedNode, setClickedNode] = useState<NodeDataResult | null>(null);
+
+  useEffect(() => {
+
+    const callback = async (pointerEvent: PointerEventData) => {
+      const nodeData = await queryMappedData(viewer, sdk, fdmClient, pointerEvent, fdmConfig);
+      setClickedNode(nodeData ?? null);
+    };
+
+    viewer.on('click', (event: PointerEventData) => void callback(event));
+  }, [viewer]);
+
+
+  return <ClickedNodeContext.Provider value={clickedNode}>
+    {children}
+  </ClickedNodeContext.Provider>
+}
+
+export const useClickedNode = (): NodeDataResult | null => {
+  const nodeData = useContext(ClickedNodeContext);
+  if (nodeData === undefined) {
+    throw Error('useClickedNode must be used within a ClickedNode context');
+  }
+  return nodeData;
+}
