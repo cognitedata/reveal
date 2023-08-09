a5241044bb0b15d4baefd3caa02e225b26662fbc
abdullah <abdullah.ali@cognite.com>
2023-08-09 09:43:48 +0200

    Revert App.tsx

--- apps/simint/src/components/app/App.tsx
+++ apps/simint/src/components/app/App.tsx
@@ -33,9 +33,10 @@ export default function App() {
   const { data: user } = useUserInfo();
   const isInitialized = useSelector(selectIsInitialized);
   const project = useSelector(selectProject);
-  const useLocal = true;
-  //const BASE_URL = process.env.REACT_APP_LOCAL_SERVICE
-  const BASE_URL = useLocal ? `https://localhost:8810` : client.getBaseUrl();
+
+  const BASE_URL = process.env.REACT_APP_LOCAL_SERVICE
+    ? `https://localhost:8810`
+    : client.getBaseUrl();
 
   simconfigApiPropertiesSlice.actions.setProperties({
     baseUrl: BASE_URL,
