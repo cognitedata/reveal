82f1dc52cc5e5e9659701ac255f336e26472d0b9
Branko Dimitrijoski <40126033+bdimitrijoski@users.noreply.github.com>
2023-08-11 11:02:40 +0200

    hotfix: fixed dev scripts (#2685)
    Co-authored-by: Branko  Dimitrijoski <branko.dimitrijoski@cognitedata.com@Brankos-Cognite-MacBook-Pro.local>

--- apps/fusion-shell/tools/scripts/fusion-post-build.sh
+++ apps/fusion-shell/tools/scripts/fusion-post-build.sh
@@ -18,14 +18,19 @@ rm -rf $build_folder/assets/dependencies/@cognite/$VERSION_HASH;
 rm -rf $build_folder/assets/dependencies/@cognite/cdf-route-tracker;
 rm -rf $build_folder/assets/dependencies/@cognite/cdf-sdk-singleton;
 
-# # And re-create them, so that we can copy the new files
+# # # And re-create them, so that we can copy the new files
 mkdir -p $build_folder/assets/dependencies/@cognite/$VERSION_HASH;
 mkdir $build_folder/assets/dependencies/@cognite/$VERSION_HASH/cdf-route-tracker;
 mkdir $build_folder/assets/dependencies/@cognite/$VERSION_HASH/cdf-sdk-singleton;
 
-# Copy the build from sdk-signleton and route-tracker to the fusion-shell assets folder
+# # Copy the build from sdk-signleton and route-tracker to the fusion-shell assets folder
 cp -r  $packages_build_folder/cdf-sdk-singleton/* $build_folder/assets/dependencies/@cognite/$VERSION_HASH/cdf-sdk-singleton;
 cp -r  $packages_build_folder/cdf-route-tracker/* $build_folder/assets/dependencies/@cognite/$VERSION_HASH/cdf-route-tracker;
+
+if [ $configuration = "production" ]
+then
+  cp apps/fusion-shell/src/environments/unified-signin/production/index.html $build_folder
+fi
 
 # Generate Import Map Configs
 node ./apps/fusion-shell/tools/scripts/import-map-generator.js $configuration $build_mode $VERSION_HASH
