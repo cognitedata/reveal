0d71191c1673feb1d2604554d10e9939675e37f8
Shubham Singh <shubham88singh@gmail.com>
2023-08-14 15:00:20 +0200

    fix(Onboarding-Modal): Adding the mixpanel metrics (#2683)
    * Adding the mixpanel metrics
    
    * Fixing the onboarding modal
    
    * Reverting the  metrics changes
    
    * UPdating the visible props in the model and mounting logic
    
    ---------
    
    Co-authored-by: Shubham SIngh <shubham.singh@cognite.com>

--- libs/shared/user-onboarding-components/src/metrics/index.ts
+++ libs/shared/user-onboarding-components/src/metrics/index.ts
@@ -8,4 +8,13 @@ export enum ORIENTATION_ACTIONS {
 
 export const ORIENTATION_EVENT = 'Orientation';
 
+export enum ONBOARDING_MODAL_ACTIONS {
+  STARTED = 'Started',
+  CLOSED = 'Closed',
+  CANCELLED = 'Cancelled',
+  SUCCESS = 'Success',
+}
+
+export const ONBOARDING_MODAL_EVENT = 'Onboarding modal';
+
 export type OrientationEvent = typeof ORIENTATION_EVENT;
