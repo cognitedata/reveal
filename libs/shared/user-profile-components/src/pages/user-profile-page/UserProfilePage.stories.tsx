5869b10a68b9b223b2cd4e83bf396a1d35d4a5a7
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2023-08-16 15:45:08 +0200

    chore: add story for onBackBtnClick prop

--- libs/shared/user-profile-components/src/pages/user-profile-page/UserProfilePage.stories.tsx
+++ libs/shared/user-profile-components/src/pages/user-profile-page/UserProfilePage.stories.tsx
@@ -1,25 +1,48 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Meta, StoryFn } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { UserProfilePage } from './UserProfilePage';
 
+type Story = StoryObj<typeof UserProfilePage>;
+
 export default {
   component: UserProfilePage,
+  args: {
+    userInfo: { name: 'John', email: 'john@example.com' },
+    selectedLanguage: { code: 'en', label: 'English | en' },
+    supportedLanguages: [{ code: 'en', label: 'English | en' }],
+    onLanguageChange: (language) => console.log(language),
+  },
 } as Meta<typeof UserProfilePage>;
 
-export const Default: StoryFn<typeof UserProfilePage> = (args) => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<UserProfilePage {...args} />} />
-    </Routes>
-  </BrowserRouter>
-);
+export const Default: Story = {
+  render: (args) => (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<UserProfilePage {...args} />} />
+      </Routes>
+    </BrowserRouter>
+  ),
+};
 
-Default.args = {
-  userInfo: { name: 'John', email: 'john@example.com' },
-  selectedLanguage: { code: 'en', label: 'English | en' },
-  supportedLanguages: [{ code: 'en', label: 'English | en' }],
-  onLanguageChange: (language) => console.log(language),
+export const OnBackBtnClick: Story = {
+  name: 'Controlling back button click',
+  render: (args) => (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <UserProfilePage
+              onBackBtnClick={action('onBackBtnClick')}
+              {...args}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  ),
 };
