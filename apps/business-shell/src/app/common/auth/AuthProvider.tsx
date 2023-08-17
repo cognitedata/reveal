33872dcbf4604a5620d5ab75b01c3bdcf3661692
Deep <70804363+deep-cognite@users.noreply.github.com>
2023-08-17 08:48:33 +0200

    fix(business-shell): revert latest auth wrapper changes (#2741)
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/business-shell/src/app/common/auth/AuthProvider.tsx
+++ apps/business-shell/src/app/common/auth/AuthProvider.tsx
@@ -1,6 +1,20 @@
 import type { FC, PropsWithChildren } from 'react';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
-import * as AuthPackage from '@cognite/apm-auth-container';
+import * as Sentry from '@sentry/browser';
+
+import {
+  AuthProvider as UnifiedSigninAuthProvider,
+  useAuth,
+} from '@cognite/auth-react';
+import { Loader } from '@cognite/cogs.js';
 import { CogniteClient } from '@cognite/sdk';
 
 import { AuthStateAuthenticated, AuthStateUser } from './types';
@@ -12,6 +26,8 @@ export type AuthContextType = {
   loginWithCustomerId: (customerId: string) => void;
 };
 
+const AuthContext = createContext<AuthContextType | null>(null);
+
 type Props = {
   appId?: string;
   mock?: {
@@ -19,17 +35,107 @@ type Props = {
     user?: AuthStateUser;
   };
 };
+const InnerAuthProvider: FC<PropsWithChildren<Props>> = ({
+  children,
+  mock,
+  appId = 'fdx',
+}) => {
+  const { getUser, project, getToken, logout, cluster } = useAuth();
+
+  const [isFetchingAuthState, setIsFetchingAuthState] =
+    useState<boolean>(false);
+  const [newAuthState, setAuthState] = useState<
+    AuthStateAuthenticated & { client: CogniteClient }
+  >();
+
+  const client = useMemo(
+    () =>
+      mock?.client ||
+      new CogniteClient({
+        appId,
+        baseUrl: `https://${cluster}`,
+        project,
+        getToken: async () =>
+          getToken().then((token: string) => {
+            return token || 'nothing';
+          }),
+      }),
+    [appId, getToken, mock?.client, project, cluster]
+  );
+
+  const fetchAuthState = useCallback(async (): Promise<
+    AuthStateAuthenticated & { client: CogniteClient }
+  > => {
+    const user = await getUser();
+    if (!user?.email || !user?.id) throw new Error('Invalid user');
+
+    if (!user.preferred_username || !user.name) {
+      throw new Error('User does not have a name');
+    }
+
+    Sentry.setUser({
+      id: user.id,
+      username: user.preferred_username || user.name,
+      email: user.email,
+    });
+
+    return {
+      status: 'AUTHENTICATED',
+      user: mock?.user || {
+        email: user.email,
+        id: user.id,
+        name: user.preferred_username || user.name,
+        idToken: '',
+      },
+      client,
+    };
+  }, [client, getUser, mock]);
+
+  useEffect(() => {
+    if (newAuthState) return;
+    if (isFetchingAuthState) return;
+    setIsFetchingAuthState(true);
+    fetchAuthState().then(setAuthState);
+  }, [fetchAuthState, getToken, isFetchingAuthState, newAuthState]);
+
+  const authContext:
+    | (Omit<AuthContextType, 'authState' | 'login'> & {
+        authState: AuthStateAuthenticated;
+      })
+    | null = useMemo(
+    () =>
+      newAuthState
+        ? {
+            client: newAuthState.client,
+            authState: newAuthState,
+            logout,
+            loginWithCustomerId: () => null,
+          }
+        : null,
+    [newAuthState, logout]
+  );
+
+  if (authContext == null) {
+    return null;
+  }
+
+  return (
+    <AuthContext.Provider value={authContext as AuthContextType}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export const AuthProvider: FC<PropsWithChildren<Props>> = (props) => {
   return (
-    <AuthPackage.AuthProvider useProductionAadApp={false} appName="FDX">
-      {props.children}
-    </AuthPackage.AuthProvider>
+    <UnifiedSigninAuthProvider loader={<Loader infoText="Loading" />}>
+      <InnerAuthProvider {...props}>{props.children}</InnerAuthProvider>
+    </UnifiedSigninAuthProvider>
   );
 };
 
 export const useAuthContext = (): AuthContextType => {
-  const context = AuthPackage.useAuthContext();
+  const context = useContext(AuthContext);
   if (context) {
     // we never even render context if it is not for already authenticated user, so we do manual type narrowing here
     return context;
