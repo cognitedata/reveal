1747e92dc55343cc9fce831a652c2e4d4768f4ee
Marissa Pang <marissa.pang@cognite.com>
2023-08-24 12:00:10 -0500

    testing

--- apps/flows/src/index.tsx
+++ apps/flows/src/index.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-import ReactDOM from 'react-dom/client';
 import './set-public-path';
 import singleSpaReact from 'single-spa-react';
 
 import App from './App';
 
 const lifecycles = singleSpaReact({
-  React,
-  ReactDOM: ReactDOM as any,
+  React: React as any,
+  ReactDOM,
   rootComponent: App,
   // @ts-ignore
   errorBoundary() {
     // eslint-disable-line
     // Customize the root error boundary for your microfrontend here.
-    return <span>An error occured in your app</span>;
+    return <span>An error occurred in your app</span>;
   },
-  renderType: 'createRoot',
 });
 
 export const { bootstrap, mount, unmount } = lifecycles;
