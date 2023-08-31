793f93d2485735a3624fb807d58f0bd3188c0963
Poyan Nabati <poyan.nabati@cognite.com>
2023-08-31 12:05:49 +0200

    fix(charts): fix deletion of timeseries rows from fileviewpage (#2922)
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/charts/src/app/components/SourceTable/SourceTable.tsx
+++ apps/charts/src/app/components/SourceTable/SourceTable.tsx
@@ -115,6 +115,7 @@ const SourceTable = ({
                       ) as ChartTimeSeries[]
                     }
                     summaries={summaries}
+                    onRemoveSourceClick={onRemoveSourceClick}
                   />
                 ) : (
                   <>
