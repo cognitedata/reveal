5615cb3bf0f1ec9ca81203442903f1d8cc7d4cac
Christopher Tannum <christopher.tannum@cognite.com>
2023-09-01 13:31:48 +0200

    feat: add HOC for adding camerastate url param

---
+++ react-components/src/higher-order-components/withCameraStateUrlParam.tsx
@@ -0,0 +1,63 @@
+/*!
+ * Copyright 2023 Cognite AS
+ */
+
+import { useEffect, type ReactElement, type FunctionComponent } from 'react';
+import { useReveal } from '..';
+import { Vector3 } from 'three';
+
+export function withCameraStateUrlParam<T extends object>(
+  Component: FunctionComponent<T>
+): FunctionComponent<T> {
+  return function CameraStateUrlParam(props: T): ReactElement {
+    const reveal = useReveal();
+
+    const setUrlParamOnCameraStop = (): void => {
+      const url = new URL(window.location.toString());
+      const { position, target } = reveal.cameraManager.getCameraState();
+      url.searchParams.set('cameraPosition', `[${position.x},${position.y},${position.z}]`);
+      url.searchParams.set('cameraTarget', `[${target.x},${target.y},${target.z}]`);
+      window.history.pushState({}, '', url);
+    };
+
+    useEffect(() => {
+      reveal.cameraManager.on('cameraStop', setUrlParamOnCameraStop);
+      return () => {
+        reveal.cameraManager.off('cameraStop', setUrlParamOnCameraStop);
+      };
+    }, []);
+
+    return <Component {...props} />;
+  };
+}
+
+export function useSetCameraStateFromUrlParam(): () => void {
+  const reveal = useReveal();
+
+  return () => {
+    const url = new URL(window.location.toString());
+    const position = url.searchParams.get('cameraPosition');
+    const target = url.searchParams.get('cameraTarget');
+
+    if (position === null || target === null) {
+      return;
+    }
+
+    const parsedPosition = getParsedVector(position);
+    const parsedTarget = getParsedVector(target);
+
+    if (parsedPosition === undefined || parsedTarget === undefined) {
+      return;
+    }
+
+    reveal.cameraManager.setCameraState({ position: parsedPosition, target: parsedTarget });
+  };
+
+  function getParsedVector(s: string): Vector3 | undefined {
+    try {
+      return new Vector3().fromArray(JSON.parse(s));
+    } catch (e) {
+      return undefined;
+    }
+  }
+}
