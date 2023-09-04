c0ce802a23339b8012ee29f4cdea4f5f8ad0540f
dewmihimanshi <102962056+dewmihimanshi@users.noreply.github.com>
2023-09-04 12:17:35 +0530

    feat(data-explorer): events e2e tests (#2987)
    * feat: events sorting
    
    * feat: event search and detail view
    
    ---------
    
    Co-authored-by: Sennen De Almeida <84778551+sennen-cognite@users.noreply.github.com>

--- apps/data-exploration-e2e/src/support/commands/table.commands.ts
+++ apps/data-exploration-e2e/src/support/commands/table.commands.ts
@@ -1,3 +1,7 @@
+import { map } from 'lodash';
+
+import { sortAscending, sortDescending } from '../utils/sort';
+
 const getTableById = (id: string) => {
   return cy.findAllByTestId(id);
 };
@@ -6,10 +10,60 @@ const tableSholudBeVisible = (id: string) => {
   cy.getTableById(id).should('be.visible');
 };
 
+const clickSortColoumn = (table: JQuery<HTMLElement>, columnName: string) => {
+  cy.wrap(table)
+    .findAllByTestId('table-head')
+    .contains(columnName)
+    .parent()
+    .find('button')
+    .should('be.visible')
+    .click();
+};
+
+const getColomnValues = (table: JQuery<HTMLElement>, coloumnId: string) => {
+  return cy
+    .wrap(table)
+    .findAllByTestId('table-body')
+    .findAllByTestId(coloumnId)
+    .then((cells) => {
+      return map(cells, 'innerText');
+    });
+};
+
+const shouldBeSortedAscending = (values: string[]) => {
+  const sortedValues = sortAscending(values);
+  expect(sortedValues).to.deep.equal(values);
+};
+
+const shouldBeSortedDescending = (values: string[]) => {
+  const sortedValues = sortDescending(values);
+  expect(sortedValues).to.deep.equal(values);
+};
+
 Cypress.Commands.add('getTableById', getTableById);
 Cypress.Commands.add('tableSholudBeVisible', tableSholudBeVisible);
+Cypress.Commands.add(
+  'clickSortColoumn',
+  { prevSubject: true },
+  clickSortColoumn
+);
+Cypress.Commands.add('getColomnValues', { prevSubject: true }, getColomnValues);
+Cypress.Commands.add(
+  'shouldBeSortedAscending',
+  { prevSubject: true },
+  shouldBeSortedAscending
+);
+Cypress.Commands.add(
+  'shouldBeSortedDescending',
+  { prevSubject: true },
+  shouldBeSortedDescending
+);
 
 export interface TableCommands {
   getTableById: (id: string) => Cypress.Chainable<JQuery<HTMLElement>>;
   tableSholudBeVisible: (id: string) => void;
+  clickSortColoumn: (coloumnName: string) => void;
+  getColomnValues: (coloumnid: string) => Cypress.Chainable<string[]>;
+  shouldBeSortedAscending: () => void;
+  shouldBeSortedDescending: () => void;
 }
