70a310ef62cbce54dfcf61aaac1ae56aeb73cb84
Fredrik Anfinsen <fredrik.anfinsen@gmail.com>
2023-09-05 23:30:08 +0200

    bulk github comments from CI (#3030)
    * bulk github comments from CI
    
    * make github actions deps available
    
    * add bulk comments for apps and shells
    
    * add missing function argument
    
    * remove duplicate github comment action
    
    * fix incorrect input var
    
    * make comments for compact
    
    * refactor code

--- scripts/github-actions/get-pr-comment/index.js
+++ scripts/github-actions/get-pr-comment/index.js
@@ -1,28 +1,86 @@
-const { setFailed, setOutput, getInput, info } = require('@actions/core');
-var fetch = require('node-fetch');
+const {
+  setFailed,
+  setOutput,
+  getInput,
+  info,
+  getBooleanInput,
+} = require('@actions/core');
+const { readFileSync } = require('node:fs');
+const { resolve } = require('node:path');
+
+function getFusionSubAppPreviewPackageName(app) {
+  try {
+    const content = readFileSync(
+      resolve(__dirname, '../../../apps', app, 'project.json'),
+      'utf-8'
+    );
+    const projectConfig = JSON.parse(content.toString('utf-8'));
+    if (projectConfig.pipeline) {
+      return projectConfig.pipeline.previewPackageName || null;
+    }
+    return null;
+  } catch {
+    return null;
+  }
+}
+
+function getAffectedSorted() {
+  const affected = JSON.parse(getInput('affected'));
+  return affected.sort();
+}
+
+function generateBadge(name, url, label, colorAsHex) {
+  return `[![AppBadge](https://img.shields.io/static/v1?label=${encodeURIComponent(
+    label
+  )}&message=${encodeURIComponent(name)}&labelColor=${colorAsHex})](${url})`;
+}
+
+const PREVIEW_BASE_URL = 'fusion-preview.preview.cogniteapp.com';
+
+function createAppComment(prNumber) {
+  const affected = getAffectedSorted();
+  const apps = affected.map((app) => {
+    const previewPackageName = getFusionSubAppPreviewPackageName(app);
+    const previewUrl = `https://${app}-${prNumber}.${PREVIEW_BASE_URL}`;
+    const url = !!previewPackageName
+      ? `https://fusion-pr-preview.cogniteapp.com/?externalOverride=${previewPackageName}&overrideUrl=${previewUrl}/index.js`
+      : previewUrl;
+    return generateBadge(app, url, 'Application', '2DB64C');
+  });
+  return ['## Build previews:', apps.join(' ')].join('\n');
+}
+
+function createShellAppComment(prNumber) {
+  const affected = getAffectedSorted();
+  const apps = affected.map((app) => {
+    const url = `https://${app}-${prNumber}.${PREVIEW_BASE_URL}`;
+    return generateBadge(app, url, 'Application', '2DB64C');
+  });
+  return ['## Build previews for shells:', apps.join(' ')].join('\n');
+}
+
+function createStorybookComment(prNumber) {
+  const affected = getAffectedSorted();
+  const storybooks = affected.map((app) => {
+    const url = `https://storybook-${app}-${prNumber}.${PREVIEW_BASE_URL}`;
+    return generateBadge(app, url, 'Storybook', 'FF4586');
+  });
+  return ['## Storybook previews:', storybooks.join(' ')].join('\n');
+}
 
 const run = async () => {
   try {
-    const project = getInput('project');
     const prNumber = getInput('prNumber');
-    const previewPackageName = getInput('previewPackageName');
+    const shell = getBooleanInput('shell');
     const storybook = getInput('storybook');
 
     let message = '';
     if (storybook) {
-      message = `Storybook preview for **${project}** available at https://storybook-${project}-${prNumber}.fusion-preview.preview.cogniteapp.com
-        <br><br>![AppBadge](https://img.shields.io/static/v1?label=Storybook&message=${project}&labelColor=FF4586)`;
-    } else if (previewPackageName) {
-      if (project === 'fusion-shell') {
-        message = `Build preview for **${project}** available at https://${project}-${prNumber}.fusion-preview.preview.cogniteapp.com
-          <br><br>![AppBadge](https://img.shields.io/static/v1?label=Application&message=${project}&labelColor=2DB64C)`;
-      } else {
-        message = `Build preview for **${project}** available at https://fusion-pr-preview.cogniteapp.com/?externalOverride=${previewPackageName}&overrideUrl=https://${project}-${prNumber}.fusion-preview.preview.cogniteapp.com/index.js
-        <br><br>![AppBadge](https://img.shields.io/static/v1?label=Application&message=${project}&labelColor=2DB64C)`;
-      }
+      message = createStorybookComment(prNumber);
+    } else if (shell) {
+      message = createShellAppComment(prNumber);
     } else {
-      message = `Build preview for **${project}** available at https://${project}-${prNumber}.fusion-preview.preview.cogniteapp.com
-        <br><br>![AppBadge](https://img.shields.io/static/v1?label=Application&message=${project}&labelColor=2DB64C)`;
+      message = createAppComment(prNumber);
     }
 
     info(`message: \n ${message}`);
