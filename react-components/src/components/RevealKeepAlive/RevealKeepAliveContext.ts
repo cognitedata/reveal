5a8900ed80ac3a1cba996d8e0cdd12a288db0f04
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2023-09-13 11:37:59 +0200

    feat: include node cache in keep-alive context (#3693)
    * feat: include node cache in keep-alive context

--- react-components/src/components/RevealKeepAlive/RevealKeepAliveContext.ts
+++ react-components/src/components/RevealKeepAlive/RevealKeepAliveContext.ts
@@ -3,10 +3,12 @@
  */
 import { type Cognite3DViewer } from '@cognite/reveal';
 import { type MutableRefObject, createContext, useContext } from 'react';
+import { type FdmNodeCache } from '../NodeCacheProvider/FdmNodeCache';
 
 export type RevealKeepAliveData = {
   viewerRef: MutableRefObject<Cognite3DViewer | undefined>;
   isRevealContainerMountedRef: MutableRefObject<boolean>;
+  fdmNodeCache: MutableRefObject<FdmNodeCache | undefined>;
 };
 
 export const RevealKeepAliveContext = createContext<RevealKeepAliveData | undefined>(undefined);
