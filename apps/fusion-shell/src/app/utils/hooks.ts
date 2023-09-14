5a04f6f4b8b50feb63afbf5883a9e920e393e47a
JiMin <borajimin@gmail.com>
2023-09-14 17:47:35 +0900

    chore: remove feature flag for functions ui (#3145)
    Co-authored-by: borajimin <jimin.ha@cognite.com>

--- apps/fusion-shell/src/app/utils/hooks.ts
+++ apps/fusion-shell/src/app/utils/hooks.ts
@@ -109,11 +109,6 @@ export const useExperimentalFeatures = () => {
     forceRerender: true,
   });
 
-  const { isEnabled: isFunctionsDisabled } = useFlag('DISABLE_FUNCTION_UI', {
-    fallback: false,
-    forceRerender: true,
-  });
-
   const experimentalFeatures: Record<string, boolean> = {
     jupyter: isDSHubEnabled, // This is the old DSHub that we should remove
     notebook: isJupyterNotebooksEnabled,
@@ -130,7 +125,6 @@ export const useExperimentalFeatures = () => {
     'coding-conventions': isCodingConventionsEnabled,
     'industry-canvas': isIndustryCanvasEnabled,
     infield: !isInfieldDisabled,
-    functions: !isFunctionsDisabled,
     // // Re-enable this if you need to hide Charts behind a feature flag.
     // charts: isChartsUIEnabled,
   };
