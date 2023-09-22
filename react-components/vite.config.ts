81df4f588c93c5b01aa5d6aa9cd316559487463e
Christopher Tannum <christopher.tannum@cognite.com>
2023-09-22 16:52:55 +0200

    chore: add yalc push watch command

--- react-components/vite.config.ts
+++ react-components/vite.config.ts
@@ -1,25 +1,53 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { type PluginOption, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { externalizeDeps } from 'vite-plugin-externalize-deps';
+import { exec } from 'node:child_process';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({ include: ['src'] }),
-    externalizeDeps({
-      devDeps: true
-    })
-  ],
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'Reveal react components',
-      // the proper extensions will be added
-      fileName: 'index',
-      formats: ['es']
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(),
+      dts({ include: ['src'] }),
+      externalizeDeps({
+        devDeps: true
+      }),
+      yalcPush(command)
+    ],
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'Reveal react components',
+        // the proper extensions will be added
+        fileName: 'index',
+        formats: ['es']
+      }
     }
-  }
+  };
 });
+
+function yalcPush(command: 'build' | 'serve'): PluginOption {
+  if (process.env.YALC !== 'true' && command === 'build') {
+    return;
+  }
+  return {
+    name: 'yalc-push',
+    closeBundle: async () => {
+      await new Promise<void>((resolve, reject) => {
+        const process = exec('yalc push');
+        process.stdout
+          ?.on('data', (data) => {
+            console.log(data);
+          })
+          .on('error', (err) => {
+            reject(err);
+          })
+          .on('close', () => {
+            resolve();
+          });
+      });
+    }
+  };
+}
