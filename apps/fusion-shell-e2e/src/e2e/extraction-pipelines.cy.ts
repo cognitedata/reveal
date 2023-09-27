a0bdf793aa0d626cd9e32a76f6744cde8f6ae219
Brian Kuzma <brian.kuzma@cognite.com>
2023-09-27 03:01:57 +0200

    skip cache for fusion-shell-e2e (#3290)
    * skip cache for fusion-shell-e2e
    
    * fix extraction-pipelines test
    
    ---------
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/fusion-shell-e2e/src/e2e/extraction-pipelines.cy.ts
+++ apps/fusion-shell-e2e/src/e2e/extraction-pipelines.cy.ts
@@ -4,7 +4,7 @@ describe('Extraction pipelines page', () => {
   });
 
   it('Should open Extraction pipelines page', () => {
-    cy.get('h3.cogs-title-3', { timeout: 20000 }).contains(
+    cy.get('h3.cogs-heading-3', { timeout: 20000 }).contains(
       /Extraction pipelines/
     );
   });
