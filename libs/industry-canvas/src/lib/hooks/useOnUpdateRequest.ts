13cacdfe886153d2362789ea149e1cc651d914b0
Poyan Nabati <poyan.nabati@cognite.com>
2023-10-02 16:08:22 +0200

    fix(industrial-canvas): fix reresolving pasted containers to fix cache issues (#3342)
    * fix reresolving pasted containers to fix cache issues
    
    * bump ufv
    
    * remove old case
    
    * clean up deps
    
    * clean up
    
    * refactor selectedContainer to be selectedContainers
    
    * fix type issue
    
    * bump ufv
    
    * chore: bump UFV again
    
    ---------
    
    Co-authored-by: Marvin Reza <marvinreza@Marvins-MBP.home>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- libs/industry-canvas/src/lib/hooks/useOnUpdateRequest.ts
+++ libs/industry-canvas/src/lib/hooks/useOnUpdateRequest.ts
@@ -1,15 +1,25 @@
 import { useCallback } from 'react';
 
+import { FileInfo } from '@cognite/sdk';
+import { CogniteClient } from '@cognite/sdk/dist/src';
+import { useSDK } from '@cognite/sdk-provider';
 import {
+  ContainerConfig,
+  ContainerType,
   UnifiedViewer,
   UnifiedViewerEventListenerMap,
   UnifiedViewerEventType,
+  UpdateRequestSource,
+  getContainerConfigFromFileInfo,
 } from '@cognite/unified-file-viewer';
 
 import { RuleType } from '../components/ContextualTooltips/AssetTooltip/types';
+import containerConfigToContainerReference from '../containerConfigToContainerReference';
 import { shamefulOnUpdateRequest } from '../state/useIndustrialCanvasStore';
-import { CanvasNode } from '../types';
+import { CanvasNode, isIndustryCanvasContainerConfig } from '../types';
 import useMetrics from '../utils/tracking/useMetrics';
+
+import resolveContainerConfig from './utils/resolveContainerConfig';
 
 export type InteractionState = {
   hoverId: string | undefined;
@@ -57,17 +67,58 @@ export const getNextUpdatedNodes = (
   ];
 };
 
+// The reason we reresolve container configs is that they might originate from
+// another tab. For documents and images particularly that might lead to us
+// trying to reuse urls that have expired and do not correspond to any
+// cache entries in the local tab.
+const reresolveContainerConfigsFromClipboard = (
+  containerConfigs: ContainerConfig[],
+  sdk: CogniteClient
+): Promise<ContainerConfig[]> => {
+  return Promise.all(
+    containerConfigs
+      .filter(isIndustryCanvasContainerConfig)
+      .map((containerConfig) =>
+        resolveContainerConfig(
+          sdk,
+          containerConfigToContainerReference(containerConfig)
+        )
+      )
+  );
+};
+
+const preprocessContainerUpdates = async (
+  {
+    source,
+    containers,
+  }: {
+    source?: UpdateRequestSource;
+    containers: ContainerConfig[];
+  },
+  sdk: CogniteClient
+): Promise<ContainerConfig[]> => {
+  if (source === undefined) {
+    return containers;
+  }
+
+  return reresolveContainerConfigsFromClipboard(containers, sdk);
+};
+
 const useOnUpdateRequest = ({
   unifiedViewer,
 }: {
   unifiedViewer: UnifiedViewer | null;
 }): UpdateHandlerFn => {
   const trackUsage = useMetrics();
+  const sdk = useSDK();
 
   return useCallback(
-    ({ containers, annotations }) => {
+    async ({ source, containers, annotations }) => {
       shamefulOnUpdateRequest({
-        containers,
+        containers: await preprocessContainerUpdates(
+          { source, containers },
+          sdk
+        ),
         annotations,
         unifiedViewer,
         trackUsage,
