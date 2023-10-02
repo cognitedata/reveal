b5478f2803ff8c96405df8f9ec35978d955834f0
Håkon Flatval <hakon.flatval@cognite.com>
2023-10-02 09:28:06 +0200

    test: add test

--- viewer/packages/360-images/unit-tests/Image360Entity.test.ts
+++ viewer/packages/360-images/unit-tests/Image360Entity.test.ts
@@ -12,39 +12,79 @@ import { DeviceDescriptor, SceneHandler } from '@reveal/utilities';
 import { Historical360ImageSet } from '@reveal/data-providers/src/types';
 import { Image360AnnotationFilter } from '../src/annotation/Image360AnnotationFilter';
 
+function createMockImage360(options?: { customTranslation?: THREE.Matrix4 }) {
+  const image360Descriptor: Historical360ImageSet = {
+    id: '0',
+    label: 'testEntity',
+    collectionId: '0',
+    collectionLabel: 'test_collection',
+    transform: new THREE.Matrix4(),
+    imageRevisions: [
+      {
+        timestamp: undefined,
+        faceDescriptors: []
+      }
+    ]
+  };
+
+  const mockSceneHandler = new Mock<SceneHandler>().setup(p => p.addCustomObject(It.IsAny())).returns();
+  const mock360ImageProvider = new Mock<Image360Provider<any>>();
+  const mock360ImageIcon = new Overlay3DIcon({ position: new THREE.Vector3(),
+                                               minPixelSize: 10,
+                                               maxPixelSize: 10,
+                                               iconRadius: 10 }, {});// new Mock<Overlay3DIcon>().object();
+
+  const testTranslation = options?.customTranslation ?? new THREE.Matrix4();
+  const desktopDevice: DeviceDescriptor = { deviceType: 'desktop' };
+
+  return new Image360Entity(
+    image360Descriptor,
+    mockSceneHandler.object(),
+    mock360ImageProvider.object(),
+    new Image360AnnotationFilter({}),
+    testTranslation,
+    mock360ImageIcon,
+    desktopDevice
+  );
+}
+
 describe(Image360Entity.name, () => {
   test('transformation should be respected', () => {
-    const image360Descriptor: Historical360ImageSet = {
-      id: '0',
-      label: 'testEntity',
-      collectionId: '0',
-      collectionLabel: 'test_collection',
-      transform: new THREE.Matrix4(),
-      imageRevisions: [
-        {
-          timestamp: undefined,
-          faceDescriptors: []
-        }
-      ]
-    };
-
-    const mockSceneHandler = new Mock<SceneHandler>().setup(p => p.addCustomObject(It.IsAny())).returns();
-    const mock360ImageProvider = new Mock<Image360Provider<any>>();
-    const mock360ImageIcon = new Mock<Overlay3DIcon>().object();
-
     const testTranslation = new THREE.Matrix4().makeTranslation(4, 5, 6);
-    const desktopDevice: DeviceDescriptor = { deviceType: 'desktop' };
-
-    const entity = new Image360Entity(
-      image360Descriptor,
-      mockSceneHandler.object(),
-      mock360ImageProvider.object(),
-      new Image360AnnotationFilter({}),
-      testTranslation,
-      mock360ImageIcon,
-      desktopDevice
-    );
+    const entity = createMockImage360({ customTranslation: testTranslation });
 
     expect(entity.transform.equals(testTranslation)).toBeTrue();
+  });
+
+  test('set icon color is returned in getter', () => {
+    const entity = createMockImage360();
+
+    const { color: originalColor } = entity.getIconStyle();
+
+    expect(originalColor).toBe(undefined);
+
+    const testColor = new THREE.Color(0.2, 0.3, 0.4);
+    entity.setIconStyle({ color: testColor });
+
+    const { color: gottenColor } = entity.getIconStyle();
+
+    expect(gottenColor?.toArray()).toEqual(testColor.toArray());
+  });
+
+  test('setting undefined icon color resets image360 icon color', () => {
+    const entity = createMockImage360();
+
+    const testColor = new THREE.Color(0.2, 0.3, 0.4);
+    entity.setIconStyle({ color: testColor });
+
+    const { color: firstColor } = entity.getIconStyle();
+
+    expect(firstColor).not.toBe(undefined);
+
+    entity.setIconStyle({ color: undefined });
+
+    const { color: secondColor } = entity.getIconStyle();
+
+    expect(secondColor).toBe(undefined);
   });
 });
