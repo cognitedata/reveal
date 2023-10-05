420bf185b5ff860c803f1d1eda5273a31a95384b
Henrik Grønbech <henrik.gronbech@cognite.com>
2023-10-05 16:43:26 +0200

    chore: Strip down types.ts

--- apps/3d-management/src/components/ContextualizeThreeDViewer/types.ts
+++ apps/3d-management/src/components/ContextualizeThreeDViewer/types.ts
@@ -1,35 +1,5 @@
-import { DEFAULT_POINT_SIZE } from '@3d-management/pages/ContextualizeEditor/constants';
-
-import { PointColorType } from '@cognite/reveal';
-
-export type ThreeDPosition = {
-  x: number;
-  y: number;
-  z: number;
-};
-
-export type CubeAnnotation = {
-  position: ThreeDPosition;
-  size: ThreeDPosition;
-};
-
-export type VisualizationOptions = {
-  pointSize: number;
-  pointColor: PointColorType;
-};
-
-export const DEFAULT_VISUALIZATION_OPTIONS: VisualizationOptions = {
-  pointSize: DEFAULT_POINT_SIZE,
-  pointColor: PointColorType.Rgb,
-};
-
 export enum ThreeDModelType {
   NONE = 'none',
   CAD = 'cad',
   POINT_CLOUD = 'pointcloud',
 }
-
-export type SelectedNode = {
-  treeIndex: number;
-  nodeId: number;
-};
