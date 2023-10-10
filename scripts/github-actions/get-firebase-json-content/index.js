d7d925c01e12c697c8cc5ac0eb0e6b6286ef5ea4
Branko Dimitrijoski <40126033+bdimitrijoski@users.noreply.github.com>
2023-10-10 09:44:38 +0200

    feat: use firebase json in CD (#3456)
    * Use firebase json in CD
    
    * refactored get-firebase-json-content
    
    * fixed bug with wrong path
    
    * fixed bug with wrong path
    
    * reverted changes in nx ci
    
    ---------
    
    Co-authored-by: Branko  Dimitrijoski <branko.dimitrijoski@cognitedata.com@Brankos-Cognite-MacBook-Pro.local>
    Co-authored-by: Branko  Dimitrijoski <branko.dimitrijoski@cognitedata.com@BrankosgniteMBP.home>
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- scripts/github-actions/get-firebase-json-content/index.js
+++ scripts/github-actions/get-firebase-json-content/index.js
@@ -1,66 +1,33 @@
 const { setFailed, setOutput, getInput, info } = require('@actions/core');
+const { readFileSync, existsSync } = require('node:fs');
+const { resolve } = require('node:path');
 
 const run = async () => {
   try {
     const site = getInput('firebaseSite');
+    const project = getInput('project');
     info(`site: ${site}`);
+    const projectFolderPath = resolve(__dirname, '../../../apps', project);
+    const projectFirebaseJsonFile = projectFolderPath + '/firebase.json';
 
-    const content = `{
-      "hosting": [
-        {
-          "site": "${site}",
-          "public": ".",
-          "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
-          "rewrites": [
-            {
-              "source": "/**",
-              "destination": "/index.html"
-            }
-          ],
-          "headers": [
-            {
-              "source": "/**",
-              "headers": [
-                {
-                  "key": "Content-Security-Policy",
-                  "value": "report-uri https://o124058.ingest.sentry.io/api/6250679/security/?sentry_key=b3b2d92e10d3468f987a24bc6b593d60; script-src https://fast.appcues.com 'self' https://localhost:* https://*.cogniteapp.com https://*.firebaseio.com https://*.google.com https://*.googleapis.com https://*.hotjar.com https://cognitedata.atlassian.net https://*.mixpanel.com https://cdn.segment.com https://*.sentry.io https://sentry.io 'unsafe-eval' https://cdn.jsdelivr.net/npm/monaco-editor@0.33.0/; frame-ancestors 'self' https://localhost:* https://*.cognite.com https://*.cogniteapp.com https://*.cognitedata.com https://*.locize.app https://locize.app; object-src 'none'; worker-src 'self' blob: https://localhost:* https://*.cogniteapp.com"
-                }
-              ]
-            },
-            {
-              "source": "**",
-              "headers": [
-                {
-                  "key": "Access-Control-Allow-Origin",
-                  "value": "*"
-                }
-              ]
-            },
-            {
-              "source": "/index.js",
-              "headers": [
-                {
-                  "key": "Cache-Control",
-                  "value": "max-age=0, no-cache, no-store, must-revalidate"
-                }
-              ]
-            },
-            {
-              "source": "/remoteEntry.js",
-              "headers": [
-                {
-                  "key": "Cache-Control",
-                  "value": "max-age=0, no-cache, no-store, must-revalidate"
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }`;
+    const firebaseJsonFilePath = existsSync(projectFirebaseJsonFile)
+      ? projectFirebaseJsonFile
+      : resolve(__dirname, './firebase.json');
 
-    info(`content: ${content}`);
-    setOutput('content', content);
+    let firebaseJsonContent = JSON.parse(
+      readFileSync(firebaseJsonFilePath, 'utf-8')
+    );
+
+    firebaseJsonContent.hosting.site = site;
+    firebaseJsonContent.hosting.public = '.';
+    firebaseJsonContent = JSON.stringify(
+      { hosting: [firebaseJsonContent.hosting] },
+      null,
+      2
+    );
+
+    info(`content: ${firebaseJsonContent}`);
+    setOutput('content', firebaseJsonContent);
   } catch (error) {
     if (error instanceof Error || typeof error === 'string') {
       setFailed(error);
