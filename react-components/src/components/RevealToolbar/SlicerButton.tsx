c7f878346ce3a8a7ac7e4946c6d74e2d910b665c
Håkon Flatval <hakon.flatval@cognite.com>
2023-10-20 15:08:10 +0200

    fix(react-components): use restricted-to-most-geometry bounding box in slicing

--- react-components/src/components/RevealToolbar/SlicerButton.tsx
+++ react-components/src/components/RevealToolbar/SlicerButton.tsx
@@ -48,7 +48,8 @@ export const SlicerButton = ({ storeStateInUrl = true }: SlicerButtonProps): Rea
     }
 
     const box = new Box3();
-    viewer.models.forEach((model) => box.union(model.getModelBoundingBox()));
+    const tempBox = new Box3();
+    viewer.models.forEach((model) => box.union(model.getModelBoundingBox(tempBox, true)));
 
     const newMaxY = box.max.y;
     const newMinY = box.min.y;
