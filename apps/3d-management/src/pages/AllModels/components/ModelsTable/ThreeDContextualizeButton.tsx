776e68bfe4bd21711e61fb872e2c8089a2f3b5ef
Henrik Grønbech <henrik.gronbech@cognite.com>
2023-10-20 16:45:44 +0200

    style(3d-management): Improve 3D models "Contextualize" button style (#3663)
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/3d-management/src/pages/AllModels/components/ModelsTable/ThreeDContextualizeButton.tsx
+++ apps/3d-management/src/pages/AllModels/components/ModelsTable/ThreeDContextualizeButton.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { createLink } from '@cognite/cdf-utilities';
-import { Button } from '@cognite/cogs.js';
+import { Button, Tooltip } from '@cognite/cogs.js';
 import { useFlag } from '@cognite/react-feature-flags';
+import { Revision3D } from '@cognite/sdk';
 
 import { useRevisions } from '../../../../hooks/revisions';
 
+const getTooltipContent = (
+  revisions: Revision3D[] | undefined
+): { isDisabled: boolean; tooltipContent: string | undefined } => {
+  if (revisions === undefined || revisions.length === 0) {
+    return {
+      isDisabled: true,
+      tooltipContent: "The model doesn't have any revisions",
+    };
+  }
+
+  if (revisions[0].status === 'Failed') {
+    return {
+      isDisabled: true,
+      tooltipContent: 'The latest revision failed',
+    };
+  }
+
+  if (revisions[0].status !== 'Done') {
+    return {
+      isDisabled: true,
+      tooltipContent: 'The latest revision is still processing',
+    };
+  }
+
+  return {
+    isDisabled: false,
+    tooltipContent: undefined,
+  };
+};
+
 const ThreeDContextualizeButton = ({ record }) => {
   const navigate = useNavigate();
-  const revisionsQuery = useRevisions(record.id);
-  const isRevisionProcessing =
-    revisionsQuery.data &&
-    revisionsQuery.data[0]?.status.toLowerCase() !== 'done';
+  const { data: revisions } = useRevisions(record.id);
   const { isEnabled: contextualizeEditorFeatureFlagIsEnabled } = useFlag(
     '3D_MANAGEMENT_contextualize_editor'
   );
@@ -22,30 +49,30 @@ const ThreeDContextualizeButton = ({ record }) => {
   }
 
   const handleClick = () => {
-    const revisionId = revisionsQuery?.data?.[0]?.id;
-    if (revisionId) {
-      navigate(
-        createLink(
-          `/3d-models/contextualize-editor/${record.id}/revisions/${revisionId}`
-        )
-      );
-    }
+    const revisionId = revisions?.[0]?.id;
+    if (revisionId === undefined) return;
+
+    navigate(
+      createLink(
+        `/3d-models/contextualize-editor/${record.id}/revisions/${revisionId}`
+      )
+    );
   };
 
+  const { isDisabled, tooltipContent } = getTooltipContent(revisions);
+
+  if (isDisabled) {
+    return (
+      <Tooltip content={tooltipContent}>
+        <Button className="right-button" type="secondary" disabled={isDisabled}>
+          Contextualize
+        </Button>
+      </Tooltip>
+    );
+  }
+
   return (
-    <Button
-      className={`right-button ${
-        isRevisionProcessing ? 'disabled-button' : ''
-      }`}
-      type="primary"
-      onClick={handleClick}
-      disabled={isRevisionProcessing}
-      title={
-        isRevisionProcessing
-          ? 'The latest revision failed or is processing'
-          : ''
-      }
-    >
+    <Button className="right-button" type="secondary" onClick={handleClick}>
       Contextualize
     </Button>
   );
