a502b977322a2f8be9ce11d5505ca5078397ae82
Bianca Voinea <bianca.voinea@cognite.com>
2023-10-23 10:57:06 +0200

    clean up

--- apps/diagram-parsing/src/main.tsx
+++ apps/diagram-parsing/src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOMClient from 'react-dom/client';
 import singleSpaReact from 'single-spa-react';
 
 import { AppWrapper } from './AppWrapper';
-import { environment } from './environments/environment';
 
 // Fusion UI will expect this lifecycle events to be exported
 // when running app on local env as standalone app, we don't need it
