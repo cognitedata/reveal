d39c2c4eb702a89288a57e5869a08579d36a2b7f
Christopher Tannum <christopher.tannum@cognite.com>
2023-10-24 10:47:57 +0200

    fix: add some more test examples

--- react-components/tests/visual-tests/example.spec.ts
+++ react-components/tests/visual-tests/example.spec.ts
@@ -1,7 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { loadStory } from './utils/loadStory';
 
-test('has title', async ({ page }) => {
+test('toolbar storybook', async ({ page }) => {
   await loadStory(page, 'example-toolbar--main');
+  await expect(page).toHaveScreenshot();
+});
+
+test('cad model container storybook', async ({ page }) => {
+  await loadStory(page, 'example-primitivewrappers-cadmodelcontainer--main');
+  await expect(page).toHaveScreenshot();
+});
+
+test('image360 details storybook', async ({ page }) => {
+  await loadStory(page, 'example-image360details--main');
   await expect(page).toHaveScreenshot();
 });
