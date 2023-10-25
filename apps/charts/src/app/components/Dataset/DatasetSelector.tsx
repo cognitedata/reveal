d90ac5d67b657f8493c1e0a1064c5e7b1371ffaa
Baiel <baielrysbekov@gmail.com>
2023-10-25 16:22:36 +0200

    fix(charts): Filter only by scoped ids in DatasetSelector (#3711)
    * Filter by WRITE timeseriesAcl and dataset scope ids
    
    * small refactor
    
    * Update DatasetSelector.tsx
    
    ---------
    
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/charts/src/app/components/Dataset/DatasetSelector.tsx
+++ apps/charts/src/app/components/Dataset/DatasetSelector.tsx
@@ -1,21 +1,30 @@
 import { Select, SelectProps } from '@cognite/cogs.js';
 import { DataSet } from '@cognite/sdk';
+import { useCapabilities } from '@cognite/sdk-react-query-hooks';
 
 import { useDatasetsListQuery } from '@data-exploration-lib/domain-layer';
 
 export const DatasetSelector = (
   props: Omit<SelectProps<string>, 'options'>
 ) => {
+  const { data: capabilities } = useCapabilities('COGNITE_IDP');
+  const timeSeriesWriteScopedIds: string[] =
+    capabilities
+      ?.filter(
+        (cap) => cap.acl === 'timeSeriesAcl' && cap.actions.includes('WRITE')
+      )
+      .flatMap((cap) => cap.scope?.datasetScope?.ids) || [];
   const { data } = useDatasetsListQuery({
     filterArchivedItems: true,
   });
-
   return (
     <Select
       {...props}
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore label is not in data set and I can't send custom option type
-      options={data || []}
+      options={(data || []).filter((dataset) =>
+        timeSeriesWriteScopedIds.includes(`${dataset.id}`)
+      )}
       getOptionLabel={(option: DataSet) => option.name}
       getOptionValue={(option: DataSet) => option.id}
       menuPortalTarget={document.body}
