dafc2db828c1ebddc96e07ae153daedb171b6254
Christopher Tannum <christopher.tannum@cognite.com>
2023-10-27 12:45:39 +0200

    fix: install chromium

--- react-components/playwright.config.ts
+++ react-components/playwright.config.ts
@@ -4,27 +4,16 @@ const PORT = 6006;
 
 export default defineConfig({
   testDir: './tests/visual-tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: process.env.CI !== undefined,
-  /* Retry on CI only */
   retries: process.env.CI !== undefined ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: '50%',
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: '100%',
   reporter: 'html',
   snapshotPathTemplate: '{testDir}/__screenshots__/{testFilePath}/{arg}{ext}',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -38,7 +27,6 @@ export default defineConfig({
     }
   ],
 
-  /* Run your local dev server before starting the tests */
   webServer: {
     command:
       process.env.CI === undefined
