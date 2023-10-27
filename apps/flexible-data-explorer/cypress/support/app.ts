b64cfa20dc84301acf458105c578aad77f7717ab
Deep <70804363+deep-cognite@users.noreply.github.com>
2023-10-27 14:01:32 +0200

    fix(fdx): imports for e2e (#3761)
    Co-authored-by: cognite-bulldozer[bot] <51074376+cognite-bulldozer[bot]@users.noreply.github.com>

--- apps/flexible-data-explorer/cypress/support/app.ts
+++ apps/flexible-data-explorer/cypress/support/app.ts
@@ -1,6 +1,3 @@
-export { Operator } from '../../src/app/containers/Filter/types';
-export type {
-  NumericRange,
-  DateRange,
-} from '../../src/app/containers/Filter/types';
-export { default as english } from '../../src/i18n/en/flexible-data-explorer.json';
+export { Operator } from '@fdx/shared/types/filters';
+export type { NumericRange, DateRange } from '@fdx/shared/types/filters';
+export { default as english } from '@fdx/shared/common/i18n/en/flexible-data-explorer.json';
