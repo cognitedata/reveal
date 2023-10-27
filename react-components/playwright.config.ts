7731188c3eab995171f07e917e1000693a5d678f
Christopher Tannum <christopher.tannum@cognite.com>
2023-10-27 11:38:23 +0200

    fix: update image

--- react-components/playwright.config.ts
+++ react-components/playwright.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   workers: process.env.CI !== undefined ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  snapshotPathTemplate: '{testDir}/__screenshots__/{testFilePath}/{arg}{ext}',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
