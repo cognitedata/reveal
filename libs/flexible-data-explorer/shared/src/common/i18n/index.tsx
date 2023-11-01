832e7e5e03b1bbd0ad2ed4e635cbbfb27dd9ea64
Deep <70804363+deep-cognite@users.noreply.github.com>
2023-11-01 12:56:42 +0100

    fix(fdx): register the translations (#3840)
    * fix: register the translations
    
    * fix: key

--- libs/flexible-data-explorer/shared/src/common/i18n/index.tsx
+++ libs/flexible-data-explorer/shared/src/common/i18n/index.tsx
@@ -1,8 +1,28 @@
+import * as de from './de/flexible-data-explorer.json';
 import * as en from './en/flexible-data-explorer.json';
+import * as es from './es/flexible-data-explorer.json';
+import * as fr from './fr/flexible-data-explorer.json';
+import * as it from './it/flexible-data-explorer.json';
+import * as ja from './ja/flexible-data-explorer.json';
+import * as ko from './ko/flexible-data-explorer.json';
+import * as nl from './nl/flexible-data-explorer.json';
+import * as pt from './pt/flexible-data-explorer.json';
+import * as sv from './sv/flexible-data-explorer.json';
+import * as zh from './zh/flexible-data-explorer.json';
 
 /**
  * flexible-data-explorer app translations
  */
 export const translations = {
   en: { 'flexible-data-explorer': en },
+  de: { 'flexible-data-explorer': de },
+  es: { 'flexible-data-explorer': es },
+  fr: { 'flexible-data-explorer': fr },
+  it: { 'flexible-data-explorer': it },
+  ja: { 'flexible-data-explorer': ja },
+  ko: { 'flexible-data-explorer': ko },
+  nl: { 'flexible-data-explorer': nl },
+  pt: { 'flexible-data-explorer': pt },
+  sv: { 'flexible-data-explorer': sv },
+  zh: { 'flexible-data-explorer': zh },
 };
