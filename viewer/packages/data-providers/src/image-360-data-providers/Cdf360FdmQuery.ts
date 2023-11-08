a4173b74059f944d85e1d4b5d9d44545dabeab27
Christopher Tannum <christopher.tannum@cognite.com>
2023-11-08 15:15:36 +0100

    chore: move to new query

---
+++ viewer/packages/data-providers/src/image-360-data-providers/Cdf360FdmQuery.ts
@@ -0,0 +1,141 @@
+/*!
+ * Copyright 2023 Cognite AS
+ */
+
+const cdf360ImageSchemaSpace = 'cdf_360_image_schema';
+
+const query = {
+  with: {
+    image_collection: {
+      nodes: {
+        filter: {
+          and: [
+            {
+              equals: {
+                property: ['node', 'externalId'],
+                value: { parameter: 'collectionExternalId' }
+              }
+            },
+            {
+              equals: {
+                property: ['node', 'space'],
+                value: { parameter: 'collectionSpace' }
+              }
+            }
+          ]
+        }
+      },
+      limit: 1
+    },
+    images: {
+      nodes: {
+        from: 'image_collection',
+        through: {
+          view: {
+            type: 'view',
+            space: cdf360ImageSchemaSpace,
+            externalId: 'Image360',
+            version: 'v1'
+          },
+          identifier: 'collection360'
+        }
+      },
+      limit: 10000
+    },
+    stations: {
+      nodes: {
+        from: 'images',
+        through: {
+          view: {
+            type: 'view',
+            space: cdf360ImageSchemaSpace,
+            externalId: 'Image360',
+            version: 'v1'
+          },
+          identifier: 'station'
+        },
+        direction: 'outwards'
+      },
+      limit: 10000
+    }
+  },
+  select: {
+    image_collection: {
+      sources: [
+        {
+          source: {
+            type: 'view',
+            space: cdf360ImageSchemaSpace,
+            externalId: 'Image360Collection',
+            version: 'v1'
+          },
+          properties: ['label']
+        }
+      ]
+    },
+    images: {
+      sources: [
+        {
+          source: {
+            type: 'view',
+            space: cdf360ImageSchemaSpace,
+            externalId: 'Image360',
+            version: 'v1'
+          },
+          properties: [
+            'translationX',
+            'translationY',
+            'translationZ',
+            'eulerRotationX',
+            'eulerRotationY',
+            'eulerRotationZ',
+            'scaleX',
+            'scaleY',
+            'scaleZ',
+            'cubeMapFront',
+            'cubeMapBack',
+            'cubeMapLeft',
+            'cubeMapRight',
+            'cubeMapTop',
+            'cubeMapBottom',
+            'collection360',
+            'station',
+            'timeTaken',
+            'label'
+          ]
+        },
+        {
+          source: {
+            type: 'view',
+            space: 'aSpace',
+            externalId: 'aExternalId',
+            version: 'v1'
+          },
+          properties: ['asdasd']
+        }
+      ]
+    },
+    stations: {
+      sources: [
+        {
+          source: {
+            type: 'view',
+            space: cdf360ImageSchemaSpace,
+            externalId: 'Station360',
+            version: 'v1'
+          },
+          properties: ['label']
+        }
+      ]
+    }
+  }
+} as const;
+
+export type Cdf360FdmQuery = typeof query;
+
+export function get360CollectionQuery(
+  externalId: string,
+  space: string
+): Cdf360FdmQuery & { parameters: { collectionExternalId: string; collectionSpace: string } } {
+  return { ...query, parameters: { collectionExternalId: externalId, collectionSpace: space } };
+}
