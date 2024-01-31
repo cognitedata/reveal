752f8e2b7141a5ca661ece4ed92518b17fd5ce43
Pramod S <pramod.s@cognite.com>
2024-01-31 18:21:45 +0100

    fixed unit test

--- react-components/src/components/RevealKeepAlive/RevealKeepAlive.tsx
+++ react-components/src/components/RevealKeepAlive/RevealKeepAlive.tsx
@@ -7,12 +7,14 @@ import { type ReactNode, type ReactElement, useRef, useEffect } from 'react';
 import { RevealKeepAliveContext } from './RevealKeepAliveContext';
 import { type FdmNodeCache } from '../NodeCacheProvider/FdmNodeCache';
 import { type AssetMappingCache } from '../NodeCacheProvider/AssetMappingCache';
+import { type PointCloudObjectCollectionCache } from '../NodeCacheProvider/PointCloudObjectCollectionCache';
 
 export function RevealKeepAlive({ children }: { children?: ReactNode }): ReactElement {
   const viewerRef = useRef<Cognite3DViewer>();
   const isRevealContainerMountedRef = useRef<boolean>(false);
   const fdmNodeCache = useRef<FdmNodeCache>();
   const assetMappingCache = useRef<AssetMappingCache>();
+  const pointCloudObjectCollectionCache = useRef<PointCloudObjectCollectionCache>();
 
   useEffect(() => {
     return () => {
@@ -22,7 +24,13 @@ export function RevealKeepAlive({ children }: { children?: ReactNode }): ReactEl
   }, []);
   return (
     <RevealKeepAliveContext.Provider
-      value={{ viewerRef, isRevealContainerMountedRef, fdmNodeCache, assetMappingCache }}>
+      value={{
+        viewerRef,
+        isRevealContainerMountedRef,
+        fdmNodeCache,
+        assetMappingCache,
+        pointCloudObjectCollectionCache
+      }}>
       {children}
     </RevealKeepAliveContext.Provider>
   );
