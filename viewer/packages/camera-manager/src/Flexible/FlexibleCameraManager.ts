3ce90d40695f04fb46ee876af91ebce630141261
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-02-15 15:31:04 +0100

    Tuning based on Kerems suggestions and requirements for Camera Control [BND3D-2702] (#4184)
    * Add possiblility to use useFlexibleCameraManager
    
    * Some minor adjustment and cleaning
    
    * Remove flag
    
    * Make the code more understandable
    
    * Update FlexibleControls.ts
    
    * A>dd rotator and translators
    
    * Made panning and rotating better
    
    * Make dampening also for key pressed
    
    * Update FlexibleControls.ts
    
    * Update FlexibleControls.ts
    
    * Remove the rotator
    
    * Fixing camera angles
    
    * Reuse Quaternion
    
    * Fix events
    
    * Hide marker when first persion
    
    * Update FlexibleControlsTranslator.ts
    
    * Removing getters
    
    * Adding function to increase readability
    
    * Fixing to close bug

--- viewer/packages/camera-manager/src/Flexible/FlexibleCameraManager.ts
+++ viewer/packages/camera-manager/src/Flexible/FlexibleCameraManager.ts
@@ -14,10 +14,10 @@ import {
   disposeOfAllEventListeners,
   PointerEventData,
   fitCameraToBoundingBox,
-  clickOrTouchEventOffset
+  getNormalizedPixelCoordinates,
+  getClickOrTouchEventPoint
 } from '@reveal/utilities';
 
-import { getNormalizedPixelCoordinates } from '@reveal/utilities';
 import {
   CameraChangeDelegate,
   CameraEventDelegate,
@@ -86,6 +86,7 @@ export class FlexibleCameraManager implements IFlexibleCameraManager {
   ) {
     this._camera = camera ?? new PerspectiveCamera(60, undefined, 0.1, 10000);
     this._controls = new FlexibleControls(this.camera, domElement, new FlexibleControlsOptions());
+    this._controls.getPickedPointByPixelCoordinates = this.getPickedPointByPixelCoordinates;
     this._domElement = domElement;
     this._inputHandler = inputHandler;
     this._raycastCallback = raycastCallback;
@@ -285,18 +286,18 @@ export class FlexibleCameraManager implements IFlexibleCameraManager {
   // INSTANCE METHODS: Calculations
   //================================================
 
-  private async getPickedPointPixelCoordinates(pixelX: number, pixelY: number): Promise<Vector3> {
+  private readonly getPickedPointByPixelCoordinates = async (pixelX: number, pixelY: number): Promise<Vector3> => {
     const raycastResult = await this._raycastCallback(pixelX, pixelY, false);
     if (raycastResult.intersection?.point) {
       return raycastResult.intersection.point;
     }
     return this.getTargetByBoundingBox(pixelX, pixelY, raycastResult.modelsBoundingBox);
-  }
+  };
 
   private getTargetByBoundingBox(pixelX: number, pixelY: number, boundingBox: Box3): Vector3 {
     const raycaster = new Raycaster();
-    const pixelCoordinates = getNormalizedPixelCoordinates(this.domElement, pixelX, pixelY);
-    raycaster.setFromCamera(pixelCoordinates, this.camera);
+    const normalizedCoords = getNormalizedPixelCoordinates(this.domElement, pixelX, pixelY);
+    raycaster.setFromCamera(normalizedCoords, this.camera);
 
     // Try to intersect the bounding box
     const closestIntersection = new Vector3();
@@ -393,9 +394,8 @@ export class FlexibleCameraManager implements IFlexibleCameraManager {
     // Added because cameraControls are disabled when doing picking, so
     // preventDefault could be not called on wheel event and produce unwanted scrolling.
     event.preventDefault();
-    const pixelPosition = clickOrTouchEventOffset(event, this.domElement);
+    const currentCoords = getClickOrTouchEventPoint(event, this.domElement);
     const currentTime = performance.now();
-    const currentCoords = new Vector2(pixelPosition.offsetX, pixelPosition.offsetY);
     const deltaTime = currentTime - this._prevTime;
     const deltaCoords = currentCoords.distanceTo(this._prevCoords);
 
@@ -410,7 +410,7 @@ export class FlexibleCameraManager implements IFlexibleCameraManager {
     this._prevTime = currentTime;
     this._prevCoords.copy(currentCoords);
 
-    const scrollCursor = await this.getPickedPointPixelCoordinates(pixelPosition.offsetX, pixelPosition.offsetY);
+    const scrollCursor = await this.getPickedPointByPixelCoordinates(currentCoords.x, currentCoords.y);
     this.controls.setScrollCursor(scrollCursor);
     this._prevTime = currentTime;
   };
@@ -420,22 +420,16 @@ export class FlexibleCameraManager implements IFlexibleCameraManager {
       return;
     }
     if (mouseActionType === FlexibleMouseActionType.SetTarget) {
-      if (this.controls.controlsType !== FlexibleControlsType.Orbit) {
-        this.controls.setControlsType(FlexibleControlsType.Orbit);
+      if (this.controls.controlsType === FlexibleControlsType.FirstPerson) {
+        return;
       }
-      const newTarget = await this.getPickedPointPixelCoordinates(event.offsetX, event.offsetY);
+      const newTarget = await this.getPickedPointByPixelCoordinates(event.offsetX, event.offsetY);
       this.controls.setTarget(newTarget);
       this.controls.triggerCameraChangeEvent();
     } else if (mouseActionType === FlexibleMouseActionType.SetTargetAndCameraDirection) {
-      if (this.controls.controlsType !== FlexibleControlsType.Orbit) {
-        this.controls.setControlsType(FlexibleControlsType.Orbit);
-      }
-      const newTarget = await this.getPickedPointPixelCoordinates(event.offsetX, event.offsetY);
+      const newTarget = await this.getPickedPointByPixelCoordinates(event.offsetX, event.offsetY);
       moveCameraTargetTo(this, newTarget, this.options.animationDuration);
     } else if (mouseActionType === FlexibleMouseActionType.SetTargetAndCameraPosition) {
-      if (this.controls.controlsType !== FlexibleControlsType.Orbit) {
-        this.controls.setControlsType(FlexibleControlsType.Orbit);
-      }
       this.setTargetAndCameraPosition(event);
     }
   }
--- viewer/packages/utilities/src/events/clickOrTouchEventOffset.ts
+++ viewer/packages/utilities/src/events/clickOrTouchEventOffset.ts
@@ -2,29 +2,31 @@
  * Copyright 2021 Cognite AS
  */
 
+import { Vector2 } from 'three';
+
 /**
  * Determines clicked or touched coordinate as offset
- * @param ev        An PointerEvent or WheelEvent.
+ * @param event     An PointerEvent or WheelEvent.
  * @param target    HTML element to find coordinates relative to.
  * @returns A struct containing coordinates relative to the HTML element provided.
  */
 export function clickOrTouchEventOffset(
-  ev: PointerEvent | WheelEvent,
+  event: PointerEvent | WheelEvent,
   target: HTMLElement
 ): { offsetX: number; offsetY: number } {
   const rect = target.getBoundingClientRect();
 
-  if (ev instanceof PointerEvent) {
-    if (ev.pointerType === 'mouse' || ev.pointerType === 'touch') {
+  if (event instanceof PointerEvent) {
+    if (event.pointerType === 'mouse' || event.pointerType === 'touch') {
       return {
-        offsetX: ev.clientX - rect.left,
-        offsetY: ev.clientY - rect.top
+        offsetX: event.clientX - rect.left,
+        offsetY: event.clientY - rect.top
       };
     }
-  } else if (ev instanceof WheelEvent) {
+  } else if (event instanceof WheelEvent) {
     return {
-      offsetX: ev.clientX - rect.left,
-      offsetY: ev.clientY - rect.top
+      offsetX: event.clientX - rect.left,
+      offsetY: event.clientY - rect.top
     };
   }
 
@@ -33,4 +35,15 @@ export function clickOrTouchEventOffset(
     offsetX: -1,
     offsetY: -1
   };
+}
+
+/**
+ * Determines clicked or touched coordinate as offset
+ * @param event     An PointerEvent or WheelEvent.
+ * @param target    HTML element to find coordinates relative to.
+ * @returns A Vector2 containing coordinates relative to the HTML element provided.
+ */
+export function getClickOrTouchEventPoint(event: PointerEvent | WheelEvent, target: HTMLElement): Vector2 {
+  const point = clickOrTouchEventOffset(event, target);
+  return new Vector2(point.offsetX, point.offsetY);
 }
