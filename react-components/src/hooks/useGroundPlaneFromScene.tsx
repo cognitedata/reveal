489d8e674e5582f6241b7ef137aaf7062d2614e7
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-02-18 19:37:38 +0100

    Update useGroundPlaneFromScene.tsx

--- react-components/src/hooks/useGroundPlaneFromScene.tsx
+++ react-components/src/hooks/useGroundPlaneFromScene.tsx
@@ -8,7 +8,7 @@ import { DoubleSide, Mesh, MeshBasicMaterial, PlaneGeometry, TextureLoader } fro
 import { useReveal } from '..';
 import { useQuery } from '@tanstack/react-query';
 import { useSDK } from '../components/RevealCanvas/SDKProvider';
-import { CDF_TO_VIEWER_TRANSFORMATION } from '@cognite/reveal';
+import { CDF_TO_VIEWER_TRANSFORMATION, CustomObject } from '@cognite/reveal';
 
 export const useGroundPlaneFromScene = (sceneExternalId: string, sceneSpaceId: string): void => {
   const { data: scene } = useSceneConfig(sceneExternalId, sceneSpaceId);
@@ -39,7 +39,7 @@ export const useGroundPlaneFromScene = (sceneExternalId: string, sceneSpaceId: s
     if (scene === undefined || groundPlanesUrls === undefined || groundPlanesUrls.length === 0) {
       return;
     }
-    const groundMeshes: Mesh[] = [];
+    const groundMeshes: CustomObject[] = [];
 
     scene.groundPlanes.forEach((groundPlane, index) => {
       if (groundPlanesUrls === undefined) {
@@ -48,6 +48,7 @@ export const useGroundPlaneFromScene = (sceneExternalId: string, sceneSpaceId: s
       const texture = groundPlanesUrls[index];
       const material = new MeshBasicMaterial({ map: texture, side: DoubleSide });
       const geometry = new PlaneGeometry(10000 * groundPlane.scaleX, 10000 * groundPlane.scaleY);
+
       geometry.name = `CogniteGroundPlane`;
 
       const mesh = new Mesh(geometry, material);
@@ -59,21 +60,25 @@ export const useGroundPlaneFromScene = (sceneExternalId: string, sceneSpaceId: s
       mesh.rotation.set(-Math.PI / 2, 0, 0);
 
       mesh.position.applyMatrix4(CDF_TO_VIEWER_TRANSFORMATION);
-      viewer.addObject3D(mesh);
 
-      groundMeshes.push(mesh);
+      const customObject = new CustomObject(mesh);
+      customObject.isPartOfBoundingBox = false;
+      viewer.addCustomObject(customObject);
+
+      groundMeshes.push(customObject);
     });
 
     return () => {
       // Cleanup function
-      groundMeshes.forEach((groundPlane) => {
+      groundMeshes.forEach((customObject) => {
+        const groundPlane = customObject.object as Mesh;
         groundPlane.geometry.dispose();
 
         const material = groundPlane.material as MeshBasicMaterial;
         material.map?.dispose();
         material.dispose();
 
-        viewer.removeObject3D(groundPlane);
+        viewer.removeCustomObject(customObject);
       });
 
       groundMeshes.splice(0, groundMeshes.length);
