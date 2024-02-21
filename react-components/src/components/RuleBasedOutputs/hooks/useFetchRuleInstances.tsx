f0ffd39707b079941e10af7e9d7de7748b71f04f
Håkon Flatval <hakon.flatval@cognite.com>
2024-02-21 15:54:41 +0100

    chore: revert more unnecessary changes

--- react-components/src/components/RuleBasedOutputs/hooks/useFetchRuleInstances.tsx
+++ react-components/src/components/RuleBasedOutputs/hooks/useFetchRuleInstances.tsx
@@ -1,22 +1,18 @@
 /*!
  * Copyright 2024 Cognite AS
  */
-import { useFdmSdk } from '../../RevealCanvas/SDKProvider';
+import { useMemo } from 'react';
+import { useSDK } from '../../RevealCanvas/SDKProvider';
 import { RULE_BASED_OUTPUTS_VIEW } from '../constants';
+import { FdmSDK } from '../../../utilities/FdmSDK';
 import { type UseQueryResult, useQuery } from '@tanstack/react-query';
 import { type RuleAndEnabled } from '../types';
-import { fdmViewsExist } from '../../../utilities/fdmViewsExist';
 
 export const useFetchRuleInstances = (): UseQueryResult<RuleAndEnabled[], unknown> => {
-  const fdmSdk = useFdmSdk();
+  const sdk = useSDK();
+  const fdmSdk = useMemo(() => new FdmSDK(sdk), [sdk]);
 
   return useQuery(['react-components', 'color-overlay-rules', 'all'], async () => {
-    const viewExists = fdmViewsExist(fdmSdk, [RULE_BASED_OUTPUTS_VIEW]);
-
-    if (!viewExists) {
-      return [];
-    }
-
     const versionedPropertiesKey = `${RULE_BASED_OUTPUTS_VIEW.externalId}/${RULE_BASED_OUTPUTS_VIEW.version}`;
 
     const filter = {
