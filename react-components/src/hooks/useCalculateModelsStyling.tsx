0bd6d3d1b094fa222224280a7138ee1c1d97c8af
Håkon Flatval <hakon.flatval@cognite.com>
2024-02-28 14:14:47 +0100

    fix(react-components): return all nodes in asset mapping search

--- react-components/src/hooks/useCalculateModelsStyling.tsx
+++ react-components/src/hooks/useCalculateModelsStyling.tsx
@@ -320,7 +320,7 @@ function calculateFdmCadModelStyling(
 
 function calculateAssetMappingCadModelStyling(
   stylingGroups: AssetStylingGroup[],
-  nodeMap: Map<AssetId, Node3D>,
+  nodeMap: Map<AssetId, Node3D[]>,
   model: CadModelOptions
 ): ModelStyleGroup {
   const treeIndexSetsWithStyle = stylingGroups
@@ -328,10 +328,13 @@ function calculateAssetMappingCadModelStyling(
       const indexSet = new IndexSet();
       group.assetIds
         .map((assetId) => nodeMap.get(assetId))
-        .filter((node): node is Node3D => node !== undefined)
-        .forEach((node) => {
-          indexSet.addRange(new NumericRange(node.treeIndex, node.subtreeSize));
-        });
+        .forEach((nodeList) =>
+          nodeList
+            ?.filter((node): node is Node3D => node !== undefined)
+            .forEach((node) => {
+              indexSet.addRange(new NumericRange(node.treeIndex, node.subtreeSize));
+            })
+        );
 
       return {
         treeIndexSet: indexSet,
