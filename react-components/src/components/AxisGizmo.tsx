095adc0ce02005b6f047c67dd5a47f690d07c793
Håkon Flatval <hakon.flatval@cognite.com>
2024-03-01 12:47:51 +0100

    chore: fix imports

--- react-components/src/components/AxisGizmo.tsx
+++ react-components/src/components/AxisGizmo.tsx
@@ -5,7 +5,7 @@
 import { useEffect } from 'react';
 
 import { AxisGizmoOptions, AxisGizmoTool } from '@cognite/reveal/tools';
-import { useReveal } from '@cognite/reveal-react-components';
+import { useReveal } from '..';
 
 export { AxisGizmoOptions };
 
@@ -13,10 +13,6 @@ export const AxisGizmo = ({ options }: { options?: AxisGizmoOptions }) => {
   const viewer = useReveal();
 
   useEffect(() => {
-    if (!viewer) {
-      return;
-    }
-
     const axisGizmoTool = new AxisGizmoTool(options);
     axisGizmoTool.connect(viewer);
 
