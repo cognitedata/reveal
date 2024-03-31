db4a33c5ac9c11e782318158b1e9f62f9587ae4d
Daniel Priori <daniel.priori@cognite.com>
2024-03-31 15:48:32 +0200

    add dms delete instance

---
+++ react-components/src/components/RuleBasedOutputs/hooks/useDeleteRuleInstance.tsx
@@ -0,0 +1,34 @@
+/*!
+ * Copyright 2024 Cognite AS
+ */
+import { useFdmSdk } from '../../RevealCanvas/SDKProvider';
+import { RULE_BASED_OUTPUTS_VIEW } from '../constants';
+import { type ExternalIdsResultList, type RuleOutputSet } from '../types';
+import { fdmViewsExist } from '../../../utilities/fdmViewsExist';
+
+export const useDeleteRuleInstance = (): ((
+  ruleOutputSet: RuleOutputSet
+) => Promise<ExternalIdsResultList<unknown>>) => {
+  const fdmSdk = useFdmSdk();
+
+  const deleteRule = async (
+    ruleOutputSet: RuleOutputSet
+  ): Promise<ExternalIdsResultList<unknown>> => {
+    const viewExists = await fdmViewsExist(fdmSdk, [RULE_BASED_OUTPUTS_VIEW]);
+
+    if (!viewExists) {
+      return { items: [] };
+    }
+
+    const resultFromSavingRule = await fdmSdk.deleteInstance([
+      {
+        instanceType: 'node',
+        space: RULE_BASED_OUTPUTS_VIEW.space,
+        externalId: ruleOutputSet.id
+      }
+    ]);
+
+    return resultFromSavingRule;
+  };
+  return deleteRule;
+};
