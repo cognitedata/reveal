33ea6c18e924918d2307c5b6fe62fcaf19febcf7
Daniel Priori <daniel.priori@cognite.com>
2024-04-02 10:21:01 +0200

    fix: changes from the review and introduce editInstance method

--- react-components/src/components/RuleBasedOutputs/hooks/useEditRuleInstance.tsx
+++ react-components/src/components/RuleBasedOutputs/hooks/useEditRuleInstance.tsx
@@ -5,48 +5,50 @@ import { useFdmSdk } from '../../RevealCanvas/SDKProvider';
 import { RULE_BASED_OUTPUTS_VIEW } from '../constants';
 import { type ExternalIdsResultList, type RuleOutputSet } from '../types';
 import { fdmViewsExist } from '../../../utilities/fdmViewsExist';
+import { useCallback } from 'react';
 
 export const useEditRuleInstance = (): ((
   ruleOutputSet: RuleOutputSet
 ) => Promise<ExternalIdsResultList<unknown>>) => {
   const fdmSdk = useFdmSdk();
 
-  const editCurrentRule = async (
-    ruleOutputSet: RuleOutputSet
-  ): Promise<ExternalIdsResultList<unknown>> => {
-    const viewExists = await fdmViewsExist(fdmSdk, [RULE_BASED_OUTPUTS_VIEW]);
+  const editCurrentRule = useCallback(
+    async (ruleOutputSet: RuleOutputSet): Promise<ExternalIdsResultList<unknown>> => {
+      const viewExists = await fdmViewsExist(fdmSdk, [RULE_BASED_OUTPUTS_VIEW]);
 
-    if (!viewExists) {
-      return { items: [] };
-    }
-
-    const resultFromSavingRule = await fdmSdk.createInstance([
-      {
-        instanceType: 'node',
-        space: RULE_BASED_OUTPUTS_VIEW.space,
-        externalId: ruleOutputSet.id,
-        sources: [
-          {
-            properties: {
-              name: ruleOutputSet.name,
-              id: ruleOutputSet.id,
-              createdAt: ruleOutputSet.createdAt,
-              createdBy: ruleOutputSet.createdBy,
-              rulesWithOutputs: JSON.parse(JSON.stringify(ruleOutputSet.rulesWithOutputs)),
-              shamefulOutputTypes: ['color']
-            },
-            source: {
-              type: RULE_BASED_OUTPUTS_VIEW.type,
-              space: RULE_BASED_OUTPUTS_VIEW.space,
-              externalId: RULE_BASED_OUTPUTS_VIEW.externalId,
-              version: RULE_BASED_OUTPUTS_VIEW.version
-            }
-          }
-        ]
+      if (!viewExists) {
+        return { items: [] };
       }
-    ]);
 
-    return resultFromSavingRule;
-  };
+      const resultFromSavingRule = await fdmSdk.editInstance([
+        {
+          instanceType: 'node',
+          space: RULE_BASED_OUTPUTS_VIEW.space,
+          externalId: ruleOutputSet.id,
+          sources: [
+            {
+              properties: {
+                name: ruleOutputSet.name,
+                id: ruleOutputSet.id,
+                createdAt: ruleOutputSet.createdAt,
+                createdBy: ruleOutputSet.createdBy,
+                rulesWithOutputs: JSON.parse(JSON.stringify(ruleOutputSet.rulesWithOutputs)),
+                shamefulOutputTypes: ['color']
+              },
+              source: {
+                type: RULE_BASED_OUTPUTS_VIEW.type,
+                space: RULE_BASED_OUTPUTS_VIEW.space,
+                externalId: RULE_BASED_OUTPUTS_VIEW.externalId,
+                version: RULE_BASED_OUTPUTS_VIEW.version
+              }
+            }
+          ]
+        }
+      ]);
+
+      return resultFromSavingRule;
+    },
+    []
+  );
   return editCurrentRule;
 };
--- react-components/src/components/RuleBasedOutputs/hooks/useSearchRuleInstance.tsx
+++ react-components/src/components/RuleBasedOutputs/hooks/useSearchRuleInstance.tsx
@@ -5,38 +5,42 @@ import { useFdmSdk } from '../../RevealCanvas/SDKProvider';
 import { RULE_BASED_OUTPUTS_VIEW } from '../constants';
 import { type NodeItem, type RuleOutputSet } from '../types';
 import { fdmViewsExist } from '../../../utilities/fdmViewsExist';
+import { useCallback } from 'react';
 
 export const useSearchRuleInstance = (): ((
   externalId: string
 ) => Promise<Array<NodeItem<RuleOutputSet>>>) => {
   const fdmSdk = useFdmSdk();
 
-  const searchRuleById = async (ruleId: string): Promise<Array<NodeItem<RuleOutputSet>>> => {
-    const viewExists = await fdmViewsExist(fdmSdk, [RULE_BASED_OUTPUTS_VIEW]);
+  const searchRuleById = useCallback(
+    async (ruleId: string): Promise<Array<NodeItem<RuleOutputSet>>> => {
+      const viewExists = await fdmViewsExist(fdmSdk, [RULE_BASED_OUTPUTS_VIEW]);
 
-    if (!viewExists) {
-      return [];
-    }
-
-    const versionedPropertiesKey = `${RULE_BASED_OUTPUTS_VIEW.externalId}/${RULE_BASED_OUTPUTS_VIEW.version}`;
-
-    const filter = {
-      in: {
-        property: [RULE_BASED_OUTPUTS_VIEW.space, versionedPropertiesKey, 'id'],
-        values: [ruleId]
+      if (!viewExists) {
+        return [];
       }
-    };
 
-    const result = await fdmSdk.searchInstances<RuleOutputSet>(
-      RULE_BASED_OUTPUTS_VIEW,
-      '',
-      'node',
-      1,
-      filter
-    );
+      const versionedPropertiesKey = `${RULE_BASED_OUTPUTS_VIEW.externalId}/${RULE_BASED_OUTPUTS_VIEW.version}`;
 
-    return result.instances;
-  };
+      const filter = {
+        in: {
+          property: [RULE_BASED_OUTPUTS_VIEW.space, versionedPropertiesKey, 'id'],
+          values: [ruleId]
+        }
+      };
+
+      const result = await fdmSdk.searchInstances<RuleOutputSet>(
+        RULE_BASED_OUTPUTS_VIEW,
+        '',
+        'node',
+        1,
+        filter
+      );
+
+      return result.instances;
+    },
+    []
+  );
 
   return searchRuleById;
 };
