ef2e6ef53d3d54d09f172d96849c57cd85692c5a
Pramod S <pramod.s@cognite.com>
2024-04-17 14:33:23 +0200

    fixed menu item rendering empty when no model is added into a specifice type into viewer

--- react-components/src/components/RevealToolbar/LayersContainer/LayersContainer.tsx
+++ react-components/src/components/RevealToolbar/LayersContainer/LayersContainer.tsx
@@ -30,21 +30,27 @@ export const LayersContainer = ({
               onClick={(event: MouseEvent<HTMLElement>) => {
                 event.stopPropagation();
               }}>
-              <LayerToggleDropdown
-                layerHandlers={modelHandlers.cadHandlers}
-                update={update}
-                label={t('CAD_MODELS', 'CAD models')}
-              />
-              <LayerToggleDropdown
-                layerHandlers={modelHandlers.pointCloudHandlers}
-                update={update}
-                label={t('POINT_CLOUDS', 'Pointclouds')}
-              />
-              <LayerToggleDropdown
-                layerHandlers={modelHandlers.image360Handlers}
-                update={update}
-                label={t('360_IMAGES', '360 images')}
-              />
+              {modelHandlers.cadHandlers.length > 0 && (
+                <LayerToggleDropdown
+                  layerHandlers={modelHandlers.cadHandlers}
+                  update={update}
+                  label={t('CAD_MODELS', 'CAD models')}
+                />
+              )}
+              {modelHandlers.pointCloudHandlers.length > 0 && (
+                <LayerToggleDropdown
+                  layerHandlers={modelHandlers.pointCloudHandlers}
+                  update={update}
+                  label={t('POINT_CLOUDS', 'Pointclouds')}
+                />
+              )}
+              {modelHandlers.image360Handlers.length > 0 && (
+                <LayerToggleDropdown
+                  layerHandlers={modelHandlers.image360Handlers}
+                  update={update}
+                  label={t('360_IMAGES', '360 images')}
+                />
+              )}
             </StyledMenu>
           </Container>
         </>
