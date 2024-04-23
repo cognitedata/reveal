750de5bc6bb802a0c27b7b7dd31a61ceeb84c424
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-04-23 08:13:57 +0200

    (refactor) Encapsulate Cognite3DViewer (#4383)
    * Initial commit
    
    * Update .prettierrc
    
    * Fixing lint errors
    
    * Update RevealRenderTarget.ts
    
    * More fixes
    
    * Update RevealRenderTarget.ts
    
    * chore: fix test
    
    Turns out the Mock'ed object could not be used in a setState
    setter (state would become undefined). Still not sure as to why, but
    probably becaute the mock object is implemented in a very obscure way
    
    * chore: remove unused code
    
    * fix: set background color properly
    
    * chore: add axis gizmo to screenshots for visual tests
    
    ---------
    
    Co-authored-by: Håkon Flatval <hakon.flatval@cognite.com>

--- react-components/src/components/RevealKeepAlive/RevealKeepAliveContext.ts
+++ react-components/src/components/RevealKeepAlive/RevealKeepAliveContext.ts
@@ -1,16 +1,16 @@
 /*!
  * Copyright 2023 Cognite AS
  */
-import { type Cognite3DViewer } from '@cognite/reveal';
 import { type MutableRefObject, createContext, useContext } from 'react';
 import { type FdmNodeCache } from '../CacheProvider/FdmNodeCache';
 import { type AssetMappingCache } from '../CacheProvider/AssetMappingCache';
 import { type PointCloudAnnotationCache } from '../CacheProvider/PointCloudAnnotationCache';
 import { type Image360AnnotationCache } from '../CacheProvider/Image360AnnotationCache';
 import { type SceneIdentifiers } from '../SceneContainer/sceneTypes';
+import { type RevealRenderTarget } from '../../architecture/RenderTarget/RevealRenderTarget';
 
 export type RevealKeepAliveData = {
-  viewerRef: MutableRefObject<Cognite3DViewer | undefined>;
+  renderTargetRef: MutableRefObject<RevealRenderTarget | undefined>;
   isRevealContainerMountedRef: MutableRefObject<boolean>;
   sceneLoadedRef: MutableRefObject<SceneIdentifiers | undefined>;
   fdmNodeCache: MutableRefObject<FdmNodeCache | undefined>;
