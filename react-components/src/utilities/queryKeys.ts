c9a50e52b105e8f50cceda82d6fe1e4d14b2dfe7
Daniel Priori <daniel.priori@cognite.com>
2024-05-12 11:52:21 +0200

    not expose internal query keys

--- react-components/src/utilities/queryKeys.ts
+++ react-components/src/utilities/queryKeys.ts
@@ -6,13 +6,19 @@ import { type IdEither } from '@cognite/sdk/';
 export const queryKeys = {
   all: ['cdf'] as const,
   // ASSETS
-  assets: () => [...queryKeys.all, 'assets'] as const,
-  assetsById: (ids: IdEither[]) => [...queryKeys.all, 'assets', ids] as const,
+  assetsById: (ids: IdEither[]) => [...assets(), ids] as const,
   // TIMESERIES
-  timeseries: () => [...queryKeys.all, 'timeseries'] as const,
-  timeseriesById: (ids: IdEither[]) => [...queryKeys.timeseries(), ids] as const,
-  timeseriesLatestDatapoint: () => [...queryKeys.all, 'timeseries', 'latest-datapoints'] as const,
+  timeseriesById: (ids: IdEither[]) => [...timeseries(), ids] as const,
+  timeseriesLatestDatapoint: () => [...timeseries(), 'latest-datapoints'] as const,
   // TIMESERIES RELATIONSHIPS WITH ASSETS
   timeseriesLinkedToAssets: () =>
     [...queryKeys.all, 'timeseries', 'timeseries-linked-assets'] as const
 } as const;
+
+const assets = () => {
+  return [...queryKeys.all, 'assets'];
+};
+
+const timeseries = () => {
+  return [...queryKeys.all, 'timeseries'];
+};
