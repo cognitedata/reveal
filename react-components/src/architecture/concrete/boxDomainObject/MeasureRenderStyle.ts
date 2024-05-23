4e61a49d9b70107549b7e3b27676b86125f2de09
Nils Petter Fremming <nils.fremming@cognite.com>
2024-05-23 14:14:18 +0200

    Update MeasureRenderStyle.ts

--- react-components/src/architecture/concrete/boxDomainObject/MeasureRenderStyle.ts
+++ react-components/src/architecture/concrete/boxDomainObject/MeasureRenderStyle.ts
@@ -12,9 +12,9 @@ export abstract class MeasureRenderStyle extends RenderStyle {
   // INSTANCE FIELDS
   // ==================================================
 
-  public depthTest = false;
+  public depthTest = true;
   public colorType = ColorType.Specified;
   public textColor = WHITE_COLOR.clone();
   public textBgColor = new Color().setScalar(0.05); // Dark gray
-  public relativeTextSize = 0.05; // Relative to diagonal of the box
+  public relativeTextSize = 0.05; // Relative to diagonal of the measurment object
 }
