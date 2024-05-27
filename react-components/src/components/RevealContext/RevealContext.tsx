c34ad2d9e761977dd1332bf080710f318955c5b2
Håkon Flatval <hakon.flatval@cognite.com>
2024-05-27 17:22:51 +0200

    fix: register event listeners if not using flexible camera

--- react-components/src/components/RevealContext/RevealContext.tsx
+++ react-components/src/components/RevealContext/RevealContext.tsx
@@ -101,7 +101,11 @@ const useRevealFromKeepAlive = ({
   function getOrInitializeRenderTarget(): RevealRenderTarget {
     let renderTarget = revealKeepAliveData?.renderTargetRef.current;
     if (renderTarget === undefined) {
-      const viewer = new Cognite3DViewer({ ...viewerOptions, sdk, hasEventListeners: false });
+      const viewer = new Cognite3DViewer({
+        ...viewerOptions,
+        sdk,
+        hasEventListeners: !viewerOptions?.useFlexibleCameraManager
+      });
       renderTarget = new RevealRenderTarget(viewer);
       if (revealKeepAliveData !== undefined) {
         revealKeepAliveData.renderTargetRef.current = renderTarget;
