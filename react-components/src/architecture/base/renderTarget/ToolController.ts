fcd1a21644bd27d39e28ba697400bf878e1772a4
Håkon Flatval <hakon.flatval@cognite.com>
2024-05-27 11:31:33 +0200

    chore: don't open context menu on right-clicking canvas

--- react-components/src/architecture/base/renderTarget/ToolController.ts
+++ react-components/src/architecture/base/renderTarget/ToolController.ts
@@ -171,17 +171,18 @@ export class ToolControllers extends PointerEvents {
     domElement.addEventListener('wheel', this._onWheel);
     domElement.addEventListener('focus', this._onFocus);
     domElement.addEventListener('blur', this._onBlur);
+    domElement.addEventListener('contextmenu', this._onContextMenu);
     this._pointerEventsTarget.addEventListeners();
   }
 
   public removeEventListeners(): void {
     const domElement = this._domElement;
     domElement.removeEventListener('keydown', this._onKeyDown);
-    domElement.addEventListener('contextmenu', this._onContextMenu);
     domElement.removeEventListener('keyup', this._onKeyUp);
     domElement.removeEventListener('wheel', this._onWheel);
     domElement.removeEventListener('focus', this._onFocus);
     domElement.removeEventListener('blur', this._onBlur);
+    domElement.removeEventListener('contextmenu', this._onContextMenu);
     this._pointerEventsTarget.removeEventListeners();
     for (const commands of this._commands) {
       commands.removeEventListeners();
