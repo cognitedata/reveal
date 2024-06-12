47f6d9c3a1490e48a582168ce6c3f9ef75789d49
Nils Petter Fremming <nils.fremming@cognite.com>
2024-06-12 14:58:44 +0200

    adjustments

--- react-components/src/architecture/base/utilities/extensions/rayExtensions.ts
+++ react-components/src/architecture/base/utilities/extensions/rayExtensions.ts
@@ -15,15 +15,15 @@ export function getClosestPointOnLine(
   ray: Ray,
   lineDirection: Vector3,
   pointOnLine: Vector3,
-  optionalClosestPointOnLine?: Vector3
+  target?: Vector3
 ): Vector3 {
-  if (optionalClosestPointOnLine === undefined) {
-    optionalClosestPointOnLine = new Vector3();
+  if (target === undefined) {
+    target = new Vector3();
   }
   // Three.js lack a distance to line function, so use the line segment function
   const lineLength = ray.distanceToPoint(pointOnLine) * 100;
   const v0 = pointOnLine.clone().addScaledVector(lineDirection, -lineLength);
   const v1 = pointOnLine.clone().addScaledVector(lineDirection, +lineLength);
-  ray.distanceSqToSegment(v0, v1, undefined, optionalClosestPointOnLine);
-  return optionalClosestPointOnLine;
+  ray.distanceSqToSegment(v0, v1, undefined, target);
+  return target;
 }
