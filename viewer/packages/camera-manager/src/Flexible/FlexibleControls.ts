30fecbe8767e35d2b85ca7b8580d807e571fd0a5
Nils Petter Fremming <nils.fremming@cognite.com>
2024-06-19 16:26:12 +0200

    Update FlexibleControls.ts

--- viewer/packages/camera-manager/src/Flexible/FlexibleControls.ts
+++ viewer/packages/camera-manager/src/Flexible/FlexibleControls.ts
@@ -975,9 +975,9 @@ export class FlexibleControls {
     if (!this.isStationary) {
       this.setControlsType(FlexibleControlsType.FirstPerson);
     }
-    const speedSpeed = this._options.getKeyboardSpeed(this._keyboard.isShiftPressed());
-    const speedXY = timeScale * speedSpeed * this._options.keyboardPanSpeed;
-    const speedZ = timeScale * speedSpeed * this._options.keyboardDollySpeed;
+    const keyboardSpeed = this._options.getKeyboardSpeed(this._keyboard.isShiftPressed());
+    const speedXY = timeScale * keyboardSpeed * this._options.keyboardPanSpeed;
+    const speedZ = timeScale * keyboardSpeed * this._options.keyboardDollySpeed;
 
     this.pan(speedXY * deltaX, speedXY * deltaY, speedZ * deltaZ, true);
     return true;
