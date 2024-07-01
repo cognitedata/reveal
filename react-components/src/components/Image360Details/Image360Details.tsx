a495cc7c72448cece7aabbd57e6a030b4e099a40
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2024-07-01 14:52:34 +0200

    fix(react-components): adopt Cogs chips and fix 360 image details panel appearance (#4651)
    * chore: adopt Cogs CounterChip in layers list
    
    * fix: make sure image details are always mounted properly
    
    * chore: use raw counter chip in details panel label

--- react-components/src/components/Image360Details/Image360Details.tsx
+++ react-components/src/components/Image360Details/Image360Details.tsx
@@ -8,6 +8,7 @@ import { Image360HistoricalDetails } from '../Image360HistoricalDetails/Image360
 import { type Image360 } from '@cognite/reveal';
 import { Button } from '@cognite/cogs.js';
 import { useReveal } from '../RevealCanvas/ViewerContext';
+import { useImage360Collections } from '../../hooks/useImage360Collections';
 
 type Image360DetailsProps = {
   appLanguage?: string;
@@ -21,15 +22,15 @@ export function Image360Details({ appLanguage }: Image360DetailsProps): ReactEle
     setIs360HistoricalPanelExpanded(isExpanded);
   }, []);
 
-  const clearEnteredImage360 = (): void => {
+  const clearEnteredImage360 = useCallback((): void => {
     setEnteredEntity(undefined);
-  };
+  }, [setEnteredEntity]);
 
-  const exitImage360Image = (): void => {
+  const exitImage360Image = useCallback((): void => {
     viewer.exit360Image();
-  };
+  }, [viewer]);
 
-  const collections = viewer.get360ImageCollections();
+  const collections = useImage360Collections();
 
   useEffect(() => {
     collections.forEach((collection) => {
@@ -42,7 +43,7 @@ export function Image360Details({ appLanguage }: Image360DetailsProps): ReactEle
         collection.off('image360Exited', clearEnteredImage360);
       });
     };
-  }, [viewer, collections]);
+  }, [viewer, collections, setEnteredEntity, clearEnteredImage360]);
 
   return (
     <>
