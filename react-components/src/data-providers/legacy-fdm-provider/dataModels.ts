061f52ced578ca1b9463731459f91be0cb56de8e
Håkon Flatval <hakon.flatval@cognite.com>
2024-07-25 14:01:00 +0200

    chore: remove unused code

--- react-components/src/data-providers/legacy-fdm-provider/dataModels.ts
+++ react-components/src/data-providers/legacy-fdm-provider/dataModels.ts
@@ -1,23 +1,12 @@
 /*!
  * Copyright 2023 Cognite AS
  */
-import { type DmsUniqueIdentifier, type Source } from '../FdmSDK';
+import { type Source } from '../FdmSDK';
 
 export const SYSTEM_SPACE_3D_SCHEMA = 'cdf_3d_schema'; // Data model, views, containers and types for edges
 
 export const SYSTEM_SPACE_3D_MODEL_ID = 'Cdf3dModel';
 export const SYSTEM_SPACE_3D_MODEL_VERSION = '1';
-
-export const SYSTEM_3D_NODE_TYPE: DmsUniqueIdentifier = {
-  externalId: 'Cdf3dEntity',
-  space: SYSTEM_SPACE_3D_SCHEMA
-};
-
-// Type of edge that connects equipment to 3D
-export const SYSTEM_3D_EDGE_TYPE: DmsUniqueIdentifier = {
-  externalId: 'cdf3dEntityConnection',
-  space: SYSTEM_SPACE_3D_SCHEMA
-};
 
 // Source of view that contains edge properties
 export const SYSTEM_3D_EDGE_SOURCE: Source = {
