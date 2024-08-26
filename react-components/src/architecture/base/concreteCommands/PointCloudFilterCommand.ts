99077dbba9d360cde98c61b37156a190beb8c79d
Håkon Flatval <hakon.flatval@cognite.com>
2024-08-26 14:53:29 +0200

    chore: export classes to fix Fusion jest

--- react-components/src/architecture/base/concreteCommands/PointCloudFilterCommand.ts
+++ react-components/src/architecture/base/concreteCommands/PointCloudFilterCommand.ts
@@ -105,7 +105,7 @@ export class PointCloudFilterCommand extends BaseFilterCommand {
 
 // Note: This is not exported, as it is only used internally
 
-class FilterItemCommand extends BaseFilterItemCommand {
+export class FilterItemCommand extends BaseFilterItemCommand {
   private readonly _modelId: number;
   private readonly _revisionId: number;
   private readonly _pointClass: PointClass;
@@ -174,7 +174,7 @@ class FilterItemCommand extends BaseFilterItemCommand {
   }
 }
 
-class PointClass {
+export class PointClass {
   name: string;
   code: number | WellKnownAsprsPointClassCodes;
   color: Color;
