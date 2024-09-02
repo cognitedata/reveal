cc9b22c5794f8d9011676d3caf2a6f486547a8a5
Håkon Flatval <hakon.flatval@cognite.com>
2024-09-02 14:24:12 +0200

    chore: update format of revision external ID

--- react-components/src/data-providers/core-dm-provider/getCdfIdFromExternalId.ts
+++ react-components/src/data-providers/core-dm-provider/getCdfIdFromExternalId.ts
@@ -10,7 +10,6 @@ export function getModelIdFromExternalId(externalId: string): ModelId {
 }
 
 export function getRevisionIdFromExternalId(externalId: string): RevisionId {
-  // The externalId should be on the form `revision_${modelId}_${revisionId}`
-  const [_prefix, _modelId, revisionId] = externalId.split('_');
-  return Number(revisionId);
+  // The externalId should be on the form `cog_3d_revision_${revisionId}`
+  return Number(externalId.slice('cog_3d_revision_'.length));
 }
