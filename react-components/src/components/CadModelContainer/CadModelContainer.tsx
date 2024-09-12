ed9b4acc31a951a3df0816bc492c332701f4dc20
Håkon Flatval <hakon.flatval@cognite.com>
2024-09-12 15:39:05 +0200

    Revert "fix(react-components): update model when geometry filter changed"
    This reverts commit 5466ca5566d02f9047c15d2e05a766c39a58cfe5.

--- react-components/src/components/CadModelContainer/CadModelContainer.tsx
+++ react-components/src/components/CadModelContainer/CadModelContainer.tsx
@@ -70,7 +70,7 @@ export function CadModelContainer({
     addModelOptions: AddModelOptions,
     transform?: Matrix4
   ): Promise<CogniteCadModel> {
-    const cadModel = await viewer.addCadModel(addModelOptions);
+    const cadModel = await getOrAddModel();
 
     if (transform !== undefined) {
       cadModel.setModelTransformation(transform);
@@ -78,6 +78,19 @@ export function CadModelContainer({
     setModel(cadModel);
 
     return cadModel;
+
+    async function getOrAddModel(): Promise<CogniteCadModel> {
+      const viewerModel = viewer.models.find(
+        (model) =>
+          model.modelId === modelId &&
+          model.revisionId === revisionId &&
+          model.getModelTransformation().equals(transform ?? new Matrix4())
+      );
+      if (viewerModel !== undefined) {
+        return await Promise.resolve(viewerModel as CogniteCadModel);
+      }
+      return await viewer.addCadModel(addModelOptions);
+    }
   }
 
   function removeModel(): void {
