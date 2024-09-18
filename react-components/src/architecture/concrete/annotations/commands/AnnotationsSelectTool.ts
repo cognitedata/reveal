28d1cdea91066d0cfe00f83c40bbe77c1180cc77
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-09-18 14:12:37 +0200

    Handle Exscat select tool + CMD on MacOs

--- react-components/src/architecture/concrete/annotations/commands/AnnotationsSelectTool.ts
+++ react-components/src/architecture/concrete/annotations/commands/AnnotationsSelectTool.ts
@@ -77,6 +77,9 @@ export class AnnotationsSelectTool extends BaseEditTool {
       }
       return;
     }
+    if (down && event.key === 'Escape') {
+      this.deselectedAnnotationInteractive();
+    }
     super.onKey(event, down);
   }
 
