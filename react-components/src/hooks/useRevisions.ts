42bd5c9e1d083e145db9b8eb41c54095e1ed6a6f
Daniel Priori <daniel.priori@cognite.com>
2024-09-23 11:25:46 +0200

    fix query keys and return the query parameters to the default

--- react-components/src/hooks/useRevisions.ts
+++ react-components/src/hooks/useRevisions.ts
@@ -7,21 +7,16 @@ import { type Model3D, type CogniteClient } from '@cognite/sdk';
 import { type ModelWithRevision } from './types';
 import { getRevisions } from './network/getRevisions';
 
-const STALE_TIME = 10 * 1000;
-const REFRESH_INTERVAL = 10 * 1000;
-
 export function useRevisions(
   sdk: CogniteClient,
   models: Model3D[] | undefined
 ): UseQueryResult<ModelWithRevision[]> {
   return useQuery({
-    queryKey: ['model-revision', models],
+    queryKey: ['model-revision', models?.map((model) => model.id)],
     queryFn: async () => {
       const fetchPromises = models?.map(async (model) => await getRevisions(model, sdk));
       return await Promise.all(fetchPromises ?? []);
     },
-    staleTime: STALE_TIME,
-    refetchInterval: REFRESH_INTERVAL,
     enabled: models !== undefined && models.length > 0
   });
 }
