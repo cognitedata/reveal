1c9ab8fb266ef1f4ea3322ca9e85db4f1fe2c830
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-10-02 14:08:20 +0200

    Add all annotation code

---
+++ react-components/src/architecture/concrete/annotations/commands/AnnotationsShowOnTopCommand.ts
@@ -0,0 +1,28 @@
+/*!
+ * Copyright 2024 Cognite AS
+ */
+
+import { ShowDomainObjectsOnTopCommand } from '../../../base/commands/ShowDomainObjectsOnTopCommand';
+import { type DomainObject } from '../../../base/domainObjects/DomainObject';
+import { type TranslateKey } from '../../../base/utilities/TranslateKey';
+import { AnnotationsDomainObject } from '../AnnotationsDomainObject';
+import { BoxGizmoDomainObject } from '../BoxGizmoDomainObject';
+import { CylinderGizmoDomainObject } from '../CylinderGizmoDomainObject';
+
+export class AnnotationsShowOnTopCommand extends ShowDomainObjectsOnTopCommand {
+  // ==================================================
+  // OVERRIDES
+  // ==================================================
+
+  public override get tooltip(): TranslateKey {
+    return { key: 'ANNOTATIONS_SHOW_ON_TOP', fallback: 'Show all annotations on top.' };
+  }
+
+  protected override isInstance(domainObject: DomainObject): boolean {
+    return (
+      domainObject instanceof AnnotationsDomainObject ||
+      domainObject instanceof BoxGizmoDomainObject ||
+      domainObject instanceof CylinderGizmoDomainObject
+    );
+  }
+}
