1b933f138cbde0a9823029b5a10ff2a9751c6fcc
Håkon Flatval <hakon.flatval@cognite.com>
2024-10-22 10:49:06 +0200

    feat(react-components): Add "expected resource count" hook

--- react-components/src/components/Reveal3DResources/Reveal3DResourcesInfoContext.tsx
+++ react-components/src/components/Reveal3DResources/Reveal3DResourcesInfoContext.tsx
@@ -8,19 +8,24 @@ import {
   useContext,
   useState,
   type ReactNode,
-  useMemo
+  useMemo,
+  Dispatch,
+  SetStateAction,
+  useEffect
 } from 'react';
 
 type Reveal3DResourcesInfoContent = {
   reveal3DResourcesCount: number;
   setRevealResourcesCount: (newCount: number) => void;
+  reveal3DResourcesExpectedToLoad: number;
+  setReveal3DResourcesExpectedToLoad: Dispatch<SetStateAction<number>>;
   model3DStylingLoading: boolean;
   setModel3DStylingLoading: (loading: boolean) => void;
 };
 
 const Reveal3DResourcesInfoContext = createContext<Reveal3DResourcesInfoContent | null>(null);
 
-const getInfoElementOfContext = (): Reveal3DResourcesInfoContent => {
+const useInfoElementOfContext = (): Reveal3DResourcesInfoContent => {
   const element = useContext(Reveal3DResourcesInfoContext);
   if (element === null) {
     throw new Error(
@@ -30,18 +35,36 @@ const getInfoElementOfContext = (): Reveal3DResourcesInfoContent => {
   return element;
 };
 
-export const useReveal3DResourcesCount = (): Reveal3DResourcesInfoContent => {
-  const element = getInfoElementOfContext();
-  return element;
+export const useReveal3DResourcesCount = (): Pick<
+  Reveal3DResourcesInfoContent,
+  'reveal3DResourcesCount' | 'setRevealResourcesCount'
+> => {
+  const element = useInfoElementOfContext();
+  return {
+    reveal3DResourcesCount: element.reveal3DResourcesCount,
+    setRevealResourcesCount: element.setRevealResourcesCount
+  };
+};
+
+export const useReveal3DResourcesExpectedToLoadCount = (): number => {
+  return useInfoElementOfContext().reveal3DResourcesExpectedToLoad;
+};
+
+export const useThisAsExpectedResourceLoad = (): void => {
+  const element = useInfoElementOfContext();
+  useEffect(() => {
+    element.setReveal3DResourcesExpectedToLoad((prev) => prev + 1);
+    return () => element.setReveal3DResourcesExpectedToLoad((prev) => prev - 1);
+  }, []);
 };
 
 export const useReveal3DResourcesStylingLoading = (): boolean => {
-  const element = getInfoElementOfContext();
+  const element = useInfoElementOfContext();
   return element.model3DStylingLoading;
 };
 
 export const useReveal3DResourcesStylingLoadingSetter = (): ((value: boolean) => void) => {
-  const element = getInfoElementOfContext();
+  const element = useInfoElementOfContext();
   return element.setModel3DStylingLoading;
 };
 
@@ -52,16 +75,21 @@ export const Reveal3DResourcesInfoContextProvider = ({
 }): ReactElement => {
   const [reveal3DResourcesCount, setRevealResourcesCount] = useState<number>(0);
   const [model3DStylingLoading, setModel3DStylingLoading] = useState<boolean>(false);
-  const memoedState = useMemo(
+  const [reveal3DResourcesExpectedToLoad, setReveal3DResourcesExpectedToLoad] = useState<number>(0);
+  const memoedState: Reveal3DResourcesInfoContent = useMemo(
     () => ({
       reveal3DResourcesCount,
       setRevealResourcesCount,
+      reveal3DResourcesExpectedToLoad,
+      setReveal3DResourcesExpectedToLoad,
       model3DStylingLoading,
       setModel3DStylingLoading
     }),
     [
       reveal3DResourcesCount,
       setRevealResourcesCount,
+      reveal3DResourcesExpectedToLoad,
+      setReveal3DResourcesExpectedToLoad,
       model3DStylingLoading,
       setModel3DStylingLoading
     ]
