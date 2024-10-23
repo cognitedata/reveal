dfb6058a35257443a8ac08735886f1c6ad76bcc1
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-10-23 11:35:19 +0200

    Update types.ts

--- react-components/src/architecture/base/treeView/types.ts
+++ react-components/src/architecture/base/treeView/types.ts
@@ -11,11 +11,20 @@ export enum CheckBoxState {
   Hidden
 }
 
+export type IconColor = string | undefined;
+
 export type TreeNodeAction = (node: ITreeNode) => void;
+
+/**
+ * This defines a type alias LoadNodesAction for a function. This type is likely used to represent asynchronous actions for loading nodes in
+ * the tree view.
+ * @param node - The parent or the sibling node to load siblings for.
+ * @param boolean - True of children should be loaded, false if the sibling
+ * @return A Promise that resolves to an array of ITreeNode objects or undefined.
+ * @beta
+ */
 
 export type LoadNodesAction = (
   node: ITreeNode,
   loadChildren: boolean
 ) => Promise<ITreeNode[] | undefined>;
-
-export type IconColor = string | undefined;
