d2d0904343d02e1a5e2d0634e1c1767bad7946a0
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-10-28 12:13:18 +0100

    Update Image360History.ts

--- viewer/packages/api/src/api-helpers/Image360History.ts
+++ viewer/packages/api/src/api-helpers/Image360History.ts
@@ -2,18 +2,18 @@
  * Copyright 2022 Cognite AS
  */
 
-import { Image360Entity } from '@reveal/360-images';
+import { Image360 } from '@reveal/360-images';
 import { Image360Action } from '../public/migration/Image360Action';
 
 export class Image360History {
-  private readonly _history: Image360Entity[] = [];
+  private readonly _history: Image360[] = [];
   private _currentIndex: number = -1; // Negative mean no current index (avoid empty because of harder logic)
 
   private isLegalIndex(index: number): boolean {
     return index >= 0 && index < this._history.length;
   }
 
-  public start(history: Image360Entity): void {
+  public start(history: Image360): void {
     if (this.isLegalIndex(this._currentIndex + 1)) {
       this._history.slice(this._currentIndex + 1);
     }
@@ -21,14 +21,14 @@ export class Image360History {
     this._currentIndex = this._history.length - 1;
   }
 
-  private current(): Image360Entity | undefined {
+  private current(): Image360 | undefined {
     if (!this.isLegalIndex(this._currentIndex)) {
       return undefined;
     }
     return this._history[this._currentIndex];
   }
 
-  private forward(): Image360Entity | undefined {
+  private forward(): Image360 | undefined {
     const currentIndex = this._currentIndex + 1;
     if (!this.isLegalIndex(currentIndex)) {
       return undefined;
@@ -37,7 +37,7 @@ export class Image360History {
     return this.current();
   }
 
-  private backward(): Image360Entity | undefined {
+  private backward(): Image360 | undefined {
     const currentIndex = this._currentIndex - 1;
     if (!this.isLegalIndex(currentIndex)) {
       return undefined;
@@ -64,7 +64,7 @@ export class Image360History {
     }
   }
 
-  public doAction(action: Image360Action): Image360Entity | undefined {
+  public doAction(action: Image360Action): Image360 | undefined {
     switch (action) {
       case Image360Action.Forward:
         return this.forward();
