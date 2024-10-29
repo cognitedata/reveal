f4a9d11ab2adffe5f103062fc135157b16307602
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-10-29 07:53:51 +0100

    Update Image360History.ts

--- viewer/packages/360-images/src/Image360History.ts
+++ viewer/packages/360-images/src/Image360History.ts
@@ -6,31 +6,34 @@ import { Image360 } from './entity/Image360';
 import { Image360Action } from './Image360Action';
 
 export class Image360History {
-  private readonly _history: Image360[] = [];
+  private readonly _images: Image360[] = [];
   private _currentIndex: number = -1; // Negative mean no current index (avoid empty because of harder logic)
 
-  public start(history: Image360): void {
+  public start(image: Image360): void {
     if (this.isLegalIndex(this._currentIndex + 1)) {
-      this._history.slice(this._currentIndex + 1);
+      this._images.slice(this._currentIndex + 1);
     }
-    this._history.push(history);
-    this._currentIndex = this._history.length - 1;
+    if (this.current() === image) {
+      return; // No need to add same history
+    }
+    this._images.push(image);
+    this._currentIndex = this._images.length - 1;
   }
 
   private current(): Image360 | undefined {
     if (!this.isLegalIndex(this._currentIndex)) {
       return undefined;
     }
-    return this._history[this._currentIndex];
+    return this._images[this._currentIndex];
   }
 
   public clear(): void {
-    this._history.splice(0, this._history.length);
+    this._images.splice(0, this._images.length);
     this._currentIndex = -1;
   }
 
   private isLegalIndex(index: number): boolean {
-    return index >= 0 && index < this._history.length;
+    return index >= 0 && index < this._images.length;
   }
 
   public canDoAction(action: Image360Action): boolean {
