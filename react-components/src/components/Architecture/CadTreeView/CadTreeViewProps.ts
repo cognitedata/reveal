081aaf65bf2c2f1906c2cce1048b1a69760c7191
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2024-11-07 08:48:48 +0100

    Update CadTreeViewProps.ts

--- react-components/src/components/Architecture/CadTreeView/CadTreeViewProps.ts
+++ react-components/src/components/Architecture/CadTreeView/CadTreeViewProps.ts
@@ -12,3 +12,17 @@ export type CadTreeViewProps = Omit<TreeViewProps, 'loadNodes' | 'root'> & {
   revisionId: number;
   onLoaded?: OnLoadedAction;
 };
+
+// NOTE: onLoaded() is called when node is loaded, but before is is added/inserted into the tree.
+// Here you can set the default `CheckBoxState `and other initialization. Typically:
+//
+// onLoaded(node: CadTreeNode, parent?: CadTreeNode): void
+// {
+//   if (parent === undefined) {
+//     return; // No parent if root, you have to figure out what to do here
+//   }
+//   node.checkBoxState = parent.checkBoxState;
+//   if (node.checkBoxState == CheckBoxState.All) {
+//     // Set the node visible in the viewer
+//   }
+// }
