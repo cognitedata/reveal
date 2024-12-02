c059fec5b60cac8df61e71653ba00963be7c7c48
Håkon Flatval <hakon.flatval@cognite.com>
2024-12-02 10:52:53 +0100

    feat: add event listener to InstanceStylingController

--- react-components/src/architecture/base/renderTarget/InstanceStylingController.ts
+++ react-components/src/architecture/base/renderTarget/InstanceStylingController.ts
@@ -2,21 +2,39 @@
  * Copyright 2024 Cognite AS
  */
 
+import { remove } from 'lodash';
 import { type InstanceStylingGroup } from '../../../components/Reveal3DResources/types';
 
 export class InstanceStylingController {
+  private readonly _eventHandlers: Array<() => void> = [];
+
   private readonly _stylingMap = new Map<symbol, InstanceStylingGroup>();
 
   setStylingGroup(symbol: symbol, group: InstanceStylingGroup | undefined): void {
     if (group === undefined) {
       this._stylingMap.delete(symbol);
+      this.fireChangeEvent();
       return;
     }
 
     this._stylingMap.set(symbol, group);
+    console.log('FIRE!');
+    this.fireChangeEvent();
   }
 
   getStylingGroups(): Iterable<InstanceStylingGroup> {
     return this._stylingMap.values();
+  }
+
+  addEventListener(eventHandler: () => void): void {
+    this._eventHandlers.push(eventHandler);
+  }
+
+  removeEventListener(eventHandler: () => void): boolean {
+    return remove(this._eventHandlers, eventHandler).length > 0;
+  }
+
+  private fireChangeEvent(): void {
+    this._eventHandlers.forEach((f) => f());
   }
 }
