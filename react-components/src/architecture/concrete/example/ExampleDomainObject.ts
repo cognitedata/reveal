e465c97681bf3f0ce2167652c0c4fbd84c196686
Pramod S <pramod.s@cognite.com>
2024-12-03 09:36:37 +0100

    fixed type error and added missing key

--- react-components/src/architecture/concrete/example/ExampleDomainObject.ts
+++ react-components/src/architecture/concrete/example/ExampleDomainObject.ts
@@ -71,7 +71,7 @@ export class ExampleDomainObject extends VisualDomainObject {
     const info = new PanelInfo();
     info.setHeader(this.typeName);
     // In production code, you should add a Key also!
-    add({ key: 'X_COORDINATE' }, this.center.x, Quantity.Length);
+    add({ key: 'X:COORDINATE' }, this.center.x, Quantity.Length);
     add({ key: 'Y_COORDINATE' }, this.center.y, Quantity.Length);
     add({ key: 'Z_COORDINATE' }, this.center.z, Quantity.Length);
     return info;
