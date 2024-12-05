cb289e994956008cdd8617cf140e37274a5e49f0
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2024-12-05 09:46:03 +0100

    feat(react-components): distinguish whether clicked node data is pending or empty, bump 0.73.6 (#4934)
    * fix(react-components): remove visibility check for poi section cmd
    
    * feat(react-components): return null on pending click result
    
    * chore: simplify normalization
    
    * chore: move inner function
    
    * chore: lint fix
    
    * chore: correct useClickedNodeData
    
    * chore: bump to 0.73.6

--- react-components/src/architecture/concrete/pointsOfInterest/index.ts
+++ react-components/src/architecture/concrete/pointsOfInterest/index.ts
@@ -2,4 +2,5 @@
  * Copyright 2024 Cognite AS
  */
 export { PointsOfInterestDomainObject } from './PointsOfInterestDomainObject';
+export { PointsOfInterestView } from './PointsOfInterestView';
 export { isPointsOfInterestIntersection, type PointOfInterest } from './types';
