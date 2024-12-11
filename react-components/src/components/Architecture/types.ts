8b2bf26bfa031944e07097762594bc5d4603b4c9
Håkon Flatval <hakon.flatval@cognite.com>
2024-12-11 15:19:58 +0100

    chore: extend type

--- react-components/src/components/Architecture/types.ts
+++ react-components/src/components/Architecture/types.ts
@@ -8,6 +8,6 @@ type Side = 'top' | 'right' | 'bottom' | 'left';
 
 type Alignment = 'start' | 'end';
 
-export type PlacementType = `${Side}-${Alignment}`;
+export type PlacementType = Side | `${Side}-${Alignment}`;
 
 export type FlexDirection = 'row' | 'column';
