f7a1387c38361f7389e0d81bbd792359c954f134
Daniel Priori <daniel.priori@cognite.com>
2025-02-10 14:02:57 -0300

    move filter to flatmap

--- react-components/src/components/RuleBasedOutputs/hooks/useExtractUniqueAssetIdsFromMapped.tsx
+++ react-components/src/components/RuleBasedOutputs/hooks/useExtractUniqueAssetIdsFromMapped.tsx
@@ -6,20 +6,21 @@ import { type InternalId } from '@cognite/sdk';
 import { type ModelWithAssetMappings } from '../../../hooks/cad/ModelWithAssetMappings';
 import { useMemo } from 'react';
 import { uniqBy } from 'lodash';
+import { isDefined } from '../../../utilities/isDefined';
 
 export const useExtractUniqueAssetIdsFromMapped = (
   assetMappings: ModelWithAssetMappings[] | undefined
 ): InternalId[] => {
   return useMemo(() => {
     const mappings = assetMappings?.map((item) => item.assetMappings).flat() ?? [];
-    const assetIds: InternalId[] = mappings.flatMap((item) => {
-      return {
-        id: item.assetId
-      };
-    });
-    const uniqueAssetIds = uniqBy(assetIds, (assetId) => assetId.id).filter(
-      (assetId) => assetId.id !== undefined
-    );
+    const assetIds: InternalId[] = mappings
+      .flatMap((item) => {
+        return {
+          id: item.assetId
+        };
+      })
+      .filter((assetId) => isDefined(assetId.id));
+    const uniqueAssetIds = uniqBy(assetIds, (assetId) => assetId.id);
     return uniqueAssetIds;
   }, [assetMappings]);
 };
