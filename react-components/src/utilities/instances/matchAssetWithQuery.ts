5daa468cc77c0b089f400c4ed8e83a59c889e1bf
Daniel Priori <daniel.priori@cognite.com>
2025-02-11 15:26:18 -0300

    cr remove unnecessary internal var

--- react-components/src/utilities/instances/matchAssetWithQuery.ts
+++ react-components/src/utilities/instances/matchAssetWithQuery.ts
@@ -6,10 +6,8 @@ import { isClassicAsset } from './typeGuards';
 
 export function matchAssetWithQuery(assetInstance: AssetInstance, query: string): boolean {
   if (isClassicAsset(assetInstance)) {
-    const instance = assetInstance;
-    const isInName = instance.name.toLowerCase().includes(query.toLowerCase());
-    const isInDescription = instance.description?.toLowerCase().includes(query.toLowerCase());
-
+    const isInName = assetInstance.name.toLowerCase().includes(query.toLowerCase());
+    const isInDescription = assetInstance.description?.toLowerCase().includes(query.toLowerCase());
     return isInName || isInDescription === true;
   } else {
     const isInName = assetInstance.properties.name.toLowerCase().includes(query.toLowerCase());
