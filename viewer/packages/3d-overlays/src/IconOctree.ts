b64c0a847052f004e2c5823f5d2a51b1d82e67b7
Håkon Flatval <hakon.flatval@cognite.com>
2025-02-14 09:47:39 +0100

    chore: use a predicate that actually works

--- viewer/packages/3d-overlays/src/IconOctree.ts
+++ viewer/packages/3d-overlays/src/IconOctree.ts
@@ -44,7 +44,7 @@ export class IconOctree<ContentType = DefaultOverlay3DContentType> extends Point
     const root = this.findNodesByLevel(0)[0];
     const selectedNodes = new Set<PointOctant<Overlay3DIcon>>();
 
-    if (root === undefined) {
+    if (!this._nodeCenters.has(root)) {
       return selectedNodes;
     }
 
