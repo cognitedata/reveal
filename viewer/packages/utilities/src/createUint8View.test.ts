eec901bfdceaf942ced3d1a88924a2961c2512e6
Håkon Flatval <hakon.flatval@cognite.com>
2025-02-14 12:08:41 +0100

    test: add test

---
+++ viewer/packages/utilities/src/createUint8View.test.ts
@@ -0,0 +1,18 @@
+import { createUint8View } from './createUint8View';
+
+describe(createUint8View.name, () => {
+  test('createUint8View creates a view of the buffer and not a copy', () => {
+    const arrayBuffer = new ArrayBuffer(8);
+    const arrayBufferView0 = new DataView(arrayBuffer);
+    const arrayBufferView1 = new DataView(arrayBuffer, 2);
+
+    const uint8View0 = createUint8View(arrayBufferView0);
+    const uint8View1 = createUint8View(arrayBufferView1);
+
+    uint8View0[2] = 0x42;
+    uint8View1[1] = 0x43;
+
+    expect(uint8View0[3]).toBe(0x43);
+    expect(uint8View1[0]).toBe(0x42);
+  });
+});
