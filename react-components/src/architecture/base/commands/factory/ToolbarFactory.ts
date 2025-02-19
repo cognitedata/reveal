5fd01d15486f4f46d689a7b0e13e0d7d7b7f56ef
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-02-19 18:51:00 +0100

    Initial commit

---
+++ react-components/src/architecture/base/commands/factory/ToolbarFactory.ts
@@ -0,0 +1,50 @@
+/*!
+ * Copyright 2025 Cognite AS
+ */
+
+import { type Class } from '../../domainObjectsHelpers/Class';
+import { type BaseCommand } from '../BaseCommand';
+import { type BaseTool } from '../BaseTool';
+
+type Toolbar = Array<BaseCommand | undefined>;
+
+/**
+ * Determines if a toolbar can be created for the given tool.
+ *
+ * @param tool - The tool to check.
+ * @returns `true` if a toolbar can be created, otherwise `false`.
+ */
+export function hasToolbar(tool: BaseTool): boolean {
+  const toolType = getClassOf(tool);
+  return _toolbar.has(toolType);
+}
+
+/**
+ * Creates a toolbar for the given tool.
+ *
+ * @param tool - The tool which the toolbar is to be created.
+ * @returns A toolbar if creation is successful, otherwise undefined.
+ */
+export function getToolbar(tool: BaseTool): Toolbar | undefined {
+  const toolType = getClassOf(tool);
+  return _toolbar.get(toolType);
+}
+
+/**
+ * Installs a toolbar creator for a given tool.
+ *
+ * @param toolType - The class of the tool for which the toolbar creator is being installed.
+ * @param creator - The toolbar creator to be installed.
+ */
+export function installToolbar(
+  toolType: Class<BaseTool>,
+  toolbar: Array<BaseCommand | undefined>
+): void {
+  _toolbar.set(toolType, toolbar);
+}
+
+const _toolbar = new Map<Class<BaseTool>, Toolbar>();
+
+function getClassOf(tool: BaseTool): Class<BaseTool> {
+  return tool.constructor as Class<BaseTool>;
+}
---
+++ react-components/src/architecture/installToolbars.ts
@@ -0,0 +1,83 @@
+/*!
+ * Copyright 2023 Cognite AS
+ */
+
+import { AlignSelectedAnnotationCommand } from './concrete/annotations/commands/AnnotationsAlignCommand';
+import { AnnotationsCreateMockCommand } from './concrete/annotations/commands/AnnotationsCreateMockCommand';
+import { AnnotationsCreateTool } from './concrete/annotations/commands/AnnotationsCreateTool';
+import { AnnotationsDeleteCommand } from './concrete/annotations/commands/AnnotationsDeleteCommand';
+import { AnnotationsSelectTool } from './concrete/annotations/commands/AnnotationsSelectTool';
+import { AnnotationsSetCreateTypeCommand } from './concrete/annotations/commands/AnnotationsSetCreateTypeCommand';
+import { ApplyClipCommand } from './concrete/clipping/commands/ApplyClipCommand';
+import { ClipTool } from './concrete/clipping/ClipTool';
+import { DeleteAllExamplesCommand } from './concrete/example/commands/DeleteAllExamplesCommand';
+import { ExampleTool } from './concrete/example/ExampleTool';
+import { installToolbar } from './base/commands/factory/ToolbarFactory';
+import { MeasurementTool } from './concrete/measurements/MeasurementTool';
+import { NextOrPrevClippingCommand } from './concrete/clipping/commands/NextClippingCommand';
+import { PrimitiveType } from './base/utilities/primitives/PrimitiveType';
+import { ResetAllExamplesCommand } from './concrete/example/commands/ResetAllExamplesCommand';
+import { SetClipTypeCommand } from './concrete/clipping/commands/SetClipTypeCommand';
+import { SetMeasurementTypeCommand } from './concrete/measurements/commands/SetMeasurementTypeCommand';
+import { ShowAllClippingCommand } from './concrete/clipping/commands/ShowAllClippingCommand';
+import { ShowAllExamplesCommand } from './concrete/example/commands/ShowAllExamplesCommand';
+import { ShowClippingOnTopCommand } from './concrete/clipping/commands/ShowClippingOnTopCommand';
+import { ShowExamplesOnTopCommand } from './concrete/example/commands/ShowExamplesOnTopCommand';
+import { ShowMeasurementsOnTopCommand } from './concrete/measurements/commands/ShowMeasurementsOnTopCommand';
+import { UndoCommand } from './base/concreteCommands/UndoCommand';
+
+export function installToolbars(): void {
+  installToolbar(AnnotationsCreateTool, [
+    new AnnotationsSelectTool(),
+    new AnnotationsSetCreateTypeCommand(PrimitiveType.Box),
+    new AnnotationsSetCreateTypeCommand(PrimitiveType.HorizontalCylinder),
+    new AnnotationsSetCreateTypeCommand(PrimitiveType.VerticalCylinder),
+    undefined,
+    new UndoCommand()
+  ]);
+
+  installToolbar(AnnotationsSelectTool, [
+    new AnnotationsCreateTool(),
+    new AnnotationsDeleteCommand(),
+    new AlignSelectedAnnotationCommand(true),
+    new AlignSelectedAnnotationCommand(false),
+    new AnnotationsCreateMockCommand(),
+    undefined,
+    new UndoCommand()
+  ]);
+
+  installToolbar(ClipTool, [
+    new SetClipTypeCommand(PrimitiveType.PlaneX),
+    new SetClipTypeCommand(PrimitiveType.PlaneY),
+    new SetClipTypeCommand(PrimitiveType.PlaneZ),
+    new SetClipTypeCommand(PrimitiveType.PlaneXY),
+    new SetClipTypeCommand(PrimitiveType.Box),
+    undefined, // Separator
+    new UndoCommand(),
+    new ApplyClipCommand(),
+    new NextOrPrevClippingCommand(false),
+    new NextOrPrevClippingCommand(true),
+    new ShowAllClippingCommand(),
+    new ShowClippingOnTopCommand()
+  ]);
+
+  installToolbar(ExampleTool, [
+    new UndoCommand(),
+    new ResetAllExamplesCommand(),
+    new ShowAllExamplesCommand(),
+    new DeleteAllExamplesCommand(),
+    new ShowExamplesOnTopCommand()
+  ]);
+
+  installToolbar(MeasurementTool, [
+    new SetMeasurementTypeCommand(PrimitiveType.Line),
+    new SetMeasurementTypeCommand(PrimitiveType.Polyline),
+    new SetMeasurementTypeCommand(PrimitiveType.Polygon),
+    new SetMeasurementTypeCommand(PrimitiveType.HorizontalArea),
+    new SetMeasurementTypeCommand(PrimitiveType.VerticalArea),
+    new SetMeasurementTypeCommand(PrimitiveType.Box),
+    undefined, // Separator
+    new UndoCommand(),
+    new ShowMeasurementsOnTopCommand()
+  ]);
+}
