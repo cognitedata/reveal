15a9c7b0c18219d3bea2dc0f04c2d40c13fed66c
Håkon Flatval <hakon.flatval@cognite.com>
2025-03-01 08:32:29 +0100

    chore: remove lint warnings

--- react-components/src/global.d.ts
+++ react-components/src/global.d.ts
@@ -6,6 +6,8 @@ import { type Cognite3DViewer, type DataSourceType } from '@cognite/reveal';
 import { type RevealRenderTarget } from './architecture';
 
 declare global {
+  /* eslint-disable no-var */
   var viewer: Cognite3DViewer<DataSourceType> | undefined;
+  /* eslint-disable no-var */
   var renderTarget: RevealRenderTarget | undefined;
 }
