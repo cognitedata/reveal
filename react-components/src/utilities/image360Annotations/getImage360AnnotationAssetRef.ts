e0e42c2835aa131b68075a00a0409cfd9f222671
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2025-03-03 18:55:12 +0100

    Update react-components/src/utilities/image360Annotations/getImage360AnnotationAssetRef.ts
    Co-authored-by: Fredrik Anfinsen <fredrik.anfinsen@gmail.com>

--- react-components/src/utilities/image360Annotations/getImage360AnnotationAssetRef.ts
+++ react-components/src/utilities/image360Annotations/getImage360AnnotationAssetRef.ts
@@ -8,11 +8,10 @@ import { type Image360AnnotationContent } from './types';
 export function getImage360AnnotationAssetRef(
   annotation: Image360AnnotationContent
 ): InstanceReference | undefined {
-  if (isClassicImage360Annotation(annotation)) {
-    if (isClassicImage360AssetAnnotationData(annotation.data)) {
-      return isIdEither(annotation.data.assetRef) ? annotation.data.assetRef : undefined;
-    }
-  } else {
+  if (!isClassicImage360Annotation(annotation)) {
     return annotation.assetRef;
+  }
+  if (isClassicImage360AssetAnnotationData(annotation.data)) {
+    return isIdEither(annotation.data.assetRef) ? annotation.data.assetRef : undefined;
   }
 }
