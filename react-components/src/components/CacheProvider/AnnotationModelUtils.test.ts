b406b86bba4740bfcd780c311b96d5e6f22443ea
Pramod S <pramod.s@cognite.com>
2025-03-04 12:05:49 +0100

    added a test case for multiple annotation with same asset mapping

--- react-components/src/components/CacheProvider/AnnotationModelUtils.test.ts
+++ react-components/src/components/CacheProvider/AnnotationModelUtils.test.ts
@@ -113,5 +113,48 @@ describe('AnnotationModelUtils', () => {
       expect(result.size).toBe(1);
       expect(result.get(1)).toEqual(assets[0]);
     });
+
+    test('should fetch asset for given point cloud annotations containing multiple annotation with same asset', async () => {
+      const annotations: PointCloudAnnotationModel[] = [
+        {
+          id: 1,
+          createdTime: new Date(),
+          lastUpdatedTime: new Date(),
+          status: 'approved',
+          annotatedResourceType: 'threedmodel',
+          annotatedResourceId: 1,
+          annotationType: 'pointCloud',
+          creatingApp: 'testApp',
+          creatingAppVersion: '1.0',
+          creatingUser: 'testUser',
+          data: {
+            assetRef: { id: 1 },
+            region: [{ box: { matrix: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1] } }]
+          }
+        },
+        {
+          id: 2,
+          createdTime: new Date(),
+          lastUpdatedTime: new Date(),
+          status: 'approved',
+          annotatedResourceType: 'threedmodel',
+          annotatedResourceId: 2,
+          annotationType: 'pointCloud',
+          creatingApp: 'testApp',
+          creatingAppVersion: '1.0',
+          creatingUser: 'testUser',
+          data: {
+            assetRef: { id: 1 },
+            region: [{ box: { matrix: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1] } }]
+          }
+        }
+      ];
+      retrieveMock.mockResolvedValueOnce([assets[0]]);
+
+      const result = await fetchPointCloudAnnotationAssets(annotations, sdkMock);
+
+      expect(result.size).toBe(2);
+      expect(result.get(1)).toEqual(assets[0]);
+    });
   });
 });
