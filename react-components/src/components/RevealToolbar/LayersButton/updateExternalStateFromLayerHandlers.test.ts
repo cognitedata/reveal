30721675e9d5e024cffa7c87b523f5ff5ec4427f
Håkon Flatval <hakon.flatval@cognite.com>
2025-03-10 08:22:09 +0100

    chore: lint fix

--- react-components/src/components/RevealToolbar/LayersButton/updateExternalStateFromLayerHandlers.test.ts
+++ react-components/src/components/RevealToolbar/LayersButton/updateExternalStateFromLayerHandlers.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, type Mock, test, vi } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { updateExternalStateFromLayerHandlers } from './updateExternalStateFromLayerHandlers';
 import {
   createCadHandlerMock,
