162b2c29f4ff8608abc952ebd35b1cb74b0e6365
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-03-12 08:33:07 +0100

    Initial commit

---
+++ react-components/src/architecture/concrete/primitives/box/BoxView.test.ts
@@ -0,0 +1,133 @@
+/*!
+ * Copyright 2025 Cognite AS
+ */
+
+import { beforeEach, describe, expect, test } from 'vitest';
+import { BoxView } from './BoxView';
+import { Object3D, Vector3 } from 'three';
+import { type BoxDomainObject } from './BoxDomainObject';
+import { FocusType } from '../../../base/domainObjectsHelpers/FocusType';
+import { Changes } from '../../../base/domainObjectsHelpers/Changes';
+import { DomainObjectChange } from '../../../base/domainObjectsHelpers/DomainObjectChange';
+import { CDF_TO_VIEWER_TRANSFORMATION, type CustomObjectIntersectInput } from '@cognite/reveal';
+import { expectEqualVector3 } from '../../../../../tests/tests-utilities/primitives/primitiveTestUtil';
+import {
+  addView,
+  createIntersectInput,
+  expectChildrenLength
+} from '../../../../../tests/tests-utilities/architecture/viewUtil';
+import { isDomainObjectIntersection } from '../../../base/domainObjectsHelpers/DomainObjectIntersection';
+import { PrimitivePickInfo } from '../common/PrimitivePickInfo';
+import { MeasureBoxDomainObject } from '../../measurements/MeasureBoxDomainObject';
+import { PrimitiveType } from '../../../base/utilities/primitives/PrimitiveType';
+import { BoxFace } from '../common/BoxFace';
+
+describe('BoxView', () => {
+  let domainObject: BoxDomainObject;
+  let view: BoxView;
+
+  beforeEach(() => {
+    domainObject = createBoxDomainObject();
+    view = new BoxView();
+    addView(domainObject, view);
+    expectChildrenLength(view, 2);
+  });
+
+  test('should have object', () => {
+    expect(view.object).toBeInstanceOf(Object3D);
+  });
+
+  test('should changed when focus change', () => {
+    const face = new BoxFace(1);
+
+    domainObject.setFocusInteractive(FocusType.Face, face);
+    expectChildrenLength(view, 10);
+
+    domainObject.setFocusInteractive(FocusType.Pending);
+    expectChildrenLength(view, 2);
+
+    domainObject.setFocusInteractive(FocusType.Rotation, face);
+    expectChildrenLength(view, 10);
+
+    domainObject.setFocusInteractive(FocusType.Body, face);
+    expectChildrenLength(view, 10);
+
+    domainObject.setFocusInteractive(FocusType.Focus);
+    expectChildrenLength(view, 10);
+
+    domainObject.setFocusInteractive(FocusType.None);
+    expectChildrenLength(view, 2);
+  });
+
+  test('should changed when render style change', () => {
+    domainObject.renderStyle.showLines = false;
+    view.update(new DomainObjectChange(Changes.renderStyle));
+    expectChildrenLength(view, 1);
+
+    domainObject.renderStyle.showLines = true;
+    view.update(new DomainObjectChange(Changes.renderStyle));
+    expectChildrenLength(view, 2);
+
+    domainObject.renderStyle.showSolid = false;
+    view.update(new DomainObjectChange(Changes.renderStyle));
+    expectChildrenLength(view, 1);
+
+    domainObject.renderStyle.showSolid = true;
+    view.update(new DomainObjectChange(Changes.renderStyle));
+    expectChildrenLength(view, 2);
+
+    domainObject.renderStyle.showLabel = false;
+    view.update(new DomainObjectChange(Changes.renderStyle));
+    expectChildrenLength(view, 2);
+  });
+
+  test('should changed when selection change', () => {
+    domainObject.isSelected = true;
+    view.update(new DomainObjectChange(Changes.selected));
+    expectChildrenLength(view, 2);
+  });
+
+  test('should intersect', () => {
+    const intersectInput = createLookingDownIntersectInput();
+    const intersection = view.intersectIfCloser(intersectInput, undefined);
+    expect(intersection).toBeDefined();
+    if (intersection === undefined) {
+      return;
+    }
+    intersection.point.applyMatrix4(CDF_TO_VIEWER_TRANSFORMATION.invert());
+    expectEqualVector3(intersection.point, new Vector3(0, 0, 1));
+    expect(intersection.customObject).toBe(view);
+    expect(intersection.distanceToCamera).toBe(1);
+    expect(intersection.userData).toBeInstanceOf(PrimitivePickInfo);
+
+    if (isDomainObjectIntersection(intersection)) {
+      expect(intersection.domainObject).toBe(domainObject);
+    }
+  });
+
+  test('should not intersect when point is hidden', () => {
+    const intersectInput = createLookingDownIntersectInput(false);
+    const intersection = view.intersectIfCloser(intersectInput, undefined);
+    expect(intersection).toBeUndefined();
+  });
+
+  test('should not intersect when distance is greater', () => {
+    const intersectInput = createLookingDownIntersectInput();
+    const intersection = view.intersectIfCloser(intersectInput, 0.5);
+    expect(intersection).toBeUndefined();
+  });
+});
+
+function createBoxDomainObject(): BoxDomainObject {
+  const domainObject = new MeasureBoxDomainObject(PrimitiveType.Box);
+  domainObject.box.size.set(2, 2, 2);
+  domainObject.box.center.set(0, 0, 0);
+  return domainObject;
+}
+
+function createLookingDownIntersectInput(isVisible = true): CustomObjectIntersectInput {
+  // Looking down towards center with distance 1
+  const origin = new Vector3(0, 0, 2);
+  const direction = new Vector3(0, 0, -1);
+  return createIntersectInput(origin, direction, isVisible);
+}
