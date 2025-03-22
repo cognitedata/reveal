27f0dd9c71346ced7984774333a8a2d58ac1a11b
Daniel Priori <daniel.priori@cognite.com>
2025-03-22 12:22:36 +0100

    lint fix

--- react-components/tests/tests-utilities/fixtures/dm/instanceFiltersForQueryPayload.ts
+++ react-components/tests/tests-utilities/fixtures/dm/instanceFiltersForQueryPayload.ts
@@ -1,16 +1,21 @@
 import { COGNITE_ASSET_SOURCE } from '../../../../src/data-providers/core-dm-provider/dataModels';
 import { simpleSourcesFixtures } from './sources';
 
-const existProperty1 = [ simpleSourcesFixtures[0].space, `${simpleSourcesFixtures[0].externalId}/${simpleSourcesFixtures[0].version}`, 'object3D' ];
-const existProperty2 = [ simpleSourcesFixtures[1].space, `${simpleSourcesFixtures[1].externalId}/${simpleSourcesFixtures[1].version}`, 'object3D' ];
+const existProperty1 = [
+  simpleSourcesFixtures[0].space,
+  `${simpleSourcesFixtures[0].externalId}/${simpleSourcesFixtures[0].version}`,
+  'object3D'
+];
+const existProperty2 = [
+  simpleSourcesFixtures[1].space,
+  `${simpleSourcesFixtures[1].externalId}/${simpleSourcesFixtures[1].version}`,
+  'object3D'
+];
 
 export const validInstanceFiltersForMappedAssetsQueryPayload = {
   and: [
     {
-      hasData: [
-        simpleSourcesFixtures[0],
-        simpleSourcesFixtures[1]
-      ]
+      hasData: [simpleSourcesFixtures[0], simpleSourcesFixtures[1]]
     },
     {
       exists: {
