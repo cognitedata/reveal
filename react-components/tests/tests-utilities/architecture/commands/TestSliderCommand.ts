1367aef9e2ff91e7ce5f36215dc1c45f7419fc8d
Håkon Flatval <hakon.flatval@cognite.com>
2025-04-02 16:26:05 +0200

    chore: add helpful names to slider constants

--- react-components/tests/tests-utilities/architecture/commands/TestSliderCommand.ts
+++ react-components/tests/tests-utilities/architecture/commands/TestSliderCommand.ts
@@ -1,10 +1,14 @@
 import { BaseSliderCommand } from '../../../../src/architecture';
 
+const TEST_SLIDER_MIN = 0;
+const TEST_SLIDER_MAX = 10;
+const TEST_SLIDER_STEP = 1;
+
 export class TestSliderCommand extends BaseSliderCommand {
   private _value: number;
 
   constructor() {
-    super(0, 10, 1);
+    super(TEST_SLIDER_MIN, TEST_SLIDER_MAX, TEST_SLIDER_STEP);
     this._value = 0;
   }
 
