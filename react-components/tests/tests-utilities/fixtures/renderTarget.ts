4f347bf1368a028892bedf049ea1acb663c8b70d
Håkon Flatval <hakon.flatval@cognite.com>
2025-04-03 13:25:42 +0200

    feat: create Quality slider command class

--- react-components/tests/tests-utilities/fixtures/renderTarget.ts
+++ react-components/tests/tests-utilities/fixtures/renderTarget.ts
@@ -9,6 +9,7 @@ import { fdmNodeCacheContentMock } from './fdmNodeCache';
 import { sdkMock } from './sdk';
 import { vi } from 'vitest';
 import { viewerMock } from './viewer';
+import { RevealSettingsController } from '../../../src/architecture/concrete/reveal/RevealSettingsController';
 
 const cdfCachesMock = new Mock<CdfCaches>()
   .setup((p) => p.fdmNodeCache)
@@ -35,7 +36,9 @@ export function createRenderTargetMock(): RevealRenderTarget {
     .setup((p) => p.commandsController)
     .returns(commandsControllerMock)
     .setup((p) => p.invalidate.bind(p))
-    .returns(vi.fn());
+    .returns(vi.fn())
+    .setup((p) => p.revealSettingsController)
+    .returns(new RevealSettingsController(viewerMock));
 
   const root = new RootDomainObject(mock.object(), sdkMock);
   mock.setup((p) => p.rootDomainObject).returns(root);
