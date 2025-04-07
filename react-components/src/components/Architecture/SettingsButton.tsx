3ba765a5c6c39536175f05d3cf812a865ba53431
Håkon Flatval <hakon.flatval@cognite.com>
2025-04-07 09:11:10 +0200

    chore: create a base class ("command") for Banner components

--- react-components/src/components/Architecture/SettingsButton.tsx
+++ react-components/src/components/Architecture/SettingsButton.tsx
@@ -34,6 +34,8 @@ import { DividerCommand } from '../../architecture/base/commands/DividerCommand'
 import { SectionCommand } from '../../architecture/base/commands/SectionCommand';
 import { useOnUpdate } from './useOnUpdate';
 import { FlexDirection, type PlacementType } from './types';
+import { BaseBannerCommand } from '../../architecture';
+import { BannerComponent } from './BannerComponent';
 
 export const SettingsButton = ({
   inputCommand,
@@ -118,6 +120,10 @@ function createMenuItem(command: BaseCommand, t: TranslateDelegate): ReactNode {
   if (command instanceof SectionCommand) {
     return createSection(command, t);
   }
+  if (command instanceof BaseBannerCommand) {
+    return <BannerComponent key={command.uniqueId} command={command} t={t} />;
+  }
+
   return createButton(command, t);
 }
 
