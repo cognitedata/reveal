64c7f14c887ea19784b3395f446e4f1803528e90
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-04-07 16:00:46 +0200

    Make the unit tests

---
+++ react-components/src/architecture/base/utilities/geometry/getCorners.test.ts
@@ -0,0 +1,28 @@
+/*!
+ * Copyright 2025 Cognite AS
+ */
+
+import { describe, expect, test } from 'vitest';
+
+import { getCorners } from './getCorners';
+import { Box3, Vector3 } from 'three';
+
+describe('getCorners', () => {
+  test('should test getCorners', () => {
+    const min = new Vector3(100, 200, 300);
+    const max = new Vector3(400, 600, 800);
+    const box = new Box3(min, max);
+    const corners = new Array<Vector3>();
+
+    // Check that each point is in the corner
+    for (const cornerPoint of getCorners(box)) {
+      expect(cornerPoint.x === min.x || cornerPoint.x === max.x).toBe(true);
+      expect(cornerPoint.y === min.y || cornerPoint.y === max.y).toBe(true);
+      expect(cornerPoint.z === min.z || cornerPoint.z === max.z).toBe(true);
+      corners.push(cornerPoint.clone());
+    }
+    // Check that all points are unique
+    const uniqueCorners = [...new Set(corners)];
+    expect(uniqueCorners.length).toBe(8);
+  });
+});
