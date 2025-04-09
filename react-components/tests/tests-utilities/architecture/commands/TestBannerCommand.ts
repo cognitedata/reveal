3250af7e793757ae4707cf2268173c9fee7d194f
Håkon Flatval <hakon.flatval@cognite.com>
2025-04-09 15:12:39 +0200

    chore: add test for BannerComponent

---
+++ react-components/tests/tests-utilities/architecture/commands/TestBannerCommand.ts
@@ -0,0 +1,21 @@
+import { BaseBannerCommand, TranslationInput } from '../../../../src/architecture';
+import { BannerStatus } from '../../../../src/architecture/base/commands/BaseBannerCommand';
+
+export class TestBannerCommand extends BaseBannerCommand {
+  private readonly _content: TranslationInput;
+  private readonly _status: BannerStatus;
+
+  constructor(properties?: { content?: TranslationInput; status?: BannerStatus }) {
+    super();
+    this._content = properties?.content ?? { untranslated: '' };
+    this._status = properties?.status ?? super.status;
+  }
+
+  public override get status(): BannerStatus {
+    return this._status;
+  }
+
+  public override get content(): TranslationInput {
+    return this._content;
+  }
+}
