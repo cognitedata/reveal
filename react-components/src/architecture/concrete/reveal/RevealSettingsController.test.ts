8375d85eca67b982e8c64c0c455f591d8ca7f87f
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2025-04-10 12:42:43 +0200

    feat(react-components): reveal settings controller (BND3D-5504 season 2, part 1) (#5084)
    * refactor: move QualitySettings to its own file
    
    * refactor: create RevealSettingsController for centralized viewer control
    
    * chore: lint fix
    
    * chore: fix storybook import
    
    * chore: rename arguments to something more informative
    
    * chore: use mock function directly in test
    
    * chore: lint fix
    
    * feat(react-components): Render quality slider (BND3D-5504 season 2, part 2) (#5085)
    
    * chore: add `marks` property on BaseSliderCommand
    
    * feat: create Quality slider command class
    
    * chore: use lodash's clamp function

---
+++ react-components/src/architecture/concrete/reveal/RevealSettingsController.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from 'vitest';
+import { RevealSettingsController } from './RevealSettingsController';
+import {
+  viewerMock,
+  viewerSetCadModelBudgetMock,
+  viewerSetPointCloudModelBudgetMock,
+  viewerSetResolutionOptionsMock
+} from '#test-utils/fixtures/viewer';
+
+describe(RevealSettingsController.name, () => {
+  test("it updates reveal's quality settings when changed", () => {
+    const settingsController = new RevealSettingsController(viewerMock);
+
+    const testQualitySettings = {
+      cadBudget: { maximumRenderCost: 1231232, highDetailProximityThreshold: 0 },
+      pointCloudBudget: { numberOfPoints: 123121212 },
+      resolutionOptions: { maxRenderResolution: 1.3e4, movingCameraResolutionFactor: 0.3 }
+    };
+
+    settingsController.renderQuality(testQualitySettings);
+
+    expect(viewerSetCadModelBudgetMock).toHaveBeenCalledWith(testQualitySettings.cadBudget);
+    expect(viewerSetPointCloudModelBudgetMock).toHaveBeenCalledWith(
+      testQualitySettings.pointCloudBudget
+    );
+    expect(viewerSetResolutionOptionsMock).toHaveBeenCalledWith(
+      testQualitySettings.resolutionOptions
+    );
+  });
+});
