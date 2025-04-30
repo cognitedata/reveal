f8f5b1408209d5365ca1206411e2b2cf032f0f31
Daniel Priori <daniel.priori@cognite.com>
2025-04-30 12:47:59 +0200

    cr: rewrite the comment

--- viewer/packages/camera-manager/src/Flexible/FlexibleCameraManager.ts
+++ viewer/packages/camera-manager/src/Flexible/FlexibleCameraManager.ts
@@ -108,9 +108,9 @@ export class FlexibleCameraManager extends PointerEvents implements IFlexibleCam
   }
 
   /**
-   * Sets camera state. All parameters are optional. If rotation and target are set at the same time
-   * then the target will be set separately as it may be out of the camera direction, such as using
-   * pivot points and then rotating the camera with the rotation value to point the camera direction.
+   * Sets camera state. All parameters are optional. If rotation and target are set at the same time,
+   * the target determines the position of the pivot point, while the rotation determines what
+   * direction the camera is facing.
    * @param state Camera state.
    * **/
   public setCameraState(state: CameraState): void {
