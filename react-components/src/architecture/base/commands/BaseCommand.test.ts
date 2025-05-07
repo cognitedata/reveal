3a3303d080d3685535d97aa70ab5d3f172fbe766
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-07 16:18:34 +0200

    Test CommandButton

---
+++ react-components/src/architecture/base/commands/BaseCommand.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, test } from 'vitest';
+import { MockCommand } from '#test-utils/architecture/mock-commands/MockCommand';
+import { BaseCommand } from './BaseCommand';
+import { Translator } from '../../../components/i18n/Translator';
+import { isUntranslatedString } from '../utilities/TranslateInput';
+
+// Help page here:  https://bogr.dev/blog/react-testing-intro/
+
+describe(BaseCommand.name, () => {
+  test('should invoke', async () => {
+    const command = new MockCommand();
+    expect(command.isInvokedTimes).toBe(0);
+
+    command.invoke();
+
+    expect(command.isInvokedTimes).toBe(1);
+  });
+
+  test('should check', async () => {
+    const command = new MockCommand();
+    command.isToggle = true;
+    expect(command.isChecked).toBe(false);
+    expect(command.isInvokedTimes).toBe(0);
+
+    command.invoke();
+
+    expect(command.isInvokedTimes).toBe(1);
+    expect(command.isChecked).toBe(true);
+  });
+
+  test('should get correct string for generating the tooltip', async () => {
+    const command = new MockCommand();
+    const label = command.getLabel(Translator.instance.translate);
+
+    if (isUntranslatedString(command.tooltip)) {
+      expect(label).toBe(command.tooltip.untranslated);
+    }
+    const keys = command.getShortCutKeys();
+    expect(keys).toHaveLength(4);
+    expect(keys).toBeOneOf([
+      ['Ctrl', 'Alt', 'Shift', 'A'],
+      ['Cmd', 'Alt', 'Shift', 'A']
+    ]);
+  });
+});
---
+++ react-components/src/components/Architecture/CommandButton.test.tsx
@@ -0,0 +1,115 @@
+import { afterEach, describe, expect, test } from 'vitest';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { type BaseCommand, RevealRenderTarget } from '../../architecture';
+import { type PropsWithChildren, type ReactElement } from 'react';
+import { viewerMock } from '#test-utils/fixtures/viewer';
+import { sdkMock } from '#test-utils/fixtures/sdk';
+import { ViewerContextProvider } from '../RevealCanvas/ViewerContext';
+import { Translator } from '../i18n/Translator';
+import { CommandButton } from './CommandButton';
+import { isUntranslatedString } from '../../architecture/base/utilities/TranslateInput';
+import { MockCommand } from '#test-utils/architecture/mock-commands/MockCommand';
+
+// Help page here:  https://bogr.dev/blog/react-testing-intro/
+
+describe(CommandButton.name, () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should render by default values', async () => {
+    const command = new MockCommand();
+    renderMe(command);
+
+    // Check button
+    const button = await screen.findByRole('button');
+    const label = command.getLabel(Translator.instance.translate);
+    expect(button.getAttribute('aria-disabled')).toBe('false');
+    expect(button.getAttribute('aria-label')).toBe(label);
+    expect(button.getAttribute('type')).toBe('button');
+
+    const buttonClass = button.getAttribute('class');
+    expect(buttonClass).not.contains('toggled');
+    expect(buttonClass).contains('type-ghost');
+    expect(buttonClass).contains('icon-only');
+    expect(buttonClass).contains('cogs-button');
+
+    // Check icon
+    const icon = await screen.findByLabelText(command.icon + 'Icon');
+    expect(icon.getAttribute('aria-label')).toBe(command.icon + 'Icon');
+    expect(icon.getAttribute('class')).contains('cogs-icon');
+  });
+
+  test('should invoke when clicked', async () => {
+    const command = new MockCommand();
+    renderMe(command);
+    const button = await screen.findByRole('button');
+    expect(command.isInvokedTimes).toBe(0);
+    fireEvent.click(button);
+    expect(command.isInvokedTimes).toBe(1);
+  });
+
+  test('should change from visible to invisible', async () => {
+    const command = new MockCommand();
+    renderMe(command);
+
+    const beforeButton = await screen.findByRole('button');
+    expect(beforeButton).not.toBeNull();
+
+    command.isVisible = false;
+
+    const afterButton = await screen.findByRole('button', { hidden: true });
+    expect(afterButton).not.toBeNull();
+  });
+
+  test('should change from enabled to disabled', async () => {
+    const command = new MockCommand();
+    renderMe(command);
+
+    const beforeButton = await screen.findByRole('button');
+    expect(beforeButton.getAttribute('aria-disabled')).toBe('false');
+
+    command.isEnabled = false;
+
+    const afterButton = await screen.findByRole('button');
+    expect(afterButton.getAttribute('aria-disabled')).toBe('true');
+  });
+
+  test('should change from unchecked to checked', async () => {
+    const command = new MockCommand();
+    command.isToggle = true;
+    renderMe(command);
+
+    const beforeButton = await screen.findByRole('button');
+    expect(beforeButton.getAttribute('class')).not.contains('toggled');
+
+    fireEvent.click(beforeButton);
+
+    const afterButton = await screen.findByRole('button');
+    expect(afterButton.getAttribute('class')).contains('toggled');
+  });
+
+  test('should change icon', async () => {
+    const command = new MockCommand();
+    renderMe(command);
+
+    const beforeIcon = await screen.findByLabelText(command.icon + 'Icon');
+    expect(beforeIcon).not.toBeNull();
+
+    command.icon = 'Snow';
+
+    const afterIcon = await screen.findByLabelText('SnowIcon');
+    expect(afterIcon).not.toBeNull();
+  });
+});
+
+function renderMe(command: BaseCommand): void {
+  const renderTargetMock = new RevealRenderTarget(viewerMock, sdkMock);
+
+  const wrapper = ({ children }: PropsWithChildren): ReactElement => (
+    <ViewerContextProvider value={renderTargetMock}>{children}</ViewerContextProvider>
+  );
+  render(<CommandButton inputCommand={command} placement={'top'} />, {
+    wrapper
+  });
+}
