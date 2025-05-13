447cc3a340b13c0ea09147909f9d5bd6adc17901
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-13 09:39:44 +0200

    Test on FitViewCommand

---
+++ react-components/src/architecture/base/concreteCommands/FitViewCommand.test.ts
@@ -0,0 +1,30 @@
+/*!
+ * Copyright 2025 Cognite AS
+ */
+import { beforeEach, describe, expect, test } from 'vitest';
+import { isEmpty } from '../utilities/TranslateInput';
+import { createFullRenderTargetMock } from '../../../../tests/tests-utilities/fixtures/createFullRenderTargetMock';
+import { FitViewCommand } from './FitViewCommand';
+import { fitCameraToBoundingBoxMock } from '../../../../tests/tests-utilities/fixtures/viewer';
+
+describe(FitViewCommand.name, () => {
+  let command: FitViewCommand;
+  const renderTarget = createFullRenderTargetMock();
+
+  beforeEach(() => {
+    command = new FitViewCommand();
+    command.attach(renderTarget);
+  });
+
+  test('Should have correct initial state', () => {
+    expect(command.icon).toBe('ExpandAlternative');
+    expect(isEmpty(command.tooltip)).toBe(false);
+    expect(command.isEnabled).toBe(true);
+  });
+
+  test('Should fit camera to bounding box', () => {
+    fitCameraToBoundingBoxMock.mockClear();
+    expect(command.invoke()).toBe(true);
+    expect(fitCameraToBoundingBoxMock).toHaveBeenCalledOnce();
+  });
+});
