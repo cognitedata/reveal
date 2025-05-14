a0abbd990fda87dec9859b50843acd1ccdfcb2d0
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-14 09:55:13 +0200

    Fix lint

--- react-components/src/architecture/base/concreteCommands/SetOrbitOrFirstPersonModeCommand.test.ts
+++ react-components/src/architecture/base/concreteCommands/SetOrbitOrFirstPersonModeCommand.test.ts
@@ -1,4 +1,4 @@
-import { assert, beforeEach, describe, expect, test, vi } from 'vitest';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import { isEmpty } from '../utilities/TranslateInput';
 import { createFullRenderTargetMock } from '../../../../tests/tests-utilities/fixtures/createFullRenderTargetMock';
 import { SetOrbitOrFirstPersonModeCommand } from './SetOrbitOrFirstPersonModeCommand';
