3138ab543edc6487e08f8f986ad3bfbba2c7799a
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-20 14:44:14 +0200

    Fix unit test

--- react-components/src/architecture/base/utilities/extensions/generatorUtils.test.ts
+++ react-components/src/architecture/base/utilities/extensions/generatorUtils.test.ts
@@ -3,32 +3,38 @@ import { count, first, last } from './generatorUtils';
 import { isOdd } from './mathExtensions';
 
 describe('generatorUtils', () => {
-  test('should correctly count the number of items in a generator', () => {
-    expect(count(getPositiveNumbers(0))).toBe(0);
-    expect(count(getPositiveNumbers(5))).toBe(5);
+  describe('first', () => {
+    test('should return undefined when no items in the generator', () => {
+      expect(first(getPositiveNumbers(0))).toBeUndefined();
+    });
+
+    test('should return the first item from a generator', () => {
+      expect(first(getPositiveNumbers(5))).toBe(0);
+    });
   });
 
-  test('should correctly count the odd number of items in a generator', () => {
-    expect(count(getPositiveNumbers(8), (a) => isOdd(a))).toBe(4);
-  });
+  describe('last', () => {
+    test('should return undefined when no items in the generator', () => {
+      expect(last(getPositiveNumbers(0))).toBeUndefined();
+    });
 
-  test('should return the first item from a generator', () => {
-    expect(first(getPositiveNumbers(0))).toBeUndefined();
-    expect(first(getPositiveNumbers(5))).toBe(0);
-  });
-
-  test('should return the last item from a generator', () => {
-    expect(last(getPositiveNumbers(0))).toBeUndefined();
-    expect(last(getPositiveNumbers(5))).toBe(4);
+    test('should return the last item from a generator', () => {
+      expect(last(getPositiveNumbers(5))).toBe(4);
+    });
   });
 
   describe('count', () => {
-    test('should count high numbers and find none', () => {
-      expect(count(getPositiveNumbers(6), (a) => a > 100)).toBe(0);
+    test('should correctly count the number of items in a generator', () => {
+      expect(count(getPositiveNumbers(0))).toBe(0);
+      expect(count(getPositiveNumbers(5))).toBe(5);
     });
 
-    test('should count odd numbers', () => {
-      expect(count(getPositiveNumbers(6), isOdd)).toBe(3);
+    test('should correctly count the odd number of items in a generator', () => {
+      expect(count(getPositiveNumbers(8), (a) => isOdd(a))).toBe(4);
+    });
+
+    test('should count high numbers and find none', () => {
+      expect(count(getPositiveNumbers(6), (a) => a > 100)).toBe(0);
     });
   });
 });
