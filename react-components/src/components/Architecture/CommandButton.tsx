c6d00c70648fd9cc5202be3d761054142430f5fb
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-20 15:49:28 +0200

    Make new hook

--- react-components/src/components/Architecture/CommandButton.tsx
+++ react-components/src/components/Architecture/CommandButton.tsx
@@ -2,18 +2,17 @@
  * Copyright 2024 Cognite AS
  */
 
-import { type ReactElement, useState, useMemo } from 'react';
+import { type ReactElement, useMemo } from 'react';
 import { useRenderTarget } from '../RevealCanvas/ViewerContext';
 import { Button, Tooltip as CogsTooltip } from '@cognite/cogs.js';
 import { useTranslation } from '../i18n/I18n';
 import { type BaseCommand } from '../../architecture/base/commands/BaseCommand';
 import { getButtonType, getDefaultCommand, getTooltipPlacement } from './utilities';
 import { LabelWithShortcut } from './LabelWithShortcut';
-import { type IconName } from '../../architecture/base/utilities/IconName';
 import { IconComponent } from './Factories/IconFactory';
-import { useOnUpdate } from './useOnUpdate';
 import { type PlacementType } from './types';
 import { TOOLTIP_DELAY } from './constants';
+import { useProperty } from './useProperty';
 
 export const CommandButton = ({
   inputCommand,
@@ -27,19 +26,11 @@ export const CommandButton = ({
   const command = useMemo<BaseCommand>(() => getDefaultCommand(inputCommand, renderTarget), []);
 
   // @update-ui-component-pattern
-  const [isChecked, setChecked] = useState(false);
-  const [isEnabled, setEnabled] = useState(true);
-  const [isVisible, setVisible] = useState(true);
-  const [uniqueId, setUniqueId] = useState(0);
-  const [icon, setIcon] = useState<IconName>(undefined);
-
-  useOnUpdate(command, () => {
-    setChecked(command.isChecked);
-    setEnabled(command.isEnabled);
-    setVisible(command.isVisible);
-    setUniqueId(command.uniqueId);
-    setIcon(command.icon);
-  });
+  const isChecked = useProperty(command, () => command.isChecked);
+  const isEnabled = useProperty(command, () => command.isEnabled);
+  const isVisible = useProperty(command, () => command.isVisible);
+  const icon = useProperty(command, () => command.icon);
+  const uniqueId = useProperty(command, () => command.uniqueId);
   // @end
 
   if (!isVisible) {
---
+++ react-components/src/components/Architecture/useProperty.tsx
@@ -0,0 +1,24 @@
+/*!
+ * Copyright 2024 Cognite AS
+ */
+
+import { useState, useEffect, useCallback } from 'react';
+import { type BaseCommand } from '../../architecture/base/commands/BaseCommand';
+
+export function useProperty<T>(command: BaseCommand, getValue: () => T): T {
+  const [value, setValue] = useState<T>(getValue);
+  const memoizedUpdate = useCallback(() => {
+    setValue(getValue());
+  }, [getValue]);
+  useEffect(() => {
+    if (command === undefined) {
+      return;
+    }
+    memoizedUpdate();
+    command.addEventListener(memoizedUpdate);
+    return () => {
+      command.removeEventListener(memoizedUpdate);
+    };
+  }, [command, memoizedUpdate]);
+  return value;
+}
