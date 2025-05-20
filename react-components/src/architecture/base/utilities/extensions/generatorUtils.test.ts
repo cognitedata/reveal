b6628e0970d81ec36276c70793980db4acc09946
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-20 13:53:53 +0200

    Add count functions

--- react-components/src/architecture/base/utilities/extensions/generatorUtils.test.ts
+++ react-components/src/architecture/base/utilities/extensions/generatorUtils.test.ts
@@ -1,10 +1,15 @@
 import { describe, expect, test } from 'vitest';
 import { count, first, last } from './generatorUtils';
+import { isOdd } from './mathExtensions';
 
 describe('generatorUtils', () => {
   test('should correctly count the number of items in a generator', () => {
     expect(count(getPositiveNumbers(0))).toBe(0);
     expect(count(getPositiveNumbers(5))).toBe(5);
+  });
+
+  test('should correctly count the odd number of items in a generator', () => {
+    expect(count(getPositiveNumbers(8), (a) => isOdd(a))).toBe(4);
   });
 
   test('should return the first item from a generator', () => {
@@ -15,6 +20,16 @@ describe('generatorUtils', () => {
   test('should return the last item from a generator', () => {
     expect(last(getPositiveNumbers(0))).toBeUndefined();
     expect(last(getPositiveNumbers(5))).toBe(4);
+  });
+
+  describe('count', () => {
+    test('should count high numbers and find none', () => {
+      expect(count(getPositiveNumbers(6), (a) => a > 100)).toBe(0);
+    });
+
+    test('should count odd numbers', () => {
+      expect(count(getPositiveNumbers(6), isOdd)).toBe(3);
+    });
   });
 });
 
