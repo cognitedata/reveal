b428caaa863adf58d437fa4f901d991e4def9e88
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-05-20 08:37:03 +0200

    test(react-components): Unit test for some commands (#5129)
    * Initial commit
    
    * Fix commands
    
    * More fixes
    
    * Add ToggleMetricUnitsCommand
    
    * Add undo command
    
    * Update UndoCommand.test.ts
    
    * Move code to separate file
    
    * Update ToggleMetricUnitsCommand.test.ts
    
    * Update react-components/src/architecture/base/concreteCommands/ToggleMetricUnitsCommand.test.ts
    
    Co-authored-by: pramod-cog <pramod.s@cognite.com>
    
    * Update react-components/src/architecture/base/utilities/extensions/generatorUtils.test.ts
    
    Co-authored-by: pramod-cog <pramod.s@cognite.com>
    
    * Update react-components/src/architecture/base/utilities/extensions/generatorUtils.test.ts
    
    Co-authored-by: pramod-cog <pramod.s@cognite.com>
    
    * Update react-components/src/architecture/base/utilities/extensions/generatorUtils.test.ts
    
    Co-authored-by: pramod-cog <pramod.s@cognite.com>
    
    * Update react-components/src/architecture/base/concreteCommands/DeleteDomainObjectCommand.test.ts
    
    Co-authored-by: anders-hopland <andershopl@hotmail.com>
    
    * Update ToggleMetricUnitsCommand.test.ts
    
    ---------
    
    Co-authored-by: pramod-cog <pramod.s@cognite.com>
    Co-authored-by: anders-hopland <andershopl@hotmail.com>

--- react-components/src/architecture/base/concreteCommands/ToggleMetricUnitsCommand.test.ts
+++ react-components/src/architecture/base/concreteCommands/ToggleMetricUnitsCommand.test.ts
@@ -1,27 +1,30 @@
-/*!
- * Copyright 2025 Cognite AS
- */
 import { beforeEach, describe, expect, test } from 'vitest';
 import { ToggleMetricUnitsCommand } from './ToggleMetricUnitsCommand';
 import { isEmpty } from '../utilities/TranslateInput';
+import { type RevealRenderTarget } from '../renderTarget/RevealRenderTarget';
+import { Changes } from '../domainObjectsHelpers/Changes';
+import { EventChangeTester } from '#test-utils/architecture/EventChangeTester';
 import { createFullRenderTargetMock } from '#test-utils/fixtures/createFullRenderTargetMock';
 
 describe(ToggleMetricUnitsCommand.name, () => {
+  let renderTarget: RevealRenderTarget;
   let command: ToggleMetricUnitsCommand;
-  const renderTarget = createFullRenderTargetMock();
 
   beforeEach(() => {
+    renderTarget = createFullRenderTargetMock();
     command = new ToggleMetricUnitsCommand();
     command.attach(renderTarget);
   });
 
-  test('Should have have icons, tooltip and toggle', () => {
-    expect(command.icon).not.toBe('');
+  test('Should have have tooltip, icon, toggle, checked and enable', async () => {
     expect(isEmpty(command.tooltip)).toBe(false);
+    expect(command.icon).toBe('RulerAlternative');
     expect(command.isToggle).toBe(true);
+    expect(command.isChecked).toBe(true);
+    expect(command.isEnabled).toBe(true);
   });
 
-  test('Should toggle between metric and ft', () => {
+  test('should switch from metric unit and imperial', async () => {
     // Check initial state
     expect(renderTarget.rootDomainObject.unitSystem.isMetric).toBe(true);
     expect(command.isChecked).toBe(true);
@@ -32,5 +35,18 @@ describe(ToggleMetricUnitsCommand.name, () => {
     // Check new state
     expect(command.isChecked).toBe(false);
     expect(renderTarget.rootDomainObject.unitSystem.isMetric).toBe(false);
+  });
+
+  test('should toggle between metric and imperial units and notify domain objects twice', async () => {
+    const tester = new EventChangeTester(renderTarget.rootDomainObject, Changes.unit);
+
+    expect(command.isChecked).toBe(true);
+    expect(command.invoke()).toBe(true);
+    expect(command.isChecked).toBe(false);
+    expect(command.invoke()).toBe(true);
+    expect(command.isChecked).toBe(true);
+
+    // Test that the unit system is notified
+    tester.toHaveBeenCalledTimes(2);
   });
 });
