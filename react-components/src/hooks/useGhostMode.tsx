f739f67b6cbefa723b40e516cb690cbcdf1859fe
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-21 11:10:20 +0200

    Update useGhostMode.tsx

--- react-components/src/hooks/useGhostMode.tsx
+++ react-components/src/hooks/useGhostMode.tsx
@@ -2,22 +2,16 @@
  * Copyright 2024 Cognite AS
  */
 
-import { useState } from 'react';
 import { useRenderTarget } from '../components/RevealCanvas';
-import { useOnUpdate } from '../components/Architecture/useOnUpdate';
 import { SetGhostModeCommand } from '../architecture/base/concreteCommands/cad/SetGhostModeCommand';
+import { useProperty } from '../components/Architecture/useProperty';
 
 export const useGhostMode = (): boolean => {
   // Hook to be used in Fusion only
-  const [isChecked, setChecked] = useState(false);
 
   const renderTarget = useRenderTarget();
   const command = renderTarget.commandsController.getCommandByTypeRecursive(SetGhostModeCommand);
-  useOnUpdate(command, () => {
-    if (command !== undefined) {
-      setChecked(command.isChecked);
-    }
-  });
+  const isChecked = useProperty(command, () => command !== undefined && command.isChecked);
   if (command === undefined) {
     return false;
   }
