d572683648ce4387bdd9337737be6db4b8445a91
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-22 13:21:32 +0200

    Add unit test

--- react-components/src/hooks/useGhostMode.tsx
+++ react-components/src/hooks/useGhostMode.tsx
@@ -8,12 +8,7 @@ import { useProperty } from '../components/Architecture/useProperty';
 
 export const useGhostMode = (): boolean => {
   // Hook to be used in Fusion only
-
   const renderTarget = useRenderTarget();
   const command = renderTarget.commandsController.getCommandByTypeRecursive(SetGhostModeCommand);
-  const isChecked = useProperty(command, () => command !== undefined && command.isChecked);
-  if (command === undefined) {
-    return false;
-  }
-  return isChecked;
+  return useProperty(command, () => command !== undefined && command.isChecked);
 };
