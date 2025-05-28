97d03bee4cb0a58e54fd5f486bdc1c1c2f0695ac
Nils Petter Fremming <nils.fremming@cognite.com>
2025-05-28 15:05:01 +0200

    Make test for DomainObjectPanel

---
+++ react-components/src/components/Architecture/DomainObjectPanel.test.tsx
@@ -0,0 +1,165 @@
+import { afterEach, describe, expect, test } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {
+  type BaseCommand,
+  CopyToClipboardCommand,
+  DeleteDomainObjectCommand,
+  DomainObject,
+  PanelInfo,
+  Quantity,
+  ToggleMetricUnitsCommand,
+  type TranslationInput
+} from '../../architecture';
+import { act, type PropsWithChildren, type ReactElement } from 'react';
+import { ViewerContextProvider } from '../RevealCanvas/ViewerContext';
+import { DomainObjectPanel } from './DomainObjectPanel';
+import { DomainObjectPanelUpdater } from '../../architecture/base/reactUpdaters/DomainObjectPanelUpdater';
+import { type IconName } from '../../architecture/base/utilities/IconName';
+import { createFullRenderTargetMock } from '../../../tests/tests-utilities/fixtures/createFullRenderTargetMock';
+
+// Help page here:  https://bogr.dev/blog/react-testing-intro/
+
+describe(DomainObjectPanel.name, () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should not be visible for no domain object', async () => {
+    render(<DomainObjectPanel />, {});
+    const buttons = screen.queryAllByRole('button');
+    expect(buttons.length).toBe(0);
+  });
+
+  test('should render with correct label and icon', async () => {
+    const domainObject = new MockDomainObject();
+    renderDomainObjectPanel(domainObject);
+    expect(screen.getByText(domainObject.label)).toBeDefined();
+    expect(screen.getByLabelText('SnowIcon')).toBeDefined();
+  });
+
+  test('should render with correct number of buttons', async () => {
+    const domainObject = new MockDomainObject();
+    renderDomainObjectPanel(domainObject);
+    const buttons = screen.getAllByRole('button');
+    const expectedButtonsLength = domainObject.getPanelToolbar().length;
+    expect(buttons.length).toBe(expectedButtonsLength);
+  });
+
+  test('should render with correct info text', async () => {
+    const domainObject = new MockDomainObject();
+    renderDomainObjectPanel(domainObject);
+    expect(screen.getByText(domainObject.label)).toBeDefined();
+    expect(screen.getByLabelText('SnowIcon')).toBeDefined();
+
+    for (const subString of EXPECTED_SUB_STRINGS) {
+      expect(screen.getByText(subString)).toBeDefined();
+    }
+  });
+
+  test('should copy text to clipboard when clicking at the copy button', async () => {
+    const domainObject = new MockDomainObject();
+    renderDomainObjectPanel(domainObject);
+    const buttons = screen.getAllByRole('button');
+    await act(async () => {
+      await userEvent.click(buttons[0]); // Copy button
+    });
+    const text = await navigator.clipboard.readText();
+    for (const subString of EXPECTED_SUB_STRINGS) {
+      expect(text).contain(subString);
+    }
+  });
+  test('should delete domain object when clicking at the delete button', async () => {
+    const domainObject = new MockDomainObject();
+    renderDomainObjectPanel(domainObject);
+    const beforeButtons = screen.getAllByRole('button');
+    await act(async () => {
+      await userEvent.click(beforeButtons[2]); // Delete button
+    });
+    const afterButtons = screen.queryAllByRole('button');
+    expect(afterButtons.length).toBe(0);
+  });
+
+  test('should hide the panel when the domain object is not selected', async () => {
+    const domainObject = new MockDomainObject();
+    renderDomainObjectPanel(domainObject);
+    const beforeButtons = screen.getAllByRole('button');
+    expect(beforeButtons.length).toBeGreaterThan(0);
+    await act(async () => {
+      domainObject.setSelectedInteractive(false);
+    });
+    const afterButtons = screen.queryAllByRole('button');
+    expect(afterButtons.length).toBe(0);
+  });
+});
+
+function renderDomainObjectPanel(domainObject: DomainObject | undefined): void {
+  const renderTarget = createFullRenderTargetMock();
+  if (domainObject !== undefined) {
+    renderTarget.rootDomainObject.addChild(domainObject);
+  }
+  if (domainObject !== undefined) {
+    domainObject.isSelected = true; // Select the domain object to show the panel
+    DomainObjectPanelUpdater.show(domainObject);
+  }
+  const wrapper = ({ children }: PropsWithChildren): ReactElement => (
+    <ViewerContextProvider value={renderTarget}>{children}</ViewerContextProvider>
+  );
+  render(<DomainObjectPanel />, {
+    wrapper
+  });
+}
+
+class MockDomainObject extends DomainObject {
+  public override get icon(): IconName {
+    return 'Snow';
+  }
+
+  public override get typeName(): TranslationInput {
+    return { untranslated: 'Mock' };
+  }
+
+  public override get hasPanelInfo(): boolean {
+    return true;
+  }
+
+  public override getPanelToolbar(): BaseCommand[] {
+    // to be overridden
+    return [
+      new CopyToClipboardCommand(),
+      new ToggleMetricUnitsCommand(),
+      new DeleteDomainObjectCommand(this)
+    ];
+  }
+
+  public override getPanelInfo(): PanelInfo | undefined {
+    const info = new PanelInfo();
+    add({ key: 'LENGTH' }, 1, Quantity.Length);
+    add({ key: 'AREA' }, 2, Quantity.Area);
+    add({ key: 'VOLUME' }, 3, Quantity.Volume);
+    add({ key: 'HORIZONTAL_ANGLE' }, 4, Quantity.Angle);
+    return info;
+
+    function add(translationInput: TranslationInput, value: number, quantity: Quantity): void {
+      info.add({ translationInput, value, quantity });
+    }
+  }
+}
+
+const EXPECTED_SUB_STRINGS: string[] = [
+  'Length',
+  '1.00',
+  'm',
+
+  'Area',
+  'm²',
+  '2.00',
+
+  'Volume',
+  'm³',
+  '3.00',
+
+  'Horizontal angle',
+  '4.0',
+  '°'
+];
