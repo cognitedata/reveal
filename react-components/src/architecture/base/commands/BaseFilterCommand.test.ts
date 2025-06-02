94d8bd5dede35b7177fc31e846a9f0c8de15ec47
Pramod S <pramod.s@cognite.com>
2025-06-02 13:01:35 +0200

    updated test file to use two test

--- react-components/src/architecture/base/commands/BaseFilterCommand.test.ts
+++ react-components/src/architecture/base/commands/BaseFilterCommand.test.ts
@@ -13,94 +13,95 @@ describe(BaseFilterCommand.name, () => {
     command.attach(createRenderTargetMock());
   });
 
-  test('should have icon', async () => {
+  test('should have icon', () => {
     expect(command.icon).toBe('Filter');
   });
 
-  test('should not have children when not initialized', async () => {
-    expect(command.children).toBeUndefined();
-    expect(command.hasChildren).toBe(false);
-    expect(command.isAllChecked).toBe(false);
-    expect(command.isSomeChecked).toBe(false);
-    expect(command.toggleAllChecked()).toBe(false);
-    expect(command.getSelectedLabel(translate)).toBe('None');
+  describe('when not initialized', () => {
+    test('should not have children', () => {
+      expect(command.children).toBeUndefined();
+      expect(command.hasChildren).toBe(false);
+      expect(command.isAllChecked).toBe(false);
+      expect(command.isSomeChecked).toBe(false);
+      expect(command.toggleAllChecked()).toBe(false);
+      expect(command.getSelectedLabel(translate)).toBe('None');
+    });
   });
 
-  test('should have children when initialized', async () => {
-    command.initializeChildrenIfNeeded();
-    assert(command.children !== undefined);
-    expect(command.children.length).toBeGreaterThan(2);
-    expect(command.hasChildren).toBe(true);
-  });
+  describe('when initialized', () => {
+    beforeEach(() => {
+      command.initializeChildrenIfNeeded();
+    });
 
-  test('should not have any checked', async () => {
-    command.initializeChildrenIfNeeded();
-    assert(command.children !== undefined);
-    for (const option of command.children) {
-      option.setChecked(false);
-    }
-    expect(command.isSomeChecked).toBe(false);
-    expect(command.isAllChecked).toBe(false);
-    expect(command.getSelectedLabel(translate)).toBe('None');
-  });
+    test('should have children', () => {
+      assert(command.children !== undefined);
+      expect(command.children.length).toBeGreaterThan(2);
+      expect(command.hasChildren).toBe(true);
+    });
 
-  test('should have some checked', async () => {
-    command.initializeChildrenIfNeeded();
-    assert(command.children !== undefined);
-    let childIndex = 0;
-    for (const option of command.children) {
-      option.setChecked(isOdd(childIndex));
-      childIndex++;
-    }
-    expect(command.isSomeChecked).toBe(true);
-    expect(command.isAllChecked).toBe(false);
-    expect(command.getSelectedLabel(translate)).toBe('7 Selected');
-  });
-
-  test('should have all checked', async () => {
-    command.initializeChildrenIfNeeded();
-    assert(command.children !== undefined);
-    for (const option of command.children) {
-      option.setChecked(true);
-    }
-    expect(command.isSomeChecked).toBe(true);
-    expect(command.isAllChecked).toBe(true);
-    expect(command.getSelectedLabel(translate)).toBe('All');
-  });
-
-  test('should toggle all checked', async () => {
-    command.initializeChildrenIfNeeded();
-    assert(command.children !== undefined);
-    for (const option of command.children) {
-      option.setChecked(false);
-    }
-    expect(command.toggleAllChecked()).toBe(true);
-    expect(command.isAllChecked).toBe(true);
-    expect(command.toggleAllChecked()).toBe(true);
-    expect(command.isSomeChecked).toBe(false);
-    expect(command.toggleAllChecked()).toBe(true);
-    expect(command.isAllChecked).toBe(true);
-  });
-
-  test('should have label and color at most options', async () => {
-    command.initializeChildrenIfNeeded();
-    assert(command.children !== undefined);
-    let childIndex = 0;
-    for (const option of command.children) {
-      expect(option.getLabel(translate)).not.toBe('');
-      if (childIndex < command.children.length - 1) {
-        expect(option.color).toBeDefined();
+    test('should not have any checked', () => {
+      assert(command.children !== undefined);
+      for (const option of command.children) {
+        option.setChecked(false);
       }
-      childIndex++;
-    }
-  });
-  test('should checked when invoke', async () => {
-    command.initializeChildrenIfNeeded();
-    assert(command.children !== undefined);
-    for (const option of command.children) {
-      option.setChecked(false);
-      expect(option.isChecked).toBe(false);
-      option.invoke();
-    }
+      expect(command.isSomeChecked).toBe(false);
+      expect(command.isAllChecked).toBe(false);
+      expect(command.getSelectedLabel(translate)).toBe('None');
+    });
+
+    test('should have some checked', () => {
+      assert(command.children !== undefined);
+      let childIndex = 0;
+      for (const option of command.children) {
+        option.setChecked(isOdd(childIndex));
+        childIndex++;
+      }
+      expect(command.isSomeChecked).toBe(true);
+      expect(command.isAllChecked).toBe(false);
+      expect(command.getSelectedLabel(translate)).toBe('7 Selected');
+    });
+
+    test('should have all checked', () => {
+      assert(command.children !== undefined);
+      for (const option of command.children) {
+        option.setChecked(true);
+      }
+      expect(command.isSomeChecked).toBe(true);
+      expect(command.isAllChecked).toBe(true);
+      expect(command.getSelectedLabel(translate)).toBe('All');
+    });
+
+    test('should toggle all checked', () => {
+      assert(command.children !== undefined);
+      for (const option of command.children) {
+        option.setChecked(false);
+      }
+      expect(command.toggleAllChecked()).toBe(true);
+      expect(command.isAllChecked).toBe(true);
+      expect(command.toggleAllChecked()).toBe(true);
+      expect(command.isSomeChecked).toBe(false);
+      expect(command.toggleAllChecked()).toBe(true);
+      expect(command.isAllChecked).toBe(true);
+    });
+
+    test('should have label and color at most options', () => {
+      assert(command.children !== undefined);
+      let childIndex = 0;
+      for (const option of command.children) {
+        expect(option.getLabel(translate)).not.toBe('');
+        if (childIndex < command.children.length - 1) {
+          expect(option.color).toBeDefined();
+        }
+        childIndex++;
+      }
+    });
+    test('should checked when invoke', () => {
+      assert(command.children !== undefined);
+      for (const option of command.children) {
+        option.setChecked(false);
+        expect(option.isChecked).toBe(false);
+        option.invoke();
+      }
+    });
   });
 });
