c135557ec55b6da11917adc12a506fd9f25dbfa5
Nils Petter Fremming <nils.fremming@cognite.com>
2025-06-04 13:04:33 +0200

    Move file

--- react-components/src/architecture/concrete/primitives/box/BoxDragger.test.ts
+++ react-components/src/architecture/concrete/primitives/box/BoxDragger.test.ts
@@ -1,25 +1,21 @@
 import { assert, describe, expect, test } from 'vitest';
 import { BoxFace } from '../common/BoxFace';
-import { Changes } from '../../../base/domainObjectsHelpers/Changes';
 import { BoxDragger } from './BoxDragger';
-import { EventChangeTester } from '../../../../../tests/tests-utilities/architecture/EventChangeTester';
-import {
-  expectEqualEuler,
-  expectEqualVector3
-} from '../../../../../tests/tests-utilities/primitives/primitiveTestUtil';
+import { expectEqualEuler, expectEqualVector3 } from '#test-utils/primitives/primitiveTestUtil';
 import { FocusType } from '../../../base/domainObjectsHelpers/FocusType';
 import { Matrix4, Ray, Vector3 } from 'three';
 import { MeasureBoxDomainObject } from '../../measurements/MeasureBoxDomainObject';
-import { Mock } from 'moq.ts';
-import { PrimitivePickInfo } from '../common/PrimitivePickInfo';
 import { PrimitiveType } from '../../../base/utilities/primitives/PrimitiveType';
-import { type BaseDragger } from '../../../base/domainObjectsHelpers/BaseDragger';
 import { type CreateDraggerProps } from '../../../base/domainObjects/VisualDomainObject';
 import { type BoxDomainObject } from './BoxDomainObject';
-import { type DomainObject } from '../../../base/domainObjects/DomainObject';
-import { type DomainObjectIntersection } from '../../../base/domainObjectsHelpers/DomainObjectIntersection';
-import { type ICustomObject } from '@cognite/reveal';
 import { degToRad } from 'three/src/math/MathUtils.js';
+import {
+  createIntersectionMock,
+  drag,
+  getTestCases,
+  getTestCasesWithSign,
+  type TestCase
+} from '#test-utils/architecture/baseDraggerUtil';
 
 describe(BoxDragger.name, () => {
   const focusType = FocusType.None;
@@ -36,13 +32,16 @@ describe(BoxDragger.name, () => {
     );
     expect(dragger).toBeInstanceOf(BoxDragger);
     assert(dragger !== undefined);
-    drag(dragger, startRay, delta, false, false, false);
+
+    const testCase = { expectedChange: false, shiftKey: false };
+    drag(dragger, startRay, delta, testCase, false);
     expect(domainObject.focusType).toBe(focusType);
   });
 
   test('translate the box', () => {
     const focusType = FocusType.Body;
-    for (const { expectedChange, shift, sign } of getSignedTestCases()) {
+    for (const testCase of getTestCasesWithSign()) {
+      const sign = testCase.sign;
       const domainObject = createBoxDomainObject();
 
       // Grab the box at top cap from above and move it in the XY plane
@@ -53,15 +52,15 @@ describe(BoxDragger.name, () => {
 
       const expectedSize = domainObject.box.size.clone();
       const expectedCenter = domainObject.box.center.clone();
-      if (expectedChange) {
-        delta.set(shift ? 0 : 1, 2, 0); // Shift only translate in dominate direction
+      if (testCase.expectedChange) {
+        delta.set(testCase.shiftKey ? 0 : 1, 2, 0); // Shift only translate in dominate direction
         expectedCenter.add(delta);
       }
       const dragger = domainObject.createDragger(
         createCreateDraggerPropsMock(domainObject, startRay, face, focusType)
       );
       assert(dragger !== undefined);
-      drag(dragger, startRay, delta, expectedChange, shift);
+      drag(dragger, startRay, delta, testCase);
 
       expectEqualVector3(domainObject.box.size, expectedSize);
       expectEqualVector3(domainObject.box.center, expectedCenter);
@@ -71,7 +70,8 @@ describe(BoxDragger.name, () => {
 
   test('move a face of the box', () => {
     const focusType = FocusType.Face;
-    for (const { expectedChange, shift, sign } of getSignedTestCases()) {
+    for (const testCase of getTestCasesWithSign()) {
+      const sign = testCase.sign;
       const domainObject = createBoxDomainObject();
       const face = new BoxFace(sign > 0 ? 2 : 5);
 
@@ -84,14 +84,14 @@ describe(BoxDragger.name, () => {
       );
       const expectedSize = domainObject.box.size.clone();
       const expectedCenter = domainObject.box.center.clone();
-      if (expectedChange) {
+      if (testCase.expectedChange) {
         delta.set(0, 0, sign);
         expectedSize.add(new Vector3(0, 0, 1));
         expectedCenter.addScaledVector(delta, 0.5);
       }
 
       assert(dragger !== undefined);
-      drag(dragger, startRay, delta, expectedChange, shift);
+      drag(dragger, startRay, delta, testCase);
 
       expectEqualVector3(domainObject.box.size, expectedSize);
       expectEqualVector3(domainObject.box.center, expectedCenter);
@@ -113,7 +113,7 @@ describe(BoxDragger.name, () => {
     //            | Dir = (0, 1, 0)
     // (-1,-2,-1) *
 
-    for (const { expectedChange, shift } of getTestCases()) {
+    for (const testCase of getTestCases()) {
       const domainObject = createBoxDomainObject();
 
       const direction = new Vector3(0, 1, 0); // Along the y axis
@@ -124,7 +124,7 @@ describe(BoxDragger.name, () => {
 
       const expectedSize = domainObject.box.size.clone();
       const expectedCenter = domainObject.box.center.clone();
-      if (expectedChange) {
+      if (testCase.expectedChange) {
         delta.set(-1, 0, -2);
         expectedSize.addScaledVector(delta, -1);
         expectedCenter.addScaledVector(delta, 0.5);
@@ -133,7 +133,7 @@ describe(BoxDragger.name, () => {
         createCreateDraggerPropsMock(domainObject, startRay, face, focusType, cornerSign)
       );
       assert(dragger !== undefined);
-      drag(dragger, startRay, delta, expectedChange, shift);
+      drag(dragger, startRay, delta, testCase);
 
       expectEqualVector3(domainObject.box.size, expectedSize);
       expectEqualVector3(domainObject.box.center, expectedCenter);
@@ -155,9 +155,9 @@ describe(BoxDragger.name, () => {
     // (-1,-2,-1) *
 
     const focusType = FocusType.Rotation;
-    for (const { expectedChange, shift, canRotate } of getTestCasesForRotation()) {
+    for (const params of getTestCasesWithCanRotate()) {
       const domainObject = createBoxDomainObject();
-      domainObject.canRotateComponent = (_component: number): boolean => canRotate;
+      domainObject.canRotateComponent = (_component: number): boolean => params.canRotate;
 
       const direction = new Vector3(0, 1, 0); // Along the y axis
       const startRay = new Ray(new Vector3(-1, -2, -1), direction); // See figure
@@ -168,17 +168,17 @@ describe(BoxDragger.name, () => {
       const expectedCenter = domainObject.box.center.clone();
       const expectedRotation = domainObject.box.rotation.clone();
 
-      if (expectedChange) {
+      if (params.expectedChange) {
         expectedRotation.y = degToRad(45);
         delta.set(0, 0, 1);
-      } else if (!canRotate) {
+      } else if (!params.canRotate) {
         delta.set(0, 0, 1); // To check if the rotation is not applied
       }
       const dragger = domainObject.createDragger(
         createCreateDraggerPropsMock(domainObject, startRay, face, focusType)
       );
       assert(dragger !== undefined);
-      drag(dragger, startRay, delta, expectedChange, shift);
+      drag(dragger, startRay, delta, params);
 
       expectEqualVector3(domainObject.box.size, expectedSize);
       expectEqualVector3(domainObject.box.center, expectedCenter);
@@ -187,49 +187,6 @@ describe(BoxDragger.name, () => {
     }
   });
 });
-
-function drag(
-  dragger: BaseDragger,
-  startRay: Ray,
-  delta: Vector3,
-  expectedChange: boolean,
-  shiftKey: boolean,
-  expectedFocusChange = false
-): void {
-  const draggerTester = new EventChangeTester(dragger.domainObject, Changes.dragging);
-  const focusTester = new EventChangeTester(dragger.domainObject, Changes.focus);
-
-  const endRay = new Ray(startRay.origin.clone().add(delta), startRay.direction.clone());
-  dragger.onPointerDown(new PointerEvent('pointerdown', { shiftKey }));
-  const actualChanged = dragger.onPointerDrag(
-    new PointerEvent('pointermove', { shiftKey }),
-    endRay
-  );
-  dragger.onPointerUp(new PointerEvent('pointermove', { shiftKey }));
-  expect(actualChanged).toBe(expectedChange);
-
-  // Test of notifications has happened
-  draggerTester.toHaveBeenCalledTimes(actualChanged ? 1 : 0);
-  if (expectedFocusChange) {
-    focusTester.toHaveBeenCalledTimes(1);
-  }
-}
-
-function createIntersectionMock(
-  domainObject: DomainObject,
-  face: BoxFace,
-  focusType: FocusType,
-  cornerSign?: Vector3
-): DomainObjectIntersection {
-  return {
-    type: 'customObject',
-    domainObject,
-    userData: new PrimitivePickInfo(face, focusType, cornerSign),
-    point: new Vector3(), // This is not used by the dragger
-    distanceToCamera: 0, // This is not used by the dragger
-    customObject: new Mock<ICustomObject>().object() // This is not used by the dragger
-  };
-}
 
 function createCreateDraggerPropsMock(
   domainObject: BoxDomainObject,
@@ -253,31 +210,9 @@ function createBoxDomainObject(): BoxDomainObject {
   return domainObject;
 }
 
-function* getSignedTestCases(): Generator<{
-  expectedChange: boolean;
-  shift: boolean;
-  sign: number;
-}> {
-  yield { expectedChange: true, shift: false, sign: 1 };
-  yield { expectedChange: true, shift: false, sign: -1 };
-  yield { expectedChange: true, shift: true, sign: 1 };
-  yield { expectedChange: true, shift: true, sign: -1 };
-  yield { expectedChange: false, shift: false, sign: 1 };
-}
-
-function* getTestCases(): Generator<{ expectedChange: boolean; shift: boolean }> {
-  yield { expectedChange: true, shift: false };
-  yield { expectedChange: true, shift: true };
-  yield { expectedChange: false, shift: false };
-}
-
-function* getTestCasesForRotation(): Generator<{
-  expectedChange: boolean;
-  shift: boolean;
-  canRotate: boolean;
-}> {
-  yield { expectedChange: true, shift: false, canRotate: true };
-  yield { expectedChange: true, shift: true, canRotate: true };
-  yield { expectedChange: false, shift: false, canRotate: true };
-  yield { expectedChange: false, shift: false, canRotate: false };
+function* getTestCasesWithCanRotate(): Generator<TestCase & { canRotate: boolean }> {
+  yield { expectedChange: true, shiftKey: false, canRotate: true };
+  yield { expectedChange: true, shiftKey: true, canRotate: true };
+  yield { expectedChange: false, shiftKey: false, canRotate: true };
+  yield { expectedChange: false, shiftKey: false, canRotate: false };
 }
---
+++ react-components/tests/tests-utilities/architecture/baseDraggerUtil.ts
@@ -0,0 +1,69 @@
+import { Changes } from '../../../src/architecture/base/domainObjectsHelpers/Changes';
+import { EventChangeTester } from './EventChangeTester';
+import { expect } from 'vitest';
+import { Mock } from 'moq.ts';
+import { PrimitivePickInfo } from '../../../src/architecture/concrete/primitives/common/PrimitivePickInfo';
+import { Ray, Vector3 } from 'three';
+import { type BaseDragger } from '../../../src/architecture/base/domainObjectsHelpers/BaseDragger';
+import { type BoxFace } from '../../../src/architecture/concrete/primitives/common/BoxFace';
+import { type DomainObject } from '../../../src/architecture/base/domainObjects/DomainObject';
+import { type DomainObjectIntersection } from '../../../src/architecture/base/domainObjectsHelpers/DomainObjectIntersection';
+import { type FocusType } from '../../../src/architecture/base/domainObjectsHelpers/FocusType';
+import { type ICustomObject } from '@cognite/reveal';
+
+export type TestCase = { expectedChange: boolean; shiftKey: boolean };
+
+export function* getTestCases(): Generator<TestCase> {
+  yield { expectedChange: true, shiftKey: false };
+  yield { expectedChange: true, shiftKey: true };
+  yield { expectedChange: false, shiftKey: false };
+}
+
+export function* getTestCasesWithSign(): Generator<TestCase & { sign: number }> {
+  yield { expectedChange: true, shiftKey: false, sign: 1 };
+  yield { expectedChange: true, shiftKey: false, sign: -1 };
+  yield { expectedChange: true, shiftKey: true, sign: 1 };
+  yield { expectedChange: true, shiftKey: true, sign: -1 };
+  yield { expectedChange: false, shiftKey: false, sign: 1 };
+}
+
+export function drag(
+  dragger: BaseDragger,
+  startRay: Ray,
+  delta: Vector3,
+  testCase: TestCase,
+  expectedFocusChange = true
+): void {
+  const draggerTester = new EventChangeTester(dragger.domainObject, Changes.dragging);
+  const focusTester = new EventChangeTester(dragger.domainObject, Changes.focus);
+  const { expectedChange, shiftKey } = testCase;
+
+  const endRay = new Ray(startRay.origin.clone().add(delta), startRay.direction.clone());
+  dragger.onPointerDown(new PointerEvent('pointerdown', { shiftKey }));
+  const actualChanged = dragger.onPointerDrag(
+    new PointerEvent('pointermove', { shiftKey }),
+    endRay
+  );
+  dragger.onPointerUp(new PointerEvent('pointermove', { shiftKey }));
+  expect(actualChanged).toBe(expectedChange);
+
+  // Test of notifications has happened
+  draggerTester.toHaveBeenCalledTimes(expectedChange ? 1 : 0);
+  focusTester.toHaveBeenCalledTimes(expectedFocusChange ? 1 : 0);
+}
+
+export function createIntersectionMock(
+  domainObject: DomainObject,
+  face: BoxFace,
+  focusType: FocusType,
+  cornerSign?: Vector3
+): DomainObjectIntersection {
+  return {
+    type: 'customObject',
+    domainObject,
+    userData: new PrimitivePickInfo(face, focusType, cornerSign),
+    point: new Vector3(), // This is not used by the dragger
+    distanceToCamera: 0, // This is not used by the dragger
+    customObject: new Mock<ICustomObject>().object() // This is not used by the dragger
+  };
+}
