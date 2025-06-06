33bf3d0d162dbc91885eccef1557b590ba8903ef
Håkon Flatval <hakon.flatval@cognite.com>
2025-06-06 10:35:30 +0200

    chore: chunk the asset fetching code

--- react-components/src/query/network/getAssetsFromAssetMappings.ts
+++ react-components/src/query/network/getAssetsFromAssetMappings.ts
@@ -1,27 +1,34 @@
 import { type AddModelOptions } from '@cognite/reveal';
 import { type AssetMapping3D, type CogniteClient, type ListResponse } from '@cognite/sdk';
 import { type ModelMappingsWithAssets } from '../useSearchMappedEquipmentAssetMappings';
+import { chunk } from 'lodash';
+
+const MODELS_MAPPING_CHUNK_SIZE = 5;
 
 export async function getAssetsFromAssetMappings(
   sdk: CogniteClient,
   modelsMappings: Array<{ model: AddModelOptions; mappings: ListResponse<AssetMapping3D[]> }>
 ): Promise<ModelMappingsWithAssets[]> {
-  const mappingsWithAssetsPromises = modelsMappings.map(async ({ mappings, model }) => {
-    if (mappings.items.length === 0) {
-      return { model, assets: [], mappings };
-    }
+  const mappingsWithAssets: ModelMappingsWithAssets[] = [];
 
-    const deduplicatedAssetIds = Array.from(
-      new Set(mappings.items.map((mapping) => mapping.assetId))
-    );
-    const assetIdObjects = deduplicatedAssetIds.map((id) => ({ id }));
+  for (const modelsMappingsChunk of chunk(modelsMappings, MODELS_MAPPING_CHUNK_SIZE)) {
+    const mappingsWithAssetsPromises = modelsMappingsChunk.map(async ({ mappings, model }) => {
+      if (mappings.items.length === 0) {
+        return { model, assets: [], mappings };
+      }
 
-    const assets = await sdk.assets.retrieve(assetIdObjects, { ignoreUnknownIds: true });
+      const deduplicatedAssetIds = Array.from(
+        new Set(mappings.items.map((mapping) => mapping.assetId))
+      );
+      const assetIdObjects = deduplicatedAssetIds.map((id) => ({ id }));
 
-    return { model, assets, mappings };
-  });
+      const assets = await sdk.assets.retrieve(assetIdObjects, { ignoreUnknownIds: true });
 
-  const mappingsWithAssets = await Promise.all(mappingsWithAssetsPromises);
+      return { model, assets, mappings };
+    });
+
+    mappingsWithAssets.concat(await Promise.all(mappingsWithAssetsPromises));
+  }
 
   return mappingsWithAssets;
 }
