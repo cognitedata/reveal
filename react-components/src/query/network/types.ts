1cce5b7a82105ee51c467fa11197a76e1dc17644
Håkon Flatval <hakon.flatval@cognite.com>
2025-06-17 15:53:08 +0200

    chore(react-components): add utilities for building filters

--- react-components/src/query/network/types.ts
+++ react-components/src/query/network/types.ts
@@ -1,6 +1,26 @@
-import { type Asset } from '@cognite/sdk';
+import { type AssetFilterProps, type Asset } from '@cognite/sdk';
 
 export type SearchClassicCadAssetsResponse = {
   nextCursor: string | undefined;
   data: Asset[];
 };
+
+export type AssetAdvancedFilterAnd = {
+  and: AssetAdvancedFilterProps[];
+};
+
+export type AssetAdvancedFilterOr = {
+  or: AssetAdvancedFilterProps[];
+};
+
+export type AssetAdvancedFilterLeaf = Record<string, unknown>;
+
+/**
+ * Advanced filters are not directly supported in the SDK, so
+ * we need to provide this type ourselves
+ */
+export type AssetAdvancedFilterProps =
+  | AssetAdvancedFilterAnd
+  | AssetAdvancedFilterOr
+  | AssetAdvancedFilterLeaf;
+
