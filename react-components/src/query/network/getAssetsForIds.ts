8b63f6cb918cc593baf290a34d1c35e30c6a098f
Håkon Flatval <hakon.flatval@cognite.com>
2025-06-19 13:36:24 +0200

    chore: use hasFilters in place of `filters === undefined`

--- react-components/src/query/network/getAssetsForIds.ts
+++ react-components/src/query/network/getAssetsForIds.ts
@@ -2,7 +2,7 @@ import type { Asset, CogniteClient, IdEither } from '@cognite/sdk';
 import { chunk } from 'lodash';
 import { buildClassicAssetIdFilter, combineClassicAssetFilters } from './buildClassicAssetFilter';
 import { getAssetsList } from '../../hooks/network/getAssetsList';
-import type { AllAssetFilterProps } from './filters';
+import { hasFilters, type AllAssetFilterProps } from './filters';
 
 const MAX_PARALLEL_ASSET_REQUESTS = 5;
 
@@ -18,7 +18,7 @@ export async function getAssetsForIds(
     return [];
   }
 
-  if (filters === undefined) {
+  if (hasFilters(filters)) {
     return await getAssetsForIdsWithoutFilter(assetRefs, sdk);
   }
 
