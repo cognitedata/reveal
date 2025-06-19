233bba7dbfaa10e6816a61a65907bb32d7180c58
Håkon Flatval <hakon.flatval@cognite.com>
2025-06-19 12:34:19 +0200

    test: test another test that exposes bug fix

--- react-components/src/query/usePointCloudModelRevisionIdsFromReveal.test.tsx
+++ react-components/src/query/usePointCloudModelRevisionIdsFromReveal.test.tsx
@@ -14,14 +14,25 @@ import { Mock } from 'moq.ts';
 import { type QueryRequest } from '@cognite/sdk';
 import { type FdmPropertyType } from '../components/Reveal3DResources/types';
 
+type RevisionDmsResponse = {
+  items: { revision: Array<FdmNode<FdmPropertyType<{ model3D: DmsUniqueIdentifier }>>> };
+};
+
+const TEST_MODEL_ID0 = 123;
+const TEST_MODEL_ID1 = 234;
+const TEST_REVISION_ID0 = 3456;
+const TEST_REVISION_ID1 = 5678;
+const TEST_MODEL_EXTERNAL_ID0 = createModelExternalId(TEST_MODEL_ID0);
+const TEST_MODEL_EXTERNAL_ID1 = createModelExternalId(TEST_MODEL_ID1);
+const TEST_REVISION_EXTERNAL_ID0 = `cog_3d_revision_${TEST_REVISION_ID0}`;
+const TEST_REVISION_EXTERNAL_ID1 = `cog_3d_revision_${TEST_REVISION_ID1}`;
+const TEST_SPACE = 'some-space';
+
 const mockUse3dModels = vi.fn<UsePointCloudModelRevisionIdsFromRevealDependencies['use3dModels']>();
 const mockUseFdmSdk = vi.fn<UsePointCloudModelRevisionIdsFromRevealDependencies['useFdmSdk']>();
 
-const mockFdmSdkQueryNodesAndEdgesMock = vi.fn<
-  (query: QueryRequest) => Promise<{
-    items: { revision: Array<FdmNode<FdmPropertyType<{ model3D: DmsUniqueIdentifier }>>> };
-  }>
->();
+const mockFdmSdkQueryNodesAndEdgesMock =
+  vi.fn<(query: QueryRequest) => Promise<RevisionDmsResponse>>();
 
 const queryClient = new QueryClient();
 
@@ -57,39 +68,20 @@ describe(usePointCloudModelRevisionIdsFromReveal.name, async () => {
   });
 
   test('returns the relevant revision ids', async () => {
-    const modelId = 123;
-    const revisionId = 345;
-    const testModelExternalId = `cog_3d_model_${modelId}`;
-    const testRevisionExternalId = `cog_3d_revision_${revisionId}`;
-    const testSpace = 'some-space';
-
     mockUse3dModels.mockReturnValue([
       createPointCloudDMMock({
-        revisionExternalId: testRevisionExternalId,
-        revisionSpace: testSpace
+        revisionExternalId: TEST_REVISION_EXTERNAL_ID0,
+        revisionSpace: TEST_SPACE
       })
     ]);
-    mockFdmSdkQueryNodesAndEdgesMock.mockResolvedValue({
-      items: {
-        revision: [
-          {
-            createdTime: 0,
-            lastUpdatedTime: 0,
-            externalId: testRevisionExternalId,
-            space: testSpace,
-            version: 1,
-            instanceType: 'node',
-            properties: {
-              cdf_cdm: {
-                'Cognite3DRevision/v1': {
-                  model3D: { externalId: testModelExternalId, space: testSpace }
-                }
-              }
-            }
-          }
-        ]
-      }
-    });
+    mockFdmSdkQueryNodesAndEdgesMock.mockResolvedValue(
+      createDmsPointcloudRevisionResponse(
+        TEST_MODEL_EXTERNAL_ID0,
+        TEST_REVISION_EXTERNAL_ID0,
+        TEST_SPACE
+      )
+    );
+
     mockUseFdmSdk.mockReturnValue(
       new Mock<FdmSDK>()
         .setup((p) => p.queryNodesAndEdges<QueryRequest>)
@@ -104,6 +96,113 @@ describe(usePointCloudModelRevisionIdsFromReveal.name, async () => {
     });
 
     expect(result.current.data).toHaveLength(1);
-    expect(result.current.data?.[0]).toEqual({ modelId, revisionId, type: 'pointcloud' });
+    expect(result.current.data?.[0]).toEqual({
+      modelId: TEST_MODEL_ID0,
+      revisionId: TEST_REVISION_ID0,
+      type: 'pointcloud'
+    });
+  });
+
+  test('returns the relevant revision ids for two different point clouds', async () => {
+    mockUse3dModels.mockReturnValue([
+      createPointCloudDMMock({
+        revisionExternalId: TEST_REVISION_EXTERNAL_ID0,
+        revisionSpace: TEST_SPACE
+      })
+    ]);
+
+    mockFdmSdkQueryNodesAndEdgesMock.mockResolvedValueOnce(
+      createDmsPointcloudRevisionResponse(
+        TEST_MODEL_EXTERNAL_ID0,
+        TEST_REVISION_EXTERNAL_ID0,
+        TEST_SPACE
+      )
+    );
+
+    mockUseFdmSdk.mockReturnValue(
+      new Mock<FdmSDK>()
+        .setup((p) => p.queryNodesAndEdges<QueryRequest>)
+        .returns(mockFdmSdkQueryNodesAndEdgesMock)
+        .object()
+    );
+
+    const { result, rerender } = renderHook(() => usePointCloudModelRevisionIdsFromReveal(), {
+      wrapper
+    });
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBeFalsy();
+    });
+
+    expect(result.current.data).toHaveLength(1);
+    expect(result.current.data?.[0]).toEqual({
+      modelId: TEST_MODEL_ID0,
+      revisionId: TEST_REVISION_ID0,
+      type: 'pointcloud'
+    });
+
+    mockUse3dModels.mockReturnValue([
+      createPointCloudDMMock({
+        revisionExternalId: TEST_REVISION_EXTERNAL_ID1,
+        revisionSpace: TEST_SPACE
+      })
+    ]);
+
+    mockFdmSdkQueryNodesAndEdgesMock.mockResolvedValueOnce(
+      createDmsPointcloudRevisionResponse(
+        TEST_MODEL_EXTERNAL_ID1,
+        TEST_REVISION_EXTERNAL_ID1,
+        TEST_SPACE
+      )
+    );
+
+    rerender();
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBeFalsy();
+    });
+
+    expect(result.current.data).toHaveLength(1);
+    expect(result.current.data?.[0]).toEqual({
+      modelId: TEST_MODEL_ID1,
+      revisionId: TEST_REVISION_ID1,
+      type: 'pointcloud'
+    });
   });
 });
+
+function createDmsPointcloudRevisionResponse(
+  modelExternalId: string,
+  revisionExternalId: string,
+  revisionSpace: string
+): RevisionDmsResponse {
+  return {
+    items: {
+      revision: [
+        {
+          createdTime: 0,
+          lastUpdatedTime: 0,
+          externalId: revisionExternalId,
+          space: revisionSpace,
+          version: 1,
+          instanceType: 'node',
+          properties: {
+            cdf_cdm: {
+              'Cognite3DRevision/v1': {
+                model3D: { externalId: modelExternalId, space: revisionSpace }
+              }
+            }
+          }
+        }
+      ]
+    }
+  };
+}
+
+function createRevisionExternalId(revisionId: number): string {
+  return `cog_3d_revision_${revisionId}`;
+}
+
+function createModelExternalId(modelId: number): string {
+  return `cog_3d_model_${modelId}`;
+}
