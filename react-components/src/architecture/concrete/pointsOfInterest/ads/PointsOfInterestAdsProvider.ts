de848c1b187b12665257657485016ff1bdad2c11
Christopher  Tannum <christopher.tannum@cognitedata.com>
2025-06-24 10:13:41 +0200

    fix: remove usage of uuid package in favor of web api

--- react-components/src/architecture/concrete/pointsOfInterest/ads/PointsOfInterestAdsProvider.ts
+++ react-components/src/architecture/concrete/pointsOfInterest/ads/PointsOfInterestAdsProvider.ts
@@ -3,7 +3,6 @@ import { type DmsUniqueIdentifier, type ExternalId } from '../../../../data-prov
 import { type CommentProperties, type PointsOfInterestInstance } from '../models';
 import { type PointsOfInterestProvider } from '../PointsOfInterestProvider';
 
-import { v4 as uuid } from 'uuid';
 import {
   type PoiExternalAssetRef,
   type PoiExternalDMRef,
@@ -155,7 +154,7 @@ export class PointsOfInterestAdsProvider implements PointsOfInterestProvider<Ext
   }
 
   public createNewId(): ExternalId {
-    return uuid();
+    return crypto.randomUUID();
   }
 }
 
