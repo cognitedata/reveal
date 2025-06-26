7a58d20f3e6e4410a9a19e7fe527d5fd02aa226e
Christopher J. Tannum <christopher.tannum@cognite.com>
2025-06-26 11:15:16 +0200

    fix: unmounting CadModelContainer does not properly remove cad model (#5197)
    * fix: unmounting CadModelContainer does not properly remove cad model
    
    * chore: update visual tests and types
    
    ---------
    
    Co-authored-by: Christopher  Tannum <christopher.tannum@cognitedata.com>

---
+++ react-components/src/components/CadModelContainer/CadModelContainer.context.ts
@@ -0,0 +1,29 @@
+import { createContext } from 'react';
+import { useRevealKeepAlive } from '../RevealKeepAlive/RevealKeepAliveContext';
+import { useRenderTarget } from '../RevealCanvas';
+import {
+  useReveal3DResourceLoadFailCount,
+  useReveal3DResourcesCount
+} from '../Reveal3DResources/Reveal3DResourcesInfoContext';
+import { useApplyCadModelStyling } from './useApplyCadModelStyling';
+import { RevealModelsUtils } from '../../architecture/concrete/reveal/RevealModelsUtils';
+
+export type CadModelContextDependencies = {
+  useRevealKeepAlive: typeof useRevealKeepAlive;
+  useRenderTarget: typeof useRenderTarget;
+  useReveal3DResourcesCount: typeof useReveal3DResourcesCount;
+  useReveal3DResourceLoadFailCount: typeof useReveal3DResourceLoadFailCount;
+  useApplyCadModelStyling: typeof useApplyCadModelStyling;
+  createCadDomainObject: typeof RevealModelsUtils.addModel;
+  removeCadDomainObject: typeof RevealModelsUtils.remove;
+};
+
+export const CadModelContext = createContext<CadModelContextDependencies>({
+  useRevealKeepAlive,
+  useRenderTarget,
+  useReveal3DResourcesCount,
+  useReveal3DResourceLoadFailCount,
+  useApplyCadModelStyling,
+  createCadDomainObject: RevealModelsUtils.addModel.bind(this),
+  removeCadDomainObject: RevealModelsUtils.remove.bind(this)
+});
