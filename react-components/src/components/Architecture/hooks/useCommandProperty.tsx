a11bca4d869cc1d54e716023216faf9d94ad7dd5
Håkon Flatval <hakon.flatval@cognite.com>
2025-07-01 14:39:44 +0200

    fix(react-components): ghost mode hook abuse

--- react-components/src/components/Architecture/hooks/useCommandProperty.tsx
+++ react-components/src/components/Architecture/hooks/useCommandProperty.tsx
@@ -1,6 +1,27 @@
 import { useState, useEffect, useCallback } from 'react';
 import { type BaseCommand } from '../../../architecture/base/commands/BaseCommand';
 
+export function useNullableCommandProperty<T>(
+  command: BaseCommand | undefined,
+  getValue: () => T | undefined
+): T | undefined {
+  const [value, setValue] = useState<T | undefined>(getValue);
+
+  const memoizedUpdate = useCallback(() => {
+    setValue(getValue());
+  }, [command]);
+
+  useEffect(() => {
+    memoizedUpdate();
+    command?.addEventListener(memoizedUpdate);
+    return () => {
+      command?.removeEventListener(memoizedUpdate);
+    };
+  }, [command, memoizedUpdate]);
+
+  return value;
+}
+
 export function useCommandProperty<T>(command: BaseCommand, getValue: () => T): T {
   const [value, setValue] = useState<T>(getValue);
 
