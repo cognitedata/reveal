57aec9fefd71d47dc425b91acf8466065f994091
Håkon Flatval <hakon.flatval@cognite.com>
2025-07-02 16:24:51 +0200

    chore: rewrite useVisibleRevealDomainObjects and add tests

---
+++ react-components/src/hooks/useVisibleRevealDomainObjects.test.tsx
@@ -0,0 +1,83 @@
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import {
+  useVisibleRevealDomainObjects,
+  UseVisibleRevealDomainObjectsContext,
+  UseVisibleRevealDomainObjectsDependencies
+} from './useVisibleRevealDomainObjects';
+import { CadDomainObject } from '../architecture/concrete/reveal/cad/CadDomainObject';
+import { createCadMock } from '#test-utils/fixtures/cadModel';
+import { PointCloudDomainObject } from '../architecture/concrete/reveal/pointCloud/PointCloudDomainObject';
+import { createPointCloudDMMock } from '#test-utils/fixtures/pointCloud';
+import { Image360CollectionDomainObject } from '../architecture/concrete/reveal/Image360Collection/Image360CollectionDomainObject';
+import { createImage360ClassicMock } from '#test-utils/fixtures/image360';
+import { createRenderTargetMock } from '#test-utils/fixtures/renderTarget';
+import { RevealRenderTarget } from '../architecture';
+import { act, renderHook } from '@testing-library/react';
+import type { PropsWithChildren, ReactElement } from 'react';
+import { RenderTarget } from 'three';
+import { RevealDomainObject } from '../architecture/concrete/reveal/RevealDomainObject';
+
+describe(useVisibleRevealDomainObjects.name, () => {
+  let renderTarget: RevealRenderTarget;
+
+  const wrapper = ({ children }: PropsWithChildren): ReactElement => (
+    <UseVisibleRevealDomainObjectsContext.Provider
+      value={{
+        useRenderTarget: () => renderTarget
+      }}>
+      {children}
+    </UseVisibleRevealDomainObjectsContext.Provider>
+  );
+
+  beforeEach(() => {
+    renderTarget = createRenderTargetMock();
+  });
+
+  test('returns only visible Reveal domain objects', () => {
+    const [domainObject0, domainObject1, domainObject2] =
+      populateRenderTargetAndGetModels(renderTarget);
+
+    domainObject0.setVisibleInteractive(true, renderTarget);
+    domainObject2.setVisibleInteractive(true, renderTarget);
+
+    const { result } = renderHook(() => useVisibleRevealDomainObjects(), { wrapper });
+
+    expect(result.current).toHaveLength(2);
+    expect(result.current[0]).toBe(domainObject0);
+    expect(result.current[1]).toBe(domainObject2);
+  });
+
+  test('returns new result when visibility state changes', () => {
+    const [domainObject0, domainObject1, domainObject2] =
+      populateRenderTargetAndGetModels(renderTarget);
+
+    domainObject0.setVisibleInteractive(true, renderTarget);
+    domainObject2.setVisibleInteractive(true, renderTarget);
+
+    const { result } = renderHook(() => useVisibleRevealDomainObjects(), { wrapper });
+
+    const initialResult = result.current;
+
+    act(() => {
+      domainObject0.setVisibleInteractive(false, renderTarget);
+      domainObject1.setVisibleInteractive(true, renderTarget);
+    });
+
+    expect(result.current).not.toBe(initialResult);
+    expect(result.current).toHaveLength(2);
+    expect(result.current[0]).toBe(domainObject1);
+    expect(result.current[1]).toBe(domainObject2);
+  });
+});
+
+function populateRenderTargetAndGetModels(renderTarget: RevealRenderTarget): RevealDomainObject[] {
+  const cadDomainObject = new CadDomainObject(createCadMock());
+  const pointCloudDomainObject = new PointCloudDomainObject(createPointCloudDMMock());
+  const image360DomainObject = new Image360CollectionDomainObject(createImage360ClassicMock());
+
+  renderTarget.rootDomainObject.addChildInteractive(cadDomainObject);
+  renderTarget.rootDomainObject.addChildInteractive(pointCloudDomainObject);
+  renderTarget.rootDomainObject.addChildInteractive(image360DomainObject);
+
+  return [cadDomainObject, pointCloudDomainObject, image360DomainObject];
+}
--- react-components/src/hooks/useVisibleRevealDomainObjects.ts
+++ react-components/src/hooks/useVisibleRevealDomainObjects.ts
@@ -1,18 +1,28 @@
-import { useMemo } from 'react';
+import { createContext, useContext, useMemo } from 'react';
 import { useRenderTarget } from '../components/RevealCanvas/ViewerContext';
-import { useRevealDomainObjects } from './useRevealDomainObjects';
+import { useDisposableSignal } from '../utilities/signal/useDisposableSignal';
+import { getRevealDomainUpdateSignal } from '../architecture/concrete/reveal/signal/getRevealDomainObjectsSignal';
+import { type RevealDomainObject } from '../architecture/concrete/reveal/RevealDomainObject';
 import { Changes, type DomainObject } from '../architecture';
-import type { RevealDomainObject } from '../architecture/concrete/reveal/RevealDomainObject';
+
+export type UseVisibleRevealDomainObjectsDependencies = {
+  useRenderTarget: typeof useRenderTarget;
+};
+
+export const UseVisibleRevealDomainObjectsContext = createContext({
+  useRenderTarget
+});
 
 export function useVisibleRevealDomainObjects(): RevealDomainObject[] {
+  const { useRenderTarget } = useContext(UseVisibleRevealDomainObjectsContext);
+
   const renderTarget = useRenderTarget();
 
-  const { predicate, additionalChangeFlags } = useMemo(() => {
-    return {
-      predicate: (domainObject: DomainObject) => domainObject.isVisible(renderTarget),
-      additionalChangeFlags: [Changes.visibleState]
-    };
+  const disposableSignal = useMemo(() => {
+    const predicate = (domainObject: DomainObject): boolean => domainObject.isVisible(renderTarget);
+    const additionalChangeFlags = [Changes.visibleState];
+    return getRevealDomainUpdateSignal(renderTarget, predicate, additionalChangeFlags);
   }, [renderTarget]);
 
-  return useRevealDomainObjects(predicate, additionalChangeFlags);
+  return useDisposableSignal(disposableSignal);
 }
