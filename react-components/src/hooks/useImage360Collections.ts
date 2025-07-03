7b79aeb3369d95201d5407bca7bf485993b92855
Håkon Flatval <hakon.flatval@cognite.com>
2025-07-03 09:32:56 +0200

    test: add test for useImage360Collections

--- react-components/src/hooks/useImage360Collections.ts
+++ react-components/src/hooks/useImage360Collections.ts
@@ -1,10 +1,20 @@
-import { useMemo } from 'react';
+import { createContext, useContext, useMemo } from 'react';
 import { type DataSourceType, type Image360Collection } from '@cognite/reveal';
 import { Image360CollectionDomainObject } from '../architecture/concrete/reveal/Image360Collection/Image360CollectionDomainObject';
 import { useRevealDomainObjects } from './useRevealDomainObjects';
 import { isDefined } from '../utilities/isDefined';
 
+export type UseImage360CollectionsDependencies = {
+  useRevealDomainObjects: typeof useRevealDomainObjects;
+};
+
+export const UseImage360CollectionsContext = createContext<UseImage360CollectionsDependencies>({
+  useRevealDomainObjects
+});
+
 export const useImage360Collections = (): Array<Image360Collection<DataSourceType>> => {
+  const { useRevealDomainObjects } = useContext(UseImage360CollectionsContext);
+
   const domainObjects = useRevealDomainObjects();
 
   return useMemo(
