cf5f7b0e6a2a4d566870c2fb116b1d291f995250
Nils Petter Fremming <nils.fremming@cognite.com>
2025-07-04 21:42:53 +0200

    Update CylinderCreator.ts

--- react-components/src/architecture/concrete/primitives/cylinder/CylinderCreator.ts
+++ react-components/src/architecture/concrete/primitives/cylinder/CylinderCreator.ts
@@ -7,11 +7,17 @@ import { type DomainObject } from '../../../base/domainObjects/DomainObject';
 import { type CylinderDomainObject } from './CylinderDomainObject';
 import { Cylinder } from '../../../base/utilities/primitives/Cylinder';
 import { getClosestPointOnLine } from '../../../base/utilities/extensions/rayUtils';
-import { rotatePiHalf } from '../../../base/utilities/extensions/vectorUtils';
+import { horizontalDistanceTo, rotatePiHalf } from '../../../base/utilities/extensions/vectorUtils';
 
 const UP_VECTOR = new Vector3(0, 0, 1);
 /**
  * Helper class for generate a CylinderDomainObject by clicking around
+ * Here is the way it works:
+ * * - The first point will be one side of the cylinder
+ * * - The second point will be the other side of the cylinder, so the diameter will be the distance between the first 2 point
+ *     and the one of the end center points will be the average of the first 2 points.
+ *     Here we also calculate the axis of to horizontal cylinder. The vertical cylinder will always have the axis as UP_VECTOR.
+ * * - The third point will be the height of the cylinder.
  */
 export class CylinderCreator extends BaseCreator {
   // ==================================================
@@ -91,9 +97,12 @@ export class CylinderCreator extends BaseCreator {
         }
         return point;
       }
-      // If the done when pointing in the "air". Try to calculate a reasonable good value, based on the ray.
+      // If pointing in the "air", try to calculate a reasonable good value, based on the ray.
       if (this._primitiveType === PrimitiveType.HorizontalCylinder) {
         const { center } = this._domainObject.cylinder;
+
+        // Create an intersection between a plane towards the ray and ray itself
+        // This will give the second point
         const plane = new Plane().setFromNormalAndCoplanarPoint(ray.direction, center);
         const pointOnRay = ray.intersectPlane(plane, new Vector3());
         if (pointOnRay !== null) {
@@ -109,18 +118,12 @@ export class CylinderCreator extends BaseCreator {
       }
       return undefined;
     } else if (this.notPendingPointCount === 2) {
-      // Calculate the other center point regardless of the input point
+      // Calculate the other center point regardless of the input point, just using the ray
       const { center, axis } = this._domainObject.cylinder;
       return getClosestPointOnLine(ray, axis, center);
     }
     return point;
   }
-
-  /**
-   * Create the cylinder by adding points. The first point will make a centerA.
-   * The second/third point will give the centerB.
-   * The third/second will give radius. The radius is already calculated in the recalculatePoint
-   */
 
   private rebuild(ray: Ray): void {
     if (this.pointCount === 0) {
@@ -128,27 +131,35 @@ export class CylinderCreator extends BaseCreator {
     }
     const { cylinder } = this._domainObject;
     if (this.pointCount === 1) {
-      // Add point 1
-      this.setCenter(this.firstPoint, ray.direction.clone());
+      // Point 1: Defined the center and the axis. This is later moved by the second point
+      if (this._primitiveType === PrimitiveType.HorizontalCylinder) {
+        const axis = new Vector3();
+        forceHorizontalDirection(axis, ray);
+        this.setCenter(this.firstPoint, axis);
+      } else {
+        this.setCenter(this.firstPoint, UP_VECTOR);
+      }
     } else if (this.pointCount === 2) {
-      // Add point 2: This will recalculate the center, the diameter ans the axis (for horizontal cylinder)
+      // Point 2: This will recalculate the center, the diameter and the axis (for horizontal cylinder)
       const center = this.firstPoint.clone().add(this.lastPoint).multiplyScalar(0.5);
       if (this._domainObject.primitiveType === PrimitiveType.HorizontalCylinder) {
-        // Calculate the center and axis for the first two points
+        // Calculate the axis:
         const axis = this.firstPoint.clone().sub(this.lastPoint);
         rotatePiHalf(axis);
+        forceHorizontalDirection(axis, ray);
         this.setCenter(center, axis);
 
+        // Calculate the radius by projecting the first and last point onto the plane defined by the axis and center
         const plane = new Plane().setFromNormalAndCoplanarPoint(axis, center);
         const firstPoint = plane.projectPoint(this.firstPoint, new Vector3());
         const lastPoint = plane.projectPoint(this.lastPoint, new Vector3());
         cylinder.radius = firstPoint.distanceTo(lastPoint) / 2;
       } else {
         this.setCenter(center, UP_VECTOR);
-        cylinder.radius = this.firstPoint.distanceTo(this.lastPoint) / 2;
+        cylinder.radius = horizontalDistanceTo(this.firstPoint, this.lastPoint) / 2;
       }
     } else if (this.pointCount === 3) {
-      // Add point 3 and define the length
+      // Point 3: Define the height of the cylinder, by setting both centers
       const center = this.firstPoint.clone().add(this.points[1]).multiplyScalar(0.5);
       cylinder.centerA.copy(center);
       cylinder.centerB.copy(this.lastPoint);
@@ -157,18 +168,30 @@ export class CylinderCreator extends BaseCreator {
   }
 
   private setCenter(center: Vector3, axis: Vector3): void {
-    if (this._primitiveType === PrimitiveType.HorizontalCylinder) {
-      axis.z = 0;
-      axis.normalize();
-    } else {
-      axis.set(0, 0, 1);
-    }
     const { cylinder } = this._domainObject;
     const { centerA, centerB } = cylinder;
     centerA.copy(center);
     centerB.copy(center);
+
+    // Set the height and radius of the cylinder to be MinSize
     centerA.addScaledVector(axis, -Cylinder.HalfMinSize);
     centerB.addScaledVector(axis, +Cylinder.HalfMinSize);
     cylinder.forceMinSize();
   }
+}
+
+function forceHorizontalDirection(vector: Vector3, ray: Ray): void {
+  vector.z = 0;
+  if (vector.length() === 0) {
+    // If the x and y component is not set, we take it from the ray direction
+    // This is to ensure that the axis is meaningful
+    vector.copy(ray.direction);
+    vector.z = 0;
+    if (vector.length() === 0) {
+      // If the x and y component is still not set, we just select an arbitrary horizontal direction
+      vector.set(1, 0, 0);
+    }
+  }
+  // Now we have a horizontal vector with some length, this need to be normalized.
+  vector.normalize();
 }
