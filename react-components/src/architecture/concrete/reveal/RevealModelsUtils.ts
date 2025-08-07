6f4aea60bec8ce5e0eb86f05d9df1350ac047315
Nils Petter Fremming <nils.fremming@cognite.com>
2025-08-07 12:37:11 +0200

    Make getName function

--- react-components/src/architecture/concrete/reveal/RevealModelsUtils.ts
+++ react-components/src/architecture/concrete/reveal/RevealModelsUtils.ts
@@ -10,11 +10,12 @@ import {
 import { CadDomainObject } from './cad/CadDomainObject';
 import { PointCloudDomainObject } from './pointCloud/PointCloudDomainObject';
 import { Image360CollectionDomainObject } from './Image360Collection/Image360CollectionDomainObject';
-import { type RevealModel } from './RevealTypes';
+import { type PointCloud, type RevealModel } from './RevealTypes';
 import { type RootDomainObject } from '../../base/domainObjects/RootDomainObject';
 import { type RevealRenderTarget } from '../../base/renderTarget/RevealRenderTarget';
 import { type AddImage360CollectionOptions } from '../../..';
 import { type RevealDomainObject } from './RevealDomainObject';
+import { type CogniteClient } from '@cognite/sdk';
 
 export class RevealModelsUtils {
   public static getByRevealModel(
@@ -43,6 +44,22 @@ export class RevealModelsUtils {
     return undefined;
   }
 
+  static async getName(sdk: CogniteClient, model: CogniteCadModel | PointCloud): Promise<string> {
+    if (model instanceof CogniteCadModel) {
+      const model3D = await sdk.models3D.retrieve(model.modelId);
+      return model3D.name;
+    }
+    if (model instanceof CognitePointCloudModel) {
+      if (isDMPointCloudModel(model)) {
+        return model.modelIdentifier.revisionExternalId;
+      } else if (isClassicPointCloudModel(model)) {
+        const model3D = await sdk.models3D.retrieve(model.modelIdentifier.modelId);
+        return model3D.name;
+      }
+    }
+    throw new Error('Unsupported model in getName');
+  }
+
   public static async addCadModel(
     renderTarget: RevealRenderTarget,
     options: AddModelOptions
@@ -54,13 +71,14 @@ export class RevealModelsUtils {
     const model = await renderTarget.viewer.addCadModel(options);
     const domainObject = new CadDomainObject(model);
     root.addChildInteractive(domainObject);
-
-    const sdk = renderTarget.rootDomainObject.sdk;
-    const model3D = await sdk.models3D.retrieve(model.modelId);
-    domainObject.name = model3D.name;
-
     if (model.visible) {
       domainObject.setVisibleInteractive(true);
+    }
+
+    try {
+      domainObject.name = await RevealModelsUtils.getName(renderTarget.rootDomainObject.sdk, model);
+    } catch (error) {
+      console.error('Can not get Cad model name', error);
     }
     return model;
   }
@@ -68,7 +86,7 @@ export class RevealModelsUtils {
   public static async addPointCloud(
     renderTarget: RevealRenderTarget,
     options: AddModelOptions<DataSourceType>
-  ): Promise<CognitePointCloudModel<DataSourceType>> {
+  ): Promise<PointCloud> {
     const root = renderTarget.rootDomainObject;
     if (root === undefined) {
       throw new Error('Root domain object is not set');
@@ -76,17 +94,14 @@ export class RevealModelsUtils {
     const model = await renderTarget.viewer.addPointCloudModel(options);
     const domainObject = new PointCloudDomainObject(model);
     root.addChildInteractive(domainObject);
-
-    const sdk = renderTarget.rootDomainObject.sdk;
-
-    if (isDMPointCloudModel(model)) {
-      domainObject.name = model.modelIdentifier.revisionExternalId;
-    } else if (isClassicPointCloudModel(model)) {
-      const model3D = await sdk.models3D.retrieve(model.modelIdentifier.modelId);
-      domainObject.name = model3D.name;
-    }
     if (model.visible) {
       domainObject.setVisibleInteractive(true);
+    }
+
+    try {
+      domainObject.name = await RevealModelsUtils.getName(renderTarget.rootDomainObject.sdk, model);
+    } catch (error) {
+      console.error('Can not get model name', error);
     }
     return model;
   }
