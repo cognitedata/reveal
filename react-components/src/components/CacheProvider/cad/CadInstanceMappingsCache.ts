7a51685e50f2ce47497baccaf1b082591917375f
Håkon Flatval <hakon.flatval@cognite.com>
2025-08-08 08:47:41 +0200

    chore: remove unused type

--- react-components/src/components/CacheProvider/cad/CadInstanceMappingsCache.ts
+++ react-components/src/components/CacheProvider/cad/CadInstanceMappingsCache.ts
@@ -1,11 +1,10 @@
 import type { Node3D } from '@cognite/sdk';
-import type { ModelRevisionKey, ModelRevisionId, CadNodeIdData, CadNodeTreeData } from '../types';
+import type { ModelRevisionKey, ModelRevisionId, CadNodeIdData } from '../types';
 import type { InstanceId, InstanceKey } from '../../../utilities/instanceIds';
 
 export type CadModelMappingsWithNodes = Map<ModelRevisionKey, Map<InstanceKey, Node3D[]>>;
 
 export type CadModelMappings = Map<ModelRevisionKey, Map<InstanceKey, CadNodeIdData[]>>;
-export type CadModelTreeIndexMappings = Map<ModelRevisionKey, Map<InstanceKey, CadNodeTreeData[]>>;
 
 export type CadInstanceMappingsCache = {
   getMappingsForModelsAndInstances: (
