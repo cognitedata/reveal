4f2f29854d63cf2acbb9b40e106aba8baa584895
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-08-11 08:51:30 +0200

    Update react-components/src/architecture/base/concreteCommands/pointCloud/PointCloudDividerCommand.test.ts
    Co-authored-by: pramod-cog <pramod.s@cognite.com>

--- react-components/src/architecture/base/concreteCommands/pointCloud/PointCloudDividerCommand.test.ts
+++ react-components/src/architecture/base/concreteCommands/pointCloud/PointCloudDividerCommand.test.ts
@@ -2,7 +2,7 @@ import { PointCloudDividerCommand } from './PointCloudDividerCommand';
 import { createFullRenderTargetMock } from '#test-utils/fixtures/createFullRenderTargetMock';
 import { describe, expect, test } from 'vitest';
 import { PointCloudDomainObject } from '../../../concrete/reveal/pointCloud/PointCloudDomainObject';
-import { createPointCloudMock } from '../../../../../tests/tests-utilities/fixtures/pointCloud';
+import { createPointCloudMock } from '#test-utils/fixtures/pointCloud';
 
 describe(PointCloudDividerCommand.name, () => {
   test('Should be visible only when having point clouds', () => {
