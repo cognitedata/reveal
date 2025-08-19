942d0d166a477084fd62e380c708bacea2e6e34b
Håkon Flatval <hakon.flatval@cognite.com>
2025-08-19 14:42:49 +0200

    chore: lint fix

--- react-components/src/utilities/getUrlRegex.test.ts
+++ react-components/src/utilities/getUrlRegex.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { getUrlRegex } from './getUrlRegex';
 
 describe(getUrlRegex.name, () => {
