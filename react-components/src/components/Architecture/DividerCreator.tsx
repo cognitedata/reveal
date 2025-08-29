cbb3c59fb3cd70ef9a3192440789252a350079e4
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-08-29 13:21:13 +0200

    Fix compiler error

--- react-components/src/components/Architecture/DividerCreator.tsx
+++ react-components/src/components/Architecture/DividerCreator.tsx
@@ -11,8 +11,12 @@ export function createDivider(
   placement: PlacementType
 ): ReactElement | undefined {
   if (command instanceof DividerCommand || command instanceof SectionCommand) {
-    const direction = getDividerDirection(placement);
-    return <Divider weight="2px" length="24px" direction={direction} key={command.uniqueId} />;
+    return createDividerByKey(command.uniqueId, placement);
   }
   return undefined;
+}
+
+export function createDividerByKey(key: string, placement: PlacementType): ReactElement {
+  const direction = getDividerDirection(placement);
+  return <Divider weight="2px" length="24px" direction={direction} key={key} />;
 }
