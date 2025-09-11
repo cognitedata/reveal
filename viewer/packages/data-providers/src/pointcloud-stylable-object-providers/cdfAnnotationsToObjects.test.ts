9869c3790f63a47d98d01377878a942679862ced
Håkon Flatval <hakon.flatval@cognite.com>
2025-09-11 12:38:24 +0200

    chore(react-components): add missing test for cdfAnnotationsToObjects

---
+++ viewer/packages/data-providers/src/pointcloud-stylable-object-providers/cdfAnnotationsToObjects.test.ts
@@ -0,0 +1,41 @@
+/*!
+ * Copyright 2025 Cognite AS
+ */
+import { CompositeShape, Cylinder } from '@reveal/utilities';
+import { cdfAnnotationsToObjects } from './cdfAnnotationsToObjects';
+import { CdfPointCloudObjectAnnotation, VolumeAnnotation } from './types';
+import { Vector3 } from 'three';
+
+describe(cdfAnnotationsToObjects.name, () => {
+  const ARBITRARY_ANNOTATION_ID = 123;
+
+  const ARBITRARY_VOLUME = new Cylinder(new Vector3(0, 0, 0), new Vector3(1, 1, 1), 2);
+  const ARBITRARY_ASSET_REF = { id: 987 };
+  const ARBITRARY_INSTANCE_REF = { externalId: 'some-external-id', space: 'space' };
+
+  test('returns empty list on empty input', () => {
+    expect(cdfAnnotationsToObjects([])).toEqual([]);
+  });
+
+  test.each([
+    {},
+    { asset: ARBITRARY_ASSET_REF },
+    { assetInstanceRef: ARBITRARY_INSTANCE_REF },
+    { asset: ARBITRARY_ASSET_REF, assetInstanceRef: ARBITRARY_INSTANCE_REF }
+  ])('returns annotation with input volume and extra volume metadata %s', extraMetadataVolumeMetadata => {
+    const input: CdfPointCloudObjectAnnotation = {
+      volumeMetadata: { annotationId: ARBITRARY_ANNOTATION_ID, ...extraMetadataVolumeMetadata },
+      region: [ARBITRARY_VOLUME]
+    };
+
+    expect(cdfAnnotationsToObjects([input])).toEqual([
+      {
+        boundingBox: ARBITRARY_VOLUME.createBoundingBox(),
+        stylableObject: { shape: new CompositeShape([ARBITRARY_VOLUME]), objectId: 1 },
+        annotationId: ARBITRARY_ANNOTATION_ID,
+        instanceRef: (input.volumeMetadata as VolumeAnnotation).assetInstanceRef,
+        assetRef: input.volumeMetadata.asset
+      }
+    ]);
+  });
+});
