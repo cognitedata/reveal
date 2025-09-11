588d148da9f230726b760cc8986099dc14392858
Pramod S <pramod.s@cognite.com>
2025-09-11 13:33:22 +0200

    updated test model indentifier data to be local model identiifer

--- viewer/packages/cad-model/src/CadModelFactory.test.ts
+++ viewer/packages/cad-model/src/CadModelFactory.test.ts
@@ -12,7 +12,7 @@ import { It, Mock } from 'moq.ts';
 import { GeometryFilter } from './types';
 
 import { jest } from '@jest/globals';
-import { createMockModelIdentifier } from '../../../test-utilities';
+import { createMockLocalModelIdentifier } from '../../../test-utilities';
 
 describe('CadModelFactory', () => {
   let materialManager: CadMaterialManager;
@@ -22,7 +22,7 @@ describe('CadModelFactory', () => {
   beforeEach(() => {
     materialManager = new CadMaterialManager();
 
-    const mockIdentifier = createMockModelIdentifier();
+    const mockIdentifier = createMockLocalModelIdentifier();
     mockIdentifierObject = mockIdentifier.object();
 
     const testOutput: BlobOutputMetadata = { blobId: 1, format: 'gltf-directory', version: 9 };
--- viewer/test-utilities/src/modelIdentifier.ts
+++ viewer/test-utilities/src/modelIdentifier.ts
@@ -1,13 +1,13 @@
 /*!
  * Copyright 2025 Cognite AS
  */
-import { ModelIdentifier } from '../../packages/data-providers';
+import { LocalModelIdentifier } from '../../packages/data-providers';
 import { IMock, Mock } from 'moq.ts';
 
-export function createMockModelIdentifier(): IMock<ModelIdentifier> {
-  return new Mock<ModelIdentifier>()
+export function createMockLocalModelIdentifier(): IMock<LocalModelIdentifier> {
+  return new Mock<LocalModelIdentifier>()
     .setup(p => p.revealInternalId)
-    .returns(Symbol('test'))
+    .returns(Symbol('test-modelIdentifier'))
     .setup(p => p.sourceModelIdentifier())
-    .returns('test: test model');
+    .returns('test-modelIdentifier');
 }
