88828148f5fd7829b21fa9c8ef0f7764d13f9678
Håkon Flatval <hakon.flatval@cognite.com>
2025-09-11 13:47:22 +0200

    chore: make mock import path slightly nicer

--- react-components/src/components/Architecture/pointsOfInterest/PointsOfInterestSidePanel.test.tsx
+++ react-components/src/components/Architecture/pointsOfInterest/PointsOfInterestSidePanel.test.tsx
@@ -12,7 +12,7 @@ import {
 import { render } from '@testing-library/react';
 import { type PropsWithChildren, type ReactElement, type FC } from 'react';
 import { createRenderTargetMock } from '#test-utils/fixtures/renderTarget';
-import { createPointOfInterestMock } from '#test-utils/fixtures/pointsOfInterest/pointOfInterest';
+import { createPointOfInterestMock } from '#test-utils/fixtures/pointsOfInterest';
 
 describe(PointsOfInterestSidePanel.name, () => {
   const dependencies = getMocksByDefaultDependencies(defaultPointsOfInterestSidePanelDependencies);
---
+++ react-components/tests/tests-utilities/fixtures/pointsOfInterest/index.ts
@@ -0,0 +1,1 @@
+export { createPointOfInterestMock } from './pointOfInterest';
