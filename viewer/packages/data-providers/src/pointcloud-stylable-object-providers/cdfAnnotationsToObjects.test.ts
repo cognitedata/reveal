2864214dabedf34a66a625248b768aa870a5c925
Håkon Flatval <hakon.flatval@cognite.com>
2025-09-12 09:11:36 +0200

    chore: created DM-annotation test too

--- viewer/packages/data-providers/src/pointcloud-stylable-object-providers/cdfAnnotationsToObjects.test.ts
+++ viewer/packages/data-providers/src/pointcloud-stylable-object-providers/cdfAnnotationsToObjects.test.ts
@@ -8,6 +8,7 @@ import { Vector3 } from 'three';
 
 describe(cdfAnnotationsToObjects.name, () => {
   const ARBITRARY_ANNOTATION_ID = 123;
+  const ARBITRARY_ANNOTATION_DM_REF = { externalId: 'annotation-external-id', space: 'annotation-space' };
 
   const ARBITRARY_VOLUME = new Cylinder(new Vector3(0, 0, 0), new Vector3(1, 1, 1), 2);
   const ARBITRARY_ASSET_REF = { id: 987 };
@@ -35,6 +36,22 @@ describe(cdfAnnotationsToObjects.name, () => {
         annotationId: ARBITRARY_ANNOTATION_ID,
         instanceRef: (input.volumeMetadata as VolumeAnnotation).assetInstanceRef,
         assetRef: input.volumeMetadata.asset
+      }
+    ]);
+  });
+
+  test('returns annotation with input volume for DM annotation', () => {
+    const input: CdfPointCloudObjectAnnotation = {
+      volumeMetadata: { instanceRef: ARBITRARY_ANNOTATION_DM_REF, asset: ARBITRARY_INSTANCE_REF },
+      region: [ARBITRARY_VOLUME]
+    };
+
+    expect(cdfAnnotationsToObjects([input])).toEqual([
+      {
+        boundingBox: ARBITRARY_VOLUME.createBoundingBox(),
+        stylableObject: { shape: new CompositeShape([ARBITRARY_VOLUME]), objectId: 1 },
+        volumeInstanceRef: ARBITRARY_ANNOTATION_DM_REF,
+        assetRef: ARBITRARY_INSTANCE_REF
       }
     ]);
   });
