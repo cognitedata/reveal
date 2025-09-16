9b971bfa8c270077c1886edcf276ccc449fd2664
Taras Khomyn <taras.khomyn@globallogic.com>
2025-09-16 10:43:52 +0200

    changed variables name

--- viewer/packages/metrics/src/Fingerprint/FingerprintHash.ts
+++ viewer/packages/metrics/src/Fingerprint/FingerprintHash.ts
@@ -1,13 +1,13 @@
 // cyrb53 hashing algorythm
 export function hashFingerprint(str: string, seed = 0) {
-  let h1 = 0xdeadbeef ^ seed,
-      h2 = 0x41c6ce57 ^ seed;
+  let firstPointer = 0xdeadbeef ^ seed,
+      secondPointer = 0x41c6ce57 ^ seed;
   for (let i = 0, ch; i < str.length; i++) {
       ch = str.charCodeAt(i);
-      h1 = Math.imul(h1 ^ ch, 2654435761);
-      h2 = Math.imul(h2 ^ ch, 1597334677);
+      firstPointer = Math.imul(firstPointer ^ ch, 2654435761);
+      secondPointer = Math.imul(secondPointer ^ ch, 1597334677);
   }
-  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+  firstPointer = Math.imul(firstPointer ^ (firstPointer >>> 16), 2246822507) ^ Math.imul(secondPointer ^ (secondPointer >>> 13), 3266489909);
+  secondPointer = Math.imul(secondPointer ^ (secondPointer >>> 16), 2246822507) ^ Math.imul(firstPointer ^ (firstPointer >>> 13), 3266489909);
+  return 4294967296 * (2097151 & secondPointer) + (firstPointer >>> 0);
 }
