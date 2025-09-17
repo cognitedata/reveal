fe395f4f8e6acea4f9958f0328e99dba45a5ecff
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-09-17 16:16:00 +0200

    Fix ShareCommand test

--- react-components/src/architecture/base/concreteCommands/general/ShareCommand.test.ts
+++ react-components/src/architecture/base/concreteCommands/general/ShareCommand.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { ShareCommand } from './ShareCommand';
 import { isEmpty } from '../../utilities/translation/TranslateInput';
 
@@ -13,12 +13,13 @@ describe(ShareCommand.name, () => {
   test('should copy the URL to clipboard', async () => {
     vi.spyOn(window, 'location', 'get').mockReturnValue({
       ...window.location,
-      href: 'http://cognite.test'
+      href: TEST_URL
     });
-
     const command = new ShareCommand();
     expect(command.invoke()).toBe(true);
     const text = await navigator.clipboard.readText();
-    expect(text).toBe('http://cognite.test');
+    expect(text).toBe(TEST_URL);
   });
 });
+
+const TEST_URL = 'http://cognite.test';
