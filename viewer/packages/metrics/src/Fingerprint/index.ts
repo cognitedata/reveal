aa8f2e474ac7ddbb65d09a39c9890d47c693120f
Taras Khomyn <taras.khomyn@globallogic.com>
2025-09-18 12:57:48 +0200

    removed promise wrapper

--- viewer/packages/metrics/src/Fingerprint/index.ts
+++ viewer/packages/metrics/src/Fingerprint/index.ts
@@ -1,23 +1,4 @@
 /*!
  * Copyright 2025 Cognite AS
  */
-import { generateCanvasFingerprint } from './generateCanvasFingerprint';
-import { generateAudioFingerprint } from './generateAudioFingerprint';
-import { hashFingerprint } from './hashFingerprint';
-
-export async function getUserFingerprint(): Promise<string> {
-  try {
-    const canvasFingerprint = generateCanvasFingerprint();
-    const audioFingerprint = await generateAudioFingerprint();
-
-    const combinedFP = `${canvasFingerprint}-${audioFingerprint}`;
-
-    // Hash the combined string to get the final unique ID
-    const fingerprintID = hashFingerprint(combinedFP).toString();
-
-    return fingerprintID;
-  } catch (error) {
-    console.warn('Could not get browser fingerprint:', error);
-    return 'unknown';
-  }
-}
+export {getUserFingerprint} from './getUserFingerprint';
