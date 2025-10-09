6e12e4d975dd7fb61d87ef3d3394a5a6cf68ff84
Daniel Priori <daniel.priori@cognite.com>
2025-10-09 09:59:18 +0200

    cr: remove unnecessary test for unknown type checking and use in operator

--- react-components/src/utilities/getInstancesFromClick.test.ts
+++ react-components/src/utilities/getInstancesFromClick.test.ts
@@ -154,22 +154,4 @@ describe(getInstancesFromClick.name, () => {
     expect(result).toEqual([dmInstanceRef]);
   });
 
-  test('returns undefined for unknown intersection type', async () => {
-    const unknownIntersectionMock = new Mock<AnyIntersection>()
-      .setup((p) => p.type)
-      .returns('unknown' as any) // Force as any to simulate unknown type
-      .setup((p) => p.point)
-      .returns(new Vector3(1, 2, 3))
-      .setup((p) => p.distanceToCamera)
-      .returns(10)
-      .object();
-
-    mockViewerGetAnyIntersectionFromPixel.mockResolvedValue(unknownIntersectionMock);
-
-    const result = await getInstancesFromClick(mockRenderTarget, mockEvent, {
-      fetchAncestorNodesTreeIndex: mockfetchAncestorNodesTreeIndex
-    });
-
-    expect(result).toBeUndefined();
-  });
 });
