509247e7ade02a21e26c552646e1b686081a740a
Håkon Flatval <hakon.flatval@cognite.com>
2025-10-16 08:35:57 +0200

    chore: clean up Reveal3DResources/utils a bit

--- react-components/src/components/Reveal3DResources/utils.ts
+++ react-components/src/components/Reveal3DResources/utils.ts
@@ -3,44 +3,22 @@ import {
   isFdmAssetStylingGroup
 } from '../../utilities/StylingGroupUtils';
 import { type InstanceStylingGroup } from './types';
-import {
-  createInstanceReferenceKey,
-  type InstanceId,
-  type InstanceReference,
-  type InstanceReferenceKey,
-  isDmsInstance,
-  isInternalId
-} from '../../utilities/instanceIds';
+import { type InstanceId, InstanceKey } from '../../utilities/instanceIds';
 import { assertNever } from '../../utilities/assertNever';
+import { createInstanceKey } from '../CacheProvider/idAndKeyTranslation';
 
-export function getInstanceKeysFromStylingGroup(
-  stylingGroup: InstanceStylingGroup
-): InstanceReferenceKey[] {
-  return getInstanceReferencesFromStylingGroup(stylingGroup).map(createInstanceReferenceKey);
+export function getInstanceKeysFromStylingGroup(stylingGroup: InstanceStylingGroup): InstanceKey[] {
+  return getInstanceReferencesFromStylingGroup(stylingGroup).map(createInstanceKey);
 }
 
 export function getInstanceReferencesFromStylingGroup(
   stylingGroup: InstanceStylingGroup
-): InstanceReference[] {
+): InstanceId[] {
   if (isClassicAssetMappingStylingGroup(stylingGroup)) {
-    return stylingGroup.assetIds.map((id) => ({ id }));
+    return stylingGroup.assetIds;
   } else if (isFdmAssetStylingGroup(stylingGroup)) {
     return stylingGroup.fdmAssetExternalIds;
   }
 
   assertNever(stylingGroup);
-}
-
-export function getInstanceIdsFromReferences(
-  instanceReferences: InstanceReference[]
-): InstanceId[] {
-  return instanceReferences.reduce<InstanceId[]>((acc, reference) => {
-    if (isInternalId(reference)) {
-      acc.push(reference.id);
-    } else if (isDmsInstance(reference)) {
-      acc.push(reference);
-    }
-
-    return acc;
-  }, []);
 }
