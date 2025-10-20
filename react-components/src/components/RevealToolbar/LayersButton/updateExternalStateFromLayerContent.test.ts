2cfd6f525813cef5a6fc3e0ba4a65786c057f8b3
Håkon Flatval <hakon.flatval@cognite.com>
2025-10-20 09:09:15 +0200

    refactor(react-components): BND3D-6147 part 1 - foundational hooks

---
+++ react-components/src/components/RevealToolbar/LayersButton/updateExternalStateFromLayerContent.test.ts
@@ -0,0 +1,91 @@
+import { describe, expect, test, vi } from 'vitest';
+import { updateExternalStateFromLayerContent } from './updateExternalStateFromLayerContent';
+import { type LayersUrlStateParam } from './types';
+import { type Dispatch, type SetStateAction } from 'react';
+import { createRenderTargetMock } from '#test-utils/fixtures/renderTarget';
+import {
+  CadDomainObject,
+  Image360CollectionDomainObject,
+  PointCloudDomainObject
+} from '../../../architecture';
+import { createCadMock } from '#test-utils/fixtures/cadModel';
+import { createPointCloudMock } from '#test-utils/fixtures/pointCloud';
+import { createImage360ClassicMock } from '#test-utils/fixtures/image360';
+
+describe(updateExternalStateFromLayerContent.name, () => {
+  test('outputs correct state for all layers', () => {
+    const setLayerStateMock = vi.fn<Dispatch<SetStateAction<LayersUrlStateParam | undefined>>>();
+
+    const cadDomainObject0 = new CadDomainObject(createCadMock({ revisionId: 123 }));
+    const cadDomainObject1 = new CadDomainObject(
+      createCadMock({ revisionId: 234, visible: false })
+    );
+
+    const pointCloudDomainObject0 = new PointCloudDomainObject(
+      createPointCloudMock({ revisionId: 345 })
+    );
+    const pointCloudDomainObject1 = new PointCloudDomainObject(
+      createPointCloudMock({ revisionId: 456, visible: false })
+    );
+
+    const image360CollectionDomainObject0 = new Image360CollectionDomainObject(
+      createImage360ClassicMock()
+    );
+
+    const image360CollectionDomainObject1 = new Image360CollectionDomainObject(
+      createImage360ClassicMock({ visible: false })
+    );
+
+    const renderTargetMock = createRenderTargetMock();
+
+    updateExternalStateFromLayerContent(
+      {
+        cadModels: [cadDomainObject0, cadDomainObject1],
+        pointClouds: [pointCloudDomainObject0, pointCloudDomainObject1],
+        image360Collections: [image360CollectionDomainObject0, image360CollectionDomainObject1]
+      },
+      setLayerStateMock,
+      renderTargetMock
+    );
+
+    const result = setLayerStateMock.mock.calls[0][0] as LayersUrlStateParam;
+
+    expect(setLayerStateMock).toHaveBeenCalledOnce();
+
+    expect(result.cadLayers).toHaveLength(2);
+
+    expect(result.cadLayers?.[0].applied).toBe(cadDomainObject0.isVisible(renderTargetMock));
+    expect(result.cadLayers?.[0].revisionId).toBe(cadDomainObject0.model.revisionId);
+    expect(result.cadLayers?.[0].index).toBe(0);
+
+    expect(result.cadLayers?.[1].applied).toBe(cadDomainObject1.isVisible(renderTargetMock));
+    expect(result.cadLayers?.[1].revisionId).toBe(cadDomainObject1.model.revisionId);
+    expect(result.cadLayers?.[1].index).toBe(1);
+
+    expect(result.pointCloudLayers).toHaveLength(2);
+
+    expect(result.pointCloudLayers?.[0].applied).toBe(
+      pointCloudDomainObject0.isVisible(renderTargetMock)
+    );
+    expect(result.pointCloudLayers?.[0].revisionId).toBe(pointCloudDomainObject0.model.revisionId);
+    expect(result.pointCloudLayers?.[0].index).toBe(0);
+
+    expect(result.pointCloudLayers?.[1].applied).toBe(
+      pointCloudDomainObject1.isVisible(renderTargetMock)
+    );
+    expect(result.pointCloudLayers?.[1].revisionId).toBe(pointCloudDomainObject1.model.revisionId);
+    expect(result.pointCloudLayers?.[1].index).toBe(1);
+
+    expect(result.image360Layers).toHaveLength(2);
+
+    expect(result.image360Layers?.[0].applied).toBe(
+      image360CollectionDomainObject0.isVisible(renderTargetMock)
+    );
+    expect(result.image360Layers?.[0].siteId).toBe(image360CollectionDomainObject0.model.id);
+
+    expect(result.image360Layers?.[1].applied).toBe(
+      image360CollectionDomainObject1.isVisible(renderTargetMock)
+    );
+    expect(result.image360Layers?.[1].siteId).toBe(image360CollectionDomainObject1.model.id);
+  });
+});
