5530f39975419767e8d65d55cbd616d2c63166eb
Håkon Flatval <hakon.flatval@cognite.com>
2025-10-22 12:48:40 +0200

    chore: fix circular dependency

--- react-components/src/components/Reveal3DResources/hooks/useCalculatePointCloudStyling.context.ts
+++ react-components/src/components/Reveal3DResources/hooks/useCalculatePointCloudStyling.context.ts
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { use3dModels } from '../../../hooks';
+import { use3dModels } from '../../../hooks/use3dModels';
 
 export type UseCalculatePointCloudStylingDependendencies = {
   use3dModels: typeof use3dModels;
