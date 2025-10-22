50b0f5d8c8e8f984d0cbadb7a4f4d51172ddcc7e
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-10-22 22:40:51 +0200

    Update tree-node.test.ts

--- react-components/src/tree-view/model/tree-node.test.ts
+++ react-components/src/tree-view/model/tree-node.test.ts
@@ -1,11 +1,8 @@
 import { describe, test, expect, beforeEach, vi, assert } from 'vitest';
 import { TreeNode } from './tree-node';
 import { CheckboxState } from '../../architecture/base/utilities/types';
-import { type OnNodeLoadedAction, type TreeNodeAction } from './types';
+import { type TreeNodeAction } from './types';
 import { count } from '../../architecture/base/utilities/extensions/generatorUtils';
-import { type ILazyLoader } from './i-lazy-loader';
-import { type TreeNodeType } from './tree-node-type';
-import { sleep } from '../../../tests/tests-utilities/tree-view/list-nodes-stk/list-nodes-sdk-mock';
 
 describe(TreeNode.name, () => {
   let root: TreeNode;
@@ -121,6 +118,15 @@ describe(TreeNode.name, () => {
       for (const expected of [true, false]) {
         root.needLoadSiblings = expected;
         expect(root.needLoadSiblings).toBe(expected);
+      }
+      expect(rootListener).toHaveBeenCalledTimes(2);
+      expect(rootListener).toHaveBeenLastCalledWith(root);
+    });
+
+    test('should set and get isLoadingChildren', () => {
+      for (const expected of [true, false]) {
+        root.isLoadingChildren = expected;
+        expect(root.isLoadingChildren).toBe(expected);
       }
       expect(rootListener).toHaveBeenCalledTimes(2);
       expect(rootListener).toHaveBeenLastCalledWith(root);
@@ -329,121 +335,12 @@ describe(TreeNode.name, () => {
       expect(count(grandChild.getThisAndAncestorsByType(TreeNode))).toBe(3);
     });
   });
-
-  describe('Lazy loading', () => {
-    test('should lazy load children', async () => {
-      root.needLoadChildren = true;
-
-      const lazyLoader = new LazyLoaderMock();
-      await root.loadChildren(lazyLoader);
-
-      expect(root.childCount).toBe(3); // 4 new children
-      expect(root.needLoadChildren).toBe(false); // Change to false
-
-      // Notified: isLoadingChildren (on/off) needLoadChildren(on/off) + 3 children = 7
-      expect(rootListener).toHaveBeenCalledTimes(7);
-      expect(rootListener).toHaveBeenLastCalledWith(root);
-      expect(lazyLoader.onNodeLoaded).toHaveBeenCalledTimes(3); // 3 children
-    });
-
-    test('should not lazy load children when the mock is empty', async () => {
-      root.needLoadChildren = true;
-
-      await root.loadChildren(new EmptyLazyLoaderMock());
-
-      expect(root.childCount).toBe(0); // 4 new children
-      expect(root.needLoadChildren).toBe(true); // Still true
-
-      // Notified: isLoadingChildren (on/off) needLoadChildren(on) = 3
-      expect(rootListener).toHaveBeenCalledTimes(3);
-      expect(rootListener).toHaveBeenLastCalledWith(root);
-    });
-
-    test('should not lazy load siblings on root', async () => {
-      root.needLoadSiblings = true;
-      const lazyLoader = new LazyLoaderMock();
-      rootListener.mockClear();
-      await root.loadSiblings(lazyLoader);
-
-      // Check that nothings has changed
-      expect(rootListener).toHaveBeenCalledTimes(0);
-      expect(lazyLoader.onNodeLoaded).toHaveBeenCalledTimes(0);
-    });
-
-    test('should lazy load siblings', async () => {
-      addChildren(root);
-      rootListener.mockClear();
-      const child = getLastChild(root);
-      assert(child !== undefined);
-
-      const childListener = vi.fn<TreeNodeAction>();
-      child.addTreeNodeListener(childListener);
-      const oldChildCount = root.childCount;
-      child.needLoadSiblings = true;
-
-      const lazyLoader = new LazyLoaderMock();
-      await child.loadSiblings(lazyLoader);
-
-      expect(root.childCount - oldChildCount).toBe(4); // 4 new children
-      expect(child.needLoadSiblings).toBe(false); // Change to false
-
-      expect(rootListener).toHaveBeenCalledTimes(4); // 4 children added
-      expect(rootListener).toHaveBeenLastCalledWith(root);
-
-      // Notified: isLoadingSiblings (on/off) needLoadSiblings(on/off)
-      expect(childListener).toHaveBeenCalledTimes(4);
-      expect(childListener).toHaveBeenLastCalledWith(child);
-      expect(lazyLoader.onNodeLoaded).toHaveBeenCalledTimes(4); // 4 children
-    });
-
-    test('should not lazy load siblings when the mock is empty', async () => {
-      addChildren(root);
-      rootListener.mockClear();
-      const child = getLastChild(root);
-      assert(child !== undefined);
-
-      const childListener = vi.fn<TreeNodeAction>();
-      child.addTreeNodeListener(childListener);
-      const oldChildCount = root.childCount;
-      child.needLoadSiblings = true;
-
-      await child.loadSiblings(new EmptyLazyLoaderMock());
-
-      expect(root.childCount - oldChildCount).toBe(0); // No new children
-      expect(child.needLoadSiblings).toBe(true); // Still true
-      expect(rootListener).toHaveBeenCalledTimes(0); // No new children added
-
-      // Notified: isLoadingSiblings (on/off) needLoadSiblings(on)
-      expect(childListener).toHaveBeenCalledTimes(3);
-      expect(childListener).toHaveBeenLastCalledWith(child);
-    });
-
-    test('get children with lazy loader', async () => {
-      root.needLoadChildren = true;
-
-      // Since we have to await for the children to load, we need to test that
-      // nothing is return yet
-      const childrenCount = count(root.getChildren(new LazyLoaderMock()));
-      expect(childrenCount).toBe(0);
-      expect(root.children).toBeUndefined();
-
-      // Wait a tick for the children to load
-      await sleep(1);
-
-      // Now the 3 children should be loaded
-      expect(root.children).toHaveLength(3);
-
-      // Notified: isLoadingChildren (on/off) needLoadChildren(on/off) + 3 children = 7
-      expect(rootListener).toHaveBeenCalledTimes(7);
-      expect(rootListener).toHaveBeenLastCalledWith(root);
-    });
-  });
 });
 
 const CHILDREN_COUNT = 5;
 const GRAND_CHILDREN_COUNT = 6;
 
-function addChildren(
+export function addChildren(
   parent: TreeNode,
   alsoAddGrandChildren = false,
   count: number = CHILDREN_COUNT
@@ -464,7 +361,7 @@ function addChildren(
   }
 }
 
-function getLastChild(node: TreeNode): TreeNode | undefined {
+export function getLastChild(node: TreeNode): TreeNode | undefined {
   const { children } = node;
   if (children === undefined || children.length === 0) {
     return undefined;
@@ -472,33 +369,7 @@ function getLastChild(node: TreeNode): TreeNode | undefined {
   return children[children.length - 1];
 }
 
-function getLastGrandChild(node: TreeNode): TreeNode | undefined {
+export function getLastGrandChild(node: TreeNode): TreeNode | undefined {
   const lastChild = getLastChild(node);
   return lastChild === undefined ? undefined : getLastChild(lastChild);
-}
-
-class LazyLoaderMock implements ILazyLoader {
-  root: TreeNodeType | undefined;
-
-  async loadChildren(_node: TreeNodeType): Promise<TreeNodeType[] | undefined> {
-    return [new TreeNode(), new TreeNode(), new TreeNode()]; // 3 children
-  }
-
-  async loadSiblings(_node: TreeNodeType): Promise<TreeNodeType[] | undefined> {
-    return [new TreeNode(), new TreeNode(), new TreeNode(), new TreeNode()]; // 4 siblings
-  }
-
-  onNodeLoaded: OnNodeLoadedAction = vi.fn<OnNodeLoadedAction>();
-}
-
-class EmptyLazyLoaderMock implements ILazyLoader {
-  root: TreeNodeType | undefined;
-
-  async loadChildren(_node: TreeNodeType): Promise<TreeNodeType[] | undefined> {
-    return undefined;
-  }
-
-  async loadSiblings(_node: TreeNodeType): Promise<TreeNodeType[] | undefined> {
-    return undefined;
-  }
 }
