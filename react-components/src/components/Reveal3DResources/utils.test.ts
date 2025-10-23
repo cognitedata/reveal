160f325bb4f19ca0d65e04602ac366357dc50db3
Håkon Flatval <hakon.flatval@cognite.com>
2025-10-23 12:54:15 +0200

    chore: add tests for getInstanceReferencesFromStylingGroup

--- react-components/src/components/Reveal3DResources/utils.test.ts
+++ react-components/src/components/Reveal3DResources/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { getInstanceKeysFromStylingGroup } from './utils';
+import { getInstanceKeysFromStylingGroup, getInstanceReferencesFromStylingGroup } from './utils';
 import { createFdmKey } from '../CacheProvider';
 
 describe(getInstanceKeysFromStylingGroup.name, () => {
@@ -16,5 +16,25 @@ describe(getInstanceKeysFromStylingGroup.name, () => {
     ];
     const result = getInstanceKeysFromStylingGroup({ fdmAssetExternalIds: dmsIdList, style: {} });
     expect(result).toEqual(dmsIdList.map(createFdmKey));
+  });
+});
+
+describe(getInstanceReferencesFromStylingGroup.name, () => {
+  test('returns all ids for classic styling group', () => {
+    const assetIdList = [1, 3, 4, 2];
+    const result = getInstanceReferencesFromStylingGroup({ assetIds: assetIdList, style: {} });
+    expect(result).toEqual(assetIdList);
+  });
+
+  test('returns all ids for dm styling group', () => {
+    const dmsIdList = [
+      { externalId: 'external-id', space: 'space' },
+      { externalId: 'another-external-id', space: 'another-space' }
+    ];
+    const result = getInstanceReferencesFromStylingGroup({
+      fdmAssetExternalIds: dmsIdList,
+      style: {}
+    });
+    expect(result).toEqual(dmsIdList);
   });
 });
