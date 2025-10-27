768ac3bd18c19a1a8cc766f83b06acec2b447fdd
Nils Petter Fremming <35219649+nilscognite@users.noreply.github.com>
2025-10-27 10:24:42 +0100

    Create IconFactory.test.tsx

---
+++ react-components/src/components/Architecture/Factories/IconFactory.test.tsx
@@ -0,0 +1,21 @@
+import { describe, expect, test } from 'vitest';
+import { IconFactory } from './IconFactory';
+import { SnowIcon, DefaultIcon } from '@cognite/cogs.js';
+
+describe(IconFactory.name, () => {
+  test('should get default icons', () => {
+    const Icon = IconFactory.getIcon('NonExistentIcon');
+    expect(Icon).toBe(DefaultIcon);
+  });
+
+  test('should get correct icon', () => {
+    const Icon = IconFactory.getIcon('Snow');
+    expect(Icon).toBe(SnowIcon);
+  });
+
+  test('should install custom icon', () => {
+    IconFactory.install('CustomIcon', () => SnowIcon);
+    const Icon = IconFactory.getIcon('CustomIcon');
+    expect(Icon).toBe(SnowIcon);
+  });
+});
