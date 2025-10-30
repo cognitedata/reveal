f1811a495613a58a381e19b457736cda25496fdf
Daniel Priori <daniel.priori@cognite.com>
2025-10-30 14:52:47 +0100

    cr: use the shared create annotation model mock

--- viewer/test-utilities/src/createCursorAndAsyncIterator.ts
+++ viewer/test-utilities/src/createCursorAndAsyncIterator.ts
@@ -5,7 +5,7 @@
 import { ListResponse, CursorAndAsyncIterator, CogniteAsyncIterator } from '@cognite/sdk';
 import { Mock } from 'moq.ts';
 
-export function createCursorAndAsyncIteratorMock<T>(response: ListResponse<T[]>): CursorAndAsyncIterator<T> {
+export function createCursorAndAsyncIterator<T>(response: ListResponse<T[]>): CursorAndAsyncIterator<T> {
   return Object.assign(Promise.resolve(response), new Mock<CogniteAsyncIterator<T>>().object(), {
     autoPagingToArray: () => response.items
   });
