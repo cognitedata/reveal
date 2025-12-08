365d9bc043fcbb615c12d645360c5e1c35e8952f
Håkon Flatval <hakon.flatval@cognite.com>
2025-12-08 12:54:40 +0100

    chore: add tests

---
+++ viewer/packages/utilities/src/fdm/typeGuards.test.ts
@@ -0,0 +1,27 @@
+import { isDmIdentifier } from './typeGuards';
+
+describe(isDmIdentifier.name, () => {
+  test('recognizes DM identifier', () => {
+    expect(isDmIdentifier({ externalId: 'external-id', space: 'space' })).toBeTrue();
+  });
+
+  test('does not recognize classic asset ID', () => {
+    expect(isDmIdentifier({ id: 123 })).toBeFalse();
+  });
+
+  test('does not recognize null', () => {
+    expect(isDmIdentifier(null)).toBeFalse();
+  });
+
+  test('does not recognize undefined', () => {
+    expect(isDmIdentifier(undefined)).toBeFalse();
+  });
+
+  test('does not recognize numbers', () => {
+    expect(isDmIdentifier(123)).toBeFalse();
+  });
+
+  test('does not recognize empty objects', () => {
+    expect(isDmIdentifier({})).toBeFalse();
+  });
+});
