90559b4623d9abc40c008a18614f0603409c4217
Håkon Flatval <70905152+haakonflatval-cognite@users.noreply.github.com>
2025-12-10 10:14:51 +0100

    fix: generalize type guard (#5466)
    * fix: generalize type guard
    
    * chore: add tests
    
    * chore: lint fix
    
    * chore: update with a test for external IDs too

---
+++ viewer/packages/utilities/src/fdm/typeGuards.test.ts
@@ -0,0 +1,34 @@
+/*!
+ * Copyright 2025 Cognite AS
+ */
+import { isDmIdentifier } from './typeGuards';
+
+describe(isDmIdentifier.name, () => {
+  test('recognizes DM identifier', () => {
+    expect(isDmIdentifier({ externalId: 'external-id', space: 'space' })).toBeTrue();
+  });
+
+  test('does not recognize classic asset internal ID', () => {
+    expect(isDmIdentifier({ id: 123 })).toBeFalse();
+  });
+
+  test('does not recognize classic asset external ID', () => {
+    expect(isDmIdentifier({ externalId: 'classic-external-id' })).toBeFalse();
+  });
+
+  test('does not recognize null', () => {
+    expect(isDmIdentifier(null)).toBeFalse();
+  });
+
+  test('does not recognize undefined', () => {
+    expect(isDmIdentifier(undefined)).toBeFalse();
+  });
+
+  test('does not recognize numbers', () => {
+    expect(isDmIdentifier(123)).toBeFalse();
+  });
+
+  test('does not recognize empty objects', () => {
+    expect(isDmIdentifier({})).toBeFalse();
+  });
+});
