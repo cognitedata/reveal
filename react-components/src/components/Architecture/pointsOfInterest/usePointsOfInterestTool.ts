4db684d244e8590e9a06f0a9bbf64ebffdd2bc0f
Pramod S <pramod.s@cognite.com>
2026-01-09 14:14:07 +0100

    chore: remove react-components from the repository (#5478)
    * deleted react-components code
    
    * removed react-components workflow of CI step, github actions, git ignore, codecov, renovate, gemini styleguide and locize
    
    * removed CI step where changes trigger as now all PR should trigger the CI workflow

--- react-components/src/components/Architecture/pointsOfInterest/usePointsOfInterestTool.ts
+++
@@ -1,7 +0,0 @@
-import { PointsOfInterestTool } from '../../../architecture/concrete/pointsOfInterest/PointsOfInterestTool';
-import { useRenderTarget } from '../../RevealCanvas';
-
-export function usePointsOfInterestTool(): PointsOfInterestTool<unknown> | undefined {
-  const renderTarget = useRenderTarget();
-  return renderTarget.commandsController.getToolByType(PointsOfInterestTool);
-}
--- react-components/src/components/RuleBasedOutputs/constants.ts
+++
@@ -1,8 +0,0 @@
-import { type Source } from '../../data-providers/FdmSDK';
-
-export const RULE_BASED_OUTPUTS_VIEW: Source = {
-  type: 'view',
-  version: '1',
-  externalId: 'FdmRuleOutputSet',
-  space: 'rule_based_outputs_space'
-};
