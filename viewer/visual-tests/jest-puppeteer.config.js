fd919753a86630a38130b384d83ad72b809d6c78
Pramod S <pramod.s@cognite.com>
2026-05-08 09:06:37 +0200

    fixing visual test failing and added headless chorme for mac

--- viewer/visual-tests/jest-puppeteer.config.js
+++ viewer/visual-tests/jest-puppeteer.config.js
@@ -1,12 +1,17 @@
 /*!
  * Copyright 2022 Cognite AS
  */
+const isMac = process.platform === 'darwin';
+
 module.exports = {
   launch: {
     ignoreHTTPSErrors: true,
     // https://peter.sh/experiments/chromium-command-line-switches/
-    args: ['--allow-insecure-localhost', '--window-size=800,600', '--no-sandbox', '--use-gl=swiftshader'],
-    headless: true,
+    // On Linux CI use SwiftShader for software WebGL; on Mac use headed mode since headless Chrome has no GPU access
+    args: isMac
+      ? ['--allow-insecure-localhost', '--window-size=800,600', '--no-sandbox']
+      : ['--allow-insecure-localhost', '--window-size=800,600', '--no-sandbox', '--use-gl=swiftshader'],
+    headless: !isMac,
     defaultViewport: {
       width: 1280,
       height: 720,
