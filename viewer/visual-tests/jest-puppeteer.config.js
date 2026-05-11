eba69c6d13f498e38351b5162e161cdca651c3d6
Pramod S <pramod.s@cognite.com>
2026-05-11 10:33:39 +0200

    reverted Mac headless mode changes

--- viewer/visual-tests/jest-puppeteer.config.js
+++ viewer/visual-tests/jest-puppeteer.config.js
@@ -1,17 +1,12 @@
 /*!
  * Copyright 2022 Cognite AS
  */
-const isMac = process.platform === 'darwin';
-
 module.exports = {
   launch: {
     ignoreHTTPSErrors: true,
     // https://peter.sh/experiments/chromium-command-line-switches/
-    // On Linux CI use SwiftShader for software WebGL; on Mac use headed mode since headless Chrome has no GPU access
-    args: isMac
-      ? ['--allow-insecure-localhost', '--window-size=800,600', '--no-sandbox']
-      : ['--allow-insecure-localhost', '--window-size=800,600', '--no-sandbox', '--use-gl=swiftshader'],
-    headless: !isMac,
+    args: ['--allow-insecure-localhost', '--window-size=800,600', '--no-sandbox', '--use-gl=swiftshader'],
+    headless: true,
     defaultViewport: {
       width: 1280,
       height: 720,
