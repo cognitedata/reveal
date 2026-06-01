df0a264ec77bb41028c3756b5f12918c88f6e49a
Håkon Flatval <hakon.flatval@cognite.com>
2026-06-01 15:30:48 +0200

    chore: WIP cache test

--- viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageAnnotationCache.test.ts
+++ viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageAnnotationCache.test.ts
@@ -1,11 +1,34 @@
 import { Mock } from 'moq.ts';
 import { Cdf360ImageAnnotationCache } from './Cdf360ImageAnnotationCache';
-import { CogniteClient } from '@cognite/sdk';
+import { AnnotationsAPI, CogniteClient } from '@cognite/sdk';
+import { InstanceReference } from '../types';
+import { ClassicDataSourceType, DMDataSourceType } from '../DataSourceType';
+import { createCursorAndAsyncIterator } from '../../../../test-utilities';
+
+import { jest } from '@jest/globals';
 
 describe(Cdf360ImageAnnotationCache, () => {
-  const mockSdk = new Mock<CogniteClient>().object();
+  const CLASSIC_ID: InstanceReference<ClassicDataSourceType> = { id: 123 };
+  const DM_ID: InstanceReference<DMDataSourceType> = { externalId: 'external-id', space: 'space' };
 
-  test('annotation cache', () => {
+  const mockReverseLookup = jest.fn<AnnotationsAPI['reverseLookup']>();
+
+  const mockSdk = new Mock<CogniteClient>()
+    .setup(p => p.annotations.reverseLookup)
+    .returns(mockReverseLookup)
+    .object();
+
+  test('caches reverse-lookup result', async () => {
+    const fileResult = { items: [{ id: 987 }] };
+    mockReverseLookup.mockReturnValue(createCursorAndAsyncIterator(fileResult));
+
     const cache = new Cdf360ImageAnnotationCache(mockSdk);
+
+    const result0 = await cache.reverseLookup(CLASSIC_ID);
+    const result1 = await cache.reverseLookup(DM_ID);
+
+    expect(result0).toEqual(fileResult.items);
+    expect(result0).toEqual(result1);
+    expect(mockReverseLookup).toHaveBeenCalledTimes(1);
   });
 });
