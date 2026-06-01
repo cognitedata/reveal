776470d053a903bd2007559f3803ec519d53630e
Håkon Flatval <hakon.flatval@cognite.com>
2026-06-01 16:25:15 +0200

    chore: add final test

--- viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageAnnotationCache.test.ts
+++ viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageAnnotationCache.test.ts
@@ -1,26 +1,35 @@
 import { Mock } from 'moq.ts';
 import { Cdf360ImageAnnotationCache } from './Cdf360ImageAnnotationCache';
-import { AnnotationsAPI, CogniteClient } from '@cognite/sdk';
+import { AnnotationsAPI, CogniteClient, FilesAPI } from '@cognite/sdk';
 import { InstanceReference } from '../types';
-import { ClassicDataSourceType, DMDataSourceType } from '../DataSourceType';
+import { ClassicDataSourceType } from '../DataSourceType';
 import { createAnnotationModel, createCursorAndAsyncIterator } from '../../../../test-utilities';
+import { createFileInfo } from '../../../../test-utilities/src/createFileInfo';
 
 describe(Cdf360ImageAnnotationCache, () => {
   const CLASSIC_ID: InstanceReference<ClassicDataSourceType> = { id: 123 };
-  const DM_ID: InstanceReference<DMDataSourceType> = { externalId: 'external-id', space: 'space' };
+
+  const FILE_ID = { id: 987 };
 
   const mockReverseLookup = vi.fn<AnnotationsAPI['reverseLookup']>();
   const mockAnnotationList = vi.fn<AnnotationsAPI['list']>();
+  const mockFileRetrieve = vi.fn<FilesAPI['retrieve']>();
 
   const mockSdk = new Mock<CogniteClient>()
     .setup(p => p.annotations.reverseLookup)
     .returns(mockReverseLookup)
     .setup(p => p.annotations.list)
     .returns(mockAnnotationList)
+    .setup(p => p.files.retrieve)
+    .returns(mockFileRetrieve)
     .object();
 
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
   test('caches reverse-lookup result', async () => {
-    const fileResult = { items: [{ id: 987 }] };
+    const fileResult = { items: [FILE_ID] };
     const resultAnnotations = { items: [createAnnotationModel({})] };
     mockReverseLookup.mockReturnValue(createCursorAndAsyncIterator(fileResult));
     mockAnnotationList.mockReturnValue(createCursorAndAsyncIterator(resultAnnotations));
@@ -44,5 +53,33 @@ describe(Cdf360ImageAnnotationCache, () => {
       filter: { annotatedResourceIds: fileResult.items, annotatedResourceType: 'file' },
       limit: 1000
     });
+  });
+
+  test('caches get-annotations-for-files result', async () => {
+    const resultAnnotations = { items: [createAnnotationModel({ annotatedResourceId: FILE_ID.id })] };
+    mockAnnotationList.mockReturnValue(createCursorAndAsyncIterator(resultAnnotations));
+
+    const cache = new Cdf360ImageAnnotationCache(mockSdk);
+
+    const result0 = await cache.getAnnotationsForFiles([FILE_ID]);
+    const result1 = await cache.getAnnotationsForFiles([FILE_ID]);
+
+    expect(result0).toEqual(resultAnnotations.items);
+    expect(result0).toEqual(result1);
+    expect(mockAnnotationList).toHaveBeenCalledExactlyOnceWith({
+      filter: { annotatedResourceIds: [FILE_ID], annotatedResourceType: 'file' },
+      limit: 1000
+    });
+  });
+
+  test('caches get-file-infos-for-file-ids result', async () => {
+    const resultFiles = [createFileInfo(FILE_ID)];
+    mockFileRetrieve.mockResolvedValue(resultFiles);
+
+    const cache = new Cdf360ImageAnnotationCache(mockSdk);
+
+    const result0 = await cache.getFileInfosForFileIds([FILE_ID]);
+
+    expect(result0).toEqual(resultFiles);
   });
 });
