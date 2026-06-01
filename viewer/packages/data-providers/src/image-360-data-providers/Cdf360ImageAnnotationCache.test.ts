9a3fc7b93b4927ed8d761e895a268da1343babfb
Håkon Flatval <hakon.flatval@cognite.com>
2026-06-01 15:39:45 +0200

    chore: test for reverse lookup

--- viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageAnnotationCache.test.ts
+++ viewer/packages/data-providers/src/image-360-data-providers/Cdf360ImageAnnotationCache.test.ts
@@ -3,32 +3,36 @@ import { Cdf360ImageAnnotationCache } from './Cdf360ImageAnnotationCache';
 import { AnnotationsAPI, CogniteClient } from '@cognite/sdk';
 import { InstanceReference } from '../types';
 import { ClassicDataSourceType, DMDataSourceType } from '../DataSourceType';
-import { createCursorAndAsyncIterator } from '../../../../test-utilities';
-
-import { jest } from '@jest/globals';
+import { createAnnotationModel, createCursorAndAsyncIterator } from '../../../../test-utilities';
 
 describe(Cdf360ImageAnnotationCache, () => {
   const CLASSIC_ID: InstanceReference<ClassicDataSourceType> = { id: 123 };
   const DM_ID: InstanceReference<DMDataSourceType> = { externalId: 'external-id', space: 'space' };
 
-  const mockReverseLookup = jest.fn<AnnotationsAPI['reverseLookup']>();
+  const mockReverseLookup = vi.fn<AnnotationsAPI['reverseLookup']>();
+  const mockAnnotationList = vi.fn<AnnotationsAPI['list']>();
 
   const mockSdk = new Mock<CogniteClient>()
     .setup(p => p.annotations.reverseLookup)
     .returns(mockReverseLookup)
+    .setup(p => p.annotations.list)
+    .returns(mockAnnotationList)
     .object();
 
   test('caches reverse-lookup result', async () => {
     const fileResult = { items: [{ id: 987 }] };
+    const resultAnnotations = { items: [createAnnotationModel({})] };
     mockReverseLookup.mockReturnValue(createCursorAndAsyncIterator(fileResult));
+    mockAnnotationList.mockReturnValue(createCursorAndAsyncIterator(resultAnnotations));
 
     const cache = new Cdf360ImageAnnotationCache(mockSdk);
 
     const result0 = await cache.reverseLookup(CLASSIC_ID);
-    const result1 = await cache.reverseLookup(DM_ID);
+    const result1 = await cache.reverseLookup(CLASSIC_ID);
 
-    expect(result0).toEqual(fileResult.items);
+    expect(result0).toEqual(resultAnnotations.items);
     expect(result0).toEqual(result1);
     expect(mockReverseLookup).toHaveBeenCalledTimes(1);
+    expect(mockAnnotationList).toHaveBeenCalledTimes(1);
   });
 });
