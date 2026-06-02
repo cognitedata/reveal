2b5f5b63b8361c1a15cff2127d7c86dea127e3a9
Christopher  Tannum <christopher.tannum@cognitedata.com>
2026-06-02 11:10:57 +0200

    chore(viewer): add consistent type import/export eslint rule

--- viewer/.eslintrc.js
+++ viewer/.eslintrc.js
@@ -71,6 +71,8 @@ module.exports = {
     '@typescript-eslint/prefer-for-of': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/consistent-type-exports": "error",
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
