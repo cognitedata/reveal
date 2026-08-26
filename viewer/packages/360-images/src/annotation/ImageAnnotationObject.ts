63b8de69815bbcb3062001ef882e957dbbf91799
Pramod S <pramod.s@cognite.com>
2026-08-26 14:26:35 +0200

    fixed failing test

--- viewer/packages/360-images/src/annotation/ImageAnnotationObject.ts
+++ viewer/packages/360-images/src/annotation/ImageAnnotationObject.ts
@@ -164,6 +164,7 @@ export class ImageAnnotationObject<T extends DataSourceType> implements Image360
     const transformation = rotationMatrix.clone().multiply(normalizationTransform);
     this._objectGroup.matrix = transformation;
     this._objectGroup.matrixAutoUpdate = false;
+    this._objectGroup.matrixWorldNeedsUpdate = true;
     this._objectGroup.updateWorldMatrix(false, true);
   }
 
